7c40c724370c9c3c4190c28bc90d167e0be60cd9
Scott Penrose <scottp@dd.com.au>
2005-07-18 03:20:43 +0000

    Fix image etc

--- doc/html/wrap.pl
+++ doc/html/wrap.pl
@@ -1,7 +1,4 @@
 #!/usr/bin/perl
-use warnings;
-use strict;
-
 my $title = shift || "unknown";
 
 print qq{<?xml version="1.0" encoding="UTF-8"?>
@@ -27,12 +24,7 @@ exit 0;
 sub xml_entify {
         my ($str, $no_escape_tilde) = @_;
 	return undef unless(defined($str));
-        my $test = $str . chr(2422) . "";
-        $test = substr($test, 0, length($test) - 1) . "";
-
         $test =~ s/[\x00-\x08\x10\x0B\x0C\x0E-\x1F]//g;
-        # NOTE - Is it necessary to entify UniCode
-        #       Should not be requried, definitely not for Perl 5.8
         $test =~ s/([&<>"\x{80}-\x{10ffff}])/"&#".ord($1).";"/eg;
         $test =~ s/~/~~/g unless $no_escape_tilde;
         return $test;
