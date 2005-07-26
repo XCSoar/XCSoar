98c11388263d5b136156d9ebb3866dcf9da73d41
John Wharington <jwharington@gmail.com>
2005-07-26 16:40:23 +0000

    minor adjustments to build

--- BuildAll.bat
+++ BuildAll.bat
@@ -2,6 +2,52 @@
 set EVC="%ProgramFiles%\Microsoft eMbedded Tools\Common\EVC\Bin\EVC.EXE"
 set EVC4="%ProgramFiles%\Microsoft eMbedded C++ 4.0\Common\EVC\Bin\EVC.EXE"
 
+:PPC2003
+
+%EVC4%  PPC2003/XCSoar/XCSoar.vcp /MAKE ALL /REBUILD
+%EVC4%  PPC2003/XCSoarSimulator/XCSoarSimulator.vcp /MAKE ALL /REBUILD
+%EVC4%  PPC2003/XCSoarLaunch/XCSoarLaunch.vcp /MAKE ALL /REBUILD
+
+echo Build: PPC2003 installer
+call buildcab.bat
+pause
+
+
+:PPC2002
+
+echo ARM > processor.lst
+echo XCSoar > project.lst
+echo XCSoarSimulator >> project.lst
+echo XCSoarLaunch >> project.lst
+echo XCSoarSetup >> project.lst
+
+for /F %%j in (processor.lst) do (
+
+echo Build: PPC2002 %%j *******************
+
+for /F %%i in (project.lst) do (
+
+  echo        %%i
+  %EVC%  PPC2002/%%i/%%i.vcp /MAKE "%%i - Win32 (WCE %%j) Release" /ceconfig="Pocket PC 2002" /REBUILD
+  if errorlevel 1 echo error build %%k/%%i/%%i.vcp
+
+)
+)
+
+del project.lst
+del processor.lst
+
+
+echo Build: PPC2002 installer
+call buildcab2002.bat
+pause
+
+:Data
+
+echo Build: Data installer
+call buildcabdata.bat
+pause
+
 :PPC
 
 echo MIPS > processor.lst
@@ -31,50 +77,9 @@ del processor.lst
 
 echo Build: PPC installer
 call buildcabPPC.bat
-
-
-:PPC2000
-
-echo ARM > processor.lst
-
-echo XCSoar > project.lst
-echo XCSoarSimulator >> project.lst
-echo XCSoarLaunch >> project.lst
-echo XCSoarSetup >> project.lst
-
-for /F %%j in (processor.lst) do (
-
-echo Build: PPC2002 %%j *******************
-
-for /F %%i in (project.lst) do (
-
-  echo        %%i
-  %EVC%  PPC2002/%%i/%%i.vcp /MAKE "%%i - Win32 (WCE %%j) Release" /ceconfig="Pocket PC 2002" /REBUILD
-  if errorlevel 1 echo error build %%k/%%i/%%i.vcp
-
-)
-)
-
-del project.lst
-del processor.lst
-
-
-echo Build: PPC2002 installer
-call buildcab2002.bat
-
-goto end
-
-pause
-
-%EVC4%  PPC2003/XCSoar/XCSoar.vcp /MAKE ALL /REBUILD
-%EVC4%  PPC2003/XCSoarSimulator/XCSoarSimulator.vcp /MAKE ALL /REBUILD
-
-%EVC4%  SmartPhone2003/XCSoar/XCSoar.vcp /MAKE ALL /REBUILD
-%EVC4%  SmartPhone2003/XCSoarSimulator/XCSoarSimulator.vcp /MAKE ALL /REBUILD
-
-
 pause
 
 :error
 
 :end
+
--- buildcab.bat
+++ buildcab.bat
@@ -1,15 +1,7 @@
 
-"C:\Program Files\Windows CE Tools\wce420\POCKET PC 2003\Tools\Cabwiz.exe" XCSoarData.inf
-
-ezsetup -l english -i XCSoarData.ini -r installmsgdata.txt -e gpl.txt -o InstallXCSoar-Data.exe
-
-
-pause
-
 
 "C:\Program Files\Windows CE Tools\wce420\POCKET PC 2003\Tools\Cabwiz.exe" XCSoar.inf /cpu armv4
 
 ezsetup -l english -i XCSoar.ini -r installmsg.txt -e gpl.txt -o InstallXCSoar-ARM2003.exe
 
-pause
 
