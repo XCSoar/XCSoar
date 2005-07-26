8a03de809ea081294935f5d90bad9118d0c6e37c
John Wharington <jwharington@gmail.com>
2005-07-26 16:40:23 +0000

    minor adjustments to build

---
+++ buildcabdata.bat
@@ -0,0 +1,5 @@
+
+"C:\Program Files\Windows CE Tools\wce420\POCKET PC 2003\Tools\Cabwiz.exe" XCSoarData.inf
+
+ezsetup -l english -i XCSoarData.ini -r installmsgdata.txt -e gpl.txt -o InstallXCSoar-Data.exe 
+
