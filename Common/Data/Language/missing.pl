5d1becdf0546ad313cadac6ef6687613b122e475
Scott Penrose <scottp@dd.com.au>
2005-08-11 06:43:23 +0000

    *** empty log message ***

---
+++ Common/Data/Language/missing.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+#
+# Find entries in template that are missing from other files
+#
+
+open (T, "template.vcl");
+my %data = ();
+while (<T>) {
+	chomp;
+	next if (/^#/);
+	s/=.*$//;
+	$data{$_} = 0;
+}
+
+while (<>) {
+	chomp;
+	next if (/^#/);
+	s/=.*$//;
+	$data{$_} = 1;
+}
+
+foreach my $key (sort keys %data) {
+	print "$key=\n" if ($data{$key} == 0);
+}
