ffebac8f19c254cf0a89f5961b9bb0309312b1f8
Scott Penrose <scottp@dd.com.au>
2005-08-31 07:36:01 +0000

    New headers

--- Common/Data/Input/h2cpp.pl
+++ Common/Data/Input/h2cpp.pl
@@ -5,6 +5,9 @@ use warnings;
 print "/* AUTOMATICALLY GENERATED FILE - DO NOT EDIT BY HAND - see Common/Data/Input/h2cpp.pl */\n";
 open (IN, "../../Header/InputEvents.h");
 my $count = 0;
+
+my $buffer = "";
+my $gce_count = 0;
 while (<IN>) {
 	chomp;
 	
@@ -12,6 +15,9 @@ while (<IN>) {
 		print qq{Text2Event[$count].text = TEXT("$1");\n};
 		print qq{Text2Event[$count].event = &event$1;\n};
 		$count++;
+	} elsif (/(GCE_[A-Z0-9_]+)/) {
+		print qq{Text2GCE[$gce_count] = TEXT("$1");\n};
+		$gce_count++;
 	}
 }
 
