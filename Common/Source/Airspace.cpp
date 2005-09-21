af5f97c5d3c81d887f3552d6ae51571d2e889806
Scott Penrose <scottp@dd.com.au>
2005-09-21 00:32:49 +0000

    Include new copyright notices

--- Common/Source/Airspace.cpp
+++ Common/Source/Airspace.cpp
@@ -1,6 +1,13 @@
 /*
-  XCSoar Glide Computer
-  Copyright (C) 2000 - 2004  M Roberts
+
+Copyright_License {
+
+  XCSoar Glide Computer - http://xcsoar.sourceforge.net/
+  Copyright (C) 2000 - 2005
+
+  	M Roberts
+	Scott Penrose <scottp@dd.com.au>
+	John Wharington <jwharington@bigfoot.com>
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
@@ -15,6 +22,9 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+}
+
 */
 
 #include "stdafx.h"
--- Common/Source/units.cpp
+++ Common/Source/units.cpp
@@ -1,6 +1,12 @@
 /*
-  XCSoar Glide Computer
-  Copyright (C) 2000 - 2004  M Roberts
+Copyright_License {
+
+  XCSoar Glide Computer - http://xcsoar.sourceforge.net/
+  Copyright (C) 2000 - 2005
+
+  	M Roberts
+	Scott Penrose <scottp@dd.com.au>
+	John Wharington <jwharington@bigfoot.com>
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
@@ -16,6 +22,7 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+}
 */
 
 // TODO
