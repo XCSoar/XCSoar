62bd826db0a81417637275f7d6bf760c200e33ee
Alastair Harrison <aharrison@magic.force9.co.uk>
2006-06-28 19:56:15 +0000

    Added files needed for GPS-NAV support.  Forgot to add them before.

---
+++ Common/Header/devCaiGpsNav.h
@@ -0,0 +1,15 @@
+#ifndef	DEVCAIGPSNAV_H
+#define	DEVCAIGPSNAV_H
+
+#include <windows.h>
+#include "sizes.h"
+#include "MapWindow.h"
+
+#include "device.h"
+
+BOOL caiGpsNavInstall(PDeviceDescriptor_t d);
+BOOL caiGpsNavRegister(void);
+
+
+#endif
+
