fb18dd802a5a6c27f304c57ba4cb2f18111a57de
Alastair Harrison <aharrison@magic.force9.co.uk>
2006-06-28 19:56:15 +0000

    Added files needed for GPS-NAV support.  Forgot to add them before.

---
+++ Common/Source/devCaiGpsNav.cpp
@@ -0,0 +1,136 @@
+// $Id: devCaiGpsNav.cpp,v 1.1 2006/06/28 19:56:15 aharrison24 Exp $
+
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://xcsoar.sourceforge.net/
+  Copyright (C) 2000 - 2005  
+
+  	M Roberts (original release)
+	Robin Birch <robinb@ruffnready.co.uk>
+	Samuel Gisiger <samuel.gisiger@triadis.ch>
+	Jeff Goodenough <jeff@enborne.f2s.com>
+	Alastair Harrison <aharrison@magic.force9.co.uk>
+	Scott Penrose <scottp@dd.com.au>
+	John Wharington <jwharington@bigfoot.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+}
+*/
+
+//
+
+
+// CAUTION!
+// caiGpsNavParseNMEA is called from com port read thread
+// all other functions are called from windows message loop thread
+
+
+#define  LOGSTREAM 0
+
+
+#include <windows.h>
+#include <tchar.h>
+
+
+#include "externs.h"
+#include "utils.h"
+#include "parser.h"
+#include "port.h"
+
+#include "devCaiGpsNav.h"
+
+#ifdef _SIM_
+static BOOL fSimMode = TRUE;
+#else
+static BOOL fSimMode = FALSE;
+#endif
+
+
+#define  CtrlC  0x03
+#define  swap(x)      x = ((((x<<8) & 0xff00) | ((x>>8) & 0x00ff)) & 0xffff)
+
+
+// Additional sentance for CAI302 support
+static BOOL cai_w(PDeviceDescriptor_t d, TCHAR *String, NMEA_INFO *GPS_INFO);
+static BOOL cai_PCAIB(TCHAR *String, NMEA_INFO *GPS_INFO);
+static BOOL cai_PCAID(TCHAR *String, NMEA_INFO *GPS_INFO);
+
+
+
+BOOL caiGpsNavOpen(PDeviceDescriptor_t d, int Port){
+
+  d->Port = Port;
+
+  if (!fSimMode){
+    (d->Com.WriteString)(TEXT("\x03"));
+    (d->Com.WriteString)(TEXT("PNP\r\n"));
+    (d->Com.WriteString)(TEXT("LOG 0\r\n"));
+  }
+
+  return(TRUE);
+}
+
+BOOL caiGpsNavClose(PDeviceDescriptor_t d){
+
+  return(TRUE);
+}
+
+
+
+
+BOOL caiGpsNavIsLogger(PDeviceDescriptor_t d){
+  // There is currently no support for task declaration
+  // from XCSoar
+  return(FALSE);
+}
+
+
+BOOL caiGpsNavIsGPSSource(PDeviceDescriptor_t d){
+  return(TRUE);
+}
+
+
+BOOL caiGpsNavInstall(PDeviceDescriptor_t d){
+
+  _tcscpy(d->Name, TEXT("CAI GPS-NAV"));
+  d->ParseNMEA = NULL;
+  d->PutMacCready = NULL;
+  d->PutBugs = NULL;
+  d->PutBallast = NULL;
+  d->Open = caiGpsNavOpen;
+  d->Close = caiGpsNavClose;
+  d->Init = NULL;
+  d->LinkTimeout = NULL;
+  d->DeclBegin = NULL;
+  d->DeclEnd = NULL;
+  d->DeclAddWayPoint = NULL;
+  d->IsLogger = caiGpsNavIsLogger;
+  d->IsGPSSource = caiGpsNavIsGPSSource;
+
+  return(TRUE);
+
+}
+
+
+BOOL caiGpsNavRegister(void){
+  return(devRegister(
+    TEXT("CAI GPS-NAV"), 
+    (1l << dfGPS),
+    caiGpsNavInstall
+  ));
+}
+
