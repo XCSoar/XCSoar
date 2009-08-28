e899475354d363e7884ee354db447b4782001378
Max Kellermann <max@duempel.org>
2009-08-28 18:18:37 +0200

    Trigger: implemented workaround for bugged SetEvent(), ResetEvent()
    In mingw32ce 0.55, SetEvent() and ResetEvent() are implemented in a
    very wrong way: they use both EVENT_PULSE.  This patch implementes a
    workaround, and calls EventModify() manually.

--- Common/Header/Trigger.hpp
+++ Common/Header/Trigger.hpp
@@ -100,7 +100,13 @@ public:
    * trigger is reset only if a thread was really woken up.
    */
   void trigger() {
+#if defined(_WIN32_WCE) && defined(__MINGW32__) && defined(EVENT_SET)
+    /* mingw32ce < 0.59 has a bugged SetEvent() implementation in
+       kfuncs.h */
+    ::EventModify(handle, EVENT_SET);
+#else
     ::SetEvent(handle);
+#endif
   }
 
   /**
@@ -114,7 +120,13 @@ public:
    * Resets the trigger
    */
   void reset() {
+#if defined(_WIN32_WCE) && defined(__MINGW32__) && defined(EVENT_RESET)
+    /* mingw32ce < 0.59 has a bugged SetEvent() implementation in
+       kfuncs.h */
+    ::EventModify(handle, EVENT_RESET);
+#else
     ::ResetEvent(handle);
+#endif
   }
 
 };
