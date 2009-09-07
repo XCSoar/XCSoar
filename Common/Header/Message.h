2b32866f23b458bafe226f4a339dc8e06eb09d1d
Max Kellermann <max@duempel.org>
2009-09-07 23:07:29 +0200

    Message: put code into PopupMessage, derived from EditWindow
    First patch to make Message.cpp object oriented.

--- Common/Header/Message.h
+++ Common/Header/Message.h
@@ -38,71 +38,54 @@ Copyright_License {
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-#include "Interface.hpp"
-#include "Thread/Mutex.hpp"
-#include "Screen/EditWindow.hpp"
-
-#define MAXMESSAGES 20
-
-struct singleMessage {
-  TCHAR text[1000];
-  int type;
-  DWORD tstart; // time message was created
-  DWORD texpiry; // time message will expire
-  DWORD tshow; // time message is visible for
-};
+#include "PopupMessage.hpp"
 
 class StatusMessageList;
 
-class Message: public CommonInterface {
+class Message : public PopupMessage {
   static StatusMessageList status_messages;
-  static EditWindow window;
+  static PopupMessage *instance;
 
  public:
-
-  enum {
-    MSG_UNKNOWN=0,
-    MSG_AIRSPACE=1,
-    MSG_USERINTERFACE=2,
-    MSG_GLIDECOMPUTER=3,
-    MSG_COMMS=4
-  };
-
   static void Initialize(RECT rc);
-  static void Destroy();
-  static bool Render(); // returns true if messages have changed
 
-  static void AddMessage(DWORD tshow, int type, TCHAR *Text);
-  static void AddMessage(const TCHAR* text, const TCHAR *data=NULL);
+  static void Destroy();
+
+  static bool Render() {
+    return instance->Render();
+  }
+
+  static void AddMessage(DWORD tshow, int type, TCHAR *Text) {
+    instance->AddMessage(tshow, type, Text);
+  }
+
+  static void AddMessage(const TCHAR* text, const TCHAR *data=NULL) {
+    instance->AddMessage(text, data);
+  }
 
   // repeats last non-visible message of specified type (or any message
   // type=0)
-  static void Repeat(int type);
+  static void Repeat(int type) {
+    instance->Repeat(type);
+  }
 
   // clears all visible messages (of specified type or if type=0, all)
-  static bool Acknowledge(int type);
+  static bool Acknowledge(int type) {
+    return instance->Acknowledge(type);
+  }
 
-  static void CheckTouch(HWND wmControl);
+  static void CheckTouch(HWND wmControl) {
+    instance->CheckTouch(wmControl);
+  }
 
-  static void BlockRender(bool doblock);
+  static void BlockRender(bool doblock) {
+    instance->BlockRender(doblock);
+  }
 
   // used by main
   static void Startup(bool first);
   static void LoadFile(void);
   static void InitFile(void);
-
- private:
-  static struct singleMessage messages[MAXMESSAGES];
-  static RECT rcmsg; // maximum message size
-  static TCHAR msgText[2000];
-  static void Resize();
-  static int GetEmptySlot();
-  static bool hidden;
-  static int nvisible;
-  static int block_ref;
-  static Mutex mutexMessage;
-  static void Lock();
-  static void Unlock();
 };
 
 #endif
---
+++ Common/Source/PopupMessage.cpp
@@ -0,0 +1,360 @@
+/*
+
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000 - 2009
+
+	M Roberts (original release)
+	Robin Birch <robinb@ruffnready.co.uk>
+	Samuel Gisiger <samuel.gisiger@triadis.ch>
+	Jeff Goodenough <jeff@enborne.f2s.com>
+	Alastair Harrison <aharrison@magic.force9.co.uk>
+	Scott Penrose <scottp@dd.com.au>
+	John Wharington <jwharington@gmail.com>
+	Lars H <lars_hn@hotmail.com>
+	Rob Dunning <rob@raspberryridgesheepfarm.com>
+	Russell King <rmk@arm.linux.org.uk>
+	Paolo Ventafridda <coolwind@email.it>
+	Tobias Lohner <tobias@lohner-net.de>
+	Mirek Jezek <mjezek@ipplc.cz>
+	Max Kellermann <max@duempel.org>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+
+*/
+
+#include "PopupMessage.hpp"
+#include "Protection.hpp"
+#include "InfoBoxLayout.h"
+#include "Screen/Fonts.hpp"
+#include "Screen/ContainerWindow.hpp"
+#include "LocalPath.hpp"
+#include "Registry.hpp"
+#include "UtilsText.hpp"
+#include "Audio/Sound.hpp"
+#include "LogFile.hpp"
+#include "SettingsComputer.hpp"
+#include "Language.hpp"
+#include "StatusMessage.hpp"
+
+#include <tchar.h>
+#include <stdio.h>
+
+PopupMessage::PopupMessage(const StatusMessageList &_status_messages,
+                           ContainerWindow &_parent)
+  :startTime(::GetTickCount()), status_messages(_status_messages),
+   parent(_parent), hidden(true),
+   nvisible(0), block_ref(0)
+{
+  for (unsigned i = 0; i < MAXMESSAGES; i++) {
+    messages[i].text[0]= _T('\0');
+    messages[i].tstart = 0;
+    messages[i].texpiry = 0;
+    messages[i].type = 0;
+  }
+}
+
+void
+PopupMessage::set(const RECT _rc)
+{
+  rc = _rc;
+  set_ro_ml(parent, rc.left, rc.top,
+            rc.right - rc.left, rc.bottom - rc.top);
+  set_font(MapWindowBoldFont);
+}
+
+void PopupMessage::Resize() {
+  int size = _tcslen(msgText);
+  RECT rthis;
+  //  RECT mRc;
+
+  if (size==0) {
+    if (!hidden) {
+      hide();
+
+      // animation
+      //      GetWindowRect(hWndMessageWindow, &mRc);
+      //      SetSourceRectangle(mRc);
+      //      mRc.top=0; mRc.bottom=0;
+      //      DrawWireRects(&mRc, 5);
+    }
+    hidden = true;
+  } else {
+    set_text(msgText);
+    SIZE tsize = parent.get_canvas().text_size(msgText);
+
+    int linecount = max((unsigned)nvisible, max(1, get_row_count()));
+
+    int width =// min((rc.right-rc.left)*0.8,tsize.cx);
+      (int)((rc.right-rc.left)*0.9);
+    int height = (int)min((rc.bottom-rc.top)*0.8,tsize.cy*(linecount+1));
+    int h1 = height/2;
+    int h2 = height-h1;
+
+    int midx = (rc.right+rc.left)/2;
+    int midy = (rc.bottom+rc.top)/2;
+
+    if (Appearance.StateMessageAlign == smAlignTopLeft){
+      rthis.top = 0;
+      rthis.left = 0;
+      rthis.bottom = height;
+      rthis.right = 206*InfoBoxLayout::scale;
+      // TODO code: this shouldn't be hard-coded
+    } else {
+      rthis.left = midx-width/2;
+      rthis.right = midx+width/2;
+      rthis.top = midy-h1;
+      rthis.bottom = midy+h2;
+    }
+    /*
+    if (hidden) {
+      RECT bigrect;
+      GetWindowRect(hWndMapWindow, &bigrect);
+      GetWindowRect(hWndMessageWindow, &mRc);
+      bigrect.bottom= bigrect.top;
+      SetSourceRectangle(mRc);
+      DrawWireRects(&mRc, 10);
+    }
+    */
+
+    move(rthis.left, rthis.top,
+         rthis.right - rthis.left,
+         rthis.bottom - rthis.top);
+    bring_to_top();
+    show();
+    hidden = false;
+  }
+
+}
+
+
+void PopupMessage::BlockRender(bool doblock) {
+  Lock();
+  if (doblock) {
+    block_ref++;
+  } else {
+    block_ref--;
+  }
+  // TODO code: add blocked time to messages' timers so they come
+  // up once unblocked.
+  Unlock();
+}
+
+
+bool PopupMessage::Render() {
+  if (!globalRunningEvent.test()) return false;
+
+  Lock();
+  if (block_ref) return false;
+  DWORD	fpsTime = ::GetTickCount() - startTime;
+
+  // this has to be done quickly, since it happens in GUI thread
+  // at subsecond interval
+
+  // first loop through all messages, and determine which should be
+  // made invisible that were previously visible, or
+  // new messages
+
+  bool changed = false;
+  int i;
+  for (i=0; i<MAXMESSAGES; i++) {
+    if (messages[i].type==0) continue; // ignore unknown messages
+
+    if (
+	(messages[i].texpiry <= fpsTime)
+	&&(messages[i].texpiry> messages[i].tstart)
+	) {
+      // this message has expired for first time
+      changed = true;
+      continue;
+    }
+
+    // new message has been added
+    if (messages[i].texpiry== messages[i].tstart) {
+      // set new expiry time.
+      messages[i].texpiry = fpsTime + messages[i].tshow;
+      // this is a new message..
+      changed = true;
+    }
+
+  }
+
+  static bool doresize= false;
+
+  if (!changed) {
+    if (doresize) {
+      doresize = false;
+      // do one extra resize after display so we are sure we get all
+      // the text (workaround bug in getlinecount)
+      Resize();
+    }
+    Unlock(); return false;
+  }
+
+  // ok, we've changed the visible messages, so need to regenerate the
+  // text box
+
+  doresize = true;
+  msgText[0]= 0;
+  nvisible=0;
+  for (i=0; i<MAXMESSAGES; i++) {
+    if (messages[i].type==0) continue; // ignore unknown messages
+
+    if (messages[i].texpiry< fpsTime) {
+      messages[i].texpiry = messages[i].tstart-1;
+      // reset expiry so we don't refresh
+      continue;
+    }
+
+    _tcscat(msgText, messages[i].text);
+    _tcscat(msgText, TEXT("\r\n"));
+    nvisible++;
+
+  }
+  Resize();
+
+  Unlock();
+  return true;
+}
+
+
+
+int PopupMessage::GetEmptySlot() {
+  // find oldest message that is no longer visible
+
+  // todo: make this more robust with respect to message types and if can't
+  // find anything to remove..
+  int i;
+  DWORD tmin=0;
+  int imin=0;
+  for (i=0; i<MAXMESSAGES; i++) {
+    if ((i==0) || (messages[i].tstart<tmin)) {
+      tmin = messages[i].tstart;
+      imin = i;
+    }
+  }
+  return imin;
+}
+
+
+void PopupMessage::AddMessage(DWORD tshow, int type, TCHAR* Text) {
+  ScopeLock protect(mutex);
+
+  int i;
+  DWORD	fpsTime = ::GetTickCount() - startTime;
+  i = GetEmptySlot();
+
+  messages[i].type = type;
+  messages[i].tshow = tshow;
+  messages[i].tstart = fpsTime;
+  messages[i].texpiry = fpsTime;
+  _tcscpy(messages[i].text, Text);
+
+}
+
+void PopupMessage::Repeat(int type) {
+  int i;
+  DWORD tmax=0;
+  int imax= -1;
+
+  Lock();
+
+  DWORD	fpsTime = ::GetTickCount() - startTime;
+
+  // find most recent non-visible message
+
+  for (i=0; i<MAXMESSAGES; i++) {
+
+    if ((messages[i].texpiry < fpsTime)
+	&&(messages[i].tstart > tmax)
+	&&((messages[i].type == type)|| (type==0))) {
+      imax = i;
+      tmax = messages[i].tstart;
+    }
+
+  }
+
+  if (imax>=0) {
+    messages[imax].tstart = fpsTime;
+    messages[imax].texpiry = messages[imax].tstart;
+  }
+
+  Unlock();
+}
+
+
+void PopupMessage::CheckTouch(HWND wmControl) {
+  if (wmControl == (HWND)window) {
+    // acknowledge with click/touch
+    Acknowledge(0);
+  }
+}
+
+
+bool PopupMessage::Acknowledge(int type) {
+  ScopeLock protect(mutex);
+  int i;
+  DWORD	fpsTime = ::GetTickCount() - startTime;
+
+  for (i=0; i<MAXMESSAGES; i++) {
+    if ((messages[i].texpiry> messages[i].tstart)
+	&& ((type==0)||(type==messages[i].type))) {
+      // message was previously visible, so make it expire now.
+      messages[i].texpiry = fpsTime-1;
+	  return true;
+    }
+  }
+  return false;
+}
+
+// DoMessage is designed to delegate what to do for a message
+// The "what to do" can be defined in a configuration file
+// Defaults for each message include:
+//	- Text to display (including multiple languages)
+//	- Text to display extra - NOT multiple language
+//		(eg: If Airspace Warning - what details - airfield name is in data file, already
+//		covers multiple languages).
+//	- ShowStatusMessage - including font size and delay
+//	- Sound to play - What sound to play
+//	- Log - Keep the message on the log/history window (goes to log file and history)
+//
+// TODO code: (need to discuss) Consider moving almost all this functionality into AddMessage ?
+
+void PopupMessage::AddMessage(const TCHAR* text, const TCHAR *data) {
+  ScopeLock protect(mutex);
+
+  StatusMessageSTRUCT LocalMessage = status_messages.First();
+  const StatusMessageSTRUCT *found = status_messages.Find(text);
+  if (found != NULL)
+    LocalMessage = *found;
+
+  if (SettingsComputer().EnableSoundModes && LocalMessage.doSound)
+    PlayResource(LocalMessage.sound);
+
+  // TODO code: consider what is a sensible size?
+  TCHAR msgcache[1024];
+  if (LocalMessage.doStatus) {
+
+    _tcscpy(msgcache, gettext(text));
+    if (data != NULL) {
+      _tcscat(msgcache, TEXT(" "));
+      _tcscat(msgcache, data);
+    }
+
+    AddMessage(LocalMessage.delay_ms, MSG_USERINTERFACE, msgcache);
+  }
+}
