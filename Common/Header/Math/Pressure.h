0efbc911769657705b846f7977cd5f34cfe3ae34
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2009-10-11 23:25:42 +0200

    Various comments added:
    Pressure.c
    Pressure.h
    
    Signed-off-by: Max Kellermann <max@duempel.org>

--- Common/Header/Math/Pressure.h
+++ Common/Header/Math/Pressure.h
@@ -48,19 +48,12 @@ extern "C" {
 
 double FindQNH(double alt_raw, double alt_known);
 
-/**
- * Converts altitude with QNH=1013.25 reference to QNH adjusted
- * altitude
- */
 double AltitudeToQNHAltitude(double alt);
 
 double StaticPressureToAltitude(double ps);
 
 double AirDensity(double altitude);
 
-/**
- * divide TAS by this number to get IAS
- */
 double AirDensityRatio(double altitude);
 
 #ifdef __cplusplus
--- Common/Source/Math/Pressure.c
+++ Common/Source/Math/Pressure.c
@@ -39,46 +39,72 @@ Copyright_License {
 
 #include <math.h>
 
-double QNH = (double)1013.2;
-double QFEAltitudeOffset= 0.0;
+double QNH = (double)1013.2; /**< Pressure on sea level */
+double QFEAltitudeOffset= 0.0; /**< not in use(?) */
 
-///////////
-
-
+/**
+ * Converts a QNH-based altitude to the corresponding pressure
+ *
+ * See http://wahiduddin.net/calc/density_altitude.htm
+ *
+ * Example:
+ * alt= 100, QNH=1014 => ps = 100203 Pa
+ * @see StaticPressureToAltitude
+ * @param alt Altitude over QNH-based zero
+ * @return Air pressure at given altitude
+ */
 double QNHAltitudeToStaticPressure(double alt) {
-  // http://wahiduddin.net/calc/density_altitude.htm
   const double k1=0.190263;
   const double k2=8.417286e-5;
   return 100.0*pow((pow(QNH,k1)-k2*alt),1.0/k1);
-  // example, alt= 100, QNH=1014
-  // ps = 100203 Pa
 }
 
+/**
+ * Converts a pressure value to the corresponding QNH-based altitude
+ *
+ * See http://wahiduddin.net/calc/density_altitude.htm
+ *
+ * Example:
+ * QNH=1014, ps=100203 => alt = 100
+ * @see QNHAltitudeToStaticPressure
+ * @param ps Air pressure
+ * @return Altitude over QNH-based zero
+ */
 double StaticPressureToAltitude(double ps) {
-  // http://wahiduddin.net/calc/density_altitude.htm
   const double k1=0.190263;
   const double k2=8.417286e-5;
   return (pow(QNH,k1) - pow(ps/100.0, k1))/k2;
-  // example, QNH=1014, ps=100203
-  // alt= 100
 }
-
+/**
+ * Converts altitude with QNH=1013.25 reference to QNH adjusted altitude
+ * @param alt 1013.25-based altitude
+ * @return QNH-based altitude
+ */
 double AltitudeToQNHAltitude(double alt) {
   const double k1=0.190263;
   double ps = pow((44330.8-alt)/4946.54,1.0/k1);
   return StaticPressureToAltitude(ps);
 }
 
+/**
+ * Calculates the current QNH by comparing a raw altitude
+ * to a known altitude of a certain location
+ *
+ * Original comment:
+ *
+ * find QNH so that the static pressure gives the specified altitude
+ * (altitude can come from GPS or known airfield altitude or terrain
+ * height on ground)
+ *
+ * This function assumes the barometric altitude (alt_raw) is
+ * already adjusted for QNH ---> the function returns the
+ * QNH value to make the barometric altitude equal to the
+ * alt_known value.
+ * @param alt_raw Current altitude
+ * @param alt_known Altitude of a known location
+ * @return QNH
+ */
 double FindQNH(double alt_raw, double alt_known) {
-  // find QNH so that the static pressure gives the specified altitude
-  // (altitude can come from GPS or known airfield altitude or terrain
-  // height on ground)
-
-  // This function assumes the barometric altitude (alt_raw) is
-  // already adjusted for QNH ---> the function returns the
-  // QNH value to make the barometric altitude equal to the
-  // alt_known value.
-
   const double k1=0.190263;
   const double k2=8.417286e-5;
 
@@ -93,17 +119,23 @@ double FindQNH(double alt_raw, double alt_known) {
   // qnh= 1016
 }
 
+/**
+ * Calculates the air density from a given QNH-based(?) altitude
+ * @param altitude QNH-based(?) altitude
+ * @return Air density
+ */
 double AirDensity(double altitude) {
   double rho = pow((44330.8-altitude)/42266.5,1.0/0.234969);
   return rho;
 }
 
+/**
+ * Divide TAS by this number to get IAS
+ * @param altitude QNH-based(?) altitude
+ * @return Air density ratio(?)
+ */
 double AirDensityRatio(double altitude) {
   double rho = AirDensity(altitude);
   double rho_rat = sqrt(1.225/rho);
   return rho_rat;
 }
-
-
-
-
