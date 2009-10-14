0c847657d4d0ef496a48c2c15251fc5bac1d6e73
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2009-10-14 22:44:55 +0200

    UtilsFLARM: Added Documentation, Corrected Indentation
    Signed-off-by: Max Kellermann <max@duempel.org>

--- Common/Source/UtilsFLARM.cpp
+++ Common/Source/UtilsFLARM.cpp
@@ -57,6 +57,9 @@ typedef struct {
 
 FLARM_Names_t FLARM_Names[MAXFLARMNAMES];
 
+/**
+ * Deletes all known FLARM names
+ */
 void CloseFLARMDetails() {
   int i;
   for (i=0; i<NumberOfFLARMNames; i++) {
@@ -65,15 +68,21 @@ void CloseFLARMDetails() {
   NumberOfFLARMNames = 0;
 }
 
+/**
+ * Opens XCSoars own FLARM details file, parses it and
+ * adds its entries as FlarmLookupItems
+ * @see AddFlarmLookupItem
+ */
 void OpenFLARMDetails() {
   StartupStore(TEXT("OpenFLARMDetails\n"));
 
+  // if (FLARM Details already there) delete them;
   if (NumberOfFLARMNames) {
     CloseFLARMDetails();
   }
 
   TCHAR filename[MAX_PATH];
-  LocalPath(filename,TEXT("xcsoar-flarm.txt"));
+  LocalPath(filename, TEXT("xcsoar-flarm.txt"));
 
   FILE *file = _tfopen(filename, TEXT("rt"));
   if (file == NULL)
@@ -85,17 +94,19 @@ void OpenFLARMDetails() {
     TCHAR Name[MAX_PATH];
 
     if (_stscanf(line, TEXT("%lx=%s"), &id, Name) == 2) {
-      if (AddFlarmLookupItem(id, Name, false) == false)
-	{
-	  break; // cant add anymore items !
-	}
+      if (AddFlarmLookupItem(id, Name, false) == false) {
+        break; // cant add anymore items !
+      }
     }
   }
 
   fclose(file);
 }
 
-
+/**
+ * Saves XCSoars own FLARM details into the
+ * corresponding file (xcsoar-flarm.txt)
+ */
 void SaveFLARMDetails(void)
 {
   TCHAR filename[MAX_PATH];
@@ -127,7 +138,12 @@ void SaveFLARMDetails(void)
   fclose(file);
 }
 
-
+/**
+ * Looks up the FLARM id in the FLARM details array
+ * and return the array id
+ * @param id FLARM id
+ * @return Array id if found, otherwise -1
+ */
 int LookupSecondaryFLARMId(int id)
 {
   for (int i=0; i<NumberOfFLARMNames; i++)
@@ -140,6 +156,12 @@ int LookupSecondaryFLARMId(int id)
   return -1;
 }
 
+/**
+ * Looks up the FLARM callsign in the FLARM details array
+ * and return the array id
+ * @param id Callsign
+ * @return Array id if found, otherwise -1
+ */
 int LookupSecondaryFLARMId(const TCHAR *cn)
 {
   for (int i=0; i<NumberOfFLARMNames; i++)
@@ -152,7 +174,12 @@ int LookupSecondaryFLARMId(const TCHAR *cn)
   return -1;
 }
 
-
+/**
+ * Looks up the FLARM id in the FLARM details array
+ * and the FLARMnet file and returns the callsign
+ * @param id FLARM id
+ * @return The corresponding callsign if found, otherwise NULL
+ */
 const TCHAR* LookupFLARMDetails(long id) {
 
   // try to find flarm from userFile
@@ -170,10 +197,16 @@ const TCHAR* LookupFLARMDetails(long id) {
       return flarmId->cn;
     }
 #endif
+
   return NULL;
 }
 
-
+/**
+ * Looks up the callsign in the FLARM details array
+ * and the FLARMnet file and returns the FLARM id
+ * @param cn Callsign
+ * @return The corresponding FLARM id if found, otherwise 0
+ */
 int LookupFLARMDetails(const TCHAR *cn)
 {
   // try to find flarm from userFile
@@ -191,38 +224,41 @@ int LookupFLARMDetails(const TCHAR *cn)
       return flarmId->GetId();
     }
 #endif
+
   return 0;
 }
 
-
-bool AddFlarmLookupItem(int id, const TCHAR *name, bool saveFile)
-{
+/**
+ * Adds a FLARM details couple (callsign + FLARM id)
+ * to the FLARM details array and saves it to the
+ * user file (if necessary)
+ * @param id FLARM id
+ * @param name Callsign
+ * @param saveFile True = FLARM details file is saved after update
+ * @return True if successfully added, False otherwise
+ */
+bool AddFlarmLookupItem(int id, const TCHAR *name, bool saveFile) {
   int index = LookupSecondaryFLARMId(id);
 
-  if (index == -1)
-    {
-      if (NumberOfFLARMNames < MAXFLARMNAMES - 1)
-	{
-	  // create new record
-	  FLARM_Names[NumberOfFLARMNames].ID = id;
-	  _tcsncpy(FLARM_Names[NumberOfFLARMNames].Name, name,20);
-	  FLARM_Names[NumberOfFLARMNames].Name[20]=0;
-	  NumberOfFLARMNames++;
-	  SaveFLARMDetails();
-	  return true;
-	}
-    }
-  else
-    {
-      // modify existing record
-      FLARM_Names[index].ID = id;
-      _tcsncpy(FLARM_Names[index].Name, name,20);
-      FLARM_Names[index].Name[20]=0;
-      if (saveFile)
-	{
-	  SaveFLARMDetails();
-	}
+  if (index == -1) {
+    if (NumberOfFLARMNames < MAXFLARMNAMES - 1) {
+      // create new record
+      FLARM_Names[NumberOfFLARMNames].ID = id;
+      _tcsncpy(FLARM_Names[NumberOfFLARMNames].Name, name, 20);
+      FLARM_Names[NumberOfFLARMNames].Name[20] = 0;
+      NumberOfFLARMNames++;
+      SaveFLARMDetails();
       return true;
     }
+  } else {
+    // modify existing record
+    FLARM_Names[index].ID = id;
+    _tcsncpy(FLARM_Names[index].Name, name, 20);
+    FLARM_Names[index].Name[20] = 0;
+    if (saveFile) {
+      SaveFLARMDetails();
+    }
+    return true;
+  }
   return false;
 }
