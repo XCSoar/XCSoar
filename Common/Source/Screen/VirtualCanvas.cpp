d404c48f64d71a5a8afa6941369ccc20688e52c4
Max Kellermann <max@duempel.org>
2009-10-20 01:11:03 +0200

    Screen/VirtualCanvas: disable SDL alpha mask
    The alpha mask creates painting problems, because SDL_BlitSurface()
    does not seem to overwrite the alpha values.  This patch disables all
    alpha masks on software surfaces.

--- Common/Source/Screen/VirtualCanvas.cpp
+++ Common/Source/Screen/VirtualCanvas.cpp
@@ -75,12 +75,12 @@ VirtualCanvas::set(unsigned _width, unsigned _height)
   rmask = 0xff000000;
   gmask = 0x00ff0000;
   bmask = 0x0000ff00;
-  amask = 0x000000ff;
+  amask = 0;
 #else
   rmask = 0x000000ff;
   gmask = 0x0000ff00;
   bmask = 0x00ff0000;
-  amask = 0xff000000;
+  amask = 0;
 #endif
 
   surface = ::SDL_CreateRGBSurface(SDL_SWSURFACE, _width, _height, 32,
