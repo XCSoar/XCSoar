656611f011e83c4d03cc8f3341dccff8a7a21fb7
John Wharington <jwharington@gmail.com>
2009-11-08 02:16:00 +1100

    documentation

--- ex/src/Waypoint/Waypoints.hpp
+++ ex/src/Waypoint/Waypoints.hpp
@@ -12,6 +12,15 @@ class WaypointVisitor;
 
 class Waypoints {
 public:
+/** 
+ * Constructor.  Task projection is updated after call to optimise().
+ * As waypoints are added they are stored temporarily before applying
+ * projection so that the bounds of the projection may be obtained.
+ * 
+ * See below for usage notes --- further work is required.
+ * 
+ * @param _task_projection projection used for flat-earth representation
+ */
   Waypoints(TaskProjection& _task_projection);
 
   typedef KDTree::KDTree<2, 
@@ -19,25 +28,107 @@ public:
                          Waypoint::kd_get_location
                          > WaypointTree;
 
+/** 
+ * Looks up nearest waypoint to the search location.
+ * Performs search according to flat-earth internal representation,
+ * so is approximate.
+ * 
+ * @param loc Location from which to search
+ * 
+ * @return Iterator to absolute nearest waypoint 
+ */
   WaypointTree::const_iterator find_nearest(const GEOPOINT &loc) const;
 
+/** 
+ * Look up waypoint by ID.
+ * 
+ * @param id Id of waypoint to find in internal tree
+ * 
+ * @return Iterator to matching waypoint (or end if not found)
+ */
   WaypointTree::const_iterator find_id(const unsigned id) const;
 
+/** 
+ * Find waypoints within approximate range (square range box)
+ * to search location.  Possible use by screen display functions.
+ * 
+ * @param loc Location from which to search
+ * @param range Distance in meters of search radius
+ * 
+ * @return Vector of waypoints within square range
+ */
   std::vector< Waypoint >
     find_within_range(const GEOPOINT &loc, const double range) const;
 
-  std::vector< Waypoint >
-    find_within_range_circle(const GEOPOINT &loc, const double range) const;
-
-  void optimise();
-  void insert(const Waypoint& wp);
-
+/** 
+ * Call visitor function on waypoints within approximate range
+ * (square range box) to search location.  Possible use by screen display
+ * functions.
+ * 
+ * @param loc Location from which to search
+ * @param range Distance in meters of search radius
+ * @param visitor Visitor to be called on waypoints within range
+ */
   void visit_within_range(const GEOPOINT &loc, const double range,
                           WaypointVisitor& visitor) const;
 
+/** 
+ * Find waypoints within specified flat-earth distance to
+ * search location.
+ * 
+ * @param loc Location from which to search
+ * @param range Distance in meters of search radius
+ * 
+ * @return Vector of waypoints within circular range
+ */
+  std::vector< Waypoint >
+    find_within_range_circle(const GEOPOINT &loc, const double range) const;
+
+/** 
+ * Add waypoint to internal store.  Internal copy is made.
+ * optimise() must be called after inserting waypoints prior to
+ * performing any queries, but can be done in batches.
+ * 
+ * @param wp Waypoint to add to internal store
+ */
+  void insert(const Waypoint& wp);
+
+/** 
+ * Optimise the internal search tree after adding/removing elements.
+ * Also performs projection to flat earth for new elements.
+ * This updates the task_projection.
+ * 
+ * Note: currently this code doesn't check for task projections
+ * being modified from multiple calls to optimise() so it should
+ * only be called once (until this is fixed).
+ */
+  void optimise();
+
+/** 
+ * Access task projection calculated from mid-point of waypoints
+ * inserted in the store, for use by other systems requiring
+ * knowledge of a reasonable center point for flat-earth projections.
+ * 
+ * @return Task projection used by internal store
+ */
   const TaskProjection &get_task_projection() const {
     return task_projection;
   }
+
+/** 
+ * Access first waypoint in store, for use in iterators.
+ * 
+ * @return First waypoint in store
+ */
+  WaypointTree::const_iterator begin() const;
+
+/** 
+ * Access end waypoint in store, for use in iterators as end point.
+ * 
+ * @return End waypoint in store
+ */
+  WaypointTree::const_iterator end() const;
+
 private:
   WaypointTree waypoint_tree;
   TaskProjection& task_projection;
@@ -46,9 +137,6 @@ private:
 
   /** @link dependency */
   /*#  Waypoint lnkWaypoint; */
-public:
-  WaypointTree::const_iterator begin() const;
-  WaypointTree::const_iterator end() const;
 };
 
 #endif
