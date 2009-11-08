2d38bceb211614c9d82bc48c483101a1a74cfafb
John Wharington <jwharington@gmail.com>
2009-11-09 03:38:32 +1100

    documentation

--- ex/src/GlideSolvers/GlideState.hpp
+++ ex/src/GlideSolvers/GlideState.hpp
@@ -6,34 +6,87 @@
 struct AIRCRAFT_STATE;
 struct GEOPOINT;
 
-struct GlideState {
-  // dummy task
+struct GlideState 
+{
+
+/** 
+ * Dummy task constructor.  Typically used for synthetic glide
+ * tasks.  Where there are real targets, the other constructors should
+ * be used instead.
+ * 
+ * @param vector Specified vector for task
+ * @param htarget Height of target (m above MSL)
+ * @param aircraft Aircraft state
+ * 
+ * @return Initialised glide task
+ */
   GlideState(const GeoVector &vector,
               const double htarget,
               const AIRCRAFT_STATE &aircraft);
-  // from target to aircraft
+
+/** 
+ * Constructor for glide task from a point to the aircraft.
+ * 
+ * @param target Location of start point
+ * @param aircraft State of aircraft at end
+ * @param htarget Height of start point (m above MSL)
+ * 
+ * @return Initialised glide task (for task travelled)
+ */
   GlideState(const GEOPOINT& target,
               const AIRCRAFT_STATE &aircraft,
               const double htarget);
-  // from aircraft to target
+
+/** 
+ * Constructor for glide task from aircraft to a point.
+ * 
+ * @param aircraft State of aircraft at start
+ * @param target Location of end point
+ * @param htarget Height of end point (m above MSL)
+ * 
+ * @return Initialised glide task for task remaining
+ */
   GlideState(const AIRCRAFT_STATE &aircraft,
               const GEOPOINT& target,
               const double htarget);
 
-  GeoVector Vector; // TODO should be average bearing
+  GeoVector Vector;             /**< Distance/bearing of task */
+  double MinHeight;             /**< Height (m above MSL) of end */
+  double WindDirection;         /**< Direction of wind (deg True) */
+  double AltitudeDifference;    /**< Aircraft height less target height */
 
-  double MinHeight; 
+/** 
+ * Calculate internal quantities to reduce computation time
+ * by clients of this class
+ * 
+ * @param aircraft Aircraft state
+ */
   void calc_speedups(const AIRCRAFT_STATE &aircraft);
-  double WindDirection;
-  double AltitudeDifference;
-  double EffectiveWindSpeed;
-  double EffectiveWindAngle;
+
+/** 
+ * Calculates average cross-country speed from effective 
+ * cross-country speed (accounting for wind)
+ * 
+ * @param Veff Effective cruise speed (m/s)
+ * 
+ * @return Average cross-country speed (m/s)
+ */
   double calc_ave_speed(const double Veff) const;
 
-  double wsq_; // speedup
-  double dwcostheta_; // speedup
-
+/** 
+ * Calculate distance a circling aircraft will drift
+ * in a given time
+ * 
+ * @param t_climb Time spent in climb (s)
+ * 
+ * @return Distance (m) of drift
+ */
   double drifted_distance(const double t_climb) const;
+
+  double EffectiveWindSpeed;    /**< (internal use) */
+  double EffectiveWindAngle;    /**< (internal use) */
+  double wsq_;                  /**< (internal use) */
+  double dwcostheta_;           /**< (internal use) */
 };
 
 #endif
