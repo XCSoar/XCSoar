0338028471fe1a458d04d1ca47bf374f75088c4f
Max Kellermann <max@duempel.org>
2009-11-17 07:46:10 +0100

    added Tobias Bieniek to the list of authors

--- Common/Source/windmeasurementlist.cpp
+++ Common/Source/windmeasurementlist.cpp
@@ -20,7 +20,7 @@ NOTE: Some portions copyright as above
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000 - 2009
+  Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
 
 	M Roberts (original release)
 	Robin Birch <robinb@ruffnready.co.uk>
@@ -36,6 +36,7 @@ Copyright_License {
 	Tobias Lohner <tobias@lohner-net.de>
 	Mirek Jezek <mjezek@ipplc.cz>
 	Max Kellermann <max@duempel.org>
+	Tobias Bieniek <tobias.bieniek@gmx.de>
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
