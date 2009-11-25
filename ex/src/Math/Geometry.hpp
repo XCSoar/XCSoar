fee22fc6a2bde73689f3f9e8d83655cc4a8e254b
John Wharington <jwharington@gmail.com>
2009-11-25 17:27:55 +1100

    remove unused functions (want minimal code for testing)

--- ex/src/Math/Geometry.hpp
+++ ex/src/Math/Geometry.hpp
@@ -38,22 +38,10 @@ Copyright_License {
 #ifndef XCSOAR_MATH_GEOMETRY_HPP
 #define XCSOAR_MATH_GEOMETRY_HPP
 
-void irotate(int &xin, int &yin, const double &angle);
-void irotatescale(int &xin, int &yin, const double &angle, const double &scale,
-                  double &x, double &y);
-void rotate(double &xin, double &yin, const double &angle);
-void frotate(float &xin, float &yin, const float &angle);
-void rotatescale(double &xin, double &yin,
-                 const double &angle, const double &scale);
-void frotatescale(float &xin, float &yin,
-                  const float &angle, const float &scale);
-
-bool AngleInRange(double Angle0, double Angle1, double x, bool is_signed=false);
 double AngleLimit180(double theta);
 double AngleLimit360(double theta);
 
-double Reciprocal(double InBound);
 double BiSector(double InBound, double OutBound);
-double HalfAngle(double Angle0, double Angle1);
+double Reciprocal(double InBound);
 
 #endif
