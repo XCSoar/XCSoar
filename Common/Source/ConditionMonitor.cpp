11daf0ea7504a1bc4d14f139162016e86dda22ff
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2009-12-11 10:48:24 +0100

    ConditionMonitor: Corrected Format and Indentation

--- Common/Source/ConditionMonitor.cpp
+++ Common/Source/ConditionMonitor.cpp
@@ -59,14 +59,18 @@ Copyright_License {
 
 // TODO: JMW: make this use GPSClock (code re-use)
 
-class ConditionMonitor {
+class ConditionMonitor
+{
 public:
-  ConditionMonitor() {
+  ConditionMonitor()
+  {
     LastTime_Notification = -1;
     LastTime_Check = -1;
   }
 
-  void Update(const GlideComputer& cmp) {
+  void
+  Update(const GlideComputer& cmp)
+  {
     if (!cmp.Calculated().Flying)
       return;
 
@@ -80,9 +84,9 @@ public:
           SaveLast();
         }
       }
-      if (restart) {
+
+      if (restart)
         SaveLast();
-      }
     }
   }
 
@@ -98,48 +102,55 @@ private:
   virtual void Notify(void) = 0;
   virtual void SaveLast(void) = 0;
 
-  bool Ready_Time_Notification(double T) {
-    if (T<=0) {
+  bool
+  Ready_Time_Notification(double T)
+  {
+    if (T <= 0)
       return false;
-    }
-    if ((T<LastTime_Notification) || (LastTime_Notification== -1)) {
+
+    if ((T < LastTime_Notification) || (LastTime_Notification == -1))
       return true;
-    }
-    if (T>= LastTime_Notification + Interval_Notification) {
+
+    if (T >= LastTime_Notification + Interval_Notification)
       return true;
-    }
+
     return false;
   }
 
-  bool Ready_Time_Check(double T, bool *restart) {
-    if (T<=0) {
+  bool
+  Ready_Time_Check(double T, bool *restart)
+  {
+    if (T <= 0)
       return false;
-    }
-    if ((T<LastTime_Check) || (LastTime_Check== -1)) {
+
+    if ((T < LastTime_Check) || (LastTime_Check == -1)) {
       LastTime_Notification = -1;
       *restart = true;
       return true;
     }
-    if (T>= LastTime_Check + Interval_Check) {
-      return true;
-    }
-    return false;
-  };
 
+    if (T >= LastTime_Check + Interval_Check)
+      return true;
+
+    return false;
+  }
 };
 
-class ConditionMonitorWind: public ConditionMonitor {
+class ConditionMonitorWind: public ConditionMonitor
+{
 public:
-  ConditionMonitorWind() {
-    Interval_Notification = 60*5;
+  ConditionMonitorWind()
+  {
+    Interval_Notification = 60 * 5;
     Interval_Check = 10;
     wind_mag = 0;
     wind_bearing = 0;
   }
+
 protected:
-
-  bool CheckCondition(const GlideComputer& cmp) {
-
+  bool
+  CheckCondition(const GlideComputer& cmp)
+  {
     wind_mag = cmp.Calculated().WindSpeed;
     wind_bearing = cmp.Calculated().WindBearing;
 
@@ -150,261 +161,295 @@ protected:
     }
 
     double mag_change = fabs(wind_mag - last_wind_mag);
-    double dir_change = fabs(AngleLimit180(wind_bearing-last_wind_bearing));
-    if (mag_change > 5/TOKNOTS) {
+    double dir_change = fabs(AngleLimit180(wind_bearing - last_wind_bearing));
+
+    if (mag_change > 5 / TOKNOTS)
       return true;
-    }
-    if ((wind_mag>10/TOKNOTS) && (dir_change > 45)) {
+
+    if ((wind_mag > 10 / TOKNOTS) && (dir_change > 45))
       return true;
-    }
+
     return false;
-  };
+  }
 
-  void Notify(void) {
+  void
+  Notify(void)
+  {
     Message::AddMessage(TEXT("Significant wind change"));
-  };
+  }
 
-  void SaveLast(void) {
+  void
+  SaveLast(void)
+  {
     last_wind_mag = wind_mag;
     last_wind_bearing = wind_bearing;
-  };
+  }
 
 private:
   double wind_mag;
   double wind_bearing;
   double last_wind_mag;
   double last_wind_bearing;
-
 };
 
-class ConditionMonitorFinalGlide: public ConditionMonitor {
+class ConditionMonitorFinalGlide: public ConditionMonitor
+{
 public:
-  ConditionMonitorFinalGlide() {
-    Interval_Notification = 60*5;
+  ConditionMonitorFinalGlide()
+  {
+    Interval_Notification = 60 * 5;
     Interval_Check = 1;
     tad = 0;
   }
 
 protected:
-  bool CheckCondition(const GlideComputer& cmp) {
-    if (!cmp.Calculated().Flying || !task.Valid()) {
+  bool
+  CheckCondition(const GlideComputer& cmp)
+  {
+    if (!cmp.Calculated().Flying || !task.Valid())
       return false;
-    }
 
-    tad = cmp.Calculated().TaskAltitudeDifference*0.2+0.8*tad;
+    // TODO: use low pass filter
+    tad = cmp.Calculated().TaskAltitudeDifference * 0.2 + 0.8 * tad;
 
-    bool BeforeFinalGlide =
-      (task.ValidTaskPoint(task.getActiveIndex()+1)
-       && !cmp.Calculated().FinalGlide);
+    bool BeforeFinalGlide = (task.ValidTaskPoint(task.getActiveIndex() + 1)
+                            && !cmp.Calculated().FinalGlide);
 
     if (BeforeFinalGlide) {
-      Interval_Notification = 60*5;
-      if ((tad>50) && (last_tad< -50)) {
+      Interval_Notification = 60 * 5;
+      if ((tad > 50) && (last_tad < -50))
         // report above final glide early
         return true;
-      } else if (tad< -50) {
+      else if (tad < -50)
         last_tad = tad;
-      }
     } else {
       Interval_Notification = 60;
       if (cmp.Calculated().FinalGlide) {
-        if ((last_tad< -50) && (tad>1)) {
+        if ((last_tad < -50) && (tad > 1))
           // just reached final glide, previously well below
           return true;
-        }
-        if ((last_tad> 1) && (tad< -50)) {
+
+        if ((last_tad > 1) && (tad < -50)) {
           // dropped well below final glide, previously above
           last_tad = tad;
           return true; // JMW this was true before
         }
       }
     }
+
     return false;
-  };
+  }
 
-  void Notify(void) {
-    if (tad>1) {
+  void
+  Notify(void)
+  {
+    if (tad > 1)
       InputEvents::processGlideComputer(GCE_FLIGHTMODE_FINALGLIDE_ABOVE);
-    }
-    if (tad<-1) {
+    if (tad < -1)
       InputEvents::processGlideComputer(GCE_FLIGHTMODE_FINALGLIDE_BELOW);
-    }
-  };
+  }
 
-  void SaveLast(void) {
+  void
+  SaveLast(void)
+  {
     last_tad = tad;
-  };
+  }
 
 private:
   double tad;
   double last_tad;
 };
 
-class ConditionMonitorSunset: public ConditionMonitor {
+class ConditionMonitorSunset: public ConditionMonitor
+{
 public:
-  ConditionMonitorSunset() {
-    Interval_Notification = 60*30;
+  ConditionMonitorSunset()
+  {
+    Interval_Notification = 60 * 30;
     Interval_Check = 60;
   }
 protected:
   SunEphemeris sun;
 
-  bool CheckCondition(const GlideComputer& cmp) {
-    if (!task.Valid() || !cmp.Calculated().Flying) {
+  bool
+  CheckCondition(const GlideComputer& cmp)
+  {
+    if (!task.Valid() || !cmp.Calculated().Flying)
       return false;
-    }
 
-    double sunsettime = sun.CalcSunTimes
-      (task.getActiveLocation(),
-       cmp.Basic(), cmp.Calculated(), GetUTCOffset()/3600);
+    double sunsettime = sun.CalcSunTimes(task.getActiveLocation(),
+        cmp.Basic(), cmp.Calculated(), GetUTCOffset() / 3600);
     double d1 = (cmp.Calculated().TaskTimeToGo
-		 +DetectCurrentTime(&cmp.Basic()))/3600;
-    double d0 = (DetectCurrentTime(&cmp.Basic()))/3600;
+        + DetectCurrentTime(&cmp.Basic())) / 3600;
+    double d0 = (DetectCurrentTime(&cmp.Basic())) / 3600;
 
-    bool past_sunset = (d1>sunsettime) && (d0<sunsettime);
+    bool past_sunset = (d1 > sunsettime) && (d0 < sunsettime);
 
-    if (past_sunset && !devIsCondor(devA())) {
+    if (past_sunset && !devIsCondor(devA()))
       // notify on change only
       return true;
-    } else {
+    else
       return false;
-    }
-  };
+  }
 
-  void Notify(void) {
+  void
+  Notify(void)
+  {
     Message::AddMessage(TEXT("Expect arrival past sunset"));
-  };
+  }
 
-  void SaveLast(void) {
-  };
+  void
+  SaveLast(void)
+  {
+  }
 
 private:
 };
 
-class ConditionMonitorAATTime: public ConditionMonitor {
+class ConditionMonitorAATTime: public ConditionMonitor
+{
 public:
-  ConditionMonitorAATTime() {
-    Interval_Notification = 60*15;
+  ConditionMonitorAATTime()
+  {
+    Interval_Notification = 60 * 15;
     Interval_Check = 10;
   }
-protected:
 
-  bool CheckCondition(const GlideComputer& cmp) {
-    if (!task.getSettings().AATEnabled || !task.Valid() || task.TaskIsTemporary()
-        || !(cmp.Calculated().ValidStart && !cmp.Calculated().ValidFinish)
-        || !cmp.Calculated().Flying) {
+protected:
+  bool
+  CheckCondition(const GlideComputer& cmp)
+  {
+    if (!task.getSettings().AATEnabled || !task.Valid()
+        || task.TaskIsTemporary() || !(cmp.Calculated().ValidStart
+        && !cmp.Calculated().ValidFinish) || !cmp.Calculated().Flying)
       return false;
-    }
+
     bool OnFinalWaypoint = !task.Valid();
-    if (OnFinalWaypoint) {
+    if (OnFinalWaypoint)
       // can't do much about it now, so don't give a warning
       return false;
-    }
-    if (cmp.Calculated().TaskTimeToGo < cmp.Calculated().AATTimeToGo) {
+
+    if (cmp.Calculated().TaskTimeToGo < cmp.Calculated().AATTimeToGo)
       return true;
-    } else {
+    else
       return false;
-    }
-  };
+  }
 
-  void Notify(void) {
+  void
+  Notify(void)
+  {
     Message::AddMessage(TEXT("Expect early task arrival"));
-  };
+  }
 
-  void SaveLast(void) {
-  };
+  void
+  SaveLast(void)
+  {
+  }
 
 private:
 };
 
-class ConditionMonitorStartRules: public ConditionMonitor {
+class ConditionMonitorStartRules: public ConditionMonitor
+{
 public:
-  ConditionMonitorStartRules() {
+  ConditionMonitorStartRules()
+  {
     Interval_Notification = 60;
     Interval_Check = 1;
     withinMargin = false;
   }
 protected:
 
-  bool CheckCondition(const GlideComputer& cmp) {
-    if (!task.Valid() || !cmp.Calculated().Flying
-        || (task.getActiveIndex()>0) || !task.ValidTaskPoint(task.getActiveIndex()+1)) {
+  bool
+  CheckCondition(const GlideComputer& cmp)
+  {
+    if (!task.Valid()
+        || !cmp.Calculated().Flying
+        || (task.getActiveIndex() > 0)
+        || !task.ValidTaskPoint(task.getActiveIndex() + 1))
       return false;
-    }
-    if (cmp.Calculated().LegDistanceToGo>task.getSettings().StartRadius) {
-      return false;
-    }
-    if (cmp.ValidStartSpeed(task.getSettings().StartMaxSpeedMargin)
-	&& cmp.InsideStartHeight(task.getSettings().StartMaxHeightMargin))
-    {
-      withinMargin = true;
-    } else {
-      withinMargin = false;
-    }
-    return !(cmp.ValidStartSpeed()
-	     && cmp.InsideStartHeight());
-  };
 
-  void Notify(void) {
+    if (cmp.Calculated().LegDistanceToGo > task.getSettings().StartRadius)
+      return false;
+
+    if (cmp.ValidStartSpeed(task.getSettings().StartMaxSpeedMargin)
+        && cmp.InsideStartHeight(task.getSettings().StartMaxHeightMargin))
+      withinMargin = true;
+    else
+      withinMargin = false;
+    return !(cmp.ValidStartSpeed() && cmp.InsideStartHeight());
+  }
+
+  void
+  Notify(void)
+  {
     if (withinMargin)
       Message::AddMessage(TEXT("Start rules slightly violated\r\nbut within margin"));
     else
       Message::AddMessage(TEXT("Start rules violated"));
-  };
+  }
 
-  void SaveLast(void) {
-  };
+  void
+  SaveLast(void)
+  {
+  }
 
 private:
   bool withinMargin;
 };
 
-class ConditionMonitorGlideTerrain: public ConditionMonitor {
+class ConditionMonitorGlideTerrain: public ConditionMonitor
+{
 public:
-  ConditionMonitorGlideTerrain() {
-    Interval_Notification = 60*5;
+  ConditionMonitorGlideTerrain()
+  {
+    Interval_Notification = 60 * 5;
     Interval_Check = 1;
     fgtt = 0;
     fgtt_last = false;
   }
 
 protected:
-
-  bool CheckCondition(const GlideComputer& cmp) {
-    if (!cmp.Calculated().Flying || !task.Valid()) {
+  bool
+  CheckCondition(const GlideComputer& cmp)
+  {
+    if (!cmp.Calculated().Flying || !task.Valid())
       return false;
-    }
 
-    fgtt = !((cmp.Calculated().TerrainWarningLocation.Latitude == 0.0) &&
-	     (cmp.Calculated().TerrainWarningLocation.Longitude == 0.0));
+    fgtt = !((cmp.Calculated().TerrainWarningLocation.Latitude == 0.0)
+        && (cmp.Calculated().TerrainWarningLocation.Longitude == 0.0));
 
     if (!cmp.Calculated().FinalGlide
-	|| (cmp.Calculated().TaskAltitudeDifference<-50)) {
+        || (cmp.Calculated().TaskAltitudeDifference < -50))
       fgtt_last = false;
-    } else if ((fgtt) && (!fgtt_last)) {
+    else if ((fgtt) && (!fgtt_last))
       // just reached final glide, previously well below
       return true;
-    }
+
     return false;
-  };
+  }
 
-  void Notify(void) {
+  void
+  Notify(void)
+  {
     InputEvents::processGlideComputer(GCE_FLIGHTMODE_FINALGLIDE_TERRAIN);
-  };
+  }
 
-  void SaveLast(void) {
+  void
+  SaveLast(void)
+  {
     fgtt_last = fgtt;
-  };
+  }
 
 private:
   bool fgtt;
   bool fgtt_last;
 };
 
-ConditionMonitorWind       cm_wind;
+ConditionMonitorWind cm_wind;
 ConditionMonitorFinalGlide cm_finalglide;
-ConditionMonitorSunset     cm_sunset;
-ConditionMonitorAATTime    cm_aattime;
+ConditionMonitorSunset cm_sunset;
+ConditionMonitorAATTime cm_aattime;
 ConditionMonitorStartRules cm_startrules;
 ConditionMonitorGlideTerrain cm_glideterrain;
 
