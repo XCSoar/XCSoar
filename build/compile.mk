100a1e05ad539f019bf1c34def4cf1f2785ab26b
Max Kellermann <max@duempel.org>
2009-12-13 13:10:49 +0100

    Makefile: whitespace fixups

--- build/compile.mk
+++ build/compile.mk
@@ -1,14 +1,13 @@
 ######## tools
 
-EXE		:=$(findstring .exe,$(MAKE))
-AR		:=$(TCPATH)ar$(EXE)
-CXX		:=$(TCPATH)g++$(EXE)
-CC		:=$(TCPATH)gcc$(EXE)
-SIZE		:=$(TCPATH)size$(EXE)
-STRIP		:=$(TCPATH)strip$(EXE)
-WINDRES		:=$(TCPATH)windres$(EXE)
-CE_VERSION	:=0x0$(CE_MAJOR)$(CE_MINOR)
-ARFLAGS		:=r
+EXE := $(findstring .exe,$(MAKE))
+AR = $(TCPATH)ar$(EXE)
+CXX = $(TCPATH)g++$(EXE)
+CC = $(TCPATH)gcc$(EXE)
+SIZE = $(TCPATH)size$(EXE)
+STRIP = $(TCPATH)strip$(EXE)
+WINDRES = $(TCPATH)windres$(EXE)
+ARFLAGS = r
 
 ifeq ($(CONFIG_WINE),y)
 AR = ar$(EXE)
@@ -18,11 +17,11 @@ endif
 
 ####### dependency handling
 
-DEPFILE		=$(dir $@).$(notdir $@).d
-DEPFLAGS	=-Wp,-MD,$(DEPFILE)
-dirtarget	=$(subst \\,_,$(subst /,_,$(dir $@)))
-cc-flags	=$(DEPFLAGS) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH)
-cxx-flags	=$(DEPFLAGS) $(CXXFLAGS) $(CPPFLAGS) $(TARGET_ARCH)
+DEPFILE = $(dir $@).$(notdir $@).d
+DEPFLAGS = -Wp,-MD,$(DEPFILE)
+dirtarget = $(subst \\,_,$(subst /,_,$(dir $@)))
+cc-flags = $(DEPFLAGS) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH)
+cxx-flags = $(DEPFLAGS) $(CXXFLAGS) $(CPPFLAGS) $(TARGET_ARCH)
 
 #
 # Useful debugging targets - make preprocessed versions of the source
