8b4a85d7715506aff41c8eb1c056e9f1b4a33155
Max Kellermann <max@duempel.org>
2009-12-18 22:31:24 +0100

    h2cpp.pl: read C++ source from standard input
    No hard coded paths.

--- Common/Data/Input/h2cpp.pl
+++ Common/Data/Input/h2cpp.pl
@@ -3,13 +3,12 @@ use strict;
 use warnings;
 
 print "/* AUTOMATICALLY GENERATED FILE - DO NOT EDIT BY HAND - see Common/Data/Input/h2cpp.pl */\n";
-open (IN, "../../Header/InputEvents.h");
 my $count = 0;
 
 my $buffer = "";
 my $gce_count = 0;
 my $ne_count = 0;
-while (<IN>) {
+while (<>) {
 	chomp;
 
 	if (/static void event([a-zA-Z0-9]+)/) {
