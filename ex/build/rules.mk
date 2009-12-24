1c4e5f6a7dd45026e1902784756989a4e1e39eb7
Max Kellermann <max@duempel.org>
2009-12-24 02:43:59 +0100

    ex/Makefile: added missing -o gcc option

--- ex/build/rules.mk
+++ ex/build/rules.mk
@@ -22,18 +22,18 @@ cxx-flags	=$(DEPFLAGS) $(CXXFLAGS) $(CPPFLAGS) $(CPPFLAGS_$(dirtarget)) $(TARGET
 #
 %-$(TARGET).o: %.c
 	@$(NQ)echo "  CC      $@"
-	$(Q)$(CC) $(cc-flags) -c $(OUTPUT_OPTION) $<
+	$(Q)$(CC) -o $@ $(cc-flags) -c $(OUTPUT_OPTION) $<
 	@sed -i '1s,^[^ :]*,$@,' $(DEPFILE)
 #	@etags -a --declarations $<
 
 %-$(TARGET).o: %.cpp
 	@$(NQ)echo "  CXX     $@"
-	$(Q)$(CXX) $(cxx-flags) -c $(OUTPUT_OPTION) $<
+	$(Q)$(CXX) -o $@ $(cxx-flags) -c $(OUTPUT_OPTION) $<
 	@sed -i '1s,^[^ :]*,$@,' $(DEPFILE)
 #	@etags -a --declarations $<
 
 %.o: %.cxx
 	@$(NQ)echo "  CXX     $@"
-	$(Q)$(CXX) $(cxx-flags) -c $(OUTPUT_OPTION) $<
+	$(Q)$(CXX) -o $@ $(cxx-flags) -c $(OUTPUT_OPTION) $<
 	@sed -i '1s,^[^ :]*,$@,' $(DEPFILE)
 
