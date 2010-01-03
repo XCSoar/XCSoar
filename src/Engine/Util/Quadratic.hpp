b3fe429651d38d6952fd10ac669f5a0e1e653695
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-01-03 18:30:25 +0100

    Engine/Util/Quadratic: Corrected Format and Indentation

--- src/Engine/Util/Quadratic.hpp
+++ src/Engine/Util/Quadratic.hpp
@@ -44,82 +44,86 @@
  */
 class Quadratic {
 public:
+  /**
+   * Constructor for quadratic function x^2+b*x+c=0
+   *
+   * @param _b Value of b
+   * @param _c Value of c
+   */
+  Quadratic(const fixed _b, const fixed _c) :
+    da(fixed_two), b(_b), denom(b * b - fixed_four * _c)
+  {
+  }
 
-/** 
- * Constructor for quadratic function x^2+b*x+c=0
- * 
- * @param _b Value of b
- * @param _c Value of c
- */
-  Quadratic(const fixed _b, const fixed _c):
-    da(fixed_two),
-    b(_b),
-    denom(b*b-fixed_four*_c)
-    {};
+  /**
+   * Constructor for quadratic function a*x^2+b*x+c=0
+   *
+   * @param _a Value of a
+   * @param _b Value of b
+   * @param _c Value of c
+   */
+  Quadratic(const fixed _a, const fixed _b, const fixed _c) :
+    da(fixed_two * _a), b(_b), denom(b * b - fixed_two * da * _c)
+  {
+  }
 
-/** 
- * Constructor for quadratic function a*x^2+b*x+c=0
- * 
- * @param _a Value of a
- * @param _b Value of b
- * @param _c Value of c
- */
-  Quadratic(const fixed _a, const fixed _b, const fixed _c):
-    da(fixed_two*_a),
-    b(_b),
-    denom(b*b-fixed_two*da*_c)
-    {};
-
-/** 
- * Check if all solutions of quadratic are real
- * 
- * @return True if quadratic has at least one real solution
- */
-  bool check() const {
-    if (negative(denom)) {
+  /**
+   * Check if all solutions of quadratic are real
+   *
+   * @return True if quadratic has at least one real solution
+   */
+  bool
+  check() const
+  {
+    if (negative(denom))
       return false;
-    }
-    if (da==fixed_zero) {
+
+    if (da == fixed_zero)
       return false;
-    }
+
     return true;
   }
 
-/** 
- * Returns largest real solution.  Valid only where check() has passed. 
- * 
- * @return greater x value of solutions 
- */
-  fixed solution_max() const {
-    return (da>0? solution(true):solution(false));
+  /**
+   * Returns largest real solution.  Valid only where check() has passed.
+   *
+   * @return greater x value of solutions
+   */
+  fixed
+  solution_max() const
+  {
+    return (da > 0 ? solution(true) : solution(false));
   }
 
-/** 
- * Returns smallest real solution.  Valid only where check() has passed. 
- * 
- * @return smallest x value of solutions 
- */
-  fixed solution_min() const {
-    return (da>0? solution(false):solution(true));
+  /**
+   * Returns smallest real solution.  Valid only where check() has passed.
+   *
+   * @return smallest x value of solutions
+   */
+  fixed
+  solution_min() const
+  {
+    return (da > 0 ? solution(false) : solution(true));
   }
 
 private:
-
-/** 
- * Calculate solution of quadratic equation using relation:
- *   x = (-b +/- sqrt(b^2-4*a*c))/(2*a)
- * 
- * @param positive whether positive or negative sqrt is used
- * 
- * @return x value of solution
- */
-  fixed solution(const bool positive) const {
-    return (-b+(positive?sqrt(denom):-sqrt(denom)))/da;
+  /**
+   * Calculate solution of quadratic equation using relation:
+   *   x = (-b +/- sqrt(b^2-4*a*c))/(2*a)
+   *
+   * @param positive whether positive or negative sqrt is used
+   *
+   * @return x value of solution
+   */
+  fixed
+  solution(const bool positive) const
+  {
+    return (-b + (positive ? sqrt(denom) : -sqrt(denom))) / da;
   }
+
   const fixed da;
   const fixed b;
   const fixed denom;
 };
-
 
 #endif
