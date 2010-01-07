6d52a790e309154bd22bf47bc6855cf648d103d7
Max Kellermann <max@duempel.org>
2010-01-07 18:01:24 +0100

    removed remaining files from directory "ex/"
    The "cloc" tool is a third-party software and shouldn't be part of the
    XCSoar repository.

--- ex/tools/mktags.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -f TAGS
-etags --declarations -a `find src/ Test/ -type f -name "*.cpp" | grep -v ~`
-etags --declarations -a `find src/ Test/ -type f -name "*.hpp" | grep -v ~`
-etags --declarations -a `find src/ Test/ -type f -name "*.h" | grep -v ~`
-etags --declarations -a `find src/ Test/ -type f -name "*.c" | grep -v ~`
