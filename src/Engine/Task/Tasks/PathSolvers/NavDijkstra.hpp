fcdbde925a74386f004c7a97f3285e7ba7549114
John Wharington <jwharington@gmail.com>
2010-01-13 11:43:47 +1100

    NavDijkstra documentation

--- src/Engine/Task/Tasks/PathSolvers/NavDijkstra.hpp
+++ src/Engine/Task/Tasks/PathSolvers/NavDijkstra.hpp
@@ -49,22 +49,65 @@ public:
 
 protected:
 
+  /** 
+   * Determine whether a finished path is valid
+   * 
+   * @param sp Point to check
+   * 
+   * @return True if this terminal point completes a valid solution
+   */
   virtual bool finish_satisfied(const ScanTaskPoint &sp) const {
     return true;
   }
 
+  /** 
+   * Retrieve point
+   * 
+   * @param sp Index to point to retrieve
+   * 
+   * @return Point at index position
+   */
   virtual const T &get_point(const ScanTaskPoint &sp) const = 0;
 
+  /** 
+   * Add edges from an origin node
+   * 
+   * @param dijkstra Dijkstra structure to add edges to
+   * @param curNode Origin node to add edges from
+   */
   virtual void add_edges(DijkstraTaskPoint &dijkstra,
                          const ScanTaskPoint &curNode) = 0;
 
+  /** 
+   * Determine whether a point is terminal (no further edges)
+   * 
+   * @param sp Point to test
+   * 
+   * @return True if point is terminal
+   */
   bool is_final(const ScanTaskPoint &sp) const {
     return sp.first+1== num_stages;
   }
+
+  /** 
+   * Determine whether a point is a starting point (no previous edges)
+   * 
+   * @param sp Point to test
+   * 
+   * @return True if point is in first layer
+   */
   bool is_first(const ScanTaskPoint &sp) const {
     return sp.first== 0;
   }
-  
+
+  /** 
+   * Iterate search algorithm
+   * 
+   * @param dijkstra Dijkstra structure to iterate
+   * @param max_steps Maximum number of steps to update
+   * 
+   * @return True if algorithm returns a terminal path or no path found
+   */
   bool distance_general(DijkstraTaskPoint &dijkstra, 
                         unsigned max_steps = 0-1) {
 
@@ -96,19 +139,38 @@ protected:
     return false; // No path found
   }
 
-  unsigned num_stages;
-  std::vector<T> solution;
-
+  /** 
+   * Distance function for free point
+   * 
+   * @param curNode Destination node
+   * @param currentLocation Origin location
+   * 
+   * @return Distance (flat) from origin to destination
+   */
   unsigned distance(const ScanTaskPoint &curNode,
                     const T &currentLocation) const {
     return get_point(curNode).flat_distance(currentLocation);
   }
 
+  /** 
+   * Distance function for edges
+   * 
+   * @param s1 Origin node
+   * @param s2 Destination node
+   * 
+   * @return Distance (flat) from origin to destination
+   */
   unsigned distance(const ScanTaskPoint &s1,
                     const ScanTaskPoint &s2) const {
     return get_point(s1).flat_distance(get_point(s2));
   }
 
+  /** 
+   * Determine optimal solution by backtracing the Dijkstra tree
+   * 
+   * @param dijkstra Dijkstra structure to retrieve solution from
+   * @param destination Terminal point to query
+   */
   void find_solution(const DijkstraTaskPoint &dijkstra, 
                      const ScanTaskPoint destination) {
     ScanTaskPoint p(destination); 
@@ -121,8 +183,8 @@ protected:
     } while ((p.second != p_last.second) || (p.first != p_last.first));
   }
 
-private:
-
+  unsigned num_stages; /**< Number of stages in search */
+  std::vector<T> solution; /**< Current solution */
 };
 
 #endif
