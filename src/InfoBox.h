aecfa064ec7bb31e65545add7c0144e0ed5269a4
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-01-23 00:00:52 +0100

    InfoBox: Added Documentation (partially)

--- src/InfoBox.h
+++ src/InfoBox.h
@@ -107,43 +107,140 @@ private:
   bool mSmallerFont;
 
   void InitializeDrawHelpers(void);
+  /**
+   * Paints the InfoBox title to the given canvas
+   * @param canvas The canvas to paint on
+   */
   void PaintTitle(Canvas &canvas);
+  /**
+   * Paints the InfoBox value to the given canvas
+   * @param canvas The canvas to paint on
+   */
   void PaintValue(Canvas &canvas);
+  /**
+   * Paints the InfoBox comment on the given canvas
+   * @param canvas The canvas to paint on
+   */
   void PaintComment(Canvas &canvas);
+  /**
+   * Paints the InfoBox selector on the given canvas if the InfoBox is focused
+   * @param canvas The canvas to paint on
+   */
   void PaintSelector(Canvas &canvas);
 
   // LRESULT CALLBACK InfoBoxWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 public:
+  /**
+   * Paints the InfoBox with borders, title, comment and value
+   */
   void Paint();
+  /**
+   * Calls the on_paint event handler to paint the selector
+   */
   void PaintFast(void);
   void PaintInto(Canvas &dest, int xoff, int yoff, int width, int height);
 
+  /**
+   * Sets the unit of the InfoBox value
+   * @param Value New unit of the InfoBox value
+   */
   void SetValueUnit(Units_t Value);
+  /**
+   * Sets the InfoBox title to the given Value
+   * @param Value New value of the InfoBox title
+   */
   void SetTitle(const TCHAR *Value);
+  /**
+   * Sets the InfoBox value to the given Value
+   * @param Value New value of the InfoBox value
+   */
   void SetValue(const TCHAR *Value);
+  /**
+   * Sets the InfoBox comment to the given Value
+   * @param Value New value of the InfoBox comment
+   */
   void SetComment(const TCHAR *Value);
   void SetSmallerFont(bool smallerFont);
 
+  /**
+   * Sets the InfoBox focus property to the given value and repaints if needed
+   * @param Value
+   */
   void SetFocus(bool Value);
 
   int GetBorderKind(void);
   int SetBorderKind(int Value);
 
+  /**
+   * Sets the color of the InfoBox value to the given value
+   * @param value New color of the InfoBox value
+   */
   void SetColor(int Value);
+  /**
+   * Sets the color of the InfoBox comment to the given value
+   * @param value New color of the InfoBox comment
+   */
   void SetColorBottom(int Value);
+  /**
+   * Sets the color of the InfoBox title to the given value
+   * @param value New color of the InfoBox title
+   */
   void SetColorTop(int Value);
 
+  /**
+   * Constructor of the InfoBox class
+   * @param Parent The parent ContainerWindow (usually MainWindow)
+   * @param X x-Coordinate of the InfoBox
+   * @param Y y-Coordinate of the InfoBox
+   * @param Width Width of the InfoBox
+   * @param Height Height of the InfoBox
+   */
   InfoBox(ContainerWindow &Parent, int X, int Y, int Width, int Height);
+  /** Destructor */
   ~InfoBox(void);
 
 protected:
+  /**
+   * This event handler is called when a key is pressed down while the InfoBox
+   * is focused
+   * @param key_code The code of the key that was pressed
+   * @return True if the event has been handled, False otherwise
+   */
   virtual bool on_key_down(unsigned key_code);
+  /**
+   * This event handler is called when a mouse button is pressed down over
+   * the InfoBox
+   * @param x x-Coordinate where the mouse button was pressed
+   * @param y y-Coordinate where the mouse button was pressed
+   * @return True if the event has been handled, False otherwise
+   */
   virtual bool on_mouse_down(int x, int y);
+  /**
+   * This event handler is called when a mouse button is double clicked over
+   * the InfoBox
+   * @param x x-Coordinate where the mouse button was pressed
+   * @param y y-Coordinate where the mouse button was pressed
+   * @return True if the event has been handled, False otherwise
+   */
   virtual bool on_mouse_double(int x, int y);
+  /**
+   * This event handler is called when the InfoBox needs to be repainted
+   * @param canvas The canvas to paint on
+   */
   virtual void on_paint(Canvas &canvas);
+  /**
+   * This event handler is called when the InfoBox is getting focused
+   */
   virtual bool on_setfocus();
+  /**
+   * This event handler is called when the InfoBox lost focus
+   */
   virtual bool on_killfocus();
+  /**
+   * This event handler is called when a timer is triggered
+   * @param id Id of the timer that triggered the handler
+   */
   virtual bool on_timer(timer_t id);
 };
 
