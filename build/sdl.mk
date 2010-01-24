f66c4e5385ff4dd8f3006b982c42318572d542ee
Max Kellermann <max@duempel.org>
2010-01-24 17:34:20 +0100

    Makefile: moved "sdl.mk" include up
    Must be included before test.mk, because test.mk depends on the
    variable ENABLE_SDL.

--- build/sdl.mk
+++ build/sdl.mk
@@ -2,19 +2,13 @@ ENABLE_SDL ?= $(call bool_not,$(HAVE_WIN32))
 
 ifeq ($(ENABLE_SDL),y)
 ifeq ($(TARGET),UNIX)
-CPPFLAGS += $(shell pkg-config --cflags sdl)
-LDLIBS += $(shell pkg-config --libs sdl)
+SDL_CPPFLAGS := $(shell pkg-config --cflags sdl)
+SDL_LDLIBS := $(shell pkg-config --libs sdl)
 else
-CPPFLAGS += -I/usr/local/i586-mingw32msvc/include
-LDLIBS += -L/usr/local/i586-mingw32msvc/lib -lSDL
+SDL_CPPFLAGS := -I/usr/local/i586-mingw32msvc/include
+SDL_LDLIBS := -L/usr/local/i586-mingw32msvc/lib -lSDL
 endif
 
-CPPFLAGS += -DENABLE_SDL
-LDLIBS += -lSDL_gfx -lSDL_ttf
-
-XCSOAR_SOURCES += $(SRC)/Screen/Timer.cpp
-else
-XCSOAR_SOURCES += \
-	$(SRC)/Screen/BufferCanvas.cpp \
-	$(SRC)/Screen/PaintCanvas.cpp
+SDL_CPPFLAGS += -DENABLE_SDL
+SDL_LDLIBS += -lSDL_gfx -lSDL_ttf
 endif
