cac1d7cee93a5b607b2cc8617cbf7df21f7f0d0a
Max Kellermann <max@duempel.org>
2010-01-28 22:56:11 +0100

    ButtonLabel: don't store menu button positions in registry
    There is no use interface to tweak that, and it looks like this is
    only a cache.

--- src/ButtonLabel.cpp
+++ src/ButtonLabel.cpp
@@ -41,7 +41,6 @@ Copyright_License {
 #include "Language.hpp"
 #include "Screen/Animation.hpp"
 #include "Screen/Layout.hpp"
-#include "Registry.hpp"
 #include "InputEvents.h"
 #include "Compatibility/string.h"
 #include "Asset.hpp"
@@ -66,88 +65,58 @@ void
 ButtonLabel::GetButtonPosition(unsigned i, RECT rc, int *x, int *y,
     int *sizex, int *sizey)
 {
-  TCHAR reggeompx[50];
-  TCHAR reggeompy[50];
-  TCHAR reggeomsx[50];
-  TCHAR reggeomsy[50];
+  int hwidth = (rc.right - rc.left) / 4;
+  int hheight = (rc.bottom - rc.top) / 4;
 
-  _stprintf(reggeompx, TEXT("ScreenButtonPosX%d"), i);
-  _stprintf(reggeompy, TEXT("ScreenButtonPosY%d"), i);
-  _stprintf(reggeomsx, TEXT("ScreenButtonSizeX%d"), i);
-  _stprintf(reggeomsy, TEXT("ScreenButtonSizeY%d"), i);
+  switch (ButtonLabelGeometry) {
+  case 0: // portrait
+    if (i == 0) {
+      *sizex = IBLSCALE(52);
+      *sizey = IBLSCALE(37);
+      *x = rc.left - (*sizex); // JMW make it offscreen for now
+      *y = rc.bottom - (*sizey);
+    } else if (i < 5) {
+      *sizex = IBLSCALE(52);
+      *sizey = IBLSCALE(40);
+      *x = rc.left + 3 + hwidth * (i - 1);
+      *y = rc.bottom - (*sizey);
+    } else {
+      *sizex = IBLSCALE(80);
+      *sizey = IBLSCALE(40);
+      *x = rc.right - (*sizex);
+      int k = rc.bottom - rc.top - IBLSCALE(46);
 
-  GetFromRegistry(reggeompx,*x);
-  GetFromRegistry(reggeompy,*y);
-  GetFromRegistry(reggeomsx,*sizex);
-  GetFromRegistry(reggeomsy,*sizey);
-
-  bool geometrychanged = true; // JMW testing
-
-  if ((*sizex == 0) || (*sizey == 0) || geometrychanged) {
-    // not defined in registry so go with defaults
-    // these will be saved back to registry
-    int hwidth = (rc.right - rc.left) / 4;
-    int hheight = (rc.bottom - rc.top) / 4;
-
-    switch (ButtonLabelGeometry) {
-    case 0: // portrait
-      if (i == 0) {
-        *sizex = IBLSCALE(52);
-        *sizey = IBLSCALE(37);
-        *x = rc.left - (*sizex); // JMW make it offscreen for now
-        *y = rc.bottom - (*sizey);
+      if (is_altair()) {
+        k = rc.bottom - rc.top;
+        // JMW need upside down button order for rotated Altair
+        *y = rc.bottom - (i - 5) * k / 5 - (*sizey) - IBLSCALE(20);
       } else {
-        if (i < 5) {
-          *sizex = IBLSCALE(52);
-          *sizey = IBLSCALE(40);
-          *x = rc.left + 3 + hwidth * (i - 1);
-          *y = rc.bottom - (*sizey);
-        } else {
-          *sizex = IBLSCALE(80);
-          *sizey = IBLSCALE(40);
-          *x = rc.right - (*sizex);
-          int k = rc.bottom - rc.top - IBLSCALE(46);
-
-          if (is_altair()) {
-            k = rc.bottom - rc.top;
-            // JMW need upside down button order for rotated Altair
-            *y = rc.bottom - (i - 5) * k / 5 - (*sizey) - IBLSCALE(20);
-          } else {
-            *y = rc.top + (i - 5) * k / 6 + (*sizey / 2 + IBLSCALE(3));
-          }
-        }
+        *y = rc.top + (i - 5) * k / 6 + (*sizey / 2 + IBLSCALE(3));
       }
-      break;
-
-    case 1: // landscape
-      hwidth = (rc.right - rc.left) / 5;
-      hheight = (rc.bottom - rc.top) / 5;
-
-      if (i == 0) {
-        *sizex = IBLSCALE(52);
-        *sizey = IBLSCALE(20);
-        *x = rc.left - (*sizex); // JMW make it offscreen for now
-        *y = (rc.top);
-      } else {
-        if (i < 5) {
-          *sizex = IBLSCALE(52);
-          *sizey = is_altair() ? IBLSCALE(20) : IBLSCALE(35);
-          *x = rc.left + 3;
-          *y = (rc.top + hheight * i - (*sizey) / 2);
-        } else {
-          *sizex = IBLSCALE(60);
-          *sizey = is_altair() ? IBLSCALE(40) : IBLSCALE(35);
-          *x = rc.left + hwidth * (i - 5);
-          *y = (rc.bottom - (*sizey));
-        }
-      }
-      break;
     }
+    break;
 
-    SetToRegistry(reggeompx, *x);
-    SetToRegistry(reggeompy, *y);
-    SetToRegistry(reggeomsx, *sizex);
-    SetToRegistry(reggeomsy, *sizey);
+  case 1: // landscape
+    hwidth = (rc.right - rc.left) / 5;
+    hheight = (rc.bottom - rc.top) / 5;
+
+    if (i == 0) {
+      *sizex = IBLSCALE(52);
+      *sizey = IBLSCALE(20);
+      *x = rc.left - (*sizex); // JMW make it offscreen for now
+      *y = (rc.top);
+    } else if (i < 5) {
+      *sizex = IBLSCALE(52);
+      *sizey = is_altair() ? IBLSCALE(20) : IBLSCALE(35);
+      *x = rc.left + 3;
+      *y = (rc.top + hheight * i - (*sizey) / 2);
+    } else {
+      *sizex = IBLSCALE(60);
+      *sizey = is_altair() ? IBLSCALE(40) : IBLSCALE(35);
+      *x = rc.left + hwidth * (i - 5);
+      *y = (rc.bottom - (*sizey));
+    }
+    break;
   }
 }
 
