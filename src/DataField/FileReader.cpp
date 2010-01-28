276a69efd3f33914b6511b40ac474731f75a37ad
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-01-28 11:19:16 +0100

    DataField/FileReader: Corrected Format and Indentation

--- src/DataField/FileReader.cpp
+++ src/DataField/FileReader.cpp
@@ -53,8 +53,11 @@ Copyright_License {
 #include <fnmatch.h>
 #endif
 
-bool IsDots(const TCHAR* str) {
-  if(_tcscmp(str,TEXT(".")) && _tcscmp(str,TEXT(".."))) return false;
+bool
+IsDots(const TCHAR* str)
+{
+  if (_tcscmp(str, TEXT(".")) && _tcscmp(str, TEXT("..")))
+    return false;
   return true;
 }
 
@@ -64,64 +67,66 @@ DataFieldFileReader::GetAsInteger(void) const
   return mValue;
 }
 
-
-int DataFieldFileReader::SetAsInteger(int Value){
+int
+DataFieldFileReader::SetAsInteger(int Value)
+{
   Set(Value);
   return mValue;
 }
 
-
-
-void DataFieldFileReader::ScanDirectoryTop(const TCHAR* filter) {
+void
+DataFieldFileReader::ScanDirectoryTop(const TCHAR* filter)
+{
 
 #ifdef ALTAIRSYNC
   ScanDirectories(TEXT("\\NOR Flash"),filter);
 #else
   TCHAR buffer[MAX_PATH] = TEXT("\0");
   LocalPath(buffer);
-  ScanDirectories(buffer,filter);
+  ScanDirectories(buffer, filter);
 #ifndef HAVE_POSIX
 #ifndef GNAV
 #if !defined(WINDOWSPC) && !defined(__GNUC__)
 #ifndef OLDPPC
-// non altair, (non windowspc e non mingw32) e non ppc2002
+  // non altair, (non windowspc e non mingw32) e non ppc2002
   static bool first = true;
 
-  bool bContinue = true;     // If true, continue searching
-                             // If false, stop searching.
-  HANDLE hFlashCard;         // Search handle for storage cards
+  bool bContinue = true; // If true, continue searching
+  // If false, stop searching.
+  HANDLE hFlashCard; // Search handle for storage cards
   WIN32_FIND_DATA FlashCardTmp; // Structure for storing card
-                                      // information temporarily
+  // information temporarily
   TCHAR FlashPath[MAX_PATH];
 
-  hFlashCard = FindFirstFlashCard (&FlashCardTmp);
+  hFlashCard = FindFirstFlashCard(&FlashCardTmp);
   if (hFlashCard == INVALID_HANDLE_VALUE) {
     Sort();
     return;
   }
-  _stprintf(FlashPath,TEXT("/%s/%S"),FlashCardTmp.cFileName, XCSDATADIR); // VENTA3 CHECK should it be double //??
-  ScanDirectories(FlashPath,filter);
+  // VENTA3 CHECK should it be double //??
+  _stprintf(FlashPath, TEXT("/%s/%S"), FlashCardTmp.cFileName, XCSDATADIR);
+  ScanDirectories(FlashPath, filter);
   if (first) {
     StartupStore(TEXT("%s\n"), FlashPath);
   }
   while (bContinue) {
-      // Search for the next storage card.
-      bContinue = FindNextFlashCard (hFlashCard, &FlashCardTmp);
-      if (bContinue) {
-        _stprintf(FlashPath,TEXT("/%s/%S"),FlashCardTmp.cFileName, XCSDATADIR);
-        ScanDirectories(FlashPath,filter);
-        if (first) {
-          StartupStore(TEXT("%s\n"), FlashPath);
-        }
+    // Search for the next storage card.
+    bContinue = FindNextFlashCard(hFlashCard, &FlashCardTmp);
+    if (bContinue) {
+      _stprintf(FlashPath, TEXT("/%s/%S"), FlashCardTmp.cFileName, XCSDATADIR);
+      ScanDirectories(FlashPath, filter);
+      if (first) {
+        StartupStore(TEXT("%s\n"), FlashPath);
       }
+    }
   }
-  FindClose (hFlashCard);          // Close the search handle.
+  FindClose(hFlashCard); // Close the search handle.
 
   first = false;
 #endif
 #else // mingw32 and PC
 #ifndef WINDOWSPC
-// VENTA2-FIVV SDCARD FIX
+  // VENTA2-FIVV SDCARD FIX
 #ifdef FIVV
   // Scan only XCSoarData in the root directory where the xcsoar.exe is placed!
   // In large SD card this was leading great confusion since .dat files are ALSO
@@ -132,8 +137,8 @@ void DataFieldFileReader::ScanDirectoryTop(const TCHAR* filter) {
     ScanDirectories(tBuffer,filter);
   }
 #else
- // To Do: RLD appending "XCSoarData" to card names is a "quick fix" for the upcoming stable release 5.2.3? and
- // the better solution involves changing multiple files, and will remove this list altogether
+  // To Do: RLD appending "XCSoarData" to card names is a "quick fix" for the upcoming stable release 5.2.3? and
+  // the better solution involves changing multiple files, and will remove this list altogether
   ScanDirectories(TEXT("\\Carte de stockage\\XCSoarData"),filter);
   ScanDirectories(TEXT("\\Storage Card\\XCSoarData"),filter);
   ScanDirectories(TEXT("\\SD-MMC Card\\XCSoarData"),filter);
@@ -150,14 +155,12 @@ void DataFieldFileReader::ScanDirectoryTop(const TCHAR* filter) {
 #endif // NOT ALTAIRSYNC
 #endif /* !HAVE_POSIX */
   Sort();
-
 }
 
-
-bool DataFieldFileReader::ScanDirectories(const TCHAR* sPath,
-					  const TCHAR* filter) {
+bool
+DataFieldFileReader::ScanDirectories(const TCHAR* sPath, const TCHAR* filter)
+{
 #ifdef HAVE_POSIX
-
   DIR *dir = opendir(sPath);
   if (dir == NULL)
     return false;
@@ -183,74 +186,72 @@ bool DataFieldFileReader::ScanDirectories(const TCHAR* sPath,
     else if (S_ISREG(st.st_mode) && fnmatch(filter, ent->d_name, 0))
       addFile(ent->d_name, FileName);
   }
-
 #else /* !HAVE_POSIX */
-
-  HANDLE hFind;  // file handle
+  HANDLE hFind; // file handle
   WIN32_FIND_DATA FindFileData;
 
   TCHAR DirPath[MAX_PATH];
   TCHAR FileName[MAX_PATH];
 
   if (sPath) {
-    _tcscpy(DirPath,sPath);
-    _tcscpy(FileName,sPath);
+    _tcscpy(DirPath, sPath);
+    _tcscpy(FileName, sPath);
   } else {
-    DirPath[0]= 0;
-    FileName[0]= 0;
+    DirPath[0] = 0;
+    FileName[0] = 0;
   }
 
   ScanFiles(FileName, filter);
 
-  _tcscat(DirPath,TEXT("\\"));
-  _tcscat(FileName,TEXT("\\*"));
+  _tcscat(DirPath, TEXT("\\"));
+  _tcscat(FileName, TEXT("\\*"));
 
-  hFind = FindFirstFile(FileName,&FindFileData); // find the first file
-  if(hFind == INVALID_HANDLE_VALUE) {
+  hFind = FindFirstFile(FileName, &FindFileData); // find the first file
+  if (hFind == INVALID_HANDLE_VALUE)
     return false;
-  }
-  _tcscpy(FileName,DirPath);
 
-  if(!IsDots(FindFileData.cFileName)) {
-    _tcscat(FileName,FindFileData.cFileName);
+  _tcscpy(FileName, DirPath);
 
-    if((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+  if (!IsDots(FindFileData.cFileName)) {
+    _tcscat(FileName, FindFileData.cFileName);
+
+    if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
       // we have found a directory, recurse
       //      if (!IsSystemDirectory(FileName)) {
-	if(!ScanDirectories(FileName,filter)) {
-	  // none deeper
-	}
-	//      }
+      if (!ScanDirectories(FileName, filter)) {
+        // none deeper
+      }
+      //      }
     }
   }
-  _tcscpy(FileName,DirPath);
+  _tcscpy(FileName, DirPath);
 
   bool bSearch = true;
-  while(bSearch) { // until we finds an entry
-    if(FindNextFile(hFind,&FindFileData)) {
-      if(IsDots(FindFileData.cFileName)) continue;
-      if((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-	// we have found a directory, recurse
-	_tcscat(FileName,FindFileData.cFileName);
-	//	if (!IsSystemDirectory(FileName)) {
-	  if(!ScanDirectories(FileName,filter)) {
-	    // none deeper
-	  }
-	  //	}
+  while (bSearch) { // until we finds an entry
+    if (FindNextFile(hFind, &FindFileData)) {
+      if (IsDots(FindFileData.cFileName))
+        continue;
+      if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+        // we have found a directory, recurse
+        _tcscat(FileName, FindFileData.cFileName);
+        //	if (!IsSystemDirectory(FileName)) {
+        if (!ScanDirectories(FileName, filter)) {
+          // none deeper
+        }
+        //	}
       }
-      _tcscpy(FileName,DirPath);
-    }
-    else {
-      if(GetLastError() == ERROR_NO_MORE_FILES) // no more files there
-	bSearch = false;
+      _tcscpy(FileName, DirPath);
+    } else {
+      if (GetLastError() == ERROR_NO_MORE_FILES) // no more files there
+        bSearch = false;
       else {
-	// some error occured, close the handle and return false
-	FindClose(hFind);
-	return false;
+        // some error occured, close the handle and return false
+        FindClose(hFind);
+        return false;
       }
     }
   }
-  FindClose(hFind);  // closing file handle
+  FindClose(hFind); // closing file handle
 
 #endif /* !HAVE_POSIX */
 
@@ -258,96 +259,97 @@ bool DataFieldFileReader::ScanDirectories(const TCHAR* sPath,
 }
 
 #ifndef HAVE_POSIX
-bool DataFieldFileReader::ScanFiles(const TCHAR* sPath,
-				    const TCHAR* filter) {
-  HANDLE hFind;  // file handle
+bool
+DataFieldFileReader::ScanFiles(const TCHAR* sPath, const TCHAR* filter)
+{
+  HANDLE hFind; // file handle
   WIN32_FIND_DATA FindFileData;
 
   TCHAR DirPath[MAX_PATH];
   TCHAR FileName[MAX_PATH];
 
-  if (sPath) {
-    _tcscpy(DirPath,sPath);
-  } else {
-    DirPath[0]= 0;
-  }
-  _tcscat(DirPath,TEXT("\\"));
-  _tcscat(DirPath,filter);
-  if (sPath) {
-    _tcscpy(FileName,sPath);
-  } else {
-    FileName[0]= 0;
-  }
-  _tcscat(FileName,TEXT("\\"));
+  if (sPath)
+    _tcscpy(DirPath, sPath);
+  else
+    DirPath[0] = 0;
 
-  hFind = FindFirstFile(DirPath,&FindFileData); // find the first file
-  if(hFind == INVALID_HANDLE_VALUE) return false;
-  _tcscpy(DirPath,FileName);
+  _tcscat(DirPath, TEXT("\\"));
+  _tcscat(DirPath, filter);
 
+  if (sPath)
+    _tcscpy(FileName, sPath);
+  else
+    FileName[0] = 0;
+
+  _tcscat(FileName, TEXT("\\"));
+
+  hFind = FindFirstFile(DirPath, &FindFileData); // find the first file
+  if (hFind == INVALID_HANDLE_VALUE)
+    return false;
+
+  _tcscpy(DirPath, FileName);
 
   // found first one
-  if(!IsDots(FindFileData.cFileName)) {
-    _tcscat(FileName,FindFileData.cFileName);
+  if (!IsDots(FindFileData.cFileName)) {
+    _tcscat(FileName, FindFileData.cFileName);
 
-    if((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+    if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
       // do nothing
-    }
-    else {
+    } else {
       // DO SOMETHING WITH FileName
       if (checkFilter(FindFileData.cFileName, filter)) {
-	addFile(FindFileData.cFileName, FileName);
+        addFile(FindFileData.cFileName, FileName);
       }
     }
-    _tcscpy(FileName,DirPath);
+    _tcscpy(FileName, DirPath);
   }
 
   bool bSearch = true;
-  while(bSearch) { // until we finds an entry
-    if(FindNextFile(hFind,&FindFileData)) {
-      if(IsDots(FindFileData.cFileName)) continue;
-      _tcscat(FileName,FindFileData.cFileName);
+  while (bSearch) { // until we finds an entry
+    if (FindNextFile(hFind, &FindFileData)) {
+      if (IsDots(FindFileData.cFileName))
+        continue;
+      _tcscat(FileName, FindFileData.cFileName);
 
-      if((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-	// do nothing
+      if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+        // do nothing
+      } else {
+        // DO SOMETHING WITH FileName
+        if (checkFilter(FindFileData.cFileName, filter)) {
+          addFile(FindFileData.cFileName, FileName);
+        }
       }
+      _tcscpy(FileName, DirPath);
+    } else {
+      if (GetLastError() == ERROR_NO_MORE_FILES) // no more files there
+        bSearch = false;
       else {
-	// DO SOMETHING WITH FileName
-	if (checkFilter(FindFileData.cFileName, filter)) {
-	  addFile(FindFileData.cFileName, FileName);
-	}
+        // some error occured, close the handle and return false
+        FindClose(hFind);
+        return false;
       }
-      _tcscpy(FileName,DirPath);
     }
-    else {
-      if(GetLastError() == ERROR_NO_MORE_FILES) // no more files there
-	bSearch = false;
-      else {
-	// some error occured, close the handle and return false
-	FindClose(hFind);
-	return false;
-      }
-
-    }
-
   }
-  FindClose(hFind);  // closing file handle
+  FindClose(hFind); // closing file handle
 
   return true;
 }
 #endif /* !HAVE_POSIX */
 
-void DataFieldFileReader::Lookup(const TCHAR *Text) {
-  int i=0;
+void
+DataFieldFileReader::Lookup(const TCHAR *Text)
+{
+  int i = 0;
   mValue = 0;
-  for (i=1; i<(int)nFiles; i++) {
-    if (_tcscmp(Text,fields[i].mTextPathFile)==0) {
+  for (i = 1; i < (int)nFiles; i++) {
+    if (_tcscmp(Text, fields[i].mTextPathFile) == 0) {
       mValue = i;
     }
   }
 }
 
 int
- DataFieldFileReader::GetNumFiles(void) const
+DataFieldFileReader::GetNumFiles(void) const
 {
   return nFiles;
 }
@@ -355,15 +357,16 @@ int
 const TCHAR *
 DataFieldFileReader::GetPathFile(void) const
 {
-  if ((mValue<=nFiles)&&(mValue)) {
+  if ((mValue <= nFiles) && (mValue)) {
     return fields[mValue].mTextPathFile;
   }
   return TEXT("\0");
 }
 
 #ifndef HAVE_POSIX /* we use fnmatch() on POSIX */
-bool DataFieldFileReader::checkFilter(const TCHAR *filename,
-				      const TCHAR *filter) {
+bool
+DataFieldFileReader::checkFilter(const TCHAR *filename, const TCHAR *filter)
+{
   TCHAR *ptr;
   TCHAR upfilter[MAX_PATH];
   // checks if the filename matches the filter exactly
@@ -372,12 +375,12 @@ bool DataFieldFileReader::checkFilter(const TCHAR *filename,
     // invalid or short filter, pass
     return true;
 
-  _tcscpy(upfilter,filter+1);
+  _tcscpy(upfilter, filter + 1);
 
   // check if trailing part of filter (*.exe => .exe) matches end
   ptr = _tcsstr(filename, upfilter);
   if (ptr) {
-    if (_tcslen(ptr)==_tcslen(upfilter)) {
+    if (_tcslen(ptr) == _tcslen(upfilter)) {
       return true;
     }
   }
@@ -385,7 +388,7 @@ bool DataFieldFileReader::checkFilter(const TCHAR *filename,
   _tcsupr(upfilter);
   ptr = _tcsstr(filename, upfilter);
   if (ptr) {
-    if (_tcslen(ptr)==_tcslen(upfilter)) {
+    if (_tcslen(ptr) == _tcslen(upfilter)) {
       return true;
     }
   }
@@ -394,14 +397,17 @@ bool DataFieldFileReader::checkFilter(const TCHAR *filename,
 }
 #endif /* !HAVE_POSIX */
 
-void DataFieldFileReader::addFile(const TCHAR *Text,
-				  const TCHAR *PText) {
+void
+DataFieldFileReader::addFile(const TCHAR *Text, const TCHAR *PText)
+{
   // TODO enhancement: remove duplicates?
-  if (nFiles<DFE_MAX_FILES) {
-    fields[nFiles].mTextFile = (TCHAR*)malloc((_tcslen(Text)+1)*sizeof(TCHAR));
+  if (nFiles < DFE_MAX_FILES) {
+    fields[nFiles].mTextFile =
+        (TCHAR*)malloc((_tcslen(Text) + 1) * sizeof(TCHAR));
     _tcscpy(fields[nFiles].mTextFile, Text);
 
-    fields[nFiles].mTextPathFile = (TCHAR*)malloc((_tcslen(PText)+1)*sizeof(TCHAR));
+    fields[nFiles].mTextPathFile =
+        (TCHAR*)malloc((_tcslen(PText) + 1) * sizeof(TCHAR));
     _tcscpy(fields[nFiles].mTextPathFile, PText);
 
     nFiles++;
@@ -411,64 +417,64 @@ void DataFieldFileReader::addFile(const TCHAR *Text,
 const TCHAR *
 DataFieldFileReader::GetAsString(void) const
 {
-  if (mValue<nFiles) {
-    return(fields[mValue].mTextFile);
-  } else {
+  if (mValue < nFiles)
+    return (fields[mValue].mTextFile);
+  else
     return NULL;
-  }
 }
 
-
-void DataFieldFileReader::Set(int Value){
-  if (Value<=(int)nFiles) {
+void
+DataFieldFileReader::Set(int Value)
+{
+  if (Value <= (int)nFiles)
     mValue = Value;
-  }
-  if (Value<0) {
+  if (Value < 0)
     mValue = 0;
-  }
 }
 
-
-void DataFieldFileReader::Inc(void){
-  if (mValue<nFiles-1) {
+void
+DataFieldFileReader::Inc(void)
+{
+  if (mValue < nFiles - 1) {
     mValue++;
     (mOnDataAccess)(this, daChange);
   }
 }
 
-
-void DataFieldFileReader::Dec(void){
-  if (mValue>0) {
+void
+DataFieldFileReader::Dec(void)
+{
+  if (mValue > 0) {
     mValue--;
     (mOnDataAccess)(this, daChange);
   }
 }
 
-
-static int _cdecl DataFieldFileReaderCompare(const void *elem1,
-                                             const void *elem2 ){
+static int _cdecl
+DataFieldFileReaderCompare(const void *elem1, const void *elem2)
+{
   return _tcscmp(((DataFieldFileReaderEntry*)elem1)->mTextFile,
                  ((DataFieldFileReaderEntry*)elem2)->mTextFile);
 }
 
-
-void DataFieldFileReader::Sort(void){
+void
+DataFieldFileReader::Sort(void)
+{
   qsort(fields+1, nFiles-1, sizeof(DataFieldFileReaderEntry),
         DataFieldFileReaderCompare);
 }
 
-int DataFieldFileReader::CreateComboList(void) {
-  unsigned int i=0;
-  for (i=0; i < nFiles; i++){
-    mComboList.ComboPopupItemList[i] = mComboList.CreateItem(
-                                          i,
-                                          i,
-                                          fields[i].mTextFile,
-                                          fields[i].mTextFile);
+int
+DataFieldFileReader::CreateComboList(void)
+{
+  unsigned int i = 0;
+  for (i = 0; i < nFiles; i++) {
+    mComboList.ComboPopupItemList[i] =
+        mComboList.CreateItem(i, i, fields[i].mTextFile, fields[i].mTextFile);
     if (i == mValue) {
-      mComboList.ComboPopupItemSavedIndex=i;
+      mComboList.ComboPopupItemSavedIndex = i;
     }
   }
-  mComboList.ComboPopupItemCount=i;
+  mComboList.ComboPopupItemCount = i;
   return mComboList.ComboPopupItemCount;
 }
--- src/DataField/FileReader.hpp
+++ src/DataField/FileReader.hpp
@@ -45,45 +45,47 @@ Copyright_License {
 
 #define DFE_MAX_FILES 100
 
-typedef struct {
+typedef struct
+{
   TCHAR *mTextFile;
   TCHAR *mTextPathFile;
 } DataFieldFileReaderEntry;
 
-class DataFieldFileReader: public DataField {
-
- private:
+class DataFieldFileReader: public DataField
+{
+private:
   unsigned int nFiles;
   unsigned int mValue;
   DataFieldFileReaderEntry fields[DFE_MAX_FILES];
 
-  public:
+public:
   DataFieldFileReader(const TCHAR *EditFormat,
 		      const TCHAR *DisplayFormat,
 		      void(*OnDataAccess)(DataField *Sender, DataAccessKind_t Mode)):
-      DataField(EditFormat, DisplayFormat, OnDataAccess){
-      mValue = 0;
-      fields[0].mTextFile= NULL;
-      fields[0].mTextPathFile= NULL; // first entry always exists and is blank
-      nFiles = 1;
+    DataField(EditFormat, DisplayFormat, OnDataAccess) {
+    mValue = 0;
+    fields[0].mTextFile = NULL;
+    fields[0].mTextPathFile = NULL; // first entry always exists and is blank
+    nFiles = 1;
 
-      SupportCombo=true;
-      (mOnDataAccess)(this, daGet);
+    SupportCombo = true;
+    (mOnDataAccess)(this, daGet);
+  }
 
-    };
-    ~DataFieldFileReader() {
-	for (unsigned int i=1; i<nFiles; i++) {
-	  if (fields[i].mTextFile) {
-	    free(fields[i].mTextFile);
-	    fields[i].mTextFile= NULL;
-	  }
-	  if (fields[i].mTextPathFile) {
-	    free(fields[i].mTextPathFile);
-	    fields[i].mTextPathFile= NULL;
-	  }
-	}
-	nFiles = 1;
+  ~DataFieldFileReader()
+  {
+    for (unsigned int i = 1; i < nFiles; i++) {
+      if (fields[i].mTextFile) {
+        free(fields[i].mTextFile);
+        fields[i].mTextFile = NULL;
+      }
+      if (fields[i].mTextPathFile) {
+        free(fields[i].mTextPathFile);
+        fields[i].mTextPathFile = NULL;
+      }
     }
+    nFiles = 1;
+  }
 
   void Inc(void);
   void Dec(void);
@@ -114,7 +116,6 @@ class DataFieldFileReader: public DataField {
  protected:
   bool ScanFiles(const TCHAR *pattern, const TCHAR *filter);
   bool ScanDirectories(const TCHAR *pattern, const TCHAR *filter);
-
 };
 
 #endif
