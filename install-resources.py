4de86bc4662df747a9704356d5ac7f9a4ab756cc
Max Kellermann <max@duempel.org>
2010-02-04 21:59:03 +0100

    install-resources.py: updated for new directory structure

--- install-resources.py
+++ install-resources.py
@@ -11,7 +11,7 @@ resource_re = re.compile(r'^(ID[BR]_\S+)\s+(\S+)\s+DISCARDABLE\s+"([^"]+)"\s*$')
 macro_re = re.compile(r'^#define\s+(ID[BR]_\S+)\s+(\d+)\s*$')
 
 resources = {}
-for line in file('Common/Source/XCSoar.rc'):
+for line in file('src/XCSoar.rc'):
     m = resource_re.match(line)
     if m is None: continue
     name, path = m.group(1), m.group(3)
@@ -24,11 +24,11 @@ try:
 except OSError:
     pass
 
-for line in file('Common/Source/resource.h'):
+for line in file('src/resource.h'):
     m = macro_re.match(line)
     if m is None: continue
     name, id = m.group(1), m.group(2)
     if not name in resources: continue
-    src_path = os.path.join('Common/Source', resources[name])
+    src_path = os.path.join('src', resources[name])
     dest_path = os.path.join(dest, id)
     shutil.copy(src_path, dest_path)
