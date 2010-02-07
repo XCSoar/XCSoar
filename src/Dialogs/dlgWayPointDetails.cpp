ce1579a4d6c7088482db38a7f48c21397a7a6927
Max Kellermann <max@duempel.org>
2010-02-07 16:05:14 +0100

    dlgWayPointDetails: disable VOIMAGE on PNA
    Another #if correction.

--- src/Dialogs/dlgWayPointDetails.cpp
+++ src/Dialogs/dlgWayPointDetails.cpp
@@ -62,7 +62,7 @@ Copyright_License {
 
 #include <assert.h>
 
-#if defined(_WIN32_WCE) && !defined(GNAV)
+#if defined(_WIN32_WCE) && !defined(GNAV) && !defined(PNA)
     #include "Screen/VOIMAGE.h"
 #endif
 
@@ -77,7 +77,7 @@ static BOOL hasimage1 = false;
 static BOOL hasimage2 = false;
 static const Waypoint *selected_waypoint = NULL;
 
-#if defined(_WIN32_WCE) && !defined(GNAV)
+#if defined(_WIN32_WCE) && !defined(GNAV) && !defined(PNA)
     static CVOImage jpgimage1;
     static CVOImage jpgimage2;
 #endif
@@ -368,7 +368,7 @@ OnImagePaint(WindowControl *Sender, Canvas &canvas)
 {
   (void)Sender;
 
-#if defined(_WIN32_WCE) && !defined(GNAV)
+#if defined(_WIN32_WCE) && !defined(GNAV) && !defined(PNA)
   if (page == 4)
     jpgimage1.Draw(canvas, 0, 0, -1, -1);
 
@@ -585,7 +585,7 @@ dlgWayPointDetailsShowModal(const Waypoint& way_point)
   if (wb)
     wb->SetOnClickNotify(OnRemoveFromTaskClicked);
 
-#if defined(_WIN32_WCE) && !defined(GNAV)
+#if defined(_WIN32_WCE) && !defined(GNAV) && !defined(PNA)
   VirtualCanvas reference_canvas;
   hasimage1 = jpgimage1.Load(reference_canvas, path_modis);
   hasimage2 = jpgimage2.Load(reference_canvas, path_google);
