d2c6badd828c7de9103824ddc6341d1d0cd72f9d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-02-19 22:56:41 +0100

    Defines: Corrected Documentation

--- src/Defines.h
+++ src/Defines.h
@@ -38,41 +38,45 @@ Copyright_License {
 
 #ifndef XCSOAR_DEFINES_H
 #define XCSOAR_DEFINES_H
-/* VENTA3
- * General defines for XCSoar
- * Paolo Ventafridda
- *
- * We put here whatever can be fine tuned. Each .cpp using one of these values should
- * include this file at the beginning.
- *
- * Some of these values could be good candidates for system config menus
- */
 
 #define INVALID_GR	999
 
-/*
+/**
  * Over this GR value, alternates and Final GR are not showing numbers anymore and
  * the variable is set to INVALIDVALUE
  */
 #define ALTERNATE_MAXVALIDGR 200
 
-/*
+/**
  * Normally GlidePolar::bestld * 0.7 makes an efficiency 40 become 28 as a safety margin
  */
 #define SAFELD_FACTOR	0.7
-#define ALTERNATE_QUIETMARGIN 250 // meters - Quiet sound mode under safetyaltitude+QUIETMARGIN
 
-/*
- * Additional height over arrival altitude needed for safety searches and green&blue Ok status
- * This is really needed since car GPS or non-baro GPS may be in error of several meters, and
- * we can't accept a +30m over safety if the gps altitude is wrong!!
+/**
+ * Quiet sound mode under safetyaltitude + QUIETMARGIN
+ * (in meters)
  */
-#define ALTERNATE_OVERSAFETY  100 // meters
-#define ALTERNATE_MAXRANGE    100 // km - bestalternate search is within this maximum limit
-#define DYNABOXTIME 3.0  // seconds between infobox dynamic flipping
+#define ALTERNATE_QUIETMARGIN 250
 
-/*
- * DOUBLECLICKINTERVAL is a general purpouse timing, used by both VK and synthetic double click
+/**
+ * Additional height over arrival altitude needed for safety searches and green
+ * and blue Ok status. This is really needed since car GPS or non-baro GPS
+ * may be in error of several meters, and we can't accept a +30m over safety
+ * if the gps altitude is wrong!!
+ * (in meters)
+ */
+#define ALTERNATE_OVERSAFETY  100
+/**
+ * bestalternate search is within this maximum limit
+ * (in kilometers)
+ */
+#define ALTERNATE_MAXRANGE    100
+
+/** seconds between infobox dynamic flipping */
+#define DYNABOXTIME 3.0
+
+/**
+ * DOUBLECLICKINTERVAL is a general purpose timing, used by both VK and synthetic double click
  *
  * No miracles. Couldn't do it any better.
  *
@@ -80,40 +84,42 @@ Copyright_License {
  * Very careful changing this value. It is used by virtual keys also!
  * This is the timing sequence for virtual keys:
  *
- * 0 - VKSHORTCLICK   single click on the waypoint
- * 0 - DCI/2+30		double click detected and VK disabled
- * DCI/2+30 - DCI        airspace click and double click conflict?
- * <DCI			possible double click
- * >DCI 		virtual key
- *
+ *   0 - VKSHORTCLICK     single click on the waypoint
+ *   0 - DCI / 2 + 30     double click detected and VK disabled
+ *   DCI / 2 + 30 - DCI   airspace click and double click conflict?
+ *   < DCI                possible double click
+ *   > DCI                virtual key
  */
-
 #define DOUBLECLICKINTERVAL 350
-#define VKSHORTCLICK 120 // must be < than DCI/2 to have a chance to make airspace click recon!!!
-#define VKLONGCLICK 1500  // triggers circling/cruis switch on aircraft icon
-#define AIRSPACECLICK 1000 // interval to look only for airspace and not WP (IF NOT USING VK)
+/** must be < than DCI/2 to have a chance to make airspace click recon!!! */
+#define VKSHORTCLICK 120
+/** triggers circling/cruis switch on aircraft icon */
+#define VKLONGCLICK 1500
+/** interval to look only for airspace and not WP (IF NOT USING VK) */
+#define AIRSPACECLICK 1000
 
 
-#define BESTALTERNATEINTERVAL 60.0 // interval in seconds between BA search (slow)
+/** interval in seconds between BA search (slow) */
+#define BESTALTERNATEINTERVAL 60.0
 
-/*
- * Global defines
- */
-// Why MPSR? (historical, it was back when it was commercial!)
 #define REGKEYNAME	"Software\\MPSR\\XCSoar"
 #define XCSDATADIR	"XCSoarData"
 #define XCSCHKLIST	"xcsoar-checklist.txt"
 #define XCSPROFILE	"xcsoar-registry.prf"
 
-// Rotary buffers and filters
+/** Max items in any rotary buffer (used in filters) */
+#define RASIZE 180
 
-#define RASIZE 180	 // MAX ITEMS IN ANY ROTARY BUFFER (used in filters)
+/**
+ * Max number of iterations during a filter convergence search
+ * just for safety, it should stop at 3-4 automatically
+ */
+#define MAXITERFILTER 10
 
-#define MAXITERFILTER 10 // Max number of iterations during a filter convergence search
-			 // just for safety, it should stop at 3-4 automatically
+/** max size of rotary buffer for LD calculation */
+#define MAXLDROTARYSIZE 180
 
-#define MAXLDROTARYSIZE 180 // max size of rotary buffer for LD calculation
-
-#define MAXEFFICIENCYSHOW 200  // over this, show INVALID_GR
+/** over this, show INVALID_GR */
+#define MAXEFFICIENCYSHOW 200
 
 #endif
