a68f9d88cd7134109e50fea260c665c7a7c9e168
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-03-21 11:15:01 +0100

    Asset: Corrected Format and Indentation

--- src/Asset.cpp
+++ src/Asset.cpp
@@ -52,10 +52,10 @@ Copyright_License {
 TCHAR strAssetNumber[MAX_LOADSTRING] = TEXT(""); //4G17DW31L0HY");
 TCHAR strRegKey[MAX_LOADSTRING] = TEXT("");
 
-#if defined(PNA) || defined(FIVV)  // VENTA-ADDON we call it model and not PNA for possible future usage even for custom PDAs
-int	GlobalModelType=0;	// see XCSoar.h for modeltype definitions
+#if defined(PNA) || defined(FIVV)  // we call it model and not PNA for possible future usage even for custom PDAs
+int	GlobalModelType = 0;	// see XCSoar.h for modeltype definitions
 TCHAR	GlobalModelName[MAX_PATH]; // there are currently no checks.. TODO check it fits here
-float	GlobalEllipse=1.1f;	// default ellipse type VENTA2-ADDON
+float	GlobalEllipse = 1.1f;	// default ellipse type
 #endif
 
 static void
@@ -73,7 +73,7 @@ void ReadAssetNumber(void)
 
   val[0]= _T('\0');
 
-  memset(strAssetNumber, 0, MAX_LOADSTRING*sizeof(TCHAR));
+  memset(strAssetNumber, 0, MAX_LOADSTRING * sizeof(TCHAR));
   // JMW clear this first just to be safe.
 
 #ifdef WINDOWSPC
@@ -85,7 +85,7 @@ void ReadAssetNumber(void)
   int len = _tcslen(val);
   for (int i = 0; i < len; i++) {
     if (((val[i] >= _T('A')) && (val[i] <= _T('Z')))
-        ||((val[i] >= _T('0')) && (val[i] <= _T('9')))) {
+        || ((val[i] >= _T('0')) && (val[i] <= _T('9')))) {
       strAssetNumber[ifound]= val[i];
       ifound++;
     }
@@ -127,11 +127,11 @@ ReadCompaqID(void)
 #if defined(WIN32) && !defined(WINDOWSPC)
   PROCESS_INFORMATION pi;
 
-  if(strAssetNumber[0] != '\0') {
+  if(strAssetNumber[0] != '\0')
     return;
-  }
 
-  CreateProcess(TEXT("\\windows\\CreateAssetFile.exe"), NULL, NULL, NULL, FALSE, 0, NULL, NULL, NULL, &pi);
+  CreateProcess(TEXT("\\windows\\CreateAssetFile.exe"), NULL, NULL, NULL,
+                     FALSE, 0, NULL, NULL, NULL, &pi);
 
   FILE *file = _tfopen(TEXT("\\windows\\cpqAssetData.dat"), TEXT("rb"));
   if (file == NULL) {
@@ -154,15 +154,13 @@ ReadUUID(void)
 #define GUIDBuffsize 100
   unsigned char GUIDbuffer[GUIDBuffsize];
 
-  int eLast=0;
+  int eLast = 0;
   int i;
   unsigned long uNumReturned=0;
   int iBuffSizeIn=0;
   unsigned long temp, Asset;
 
-
   GUID Guid;
-
 
   // approach followed: http://blogs.msdn.com/jehance/archive/2004/07/12/181116.aspx
   // 1) send 16 byte buffer - some older devices need this
@@ -170,45 +168,41 @@ ReadUUID(void)
   // 3) take first 16 bytes of buffer and process.  Buffer returned may be any size
   // First try exactly 16 bytes, some older PDAs require exactly 16 byte buffer
 
-      #ifdef HAVEEXCEPTIONS
-    __try {
-      #else
 	  strAssetNumber[0]= '\0';
-      #endif
 
-	  iBuffSizeIn=sizeof(Guid);
+	  iBuffSizeIn = sizeof(Guid);
 	  memset(GUIDbuffer, 0, iBuffSizeIn);
-	  fRes = KernelIoControl(IOCTL_HAL_GET_DEVICEID, 0, 0, GUIDbuffer, iBuffSizeIn, &uNumReturned);
-	  if(fRes == FALSE)
-	  { // try larger buffer
+	  fRes = KernelIoControl(IOCTL_HAL_GET_DEVICEID, 0, 0, GUIDbuffer,
+	                         iBuffSizeIn, &uNumReturned);
+	  if(fRes == false) {
+	    // try larger buffer
 		  eLast = GetLastError();
 		  if (ERROR_INSUFFICIENT_BUFFER != eLast)
-		  {
-			return;
-		  }
-		  else
-		  { // wrong buffer
-			iBuffSizeIn = uNumReturned;
-			memset(GUIDbuffer, 0, iBuffSizeIn);
-			fRes = KernelIoControl(IOCTL_HAL_GET_DEVICEID, 0, 0, GUIDbuffer, iBuffSizeIn, &uNumReturned);
-  			eLast = GetLastError();
-			if(FALSE == fRes)
-				return;
+			  return;
+		  else {
+		    // wrong buffer
+        iBuffSizeIn = uNumReturned;
+        memset(GUIDbuffer, 0, iBuffSizeIn);
+        fRes = KernelIoControl(IOCTL_HAL_GET_DEVICEID, 0, 0, GUIDbuffer,
+                               iBuffSizeIn, &uNumReturned);
+        eLast = GetLastError();
+
+        if(fRes == false)
+          return;
 		  }
 	  }
 
 	  // here assume we have data in GUIDbuffer of length uNumReturned
-	  memcpy(&Guid,GUIDbuffer, sizeof(Guid));
+	  memcpy(&Guid, GUIDbuffer, sizeof(Guid));
 
+	  temp = Guid.Data2;
+	  temp = temp << 16;
+	  temp += Guid.Data3;
 
-	  temp = Guid.Data2; temp = temp << 16;
-	  temp += Guid.Data3 ;
-
-	  Asset = temp ^ Guid.Data1 ;
+	  Asset = temp ^ Guid.Data1;
 
 	  temp = 0;
-	  for(i=0;i<4;i++)
-		{
+	  for(i = 0; i < 4; i++) {
 		  temp = temp << 8;
 		  temp += Guid.Data4[i];
 		}
@@ -216,25 +210,15 @@ ReadUUID(void)
 	  Asset = Asset ^ temp;
 
 	  temp = 0;
-	  for(i=0;i<4;i++)
-		{
+	  for(i = 0; i < 4; i++) {
 		  temp = temp << 8;
-		  temp += Guid.Data4[i+4];
+		  temp += Guid.Data4[i + 4];
 		}
 
 	  Asset = Asset ^ temp;
 
-	  _stprintf(strAssetNumber,TEXT("%08X%08X"),Asset,Guid.Data1 );
-
-#ifdef HAVEEXCEPTIONS
-  }
-  __except(EXCEPTION_EXECUTE_HANDLER)
-  {
-	  strAssetNumber[0]= '\0';
-  }
+	  _stprintf(strAssetNumber, TEXT("%08X%08X"), Asset, Guid.Data1);
 #endif
-#endif
-  return;
 }
 
 #if 0
@@ -256,21 +240,21 @@ void ReadUUIDold(void)
   memset(&DevID, 0, sizeof(DEVICE_ID));
   DevID.dwSize = sizeof(DEVICE_ID);
 
-  fRes = KernelIoControl( IOCTL_HAL_GET_DEVICEID, NULL, 0,
-			  &DevID, sizeof( DEVICE_ID ), &dwBytesReturned );
+  fRes = KernelIoControl(IOCTL_HAL_GET_DEVICEID, NULL, 0, &DevID,
+                         sizeof(DEVICE_ID), &dwBytesReturned);
 
   wSize = DevID.dwSize;
 
-  if( (FALSE != fRes) || (ERROR_INSUFFICIENT_BUFFER != GetLastError()))
+  if((fRes != false) || (ERROR_INSUFFICIENT_BUFFER != GetLastError()))
     return;
 
   memset(&DevID, 0, sizeof(wSize));
   DevID.dwSize = wSize;
 
-  fRes = KernelIoControl( IOCTL_HAL_GET_DEVICEID, NULL, 0,
-			  &DevID, wSize, &dwBytesReturned );
+  fRes = KernelIoControl(IOCTL_HAL_GET_DEVICEID, NULL, 0, &DevID,
+                         wSize, &dwBytesReturned);
 
-  if((FALSE == fRes) || (ERROR_INSUFFICIENT_BUFFER == GetLastError()) )
+  if((fRes == false) || (ERROR_INSUFFICIENT_BUFFER == GetLastError()))
     return;
 
   BYTE* pDat = (BYTE*)&Guid.Data1;
@@ -280,36 +264,36 @@ void ReadUUIDold(void)
   pSrc =  (BYTE*)(&DevID) + DevID.dwPlatformIDOffset;
   memcpy(pDat, pSrc, DevID.dwPlatformIDBytes);
 
-  temp = Guid.Data2; temp = temp << 16;
+  temp = Guid.Data2;
+  temp = temp << 16;
   temp += Guid.Data3 ;
 
   Asset = temp ^ Guid.Data1 ;
 
   temp = 0;
-  for(i=0;i<4;i++)
-    {
-      temp = temp << 8;
-      temp += Guid.Data4[i];
-    }
+  for(i = 0; i < 4; i++) {
+    temp = temp << 8;
+    temp += Guid.Data4[i];
+  }
 
   Asset = Asset ^ temp;
 
   temp = 0;
-  for(i=0;i<4;i++)
-    {
-      temp = temp << 8;
-      temp += Guid.Data4[i+4];
-    }
+  for(i = 0; i < 4; i++) {
+    temp = temp << 8;
+    temp += Guid.Data4[i + 4];
+  }
 
   Asset = Asset ^ temp;
 
-  _stprintf(strAssetNumber,TEXT("%08X%08X"),Asset,Guid.Data1 );
-  return;
+  _stprintf(strAssetNumber, TEXT("%08X%08X"), Asset, Guid.Data1);
 #endif
 }
 #endif
 
-void InitAsset() {
+void
+InitAsset()
+{
   if (is_altair()) {
     #ifdef FORCEPORTRAIT
     // JMW testing only for portrait mode of Altair
@@ -318,48 +302,47 @@ void InitAsset() {
   }
 
   // VENTA2- delete registries at startup, but not on PC!
-  #if defined(FIVV) && ( !defined(WINDOWSPC) || WINDOWSPC==0 )
-  if (!is_pna()) {
+  #if defined(FIVV) && (!defined(WINDOWSPC) || WINDOWSPC == 0)
+  if (!is_pna())
     RegDeleteKey(HKEY_CURRENT_USER, _T(REGKEYNAME));
-  }
   #endif
 
   #ifdef PNA
-    // VENTA2-ADDON MODEL TYPE
-    /*
-    LocalPath is called for the very first time by CreateDirectoryIfAbsent.
-    In order to be able in the future to behave differently for each PNA device
-    and maybe also for common PDAs, we need to know the PNA/PDA Model Type
-    BEFORE calling LocalPath. This was critical.
-     */
+  // VENTA2-ADDON MODEL TYPE
+  /*
+  LocalPath is called for the very first time by CreateDirectoryIfAbsent.
+  In order to be able in the future to behave differently for each PNA device
+  and maybe also for common PDAs, we need to know the PNA/PDA Model Type
+  BEFORE calling LocalPath. This was critical.
+   */
 
-    SmartGlobalModelType(); // First we check the exec filename, which
-                            // has priority over registry values
+  SmartGlobalModelType(); // First we check the exec filename, which
+                          // has priority over registry values
 
-    if (!_tcscmp(GlobalModelName, _T("UNKNOWN"))) // Then if there is no smart name...
-      SetModelType();                             // get the modeltype from
-                                                  // the registry as usual
+  if (!_tcscmp(GlobalModelName, _T("UNKNOWN"))) // Then if there is no smart name...
+    SetModelType();                             // get the modeltype from
+                                                // the registry as usual
   #endif
 
-// VENTA2- TODO fix these directories are not used always!
+  // VENTA2- TODO fix these directories are not used always!
   CreateDirectoryIfAbsent(TEXT(""));  // RLD make sure the LocalPath folder actually exists
   CreateDirectoryIfAbsent(TEXT("persist"));
   CreateDirectoryIfAbsent(TEXT("logs"));
   CreateDirectoryIfAbsent(TEXT("config"));
 
-// VENTA2-ADDON install fonts on PDAs and check XCSoarData existance
-  #if defined(FIVV) && ( !defined(WINDOWSPC) || WINDOWSPC==0 )
+  // VENTA2-ADDON install fonts on PDAs and check XCSoarData existance
+  #if defined(FIVV) && (!defined(WINDOWSPC) || WINDOWSPC == 0)
   //#ifndef PNA
+  bool datadir = CheckDataDir();
+  if (datadir)
+    LogStartUp(TEXT("XCSoarData directory found."));
+  else
+    LogStartUp(TEXT("ERROR: NO XCSOARDATA DIRECTORY FOUND!"));
 
-    bool datadir=CheckDataDir();
-    if (datadir) LogStartUp(TEXT("XCSoarData directory found."));
-    else LogStartUp(TEXT("ERROR: NO XCSOARDATA DIRECTORY FOUND!"));
-
-    LogStartUp(TEXT("Check for installing fonts"));
-    short didfonts=InstallFonts();  // check if really did it, and maybe restart
-    LogStartUp(_T("InstallFonts() result=%d (0=installed >0 not installed)"), didfonts);
-
-    //#endif
+  LogStartUp(TEXT("Check for installing fonts"));
+  short didfonts = InstallFonts();  // check if really did it, and maybe restart
+  LogStartUp(_T("InstallFonts() result=%d (0=installed >0 not installed)"), didfonts);
+  //#endif
   #endif
 
   StartupLogFreeRamAndStorage();
