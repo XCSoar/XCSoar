ee9a6a3ae65eee40259666d62a5319ac0844de5e
Rob Dunning <rob@raspberryridgesheepfarm.com>
2010-04-08 22:29:50 -0400

    Resource.h: add IDB_SWIFT2 "small version for startup"

--- src/resource.h
+++ src/resource.h
@@ -77,6 +77,7 @@
 #define IDB_THERMALSOURCE2              329
 #define IDB_LAUNCHER1                   330
 #define IDB_LAUNCHER2                   331
+#define IDB_SWIFT2                      332
 #define IDC_MESSAGE                     1094
 #define IDC_PROGRESS1                   1239
 #define IDC_VERSION                     1254
