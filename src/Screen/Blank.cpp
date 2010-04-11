1dcf91ec8ce1bd9f93372fe092b91dda26cdc89f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-04-11 15:40:34 +0200

    Screen/Blank: Corrected Format and Indentation

--- src/Screen/Blank.cpp
+++ src/Screen/Blank.cpp
@@ -50,65 +50,65 @@ Copyright_License {
 // GDI Escapes for ExtEscape()
 #define QUERYESCSUPPORT    8
 // The following are unique to CE
-#define GETVFRAMEPHYSICAL   6144
-#define GETVFRAMELEN    6145
-#define DBGDRIVERSTAT    6146
+#define GETVFRAMEPHYSICAL    6144
+#define GETVFRAMELEN         6145
+#define DBGDRIVERSTAT        6146
 #define SETPOWERMANAGEMENT   6147
 #define GETPOWERMANAGEMENT   6148
 
 typedef enum _VIDEO_POWER_STATE {
-    VideoPowerOn = 1,
-    VideoPowerStandBy,
-    VideoPowerSuspend,
-    VideoPowerOff
+  VideoPowerOn = 1,
+  VideoPowerStandBy,
+  VideoPowerSuspend,
+  VideoPowerOff
 } VIDEO_POWER_STATE, *PVIDEO_POWER_STATE;
 
 typedef struct _VIDEO_POWER_MANAGEMENT {
-    ULONG Length;
-    ULONG DPMSVersion;
-    ULONG PowerState;
+  ULONG Length;
+  ULONG DPMSVersion;
+  ULONG PowerState;
 } VIDEO_POWER_MANAGEMENT, *PVIDEO_POWER_MANAGEMENT;
 
 int DisplayTimeOut = 0;
 
-void BlankDisplay(bool doblank) {
+void
+BlankDisplay(bool doblank)
+{
   static bool oldblank = false;
 
   BATTERYINFO BatteryInfo;
-  BatteryInfo.acStatus = 0; // JMW initialise
+  BatteryInfo.acStatus = 0;
 
   if (GetBatteryInfo(&BatteryInfo)) {
     PDABatteryPercent = BatteryInfo.BatteryLifePercent;
-    PDABatteryTemperature = BatteryInfo.BatteryTemperature; // VENTA3
-/*
-	// All you need to display extra Battery informations...
+    PDABatteryTemperature = BatteryInfo.BatteryTemperature;
 
-	TCHAR vtemp[1000];
-	_stprintf(vtemp,_T("Battpercent=%d Volt=%d Curr=%d AvCurr=%d mAhC=%d Temp=%d Lifetime=%d Fulllife=%d"),
- 		BatteryInfo.BatteryLifePercent, BatteryInfo.BatteryVoltage,
- 		BatteryInfo.BatteryCurrent, BatteryInfo.BatteryAverageCurrent,
- 		BatteryInfo.BatterymAHourConsumed,
-		BatteryInfo.BatteryTemperature, BatteryInfo.BatteryLifeTime, BatteryInfo.BatteryFullLifeTime);
-	LogStartUp( vtemp );
- */
+    // All you need to display extra Battery informations...
+    /*
+    TCHAR vtemp[1000];
+    _stprintf(vtemp,_T("Battpercent=%d Volt=%d Curr=%d AvCurr=%d mAhC=%d Temp=%d Lifetime=%d Fulllife=%d"),
+      BatteryInfo.BatteryLifePercent, BatteryInfo.BatteryVoltage,
+      BatteryInfo.BatteryCurrent, BatteryInfo.BatteryAverageCurrent,
+      BatteryInfo.BatterymAHourConsumed,
+      BatteryInfo.BatteryTemperature, BatteryInfo.BatteryLifeTime, BatteryInfo.BatteryFullLifeTime);
+    LogStartUp( vtemp );
+    */
   }
 
-  if (!XCSoarInterface::SettingsMap().EnableAutoBlank) {
+  if (!XCSoarInterface::SettingsMap().EnableAutoBlank)
     return;
-  }
-  if (doblank == oldblank) {
+
+  if (doblank == oldblank)
     return;
-  }
 
   HDC gdc;
-  int iESC=SETPOWERMANAGEMENT;
+  int iESC = SETPOWERMANAGEMENT;
 
   gdc = ::GetDC(NULL);
   if (ExtEscape(gdc, QUERYESCSUPPORT, sizeof(int), (LPCSTR)&iESC,
                 0, NULL)==0) {
     // can't do it, not supported
   } else {
-
     VIDEO_POWER_MANAGEMENT vpm;
     vpm.Length = sizeof(VIDEO_POWER_MANAGEMENT);
     vpm.DPMSVersion = 0x0001;
@@ -119,7 +119,6 @@ void BlankDisplay(bool doblank) {
     // something else is wrong
 
     if (doblank) {
-
       /* Battery status - simulator only - for safety of battery data
          note: Simulator only - more important to keep running in your plane
       */
@@ -127,7 +126,7 @@ void BlankDisplay(bool doblank) {
       // JMW, maybe this should be active always...
       // we don't want the PDA to be completely depleted.
 
-      if (BatteryInfo.acStatus==0) {
+      if (BatteryInfo.acStatus == 0) {
         if (is_simulator() && (PDABatteryPercent < BATTERY_EXIT)) {
           LogStartUp(TEXT("Battery low exit..."));
           // TODO feature: Warning message on battery shutdown
@@ -146,8 +145,7 @@ void BlankDisplay(bool doblank) {
         }
       }
 
-      if (BatteryInfo.acStatus==0) {
-
+      if (BatteryInfo.acStatus == 0) {
         // Power off the display
         vpm.PowerState = VideoPowerOff;
         ExtEscape(gdc, SETPOWERMANAGEMENT, vpm.Length, (LPCSTR) &vpm,
@@ -167,34 +165,30 @@ void BlankDisplay(bool doblank) {
         XCSoarInterface::SetSettingsMap().ScreenBlanked = false;
       }
     }
-
   }
   ::ReleaseDC(NULL, gdc);
 }
 
-void CheckDisplayTimeOut(bool sticky)
+void
+CheckDisplayTimeOut(bool sticky)
 {
 #ifndef WINDOWSPC
   SystemIdleTimerReset();
 #endif
 
   if (!sticky) {
-    if (DisplayTimeOut< DISPLAYTIMEOUTMAX)
+    if (DisplayTimeOut < DISPLAYTIMEOUTMAX)
       DisplayTimeOut++;
   } else {
     // JMW don't let display timeout while a dialog is active,
     // but allow button presses to trigger redisplay
-    if (DisplayTimeOut>1) {
-      DisplayTimeOut=1;
-    }
+    if (DisplayTimeOut > 1)
+      DisplayTimeOut = 1;
   }
-  if (DisplayTimeOut >= DISPLAYTIMEOUTMAX) {
+  if (DisplayTimeOut >= DISPLAYTIMEOUTMAX)
     BlankDisplay(true);
-  } else {
+  else
     BlankDisplay(false);
-  }
 }
 
 #endif /* HAVE_BLANK */
-
-
