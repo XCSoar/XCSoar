c37a656ad40129dfcedbef12f593af842db806a2
Max Kellermann <max@duempel.org>
2010-04-14 21:14:33 +0200

    Makefile: build the manual

--- build/doco.mk
+++ build/doco.mk
@@ -1,3 +1,13 @@
+.PHONY: manual clean-manual doco
+
+MANUAL_OUTPUT_DIR = $(OUT)/all/manual
+
+manual: $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf
+
+$(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(DOC)/manual/XCSoar-manual.tex $(MANUAL_OUTPUT_DIR)/dirstamp
+	# run TeX twice to make sure that all references are resolved
+	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) $(<F)
+	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) $(<F)
 
 doco: FORCE
 #	$(topdir)/ex/tools/cloc --exclude-lang=D src > doc/cloc.txt
