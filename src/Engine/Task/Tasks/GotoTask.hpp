7173815fa1b9d7d2d70f7d3c1a5aaa3a873bdfcc
John Wharington <jwharington@gmail.com>
2010-04-21 08:25:00 +1000

    GotoTask: reverse iterator not required

--- src/Engine/Task/Tasks/GotoTask.hpp
+++ src/Engine/Task/Tasks/GotoTask.hpp
@@ -144,7 +144,6 @@ public:
  * @param visitor Visitor to accept
  * @param reverse Visit task points in reverse order 
  *
- * \todo reverse not implemented yet
  */
   void tp_CAccept(TaskPointConstVisitor& visitor, const bool reverse=false) const;
   void tp_Accept(TaskPointVisitor& visitor, const bool reverse=false) {};
