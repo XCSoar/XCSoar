de566574eef75a19e0b148d32a9b1cca12942986
Max Kellermann <max@duempel.org>
2010-04-30 07:15:05 +0200

    moved XCSoar.rc and bitmaps to Data/
    Move sounds (*.wav) to a separate directory.

--- build/resource.mk
+++ build/resource.mk
@@ -1,6 +1,6 @@
 ifeq ($(HAVE_WIN32),y)
 
-RESOURCE_TEXT = src/XCSoar.rc
+RESOURCE_TEXT = Data/XCSoar.rc
 RESOURCE_BINARY = $(TARGET_OUTPUT_DIR)/$(notdir $(RESOURCE_TEXT:.rc=.rsc))
 
 $(RESOURCE_BINARY): $(RESOURCE_TEXT) $(wildcard Data/Dialogs/*.xml) | $(TARGET_OUTPUT_DIR)/%/../dirstamp
