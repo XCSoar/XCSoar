929424c6d0746369e3713b88a19f1daa65341728
Max Kellermann <max@duempel.org>
2010-04-30 07:12:07 +0200

    Makefile: added variable RESOURCE_FILES

--- build/resource.mk
+++ build/resource.mk
@@ -2,8 +2,9 @@ ifeq ($(HAVE_WIN32),y)
 
 RESOURCE_TEXT = Data/XCSoar.rc
 RESOURCE_BINARY = $(TARGET_OUTPUT_DIR)/$(notdir $(RESOURCE_TEXT:.rc=.rsc))
+RESOURCE_FILES = $(wildcard Data/Dialogs/*.xml)
 
-$(RESOURCE_BINARY): $(RESOURCE_TEXT) $(wildcard Data/Dialogs/*.xml) | $(TARGET_OUTPUT_DIR)/%/../dirstamp
+$(RESOURCE_BINARY): $(RESOURCE_TEXT) $(RESOURCE_FILES) | $(TARGET_OUTPUT_DIR)/%/../dirstamp
 	@$(NQ)echo "  WINDRES $@"
 	$(Q)$(WINDRES) $(WINDRESFLAGS) -o $@ $<
 
