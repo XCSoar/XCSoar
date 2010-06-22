1379c3fc3e6f3710d41b54852d9a7c47f99d4137
Max Kellermann <max@duempel.org>
2010-06-22 22:23:29 +0200

    IO: added class BatchTextWriter

--- build/io.mk
+++ build/io.mk
@@ -9,6 +9,7 @@ IO_SOURCES = \
 	$(IO_SRC_DIR)/ConvertLineReader.cpp \
 	$(IO_SRC_DIR)/FileLineReader.cpp \
 	$(IO_SRC_DIR)/ZipLineReader.cpp \
+	$(IO_SRC_DIR)/BatchTextWriter.cpp \
 	$(IO_SRC_DIR)/TextWriter.cpp
 
 IO_OBJS = $(call SRC_TO_OBJ,$(IO_SOURCES))
---
+++ src/IO/BatchTextWriter.hpp
@@ -0,0 +1,65 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
+
+	M Roberts (original release)
+	Robin Birch <robinb@ruffnready.co.uk>
+	Samuel Gisiger <samuel.gisiger@triadis.ch>
+	Jeff Goodenough <jeff@enborne.f2s.com>
+	Alastair Harrison <aharrison@magic.force9.co.uk>
+	Scott Penrose <scottp@dd.com.au>
+	John Wharington <jwharington@gmail.com>
+	Lars H <lars_hn@hotmail.com>
+	Rob Dunning <rob@raspberryridgesheepfarm.com>
+	Russell King <rmk@arm.linux.org.uk>
+	Paolo Ventafridda <coolwind@email.it>
+	Tobias Lohner <tobias@lohner-net.de>
+	Mirek Jezek <mjezek@ipplc.cz>
+	Max Kellermann <max@duempel.org>
+	Tobias Bieniek <tobias.bieniek@gmx.de>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_BATCH_TEXT_WRITER_HPP
+#define XCSOAR_BATCH_TEXT_WRITER_HPP
+
+#include "Util/BatchBuffer.hpp"
+#include "Util/tstring.hpp"
+
+/**
+ * A wrapper for #TextWriter which collects 256 lines, and writes them
+ * in a one batch.
+ */
+class BatchTextWriter {
+  tstring path;
+  bool append;
+  BatchBuffer<tstring,256> buffer;
+
+public:
+  BatchTextWriter(const TCHAR *_path, bool _append=false)
+    :path(_path), append(_append) {}
+  ~BatchTextWriter() {
+    flush();
+  }
+
+  bool writeln(const TCHAR *line);
+  bool flush();
+};
+
+#endif
