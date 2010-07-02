4ced779eefffd1bb4754bc69a17025d50b5c6b88
Max Kellermann <max@duempel.org>
2010-07-02 19:31:56 +0200

    Screen/Bitmap: configure BGR in create()
    Previously, Bitmap::create() used a "RGB" layout, but the BGRColor
    class uses "BGR".  I hope the masks for big-endian are correct, I
    can't test currently.

--- src/Screen/Bitmap.cpp
+++ src/Screen/Bitmap.cpp
@@ -179,13 +179,13 @@ Bitmap::create(unsigned width, unsigned height)
   Uint32 rmask, gmask, bmask, amask;
 
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-  rmask = 0xff000000;
-  gmask = 0x00ff0000;
-  bmask = 0x0000ff00;
-#else
   rmask = 0x000000ff;
   gmask = 0x0000ff00;
   bmask = 0x00ff0000;
+#else
+  rmask = 0x00ff0000;
+  gmask = 0x0000ff00;
+  bmask = 0x000000ff;
 #endif
   amask = 0x00000000;
 
