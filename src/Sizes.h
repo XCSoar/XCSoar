f1eb9d1eae9af4cd4eacd4834e5291cabbc61af2
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-07-15 23:07:24 +0200

    Sizes.h: Removed unused #defines

--- src/Sizes.h
+++ src/Sizes.h
@@ -1,10 +1,6 @@
 #if !defined(AFX_SIZES_H__695AAC30_F401_4CFF_9BD9_FE62A2A2D0D2__INCLUDED_)
 #define AFX_SIZES_H__695AAC30_F401_4CFF_9BD9_FE62A2A2D0D2__INCLUDED_
 
-#define DISTANCE_ROUNDING 10.0
-// Rounding of task distances for entry (sector radius etc)
-// 10.0 means rounding to 0.1 user units
-
 #define MINFREESTORAGE 500
 // 500 kb must be free for logger to be active this is based on rough
 // estimate that a long flight will detailed logging is about 200k,
@@ -18,38 +14,19 @@
 // max length of waypoint comment names
 #define COMMENT_SIZE 50
 
-#define WAY_POINT_ID_SIZE 20
-
-#define MENU_HEIGHT 26
-
 #define MAXINFOWINDOWS 14
 
 #define REGKEYSIZE 64
 
 #define MAXSATELLITES 12
 
-#define DESCRIPTION_SIZE 30
-#define TITLE_SIZE 30
 #define FORMAT_SIZE 20
-#define MAXTASKPOINTS 10
 #define MAXSTARTPOINTS 10
 
 #define MAX_LOADSTRING 100
 
 #define CONTROLHEIGHTRATIO 7.4
 #define TITLEHEIGHTRATIO 3.1
-
-#define MENUBUTTONWIDTHRATIO 0.6
-
-// stepsize of pixel grid, should be multiple of 2
-#ifdef WINDOWSPC
-#define DTQUANT 6
-#else
-#define DTQUANT 6
-#endif
-
-// ratio of smoothed bitmap size to pixel grid
-#define OVS 2
 
 // number of points along final glide to scan for terrain
 #define NUMFINALGLIDETERRAIN 30
@@ -67,10 +44,6 @@
 #define NUMAIRSPACEBRUSHES 8
 
 #define AIRSPACE_SCANSIZE_X 16
-#define AIRSPACE_SCANSIZE_H 16
-#define OUTSIDE_CHECK_INTERVAL 4
-
-#define READLINE_LENGTH 300
 
 // Size of Status message cache - Note 1000 messages may not be enough...
 // TODO If we continue with the reading one at a time - then consider using
@@ -78,8 +51,6 @@
 // RAM used as required - easy to do with struct above - just point to next.
 // (NOTE: This is used for all the caches for now - temporary)
 #define MAXSTATUSMESSAGECACHE 1000
-
-#define MAXISOLINES 32
 
 #define ERROR_TIME 1.0e6
 
