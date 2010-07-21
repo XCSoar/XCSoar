27e01f2c18a2f8d3bf580b8fa96918e502bde60b
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-07-21 10:38:33 +0200

    Removed outdated buildcabdata.bat file

--- buildcabdata.bat
+++
@@ -1,5 +0,0 @@
-
-"C:\Program Files\Windows CE Tools\wce420\POCKET PC 2003\Tools\Cabwiz.exe" XCSoarData.inf
-
-ezsetup -l english -i XCSoarData.ini -r installmsgdata.txt -e gpl.txt -o InstallXCSoar-Data.exe
-
