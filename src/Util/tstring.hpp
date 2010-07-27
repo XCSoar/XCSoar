62504feac75a94ad1cb11676ea41ad6e95d45db0
Max Kellermann <max@duempel.org>
2010-07-27 20:17:53 +0200

    tstring: remove unused typedef tstringstream
    This patch finally eliminates iostream from the XCSoar static binary,
    shrinking it by more than 400 kB.

--- src/Util/tstring.hpp
+++ src/Util/tstring.hpp
@@ -2,16 +2,13 @@
 #define TSTRING_HPP
 
 #include <string>
-#include <sstream>
 
 #include <tchar.h>
 
 #ifdef _UNICODE
 typedef std::wstring tstring;
-typedef std::wostringstream tstringstream;
 #else
 typedef std::string tstring;
-typedef std::ostringstream tstringstream;
 #endif
 
 tstring trim(const tstring& the_string);
