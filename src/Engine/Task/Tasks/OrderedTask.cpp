77afee9ec2fdbb098d2db63f623aa8b9a3086bab
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-08-03 18:36:35 +0200

    OrderedTask: Added documentation

--- src/Engine/Task/Tasks/OrderedTask.cpp
+++ src/Engine/Task/Tasks/OrderedTask.cpp
@@ -627,15 +627,22 @@ OrderedTask::calc_gradient(const AIRCRAFT_STATE &state)
   fixed d_acc = fixed_zero;
   fixed h_this = state.NavAltitude;
 
+  // Iterate through remaining turnpoints
   for (unsigned i = activeTaskPoint; i < tps.size(); i++) {
+    // Sum up the leg distances
     d_acc += tps[i]->get_vector_remaining(state).Distance;
     if (!d_acc)
       continue;
 
+    // Calculate gradient to the i-th turnpoint of the remaining task
     const fixed g_this = (h_this - tps[i]->get_elevation()) / d_acc;
     if (i == activeTaskPoint)
+      // Set best gradient to calculated gradient for first turnpoint
       g_best = g_this;
     else
+      // Check if the calculated gradient is lower then the best we got yet
+      // This is useful if the second last turnpoint is located far above
+      // the finish
       g_best = min(g_best, g_this);
   }
   return g_best;
