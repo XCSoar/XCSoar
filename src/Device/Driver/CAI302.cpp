c105baa7531e27c5e58c983580603bae0be4d518
Max Kellermann <max@duempel.org>
2010-08-03 22:14:55 +0200

    Driver/CAI302: add 180 degrees to wind direction
    The CAI302 emits the direction the wind moves to, not the direction
    from which the wind originates.  During my flights, I always thought
    XCSoar displayed it all wrong, but on a strong wind day, it was
    obvious that XCSoar miscalculated the wind - which was because it read
    the wrong direction from my CAI302 in the first place.  What a dumb
    bug!

--- src/Device/Driver/CAI302.cpp
+++ src/Device/Driver/CAI302.cpp
@@ -577,6 +577,8 @@ static bool
 cai_w(NMEAInputLine &line, NMEA_INFO *GPS_INFO, bool enable_baro)
 {
   GPS_INFO->ExternalWindAvailable = ReadSpeedVector(line, GPS_INFO->wind);
+  if (GPS_INFO->ExternalWindAvailable)
+    GPS_INFO->wind.bearing = GPS_INFO->wind.bearing.Reciprocal();
 
   line.skip(2);
 
