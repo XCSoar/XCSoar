9519ac786877f39c232f4f0a72371aeab143b083
Simon Taylor <simon.taylor.uk@gmail.com>
2010-08-28 21:08:07 +0100

    Data/XCSoarLaunch, Makefile: Renders launcher from SVG at build.
    Launcher graphics are now rendered from SVG at build time.
    Additional graphic sizes (for higher resolutions) can be added by
    altering build/resource.mk.

--- build/launch.mk
+++ build/launch.mk
@@ -1,6 +1,28 @@
 # Rules for XCSoarLaunch.dll, the launcher for the PocketPC shell
+SVG_LAUNCH = Data/graphics/launcher.svg
+PNG_LAUNCH_224 = $(patsubst Data/graphics/%.svg,output/data/graphics/%-224.png,$(SVG_LAUNCH))
+BMP_LAUNCH_FLY_224 = $(PNG_LAUNCH_224:.png=-1.bmp)
+BMP_LAUNCH_SIM_224 = $(PNG_LAUNCH_224:.png=-2.bmp)
 
 ifeq ($(HAVE_CE)$(findstring $(TARGET),ALTAIR ALTAIRPORTRAIT),y)
+
+
+# render from SVG to PNG
+$(PNG_LAUNCH_224): output/data/graphics/%-224.png: Data/graphics/%.svg | output/data/graphics/dirstamp
+	@$(NQ)echo "  SVG     $@"
+	$(Q)rsvg-convert --width=224 $< -o $@
+
+# split into two uncompressed 8-bit BMPs (single 'convert' operation)
+$(BMP_LAUNCH_FLY_224): %-1.bmp: %.png
+	@$(NQ)echo "  BMP     $@"
+	@$(NQ)echo "  BMP     $(@:1.bmp=2.bmp)"
+	$(Q)$(IM_PREFIX)convert $< -background blue -layers flatten +matte +dither -compress none -type optimize -colors 256 -crop '50%x100%' -scene 1 $(@:1.bmp=%d.bmp)
+$(BMP_LAUNCH_SIM_224): %-2.bmp: %.png
+	@$(NQ)echo "  BMP     $@"
+	@$(NQ)echo "  BMP     $(@:1.bmp=2.bmp)"
+	$(Q)$(IM_PREFIX)convert $< -background blue -layers flatten +matte +dither -compress none -type optimize -colors 256 -crop '50%x100%' -scene 1 $(@:1.bmp=%d.bmp)
+
+LAUNCH_RESOURCE_FILES = $(BMP_LAUNCH_FLY_224) $(BMP_LAUNCH_SIM_224) 
 
 XCSOARLAUNCH_DLL = $(TARGET_BIN_DIR)/XCSoarLaunch.dll
 XCSOARLAUNCH_SOURCES = \
@@ -11,7 +33,7 @@ $(XCSOARLAUNCH_OBJS): CFLAGS += -Wno-missing-declarations -Wno-missing-prototype
 $(TARGET_OUTPUT_DIR)/XCSoarLaunch.e: $(SRC)/XCSoarLaunch.def $(XCSOARLAUNCH_OBJS) | $(TARGET_BIN_DIR)/dirstamp
 	$(Q)$(DLLTOOL) -e $@ -d $^
 
-$(TARGET_OUTPUT_DIR)/XCSoarLaunch.rsc: Data/XCSoarLaunch.rc | $(TARGET_OUTPUT_DIR)/dirstamp
+$(TARGET_OUTPUT_DIR)/XCSoarLaunch.rsc: Data/XCSoarLaunch.rc $(LAUNCH_RESOURCE_FILES) | $(TARGET_OUTPUT_DIR)/dirstamp
 	@$(NQ)echo "  WINDRES $@"
 	$(Q)$(WINDRES) $(WINDRESFLAGS) -o $@ $<
 
