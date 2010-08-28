a4aac558d3891d7a004871eb4140e66d39714e25
Simon Taylor <simon.taylor.uk@gmail.com>
2010-08-28 20:52:25 +0100

    Data/XCSoar.rc, Makefile: generates splash screen from SVG at build.
    Splash logo is now rendered from SVG at build time - support for
    new resolutions can be added by editing build/resource.mk.

--- build/resource.mk
+++ build/resource.mk
@@ -5,16 +5,26 @@ BMP_ICONS_19 = $(PNG_ICONS_19:.png=.bmp)
 PNG_ICONS_31 = $(patsubst Data/icons/%.svg,output/data/icons/%-31.png,$(SVG_ICONS))
 BMP_ICONS_31 = $(PNG_ICONS_31:.png=.bmp)
 
+SVG_SPLASH = Data/graphics/xcsoarswiftsplash.svg
+PNG_SPLASH_220 = $(patsubst Data/graphics/%.svg,output/data/graphics/%-220.png,$(SVG_SPLASH))
+BMP_SPLASH_220 = $(PNG_SPLASH_220:.png=.bmp)
+PNG_SPLASH_160 = $(patsubst Data/graphics/%.svg,output/data/graphics/%-160.png,$(SVG_SPLASH))
+BMP_SPLASH_160 = $(PNG_SPLASH_160:.png=.bmp)
+
 ifeq ($(WINHOST),y)
   IM_PREFIX := im-
 else
   IM_PREFIX :=
 endif
 
+####### icons
+
+# modify working copy of SVG to improve rendering
 $(SVG_NOALIAS_ICONS): output/data/icons/%.svg: build/no_anti_aliasing.xsl Data/icons/%.svg | output/data/icons/dirstamp
 	@$(NQ)echo "  XSLT    $@"
 	$(Q)xsltproc --output $@ $^
 
+# render from SVG to PNG
 $(PNG_ICONS_19): output/data/icons/%-19.png: output/data/icons/%.svg | output/data/icons/dirstamp
 	@$(NQ)echo "  SVG     $@"
 	$(Q)rsvg-convert --width=19 $< -o $@
@@ -37,17 +47,37 @@ $(PNG_ICONS_31): output/data/icons/%-31.png: output/data/icons/%.svg | output/da
 # convert to uncompressed 8-bit BMP
 $(BMP_ICONS_19): %.bmp: %-tile.png
 	@$(NQ)echo "  BMP     $@"
-	$(Q)$(IM_PREFIX)convert $< +dither -compress none -colors 256 $@
+	$(Q)$(IM_PREFIX)convert $< +dither -compress none -type optimize -colors 256 $@
 $(BMP_ICONS_31): %.bmp: %-tile.png
 	@$(NQ)echo "  BMP     $@"
-	$(Q)$(IM_PREFIX)convert $< +dither -compress none -colors 256 $@
+	$(Q)$(IM_PREFIX)convert $< +dither -compress none -type optimize -colors 256 $@
+
+####### splash logo
+
+# render from SVG to PNG
+$(PNG_SPLASH_220): output/data/graphics/%-220.png: Data/graphics/%.svg | output/data/graphics/dirstamp
+	@$(NQ)echo "  SVG     $@"
+	$(Q)rsvg-convert --width=220 $< -o $@
+$(PNG_SPLASH_160): output/data/graphics/%-160.png: Data/graphics/%.svg | output/data/graphics/dirstamp
+	@$(NQ)echo "  SVG     $@"
+	$(Q)rsvg-convert --width=160 $< -o $@
+
+# convert to uncompressed 8-bit BMP
+$(BMP_SPLASH_220): %.bmp: %.png
+	@$(NQ)echo "  BMP     $@"
+	$(Q)$(IM_PREFIX)convert $< -background white -layers flatten +matte +dither -compress none -type optimize -colors 256 $@
+$(BMP_SPLASH_160): %.bmp: %.png
+	@$(NQ)echo "  BMP     $@"
+	$(Q)$(IM_PREFIX)convert $< -background white -layers flatten +matte +dither -compress none -type optimize -colors 256 $@
 
 ifeq ($(HAVE_WIN32),y)
 
 RESOURCE_TEXT = Data/XCSoar.rc
 RESOURCE_BINARY = $(TARGET_OUTPUT_DIR)/$(notdir $(RESOURCE_TEXT:.rc=.rsc))
 RESOURCE_FILES = $(wildcard Data/Dialogs/*.xml)
-RESOURCE_FILES += $(BMP_ICONS_19) $(BMP_ICONS_31)
+RESOURCE_FILES += $(BMP_ICONS_19) $(BMP_ICONS_31) 
+RESOURCE_FILES += $(BMP_SPLASH_220) $(BMP_SPLASH_160)
+
 
 $(RESOURCE_BINARY): $(RESOURCE_TEXT) $(RESOURCE_FILES) | $(TARGET_OUTPUT_DIR)/%/../dirstamp
 	@$(NQ)echo "  WINDRES $@"
