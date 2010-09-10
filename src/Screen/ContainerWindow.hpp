fbcbaecca6b17bea5bf8763e0dc7ce8c9a12e721
Max Kellermann <max@duempel.org>
2010-09-10 18:50:31 +0200

    ContainerWindow: asynchronous redraw in bring_child_to_top()

--- src/Screen/ContainerWindow.hpp
+++ src/Screen/ContainerWindow.hpp
@@ -98,7 +98,7 @@ public:
   void bring_child_to_top(Window &child) {
     children.remove(&child);
     children.insert(children.begin(), &child);
-    child.expose();
+    invalidate();
   }
 
   /**
