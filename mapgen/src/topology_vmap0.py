1b9d18c150e70ce9053ad70b81b47782f4a47e11
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-09-23 01:10:47 +0200

    mapgen/topology_vmap0: Removed unnecessary class vmap0
    vmap0 is neither a class nor an object, it's a name.

--- mapgen/src/topology_vmap0.py
+++ mapgen/src/topology_vmap0.py
@@ -4,76 +4,74 @@ from georect import GeoRect
 
 cmd_ogr2ogr = "ogr2ogr"
 
-class vmap0:
-    __maps = ["eur", "noa", "sas", "soa"]
-    __layers = [["pop-miscellaneous-population-p", "mispopppop_point", "", "txt"],
-                ["pop-built-up-a", "builtupapop_area", "", "nam"],
-                ["hydro-water-course-l", "watrcrslhydro_line", "hyc=8", "nam"],
-                ["hydro-inland-water-a", "inwaterahydro_area", "hyc=8", "nam"],
-                ["trans-railroad-l", "railrdltrans_line", "exs=28", "fco"],
-                ["trans-road-l", "roadltrans_line", "rtt=14", "med"]]
+__maps = ["eur", "noa", "sas", "soa"]
+__layers = [["pop-miscellaneous-population-p", "mispopppop_point", "", "txt"],
+            ["pop-built-up-a", "builtupapop_area", "", "nam"],
+            ["hydro-water-course-l", "watrcrslhydro_line", "hyc=8", "nam"],
+            ["hydro-inland-water-a", "inwaterahydro_area", "hyc=8", "nam"],
+            ["trans-railroad-l", "railrdltrans_line", "exs=28", "fco"],
+            ["trans-road-l", "roadltrans_line", "rtt=14", "med"]]
         
-    def copy_clipped(self, rc, dir_data, dir_temp):
-        for i in range(len(self.__maps)):
-            for layer in self.__layers:
-                arg = [cmd_ogr2ogr]
-                
-                if i > 0:
-                    arg.append("-update")
-                    arg.append("-append")
-                else:
-                    arg.append("-overwrite")
-                    
-                if layer[1] != "":
-                    arg.append("-where")
-                    arg.append(layer[2])
-                    
-                arg.append("-select")
-                arg.append(layer[3])
-                
-                arg.append("-spat")
-                arg.append(str(rc.left.value_degrees())) 
-                arg.append(str(rc.bottom.value_degrees())) 
-                arg.append(str(rc.right.value_degrees()))
-                arg.append(str(rc.top.value_degrees()))
-
-                arg.append(dir_temp)
-                arg.append(os.path.join(dir_data, self.__maps[i]))
-
-                arg.append(layer[0])
-                arg.append("-nln")
-                arg.append(layer[1])
-                
-                p = subprocess.Popen(arg)
-                p.wait()
-             
-        files = []
-        for file in os.listdir(dir_temp):
-            if (file.endswith(".shp") or file.endswith(".shx") or
-                file.endswith(".prj") or file.endswith(".dbf")):
-                files.append([os.path.join(dir_temp, file), True])                
+def copy_clipped(rc, dir_data, dir_temp):
+    for i in range(len(__maps)):
+        for layer in __layers:
+            arg = [cmd_ogr2ogr]
             
-        return files
-            
+            if i > 0:
+                arg.append("-update")
+                arg.append("-append")
+            else:
+                arg.append("-overwrite")
                 
-    def generate_tpl_file(self, dir_temp):
-        file = open(os.path.join(dir_temp, "topology.tpl"), "w")
-        file.write("* filename,range,icon,field\n");
-        file.write("inwaterahydro_area, 100,,,64,96,240\n");
-        file.write("watrcrslhydro_line,    7,,,64,96,240\n");
-        file.write("builtupapop_area,  15,,1,223,223,0\n");
-        file.write("roadltrans_line,     15,,,240,64,64\n");
-        file.write("railrdltrans_line,   10,,,64,64,64\n");
-        file.write("mispopppop_point,    5,218,1,223,223,0\n");
-        file.close()
-        return os.path.join(dir_temp, "/topology.tpl")
+            if layer[1] != "":
+                arg.append("-where")
+                arg.append(layer[2])
+                
+            arg.append("-select")
+            arg.append(layer[3])
+            
+            arg.append("-spat")
+            arg.append(str(rc.left.value_degrees())) 
+            arg.append(str(rc.bottom.value_degrees())) 
+            arg.append(str(rc.right.value_degrees()))
+            arg.append(str(rc.top.value_degrees()))
+
+            arg.append(dir_temp)
+            arg.append(os.path.join(dir_data, __maps[i]))
+
+            arg.append(layer[0])
+            arg.append("-nln")
+            arg.append(layer[1])
+            
+            p = subprocess.Popen(arg)
+            p.wait()
+         
+    files = []
+    for file in os.listdir(dir_temp):
+        if (file.endswith(".shp") or file.endswith(".shx") or
+            file.endswith(".prj") or file.endswith(".dbf")):
+            files.append([os.path.join(dir_temp, file), True])                
+        
+    return files
+        
+            
+def generate_tpl_file(dir_temp):
+    file = open(os.path.join(dir_temp, "topology.tpl"), "w")
+    file.write("* filename,range,icon,field\n");
+    file.write("inwaterahydro_area, 100,,,64,96,240\n");
+    file.write("watrcrslhydro_line,    7,,,64,96,240\n");
+    file.write("builtupapop_area,  15,,1,223,223,0\n");
+    file.write("roadltrans_line,     15,,,240,64,64\n");
+    file.write("railrdltrans_line,   10,,,64,64,64\n");
+    file.write("mispopppop_point,    5,218,1,223,223,0\n");
+    file.close()
+    return os.path.join(dir_temp, "/topology.tpl")
 
 def Create(bounds, dir_data = "../data/", dir_temp = "../tmp/"):
     dir_data = os.path.abspath(os.path.join(dir_data, "vmap0")) 
     dir_temp = os.path.abspath(dir_temp) 
     
-    o = vmap0()
-    files = o.copy_clipped(bounds, dir_data, dir_temp)
-    files.append([o.generate_tpl_file(dir_temp), True])
+    files = copy_clipped(bounds, dir_data, dir_temp)
+    files.append([generate_tpl_file(dir_temp), True])
         
     return files
