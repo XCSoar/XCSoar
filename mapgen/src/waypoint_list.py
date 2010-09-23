4d0042bdd9413da62a0d8d1f0da1e3ec37e4ac3f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-09-23 02:21:06 +0200

    mapgen/waypoint: Moved Waypoint class into waypoint_list module

--- mapgen/src/waypoint_list.py
+++ mapgen/src/waypoint_list.py
@@ -1,6 +1,15 @@
-from waypoint import Waypoint
 from georect import GeoRect
 from angle import Angle
+from geopoint import GeoPoint
+
+class Waypoint:
+    def __init__(self):
+        self.location = GeoPoint()
+        self.altitude = 0
+        self.name = ""
+        
+    def __str__(self):
+        return str(self.name) + ", " + str(self.location) + ", " + str(self.altitude)
 
 class WaypointList:
     def __init__(self):
