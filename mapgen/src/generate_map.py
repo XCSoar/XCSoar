971bac8eba04701762df25419d5a3928a2d25289
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-09-23 02:29:20 +0200

    mapgen/generate_map: Added output_file command line parameter

--- mapgen/src/generate_map.py
+++ mapgen/src/generate_map.py
@@ -2,8 +2,8 @@ import sys
 from map_generator import MapGenerator
 
 def print_help():
-    print "Usage:   generate_map.py [Options] -wpf waypoint_file"
-    print "         generate_map.py [Options] -b latmin latmax lonmin lonmax"
+    print "Usage:   generate_map.py [Options] -wpf waypoint_file output_file"
+    print "         generate_map.py [Options] -b latmin latmax lonmin lonmax output_file"
     print ""
     print "Options: -r resolution (terrain resolution, arcseconds per pixel)"
 
@@ -12,19 +12,22 @@ def main():
     
     wp = None
     latmin = None
+    output_file = None
     res = 9.0
     for i in range(len(args)):
         if args[i] == "-r" and i + 1 < len(args):
             res = args[i + 1]
-        if args[i] == "-wpf" and i + 1 < len(args):
+        if args[i] == "-wpf" and i + 2 < len(args):
             wp = args[i + 1]
-        if args[i] == "-b" and i + 4 < len(args):
+            output_file = args[i + 2]
+        if args[i] == "-b" and i + 5 < len(args):
             latmin = args[i + 1]
             latmax = args[i + 2]
             lonmin = args[i + 3]
-            lonmax = args[i + 4]            
+            lonmax = args[i + 4]
+            output_file = args[i + 5]       
         
-    if wp == None and latmin == None:
+    if (wp == None and latmin == None) or output_file == None:
         print_help()
         return
     
@@ -36,7 +39,7 @@ def main():
         m.SetBoundsSeperatly(latmin, latmax, lonmin, lonmax)
     m.AddTopology()
     m.AddTerrain(res)
-    m.Create("../test.xcm.zip")
+    m.Create(output_file)
 
 if __name__ == "__main__":
     main()
