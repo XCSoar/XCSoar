c36b4326703170ce0919b1ef1ff1ce2788bfb752
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-09-24 03:45:49 +0200

    mapgen/topology_vmap0: Download missing maps from internet server

--- mapgen/src/topology_vmap0.py
+++ mapgen/src/topology_vmap0.py
@@ -1,8 +1,14 @@
-import os.path
+import urllib
+import os
 import subprocess
+import socket
+
 from georect import GeoRect
 
 cmd_ogr2ogr = "ogr2ogr"
+cmd_7zip = "7za"
+#gather_from_server = None
+gather_from_server = "http://gis-lab.info/data/vmap0/"
 
 __maps = ["eur", "noa", "sas", "soa"]
 __layers = [["pop-miscellaneous-population-p", "mispopppop_point", "", "txt"],
@@ -12,9 +18,36 @@ __layers = [["pop-miscellaneous-population-p", "mispopppop_point", "", "txt"],
             ["trans-railroad-l", "railrdltrans_line", "exs=28", "fco"],
             ["trans-road-l", "roadltrans_line", "rtt=14", "med"]]
 
+def __gather_map(dir_data, map):
+    zip_file = os.path.join(dir_data, map + ".7z")
+    if not os.path.exists(zip_file):
+        print "Downloading map file " + map + ".7z ..."
+        socket.setdefaulttimeout(10)
+        urllib.urlretrieve(gather_from_server + map + ".7z", zip_file)
+
+    if not os.path.exists(zip_file):
+        return False
+     
+    print "Decompressing map file " + map + ".7z ..."
+    arg = [cmd_7zip, "x", "-o" + dir_data, zip_file]
+    p = subprocess.Popen(arg)
+    p.wait()
+    
+    os.unlink(zip_file)
+
+    return True
+    
+
+def __check_map(dir_data, map):
+    return os.path.exists(os.path.join(dir_data, map))
+    
 def __create_layer_from_map(bounds, layer, map, overwrite, dir_data, dir_temp):
     if not isinstance(bounds, GeoRect):
         return False
+    
+    if not __check_map(dir_data, map):
+        if not __gather_map(dir_data, map):
+            return False
 
     arg = [cmd_ogr2ogr]
 
