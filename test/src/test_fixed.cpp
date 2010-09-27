f033458a5c331b88442a4253ee3ddab2fa90570e
Max Kellermann <max@duempel.org>
2010-09-27 07:27:13 +0200

    test/test_fixed: add more division tests

--- test/src/test_fixed.cpp
+++ test/src/test_fixed.cpp
@@ -1,5 +1,6 @@
 #include "test_debug.hpp"
 #include "Math/Earth.hpp"
+#include "TestUtil.hpp"
 
 #include <stdio.h>
 
@@ -9,7 +10,7 @@ int main(int argc, char** argv) {
     return 0;
   }
 
-  plan_tests(14);
+  plan_tests(19);
 
   /* check the division operator */
   ok((fixed_one / fixed_one) * fixed(1000) == fixed(1000), "1/1", 0);
@@ -25,6 +26,13 @@ int main(int argc, char** argv) {
   ok((long)((fixed_one / (fixed_one / fixed(10))) * fixed(1000)) == (10000), "1/0.1", 0);
   ok((long)((fixed_one / (fixed_one / fixed(-10))) * fixed(1000)) == -(10000) ||
      (long)((fixed_one / (fixed_one / fixed(-10))) * fixed(1000)) == -(10001), "1/-0.1", 0);
+
+  ok(equals(fixed_one / fixed_half, 2), "1/0.5", 0);
+  ok(equals(fixed(1000) / fixed_half, 2000), "1/0.5", 0);
+  ok(equals(fixed(1000) / (fixed_one / 5), 5000), "1/0.5", 0);
+
+  ok(equals(fixed(1000000) / (fixed_one / 5), 5000000), "1/0.5", 0);
+  ok(equals(fixed(10000000) / (fixed_one / 5), 50000000), "1/0.5", 0);
 
   double da = 20.0;
   double dsina = sin(da);
