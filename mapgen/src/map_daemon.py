26d98c243a319f8fe626c67f596f68425d322b0c
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-10-02 13:00:49 +0200

    mapgen/map_daemon: Moved code from __execute_job() to __execute_generate_job()

--- mapgen/src/map_daemon.py
+++ mapgen/src/map_daemon.py
@@ -131,75 +131,78 @@ class MapDaemon:
         # Not jobs found
         return None
     
+    def __execute_generate_job(self, job):
+        # Check if there is anything defining the boundaries
+        if job.use_waypoint_file == False and job.bounds == None:
+            print "No waypoint file or bounds set. Aborting."
+            # If not -> Delete the job
+            self.__delete_job(job.uuid)
+            return
+
+        # Create a MapGenerator instance for creating the map file
+        m = MapGenerator()
+        
+        # Check if we should use a waypoint file
+        if job.use_waypoint_file:
+            self.__update_job_status(job.uuid, 
+                                     "Adding waypoint file...")
+            # Add the waypoint file to the MapGenerator
+            m.AddWaypointFile(os.path.join(job.dir_job, "waypoints.dat"))
+            
+        # If the job has fixed bounds defined 
+        if job.bounds != None:
+            # ... use them
+            m.SetBounds(job.bounds)
+        else:
+            # If not -> Calculate the bounds of the waypoint file
+            m.SetBoundsByWaypointFile(os.path.join(job.dir_job, 
+                                                   "waypoints.dat"))
+        
+        # Check if we should create topology files
+        if job.use_topology:
+            self.__update_job_status(job.uuid, 
+                                     "Creating topology files...")
+            m.AddTopology()
+
+        # Check if we should create terrain files
+        if job.use_terrain:
+            self.__update_job_status(job.uuid, 
+                                     "Creating terrain files...")
+            m.AddTerrain(job.resolution)
+        
+        # Check if we should use a waypoint details file
+        if job.use_waypoint_details_file:
+            self.__update_job_status(job.uuid, 
+                                     "Adding waypoint details file...")
+            # Add the waypoint details file to the MapGenerator
+            m.AddWaypointDetailsFile(os.path.join(job.dir_job, "details.txt"))
+            
+        # Check if we should use a airspace file
+        if job.use_airspace_file:
+            self.__update_job_status(job.uuid, 
+                                     "Adding airspace file...")
+            # Add the airspace file to the MapGenerator
+            m.AddAirspaceFile(os.path.join(job.dir_job, "sua.txt"))
+            
+        self.__update_job_status(job.uuid, 
+                                 "Creating map file...")
+        # Create the map file
+        m.Create(os.path.join(job.dir_job, "map.xcm"))
+        # Clean up the temporary files
+        m.Cleanup()
+        self.__update_job_status(job.uuid,  
+                                 "Done")
+        print "Map ready for use (" + os.path.join(job.dir_job, "map.xcm") + ")"
+        
+        # Activate the download lock
+        self.__lock_download(job.uuid)
+        
     def __execute_job(self, job):
         # Check for "generate" command
         if job.command == "generate":
             print "Command \"generate\" found. Generating map file."
-            
-            # Check if there is anything defining the boundaries
-            if job.use_waypoint_file == False and job.bounds == None:
-                print "No waypoint file or bounds set. Aborting."
-                # If not -> Delete the job
-                self.__delete_job(job.uuid)
-                return
-
-            # Create a MapGenerator instance for creating the map file
-            m = MapGenerator()
-            
-            # Check if we should use a waypoint file
-            if job.use_waypoint_file:
-                self.__update_job_status(job.uuid, 
-                                         "Adding waypoint file...")
-                # Add the waypoint file to the MapGenerator
-                m.AddWaypointFile(os.path.join(job.dir_job, "waypoints.dat"))
-                
-            # If the job has fixed bounds defined 
-            if job.bounds != None:
-                # ... use them
-                m.SetBounds(job.bounds)
-            else:
-                # If not -> Calculate the bounds of the waypoint file
-                m.SetBoundsByWaypointFile(os.path.join(job.dir_job, 
-                                                       "waypoints.dat"))
-            
-            # Check if we should create topology files
-            if job.use_topology:
-                self.__update_job_status(job.uuid, 
-                                         "Creating topology files...")
-                m.AddTopology()
-
-            # Check if we should create terrain files
-            if job.use_terrain:
-                self.__update_job_status(job.uuid, 
-                                         "Creating terrain files...")
-                m.AddTerrain(job.resolution)
-            
-            # Check if we should use a waypoint details file
-            if job.use_waypoint_details_file:
-                self.__update_job_status(job.uuid, 
-                                         "Adding waypoint details file...")
-                # Add the waypoint details file to the MapGenerator
-                m.AddWaypointDetailsFile(os.path.join(job.dir_job, "details.txt"))
-                
-            # Check if we should use a airspace file
-            if job.use_airspace_file:
-                self.__update_job_status(job.uuid, 
-                                         "Adding airspace file...")
-                # Add the airspace file to the MapGenerator
-                m.AddAirspaceFile(os.path.join(job.dir_job, "sua.txt"))
-                
-            self.__update_job_status(job.uuid, 
-                                     "Creating map file...")
-            # Create the map file
-            m.Create(os.path.join(job.dir_job, "map.xcm"))
-            # Clean up the temporary files
-            m.Cleanup()
-            self.__update_job_status(job.uuid,  
-                                     "Done")
-            print "Map ready for use (" + os.path.join(job.dir_job, "map.xcm") + ")"
-            
-            # Activate the download lock
-            self.__lock_download(job.uuid)
+            # Execute "generate" job
+            self.__execute_generate_job(job)
         
         # Check for "stop" command
         elif job.command == "stop":
