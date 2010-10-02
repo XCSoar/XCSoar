c59002ead9aee50dbda7a6c288b2a7264b8d3c90
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-10-02 12:28:18 +0200

    mapgen/map_daemon: Added documentation

--- mapgen/src/map_daemon.py
+++ mapgen/src/map_daemon.py
@@ -64,89 +64,130 @@ class MapDaemon:
         return (time.time() - os.path.getctime(file_lock) > 60 * 60 * 24 * 7)
     
     def __check_jobs(self):
+        # Iterate through files/folders in our jobs folder
         for file in os.listdir(self.__dir_jobs):
             dir_job = os.path.join(self.__dir_jobs, file)
+            
+            # Skip any non-folder entries
             if not os.path.isdir(dir_job):
                 continue
 
             file_job = os.path.join(dir_job, "job")
+
+            # Check if the job is locked by the creator
             if os.path.exists(file_job + ".lock"):
+                # Check if the lock is expired
                 if self.__check_job_lock_expired(file_job + ".lock"):
+                    # If expired -> Delete the outdated job
                     print "---------------------"
                     print "Job lock expired (" + file_job + ".lock)"
                     self.__delete_job(file_job)
+                # If you exists -> Continue with next job
                 continue
             
+            # If the job isn't locked -> Check if the job file exists
             if not os.path.exists(file_job):
+                # If no job file exists -> Check if the download lock exists
                 file_download = os.path.join(dir_job, "download.lock")
                 if os.path.exists(file_download):
+                    # Check if download lock is expired
                     if self.__check_download_lock_expired(file_download):
+                        # If expired -> Delete the job
                         print "---------------------"
                         print "Download lock expired (" + file_download + ")"
                         self.__delete_job(file_job)
+                # If no job file exists -> Continue with next job
                 continue
 
+            # We've found a job! -> Try to read the MapJob instance from the job file
             print "---------------------"
             print "Job found (" + file_job + ")"
             job = self.__read_job(file_job)
             if job != None:
                 return job
             
+            # If job file could not be read -> Delete the job
             self.__delete_job(file_job)
-            
+        
+        # Not jobs found
         return None
     
     def __execute_job(self, job):
+        # Check for "generate" command
         if job.command == "generate":
             print "Command \"generate\" found. Generating map file."
+            
+            # Check if there is anything defining the boundaries
             if job.use_waypoint_file == False and job.bounds == None:
                 print "No waypoint file or bounds set. Aborting."
+                # If not -> Delete the job
                 self.__delete_job(job.file_job)
                 return
 
+            # Create a MapGenerator instance for creating the map file
             m = MapGenerator()
+            
+            # Check if we should use a waypoint file
             if job.use_waypoint_file:
                 self.__update_job_status(job.dir_job, 
                                          "Adding waypoint file...")
+                # Add the waypoint file to the MapGenerator
                 m.AddWaypointFile(os.path.join(job.dir_job, "waypoints.dat"))
+                # Calculate the bounds of the waypoint file
                 m.SetBoundsByWaypointFile(os.path.join(job.dir_job, "waypoints.dat"))
+            
+            # If the job has fixed bounds defined use them instead
             if job.bounds != None:
                 m.SetBounds(job.bounds)
             
+            # Check if we should create topology files
             if job.use_topology:
                 self.__update_job_status(job.dir_job, 
                                          "Creating topology files...")
                 m.AddTopology()
 
+            # Check if we should create terrain files
             if job.use_terrain:
                 self.__update_job_status(job.dir_job, 
                                          "Creating terrain files...")
                 m.AddTerrain(job.resolution)
             
+            # Check if we should use a waypoint details file
             if job.use_waypoint_details_file:
                 self.__update_job_status(job.dir_job, 
                                          "Adding waypoint details file...")
+                # Add the waypoint details file to the MapGenerator
                 m.AddWaypointDetailsFile(os.path.join(job.dir_job, "details.txt"))
                 
+            # Check if we should use a airspace file
             if job.use_airspace_file:
                 self.__update_job_status(job.dir_job, 
                                          "Adding airspace file...")
+                # Add the airspace file to the MapGenerator
                 m.AddAirspaceFile(os.path.join(job.dir_job, "sua.txt"))
                 
             self.__update_job_status(job.dir_job, 
                                      "Creating map file...")
+            # Create the map file
             m.Create(os.path.join(job.dir_job, "map.xcm"))
+            # Clean up the temporary files
             m.Cleanup()
             self.__update_job_status(job.dir_job,  
                                      "Done")
             print "Map ready for use (" + os.path.join(job.dir_job, "map.xcm") + ")"
+            
+            # Activate the download lock
             self.__lock_download(job.dir_job)
+            # Delete job file
             self.__delete_job(job.file_job, False)
             return
-            
+        
+        # Check for "stop" command
         if job.command == "stop":
             print "Command \"stop\" found. Stopping daemon."
+            # Delete the "stop" job
             self.__delete_job(job.file_job)
+            # ... and stop the daemon 
             self.__run = False
             return
         
@@ -159,10 +200,13 @@ class MapDaemon:
         
         print "Monitoring " + self.__dir_jobs + " for new jobs ..."
         while self.__run:
+            # Check if there are new jobs
             job = self.__check_jobs()
             if job != None:
+                # If we found a job -> execute it
                 self.__execute_job(job)
             else:
+                # Otherwise sleep for a little while
                 time.sleep(0.5)
     
 if __name__ == '__main__':    
