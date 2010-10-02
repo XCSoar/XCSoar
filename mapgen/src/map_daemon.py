614afec416cdb829ba239bad564fe6328170034b
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-10-02 13:07:05 +0200

    mapgen/map_daemon: Used __get_file() function instead of MapJob.dir_job variable

--- mapgen/src/map_daemon.py
+++ mapgen/src/map_daemon.py
@@ -149,7 +149,7 @@ class MapDaemon:
             self.__update_job_status(job.uuid, 
                                      "Adding waypoint file...")
             # Add the waypoint file to the MapGenerator
-            m.AddWaypointFile(os.path.join(job.dir_job, "waypoints.dat"))
+            m.AddWaypointFile(self.__get_file(job.uuid, "waypoints.dat"))
             
         # If the job has fixed bounds defined 
         if job.bounds != None:
@@ -157,8 +157,7 @@ class MapDaemon:
             m.SetBounds(job.bounds)
         else:
             # If not -> Calculate the bounds of the waypoint file
-            m.SetBoundsByWaypointFile(os.path.join(job.dir_job, 
-                                                   "waypoints.dat"))
+            m.SetBoundsByWaypointFile(self.__get_file(job.uuid, "waypoints.dat"))
         
         # Check if we should create topology files
         if job.use_topology:
@@ -177,24 +176,24 @@ class MapDaemon:
             self.__update_job_status(job.uuid, 
                                      "Adding waypoint details file...")
             # Add the waypoint details file to the MapGenerator
-            m.AddWaypointDetailsFile(os.path.join(job.dir_job, "details.txt"))
+            m.AddWaypointDetailsFile(self.__get_file(job.uuid, "details.txt"))
             
         # Check if we should use a airspace file
         if job.use_airspace_file:
             self.__update_job_status(job.uuid, 
                                      "Adding airspace file...")
             # Add the airspace file to the MapGenerator
-            m.AddAirspaceFile(os.path.join(job.dir_job, "sua.txt"))
+            m.AddAirspaceFile(self.__get_file(job.uuid, "sua.txt"))
             
         self.__update_job_status(job.uuid, 
                                  "Creating map file...")
         # Create the map file
-        m.Create(os.path.join(job.dir_job, "map.xcm"))
+        m.Create(self.__get_file(job.uuid, "map.xcm"))
         # Clean up the temporary files
         m.Cleanup()
         self.__update_job_status(job.uuid,  
                                  "Done")
-        print "Map ready for use (" + os.path.join(job.dir_job, "map.xcm") + ")"
+        print "Map ready for use (" + self.__get_file(job.uuid, "map.xcm") + ")"
         
         # Activate the download lock
         self.__lock_download(job.uuid)
