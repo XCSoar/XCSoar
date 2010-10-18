8122f4080cc97d832757fa74e41e6bb264d465f4
Max Kellermann <max@duempel.org>
2010-10-18 20:08:53 +0200

    Makefile: hard-code SDL settings on Android

--- build/sdl.mk
+++ build/sdl.mk
@@ -5,10 +5,17 @@ ifeq ($(TARGET),UNIX)
 SDL_CPPFLAGS := $(shell pkg-config --cflags sdl 2>/dev/null)
 SDL_LDLIBS := $(shell pkg-config --libs sdl 2>/dev/null)
 else
+ifeq ($(TARGET),ANDROID)
+SDL_CPPFLAGS := -I/usr/local/android-arm/include/SDL
+SDL_LDLIBS := -L/usr/local/android-arm/lib -lsdl-1.2 -lsdl_gfx -lsdl_ttf
+else
 SDL_CPPFLAGS := -I/usr/local/i586-mingw32msvc/include/SDL
 SDL_LDLIBS := -L/usr/local/i586-mingw32msvc/lib -lSDL
 endif
+endif
 
 SDL_CPPFLAGS += -DENABLE_SDL
+ifneq ($(TARGET),ANDROID)
 SDL_LDLIBS += -lSDL_gfx -lSDL_ttf
+endif
 endif
