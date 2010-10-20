333dd159e219070b464d093f2a8a1c247ea7cb0a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-10-20 19:09:38 +0200

    NavDijkstra: Removed empty source file

--- build/task.mk
+++ build/task.mk
@@ -91,7 +91,6 @@ TASK_SRC	:=\
 	$(ENGINE_SRC_DIR)/Task/ObservationZones/BGAFixedCourseZone.cpp \
 	$(ENGINE_SRC_DIR)/Task/ObservationZones/BGAEnhancedOptionZone.cpp \
 	$(ENGINE_SRC_DIR)/Task/Tasks/PathSolvers/TaskDijkstra.cpp \
-	$(ENGINE_SRC_DIR)/Task/Tasks/PathSolvers/NavDijkstra.cpp \
 	$(ENGINE_SRC_DIR)/Task/Tasks/PathSolvers/ContestDijkstra.cpp \
 	$(ENGINE_SRC_DIR)/Task/Tasks/PathSolvers/OLCSprint.cpp \
 	$(ENGINE_SRC_DIR)/Task/Tasks/PathSolvers/OLCClassic.cpp \
