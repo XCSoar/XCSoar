464cd331c4c66008eb4661f7919705f81f3554ec
Max Kellermann <max@duempel.org>
2010-10-27 09:17:30 +0200

    Screen/ButtonWindow: draw black text with transparent background

--- src/Screen/ButtonWindow.cpp
+++ src/Screen/ButtonWindow.cpp
@@ -84,6 +84,8 @@ ButtonWindow::on_paint(Canvas &canvas)
 {
   canvas.draw_button(get_client_rect(), down);
 
+  canvas.set_text_color(Color::BLACK);
+  canvas.background_transparent();
   SIZE size = canvas.text_size(text.c_str());
   canvas.text((get_width() - size.cx) / 2 + down,
               (get_height() - size.cy) / 2 + down, text.c_str());
