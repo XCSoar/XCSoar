f0318c40ea3576081105f732a83e4f9f2043dca9
Max Kellermann <max@duempel.org>
2010-10-27 22:31:16 +0200

    Screen/Font: use DroidSans.ttf on Android
    This hard-coded path seems to be ok on Android, since that font file
    is supposed to be present always.

--- src/Screen/Font.cpp
+++ src/Screen/Font.cpp
@@ -72,7 +72,7 @@ Font::set(const LOGFONT &log_font)
     "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansCondensed.ttf";
 
 #ifdef ANDROID
-  dejavu_ttf = "/sdcard/XCSoarData/DejaVuSansCondensed.ttf";
+  dejavu_ttf = "/system/fonts/DroidSans.ttf";
 #endif
 
   return set(dejavu_ttf, log_font.lfHeight > 0 ? log_font.lfHeight : 10,
