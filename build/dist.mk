9f7e14f0904ee6053eea21c8d68b7b912a7117e9
Max Kellermann <max@duempel.org>
2010-11-02 22:35:45 +0100

    Makefile: add target XCSoarAltair.zip
    Builds a ZIP file with a compressed EXE file.

--- build/dist.mk
+++ build/dist.mk
@@ -12,3 +12,15 @@ dist: $(OUTPUTS)
 		$(TARGET_DIST_DIR)/$(TARGET_DIST_NAME)/
 	cd $(TARGET_DIST_DIR) && zip -qr $(TARGET_DIST_NAME).zip $(TARGET_DIST_NAME)
 	mv $(TARGET_DIST_DIR)/$(TARGET_DIST_NAME).zip $(OUT)/
+
+ifeq ($(TARGET),ALTAIR)
+# Build a ZIP file to be unpacked on a USB stick.  The Altair will
+# automatically pick up the EXE file with the "magic" file name
+$(TARGET_OUTPUT_DIR)/XCSoarAltair.zip: $(TARGET_OUTPUT_DIR)/bin/XCSoar.exe
+	rm -rf $(@D)/ToAltair
+	mkdir -p $(@D)/ToAltair
+	cp $< $(@D)/ToAltair/
+	upx --best $(@D)/ToAltair/XCSoar.exe
+	mv $(@D)/ToAltair/XCSoar.exe $(@D)/ToAltair/XCSoarAltair-600-CRC3E.exe
+	cd $(@D) && zip -r -Z store $(@F) ToAltair
+endif
