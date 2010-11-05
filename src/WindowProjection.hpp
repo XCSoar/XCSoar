7c698c5d63ff864e69c591ec7d6c6ac6c9b6d8f1
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-11-05 12:35:44 +0100

    WindowProjection: Removed unused DistanceScreenToUser() function

--- src/WindowProjection.hpp
+++ src/WindowProjection.hpp
@@ -79,11 +79,6 @@ public:
     return x * GetMapScale() / GetMapResolutionFactor();
   }
 
-  gcc_pure
-  fixed DistanceScreenToUser(const int x) const {
-    return x * GetMapScaleUser() / GetMapResolutionFactor();
-  }
-
   /**
    * Returns the width of the map area in meters.
    */
