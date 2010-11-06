b468ed625ed82805cb4cb96a8a8d0ae127a200c6
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-11-06 18:46:25 +0100

    MapWindowProjection: Include cleanup

--- src/MapWindowProjection.cpp
+++ src/MapWindowProjection.cpp
@@ -22,10 +22,8 @@ Copyright_License {
 */
 
 #include "MapWindowProjection.hpp"
-#include "Math/FastRotation.hpp"
 #include "Screen/Layout.hpp"
 #include "NMEA/Info.hpp"
-#include "NMEA/Derived.hpp"
 #include "Waypoint/Waypoint.hpp"
 #include "Units.hpp"
 
