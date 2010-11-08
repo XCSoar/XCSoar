25b76486621fb0d831029921c875173804661b61
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-11-08 12:40:12 +0100

    doc/XCSoar.doxyfile: Create doxygen files in output folder

--- build/doco.mk
+++ build/doco.mk
@@ -1,6 +1,7 @@
 .PHONY: manual clean-manual doco
 
 MANUAL_OUTPUT_DIR = $(OUT)/all/manual
+DOXYGEN_OUTPUT_DIR = $(OUT)/doc
 
 manual: $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf
 
@@ -11,4 +12,6 @@ $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(DOC)/manual/XCSoar-manual.tex $(MANUAL
 
 doco: FORCE
 #	$(topdir)/ex/tools/cloc --exclude-lang=D src > doc/cloc.txt
+	rm -rf $(DOXYGEN_OUTPUT_DIR)
+	mkdir -p $(DOXYGEN_OUTPUT_DIR)
 	cd $(DOC) && doxygen XCSoar.doxyfile
