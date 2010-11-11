18cbe5bea84cf47a019a5b2c5fdb9aec1463285b
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-11-11 19:59:00 +0100

    harness_flight: Fixed Format

--- test/src/harness_flight.cpp
+++ test/src/harness_flight.cpp
@@ -2,6 +2,7 @@
 #include "harness_airspace.hpp"
 #include "TaskEventsPrint.hpp"
 #include "Util/AircraftStateFilter.hpp"
+
 #ifdef DO_PRINT
 #include <fstream>
 #endif
@@ -10,13 +11,11 @@ Airspaces *airspaces = NULL;
 AirspaceWarningManager *airspace_warnings = NULL;
 AircraftStateFilter *aircraft_filter = NULL;
 
-
-
-double time_elapsed=0.0;
-double time_planned=1.0;
-double time_remaining=0;
-double calc_cruise_efficiency=1.0;
-double calc_effective_mc=1.0;
+double time_elapsed = 0.0;
+double time_planned = 1.0;
+double time_remaining = 0;
+double calc_cruise_efficiency = 1.0;
+double calc_effective_mc = 1.0;
 
 fixed
 aat_min_time(int test_num)
@@ -33,44 +32,42 @@ aat_min_time(int test_num)
 static fixed
 wind_to_mag(int n_wind)
 {
-  if (n_wind) {
+  if (n_wind)
     return (fixed(n_wind - 1) / 4 + fixed_one) * 5;
-  }
+
   return fixed_zero;
 }
 
 static Angle
 wind_to_dir(int n_wind)
 {
-  if (n_wind) {
-    return Angle::degrees(fixed(90*((n_wind-1)%4)));
-  }
+  if (n_wind)
+    return Angle::degrees(fixed(90 * ((n_wind - 1) % 4)));
+
   return Angle();
 }
 
-const char* wind_name(int n_wind) {
+const char*
+wind_name(int n_wind)
+{
   static char buffer[80];
-  sprintf(buffer,"%d m/s @ %d",
-          (int)wind_to_mag(n_wind),
+  sprintf(buffer,"%d m/s @ %d", (int)wind_to_mag(n_wind),
           (int)wind_to_dir(n_wind).value_degrees());
   return buffer;
 }
 
-
-bool run_flight(TaskManager &task_manager,
-                bool goto_target,
-                double random_mag,
-                int n_wind,
-                const double speed_factor) 
+bool
+run_flight(TaskManager &task_manager, bool goto_target, double random_mag,
+           int n_wind, const double speed_factor)
 {
   srand(0);
 
   AircraftSim ac(0, task_manager, random_mag, goto_target);
-  unsigned print_counter=0;
+  unsigned print_counter = 0;
 
-  if (n_wind) {
-    ac.set_wind(wind_to_mag(n_wind),wind_to_dir(n_wind));
-  }
+  if (n_wind)
+    ac.set_wind(wind_to_mag(n_wind), wind_to_dir(n_wind));
+
   ac.set_speed_factor(fixed(speed_factor));
 
 #ifdef DO_PRINT
@@ -81,42 +78,37 @@ bool run_flight(TaskManager &task_manager,
   bool do_print = verbose;
   bool first = true;
 
-  time_elapsed=0.0;
-  time_planned=1.0;
-  time_remaining=0;
-  calc_cruise_efficiency=1.0;
-  calc_effective_mc=1.0;
+  time_elapsed = 0.0;
+  time_planned = 1.0;
+  time_remaining = 0;
+  calc_cruise_efficiency = 1.0;
+  calc_effective_mc = 1.0;
 
   static const fixed fixed_10(10);
 
   AirspaceAircraftPerformanceGlide perf(task_manager.get_glide_polar());
 
-  if (aircraft_filter) {
+  if (aircraft_filter)
     aircraft_filter->reset(ac.get_state());
-  }
 
-  if (airspaces) {
-    airspace_warnings = new AirspaceWarningManager(*airspaces,
-                                                   ac.get_state(),
-                                                   task_manager);
-  }
+  if (airspaces)
+    airspace_warnings =
+        new AirspaceWarningManager(*airspaces, ac.get_state(),task_manager);
 
   do {
-
-    if ((task_manager.getActiveTaskPointIndex()==1) && first 
-        && (task_manager.get_stats().total.TimeElapsed>fixed_10)) {
+    if (task_manager.getActiveTaskPointIndex() == 1 &&
+        first && task_manager.get_stats().total.TimeElapsed > fixed_10) {
       time_remaining = task_manager.get_stats().total.TimeRemaining;
       first = false;
 
       time_planned = task_manager.get_stats().total.TimePlanned;
 
 #ifdef DO_PRINT
-      if (verbose>1) {
+      if (verbose > 1) {
         printf("# time remaining %g\n", time_remaining);
         printf("# time planned %g\n", time_planned);
       }
 #endif
-
     }
 
 #ifdef DO_PRINT
@@ -133,17 +125,17 @@ bool run_flight(TaskManager &task_manager,
     }
 #endif
 
-    if (airspaces) {
-      scan_airspaces(ac.get_state(), *airspaces, perf,
-                     do_print, 
+    if (airspaces)
+      scan_airspaces(ac.get_state(), *airspaces, perf, do_print,
                      ac.target(task_manager));
-    }
+
     if (airspace_warnings) {
 #ifdef DO_PRINT
-      if (verbose>1) {
+      if (verbose > 1) {
         bool warnings_updated = airspace_warnings->update(ac.get_state());
         if (warnings_updated) {
-          printf("# airspace warnings updated, size %d\n", (int)airspace_warnings->size());
+          printf("# airspace warnings updated, size %d\n",
+                 (int)airspace_warnings->size());
           print_warnings();
           wait_prompt(ac.get_state().Time);
         }
@@ -153,12 +145,10 @@ bool run_flight(TaskManager &task_manager,
 
     n_samples++;
 
-    do_print = (++print_counter % output_skip ==0) && verbose;
+    do_print = (++print_counter % output_skip == 0) && verbose;
 
-    if (aircraft_filter) {
+    if (aircraft_filter)
       aircraft_filter->update(ac.get_state());
-    }
-
   } while (ac.advance(task_manager));
 
 #ifdef DO_PRINT
@@ -177,20 +167,19 @@ bool run_flight(TaskManager &task_manager,
   calc_cruise_efficiency = task_manager.get_stats().cruise_efficiency;
   calc_effective_mc = task_manager.get_stats().effective_mc;
 
-  if (verbose) {
+  if (verbose)
     distance_counts();
-  }
 
-  if (airspace_warnings) {
+  if (airspace_warnings)
     delete airspace_warnings;
-  }
 
   return true;
 }
 
 
-bool test_flight(int test_num, int n_wind, const double speed_factor,
-                 const bool auto_mc) 
+bool
+test_flight(int test_num, int n_wind, const double speed_factor,
+            const bool auto_mc)
 {
   // multipurpose flight test
 
@@ -198,15 +187,11 @@ bool test_flight(int test_num, int n_wind, const double speed_factor,
   Waypoints waypoints;
   setup_waypoints(waypoints);
 
-  if (verbose) {
+  if (verbose)
     distance_counts();
-  }
 
   TaskEventsPrint default_events(verbose);
-
-  TaskManager task_manager(default_events,
-                           waypoints);
-
+  TaskManager task_manager(default_events, waypoints);
   task_manager.set_glide_polar(glide_polar);
 
   TaskBehaviour& task_behaviour = task_manager.get_task_behaviour();
@@ -215,9 +200,8 @@ bool test_flight(int test_num, int n_wind, const double speed_factor,
 
   task_behaviour.auto_mc = auto_mc;
   task_behaviour.calc_glide_required = false;
-  if ((test_num ==0) || (test_num==2)) {
+  if ((test_num == 0) || (test_num == 2))
     task_behaviour.optimise_targets_bearing = false;
-  }
 
   bool goto_target = false;
 
@@ -233,7 +217,7 @@ bool test_flight(int test_num, int n_wind, const double speed_factor,
     break;
   default:
     break;
-  };
+  }
 
   test_task(task_manager, waypoints, test_num);
 
@@ -241,15 +225,15 @@ bool test_flight(int test_num, int n_wind, const double speed_factor,
                     speed_factor);
 }
 
-bool test_flight_times(int test_num, int n_wind) 
+bool
+test_flight_times(int test_num, int n_wind)
 {
-
   // tests whether elapsed/planned times are consistent
   // there will be small error due to start location
 
   bool fine = test_flight(test_num, n_wind);
-  const double t_rat = fabs(time_elapsed/time_planned-1.0);
-  fine &= t_rat<0.02;
+  const double t_rat = fabs(time_elapsed / time_planned - 1.0);
+  fine &= t_rat < 0.02;
 
   if ((verbose) || !fine) {
     printf("# time remaining %g\n", time_remaining);
@@ -257,7 +241,6 @@ bool test_flight_times(int test_num, int n_wind)
     printf("# time planned %g\n", time_planned);
     printf("# time ratio error (elapsed/planned) %g\n", t_rat);
   }
+
   return fine;
 }
-
-
