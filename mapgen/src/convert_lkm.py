1063d2fa96c55d025fa32113de55f52f58983b9f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2010-11-22 21:58:28 +0100

    mapgen/convert_lkm: Added LKM maps converter
    Usage: python convert_lkm.py GER.txt
    
    GER.txt and GER.lkm are necessary for converting the GER map and should be in the same folder

---
+++ mapgen/src/convert_lkm.py
@@ -0,0 +1,63 @@
+import sys
+import os
+from map_generator import MapGenerator
+from angle import Angle
+import shutil
+
+def read_template(file):
+    map = {}
+
+    print "Opening template file \"" + file + "\" ..."
+    f = open(file, "r")
+    for line in f:
+        if line[0] == "#":
+            continue
+        
+        line = line.strip().upper().split("=", 1)
+        map[line[0]] = line[1]
+        
+    return map
+
+def convert(template, working_dir):
+    if not "NAME" in template:
+        print "Template file has no NAME specified!"
+        return
+    
+    name = template["NAME"]
+
+    lkm_file = os.path.join(working_dir, name + ".LKM")
+    xcm_file = os.path.join(working_dir, name + ".xcm")
+
+    if os.path.exists(xcm_file):
+        os.unlink(xcm_file)
+        
+    if not os.path.exists(lkm_file):
+        print "LKM file \"" + lkm_file + "\" does not exist!"
+        return
+    
+    shutil.copy(lkm_file, xcm_file)
+
+    m = MapGenerator()
+    m.SetBoundsSeperatly(Angle.degrees(float(template["LATMIN"])), 
+                         Angle.degrees(float(template["LATMAX"])), 
+                         Angle.degrees(float(template["LONMIN"])), 
+                         Angle.degrees(float(template["LONMAX"])))
+    m.AddTerrain(9)
+    m.Create(xcm_file, True)
+
+def main():
+    if len(sys.argv) < 2:
+        print "Too few arguments given! Please provide a template file."
+        return
+    
+    file = sys.argv[1]
+    if not os.path.exists(file):
+        print "Template file \"" + file + "\" does not exist!"
+        return
+    
+    working_dir = os.path.dirname(os.path.abspath(file))
+    template = read_template(file)
+    convert(template, working_dir)
+    
+if __name__ == '__main__':
+    main()    
