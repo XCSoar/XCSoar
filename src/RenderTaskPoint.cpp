f1c6409d1acefdbbe4cd0d2308cdda54a045eaec
Max Kellermann <max@duempel.org>
2010-11-30 18:01:32 +0100

    RenderTaskPoint: use polygon() instead of polyline()
    With Canvas::polygon(), we don't need to specify the end point
    explicitly.

--- src/RenderTaskPoint.cpp
+++ src/RenderTaskPoint.cpp
@@ -269,13 +269,14 @@ RenderTaskPoint::draw_isoline(const AATPoint& tp)
   if (m_proj.GeoToScreenDistance(seg.parametric(fixed_zero).
                                     distance(seg.parametric(fixed_one)))>2) {
     
-    for (fixed t = fixed_zero; t<=fixed_one; t+= fixed_twentieth) {
+    for (fixed t = fixed_zero; t<fixed_one; t+= fixed_twentieth) {
       GeoPoint ga = seg.parametric(t);
       screen.push_back(m_proj.GeoToScreen(ga));
     }
     if (screen.size()>=2) {
       m_buffer.select(pen_isoline);
-      m_buffer.polyline(&screen[0], screen.size());
+      m_buffer.hollow_brush();
+      m_buffer.polygon(&screen[0], screen.size());
     }
   }
 }
