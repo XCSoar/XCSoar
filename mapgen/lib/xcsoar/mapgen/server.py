754ecf27012f0cd0a9e4ea59531905496b311998
Daniel Mendler <mail@daniel-mendler.de>
2010-12-11 00:02:21 +0100

    mapgen: add osm widget to select map bounds

--- mapgen/lib/xcsoar/mapgen/server.py
+++ mapgen/lib/xcsoar/mapgen/server.py
@@ -4,6 +4,7 @@ import cherrypy
 from genshi.filters import HTMLFormFiller
 from xcsoar.mapgen.job import Job
 from xcsoar.mapgen import view
+from xcsoar.mapgen.georect import GeoRect
 
 class Server(object):
     def __init__(self, dir_jobs):
@@ -18,14 +19,10 @@ class Server(object):
 
     @cherrypy.expose
     @view.output('index.html')
-    def generate(self, name, mail, waypoint_file):
+    def generate(self, name, mail, waypoint_file, latmin, latmax, lonmin, lonmax):
         name = name.strip()
         if name == "":
             return view.render(error='No map name given!') | HTMLFormFiller(data=dict(name=name, mail=mail))
-
-        mail = mail.strip()
-        if mail == "":
-            return view.render(error='No E-Mail given!') | HTMLFormFiller(data=dict(name=name, mail=mail))
 
         if not waypoint_file.file:
             return view.render(error='Waypoint file could not be read!') | HTMLFormFiller(data=dict(name=name, mail=mail))
@@ -35,14 +32,23 @@ class Server(object):
         job.description.mail = mail
         job.description.waypoint_file = 'waypoints.dat'
 
+        try:
+            job.description.bounds = GeoRect(float(lonmin.strip()), float(lonmax.strip()), float(latmax.strip()), float(latmin.strip()))
+        except:
+            pass
+
         path = job.file_path(job.description.waypoint_file)
         f = open(path, "w")
         shutil.copyfileobj(fsrc=waypoint_file.file, fdst=f, length=1024 * 64)
         f.close()
 
         if os.path.getsize(path) == 0:
+            os.unlink(path)
+            job.description.waypoint_file = None
+
+        if job.description.bounds == None and job.description.waypoint_file == None:
             job.delete()
-            return view.render(error='Waypoint file is empty!') | HTMLFormFiller(data=dict(name=name, mail=mail))
+            return view.render(error='Waypoint file or bounds are required!') | HTMLFormFiller(data=dict(name=name, mail=mail))
 
         job.enqueue()
         raise cherrypy.HTTPRedirect('/status?uuid=' + job.uuid)
