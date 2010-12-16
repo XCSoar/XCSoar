732f33e1d3aab23378999b593c06f9cf961b6a32
Max Kellermann <max@duempel.org>
2010-12-16 18:37:05 +0100

    Compatibility/vk.h: remove PPC2000 macros
    These are duplicate, they are already defined by mingw32ce system
    headers.

--- src/Compatibility/vk.h
+++ src/Compatibility/vk.h
@@ -35,22 +35,6 @@ Copyright_License {
 #define VK_APP5 0x35
 #define VK_APP6 0x36
 
-#elif defined(WIN32_PLATFORM_PSPC)
-// Pocket PC
-
-	#if (_WIN32_WCE == 300)
-	// Pocket PC 2000
-		// App keys
-		#define VK_APP1     0xC1
-		#define VK_APP2     0xC2
-		#define VK_APP3     0xC3
-		#define VK_APP4     0xC4
-		#define VK_APP5     0xC5
-		#define VK_APP6     0xC6
-	    // Note - note used on most builds...
-		// #define VK_APP7     0xC7
-		// #define VK_APP8     0xC8
-	#endif
 #endif
 
 #endif
