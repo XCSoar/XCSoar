3bf0d46b919c374d9ad6233a9493b19150ba46e8
Helmut J. Rohs <helmut.j.rohs@web.de>
2011-01-03 00:58:50 +0100

    Better dependencies for make manual.

--- build/doco.mk
+++ build/doco.mk
@@ -2,13 +2,15 @@
 
 MANUAL_OUTPUT_DIR = $(OUT)/all/manual
 DOXYGEN_OUTPUT_DIR = $(OUT)/doc
+SOURCES = $(DOC)/manual/*.tex $(DOC)/manual/*.sty
+FIGURES = $(DOC)/manual/figures/*.png
 
 manual: $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf
 
-$(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(DOC)/manual/XCSoar-manual.tex $(MANUAL_OUTPUT_DIR)/dirstamp
+$(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(SOURCES) $(FIGURES) $(MANUAL_OUTPUT_DIR)/dirstamp
 	# run TeX twice to make sure that all references are resolved
-	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) $(<F)
-	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) $(<F)
+	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) ./XCSoar-manual.tex
+	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) ./XCSoar-manual.tex
 
 doco: FORCE
 #	$(topdir)/ex/tools/cloc --exclude-lang=D src > doc/cloc.txt
