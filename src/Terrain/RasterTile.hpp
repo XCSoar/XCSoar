3af9ce4db0825c212027dad1e6c9a273824b95a8
Max Kellermann <max@duempel.org>
2011-01-12 19:53:55 +0100

    Terrain/RasterTile: allow 32 active tiles on modern devices
    Old devices may suffer from scarce memory, but new devices usually
    have at least 64 MB.  It seems reasonable to allow more loaded raster
    tiles at a time on these.  Carefully increasing MAX_ACTIVE_TILES, to
    see how it works out in XCSoar 6.0.2, trying to work around #780.
    
    One day, we'll make that dynamic, depending on the amounf of memory
    really available.

--- src/Terrain/RasterTile.hpp
+++ src/Terrain/RasterTile.hpp
@@ -87,7 +87,16 @@ class RasterTileCache : private NonCopyable {
    * The maximum number of tiles which are loaded at a time.  This
    * must be limited because the amount of memory is finite.
    */
+#if !defined(_WIN32_WCE) && !defined(ANDROID)
+  // desktop: use a lot of memory
+  static const unsigned MAX_ACTIVE_TILES = 64;
+#elif !defined(_WIN32_WCE) || _WIN32_WCE >= 0x0400
+  // embedded: use less memory
+  static const unsigned MAX_ACTIVE_TILES = 32;
+#else
+  // old Windows CE: use only little memory
   static const unsigned MAX_ACTIVE_TILES = 16;
+#endif
 
   static const unsigned RTC_SUBSAMPLING = 16;
 
