d96aa90ef68d3eb0cc975fad61553c84d476bfa2
Max Kellermann <max@duempel.org>
2011-01-18 08:14:19 +0100

    copyright year 2011

--- src/Engine/Task/Factory/FAIGoalTaskFactory.hpp
+++ src/Engine/Task/Factory/FAIGoalTaskFactory.hpp
@@ -1,7 +1,7 @@
 /* Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2010 The XCSoar Project
+  Copyright (C) 2000-2011 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
