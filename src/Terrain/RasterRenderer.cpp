XCSoar/XCSoar

commit dcccc9187fa919b0b87d50f4bae1db58f405c1f0

Terrain/RasterRenderer: round quantisation_effective down
It turns out that by using ceil(), we get quantisation_effective==2
for q=1.2, and that reveals spurious artefacts by the slope shading
algorithm.  This is because the interpolation in RasterBuffer creates
some sort of moiree effect.  By rounding down quantisation_effective,
these disappear mostly, making a better fit for slope shading.

This was reported to TRAC as ticket #760.

src/Terrain/RasterRenderer.cpp
@@ -100,7 +100,10 @@ RasterRenderer::ScanMap(const RasterMap &map, const WindowProjection &projection
   fixed map_pixel_size = map.pixel_distance(Gmid, 1);
   fixed q = map_pixel_size / pixel_size;
   if (pixel_size < fixed(3000)) {
-    quantisation_effective = (int)ceil(q);
+    /* round down to reduce slope shading artefacts (caused by
+       RasterBuffer interpolation) */
+    quantisation_effective = (int)q;
+
     if (quantisation_effective > 25)
       /* disable slope shading when zoomed in very near (not enough
          terrain resolution to make a useful slope calculation) */

commit c7bdd37f788c5160fd2e12db89fcee742fbac0bb

RasterRenderer: Fixed slope shading threshold
This was broken by dcccc9187fa919b0b87d50f4bae1db58f405c1f0 when converting from ceil() to implicit floor()

There are cases when quantisation_effective is smaller then 1 and would be rounded/floored to 0,
which results in completely disabling the slope shading algorithm.

src/Terrain/RasterRenderer.cpp
@@ -102,7 +102,7 @@ RasterRenderer::ScanMap(const RasterMap &map, const WindowProjection &projection
   if (pixel_size < fixed(3000)) {
     /* round down to reduce slope shading artefacts (caused by
        RasterBuffer interpolation) */
-    quantisation_effective = (int)q;
+    quantisation_effective = std::max(1, (int)q);
 
     if (quantisation_effective > 25)
       /* disable slope shading when zoomed in very near (not enough