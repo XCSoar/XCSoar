66c0e8c6781c977b91dab8e3d5baeb8791358312
Max Kellermann <max@duempel.org>
2011-01-20 01:00:24 +0100

    Makefile: the manual depends on only one .tex file
    Prepare for adding the developer manual.

--- build/manual.mk
+++ build/manual.mk
@@ -1,5 +1,5 @@
 MANUAL_OUTPUT_DIR = $(OUT)/all/manual
-SOURCES = $(DOC)/manual/*.tex $(DOC)/manual/*.sty
+TEX_INCLUDES = $(wildcard $(DOC)/manual/*.sty)
 FIGURES = $(DOC)/manual/figures/*.png
 SVG_ICON_LIST = \
 	alt2_landable_airport \
@@ -17,10 +17,11 @@ SVG_ICONS = $(patsubst %,$(DOC)/manual/generated/icons/%.png,$(SVG_ICON_LIST))
 .PHONY: manual
 manual: $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf
 
-$(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(SOURCES) $(FIGURES) $(SVG_ICONS) $(MANUAL_OUTPUT_DIR)/dirstamp
+$(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(DOC)/manual/XCSoar-manual.tex $(TEX_INCLUDES) \
+	$(FIGURES) $(SVG_ICONS) | $(MANUAL_OUTPUT_DIR)/dirstamp
 	# run TeX twice to make sure that all references are resolved
-	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) ./XCSoar-manual.tex
-	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) ./XCSoar-manual.tex
+	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) $(<F)
+	cd $(<D) && pdflatex -output-directory $(abspath $(@D)) $(<F)
 
 $(SVG_ICONS): $(DOC)/manual/generated/icons/%.png: $(topdir)/Data/icons/%.svg | $(DOC)/manual/generated/icons/dirstamp
 	rsvg-convert -a -w 32 $< -o $@
