84bb4b60681decc0c425619cfde72507a1431960
Max Kellermann <max@duempel.org>
2011-02-03 22:24:02 +0100

    Java: add class "String"
    Wraps a "jstring".

---
+++ src/Java/String.hpp
@@ -0,0 +1,43 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2011 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_JAVA_STRING_HPP
+#define XCSOAR_JAVA_STRING_HPP
+
+#include "Java/Ref.hpp"
+
+#include <assert.h>
+#include <jni.h>
+
+namespace Java {
+  /**
+   * Wrapper for a local "jstring" reference.
+   */
+  class String : public LocalRef<jstring> {
+  public:
+    String(JNIEnv *_env, const char *_value)
+      :LocalRef<jstring>(_env, _env->NewStringUTF(_value)) {}
+  };
+}
+
+#endif
