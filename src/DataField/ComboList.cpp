XCSoar/XCSoar

commit 555fa5e7686b631486a66717bad6bd9700454e1a

Enum.cpp, Base.cpp, ListPicker.cpp, ComboList.cpp, dlgComboPicker: add Item-level help
Add item-level help in the ComboList for enums.  Adds a help text field to the Item
in the Enum class.  Allows the ListPicker and the ComboList classes to
optionally display it.

src/DataField/ComboList.cpp
@@ -28,14 +28,17 @@ Copyright_License {
 
 ComboList::Item::Item(int _DataFieldIndex,
                         const TCHAR *_StringValue,
-                        const TCHAR *_StringValueFormatted)
+                        const TCHAR *_StringValueFormatted,
+                        const TCHAR *_StringHelp)
   :DataFieldIndex(_DataFieldIndex),
    StringValue(_tcsdup(_StringValue)),
-   StringValueFormatted(_tcsdup(_StringValueFormatted)) {}
+   StringValueFormatted(_tcsdup(_StringValueFormatted)),
+   StringHelp(_tcsdup(_StringHelp)) {}
 
 ComboList::Item::~Item() {
   free(StringValue);
   free(StringValueFormatted);
+  free(StringHelp);
 }
 
 void

src/DataField/ComboList.hpp
@@ -40,9 +40,10 @@ public:
     int DataFieldIndex;
     TCHAR *StringValue;
     TCHAR *StringValueFormatted;
+    TCHAR *StringHelp;
 
     Item(int _DataFieldIndex,
-         const TCHAR *_StringValue, const TCHAR *_StringValueFormatted);
+         const TCHAR *_StringValue, const TCHAR *_StringValueFormatted, const TCHAR *_StringHelp = _T(""));
     ~Item();
   };
 
@@ -75,9 +76,10 @@ private:
 
   unsigned Append(int DataFieldIndex,
                   const TCHAR *StringValue,
-                  const TCHAR *StringValueFormatted) {
+                  const TCHAR *StringValueFormatted,
+                  const TCHAR *StringHelp = NULL) {
     return Append(new Item(DataFieldIndex,
-                           StringValue, StringValueFormatted));
+                           StringValue, StringValueFormatted, StringHelp));
   }
 
   unsigned Append(int DataFieldIndex, const TCHAR *StringValue) {

src/DataField/Enum.cpp
@@ -57,13 +57,14 @@ DataFieldEnum::replaceEnumText(unsigned int i, const TCHAR *Text)
 }
 
 bool
-DataFieldEnum::addEnumText(const TCHAR *Text, unsigned id)
+DataFieldEnum::addEnumText(const TCHAR *Text, unsigned id, const TCHAR *ItemHelpText)
 {
   if (entries.full())
     return false;
 
   Entry &entry = entries.append();
   entry.mText = _tcsdup(Text);
+  entry.mHelp = _tcsdup(ItemHelpText);
   entry.id = id;
   return true;
 }
@@ -179,7 +180,7 @@ DataFieldEnum::CreateComboList() const
   ComboList *combo_list = new ComboList();
 
   for (unsigned i = 0; i < entries.size(); i++)
-    combo_list->Append(entries[i].id, entries[i].mText);
+    combo_list->Append(entries[i].id, entries[i].mText,  entries[i].mText, entries[i].mHelp);
 
   combo_list->ComboPopupItemSavedIndex = mValue;
   return combo_list;

Data/Dialogs/dlgComboPicker.xml
@@ -8,4 +8,5 @@
   <Button Name="cmdHelp" Caption="Help" X="160" Y="-37" Width="70" Height="35" OnClick="OnHelpClicked"/>
 
   <List Name="frmComboPopupList" X="1" Y="1" Width="-1" Height="-39" ItemHeight="18"/>
+  <Label Name="lblItemHelp" X="1" Y="-80" Height="41" Width="-1" Visible="0" Border="1"/>
 </Form>

Data/Dialogs/dlgComboPicker_L.xml
@@ -8,4 +8,5 @@
   <Button Name="cmdHelp" Caption="Help" X="5" Y="180" Width="70" Height="35" OnClick="OnHelpClicked"/>
 
   <List Name="frmComboPopupList" X="80" Y="1" Width="-1" Height="-1" ItemHeight="18"/>
+  <Label Name="lblItemHelp" X="80" Y="172 Height="40" Width="-1" Visible="0" Border="1"/>
 </Form>

src/DataField/Base.cpp
@@ -88,7 +88,7 @@ __Dummy(DataField *Sender, DataField::DataAccessKind_t Mode)
 DataField::DataField(const TCHAR *EditFormat, const TCHAR *DisplayFormat,
                      DataAccessCallback_t OnDataAccess)
   :mOnDataAccess(OnDataAccess != NULL ? OnDataAccess : __Dummy),
-   mUsageCounter(0), mDisableSpeedup(false), mDetachGUI(false)
+   mItemHelp(false), mUsageCounter(0), mDisableSpeedup(false), mDetachGUI(false)
 {
   _tcscpy(mEditFormat, EditFormat);
   _tcscpy(mDisplayFormat, DisplayFormat);

src/DataField/Base.hpp
@@ -77,6 +77,7 @@ public:
     mUsageCounter--;
     return mUsageCounter;
   }
+  virtual void EnableItemHelp(bool value) {};
 
   void SetDisplayFormat(TCHAR *Value);
   // allows combolist to iterate all values
@@ -92,6 +93,7 @@ public:
   {
     SetAsInteger(iDataFieldIndex);
   }
+  bool GetItemHelpEnabled() { return mItemHelp; }
 
   void CopyString(TCHAR * szStringOut, bool bFormatted);
   bool SupportCombo;  // all Types dataField support combolist except DataFieldString.
@@ -101,6 +103,7 @@ protected:
   TCHAR mEditFormat[FORMATSIZE + 1];
   TCHAR mDisplayFormat[FORMATSIZE + 1];
   TCHAR mUnits[UNITSIZE + 1];
+  bool mItemHelp;
 
 private:
   int mUsageCounter;

src/DataField/Enum.hpp
@@ -34,8 +34,9 @@ public:
   struct Entry : private NonCopyable {
     TCHAR *mText;
     unsigned id;
+    TCHAR *mHelp;
 
-    Entry():mText(NULL) {}
+    Entry():mText(NULL), mHelp(NULL) {}
     ~Entry();
   };
 
@@ -61,12 +62,19 @@ public:
   virtual ComboList *CreateComboList() const;
 
   void replaceEnumText(unsigned int i, const TCHAR *Text);
-  bool addEnumText(const TCHAR *Text, unsigned id);
+  bool addEnumText(const TCHAR *Text, unsigned id, const TCHAR *ItemHelpText = NULL);
   unsigned addEnumText(const TCHAR *Text);
   void addEnumTexts(const TCHAR *const*list);
 
   virtual int GetAsInteger() const;
   virtual const TCHAR *GetAsString() const;
+
+  /**
+   * @param value True if display item help in text box below picker
+   * Displays help strings associated with enums Items
+   */
+  void EnableItemHelp(bool value) { mItemHelp = value; }
+
 
   #if defined(__BORLANDC__)
   #pragma warn -hid

commit 551428e1562a639b4024469d0b967a2d38a76d25

Enum.cpp, ComboList.cpp: fix seg fault error
error was introduced in 555fa5e7686b631486a66717bad6bd9700454e1a

src/DataField/ComboList.cpp
@@ -33,7 +33,7 @@ ComboList::Item::Item(int _DataFieldIndex,
   :DataFieldIndex(_DataFieldIndex),
    StringValue(_tcsdup(_StringValue)),
    StringValueFormatted(_tcsdup(_StringValueFormatted)),
-   StringHelp(_tcsdup(_StringHelp)) {}
+   StringHelp(_StringHelp ? _tcsdup(_StringHelp) : NULL) {}
 
 ComboList::Item::~Item() {
   free(StringValue);

src/DataField/ComboList.hpp
@@ -43,7 +43,7 @@ public:
     TCHAR *StringHelp;
 
     Item(int _DataFieldIndex,
-         const TCHAR *_StringValue, const TCHAR *_StringValueFormatted, const TCHAR *_StringHelp = _T(""));
+         const TCHAR *_StringValue, const TCHAR *_StringValueFormatted, const TCHAR *_StringHelp = NULL);
     ~Item();
   };
 

src/DataField/Enum.cpp
@@ -64,7 +64,7 @@ DataFieldEnum::addEnumText(const TCHAR *Text, unsigned id, const TCHAR *ItemHelp
 
   Entry &entry = entries.append();
   entry.mText = _tcsdup(Text);
-  entry.mHelp = _tcsdup(ItemHelpText);
+  entry.mHelp = ItemHelpText ? _tcsdup(ItemHelpText) : NULL;
   entry.id = id;
   return true;
 }
@@ -78,7 +78,7 @@ DataFieldEnum::addEnumText(const TCHAR *Text, const TCHAR *ItemHelpText)
   unsigned i = entries.size();
   Entry &entry = entries.append();
   entry.mText = _tcsdup(Text);
-  entry.mHelp = _tcsdup(ItemHelpText);
+  entry.mHelp = ItemHelpText ? _tcsdup(ItemHelpText) : NULL;
   entry.id = i;
   return i;
 }