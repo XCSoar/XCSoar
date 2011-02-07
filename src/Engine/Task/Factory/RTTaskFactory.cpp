58492c28e753e16b9d700724b88be54fc8a5811d
Rob Dunning <rob@raspberryridgesheepfarm.com>
2011-02-06 21:06:31 -0500

    RTTaskFactory.cpp: att FAI start/finish/quadrand
    Merge FAI tasks into Racing task type.

--- src/Engine/Task/Factory/RTTaskFactory.cpp
+++ src/Engine/Task/Factory/RTTaskFactory.cpp
@@ -29,13 +29,16 @@ RTTaskFactory::RTTaskFactory(OrderedTask& _task,
 {
   m_start_types.push_back(START_LINE);
   m_start_types.push_back(START_CYLINDER);
+  m_start_types.push_back(START_SECTOR);
   m_start_types.push_back(START_BGA);
   m_intermediate_types.push_back(AST_CYLINDER);
   m_intermediate_types.push_back(KEYHOLE_SECTOR);
   m_intermediate_types.push_back(BGAFIXEDCOURSE_SECTOR);
   m_intermediate_types.push_back(BGAENHANCEDOPTION_SECTOR);
+  m_intermediate_types.push_back(FAI_SECTOR);
   m_finish_types.push_back(FINISH_LINE);
   m_finish_types.push_back(FINISH_CYLINDER);
+  m_finish_types.push_back(FINISH_SECTOR);
 }
 
 bool 
