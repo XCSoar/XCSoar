3a70c5bec4b42e0d2ccc47e1ebab197858a7a8a3
Max Kellermann <max@duempel.org>
2011-02-18 22:44:53 +0100

    Makefile: remove the remains of GConf

--- build/libprofile.mk
+++ build/libprofile.mk
@@ -7,18 +7,9 @@ ifeq ($(HAVE_WIN32),y)
 PROFILE_SOURCES += $(SRC)/Profile/Registry.cpp
 endif
 
-ifeq ($(USE_GCONF),y)
-PROFILE_SOURCES += $(SRC)/Profile/GConf.cpp
-endif
-
 PROFILE_OBJS = $(call SRC_TO_OBJ,$(PROFILE_SOURCES))
 PROFILE_LIBS = $(TARGET_OUTPUT_DIR)/profile.a
 PROFILE_LDLIBS =
-
-ifeq ($(USE_GCONF),y)
-PROFILE_LDLIBS += $(GCONF_LDLIBS)
-$(PROFILE_OBJS): CPPFLAGS += $(GCONF_CPPFLAGS)
-endif
 
 $(PROFILE_LIBS): $(PROFILE_OBJS)
 	@$(NQ)echo "  AR      $@"
