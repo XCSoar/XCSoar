cb9601842d70c62c32ba6d2a5175428ea6b15ba6
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-02-28 11:13:14 +0100

    Polar: GetString() is const

--- src/Polar/Polar.cpp
+++ src/Polar/Polar.cpp
@@ -92,7 +92,7 @@ SimplePolar::CopyIntoGlidePolar(GlidePolar &polar) const
 }
 
 void
-SimplePolar::GetString(TCHAR* line, size_t size)
+SimplePolar::GetString(TCHAR* line, size_t size) const
 {
   _sntprintf(line, size, _T("%.0f,%.0f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f"),
              dry_mass, max_ballast, v1, w1, v2, w2, v3, w3, wing_area);
