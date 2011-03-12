46f5ea5d7778a6c6379d46fb7ea20e8b3e089435
John Wharington <jwharington@gmail.com>
2011-03-12 18:01:32 +1100

    AirspaceRoute: exclude airspaces inside origin and destination
    Previously, planning will fail if the origin or destination are inside
    airspace.  This patch excludes those airspaces from the local store used by the
    planning system so valid paths will be produced even in those cases.
    
    It is up to the pilot whether he/she wants to fly to a target inside airspace,
    or is already in airspace!

--- src/Engine/Airspace/AirspacePredicate.hpp
+++ src/Engine/Airspace/AirspacePredicate.hpp
@@ -1,7 +1,29 @@
+/* Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2010 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+ */
 #ifndef AIRSPACE_PREDICATE_HPP
 #define AIRSPACE_PREDICATE_HPP
 
 #include "Compiler.h"
+#include "Navigation/GeoPoint.hpp"
 
 class AbstractAirspace;
 class AirspacePredicateTrue;
@@ -84,10 +106,39 @@ public:
   AirspacePredicateHeightRange(const short _h_min, const short _h_max):
     h_min(_h_min), h_max(_h_max) {};
 
-  bool operator()( const AbstractAirspace& t ) const;
+  bool operator()( const AbstractAirspace& t ) const {
+    return check_height(t);
+  }
+protected:
+  bool check_height(const AbstractAirspace& t) const;
 private:
   const short h_min;
   const short h_max;
+};
+
+/**
+ * Convenience predicate for height within a specified range, excluding
+ * airspaces enclosing two points
+ */
+class AirspacePredicateHeightRangeExcludeTwo: public AirspacePredicateHeightRange
+{
+public:
+  /**
+   * Constructor
+   *
+   * @param _h_min Lower bound on airspace (m)
+   * @param _h_max Upper bound on airspace (m)
+   *
+   * @return Initialised object
+   */
+  AirspacePredicateHeightRangeExcludeTwo(const short _h_min, const short _h_max,
+                                         const AGeoPoint& _p1, const AGeoPoint& _p2):
+    AirspacePredicateHeightRange(_h_min, _h_max), p1(_p1), p2(_p2) {};
+
+  bool operator()( const AbstractAirspace& t ) const;
+private:
+  const AGeoPoint p1;
+  const AGeoPoint p2;
 };
 
 #endif
