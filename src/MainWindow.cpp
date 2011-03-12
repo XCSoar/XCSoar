e48ec56c379e38e017f6be8073d7f12714e3a500
Max Kellermann <max@duempel.org>
2011-03-12 23:33:30 +0100

    MainWindow: don't check SingleWindow::on_close()
    There is nothing useful here, and on SDL, it may invoke the default
    implementation (destroy the Window).

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -236,9 +236,6 @@ bool MainWindow::on_destroy(void) {
 }
 
 bool MainWindow::on_close() {
-  if (SingleWindow::on_close())
-    return true;
-
   if (XCSoarInterface::CheckShutdown()) {
     XCSoarInterface::Shutdown();
   }
