8ddea5873b220206bff72d6b1a0a3bf1e892c0f5
Simon Taylor <simon.taylor.uk@gmail.com>
2011-03-27 07:34:17 +0100

    Build/Manual: Adds figures directory (SVG files for conversion to PDF)
    Adds the doc/manual/shared/figures directory and modifies the makefile
    to convert all SVG files in this directory to PDF at compile time using
    rsvg-convert.

--- build/manual.mk
+++ build/manual.mk
@@ -17,6 +17,9 @@ SVG_ICON_LIST = \
 	map_turnpoint
 SVG_ICONS = $(patsubst %,$(GENERATED_DIR)/icons/%.png,$(SVG_ICON_LIST))
 
+SVG_FIGURES_SHARED = $(wildcard $(DOC)/manual/shared/figures/*.svg)
+SVG_FIGURES = $(patsubst $(DOC)/manual/shared/figures/%.svg,$(GENERATED_DIR)/figures/%.pdf,$(SVG_FIGURES_SHARED))
+
 TEX_INCLUDES_DE =  $(wildcard $(DOC)/manual/*.sty) $(wildcard $(DOC)/manual/de/*.sty)
 FIGURES_DE = $(DOC)/manual/de/Bilder/*.png
  
@@ -30,22 +33,26 @@ manual: \
 
 $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(DOC)/manual/en/XCSoar-manual.tex \
 	$(TEX_FILES_EN) $(TEX_INCLUDES_EN) \
-	$(FIGURES_EN) $(SVG_ICONS) | $(MANUAL_OUTPUT_DIR)/dirstamp
+	$(FIGURES_EN) $(SVG_ICONS) $(SVG_FIGURES) | $(MANUAL_OUTPUT_DIR)/dirstamp
 	# run TeX twice to make sure that all references are resolved
 	cd $(<D) && pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D)) $(<F)
 	cd $(<D) && pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D)) $(<F)
 
 $(MANUAL_OUTPUT_DIR)/XCSoar-developer-manual.pdf: $(DOC)/manual/en/XCSoar-developer-manual.tex $(TEX_INCLUDES_EN) \
-	$(FIGURES_EN) $(SVG_ICONS) | $(MANUAL_OUTPUT_DIR)/dirstamp
+	$(FIGURES_EN) $(SVG_ICONS) $(SVG_FIGURES) | $(MANUAL_OUTPUT_DIR)/dirstamp
 	# run TeX twice to make sure that all references are resolved
 	cd $(<D) && pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D)) $(<F)
 	cd $(<D) && pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D)) $(<F)
 
 $(MANUAL_OUTPUT_DIR)/XCSoar-Handbuch.pdf: $(DOC)/manual/de/XCSoar-Handbuch.tex $(DOC)/manual/de/Blitzeinstieg.tex $(TEX_INCLUDES_DE) \
-	$(FIGURES_DE) $(SVG_ICONS) | $(MANUAL_OUTPUT_DIR)/dirstamp
+	$(FIGURES_DE) $(SVG_ICONS) $(SVG_FIGURES) | $(MANUAL_OUTPUT_DIR)/dirstamp
 	# run TeX twice to make sure that all references are resolved
 	cd $(<D) && pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D)) $(<F)
 	cd $(<D) && pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D)) $(<F)
 
 $(SVG_ICONS): $(GENERATED_DIR)/icons/%.png: $(topdir)/Data/icons/%.svg | $(GENERATED_DIR)/icons/dirstamp
 	rsvg-convert -a -w 32 $< -o $@
+
+$(SVG_FIGURES): $(GENERATED_DIR)/figures/%.pdf: $(topdir)/doc/manual/shared/figures/%.svg | $(GENERATED_DIR)/figures/dirstamp
+	rsvg-convert -a -f pdf $< -o $@
+
