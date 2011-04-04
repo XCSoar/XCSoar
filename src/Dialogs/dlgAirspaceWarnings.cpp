4117423823c1900025556a83289a479a12786b96
Max Kellermann <max@duempel.org>
2011-04-04 20:43:12 +0200

    dlgAirspaceWarnings: "Enable" reactivates "inside" warnings

--- src/Dialogs/dlgAirspaceWarnings.cpp
+++ src/Dialogs/dlgAirspaceWarnings.cpp
@@ -151,6 +151,7 @@ OnEnableClicked(WndButton &Sender)
 
   const AbstractAirspace *airspace = GetSelectedAirspace();
   if (airspace != NULL) {
+    airspace_warnings->acknowledge_inside(*airspace, false);
     airspace_warnings->acknowledge_warning(*airspace, false);
     airspace_warnings->acknowledge_day(*airspace, false);
     wAirspaceList->invalidate();
