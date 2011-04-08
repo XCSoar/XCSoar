f1e5c3d94ad41ee070602afe8db5726ad26fb9d6
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-04-08 22:16:56 +0200

    TaskFileSeeYou: Simplified Count() function

--- src/Task/TaskFileSeeYou.cpp
+++ src/Task/TaskFileSeeYou.cpp
@@ -511,7 +511,7 @@ TaskFileSeeYou::Count()
   unsigned count = 0;
   bool in_task_section = false;
   TCHAR *line;
-  for (unsigned i = 0; (line = reader.read()) != NULL; i++) {
+  while ((line = reader.read()) != NULL) {
     if (in_task_section) {
       if (line[0] == _T('\"') || line[0] == _T(',')) {
 
