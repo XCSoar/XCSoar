4f4b551599d52d04eb9d6269efb045e990a2ac0e
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-04-13 15:09:20 +0200

    Driver/FlyTec: Added Bräuniger devices to the driver comment

--- src/Device/Driver/Flytec.hpp
+++ src/Device/Driver/Flytec.hpp
@@ -25,9 +25,10 @@ Copyright_License {
 #define XCSOAR_DEVICE_DRIVER_FLYTEC_HPP
 
 /**
- * A driver for Flytec 5030.
+ * A driver for Brauniger devices and the Flytec 5030.
  *
  * @see http://www.andrewvanis.com/HangGliding/NMEA%20Output%205030.pdf
+ * @see http://www.brauniger.com/deutsch/service/faq.html?id_section=4
  */
 extern const struct DeviceRegister flytec_device_driver;
 
