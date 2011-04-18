2659f3033da32ef5817aeacc4b57e8e4b9709205
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-04-18 14:27:21 +0200

    DeviceBlackboard: Don't calculate unnecessary values if no targets are in reach

--- src/DeviceBlackboard.cpp
+++ src/DeviceBlackboard.cpp
@@ -256,7 +256,7 @@ DeviceBlackboard::ProcessFLARM()
   const FLARM_STATE &last_flarm = LastBasic().flarm;
 
   // if (FLARM data is available)
-  if (!flarm.available)
+  if (!flarm.available || flarm.traffic.empty())
     return;
 
   // calculate relative east and north projection to lat/lon
