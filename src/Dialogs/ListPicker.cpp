7081d8b989d71d959bb76df9f4a0a2ea53d42944
Helmut J. Rohs <helmut.j.rohs@web.de>
2011-04-26 01:00:07 +0200

    Removed help text and button from item picker dialog in favor of the item help, increased item help widget seperation.

--- src/Dialogs/ListPicker.cpp
+++ src/Dialogs/ListPicker.cpp
@@ -131,10 +131,11 @@ ListPicker(SingleWindow &parent, const TCHAR *caption,
     wItemHelp->set_visible(true);
     const unsigned help_height = wItemHelp->get_height();
     const PixelRect rc = list_control->get_position();
-    assert(rc.bottom - rc.top - help_height - 2 > 0);
-    list_control->move(rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top - help_height - 2);
+    assert(rc.bottom - rc.top - help_height - 10 > 0);
+    list_control->move(rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top - help_height - 10);
     list_control->SetCursorCallback(OnPointCursorCallback);
     OnPointCursorCallback(initial_value);
+    help_callback = NULL;
   }
   else
     wItemHelp = NULL;
