ac216c1d1c45f8ceab12056cba8c2b27d29af6c4
Max Kellermann <max@duempel.org>
2011-05-10 21:58:13 +0200

    Makefile: remove obsolete "cloc" call

--- build/doxygen.mk
+++ build/doxygen.mk
@@ -2,7 +2,6 @@ DOXYGEN_OUTPUT_DIR = $(OUT)/doc
 
 .PHONY: doco
 doco: FORCE
-#	$(topdir)/ex/tools/cloc --exclude-lang=D src > doc/cloc.txt
 	rm -rf $(DOXYGEN_OUTPUT_DIR)
 	mkdir -p $(DOXYGEN_OUTPUT_DIR)
 	cd $(DOC) && doxygen XCSoar.doxyfile
