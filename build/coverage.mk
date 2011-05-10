b11ca33a237e281a601d503fdaf44d3835779547
Max Kellermann <max@duempel.org>
2011-05-10 21:47:35 +0200

    Makefile: store lcov data files in $(TARGET_OUTPUT_DIR)

--- build/coverage.mk
+++ build/coverage.mk
@@ -6,24 +6,28 @@ FLAGS_COVERAGE :=
 COVERAGE_LDLIBS :=
 endif
 
+COVERAGE_DIR = $(TARGET_OUTPUT_DIR)/coverage
+COVERAGE_DATA_DIR = $(COVERAGE_DIR)/data
+COVERAGE_HTML_DIR = $(COVERAGE_DIR)/html
+
 # uses lcov
 #COVSTART = geninfo -q --no-checksum -b $(topdir) -i $(topdir)/src
 #COVEND = geninfo -q --no-checksum -b $(topdir) $(topdir)/src
 
 LCOVOPTS = --no-checksum -q -b $(topdir) -d $(TARGET_OUTPUT_DIR)/src
 
-COVSTART = lcov -c -i $(LCOVOPTS) -o app_base.info
-COVEND = lcov -c $(LCOVOPTS) -o app_test.info
-COVMERGE = lcov -q -a app_base.info -a app_test.info -o app_total.info
-COVCLEAN = lcov -q -r app_total.info "/usr/*" -o app_totalr.info
-COVPROC = genhtml --frames -q -o $(DOC)/lcov app_totalr.info
+COVSTART = lcov -c -i $(LCOVOPTS) -o $(COVERAGE_DATA_DIR)/app_base.info
+COVEND = lcov -c $(LCOVOPTS) -o $(COVERAGE_DATA_DIR)/app_test.info
+COVMERGE = lcov -q -a $(COVERAGE_DATA_DIR)/app_base.info -a $(COVERAGE_DATA_DIR)/app_test.info -o $(COVERAGE_DATA_DIR)/app_total.info
+COVCLEAN = lcov -q -r $(COVERAGE_DATA_DIR)/app_total.info "/usr/*" -o $(COVERAGE_DATA_DIR)/app_totalr.info
+COVPROC = genhtml --frames -q -o $(COVERAGE_HTML_DIR) $(COVERAGE_DATA_DIR)/app_totalr.info
 
-covstart:	FORCE
+covstart: FORCE | $(COVERAGE_DATA_DIR)/dirstamp
 	@$(NQ)echo "coverage initialise"
 	echo $(COVSTART)
 	@$(Q)$(COVSTART)
 
-covend:	FORCE
+covend:	FORCE | $(COVERAGE_DATA_DIR)/dirstamp $(COVERAGE_HTML_DIR)/dirstamp
 	@$(NQ)echo "coverage report"
 	echo $(COVEND)
 	echo $(COVPROC)
