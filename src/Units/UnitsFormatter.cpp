d4e64736e2fa0bb90364b359678fe05dcb105c40
Helmut J. Rohs <helmut.j.rohs@web.de>
2011-06-18 13:31:51 +0200

    Gui text, space inbetween figure and unit; i18n on button caption.

--- src/Units/UnitsFormatter.cpp
+++ src/Units/UnitsFormatter.cpp
@@ -175,7 +175,7 @@ Units::FormatUserAltitude(fixed Altitude, TCHAR *Buffer, size_t size,
   Altitude = Altitude * pU->ToUserFact; // + pU->ToUserOffset;
 
   if (IncludeUnit)
-    _stprintf(sTmp, _T("%d%s"), iround(Altitude), pU->Name);
+    _stprintf(sTmp, _T("%d %s"), iround(Altitude), pU->Name);
   else
     _stprintf(sTmp, _T("%d"), iround(Altitude));
 
@@ -220,7 +220,7 @@ Units::FormatUserArrival(fixed Altitude, TCHAR *Buffer, size_t size,
   Altitude = Altitude * pU->ToUserFact; // + pU->ToUserOffset;
 
   if (IncludeUnit)
-    _stprintf(sTmp, _T("%+d%s"), iround(Altitude), pU->Name);
+    _stprintf(sTmp, _T("%+d %s"), iround(Altitude), pU->Name);
   else
     _stprintf(sTmp, _T("%+d"), iround(Altitude));
 
