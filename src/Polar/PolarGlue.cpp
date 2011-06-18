5e9128ef008d226eeb0a16f987fae3dee20c5f6f
Scott Penrose <scottp@dd.com.au>
2011-06-17 19:01:58 -0700

    PolarGlue: Display Polar message box only if polar was invalid.
    The PolarGlue will show a Message Box showing the polar is wrong
    even if you don't have a polar set. This means the first thing a new
    user sees is an error "Polar has invalid coefficients".
    This now checks the profile, and if it is empty, does not show the message
    but still resets the polar. If the polar is wrong, it still shows the message.

--- src/Polar/PolarGlue.cpp
+++ src/Polar/PolarGlue.cpp
@@ -116,8 +116,10 @@ PolarGlue::LoadFromProfile(GlidePolar &gp, SETTINGS_POLAR &settings)
 {
   PolarInfo polar;
   if (!LoadFromProfile(polar) || !polar.CopyIntoGlidePolar(gp)) {
-    MessageBoxX(_("Polar has invalid coefficients.\nUsing LS8 polar instead!"),
-                _("Warning"), MB_OK);
+    unsigned Temp;
+    if (Profile::Get(szProfilePolarID, Temp))
+      MessageBoxX(_("Polar has invalid coefficients.\nUsing LS8 polar instead!"),
+                  _("Warning"), MB_OK);
     LoadDefault(polar);
     polar.CopyIntoGlidePolar(gp);
   }
