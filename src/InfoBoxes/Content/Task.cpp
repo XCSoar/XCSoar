061053e46411927d71d5c8e7268503d366b222bf
Andreas Pfaller <pfaller@gmail.com>
2011-06-21 13:45:08 +0200

    InfoBoxes/Content/Task: Display frequency in infobox comment area if it exists.
    Frequency is an important information when a landing on a foreign
    airfield is imminent due to height. Having to search for this information
    in a paper map or trying to look it up in the WP detail dialog during
    a high workload situation is potentially dangerous. Showing it in the
    next WP infobox makes the frequency easily available for use.

--- src/InfoBoxes/Content/Task.cpp
+++ src/InfoBoxes/Content/Task.cpp
@@ -90,7 +90,15 @@ InfoBoxContentNextWaypoint::Update(InfoBoxWindow &infobox)
   SetTitleFromWaypointName(infobox, way_point);
 
   // Set Comment
-  infobox.SetComment(way_point->Comment.c_str());
+  if (way_point->radio_frequency.IsDefined()) {
+    StaticString<128> comment;
+    const unsigned freq = way_point->radio_frequency.GetKiloHertz();
+    _sntprintf(comment.buffer(), comment.MAX_SIZE, _T("%u.%03u %s"),
+               freq / 1000, freq % 1000, way_point->Comment.c_str());
+    infobox.SetComment(comment);
+  }
+  else
+    infobox.SetComment(way_point->Comment.c_str());
 
   const GlideResult &solution_remaining =
     XCSoarInterface::Calculated().task_stats.current_leg.solution_remaining;
