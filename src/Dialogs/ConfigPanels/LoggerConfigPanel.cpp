f61986d35b21276a7aaaf99fdc7fb0ff674af237
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-07-06 11:46:27 +0200

    LoggerConfigPanel: Used assert() function instead of conditions

--- src/Dialogs/ConfigPanels/LoggerConfigPanel.cpp
+++ src/Dialogs/ConfigPanels/LoggerConfigPanel.cpp
@@ -49,46 +49,41 @@ UpdateButtons(void)
 {
   TCHAR text[120];
   TCHAR val[100];
-  if (buttonPilotName) {
-    Profile::Get(szProfilePilotName, val, 100);
-    if (string_is_empty(val))
-      _tcscpy(val, _("(blank)"));
 
-    _stprintf(text, _T("%s: %s"), _("Pilot name"), val);
-    buttonPilotName->SetCaption(text);
-  }
-  if (buttonAircraftType) {
-    Profile::Get(szProfileAircraftType, val, 100);
-    if (string_is_empty(val))
-      _tcscpy(val, _("(blank)"));
+  Profile::Get(szProfilePilotName, val, 100);
+  if (string_is_empty(val))
+    _tcscpy(val, _("(blank)"));
 
-    _stprintf(text, _T("%s: %s"), _("Aircraft type"), val);
-    buttonAircraftType->SetCaption(text);
-  }
-  if (buttonAircraftReg) {
-    Profile::Get(szProfileAircraftReg, val, 100);
-    if (string_is_empty(val))
-      _tcscpy(val, _("(blank)"));
+  _stprintf(text, _T("%s: %s"), _("Pilot name"), val);
+  buttonPilotName->SetCaption(text);
 
-    _stprintf(text, _T("%s: %s"), _("Aircraft reg."), val);
-    buttonAircraftReg->SetCaption(text);
-  }
-  if (buttonCompetitionId) {
-    Profile::Get(szProfileCompetitionId, val, 100);
-    if (string_is_empty(val))
-      _tcscpy(val, _("(blank)"));
+  Profile::Get(szProfileAircraftType, val, 100);
+  if (string_is_empty(val))
+    _tcscpy(val, _("(blank)"));
 
-    _stprintf(text, _T("%s: %s"), _("Competition ID"), val);
-    buttonCompetitionId->SetCaption(text);
-  }
-  if (buttonLoggerID) {
-    Profile::Get(szProfileLoggerID, val, 100);
-    if (string_is_empty(val))
-      _tcscpy(val, _("(blank)"));
+  _stprintf(text, _T("%s: %s"), _("Aircraft type"), val);
+  buttonAircraftType->SetCaption(text);
 
-    _stprintf(text, _T("%s: %s"), _("Logger ID"), val);
-    buttonLoggerID->SetCaption(text);
-  }
+  Profile::Get(szProfileAircraftReg, val, 100);
+  if (string_is_empty(val))
+    _tcscpy(val, _("(blank)"));
+
+  _stprintf(text, _T("%s: %s"), _("Aircraft reg."), val);
+  buttonAircraftReg->SetCaption(text);
+
+  Profile::Get(szProfileCompetitionId, val, 100);
+  if (string_is_empty(val))
+    _tcscpy(val, _("(blank)"));
+
+  _stprintf(text, _T("%s: %s"), _("Competition ID"), val);
+  buttonCompetitionId->SetCaption(text);
+
+  Profile::Get(szProfileLoggerID, val, 100);
+  if (string_is_empty(val))
+    _tcscpy(val, _("(blank)"));
+
+  _stprintf(text, _T("%s: %s"), _("Logger ID"), val);
+  buttonLoggerID->SetCaption(text);
 }
 
 
@@ -96,13 +91,11 @@ static void
 OnCompetitionIdClicked(gcc_unused WndButton &button)
 {
   TCHAR Temp[100];
-  if (buttonCompetitionId) {
-    Profile::Get(szProfileCompetitionId, Temp, 100);
-    if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                              Temp, 100)) {
-      Profile::Set(szProfileCompetitionId, Temp);
-      changed = true;
-    }
+  Profile::Get(szProfileCompetitionId, Temp, 100);
+  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
+                            Temp, 100)) {
+    Profile::Set(szProfileCompetitionId, Temp);
+    changed = true;
   }
   UpdateButtons();
 }
@@ -111,13 +104,11 @@ static void
 OnAircraftTypeClicked(gcc_unused WndButton &button)
 {
   TCHAR Temp[100];
-  if (buttonAircraftType) {
-    Profile::Get(szProfileAircraftType, Temp, 100);
-    if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                              Temp, 100)) {
-      Profile::Set(szProfileAircraftType, Temp);
-      changed = true;
-    }
+  Profile::Get(szProfileAircraftType, Temp, 100);
+  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
+                            Temp, 100)) {
+    Profile::Set(szProfileAircraftType, Temp);
+    changed = true;
   }
   UpdateButtons();
 }
@@ -126,13 +117,11 @@ static void
 OnAircraftRegClicked(gcc_unused WndButton &button)
 {
   TCHAR Temp[100];
-  if (buttonAircraftReg) {
-    Profile::Get(szProfileAircraftReg, Temp, 100);
-    if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                              Temp, 100)) {
-      Profile::Set(szProfileAircraftReg, Temp);
-      changed = true;
-    }
+  Profile::Get(szProfileAircraftReg, Temp, 100);
+  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
+                            Temp, 100)) {
+    Profile::Set(szProfileAircraftReg, Temp);
+    changed = true;
   }
   UpdateButtons();
 }
@@ -141,13 +130,11 @@ static void
 OnPilotNameClicked(gcc_unused WndButton &button)
 {
   TCHAR Temp[100];
-  if (buttonPilotName) {
-    Profile::Get(szProfilePilotName, Temp, 100);
-    if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                              Temp, 100)) {
-      Profile::Set(szProfilePilotName, Temp);
-      changed = true;
-    }
+  Profile::Get(szProfilePilotName, Temp, 100);
+  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
+                            Temp, 100)) {
+    Profile::Set(szProfilePilotName, Temp);
+    changed = true;
   }
   UpdateButtons();
 }
@@ -156,15 +143,13 @@ static void
 OnLoggerIDClicked(gcc_unused WndButton &button)
 {
   TCHAR Temp[100];
-  if (buttonLoggerID) {
-    Profile::Get(szProfileLoggerID, Temp, 100);
-    if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                              Temp, 100)) {
-      Profile::Set(szProfileLoggerID, Temp);
-      changed = true;
-    }
-    ReadAssetNumber();
+  Profile::Get(szProfileLoggerID, Temp, 100);
+  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
+                            Temp, 100)) {
+    Profile::Set(szProfileLoggerID, Temp);
+    changed = true;
   }
+  ReadAssetNumber();
   UpdateButtons();
 }
 
@@ -177,24 +162,24 @@ LoggerConfigPanel::Init(WndForm *_wf)
   changed = false;
 
   buttonPilotName = ((WndButton *)wf->FindByName(_T("cmdPilotName")));
-  if (buttonPilotName)
-    buttonPilotName->SetOnClickNotify(OnPilotNameClicked);
+  assert(buttonPilotName != NULL);
+  buttonPilotName->SetOnClickNotify(OnPilotNameClicked);
 
   buttonAircraftType = ((WndButton *)wf->FindByName(_T("cmdAircraftType")));
-  if (buttonAircraftType)
-    buttonAircraftType->SetOnClickNotify(OnAircraftTypeClicked);
+  assert(buttonAircraftType != NULL);
+  buttonAircraftType->SetOnClickNotify(OnAircraftTypeClicked);
 
   buttonAircraftReg = ((WndButton *)wf->FindByName(_T("cmdAircraftReg")));
-  if (buttonAircraftReg)
-    buttonAircraftReg->SetOnClickNotify(OnAircraftRegClicked);
+  assert(buttonAircraftReg != NULL);
+  buttonAircraftReg->SetOnClickNotify(OnAircraftRegClicked);
 
   buttonCompetitionId = ((WndButton *)wf->FindByName(_T("cmdCompetitionId")));
-  if (buttonCompetitionId)
-    buttonCompetitionId->SetOnClickNotify(OnCompetitionIdClicked);
+  assert(buttonCompetitionId != NULL);
+  buttonCompetitionId->SetOnClickNotify(OnCompetitionIdClicked);
 
   buttonLoggerID = ((WndButton *)wf->FindByName(_T("cmdLoggerID")));
-  if (buttonLoggerID)
-    buttonLoggerID->SetOnClickNotify(OnLoggerIDClicked);
+  assert(buttonLoggerID != NULL);
+  buttonLoggerID->SetOnClickNotify(OnLoggerIDClicked);
 
   UpdateButtons();
 
