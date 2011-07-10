f2b6e8da09c87b3bf1c25207142334efb9d4f8c3
Max Kellermann <max@duempel.org>
2011-07-10 19:08:26 +0200

    Device/SerialPort: wait for cancelled I/O to complete [PC]
    This fixes another OVERLAPPED bug, one which led to program crashes.
    Problem: the OVERLAPPED object was allocated on the stack, and WIN32
    kept using it even though XCSoar had already left that stack frame.
    Cause: after CancelIo(), you need to wait for I/O completion;
    CancelIo() just cancels the operation, but does not finish it.

--- src/Device/SerialPort.cpp
+++ src/Device/SerialPort.cpp
@@ -206,6 +206,8 @@ SerialPort::WaitDataPending(OverlappedEvent &overlapped,
     if (overlapped.Wait(timeout_ms) != OverlappedEvent::FINISHED) {
       /* the operation may still be running, we have to cancel it */
       ::CancelIo(hPort);
+      ::SetCommMask(hPort, 0);
+      overlapped.Wait();
       return -1;
     }
 
@@ -269,6 +271,8 @@ SerialPort::Run()
 
       if (osReader.Wait() != OverlappedEvent::FINISHED) {
         ::CancelIo(hPort);
+        ::SetCommMask(hPort, 0);
+        osReader.Wait();
         continue;
       }
 
@@ -384,6 +388,8 @@ SerialPort::Write(const void *data, size_t length)
 
   default:
     ::CancelIo(hPort);
+    ::SetCommMask(hPort, 0);
+    osWriter.Wait();
     return 0;
   }
 #endif
