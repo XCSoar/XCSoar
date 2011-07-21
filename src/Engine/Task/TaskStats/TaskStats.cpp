75ff06b6f3808a9dce91ec1dc02eb3576d4c21b7
Max Kellermann <max@duempel.org>
2011-07-21 07:37:30 +0200

    TaskStats: check GlideResult::defined() before accessing it

--- src/Engine/Task/TaskStats/TaskStats.cpp
+++ src/Engine/Task/TaskStats/TaskStats.cpp
@@ -50,7 +50,7 @@ TaskStats::calc_flight_mode()
 {
   const int margin = (flight_mode_final_glide? 1:0)*flight_mode_height_margin;
 
-  const bool this_is_final = 
+  const bool this_is_final = total.solution_remaining.defined() &&
     positive(total.solution_remaining.AltitudeDifference + fixed(margin));
 
   const bool changed = (flight_mode_final_glide != this_is_final);
