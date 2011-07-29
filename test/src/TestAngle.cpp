3d964e097f30ef0f88f28af899034a15ef4e8b7a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-07-29 15:04:48 +0200

    TestAngle: Added Documentation

--- test/src/TestAngle.cpp
+++ test/src/TestAngle.cpp
@@ -29,61 +29,68 @@ int main(int argc, char **argv)
 {
   plan_tests(38);
 
+  // Test magnitude_degrees()
   ok1(equals(Angle::degrees(fixed_90).magnitude_degrees(), 90));
   ok1(equals(Angle::degrees(-fixed_90).magnitude_degrees(), 90));
 
+  // Test Reciprocal()
   ok1(equals(Angle::degrees(fixed_90).Reciprocal(), 270));
   ok1(equals(Angle::degrees(fixed_270).Reciprocal(), 90));
 
+  // Test as_bearing()
   ok1(equals(Angle::degrees(fixed(361)).as_bearing(), 1));
   ok1(equals(Angle::degrees(fixed(180)).as_bearing(), 180));
   ok1(equals(Angle::degrees(fixed(-180)).as_bearing(), 180));
   ok1(equals(Angle::degrees(fixed(-270)).as_bearing(), 90));
 
+  // Test as_delta()
   ok1(equals(Angle::degrees(fixed_90).as_delta(), 90));
   ok1(equals(Angle::degrees(fixed(179)).as_delta(), 179));
   ok1(equals(Angle::degrees(fixed(-179)).as_delta(), -179));
   ok1(equals(Angle::degrees(fixed_270).as_delta(), -90));
 
+  // Test between()
   ok1(Angle::degrees(fixed_90).between(Angle::degrees(fixed_zero),
-                                          Angle::degrees(fixed_180)));
+                                       Angle::degrees(fixed_180)));
 
   ok1(!Angle::degrees(fixed_90).between(Angle::degrees(fixed_180),
-                                           Angle::degrees(fixed_zero)));
+                                        Angle::degrees(fixed_zero)));
 
   ok1(Angle::degrees(fixed_zero).between(Angle::degrees(fixed_270),
-                                            Angle::degrees(fixed_90)));
+                                         Angle::degrees(fixed_90)));
 
+  // Test sin()
   ok1(equals(Angle::degrees(fixed_zero).sin(), 0));
   ok1(equals(Angle::degrees(fixed_90).sin(), 1));
   ok1(equals(Angle::degrees(fixed_180).sin(), 0));
   ok1(equals(Angle::degrees(fixed_270).sin(), -1));
   ok1(equals(Angle::degrees(fixed_360).sin(), 0));
 
+  // Test cos()
   ok1(equals(Angle::degrees(fixed_zero).cos(), 1));
   ok1(equals(Angle::degrees(fixed_90).cos(), 0));
   ok1(equals(Angle::degrees(fixed_180).cos(), -1));
   ok1(equals(Angle::degrees(fixed_270).cos(), 0));
   ok1(equals(Angle::degrees(fixed_360).cos(), 1));
 
+  // Test fastsine()
   ok1(equals(Angle::degrees(fixed_zero).fastsine(), 0));
   ok1(equals(Angle::degrees(fixed_90).fastsine(), 1));
   ok1(equals(Angle::degrees(fixed_180).fastsine(), 0));
   ok1(equals(Angle::degrees(fixed_270).fastsine(), -1));
   ok1(equals(Angle::degrees(fixed_360).fastsine(), 0));
 
+  // Test fastcosine()
   ok1(equals(Angle::degrees(fixed_zero).fastcosine(), 1));
   ok1(equals(Angle::degrees(fixed_90).fastcosine(), 0));
   ok1(equals(Angle::degrees(fixed_180).fastcosine(), -1));
   ok1(equals(Angle::degrees(fixed_270).fastcosine(), 0));
   ok1(equals(Angle::degrees(fixed_360).fastcosine(), 1));
 
-  ok1(equals(Angle::degrees(fixed_90).BiSector(Angle::degrees(fixed_180)),
-                45));
-  ok1(equals(Angle::degrees(fixed_270).BiSector(Angle::degrees(fixed_zero)),
-                225));
-  ok1(equals(Angle::degrees(fixed_270).BiSector(Angle::degrees(fixed_180)),
-                315));
+  // Test BiSector()
+  ok1(equals(Angle::degrees(fixed_90).BiSector(Angle::degrees(fixed_180)), 45));
+  ok1(equals(Angle::degrees(fixed_270).BiSector(Angle::degrees(fixed_zero)), 225));
+  ok1(equals(Angle::degrees(fixed_270).BiSector(Angle::degrees(fixed_180)), 315));
 
   return exit_status();
 }
