57700402812963fc6f0b66b5f682ea18c1c79db7
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-08-07 19:24:18 +0200

    WindStore: Fixed Validity::Update() call
    This fixes ticket #1397 (broken wind calculation)

--- src/Wind/WindStore.cpp
+++ src/Wind/WindStore.cpp
@@ -124,7 +124,7 @@ WindStore::NewWind(const NMEAInfo &info, DerivedInfo &derived,
 
   if (mag < fixed(30)) { // limit to reasonable values
     derived.estimated_wind = SpeedVector(bearing.as_bearing(), mag);
-    derived.estimated_wind_available.Update(info.time);
+    derived.estimated_wind_available.Update(info.clock);
   } else {
     // TODO code: give warning, wind estimate bogus or very strong!
   }
