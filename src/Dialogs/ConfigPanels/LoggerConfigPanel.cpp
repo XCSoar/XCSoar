4406e350a73ded5dcb3ccf2438086a2a3116e17e
Max Kellermann <max@duempel.org>
2011-08-07 15:01:18 +0200

    dlgConfiguration: use DataFieldString on the logger page
    .. instead of having a push button with the value in the caption,
    which seems unnatural.

--- src/Dialogs/ConfigPanels/LoggerConfigPanel.cpp
+++ src/Dialogs/ConfigPanels/LoggerConfigPanel.cpp
@@ -21,186 +21,35 @@ Copyright_License {
 }
 */
 
-#include "DataField/Enum.hpp"
-#include "DataField/ComboList.hpp"
-#include "DataField/Float.hpp"
-#include "Form/Edit.hpp"
-#include "Form/Util.hpp"
-#include "Form/Frame.hpp"
-#include "Dialogs/TextEntry.hpp"
-#include "Profile/ProfileKeys.hpp"
-#include "Profile/Profile.hpp"
-#include "Interface.hpp"
-#include "Asset.hpp"
-#include "StringUtil.hpp"
 #include "LoggerConfigPanel.hpp"
-#include "Language/Language.hpp"
+#include "Form/Util.hpp"
+#include "Profile/ProfileKeys.hpp"
+#include "Interface.hpp"
 #include "Plane/PlaneGlue.hpp"
 
 static WndForm* wf = NULL;
-static WndButton *buttonPilotName = NULL;
-static WndButton *buttonAircraftType = NULL;
-static WndButton *buttonAircraftReg = NULL;
-static WndButton *buttonCompetitionId = NULL;
-static WndButton *buttonLoggerID = NULL;
-static bool changed = false;
-
-static void
-UpdateButtons(void)
-{
-  TCHAR text[120];
-  StaticString<100> val;
-
-  const Plane &plane = CommonInterface::SettingsComputer().plane;
-
-  if (!Profile::Get(szProfilePilotName, val) || val.empty())
-    val = _("(blank)");
-
-  _stprintf(text, _T("%s: %s"), _("Pilot name"), val.c_str());
-  buttonPilotName->SetCaption(text);
-
-  val = plane.type;
-  if (val.empty())
-    val = _("(blank)");
-
-  _stprintf(text, _T("%s: %s"), _("Aircraft type"), val.c_str());
-  buttonAircraftType->SetCaption(text);
-
-  val = plane.registration;
-  if (val.empty())
-    val = _("(blank)");
-
-  _stprintf(text, _T("%s: %s"), _("Aircraft reg."), val.c_str());
-  buttonAircraftReg->SetCaption(text);
-
-  val = plane.competition_id;
-  if (val.empty())
-    val = _("(blank)");
-
-  _stprintf(text, _T("%s: %s"), _("Competition ID"), val.c_str());
-  buttonCompetitionId->SetCaption(text);
-
-  if (!Profile::Get(szProfileLoggerID, val) || val.empty())
-    val = _("(blank)");
-
-  _stprintf(text, _T("%s: %s"), _("Logger ID"), val.c_str());
-  buttonLoggerID->SetCaption(text);
-}
-
-
-static void
-OnCompetitionIdClicked(gcc_unused WndButton &button)
-{
-  Plane &plane = CommonInterface::SetSettingsComputer().plane;
-
-  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                            plane.competition_id.buffer(),
-                            plane.competition_id.MAX_SIZE,
-                            _("Competition ID")))
-    changed = true;
-
-  UpdateButtons();
-}
-
-static void
-OnAircraftTypeClicked(gcc_unused WndButton &button)
-{
-  Plane &plane = CommonInterface::SetSettingsComputer().plane;
-
-  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                            plane.type.buffer(),
-                            plane.type.MAX_SIZE,
-                            _("Aircraft type")))
-    changed = true;
-
-  UpdateButtons();
-}
-
-static void
-OnAircraftRegClicked(gcc_unused WndButton &button)
-{
-  Plane &plane = CommonInterface::SetSettingsComputer().plane;
-
-  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                            plane.registration.buffer(),
-                            plane.registration.MAX_SIZE,
-                            _("Aircraft reg.")))
-    changed = true;
-
-  UpdateButtons();
-}
-
-static void
-OnPilotNameClicked(gcc_unused WndButton &button)
-{
-  StaticString<100> tmp;
-  if (!Profile::Get(szProfilePilotName, tmp))
-    tmp.clear();
-
-  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                            tmp.buffer(), tmp.MAX_SIZE,
-                            _("Pilot name"))) {
-    Profile::Set(szProfilePilotName, tmp);
-    changed = true;
-  }
-  UpdateButtons();
-}
-
-static void
-OnLoggerIDClicked(gcc_unused WndButton &button)
-{
-  StaticString<100> tmp;
-  if (!Profile::Get(szProfileLoggerID, tmp))
-    tmp.clear();
-
-  if (dlgTextEntryShowModal(*(SingleWindow *)button.get_root_owner(),
-                            tmp.buffer(), tmp.MAX_SIZE,
-                            _("Logger ID"))) {
-    Profile::Set(szProfileLoggerID, tmp);
-    changed = true;
-  }
-  ReadAssetNumber();
-  UpdateButtons();
-}
-
 
 void
 LoggerConfigPanel::Init(WndForm *_wf)
 {
   assert(_wf != NULL);
   wf = _wf;
-  changed = false;
-
-  buttonPilotName = ((WndButton *)wf->FindByName(_T("cmdPilotName")));
-  assert(buttonPilotName != NULL);
-  buttonPilotName->SetOnClickNotify(OnPilotNameClicked);
-
-  buttonAircraftType = ((WndButton *)wf->FindByName(_T("cmdAircraftType")));
-  assert(buttonAircraftType != NULL);
-  buttonAircraftType->SetOnClickNotify(OnAircraftTypeClicked);
-
-  buttonAircraftReg = ((WndButton *)wf->FindByName(_T("cmdAircraftReg")));
-  assert(buttonAircraftReg != NULL);
-  buttonAircraftReg->SetOnClickNotify(OnAircraftRegClicked);
-
-  buttonCompetitionId = ((WndButton *)wf->FindByName(_T("cmdCompetitionId")));
-  assert(buttonCompetitionId != NULL);
-  buttonCompetitionId->SetOnClickNotify(OnCompetitionIdClicked);
-
-  buttonLoggerID = ((WndButton *)wf->FindByName(_T("cmdLoggerID")));
-  assert(buttonLoggerID != NULL);
-  buttonLoggerID->SetOnClickNotify(OnLoggerIDClicked);
-
-  UpdateButtons();
 
   const SETTINGS_COMPUTER &settings_computer =
-    XCSoarInterface::SettingsComputer();
+    CommonInterface::SettingsComputer();
+  const Plane &plane = settings_computer.plane;
 
   LoadFormProperty(*wf, _T("prpLoggerTimeStepCruise"),
                    settings_computer.LoggerTimeStepCruise);
 
   LoadFormProperty(*wf, _T("prpLoggerTimeStepCircling"),
                    settings_computer.LoggerTimeStepCircling);
+
+  LoadFormPropertyFromProfile(*wf, _T("PilotName"), szProfilePilotName);
+  LoadFormProperty(*wf, _T("AircraftType"), plane.type);
+  LoadFormProperty(*wf, _T("AircraftReg"), plane.registration);
+  LoadFormProperty(*wf, _T("CompetitionID"), plane.competition_id);
+  LoadFormPropertyFromProfile(*wf, _T("LoggerID"), szProfileLoggerID);
 
   LoadFormProperty(*wf, _T("prpLoggerShortName"),
                    settings_computer.LoggerShortName);
@@ -215,6 +64,9 @@ LoggerConfigPanel::Save()
 {
   SETTINGS_COMPUTER &settings_computer =
     XCSoarInterface::SetSettingsComputer();
+  Plane &plane = settings_computer.plane;
+
+  bool changed = false;
 
   changed |= SaveFormProperty(*wf, _T("prpLoggerTimeStepCruise"),
                               szProfileLoggerTimeStepCruise,
@@ -223,6 +75,13 @@ LoggerConfigPanel::Save()
   changed |= SaveFormProperty(*wf, _T("prpLoggerTimeStepCircling"),
                               szProfileLoggerTimeStepCircling,
                               settings_computer.LoggerTimeStepCircling);
+
+  changed |= SaveFormPropertyToProfile(*wf, _T("PilotName"),
+                                       szProfilePilotName);
+  changed |= SaveFormProperty(*wf, _T("AircraftType"), plane.type);
+  changed |= SaveFormProperty(*wf, _T("AircraftReg"), plane.registration);
+  changed |= SaveFormProperty(*wf, _T("CompetitionID"), plane.competition_id);
+  changed |= SaveFormPropertyToProfile(*wf, _T("LoggerID"), szProfileLoggerID);
 
   changed |= SaveFormProperty(*wf, _T("prpLoggerShortName"),
                               szProfileLoggerShort,
