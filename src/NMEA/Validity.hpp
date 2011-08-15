97f9290a065bc0da2739d617c2715c068e9d5c24
Max Kellermann <max@duempel.org>
2011-08-15 22:57:38 +0200

    NMEA/Validity: use a 32 bit integer instead of 64 bit fixed
    Reduce the memory usage of the Validity type.

--- src/NMEA/Validity.hpp
+++ src/NMEA/Validity.hpp
@@ -26,13 +26,25 @@ Copyright_License {
 
 #include "Math/fixed.hpp"
 
+#include <stdint.h>
+
 /**
  * This keeps track when a value was last changed, to check if it was
  * updated recently or to see if it has expired.  Additionally, it can
  * track if the attribute is not set (timestamp is zero).
  */
 class Validity {
-  fixed last;
+  uint32_t last;
+
+  gcc_pure
+  static uint32_t Import(fixed time) {
+    return (uint32_t)(time * 64);
+  }
+
+  gcc_pure
+  static fixed Export(uint32_t i) {
+    return fixed(i) / 64;
+  }
 
 public:
   /**
@@ -43,14 +55,14 @@ public:
   /**
    * Initialize the object with the specified timestamp.
    */
-  explicit Validity(fixed _last):last(_last) {}
+  explicit Validity(fixed _last):last(Import(_last)) {}
 
 public:
   /**
    * Clears the time stamp, marking the referenced value "invalid".
    */
   void Clear() {
-    last = fixed_zero;
+    last = 0;
   }
 
   /**
@@ -60,7 +72,7 @@ public:
    * @param now the current time stamp in seconds
    */
   void Update(fixed now) {
-    last = now;
+    last = Import(now);
   }
 
   /**
@@ -70,8 +82,11 @@ public:
    * @param max_age the maximum age in seconds
    * @return true if the value is expired
    */
-  bool Expire(fixed now, fixed max_age) {
-    if (positive(last) &&
+  bool Expire(fixed _now, fixed _max_age) {
+    const uint32_t now = Import(_now);
+    const uint32_t max_age = Import(_max_age);
+
+    if (IsValid() &&
         (now < last || /* time warp? */
          now > last + max_age)) { /* expired? */
       Clear();
@@ -82,7 +97,7 @@ public:
   }
 
   bool IsValid() const {
-    return positive(last);
+    return last > 0;
   }
 
   /**
@@ -91,10 +106,10 @@ public:
    * @return The time difference in seconds
    */
   fixed GetTimeDifference(const Validity &other) const {
-    assert(positive(last));
-    assert(positive(other.last));
+    assert(IsValid());
+    assert(other.IsValid());
 
-    return last - other.last;
+    return Export(last - other.last);
   }
 
   /**
