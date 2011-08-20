0815683c22508b3a56333c0556854a9be276b5f7
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-08-20 20:11:50 +0200

    AirspaceClass: Use arrays in airspace_class_as_text() function

--- src/Engine/Airspace/AirspaceClass.cpp
+++ src/Engine/Airspace/AirspaceClass.cpp
@@ -1,104 +1,53 @@
 #include "AirspaceClass.hpp"
-#include <assert.h>
+#include "Util/Macros.hpp"
+
+static const TCHAR *airspace_class_names[] = {
+  _T("Unknown"),
+  _T("Restricted"),
+  _T("Prohibited"),
+  _T("Danger Area"),
+  _T("Class A"),
+  _T("Class B"),
+  _T("Class C"),
+  _T("Class D"),
+  _T("No Gliders"),
+  _T("CTR"),
+  _T("Wave"),
+  _T("Task Area"),
+  _T("Class E"),
+  _T("Class F"),
+  _T("Transponder Mandatory Zone"),
+  _T("Class G"),
+};
+
+static const TCHAR *airspace_class_short_names[] = {
+  _T("?"),
+  _T("R"),
+  _T("P"),
+  _T("Q"),
+  _T("A"),
+  _T("B"),
+  _T("C"),
+  _T("D"),
+  _T("GP"),
+  _T("CTR"),
+  _T("W"),
+  _T("AAT"),
+  _T("E"),
+  _T("F"),
+  _T("TMZ"),
+  _T("G"),
+};
 
 const TCHAR *
-airspace_class_as_text(const AirspaceClass item,
-                       const bool concise)
+airspace_class_as_text(const AirspaceClass item, const bool concise)
 {
-  switch(item) {
-  case RESTRICT:
-    if (!concise) {
-      return _T("Restricted");
-    } else {
-      return _T("R"); // was LxR
-    }
-  case PROHIBITED:
-    if (!concise) {
-      return _T("Prohibited");
-    } else {
-      return _T("P"); // was LxP 
-    }
-  case DANGER:
-    if (!concise) {
-      return _T("Danger Area");
-    } else {
-      return _T("Q"); // was lxD
-    }
-  case CLASSA:
-    if (!concise) {
-      return _T("Class A");
-    } else {
-      return _T("A");
-    }
-  case CLASSB:
-    if (!concise) {
-      return _T("Class B");
-    } else {
-      return _T("B");
-    }
-  case CLASSC:
-    if (!concise) {
-      return _T("Class C");
-    } else {
-      return _T("C");
-    }
-  case CLASSD:
-    if (!concise) {
-      return _T("Class D");
-    } else {
-      return _T("D");
-    }
-  case CLASSE:
-    if (!concise) {
-      return _T("Class E");
-    } else {
-      return _T("E");
-    }
-  case CLASSF:
-    if (!concise) {
-      return _T("Class F");
-    } else {
-      return _T("F");
-    }
-  case CLASSG:
-    if (!concise) {
-      return _T("Class G");
-    } else {
-      return _T("G");
-    }
-  case NOGLIDER:
-    if (!concise) {
-      return _T("No Glider");
-    } else {
-      return _T("GP"); // was NoGld
-    }
-  case CTR:
-    if (!concise) {
-      return _T("CTR");
-    } else {
-      return _T("CTR");
-    }
-  case WAVE:
-    if (!concise) {
-      return _T("Wave");
-    } else {
-      return _T("W"); // was Wav
-    }
-  case TMZ:
-    if (!concise) {
-      return _T("Transponder Mandatory Zone");
-    } else {
-      return _T("TMZ");
-    }
-  case AATASK:
-    if (!concise) {
-      return _T("AAT");
-    } else {
-      return _T("T");
-    }
-  default:
-    break;
-  };
-  assert(1);
-  return _T("Unknown");
+  unsigned i = (unsigned)item;
+
+  if (!concise)
+    return i < ARRAY_SIZE(airspace_class_names) ?
+           airspace_class_names[i] : NULL;
+
+  return i < ARRAY_SIZE(airspace_class_short_names) ?
+         airspace_class_short_names[i] : NULL;
 }
