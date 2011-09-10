e4eaabd93e5054f496b9b829ed90994ca08dbff4
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-09-10 21:35:41 +0200

    GlueMapWindowOverlays: Simplified DrawFinalGlide() function

--- src/MapWindow/GlueMapWindowOverlays.cpp
+++ src/MapWindow/GlueMapWindowOverlays.cpp
@@ -159,149 +159,150 @@ GlueMapWindow::DrawFinalGlide(Canvas &canvas, const PixelRect &rc) const
   int Offset0;
   int i;
 
-  if (Calculated().task_stats.task_valid &&
-      Calculated().task_stats.total.solution_remaining.IsOk() &&
-      Calculated().task_stats.total.solution_mc0.IsDefined()) {
-    const int y0 = ((rc.bottom - rc.top) / 2) + rc.top;
+  if (!Calculated().task_stats.task_valid ||
+      !Calculated().task_stats.total.solution_remaining.IsOk() ||
+      !Calculated().task_stats.total.solution_mc0.IsDefined())
+    return;
 
-    // 60 units is size, div by 8 means 60*8 = 480 meters.
-    Offset = ((int)Calculated().task_stats.total.solution_remaining.altitude_difference) / 8;
+  const int y0 = ((rc.bottom - rc.top) / 2) + rc.top;
 
-    // JMW OLD_TASK this is broken now
-    Offset0 = ((int)Calculated().task_stats.total.solution_mc0.altitude_difference) / 8;
-    // TODO feature: should be an angle if in final glide mode
+  // 60 units is size, div by 8 means 60*8 = 480 meters.
+  Offset = ((int)Calculated().task_stats.total.solution_remaining.altitude_difference) / 8;
 
-    if (Offset > 60)
-      Offset = 60;
-    if (Offset < -60)
-      Offset = -60;
+  // JMW OLD_TASK this is broken now
+  Offset0 = ((int)Calculated().task_stats.total.solution_mc0.altitude_difference) / 8;
+  // TODO feature: should be an angle if in final glide mode
 
-    Offset = Layout::Scale(Offset);
-    if (Offset < 0)
-      GlideBar[1].y = Layout::Scale(9);
+  if (Offset > 60)
+    Offset = 60;
+  if (Offset < -60)
+    Offset = -60;
 
-    if (Offset0 > 60)
-      Offset0 = 60;
-    if (Offset0 < -60)
-      Offset0 = -60;
+  Offset = Layout::Scale(Offset);
+  if (Offset < 0)
+    GlideBar[1].y = Layout::Scale(9);
 
-    Offset0 = Layout::Scale(Offset0);
-    if (Offset0 < 0)
-      GlideBar0[1].y = Layout::Scale(9);
+  if (Offset0 > 60)
+    Offset0 = 60;
+  if (Offset0 < -60)
+    Offset0 = -60;
 
-    for (i = 0; i < 6; i++) {
-      GlideBar[i].y += y0;
-      GlideBar[i].x = Layout::Scale(GlideBar[i].x) + rc.left;
-    }
+  Offset0 = Layout::Scale(Offset0);
+  if (Offset0 < 0)
+    GlideBar0[1].y = Layout::Scale(9);
 
-    GlideBar[0].y -= Offset;
-    GlideBar[1].y -= Offset;
-    GlideBar[2].y -= Offset;
+  for (i = 0; i < 6; i++) {
+    GlideBar[i].y += y0;
+    GlideBar[i].x = Layout::Scale(GlideBar[i].x) + rc.left;
+  }
 
-    for (i = 0; i < 6; i++) {
-      GlideBar0[i].y += y0;
-      GlideBar0[i].x = Layout::Scale(GlideBar0[i].x) + rc.left;
-    }
+  GlideBar[0].y -= Offset;
+  GlideBar[1].y -= Offset;
+  GlideBar[2].y -= Offset;
 
-    GlideBar0[0].y -= Offset0;
-    GlideBar0[1].y -= Offset0;
-    GlideBar0[2].y -= Offset0;
+  for (i = 0; i < 6; i++) {
+    GlideBar0[i].y += y0;
+    GlideBar0[i].x = Layout::Scale(GlideBar0[i].x) + rc.left;
+  }
 
-    if ((Offset < 0) && (Offset0 < 0)) {
-      // both below
-      if (Offset0 != Offset) {
-        int dy = (GlideBar0[0].y - GlideBar[0].y) +
-            (GlideBar0[0].y - GlideBar0[3].y);
-        dy = max(Layout::Scale(3), dy);
-        GlideBar[3].y = GlideBar0[0].y - dy;
-        GlideBar[4].y = GlideBar0[1].y - dy;
-        GlideBar[5].y = GlideBar0[2].y - dy;
+  GlideBar0[0].y -= Offset0;
+  GlideBar0[1].y -= Offset0;
+  GlideBar0[2].y -= Offset0;
 
-        GlideBar0[0].y = GlideBar[3].y;
-        GlideBar0[1].y = GlideBar[4].y;
-        GlideBar0[2].y = GlideBar[5].y;
-      } else {
-        Offset0 = 0;
-      }
-    } else if ((Offset > 0) && (Offset0 > 0)) {
-      // both above
-      GlideBar0[3].y = GlideBar[0].y;
-      GlideBar0[4].y = GlideBar[1].y;
-      GlideBar0[5].y = GlideBar[2].y;
+  if ((Offset < 0) && (Offset0 < 0)) {
+    // both below
+    if (Offset0 != Offset) {
+      int dy = (GlideBar0[0].y - GlideBar[0].y) +
+          (GlideBar0[0].y - GlideBar0[3].y);
+      dy = max(Layout::Scale(3), dy);
+      GlideBar[3].y = GlideBar0[0].y - dy;
+      GlideBar[4].y = GlideBar0[1].y - dy;
+      GlideBar[5].y = GlideBar0[2].y - dy;
 
-      if (abs(Offset0 - Offset) < Layout::Scale(4))
-        Offset = Offset0;
-    }
-
-    // draw actual glide bar
-    if (Offset <= 0) {
-      if (Calculated().common_stats.landable_reachable) {
-        canvas.select(Graphics::hpFinalGlideBelowLandable);
-        canvas.select(Graphics::hbFinalGlideBelowLandable);
-      } else {
-        canvas.select(Graphics::hpFinalGlideBelow);
-        canvas.select(Graphics::hbFinalGlideBelow);
-      }
+      GlideBar0[0].y = GlideBar[3].y;
+      GlideBar0[1].y = GlideBar[4].y;
+      GlideBar0[2].y = GlideBar[5].y;
     } else {
-      canvas.select(Graphics::hpFinalGlideAbove);
-      canvas.select(Graphics::hbFinalGlideAbove);
+      Offset0 = 0;
     }
-    canvas.polygon(GlideBar, 6);
+  } else if ((Offset > 0) && (Offset0 > 0)) {
+    // both above
+    GlideBar0[3].y = GlideBar[0].y;
+    GlideBar0[4].y = GlideBar[1].y;
+    GlideBar0[5].y = GlideBar[2].y;
 
-    // draw glide bar at mc 0
-    if (Offset0 <= 0) {
-      if (Calculated().common_stats.landable_reachable) {
-        canvas.select(Graphics::hpFinalGlideBelowLandable);
-        canvas.hollow_brush();
-      } else {
-        canvas.select(Graphics::hpFinalGlideBelow);
-        canvas.hollow_brush();
-      }
+    if (abs(Offset0 - Offset) < Layout::Scale(4))
+      Offset = Offset0;
+  }
+
+  // draw actual glide bar
+  if (Offset <= 0) {
+    if (Calculated().common_stats.landable_reachable) {
+      canvas.select(Graphics::hpFinalGlideBelowLandable);
+      canvas.select(Graphics::hbFinalGlideBelowLandable);
     } else {
-      canvas.select(Graphics::hpFinalGlideAbove);
+      canvas.select(Graphics::hpFinalGlideBelow);
+      canvas.select(Graphics::hbFinalGlideBelow);
+    }
+  } else {
+    canvas.select(Graphics::hpFinalGlideAbove);
+    canvas.select(Graphics::hbFinalGlideAbove);
+  }
+  canvas.polygon(GlideBar, 6);
+
+  // draw glide bar at mc 0
+  if (Offset0 <= 0) {
+    if (Calculated().common_stats.landable_reachable) {
+      canvas.select(Graphics::hpFinalGlideBelowLandable);
+      canvas.hollow_brush();
+    } else {
+      canvas.select(Graphics::hpFinalGlideBelow);
       canvas.hollow_brush();
     }
-
-    if (Offset != Offset0)
-      canvas.polygon(GlideBar0, 6);
-
-    // draw cross (x) on final glide bar if unreachable at current Mc
-    // or above final glide but impeded by obstacle
-    int cross_sign = 0;
-
-    if (!Calculated().task_stats.total.IsAchievable())
-      cross_sign = 1;
-    if (Calculated().terrain_warning && (Offset>0))
-      cross_sign = -1;
-
-    if (cross_sign != 0) {
-      canvas.select(task_look.bearing_pen);
-      canvas.line(Layout::Scale(9 - 5), y0 + cross_sign * Layout::Scale(9 - 5),
-                  Layout::Scale(9 + 5), y0 + cross_sign * Layout::Scale(9 + 5));
-      canvas.line(Layout::Scale(9 - 5), y0 + cross_sign * Layout::Scale(9 + 5),
-                  Layout::Scale(9 + 5), y0 + cross_sign * Layout::Scale(9 - 5));
-    }
-
-    Units::FormatUserAltitude(Calculated().task_stats.total.solution_remaining.altitude_difference,
-                              Value, ARRAY_SIZE(Value),
-                              false);
-
-    if (Offset >= 0)
-      Offset = GlideBar[2].y + Offset + Layout::Scale(5);
-    else if (Offset0 > 0)
-      Offset = GlideBar0[1].y - Layout::Scale(15);
-    else
-      Offset = GlideBar[2].y + Offset - Layout::Scale(15);
-
-    canvas.set_text_color(COLOR_BLACK);
-    canvas.set_background_color(COLOR_WHITE);
-
-    TextInBoxMode_t TextInBoxMode;
-    TextInBoxMode.Mode = RoundedBlack;
-    TextInBoxMode.Bold = true;
-    TextInBoxMode.MoveInView = true;
-    TextInBox(canvas, Value, 0, (int)Offset, TextInBoxMode, rc);
+  } else {
+    canvas.select(Graphics::hpFinalGlideAbove);
+    canvas.hollow_brush();
   }
+
+  if (Offset != Offset0)
+    canvas.polygon(GlideBar0, 6);
+
+  // draw cross (x) on final glide bar if unreachable at current Mc
+  // or above final glide but impeded by obstacle
+  int cross_sign = 0;
+
+  if (!Calculated().task_stats.total.IsAchievable())
+    cross_sign = 1;
+  if (Calculated().terrain_warning && (Offset>0))
+    cross_sign = -1;
+
+  if (cross_sign != 0) {
+    canvas.select(task_look.bearing_pen);
+    canvas.line(Layout::Scale(9 - 5), y0 + cross_sign * Layout::Scale(9 - 5),
+                Layout::Scale(9 + 5), y0 + cross_sign * Layout::Scale(9 + 5));
+    canvas.line(Layout::Scale(9 - 5), y0 + cross_sign * Layout::Scale(9 + 5),
+                Layout::Scale(9 + 5), y0 + cross_sign * Layout::Scale(9 - 5));
+  }
+
+  Units::FormatUserAltitude(Calculated().task_stats.total.solution_remaining.altitude_difference,
+                            Value, ARRAY_SIZE(Value),
+                            false);
+
+  if (Offset >= 0)
+    Offset = GlideBar[2].y + Offset + Layout::Scale(5);
+  else if (Offset0 > 0)
+    Offset = GlideBar0[1].y - Layout::Scale(15);
+  else
+    Offset = GlideBar[2].y + Offset - Layout::Scale(15);
+
+  canvas.set_text_color(COLOR_BLACK);
+  canvas.set_background_color(COLOR_WHITE);
+
+  TextInBoxMode_t TextInBoxMode;
+  TextInBoxMode.Mode = RoundedBlack;
+  TextInBoxMode.Bold = true;
+  TextInBoxMode.MoveInView = true;
+  TextInBox(canvas, Value, 0, (int)Offset, TextInBoxMode, rc);
 }
 
 void
