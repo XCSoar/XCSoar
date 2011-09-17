8405e5e510012115c671f56b9eea74f8ed61cea4
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-09-17 13:36:20 +0200

    Renderer: Added AirspacePreviewRenderer namespace

---
+++ src/Renderer/AirspacePreviewRenderer.cpp
@@ -0,0 +1,47 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2011 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#include "AirspacePreviewRenderer.hpp"
+#include "Screen/Canvas.hpp"
+#include "Screen/Features.hpp"
+#include "Airspace/AbstractAirspace.hpp"
+#include "Airspace/AirspaceRendererSettings.hpp"
+#include "Look/AirspaceLook.hpp"
+
+void
+AirspacePreviewRenderer::Draw(Canvas &canvas, const AbstractAirspace &airspace,
+                              const RasterPoint pt, unsigned radius,
+                              const AirspaceRendererSettings &settings,
+                              const AirspaceLook &look)
+{
+  if (settings.black_outline)
+    canvas.black_pen();
+  else
+    canvas.select(look.pens[airspace.GetType()]);
+
+  if (airspace.shape == AbstractAirspace::CIRCLE)
+    canvas.circle(pt.x, pt.y, radius);
+  else
+    canvas.rectangle(pt.x - radius, pt.y - radius,
+                     pt.x + radius, pt.y + radius);
+}
---
+++ src/Renderer/AirspacePreviewRenderer.hpp
@@ -0,0 +1,47 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2011 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_AIRSPACE_PREVIEW_RENDERER_HPP
+#define XCSOAR_AIRSPACE_PREVIEW_RENDERER_HPP
+
+#include "Screen/Point.hpp"
+
+class Canvas;
+class AbstractAirspace;
+struct AirspaceRendererSettings;
+struct AirspaceLook;
+
+namespace AirspacePreviewRenderer
+{
+  /**
+   * Draw a scaled preview of the given airspace
+   * @todo fill interior with hatched brush or half transparent color
+   * @todo real scaled outline for polygon airspaces
+   */
+  void Draw(Canvas &canvas, const AbstractAirspace &airspace,
+            const RasterPoint pt, unsigned radius,
+            const AirspaceRendererSettings &settings,
+            const AirspaceLook &look);
+}
+
+#endif
