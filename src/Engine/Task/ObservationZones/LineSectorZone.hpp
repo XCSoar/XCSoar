1b98b1b00f6cc8d2975d03f7f798a5f564c6c552
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-09-24 11:20:06 +0200

    LineSectorZone: Fixed Format

--- src/Engine/Task/ObservationZones/LineSectorZone.hpp
+++ src/Engine/Task/ObservationZones/LineSectorZone.hpp
@@ -23,6 +23,7 @@
 
 #ifndef LINESECTORZONE_HPP
 #define LINESECTORZONE_HPP
+
 #include "SymmetricSectorZone.hpp"
 
 /**
@@ -32,77 +33,74 @@
  * The constraint test ensures transitioning to exit only occurs if the
  * line is crossed (rather than exiting from the back semi-circle).
  */
-class LineSectorZone: 
-  public SymmetricSectorZone 
+class LineSectorZone: public SymmetricSectorZone
 {
   LineSectorZone(const LineSectorZone &other, const GeoPoint &reference)
     :SymmetricSectorZone((const SymmetricSectorZone &)other, reference) {}
 
 public:
-/** 
- * Constructor
- * 
- * @param loc Location of center point of line
- * @param length Length of line (m)
- * 
- * @return Initialised object
- */
-  LineSectorZone(const GeoPoint loc, const fixed length=fixed(1000.0)):
-    SymmetricSectorZone(LINE, loc, half(length),
-                        Angle::radians(fixed_pi))
+  /**
+   * Constructor
+   *
+   * @param loc Location of center point of line
+   * @param length Length of line (m)
+   *
+   * @return Initialised object
+   */
+  LineSectorZone(const GeoPoint loc, const fixed length = fixed(1000.0))
+    :SymmetricSectorZone(LINE, loc, half(length), Angle::radians(fixed_pi))
   {
     updateSector();
   }
 
-  ObservationZonePoint* clone(const GeoPoint * _location=0) const {
-    if (_location) {
+  ObservationZonePoint* clone(const GeoPoint *_location = NULL) const {
+    if (_location)
       return new LineSectorZone(*this, *_location);
-    } else {
-      return new LineSectorZone(*this, get_location());
-    }
+
+    return new LineSectorZone(*this, get_location());
   }
 
-/** 
- * Check transition constraints -- for lines, both points have to
- * be within radius of OZ (otherwise it is a circumference border crossing)
- * 
- * @param ref_now Current aircraft state
- * @param ref_last Previous aircraft state
- * 
- * @return True if constraints are satisfied
- */
+  /**
+   * Check transition constraints -- for lines, both points have to
+   * be within radius of OZ (otherwise it is a circumference border crossing)
+   *
+   * @param ref_now Current aircraft state
+   * @param ref_last Previous aircraft state
+   *
+   * @return True if constraints are satisfied
+   */
   bool TransitionConstraint(const AircraftState & ref_now, 
-                             const AircraftState & ref_last) const {
-    return CylinderZone::IsInSector(ref_now) && CylinderZone::IsInSector(ref_last);
+                            const AircraftState & ref_last) const {
+    return CylinderZone::IsInSector(ref_now) &&
+           CylinderZone::IsInSector(ref_last);
   }
 
   GeoPoint GetBoundaryParametric(fixed t) const;  
 
-/** 
- * Distance reduction for scoring when outside this OZ
- * 
- * @return Distance (m) to subtract from score
- */
+  /**
+   * Distance reduction for scoring when outside this OZ
+   *
+   * @return Distance (m) to subtract from score
+   */
   fixed ScoreAdjustment() const;
 
-/** 
- * Set length property
- * 
- * @param new_length Length (m) of line
- */
+  /**
+   * Set length property
+   *
+   * @param new_length Length (m) of line
+   */
   void setLength(const fixed new_length) {
     setRadius(half(new_length));
   }
   
-/** 
- * Get length property value
- * 
- * @return Length (m) of line
- */
+  /**
+   * Get length property value
+   *
+   * @return Length (m) of line
+   */
   fixed getLength() const {
-    return getRadius()*fixed_two;
+    return getRadius() * fixed_two;
   }
-
 
   /**
    * If zone when used for start can trigger start via vertical exit
@@ -110,7 +108,9 @@ public:
    * @return False for line start sectors
    */
   gcc_pure
-  virtual bool CanStartThroughTop() const { return false; }
+  virtual bool CanStartThroughTop() const {
+    return false;
+  }
 };
 
 #endif
