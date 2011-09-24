fd2c5c9fb264fabde85d36c5c17e7660c7f6f89f
Max Kellermann <max@duempel.org>
2011-09-24 15:40:46 +0200

    Makefile: don't change directory before calling LaTeX
    By adjusting TEXINPUTS and \graphicspath, we can go without this hack.

--- build/manual.mk
+++ build/manual.mk
@@ -33,9 +33,9 @@ SVG_GRAPHICS = $(patsubst $(topdir)/Data/graphics/%.svg,$(GENERATED_DIR)/graphic
 TEX_INCLUDES_DE =  $(wildcard $(DOC)/manual/*.sty) $(wildcard $(DOC)/manual/de/*.sty)
 FIGURES_DE = $(DOC)/manual/de/Bilder/*.png
  
-TEX_VARS = TEXINPUTS=".:..:../shared:../../..:"
+TEX_VARS = TEXINPUTS="$(<D):$(DOC)/manual:$(DOC)/manual/shared:.:"
 TEX_FLAGS = -halt-on-error -interaction=nonstopmode
-TEX_RUN = cd $(<D) && $(TEX_VARS) pdflatex $(TEX_FLAGS) -output-directory $(abspath $(@D))
+TEX_RUN = $(TEX_VARS) pdflatex $(TEX_FLAGS) -output-directory $(@D)
 
 .PHONY: manual
 manual: \
@@ -48,25 +48,25 @@ $(MANUAL_OUTPUT_DIR)/XCSoar-manual.pdf: $(DOC)/manual/en/XCSoar-manual.tex \
 	$(FIGURES_EN) $(SVG_ICONS) $(SVG_FIGURES) $(SVG_GRAPHICS) | $(MANUAL_OUTPUT_DIR)/dirstamp
 ifeq ($(DEBUG),n)
 	# run TeX twice to make sure that all references are resolved
-	$(TEX_RUN) $(<F)
+	$(TEX_RUN) $<
 endif
-	$(TEX_RUN) $(<F)
+	$(TEX_RUN) $<
 
 $(MANUAL_OUTPUT_DIR)/XCSoar-developer-manual.pdf: $(DOC)/manual/en/XCSoar-developer-manual.tex $(TEX_INCLUDES_EN) \
 	$(FIGURES_EN) $(SVG_ICONS) $(SVG_FIGURES) $(SVG_GRAPHICS) | $(MANUAL_OUTPUT_DIR)/dirstamp
 ifeq ($(DEBUG),n)
 	# run TeX twice to make sure that all references are resolved
-	$(TEX_RUN) $(<F)
+	$(TEX_RUN) $<
 endif
-	$(TEX_RUN) $(<F)
+	$(TEX_RUN) $<
 
 $(MANUAL_OUTPUT_DIR)/XCSoar-Handbuch.pdf: $(DOC)/manual/de/XCSoar-Handbuch.tex $(DOC)/manual/de/Blitzeinstieg.tex $(TEX_INCLUDES_DE) \
 	$(FIGURES_DE) $(SVG_ICONS) $(SVG_FIGURES) $(SVG_GRAPHICS) | $(MANUAL_OUTPUT_DIR)/dirstamp
 ifeq ($(DEBUG),n)
 	# run TeX twice to make sure that all references are resolved
-	$(TEX_RUN) $(<F)
+	$(TEX_RUN) $<
 endif
-	$(TEX_RUN) $(<F)
+	$(TEX_RUN) $<
 
 $(SVG_ICONS): $(GENERATED_DIR)/icons/%.pdf: $(topdir)/Data/icons/%.svg | $(GENERATED_DIR)/icons/dirstamp
 	rsvg-convert -a -f pdf -w 32 $< -o $@
