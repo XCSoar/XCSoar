6c07ae2984aa3b747a2a66df9ff111fcc5aed208
Max Kellermann <max@duempel.org>
2011-09-25 11:21:55 +0200

    Screen/Graphics: make the snail color arrays static
    Compile-time initialisation.

--- src/Screen/Graphics.cpp
+++ src/Screen/Graphics.cpp
@@ -191,20 +191,20 @@ Graphics::InitialiseConfigured(const SETTINGS_MAP &settings_map)
 void
 Graphics::InitSnailTrail(const SETTINGS_MAP &settings_map)
 {
-  const ColorRamp snail_colors_vario[] = {
+  static const ColorRamp snail_colors_vario[] = {
     {0,   0xc4, 0x80, 0x1e}, // sinkColor
     {100, 0xa0, 0xa0, 0xa0},
     {200, 0x1e, 0xf1, 0x73} // liftColor
   };
 
-  const ColorRamp snail_colors_vario2[] = {
+  static const ColorRamp snail_colors_vario2[] = {
     {0,   0x00, 0x00, 0xff},
     {99,  0x00, 0xff, 0xff},
     {100, 0xff, 0xff, 0x00},
     {200, 0xff, 0x00, 0x00}
   };
 
-  const ColorRamp snail_colors_alt[] = {
+  static const ColorRamp snail_colors_alt[] = {
     {0,   0xff, 0x00, 0x00},
     {50,  0xff, 0xff, 0x00},
     {100, 0x00, 0xff, 0x00},
