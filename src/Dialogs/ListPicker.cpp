XCSoar/XCSoar

commit 82bfcb363c542251873355c63cd22ac7287b92e9

Dialogs: simplify callback initialization; move callback tables down

src/Dialogs/ListPicker.cpp
@@ -84,6 +84,7 @@ OnPointCursorCallback(unsigned i)
 static CallBackTableEntry CallBackTable[] = {
   DeclareCallBackEntry(OnCloseClicked),
   DeclareCallBackEntry(OnCancelClicked),
+  DeclareCallBackEntry(OnHelpClicked),
   DeclareCallBackEntry(NULL)
 };
 
@@ -132,13 +133,6 @@ ListPicker(SingleWindow &parent, const TCHAR *caption,
   }
   else
     wItemHelp = NULL;
-
-  WndButton *help_button = (WndButton *)wf->FindByName(_T("cmdHelp"));
-  assert(help_button != NULL);
-  if (help_callback != NULL)
-    help_button->SetOnClickNotify(OnHelpClicked);
-  else
-    help_button->hide();
 
   if (update)
     wf->SetTimerNotify(OnTimerNotify);

Data/Dialogs/dlgAnalysis.xml
@@ -11,5 +11,5 @@
   <SymbolButton Name="cmdPrev" Caption="&lt;" X="5" Y="-60" Width="37" Height="28" OnClick="OnPrevClicked" />
   <SymbolButton Name="cmdNext" Caption="&gt;" X="48" Y="-60" Width="37" Height="28" OnClick="OnNextClicked" />
 
-  <Button Name="cmdClose" Caption="Close" X="5" Y="-30" Width="80" Height="28" />
+  <Button Name="cmdClose" Caption="Close" X="5" Y="-30" Width="80" Height="28" OnClick="OnCloseClicked" />
 </Form>

Data/Dialogs/dlgAnalysis_L.xml
@@ -11,5 +11,5 @@
   <SymbolButton Name="cmdPrev" Caption="&lt;" X="5" Y="160" Width="37" Height="28" OnClick="OnPrevClicked" />
   <SymbolButton Name="cmdNext" Caption="&gt;" X="48" Y="160" Width="37" Height="28" OnClick="OnNextClicked" />
 
-  <Button Name="cmdClose" Caption="Close" X="5" Y="190" Width="80" Height="28" />
+  <Button Name="cmdClose" Caption="Close" X="5" Y="190" Width="80" Height="28" OnClick="OnCloseClicked" />
 </Form>

Data/Dialogs/dlgChecklist.xml
@@ -5,7 +5,7 @@
 
   <SymbolButton Name="cmdNext" Caption="&gt;" X="60" Y="-35" Width="60" Height="35" OnClick="OnNextClicked" />
 
-  <Button Name="cmdClose" Caption="Close" X="-120" Y="-35" Height="35" />
+  <Button Name="cmdClose" Caption="Close" X="-120" Y="-35" Height="35" OnClick="OnCloseClicked" />
 
   <SymbolButton Name="cmdPrev" Caption="&lt;" X="0" Y="-35" Width="60" Height="35" OnClick="OnPrevClicked" />
 </Form>

Data/Dialogs/dlgChecklist_L.xml
@@ -3,7 +3,7 @@
 <Form Caption="Checklist">
   <SymbolButton Name="cmdNext" Caption="&gt;" X="0" Y="-70" Width="60" Height="35" OnClick="OnNextClicked" />
 
-  <Button Name="cmdClose" Caption="Close" X="0" Y="-35" Width="60" Height="35" />
+  <Button Name="cmdClose" Caption="Close" X="0" Y="-35" Width="60" Height="35" OnClick="OnCloseClicked" />
 
   <Edit Name="frmDetails" X="60" Y="0" ReadOnly="1" MultiLine="1" />
 

Data/Dialogs/dlgConfigFonts.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 
 <Form Caption="Font Configuration">
-  <Button Name="cmdClose" Caption="Close" X="5" Y="-37" Width="80" Height="35"/>
+  <Button Name="cmdClose" Caption="Close" X="5" Y="-37" Width="80" Height="35" OnClick="OnCloseClicked"/>
 
   <Edit Name="prpUseCustomFonts" Caption="Customize fonts" X="2" Y="2" Width="-2" Height="22" CaptionWidth="150">
     <DataField DataType="boolean" OnDataAccess="OnUseCustomFontData"/>

Data/Dialogs/dlgConfigFonts_L.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 
 <Form Caption="Font Configuration">
-  <Button Name="cmdClose" Caption="Close" X="5" Y="-37" Width="60" Height="35"/>
+  <Button Name="cmdClose" Caption="Close" X="5" Y="-37" Width="60" Height="35" OnClick="OnCloseClicked"/>
 
   <Edit Name="prpUseCustomFonts" Caption="Customize fonts" X="70" Y="1" Width="-2" Height="22" CaptionWidth="150">
     <DataField DataType="boolean" OnDataAccess="OnUseCustomFontData" />

Data/Dialogs/dlgConfigWaypoints.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 
 <Form Caption="Waypoints Editor">
-  <Button Name="cmdClose" Caption="Close" X="5" Y="-37" Width="80" Height="35" />
+  <Button Name="cmdClose" Caption="Close" X="5" Y="-37" Width="80" Height="35" OnClick="OnCloseClicked" />
 
   <Button Name="cmdWaypointNew" Caption="New" X="50" Y="10" Width="150" Height="28" OnClick="OnWaypointNewClicked" />
   <Button Name="cmdWaypointEdit" Caption="Edit" X="50" Width="150" Height="28" OnClick="OnWaypointEditClicked" />

commit 21ec6a2dc4a5a84524c7bef4e2237ee0ab118272

ListPicker: Fixed help button bug (ticket #1508)
This regression was introduced by commit 82bfcb363c542251873355c63cd22ac7287b92e9

src/Dialogs/ListPicker.cpp
@@ -134,6 +134,11 @@ ListPicker(SingleWindow &parent, const TCHAR *caption,
   else
     wItemHelp = NULL;
 
+  WndButton *help_button = (WndButton *)wf->FindByName(_T("cmdHelp"));
+  assert(help_button != NULL);
+  if (help_callback == NULL)
+    help_button->hide();
+
   if (update)
     wf->SetTimerNotify(OnTimerNotify);
 