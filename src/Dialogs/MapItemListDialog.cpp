6a26ada3908482f457cb4a27048b57ceda458a0f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-10-17 00:11:31 +0200

    MapItemListDialog: Use separate dialog for MapItemList
    Prepares for additional controls

--- src/Dialogs/MapItemListDialog.cpp
+++ src/Dialogs/MapItemListDialog.cpp
@@ -24,7 +24,6 @@ Copyright_License {
 #include "Dialogs/MapItemListDialog.hpp"
 #include "Screen/Layout.hpp"
 #include "Screen/Fonts.hpp"
-#include "Dialogs/ListPicker.hpp"
 #include "Dialogs/Airspace.hpp"
 #include "Dialogs/Task.hpp"
 #include "Dialogs/Waypoint.hpp"
@@ -33,6 +32,11 @@ Copyright_License {
 #include "MapWindow/MapItemList.hpp"
 #include "MapWindow/MapItemListBuilder.hpp"
 #include "Renderer/MapItemListRenderer.hpp"
+#include "Dialogs/XML.hpp"
+#include "Dialogs/dlgTools.h"
+#include "Form/Form.hpp"
+#include "Form/List.hpp"
+#include "Form/Button.hpp"
 
 static const AircraftLook *aircraft_look;
 static const AirspaceLook *airspace_look;
@@ -40,6 +44,7 @@ static const WaypointLook *waypoint_look;
 static const TaskLook *task_look;
 static const SETTINGS_MAP *settings;
 static const MapItemList *list;
+static WndForm *wf;
 
 static void
 PaintListItem(Canvas &canvas, const PixelRect rc, unsigned idx)
@@ -47,6 +52,63 @@ PaintListItem(Canvas &canvas, const PixelRect rc, unsigned idx)
   const MapItem &item = *(*list)[idx];
   MapItemListRenderer::Draw(canvas, rc, item, *aircraft_look, *airspace_look,
                             *waypoint_look, *task_look, *settings);
+}
+
+static void
+OnCloseClicked(gcc_unused WndButton &Sender)
+{
+  wf->SetModalResult(mrOK);
+}
+
+static void
+OnComboPopupListEnter(gcc_unused unsigned i)
+{
+  wf->SetModalResult(mrOK);
+}
+
+static void
+OnCancelClicked(gcc_unused WndButton &Sender)
+{
+  wf->SetModalResult(mrCancel);
+}
+
+static CallBackTableEntry CallBackTable[] = {
+  DeclareCallBackEntry(OnCloseClicked),
+  DeclareCallBackEntry(OnCancelClicked),
+  DeclareCallBackEntry(NULL)
+};
+
+static int
+ShowMapItemListDialog(SingleWindow &parent)
+{
+  unsigned num_items = list->size();
+
+  UPixelScalar item_height = Fonts::MapBold.get_height() + Layout::Scale(6) +
+                             Fonts::MapLabel.get_height();
+
+  assert(num_items <= 0x7fffffff);
+  assert(item_height > 0);
+
+  wf = LoadDialog(CallBackTable, parent, Layout::landscape
+                  ? _T("IDR_XML_MAPITEMLIST_L")
+                  : _T("IDR_XML_MAPITEMLIST"));
+  assert(wf != NULL);
+
+  WndListFrame *list_control =
+      (WndListFrame *)wf->FindByName(_T("frmComboPopupList"));
+  assert(list_control != NULL);
+  list_control->SetItemHeight(item_height);
+  list_control->SetLength(num_items);
+  list_control->SetCursorIndex(0);
+  list_control->SetActivateCallback(OnComboPopupListEnter);
+  list_control->SetPaintItemCallback(PaintListItem);
+
+  int result = wf->ShowModal() == mrOK
+    ? (int)list_control->GetCursorIndex()
+    : -1;
+  delete wf;
+
+  return result;
 }
 
 void
@@ -90,8 +152,6 @@ ShowMapItemListDialog(SingleWindow &parent,
 
   default:
     /* more than one map item: show a list */
-    UPixelScalar line_height = Fonts::MapBold.get_height() + Layout::Scale(6) +
-                           Fonts::MapLabel.get_height();
     list = &_list;
 
     aircraft_look = &_aircraft_look;
@@ -100,8 +160,7 @@ ShowMapItemListDialog(SingleWindow &parent,
     task_look = &_task_look;
     settings = &_settings;
 
-    int i = ListPicker(parent, _("Map elements at this location"),
-                       _list.size(), 0, line_height, PaintListItem);
+    int i = ShowMapItemListDialog(parent);
     assert(i >= -1 && i < (int)_list.size());
     if (i >= 0)
       ShowMapItemDialog(*_list[i], parent);
