5eb5ac7a87038bb8c633022ccb029d9ddb4bab60
Max Kellermann <max@duempel.org>
2011-10-28 17:22:08 +0200

    Util/AllocatedArray: use std::move() in GrowPreserve()
    Reduce overhead.

--- src/Util/AllocatedArray.hpp
+++ src/Util/AllocatedArray.hpp
@@ -167,7 +167,13 @@ public:
     T *new_data = new T[_size];
     assert(_size == 0 || new_data != NULL);
 
+#ifdef ANDROID
+    /* STLport doesn't have std::move() */
     std::copy(data, data + preserve, new_data);
+#else
+    std::move(data, data + preserve, new_data);
+#endif
+
     delete[] data;
     data = new_data;
     size = _size;
