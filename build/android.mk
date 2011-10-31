341161678c96dbf71d20c77301587d64d5a906ec
Max Kellermann <max@duempel.org>
2011-10-31 08:30:48 +0100

    Makefile: add "Settings" to NATIVE_CLASSES

--- build/android.mk
+++ build/android.mk
@@ -38,7 +38,7 @@ CLASS_NAME = $(JAVA_PACKAGE).NativeView
 CLASS_SOURCE = $(subst .,/,$(CLASS_NAME)).java
 CLASS_CLASS = $(patsubst %.java,%.class,$(CLASS_SOURCE))
 
-NATIVE_CLASSES = NativeView EventBridge Timer InternalGPS
+NATIVE_CLASSES = NativeView EventBridge Timer InternalGPS Settings
 NATIVE_PREFIX = $(TARGET_OUTPUT_DIR)/include/$(subst .,_,$(JAVA_PACKAGE))_
 NATIVE_HEADERS = $(patsubst %,$(NATIVE_PREFIX)%.h,$(NATIVE_CLASSES))
 
