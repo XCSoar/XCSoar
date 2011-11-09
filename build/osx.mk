341804560c5364e7d035f830aa21ee60377b8758
Scott Penrose <scottp@dd.com.au>
2011-11-09 14:29:42 +1100

    Build: Fix Mac OS X Application paths to standard

--- build/osx.mk
+++ build/osx.mk
@@ -7,9 +7,9 @@ DMG_TMP = $(TARGET_OUTPUT_DIR)/tmp.dmg
 
 $(DMG_TMP): $(TARGET_BIN_DIR)/xcsoar build/Info.plist
 	rm -rf $(DMG_TMPDIR)
-	mkdir -p $(DMG_TMPDIR)/XCSoar.app/MacOS
-	sed -e "s,VERSION,$(FULL_VERSION)," <build/Info.plist >$(DMG_TMPDIR)/XCSoar.app/Info.plist
-	cp $(TARGET_BIN_DIR)/xcsoar $(DMG_TMPDIR)/XCSoar.app/MacOS/
+	mkdir -p $(DMG_TMPDIR)/XCSoar.app/Contents/MacOS
+	sed -e "s,VERSION,$(FULL_VERSION)," <build/Info.plist >$(DMG_TMPDIR)/XCSoar.app/Contents/Info.plist
+	cp $(TARGET_BIN_DIR)/xcsoar $(DMG_TMPDIR)/XCSoar.app/Contents/MacOS/
 	$(HDIUTIL) create -fs HFS+ -volname "XCSoar" -srcfolder $(DMG_TMPDIR) $@
 
 $(TARGET_OUTPUT_DIR)/XCSoar.dmg: $(DMG_TMP)
