2837c1b21adc39f2ddd787d58245c1b16e1fe59e
Scott Penrose <scottp@dd.com.au>
2011-11-11 00:26:06 +1100

    Build: Add Mac OS X Icon to Application package
    Icon is generated by converting SVG -> PNG -> Icns. Added to Info.plist
    and instlaled into the Contents/Resources directory

--- build/osx.mk
+++ build/osx.mk
@@ -5,11 +5,13 @@ HDIUTIL = hdiutil
 DMG_TMPDIR = $(TARGET_OUTPUT_DIR)/dmg
 DMG_TMP = $(TARGET_OUTPUT_DIR)/tmp.dmg
 
-$(DMG_TMP): $(TARGET_BIN_DIR)/xcsoar build/Info.plist
+$(DMG_TMP): $(TARGET_BIN_DIR)/xcsoar build/Info.plist output/data/graphics/xcsoarswiftsplash_128.icns
 	rm -rf $(DMG_TMPDIR)
 	mkdir -p $(DMG_TMPDIR)/XCSoar.app/Contents/MacOS
 	sed -e "s,VERSION,$(FULL_VERSION)," <build/Info.plist >$(DMG_TMPDIR)/XCSoar.app/Contents/Info.plist
 	cp $(TARGET_BIN_DIR)/xcsoar $(DMG_TMPDIR)/XCSoar.app/Contents/MacOS/
+	mkdir -p $(DMG_TMPDIR)/XCSoar.app/Contents/Resources
+	cp output/data/graphics/xcsoarswiftsplash_128.icns $(DMG_TMPDIR)/XCSoar.app/Contents/Resources/
 	$(HDIUTIL) create -fs HFS+ -volname "XCSoar" -srcfolder $(DMG_TMPDIR) $@
 
 $(TARGET_OUTPUT_DIR)/XCSoar.dmg: $(DMG_TMP)
