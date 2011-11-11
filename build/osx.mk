49cbd919cd7bc3a9413348fc9aa43df043c6cdc5
Max Kellermann <max@duempel.org>
2011-11-11 17:15:18 +0100

    Makefile: pass "-quiet" to hdiutil

--- build/osx.mk
+++ build/osx.mk
@@ -1,6 +1,11 @@
 ifeq ($(shell uname -s),Darwin)
 
 HDIUTIL = hdiutil
+HDIUTIL_OPTIONS =
+
+ifneq ($(V),2)
+HDIUTIL_OPTIONS += -quiet
+endif
 
 DMG_TMPDIR = $(TARGET_OUTPUT_DIR)/dmg
 DMG_TMP = $(TARGET_OUTPUT_DIR)/tmp.dmg
@@ -13,11 +18,11 @@ $(DMG_TMP): $(TARGET_BIN_DIR)/xcsoar build/Info.plist output/data/graphics/xcsoa
 	$(Q)cp $(TARGET_BIN_DIR)/xcsoar $(DMG_TMPDIR)/XCSoar.app/Contents/MacOS/
 	$(Q)mkdir -p $(DMG_TMPDIR)/XCSoar.app/Contents/Resources
 	$(Q)cp output/data/graphics/xcsoarswiftsplash_128.icns $(DMG_TMPDIR)/XCSoar.app/Contents/Resources/
-	$(Q)$(HDIUTIL) create -fs HFS+ -volname "XCSoar" -srcfolder $(DMG_TMPDIR) $@
+	$(Q)$(HDIUTIL) create $(HDIUTIL_OPTIONS) -fs HFS+ -volname "XCSoar" -srcfolder $(DMG_TMPDIR) $@
 
 $(TARGET_OUTPUT_DIR)/XCSoar.dmg: $(DMG_TMP)
 	@$(NQ)echo "  DMG     $@"
-	$(Q)$(HDIUTIL) convert $< -format UDCO -o $@
-	$(Q)$(HDIUTIL) internet-enable -yes $@
+	$(Q)$(HDIUTIL) convert $(HDIUTIL_OPTIONS) $< -format UDCO -o $@
+	$(Q)$(HDIUTIL) internet-enable $(HDIUTIL_OPTIONS) -yes $@
 
 endif
