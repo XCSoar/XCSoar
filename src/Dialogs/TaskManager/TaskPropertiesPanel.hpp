XCSoar/XCSoar

commit 30067938cdbce7fbcd20f2a8906321a95e86a35e

dlgTaskManager: refactor remaining panels to Widgets

src/Dialogs/TaskManager/TaskPropertiesPanel.hpp
@@ -24,61 +24,38 @@ Copyright_License {
 #ifndef XCSOAR_TASK_PROPERTIES_PANEL_HPP
 #define XCSOAR_TASK_PROPERTIES_PANEL_HPP
 
-#include "DataField/Base.hpp"
-#include "Screen/Point.hpp"
+#include "Form/XMLWidget.hpp"
+#include "Engine/Task/TaskBehaviour.hpp"
 
-class Window;
-class SingleWindow;
-class WndForm;
-class TabBarControl;
+class WndOwnerDrawFrame;
 class OrderedTask;
+class DataFieldBoolean;
+class DataFieldEnum;
 
-class pnlTaskProperties
-{
+class TaskPropertiesPanel : public XMLWidget {
+  WndOwnerDrawFrame *wTaskView;
+
+  OrderedTask **ordered_task_pointer, *ordered_task;
+  bool *task_changed;
+
+  TaskBehaviour::FactoryType orig_taskType;
+
 public:
-  /**
-   * creates the control from its XML file and does any init work
-   * @param parent
-   * @param task_modified Sets to True if changes it.
-   * @param wf
-   * @return Window* that points to the control created
-   */
-  static Window* Load(SingleWindow &parent, TabBarControl* wTabBar,
-                      WndForm* wf, OrderedTask** task, bool* _task_modified);
+  TaskPropertiesPanel(OrderedTask **_active_task, bool *_task_modified)
+    :ordered_task_pointer(_active_task), task_changed(_task_modified) {}
 
-  /**
-   * copies values from form to ordered_task
-   * @return true
-   */
-  static bool OnTabPreHide();
+  void OnFAIFinishHeightChange(DataFieldBoolean &df);
+  void OnTaskTypeChange(DataFieldEnum &df);
 
-  /**
-   * Shows/Hides normal finish height depending on whether FAI
-   * Finish Height is on or off
-   * @param Sender
-   * @param Mode
-   */
-  static void OnFAIFinishHeightData(DataField *Sender, DataField::DataAccessKind_t Mode);
+  virtual void Prepare(ContainerWindow &parent, const PixelRect &rc);
+  virtual void ReClick();
+  virtual void Show(const PixelRect &rc);
+  virtual void Hide();
 
-  /**
-   *
-   * Changes the task type and mutates the task points as necessary
-   * @param Sender datafield for prpTaskType
-   * @param Mode
-   */
-  static void OnTaskTypeData(DataField *Sender, DataField::DataAccessKind_t Mode);
-
-  /**
-   * copies values from ordered_task to form
-   * @param EventType 0 = Mouse Click, 1 = up/dn/left/right key
-   * @return true
-   */
-  static bool OnTabPreShow();
-
-  /**
-   * unzooms the task view if it is maximized
-   */
-  static void OnTabReClick();
+protected:
+  void InitView();
+  void RefreshView();
+  void ReadValues();
 };
 
 #endif

Data/Dialogs/pnlTaskEdit.xml
@@ -6,11 +6,11 @@
 
     <Label Name="frmSummary" Y="-90" Height="55" Border="0"/>
 
-    <Button Name="cmdEditTurnpoint" Caption="Edit Point" X="0" Y="-35" Width="60" Height="35" OnClick="pnlTaskEdit::OnEditTurnpointClicked"/>
-    <Button Name="cmdMakeFinish" Caption="Make Finish" X="60" Y="-35" Width="60" Height="35" OnClick="pnlTaskEdit::OnMakeFinish"/>
-    <SymbolButton Name="cmdDown" Caption="v" X="60" Y="-35" Width="60" Height="35" OnClick="pnlTaskEdit::OnMoveDownClicked"/>
-    <SymbolButton Name="cmdUp" Caption="^" X="120" Y="-35" Width="60" Height="35" OnClick="pnlTaskEdit::OnMoveUpClicked"/>
-    <Button Name="cmdNew" Caption="Clear All" X="180" Y="-35" Width="60" Height="35" OnClick="pnlTaskEdit::OnClearAllClicked"/>
+    <Button Name="cmdEditTurnpoint" Caption="Edit Point" X="0" Y="-35" Width="60" Height="35" OnClick="OnEditTurnpointClicked"/>
+    <Button Name="cmdMakeFinish" Caption="Make Finish" X="60" Y="-35" Width="60" Height="35" OnClick="OnMakeFinish"/>
+    <SymbolButton Name="cmdDown" Caption="v" X="60" Y="-35" Width="60" Height="35" OnClick="OnMoveDownClicked"/>
+    <SymbolButton Name="cmdUp" Caption="^" X="120" Y="-35" Width="60" Height="35" OnClick="OnMoveUpClicked"/>
+    <Button Name="cmdNew" Caption="Clear All" X="180" Y="-35" Width="60" Height="35" OnClick="OnClearAllClicked"/>
   </Panel>
   
   <Canvas Name="frmTaskView" X="0" Y="0" Width="61" Height="71" Border="0" OnPaint="dlgTaskManager::OnTaskPaint" />

Data/Dialogs/pnlTaskEdit_L.xml
@@ -5,11 +5,11 @@
 
   <Label Name="frmSummary" X="80" Y="150" Height="38" Border="0"/>
 
-  <Button Name="cmdEditTurnpoint" Caption="Edit Point" X="82" Y="188" Width="58" Height="35" OnClick="pnlTaskEdit::OnEditTurnpointClicked"/>
-  <Button Name="cmdMakeFinish" Caption="Make Finish" X="142" Y="188" Width="58" Height="35" OnClick="pnlTaskEdit::OnMakeFinish"/>
-  <SymbolButton Name="cmdDown" Caption="v" X="142" Y="188" Width="58" Height="35" OnClick="pnlTaskEdit::OnMoveDownClicked"/>
-  <SymbolButton Name="cmdUp" Caption="^" X="202" Y="188" Width="58" Height="35" OnClick="pnlTaskEdit::OnMoveUpClicked"/>
-  <Button Name="cmdNew" Caption="Clear All" X="262" Y="188" Width="57" Height="35" OnClick="pnlTaskEdit::OnClearAllClicked"/>
+  <Button Name="cmdEditTurnpoint" Caption="Edit Point" X="82" Y="188" Width="58" Height="35" OnClick="OnEditTurnpointClicked"/>
+  <Button Name="cmdMakeFinish" Caption="Make Finish" X="142" Y="188" Width="58" Height="35" OnClick="OnMakeFinish"/>
+  <SymbolButton Name="cmdDown" Caption="v" X="142" Y="188" Width="58" Height="35" OnClick="OnMoveDownClicked"/>
+  <SymbolButton Name="cmdUp" Caption="^" X="202" Y="188" Width="58" Height="35" OnClick="OnMoveUpClicked"/>
+  <Button Name="cmdNew" Caption="Clear All" X="262" Y="188" Width="57" Height="35" OnClick="OnClearAllClicked"/>
 
   <Canvas Name="frmTaskView" X="0" Y="0" Width="80" Height="53" Border="0" OnPaint="dlgTaskManager::OnTaskPaint" />
 </Panel>

Data/Dialogs/pnlTaskManagerClose.xml
@@ -2,10 +2,10 @@
 
 <Panel Name="frmTaskClose" Border="0">
   <Panel Name="TaskCloseTabOffset" Border="0" X="0" Y="76">
-    <Button Name="cmdClose" Caption="Fly" X="60" Y="40" Width="120" Height="40" OnClick="pnlTaskManagerClose::OnCloseClicked"/>
+    <Button Name="cmdClose" Caption="Fly" X="60" Y="40" Width="120" Height="40" OnClick="OnCloseClicked"/>
     <Label Name="frmStatus" X="40" Y="97" Width="160" Height="20" Border="0"/>
   
-    <Button Name="cmdRevert" Caption="Revert Changes" X="60" Y="117" Width="120" Height="35" OnClick="pnlTaskManagerClose::OnRevertClicked"/>
+    <Button Name="cmdRevert" Caption="Revert Changes" X="60" Y="117" Width="120" Height="35" OnClick="OnRevertClicked"/>
   </Panel>
   <Canvas Name="frmTaskViewClose" X="0" Y="0" Width="61" Height="71" Border="0" OnPaint="dlgTaskManager::OnTaskPaint" />
   

Data/Dialogs/pnlTaskManagerClose_L.xml
@@ -3,9 +3,9 @@
 <Panel Name="frmTaskClose" Border="0">
   <Panel Name="frmProperties" X="100" Y="0">
 
-    <Button Name="cmdClose" Caption="Fly" X="30" Y="25" Width="120" Height="40" OnClick="pnlTaskManagerClose::OnCloseClicked"/>
+    <Button Name="cmdClose" Caption="Fly" X="30" Y="25" Width="120" Height="40" OnClick="OnCloseClicked"/>
     <Label Name="frmStatus" X="10" Y="86" Width="160" Height="20" Border="0"/>
-    <Button Name="cmdRevert" Caption="Revert Changes" X="30" Y="106" Width="120" Height="35" OnClick="pnlTaskManagerClose::OnRevertClicked"/>
+    <Button Name="cmdRevert" Caption="Revert Changes" X="30" Y="106" Width="120" Height="35" OnClick="OnRevertClicked"/>
   </Panel>
   <Canvas Name="frmTaskViewClose" X="0" Y="0" Width="80" Height="53" Border="0" OnPaint="dlgTaskManager::OnTaskPaint" />
 

Data/Dialogs/pnlTaskProperties.xml
@@ -34,11 +34,11 @@
 
     <Edit Name="prpFAIFinishHeight" Caption="FAI start / finish rules" X="2" Width="-2" Height="22" CaptionWidth="140"
       Help="If enabled, has no max start height or max start speed and requires the minimum height above ground for finish to be greater than 1000m below the start height.">
-      <DataField DataType="boolean" OnDataAccess="pnlTaskProperties::OnFAIFinishHeightData"/>
+      <DataField DataType="boolean" OnDataAccess="OnFAIFinishHeightData"/>
     </Edit>
     <Edit Name="prpTaskType" Caption="Task type" X="2" Width="-2" Height="22" CaptionWidth="140"
       Help="Sets the behaviour for the current task.">
-      <DataField DataType="enum" OnDataAccess="pnlTaskProperties::OnTaskTypeData"/>
+      <DataField DataType="enum" OnDataAccess="OnTaskTypeData"/>
     </Edit>
 
 

Data/Dialogs/pnlTaskProperties_L.xml
@@ -35,11 +35,11 @@
 
   <Edit Name="prpFAIFinishHeight" Caption="FAI start / finish rules" X="2" Width="-2" Height="22" CaptionWidth="140"
     Help="If enabled, has no max start height or max start speed and requires the minimum height above ground for finish to be greater than 1000m below the start height.">
-    <DataField DataType="boolean" OnDataAccess="pnlTaskProperties::OnFAIFinishHeightData"/>
+    <DataField DataType="boolean" OnDataAccess="OnFAIFinishHeightData"/>
   </Edit>
   <Edit Name="prpTaskType" Caption="Task type" X="2" Width="-2" Height="22" CaptionWidth="140"
     Help="Sets the behaviour for the current task.">
-    <DataField DataType="enum" OnDataAccess="pnlTaskProperties::OnTaskTypeData"/>
+    <DataField DataType="enum" OnDataAccess="OnTaskTypeData"/>
   </Edit>  
   
   </Panel>

src/Dialogs/TaskManager/TaskClosePanel.cpp
@@ -24,6 +24,7 @@ Copyright_License {
 #include "TaskClosePanel.hpp"
 #include "Internal.hpp"
 #include "Dialogs/XML.hpp"
+#include "Dialogs/dlgTools.h"
 #include "Form/Button.hpp"
 #include "Form/Draw.hpp"
 #include "Form/Frame.hpp"
@@ -37,15 +38,12 @@ Copyright_License {
 #include <assert.h>
 #include <stdio.h> //debug
 
-static WndForm* wf = NULL;
-static WndFrame* wStatus = NULL;
-static WndButton* cmdRevert = NULL;
-static WndButton* cmdClose = NULL;
-static bool* task_modified = NULL;
-static WndOwnerDrawFrame* wTaskView = NULL;
+/** XXX this hack is needed because the form callbacks don't get a
+    context pointer - please refactor! */
+static TaskClosePanel *instance;
 
-static void
-RefreshStatus()
+void
+TaskClosePanel::RefreshStatus()
 {
   wStatus->SetText(*task_modified ?
                    _("Task has been modified") : _("Task unchanged"));
@@ -53,21 +51,55 @@ RefreshStatus()
   cmdRevert->set_visible(*task_modified);
 }
 
-void
-pnlTaskManagerClose::OnCloseClicked(gcc_unused WndButton &Sender)
+static void
+OnCloseClicked(gcc_unused WndButton &Sender)
 {
   dlgTaskManager::OnClose();
 }
 
-void
-pnlTaskManagerClose::OnRevertClicked(gcc_unused WndButton &Sender)
+static void
+OnRevertClicked(gcc_unused WndButton &Sender)
 {
   dlgTaskManager::RevertTask();
-  RefreshStatus();
+  instance->RefreshStatus();
+}
+
+static gcc_constexpr_data CallBackTableEntry task_close_callbacks[] = {
+  DeclareCallBackEntry(dlgTaskManager::OnTaskPaint),
+
+  DeclareCallBackEntry(OnCloseClicked),
+  DeclareCallBackEntry(OnRevertClicked),
+
+  DeclareCallBackEntry(NULL)
+};
+
+void
+TaskClosePanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
+{
+  instance = this;
+
+  LoadWindow(task_close_callbacks, parent,
+             Layout::landscape
+             ? _T("IDR_XML_TASKMANAGERCLOSE_L") : _T("IDR_XML_TASKMANAGERCLOSE"));
+
+  wTaskView = (WndOwnerDrawFrame *)form.FindByName(_T("frmTaskViewClose"));
+  assert(wTaskView != NULL);
+  wTaskView->SetOnMouseDownNotify(dlgTaskManager::OnTaskViewClick);
+
+  wStatus = (WndFrame *)form.FindByName(_T("frmStatus"));
+  assert(wStatus);
+
+  cmdRevert = (WndButton *)form.FindByName(_T("cmdRevert"));
+  assert(cmdRevert);
+
+  cmdClose = (WndButton *)form.FindByName(_T("cmdClose"));
+  assert(cmdClose);
+
+  wStatus->SetAlignCenter();
 }
 
 bool
-pnlTaskManagerClose::OnTabClick()
+TaskClosePanel::Click()
 {
   if (!(*task_modified)) {
     dlgTaskManager::OnClose();
@@ -78,54 +110,16 @@ pnlTaskManagerClose::OnTabClick()
 }
 
 void
-pnlTaskManagerClose::OnTabReClick()
+TaskClosePanel::ReClick()
 {
   dlgTaskManager::OnClose();
 }
 
-bool
-pnlTaskManagerClose::OnTabPreShow()
+void
+TaskClosePanel::Show(const PixelRect &rc)
 {
   dlgTaskManager::TaskViewRestore(wTaskView);
-
   RefreshStatus();
-  return true;
-}
 
-Window*
-pnlTaskManagerClose::Load(SingleWindow &parent, TabBarControl* wTabBar,
-                          WndForm* _wf, OrderedTask** task,
-                          bool* _task_modified)
-{
-  assert(wTabBar);
-
-  assert(_wf);
-  wf = _wf;
-
-  assert(_task_modified);
-  task_modified = _task_modified;
-
-  Window *wTaskManagerClose =
-      LoadWindow(dlgTaskManager::CallBackTable, wf,
-                 wTabBar->GetClientAreaWindow(),
-                 Layout::landscape ?
-                     _T("IDR_XML_TASKMANAGERCLOSE_L") :
-                     _T("IDR_XML_TASKMANAGERCLOSE"));
-  assert(wTaskManagerClose);
-
-  wTaskView = (WndOwnerDrawFrame*)wf->FindByName(_T("frmTaskViewClose"));
-  assert(wTaskView != NULL);
-  wTaskView->SetOnMouseDownNotify(dlgTaskManager::OnTaskViewClick);
-
-  wStatus = (WndFrame *)wf->FindByName(_T("frmStatus"));
-  assert(wStatus);
-
-  cmdRevert = (WndButton *)wf->FindByName(_T("cmdRevert"));
-  assert(cmdRevert);
-
-  cmdClose = (WndButton *)wf->FindByName(_T("cmdClose"));
-  assert(cmdClose);
-
-  wStatus->SetAlignCenter();
-  return wTaskManagerClose;
+  XMLWidget::Show(rc);
 }

commit 5af2adf13e7ff1a8937b2107db5c90967085d254

Dialogs/TaskManager/TaskPropertiesPanel: Initialize ordered_task
Fixes regression introduced with commit
30067938cdbce7fbcd20f2a8906321a95e86a35e (see ticket #1585)

src/Dialogs/TaskManager/TaskPropertiesPanel.hpp
@@ -42,7 +42,8 @@ class TaskPropertiesPanel : public XMLWidget {
 
 public:
   TaskPropertiesPanel(OrderedTask **_active_task, bool *_task_modified)
-    :ordered_task_pointer(_active_task), task_changed(_task_modified) {}
+    :ordered_task_pointer(_active_task), ordered_task(*ordered_task_pointer),
+     task_changed(_task_modified) {}
 
   void OnFAIFinishHeightChange(DataFieldBoolean &df);
   void OnTaskTypeChange(DataFieldEnum &df);