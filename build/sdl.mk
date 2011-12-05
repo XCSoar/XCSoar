f3bb9cfd9e27ed67a5cc9caed8c3a63fbfd191bc
Max Kellermann <max@duempel.org>
2011-12-05 09:15:53 +0100

    Makefile: add zlib to SDL_LDLIBS on Mac OS X
    Needed by libfreetype.  Since we're doing manual dependencies on OS X
    (for static linking), we have to add this library manually.

--- build/sdl.mk
+++ build/sdl.mk
@@ -37,6 +37,7 @@ ifeq ($(shell uname -s),Darwin)
 SDL_LDLIBS := $(filter-out -l%,$(SDL_LDLIBS))
 SDL_LDLIBS += /opt/local/lib/libSDL_ttf.a /opt/local/lib/libfreetype.a
 SDL_LDLIBS += /opt/local/lib/libbz2.a
+SDL_LDLIBS += /opt/local/lib/libz.a
 else
 SDL_LDLIBS += -lSDL_ttf
 endif
