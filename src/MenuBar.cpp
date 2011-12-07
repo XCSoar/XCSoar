XCSoar/XCSoar

commit 51dc2bb2234a1911caa5657a4717f19c29e09b46

MenuBar/Button: store event id, not button index
Call InputEvents::ProcessEvent() instead of
InputEvents::ProcessButton().  This eliminates the ProcessButton()
function, which was kind of redundant.  And it allows us to configure
the menu with contents that are not in InputConfig.

src/MenuBar.cpp
@@ -96,7 +96,8 @@ GetButtonPosition(unsigned i, PixelRect rc,
 bool
 MenuBar::Button::on_clicked()
 {
-  InputEvents::processButton(index);
+  if (event > 0)
+    InputEvents::ProcessEvent(event);
   return true;
 }
 
@@ -145,14 +146,16 @@ MenuBar::SetFont(const Font &font)
 }
 
 void
-MenuBar::ShowButton(unsigned i, bool enabled, const TCHAR *text)
+MenuBar::ShowButton(unsigned i, bool enabled, const TCHAR *text,
+                    unsigned event)
 {
   assert(i < MAX_BUTTONS);
 
-  ButtonWindow &button = buttons[i];
+  Button &button = buttons[i];
 
   button.set_text(text);
-  button.set_enabled(enabled);
+  button.set_enabled(enabled && event > 0);
+  button.SetEvent(event);
   button.show_on_top();
 }
 

src/ButtonLabel.cpp
@@ -52,7 +52,7 @@ ButtonLabel::Destroy()
 }
 
 void
-ButtonLabel::SetLabelText(unsigned index, const TCHAR *text)
+ButtonLabel::SetLabelText(unsigned index, const TCHAR *text, unsigned event)
 {
   const TCHAR *dollar;
 
@@ -60,7 +60,7 @@ ButtonLabel::SetLabelText(unsigned index, const TCHAR *text)
     bar->HideButton(index);
   } else if ((dollar = _tcschr(text, '$')) == NULL) {
     /* no macro, we can just translate the text */
-    bar->ShowButton(index, true, gettext(text));
+    bar->ShowButton(index, true, gettext(text), event);
   } else {
     const TCHAR *macros = dollar;
     /* backtrack until the first non-whitespace character, because we
@@ -90,7 +90,7 @@ ButtonLabel::SetLabelText(unsigned index, const TCHAR *text)
       _tcscpy(buffer, translated);
       _tcscat(buffer, s + (macros - text));
 
-      bar->ShowButton(index, !greyed, buffer);
+      bar->ShowButton(index, !greyed, buffer, event);
     }
   }
 }
@@ -102,7 +102,7 @@ ButtonLabel::Set(const Menu &menu, bool full)
     const MenuItem &item = menu[i];
 
     if (full || item.IsDynamic())
-      SetLabelText(i, item.label);
+      SetLabelText(i, item.label, item.event);
   }
 }
 

src/ButtonLabel.hpp
@@ -34,7 +34,7 @@ namespace ButtonLabel {
   void CreateButtonLabels(ContainerWindow &parent);
   void SetFont(const Font &Font);
   void Destroy();
-  void SetLabelText(unsigned i, const TCHAR *text);
+  void SetLabelText(unsigned i, const TCHAR *text, unsigned event);
   bool IsEnabled(unsigned i);
 
   bool ExpandMacros(const TCHAR *In, TCHAR *OutBuffer, size_t Size);

src/InputEvents.cpp
@@ -193,25 +193,6 @@ InputEvents::ProcessEvent(unsigned event_id)
     drawButtons(lastMode);
 }
 
-// Input is a via the user touching the label on a touch screen / mouse
-bool
-InputEvents::processButton(unsigned bindex)
-{
-  if (!globalRunningEvent.Test())
-    return false;
-
-  if (bindex >= Menu::MAX_ITEMS)
-    return false;
-
-  Mode lastMode = getModeID();
-  const MenuItem &item = input_config.GetMenuItem(lastMode, bindex);
-  if (!item.IsDefined())
-    return false;
-
-  ProcessEvent(item.event);
-  return true;
-}
-
 unsigned
 InputEvents::key_to_event(Mode mode, unsigned key_code)
 {

src/InputEvents.hpp
@@ -74,8 +74,6 @@ namespace InputEvents
   bool processKey(unsigned key);
   bool processGesture(const TCHAR *data);
 
-  bool processButton(unsigned bindex);
-
   bool processNmea_real(unsigned key);
   bool processGlideComputer_real(unsigned gce_id);
 

src/MenuBar.hpp
@@ -42,20 +42,14 @@ public:
 
 protected:
   class Button : public ButtonWindow {
-    unsigned index;
+    unsigned event;
 
   public:
-    virtual bool on_clicked();
-
-  public:
-    void set(ContainerWindow &parent, const TCHAR *text, unsigned _index,
-             PixelScalar left, PixelScalar top,
-             UPixelScalar width, UPixelScalar height,
-             ButtonWindowStyle style=ButtonWindowStyle()) {
-      index = _index;
-
-      ButtonWindow::set(parent, text, left, top, width, height, style);
+    void SetEvent(unsigned _event) {
+      event = _event;
     }
+
+    virtual bool on_clicked();
 
 #ifdef USE_GDI
   protected:
@@ -71,7 +65,8 @@ public:
 
 public:
   void SetFont(const Font &font);
-  void ShowButton(unsigned i, bool enabled, const TCHAR *text);
+  void ShowButton(unsigned i, bool enabled, const TCHAR *text,
+                  unsigned event);
   void HideButton(unsigned i);
 
   bool IsButtonEnabled(unsigned i) const {

commit ba6af705cdfb5c8ef7fb05566dbb48bd14fae781

MenuBar: don't pass button index to Button::set()
This fixes a major regression caused by 51dc2bb2: by removing
Button::set(), it would pass the index to ButtonWindow::set(), and the
COMMAND_BOUNCE_ID code got disabled, breaking all menu buttons on
WIN32.  This breakage went unnoticed for two days, which shows that
all developers have migrated away from Windows already ..

src/MenuBar.cpp
@@ -133,7 +133,7 @@ MenuBar::MenuBar(ContainerWindow &parent)
     PixelScalar x, y;
     UPixelScalar xsize, ysize;
     GetButtonPosition(i, rc, &x, &y, &xsize, &ysize);
-    buttons[i].set(parent, _T(""), i, x, y, xsize, ysize,
+    buttons[i].set(parent, _T(""), x, y, xsize, ysize,
                    style);
   }
 }