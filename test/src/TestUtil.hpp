1496f3ce798ff67c66559d328fbe5ad876bbd92b
Max Kellermann <max@duempel.org>
2011-12-17 12:26:39 +0100

    test/TestUtil: add macro ACCURACY

--- test/src/TestUtil.hpp
+++ test/src/TestUtil.hpp
@@ -26,6 +26,14 @@
 #include "Engine/Navigation/GeoPoint.hpp"
 #include "Math/Angle.hpp"
 
+#ifndef ACCURACY
+/**
+ * Define this macro before including TestUtil.hpp to compare with a
+ * different accuracy.
+ */
+#define ACCURACY 10000
+#endif
+
 extern "C" {
 #include "tap.h"
 }
@@ -35,7 +43,7 @@ int verbose;
 static inline bool
 is_zero(const fixed value)
 {
-  return (long)fabs(value * 10000) == 0;
+  return (long)fabs(value * ACCURACY) == 0;
 }
 
 static inline bool
