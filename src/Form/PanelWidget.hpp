01c69b3864694813b4120edb4cace65675e2d7cf
Rob Dunning <rob@raspberryridgesheepfarm.com>
2011-12-23 14:55:27 -0500

    PanelWidget: new class
    Is a widget that creates and deletes a
    panel in the Prepare() and Unprepare() virtual
    methods

---
+++ src/Form/PanelWidget.hpp
@@ -0,0 +1,44 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2011 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_PANEL_WIDGET_HPP
+#define XCSOAR_PANEL_WIDGET_HPP
+
+#include "Form/WindowWidget.hpp"
+
+
+/**
+ * Class to be inherited.
+ * Window widget with a panel the size of the rect passed to Prepare().
+ * Panel is automatically created by Prepare() and deleted in UnPrepare().
+ * Panel is accessed via WindowWidget's GetWindow().
+ */
+class PanelWidget : public WindowWidget {
+protected:
+
+public:
+  virtual void Prepare(ContainerWindow &parent, const PixelRect &rc);
+  virtual void Unprepare();
+};
+
+#endif
