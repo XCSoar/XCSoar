7dc7f16af6dcc87fe9e793d0a7c7c65834019807
Max Kellermann <max@duempel.org>
2011-12-23 21:41:13 +0100

    Util/ZeroFinder: split assertion
    Make it easier to debug.

--- src/Engine/Util/ZeroFinder.cpp
+++ src/Engine/Util/ZeroFinder.cpp
@@ -245,7 +245,8 @@ fixed ZeroFinder::find_zero_actual(const fixed xstart) {
       fc = fa;
     }
 
-    assert((b <= xmax) && (b >= xmin));
+    assert(b >= xmin);
+    assert(b <= xmax);
   }
 }
 
