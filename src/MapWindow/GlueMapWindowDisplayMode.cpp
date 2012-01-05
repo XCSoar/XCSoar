XCSoar/XCSoar

commit f989ed4caf5beef8c904229e47894bde053c43de

GlueMapWindow: use std::array in OffsetHistory

src/MapWindow/GlueMapWindowDisplayMode.cpp
@@ -31,14 +31,10 @@ ZoomClimb_t::ZoomClimb_t():
   ClimbScale(fixed_one / 2),
   last_isclimb(false) {}
 
-
-const RasterPoint OffsetHistory::zeroPoint = {0, 0};
-
 void
 OffsetHistory::reset()
 {
-  for (unsigned int i = 0; i < historySize; i++)
-    offsets[i] = zeroPoint;
+  offsets.fill(RasterPoint{0, 0});
 }
 
 void
@@ -48,7 +44,7 @@ OffsetHistory::add(PixelScalar x, PixelScalar y)
   point.x = x;
   point.y = y;
   offsets[pos] = point;
-  pos = (pos + 1) % historySize;
+  pos = (pos + 1) % offsets.size();
 }
 
 RasterPoint
@@ -57,14 +53,14 @@ OffsetHistory::average() const
   int x = 0;
   int y = 0;
 
-  for (unsigned int i = 0; i < historySize; i++) {
-    x += offsets[i].x;
-    y += offsets[i].y;
+  for (auto i = offsets.begin(), end = offsets.end(); i != end; ++i) {
+    x += i->x;
+    y += i->y;
   }
 
   RasterPoint avg;
-  avg.x = x / (int) historySize;
-  avg.y = y / (int) historySize;
+  avg.x = x / offsets.size();
+  avg.y = y / offsets.size();
 
   return avg;
 }
@@ -284,7 +280,7 @@ GlueMapWindow::UpdateProjection()
   if (GetDisplayMode() == DM_CIRCLING || !IsNearSelf())
     visible_projection.SetScreenOrigin(center.x, center.y);
   else if (settings_map.cruise_orientation == NORTHUP) {
-    RasterPoint offset = OffsetHistory::zeroPoint;
+    RasterPoint offset{0, 0};
     if (settings_map.glider_screen_position != 50 &&
         settings_map.map_shift_bias != MAP_SHIFT_BIAS_NONE) {
       fixed x = fixed_zero;

src/MapWindow/GlueMapWindow.hpp
@@ -33,6 +33,8 @@ Copyright_License {
 #include "Screen/Features.hpp"
 #include "DisplayMode.hpp"
 
+#include <array>
+
 struct Look;
 class Logger;
 class SingleWindow;
@@ -48,9 +50,8 @@ struct ZoomClimb_t
 
 
 class OffsetHistory {
-  static const unsigned int historySize = 30;
   unsigned int pos;
-  RasterPoint offsets[historySize];
+  std::array<RasterPoint, 30> offsets;
 
   friend class GlueMapWindow;
 
@@ -60,8 +61,6 @@ protected:
   void add(PixelScalar x, PixelScalar y);
   void add(const RasterPoint &p) { add(p.x, p.y); }
   RasterPoint average() const;
-
-  static const RasterPoint zeroPoint;
 };
 
 

commit 00bacc49c6304f4d480076f958f7b36b92a80f13

MapWindow/GlueMapWindowDisplayMode: Fix OffsetHistory average calculation regression
Regression introduced in commit f989ed4caf5beef8c904229e47894bde053c43de

src/MapWindow/GlueMapWindowDisplayMode.cpp
@@ -56,8 +56,8 @@ OffsetHistory::average() const
   }
 
   RasterPoint avg;
-  avg.x = x / offsets.size();
-  avg.y = y / offsets.size();
+  avg.x = x / (int) offsets.size();
+  avg.y = y / (int) offsets.size();
 
   return avg;
 }