XCSoar/XCSoar

commit 227cbefb08650f018bc81e63ac4f95bc2c21610e

DistanceStat: add method IsDefined()
Make the check mandatory, protect with an assertion.

src/InfoBoxes/Content/Task.cpp
@@ -315,7 +315,8 @@ InfoBoxContentFinalDistance::Update(InfoBoxWindow &infobox)
   const TaskStats &task_stats = XCSoarInterface::Calculated().task_stats;
 
   if (!task_stats.task_valid ||
-      !task_stats.current_leg.solution_remaining.IsDefined()) {
+      !task_stats.current_leg.solution_remaining.IsDefined() ||
+      !task_stats.total.remaining.IsDefined()) {
     infobox.SetInvalid();
     return;
   }
@@ -415,7 +416,7 @@ void
 InfoBoxContentTaskSpeed::Update(InfoBoxWindow &infobox)
 {
   const TaskStats &task_stats = XCSoarInterface::Calculated().task_stats;
-  if (!task_stats.task_valid) {
+  if (!task_stats.task_valid || !task_stats.total.travelled.IsDefined()) {
     infobox.SetInvalid();
     return;
   }
@@ -432,7 +433,8 @@ void
 InfoBoxContentTaskSpeedAchieved::Update(InfoBoxWindow &infobox)
 {
   const TaskStats &task_stats = XCSoarInterface::Calculated().task_stats;
-  if (!task_stats.task_valid) {
+  if (!task_stats.task_valid ||
+      !task_stats.total.remaining_effective.IsDefined()) {
     infobox.SetInvalid();
     return;
   }
@@ -449,7 +451,7 @@ void
 InfoBoxContentTaskSpeedInstant::Update(InfoBoxWindow &infobox)
 {
   const TaskStats &task_stats = XCSoarInterface::Calculated().task_stats;
-  if (!task_stats.task_valid) {
+  if (!task_stats.task_valid || !task_stats.IsPirkerSpeedAvailable()) {
     infobox.SetInvalid();
     return;
   }
@@ -633,7 +635,8 @@ InfoBoxContentTaskAADistance::Update(InfoBoxWindow &infobox)
   const CommonStats &common_stats = XCSoarInterface::Calculated().common_stats;
 
   if (!common_stats.ordered_has_targets ||
-      !task_stats.task_valid) {
+      !task_stats.task_valid ||
+      !task_stats.total.planned.IsDefined()) {
     infobox.SetInvalid();
     return;
   }
@@ -766,7 +769,8 @@ InfoBoxContentNextETEVMG::Update(InfoBoxWindow &infobox)
   const NMEAInfo &basic = CommonInterface::Basic();
   const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
 
-  if (!basic.ground_speed_available) {
+  if (!basic.ground_speed_available || task_stats.task_valid ||
+      !task_stats.current_leg.remaining.IsDefined()) {
     infobox.SetInvalid();
     return;
   }
@@ -796,7 +800,8 @@ InfoBoxContentFinalETEVMG::Update(InfoBoxWindow &infobox)
   const NMEAInfo &basic = CommonInterface::Basic();
   const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
 
-  if (!basic.ground_speed_available) {
+  if (!basic.ground_speed_available || task_stats.task_valid ||
+      task_stats.total.remaining.IsDefined()) {
     infobox.SetInvalid();
     return;
   }

src/Computer/GlideComputerStats.cpp
@@ -81,8 +81,10 @@ GlideComputerStats::DoLogging()
                                    Calculated().terrain_altitude);
     flightstats.AddAltitude(Calculated().flight.flight_time,
                             Basic().NavAltitude);
-    flightstats.AddTaskSpeed(Calculated().flight.flight_time,
-                             Calculated().task_stats.get_pirker_speed());
+
+    if (Calculated().task_stats.IsPirkerSpeedAvailable())
+      flightstats.AddTaskSpeed(Calculated().flight.flight_time,
+                               Calculated().task_stats.get_pirker_speed());
   }
 
   return true;

src/Dialogs/dlgStatus.cpp
@@ -419,33 +419,42 @@ UpdateValuesTask(void)
   Units::TimeToTextHHMMSigned(Temp, task_stats.total.time_remaining);
   wp->SetText(Temp);
 
-  wp = (WndProperty*)wf->FindByName(_T("prpTaskDistance"));
-  assert(wp != NULL);
-  _stprintf(Temp, _T("%.0f %s"), (double)Units::ToUserDistance(
-            task_stats.total.planned.get_distance()),
-            Units::GetDistanceName());
-  wp->SetText(Temp);
+  if (task_stats.total.planned.IsDefined()) {
+    wp = (WndProperty*)wf->FindByName(_T("prpTaskDistance"));
+    assert(wp != NULL);
+    _stprintf(Temp, _T("%.0f %s"),
+              (double)Units::ToUserDistance(task_stats.total.planned.get_distance()),
+              Units::GetDistanceName());
+    wp->SetText(Temp);
+  }
 
-  wp = (WndProperty*)wf->FindByName(_T("prpRemainingDistance"));
-  assert(wp != NULL);
-  _stprintf(Temp, _T("%.0f %s"), (double)Units::ToUserDistance(
-            task_stats.total.remaining.get_distance()),
-            Units::GetDistanceName());
-  wp->SetText(Temp);
+  if (task_stats.total.remaining.IsDefined()) {
+    wp = (WndProperty*)wf->FindByName(_T("prpRemainingDistance"));
+    assert(wp != NULL);
+    _stprintf(Temp, _T("%.0f %s"),
+              (double)Units::ToUserDistance(task_stats.total.remaining.get_distance()),
+              Units::GetDistanceName());
+    wp->SetText(Temp);
+  }
 
-  wp = (WndProperty*)wf->FindByName(_T("prpEstimatedSpeed"));
-  assert(wp != NULL);
-  _stprintf(Temp, _T("%.0f %s"), (double)Units::ToUserTaskSpeed(
-            task_stats.total.planned.get_speed()),
-            Units::GetTaskSpeedName());
-  wp->SetText(Temp);
+  if (task_stats.total.planned.IsDefined()) {
+    wp = (WndProperty*)wf->FindByName(_T("prpEstimatedSpeed"));
+    assert(wp != NULL);
+    if (task_stats.total.planned.IsDefined())
+      _stprintf(Temp, _T("%.0f %s"),
+                (double)Units::ToUserTaskSpeed(task_stats.total.planned.get_speed()),
+                Units::GetTaskSpeedName());
+    wp->SetText(Temp);
+  }
 
-  wp = (WndProperty*)wf->FindByName(_T("prpAverageSpeed"));
-  assert(wp != NULL);
-  _stprintf(Temp, _T("%.0f %s"), (double)Units::ToUserTaskSpeed(
-            task_stats.total.travelled.get_speed()),
-            Units::GetTaskSpeedName());
-  wp->SetText(Temp);
+  if (task_stats.total.travelled.IsDefined()) {
+    wp = (WndProperty*)wf->FindByName(_T("prpAverageSpeed"));
+    assert(wp != NULL);
+    _stprintf(Temp, _T("%.0f %s"),
+              (double)Units::ToUserTaskSpeed(task_stats.total.travelled.get_speed()),
+              Units::GetTaskSpeedName());
+    wp->SetText(Temp);
+  }
 }
 
 static void

src/Dialogs/dlgTarget.cpp
@@ -285,10 +285,13 @@ RefreshCalculator()
   }
 
   const ElementStat &total = CommonInterface::Calculated().task_stats.total;
-  LoadFormProperty(*wf, _T("prpSpeedRemaining"), ugTaskSpeed,
-                   total.remaining_effective.get_speed());
-  LoadOptionalFormProperty(*wf, _T("prpSpeedAchieved"), ugTaskSpeed,
-                           total.travelled.get_speed());
+  if (total.remaining_effective.IsDefined())
+    LoadFormProperty(*wf, _T("prpSpeedRemaining"), ugTaskSpeed,
+                     total.remaining_effective.get_speed());
+
+  if (total.travelled.IsDefined())
+    LoadOptionalFormProperty(*wf, _T("prpSpeedAchieved"), ugTaskSpeed,
+                             total.travelled.get_speed());
 }
 
 static void

src/Dialogs/pnlTaskCalculator.cpp
@@ -137,8 +137,9 @@ RefreshCalculator(void)
   }
   */
 
-  wp = (WndProperty*)wf->FindByName(_T("prpSpeedRemaining"));
-  if (wp) {
+  if (task_stats.total.remaining_effective.IsDefined()) {
+    wp = (WndProperty*)wf->FindByName(_T("prpSpeedRemaining"));
+    assert(wp != NULL);
     DataFieldFloat &df = *(DataFieldFloat *)wp->GetDataField();
     df.SetAsFloat(Units::ToUserTaskSpeed(
         task_stats.total.remaining_effective.get_speed()));
@@ -146,8 +147,9 @@ RefreshCalculator(void)
     wp->RefreshDisplay();
   }
 
-  wp = (WndProperty*)wf->FindByName(_T("prpSpeedAchieved"));
-  if (wp) {
+  if (task_stats.total.travelled.IsDefined()) {
+    wp = (WndProperty*)wf->FindByName(_T("prpSpeedAchieved"));
+    assert(wp != NULL);
     DataFieldFloat &df = *(DataFieldFloat *)wp->GetDataField();
     df.SetAsFloat(Units::ToUserTaskSpeed(
         task_stats.total.travelled.get_speed()));

src/Engine/Task/TaskManager.cpp
@@ -137,7 +137,8 @@ TaskManager::update_common_stats_times(const AircraftState &state)
         max(fixed_zero, task_ordered.get_ordered_task_behaviour().aat_min_time -
                         task_stats.total.time_elapsed);
 
-    if (positive(common_stats.aat_time_remaining))
+    if (task_stats.total.remaining.IsDefined() &&
+        positive(common_stats.aat_time_remaining))
       common_stats.aat_speed_remaining =
           fixed(task_stats.total.remaining.get_distance()) /
           common_stats.aat_time_remaining;

src/Engine/Task/TaskStats/DistanceStat.cpp
@@ -31,7 +31,7 @@ DistanceStatComputer::DistanceStatComputer(DistanceStat &_data,
 void
 DistanceStatComputer::calc_incremental_speed(const fixed dt)
 {
-  if ((dt + fixed_half >= fixed_one) && positive(data.distance)) {
+  if ((dt + fixed_half >= fixed_one) && data.IsDefined()) {
     if (av_dist.update(data.distance)) {
       const fixed d_av = av_dist.average() / N_AV;
       av_dist.reset();
@@ -43,7 +43,7 @@ DistanceStatComputer::calc_incremental_speed(const fixed dt)
       }
       data.speed_incremental = (is_positive ? -v_f : v_f);
     }
-  } else if (!positive(dt) || !positive(data.distance)) {
+  } else if (!positive(dt) || !data.IsDefined()) {
     reset_incremental_speed();
   }
 }
@@ -51,8 +51,11 @@ DistanceStatComputer::calc_incremental_speed(const fixed dt)
 void
 DistanceStatComputer::reset_incremental_speed()
 {
-  df.reset(fixed(data.distance), (is_positive ? -1 : 1) * fixed(data.speed));
-  v_lpf.reset((is_positive ? -1 : 1) * fixed(data.speed));
+  fixed distance = data.IsDefined() ? data.get_distance() : fixed_zero;
+  fixed speed = data.IsDefined() ? data.get_speed() : fixed_zero;
+
+  df.reset(distance, (is_positive ? -1 : 1) * speed);
+  v_lpf.reset((is_positive ? -1 : 1) * speed);
   data.speed_incremental = fixed_zero; // data.speed;
   av_dist.reset();
 }
@@ -60,8 +63,8 @@ DistanceStatComputer::reset_incremental_speed()
 void
 DistanceStatComputer::calc_speed(fixed time)
 {
-  if (positive(time))
-    data.speed = fixed(data.distance) / time;
+  if (positive(time) && data.IsDefined())
+    data.speed = data.get_distance() / time;
   else
     data.speed = fixed_zero;
 }

src/Engine/Task/TaskStats/DistanceStat.hpp
@@ -32,6 +32,8 @@
 #include "Util/AvFilter.hpp"
 #include "Util/DiffFilter.hpp"
 
+#include <assert.h>
+
 /**
  * Simple distance statistics with derived values (speed, incremental speed)
  * Incremental speeds track the short-term variation of distance with time,
@@ -51,9 +53,13 @@ protected:
 
 public:
   void Reset() {
-    distance = fixed_zero;
+    distance = fixed_minus_one;
     speed = fixed_zero;
     speed_incremental = fixed_zero;
+  }
+
+  bool IsDefined() const {
+    return !negative(distance);
   }
 
   /**
@@ -71,6 +77,8 @@ public:
    * @return Distance value (m)
    */
   fixed get_distance() const {
+    assert(IsDefined());
+
     return distance;
   }
 
@@ -80,6 +88,8 @@ public:
    * @return Speed (m/s)
    */
   fixed get_speed() const {
+    assert(IsDefined());
+
     return speed;
   }
 
@@ -90,6 +100,8 @@ public:
    * @return Speed incremental (m/s)
    */
   fixed get_speed_incremental() const {
+    assert(IsDefined());
+
     return speed_incremental;
   }
 

commit 07377ec634e2d1e0960e102120caf23bce5bff4c

InfoBoxes/Content/Task: Fix "Fin ETE VMG" and "WP ETE VMG" infoboxes
Fixes trac issue #1670.
Regression introduced with commit 227cbefb08650f018bc81e63ac4f95bc2c21610e.

src/InfoBoxes/Content/Task.cpp
@@ -766,7 +766,7 @@ InfoBoxContentNextETEVMG::Update(InfoBoxWindow &infobox)
   const NMEAInfo &basic = CommonInterface::Basic();
   const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
 
-  if (!basic.ground_speed_available || task_stats.task_valid ||
+  if (!basic.ground_speed_available || !task_stats.task_valid ||
       !task_stats.current_leg.remaining.IsDefined()) {
     infobox.SetInvalid();
     return;
@@ -797,8 +797,8 @@ InfoBoxContentFinalETEVMG::Update(InfoBoxWindow &infobox)
   const NMEAInfo &basic = CommonInterface::Basic();
   const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
 
-  if (!basic.ground_speed_available || task_stats.task_valid ||
-      task_stats.total.remaining.IsDefined()) {
+  if (!basic.ground_speed_available || !task_stats.task_valid ||
+      !task_stats.total.remaining.IsDefined()) {
     infobox.SetInvalid();
     return;
   }