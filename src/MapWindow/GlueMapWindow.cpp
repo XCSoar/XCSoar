XCSoar/XCSoar

commit d140ca36f14b2a0ab07cb0bd5d0a034bac38dc7e

MapWindow/GlueMapWindow: Don't show MapItemList on first click of unfocused window

src/MapWindow/GlueMapWindow.cpp
@@ -33,6 +33,7 @@ GlueMapWindow::GlueMapWindow(const Look &look)
    idle_robin(2),
    drag_mode(DRAG_NONE),
    ignore_single_click(false),
+   first_single_click_after_focus(false),
    DisplayMode(DM_CRUISE),
    thermal_band_renderer(look.thermal_band, look.chart),
    final_glide_bar_renderer(look.final_glide_bar, look.map.task),

src/MapWindow/GlueMapWindow.hpp
@@ -91,8 +91,8 @@ class GlueMapWindow : public MapWindow {
   GestureManager gestures;
   bool ignore_single_click;
 
-  /** if mouse pan drag has moved over ~10 pixels */
-  bool dragOverMinDist;
+  /** single click on un-focused window */
+  bool first_single_click_after_focus;
 
   ZoomClimb_t zoomclimb;
 

src/MapWindow/GlueMapWindowEvents.cpp
@@ -63,9 +63,9 @@ GlueMapWindow::OnMouseMove(PixelScalar x, PixelScalar y, unsigned keys)
 {
   /* allow a bigger threshold on touch screens */
   const int threshold = IsEmbedded() ? 50 : 10;
-  if (drag_mode != DRAG_NONE && !dragOverMinDist &&
+  if (drag_mode != DRAG_NONE && first_single_click_after_focus &&
       (abs(drag_start.x - x) + abs(drag_start.y - y)) > Layout::Scale(threshold))
-    dragOverMinDist = true;
+    first_single_click_after_focus = false;
 
   switch (drag_mode) {
   case DRAG_NONE:
@@ -102,7 +102,7 @@ GlueMapWindow::OnMouseDown(PixelScalar x, PixelScalar y)
     return true;
 
   mouse_down_clock.update();
-  dragOverMinDist = false;
+  first_single_click_after_focus = !has_focus();
 
   set_focus();
 
@@ -210,7 +210,7 @@ GlueMapWindow::OnMouseUp(PixelScalar x, PixelScalar y)
     break;
   }
 
-  if (!dragOverMinDist) {
+  if (!first_single_click_after_focus) {
     map_item_timer.Schedule(200);
     return true;
   }

commit 42f94c2f51e7b96ddb20c1017ac858a512ea6caa

MapWindow/GlueMapWindow: Fix regression of MapItemList in pan mode.
d140ca36f14b2a0ab07cb0bd5d0a034bac38dc7e caused showing of the
MapItemList after every mouse-up while in pan mode.

src/MapWindow/GlueMapWindow.cpp
@@ -33,7 +33,7 @@ GlueMapWindow::GlueMapWindow(const Look &look)
    idle_robin(2),
    drag_mode(DRAG_NONE),
    ignore_single_click(false),
-   first_single_click_after_focus(false),
+   arm_mapitem_list(false),
    DisplayMode(DM_CRUISE),
    thermal_band_renderer(look.thermal_band, look.chart),
    final_glide_bar_renderer(look.final_glide_bar, look.map.task),

src/MapWindow/GlueMapWindow.hpp
@@ -91,8 +91,8 @@ class GlueMapWindow : public MapWindow {
   GestureManager gestures;
   bool ignore_single_click;
 
-  /** single click on un-focused window */
-  bool first_single_click_after_focus;
+  /** flag to indicate if the MapItemList should be shown on mouse up */
+  bool arm_mapitem_list;
 
   ZoomClimb_t zoomclimb;
 

src/MapWindow/GlueMapWindowEvents.cpp
@@ -63,9 +63,9 @@ GlueMapWindow::OnMouseMove(PixelScalar x, PixelScalar y, unsigned keys)
 {
   /* allow a bigger threshold on touch screens */
   const int threshold = IsEmbedded() ? 50 : 10;
-  if (drag_mode != DRAG_NONE && first_single_click_after_focus &&
+  if (drag_mode != DRAG_NONE && arm_mapitem_list &&
       (abs(drag_start.x - x) + abs(drag_start.y - y)) > Layout::Scale(threshold))
-    first_single_click_after_focus = false;
+    arm_mapitem_list = false;
 
   switch (drag_mode) {
   case DRAG_NONE:
@@ -102,7 +102,7 @@ GlueMapWindow::OnMouseDown(PixelScalar x, PixelScalar y)
     return true;
 
   mouse_down_clock.update();
-  first_single_click_after_focus = !has_focus();
+  arm_mapitem_list = has_focus();
 
   set_focus();
 
@@ -210,7 +210,7 @@ GlueMapWindow::OnMouseUp(PixelScalar x, PixelScalar y)
     break;
   }
 
-  if (!first_single_click_after_focus) {
+  if (arm_mapitem_list) {
     map_item_timer.Schedule(200);
     return true;
   }