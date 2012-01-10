XCSoar/XCSoar

commit f9b0b73977634fade59bd33f0c6460bac67026ec

Profile/ComputerProfile: Enforce proper range for UTC offset
Fixes trac issue #1630.

Since we allow setting a UTC offset of -13 to +13 leave values in
that range unmodified. +13 exists in the real world, -13 probably
not.

src/Profile/ComputerProfile.cpp
@@ -116,8 +116,8 @@ Profile::Load(SETTINGS_COMPUTER &settings)
 
   Get(szProfileSetSystemTimeFromGPS, settings.SetSystemTimeFromGPS);
   Get(szProfileUTCOffset, settings.UTCOffset);
-  if (settings.UTCOffset > 12 * 3600)
-    settings.UTCOffset -= 24 * 3600;
+  if (settings.UTCOffset > 13 * 3600 || settings.UTCOffset < -13 * 3600)
+    settings.UTCOffset = 0;
 
   Load(settings.task);
 }

commit 1cc42f910da6377a21ed2785930d510d031edba6

Profile: Add new UTCOffsetSigned settings key.
Previously the UTC offset was stored in an unsigned settings key
(UTCOffset) which encoded values in the range
  [ 0*3600 .. 12*3600] for positive UTC offsets and
  [12*3600 .. 24*3600] for negative UTC offsets.

This caused ambiguities for +12/-12 offsets. Bigger offsets
(e.g. +13) which exist in the real world) were also not handled
correctly.

This commit migrates the old values to a new "UTCOffsetSigned"
settings key.

Fixes trac issue #1682 which is a regression introduced by commit
f9b0b73977634fade59bd33f0c6460bac67026ec.

src/Dialogs/ConfigPanels/UnitsConfigPanel.cpp
@@ -413,12 +413,7 @@ UnitsConfigPanel::Save()
   int ival = iround(df.GetAsFixed() * 3600);
   if (settings_computer.UTCOffset != ival) {
     settings_computer.UTCOffset = ival;
-
-    // have to do this because registry variables can't be negative!
-    if (ival < 0)
-      ival += 24 * 3600;
-
-    Profile::Set(szProfileUTCOffset, ival);
+    Profile::Set(szProfileUTCOffsetSigned, ival);
     changed = true;
   }
 

src/Profile/ComputerProfile.cpp
@@ -115,7 +115,16 @@ Profile::Load(SETTINGS_COMPUTER &settings)
   GetEnum(szProfileAverEffTime, settings.AverEffTime);
 
   Get(szProfileSetSystemTimeFromGPS, settings.SetSystemTimeFromGPS);
-  Get(szProfileUTCOffset, settings.UTCOffset);
+
+  // NOTE: Until 6.2.4 UTCOffset was stored as a positive int in the
+  // settings file (with negative offsets stored as "UTCOffset + 24 * 3600").
+  // Later versions will create a new signed settings key.
+  if (!Get(szProfileUTCOffsetSigned, settings.UTCOffset)) {
+    if (Get(szProfileUTCOffset, settings.UTCOffset)) {
+      if (settings.UTCOffset > 12 * 3600)
+        settings.UTCOffset = (settings.UTCOffset % (24 * 3600)) - 24 * 3600;
+    }
+  }
   if (settings.UTCOffset > 13 * 3600 || settings.UTCOffset < -13 * 3600)
     settings.UTCOffset = 0;
 

src/Profile/ProfileKeys.cpp
@@ -185,6 +185,7 @@ const TCHAR szProfileAppAveNeedle[] = _T("AppAveNeedle");
 
 const TCHAR szProfileAutoAdvance[] = _T("AutoAdvance");
 const TCHAR szProfileUTCOffset[] = _T("UTCOffset");
+const TCHAR szProfileUTCOffsetSigned[] = _T("UTCOffsetSigned");
 const TCHAR szProfileBlockSTF[] = _T("BlockSpeedToFly");
 const TCHAR szProfileAutoZoom[] = _T("AutoZoom");
 const TCHAR szProfileMenuTimeout[] = _T("MenuTimeout");

src/Profile/ProfileKeys.hpp
@@ -122,6 +122,7 @@ extern const TCHAR szProfileAppInfoBoxModel[]; // VENTA-ADDON MODEL CHANGE
 extern const TCHAR szProfileAppAveNeedle[];
 extern const TCHAR szProfileAutoAdvance[];
 extern const TCHAR szProfileUTCOffset[];
+extern const TCHAR szProfileUTCOffsetSigned[];
 extern const TCHAR szProfileBlockSTF[];
 extern const TCHAR szProfileAutoZoom[];
 extern const TCHAR szProfileMenuTimeout[];