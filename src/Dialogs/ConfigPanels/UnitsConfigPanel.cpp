e7873bd9e97819a0301e40b86f03707d17c83ab8
Helmut J. Rohs <helmut.j.rohs@web.de>
2012-01-11 00:30:58 +0100

    Dialogs/ConfigPanels/UnitsConfigPanel.cpp: Converted from XML to RowForm.

--- src/Dialogs/ConfigPanels/UnitsConfigPanel.cpp
+++ src/Dialogs/ConfigPanels/UnitsConfigPanel.cpp
@@ -24,49 +24,47 @@ Copyright_License {
 #include "UnitsConfigPanel.hpp"
 #include "UIGlobals.hpp"
 #include "DataField/Enum.hpp"
-#include "DataField/ComboList.hpp"
-#include "DataField/Float.hpp"
 #include "Form/Form.hpp"
-#include "Form/Button.hpp"
-#include "Form/Edit.hpp"
-#include "Form/Util.hpp"
 #include "Form/Frame.hpp"
-#include "Dialogs/ComboPicker.hpp"
 #include "Units/Units.hpp"
 #include "Units/UnitsStore.hpp"
-#include "Profile/ProfileKeys.hpp"
 #include "Profile/Profile.hpp"
 #include "Interface.hpp"
 #include "Asset.hpp"
 #include "Language/Language.hpp"
 #include "DataField/Base.hpp"
-#include "Form/XMLWidget.hpp"
+#include "Form/RowFormWidget.hpp"
 #include "Screen/Layout.hpp"
-#include "Dialogs/CallBackTable.hpp"
+#include "UIGlobals.hpp"
 
-class UnitsConfigPanel : public XMLWidget {
+enum ControlIndex {
+  UnitsPreset,
+  spacer_1,
+  UnitsSpeed,
+  UnitsDistance,
+  UnitsLift,
+  UnitsAltitude,
+  UnitsTemperature,
+  UnitsTaskSpeed,
+  UnitsPressure,
+  spacer_2,
+  UnitsLatLon
+};
 
+static const TCHAR *custom_preset_label = N_("Custom");
+
+class UnitsConfigPanel : public RowFormWidget {
 private:
-  unsigned SpeedUnits; // default is knots
-  unsigned TaskSpeedUnits; // default is kph
-  unsigned DistanceUnits; // default is km
-  unsigned LiftUnits;
-  unsigned AltitudeUnits; //default ft
-  unsigned TemperatureUnits; //default is celcius
   bool loading;
 
 public:
-  UnitsConfigPanel() : SpeedUnits(1), TaskSpeedUnits(2), DistanceUnits(3),
-    LiftUnits(0), AltitudeUnits(0), TemperatureUnits(0), loading(false) {
-  }
+  UnitsConfigPanel() : RowFormWidget(UIGlobals::GetDialogLook(), Layout::Scale(150)),
+                          loading(false) {}
+
   virtual void Prepare(ContainerWindow &parent, const PixelRect &rc);
   virtual bool Save(bool &changed, bool &require_restart);
-  virtual void Show(const PixelRect &rc);
-  virtual void Hide();
-  void UpdateUnitsTitle();
-  void SetUnitsTitle(const TCHAR* title);
   void UpdateUnitFields(const UnitSetting &units);
-  void OnFieldData(DataField::DataAccessKind_t mode);
+  void SetToCustom(DataField::DataAccessKind_t mode);
 };
 
 /** XXX this hack is needed because the form callbacks don't get a
@@ -74,108 +72,57 @@ public:
 static UnitsConfigPanel *instance;
 
 void
-UnitsConfigPanel::Show(const PixelRect &rc)
-{
-  XMLWidget::Show(rc);
-}
-
-void
-UnitsConfigPanel::Hide()
-{
-  XMLWidget::Hide();
-}
-
-void
 UnitsConfigPanel::UpdateUnitFields(const UnitSetting &units)
 {
-  LoadFormProperty(form, _T("prpUnitsLatLon"), units.coordinate_format);
+  loading = true;
 
-  unsigned index;
-  index = (units.speed_unit == unStatuteMilesPerHour) ? 0 :
-          (units.speed_unit == unKnots) ? 1 : 2;
-  LoadFormProperty(form, _T("prpUnitsSpeed"), index);
-  if (loading)
-    SpeedUnits = index;
+  GetDataField(UnitsSpeed).SetAsInteger(units.speed_unit);
+  GetControl(UnitsSpeed).RefreshDisplay();
 
-  index = (units.task_speed_unit == unStatuteMilesPerHour) ? 0 :
-          (units.task_speed_unit == unKnots) ? 1 : 2;
-  LoadFormProperty(form, _T("prpUnitsTaskSpeed"), index);
-  if (loading)
-    TaskSpeedUnits = index;
+  GetDataField(UnitsDistance).SetAsInteger(units.distance_unit);
+  GetControl(UnitsDistance).RefreshDisplay();
 
-  index = (units.distance_unit == unStatuteMiles) ? 0 :
-          (units.distance_unit == unNauticalMiles) ? 1 : 2;
-  LoadFormProperty(form, _T("prpUnitsDistance"), index);
-  if (loading)
-    DistanceUnits = index;
+  GetDataField(UnitsLift).SetAsInteger(units.vertical_speed_unit);
+  GetControl(UnitsLift).RefreshDisplay();
 
-  index = (units.altitude_unit == unFeet) ? 0 : 1;
-  LoadFormProperty(form, _T("prpUnitsAltitude"), index);
-  if (loading)
-    AltitudeUnits = index;
+  GetDataField(UnitsAltitude).SetAsInteger(units.altitude_unit);
+  GetControl(UnitsAltitude).RefreshDisplay();
 
-  index = (units.temperature_unit == unGradFahrenheit) ? 1 : 0;
-  LoadFormProperty(form, _T("prpUnitsTemperature"), index);
-  if (loading)
-    TemperatureUnits = index;
+  GetDataField(UnitsTemperature).SetAsInteger(units.temperature_unit);
+  GetControl(UnitsTemperature).RefreshDisplay();
 
-  index = (units.vertical_speed_unit == unKnots) ? 0 :
-          (units.vertical_speed_unit == unFeetPerMinute) ? 2 : 1;
-  LoadFormProperty(form, _T("prpUnitsLift"), index);
-  if (loading)
-    LiftUnits = index;
+  GetDataField(UnitsTaskSpeed).SetAsInteger(units.task_speed_unit);
+  GetControl(UnitsTaskSpeed).RefreshDisplay();
 
-  LoadFormProperty(form, _T("prpUnitsPressure"), units.pressure_unit);
-}
+  GetDataField(UnitsPressure).SetAsInteger(units.pressure_unit);
+  GetControl(UnitsPressure).RefreshDisplay();
 
-void
-UnitsConfigPanel::SetUnitsTitle(const TCHAR* title)
-{
-  TCHAR caption[255];
-  _tcscpy(caption,  _("Units"));
-  _tcscat(caption, _T(": "));
-  _tcscat(caption, title);
-  ((WndFrame *)form.FindByName(_T("lblUnitsSetting")))->SetCaption(caption);
-}
-
-void
-UnitsConfigPanel::UpdateUnitsTitle()
-{
-  TCHAR title[255];
-  if (Profile::Get(szProfileUnitsPresetName, title, 255))
-    SetUnitsTitle(title);
+  loading = false;
 }
 
 static void
-OnLoadPreset(WndButton &button)
+OnUnitsPreset(DataField *Sender, DataField::DataAccessKind_t Mode)
 {
-  ComboList list;
-  unsigned len = Units::Store::Count();
-  for (unsigned i = 0; i < len; i++)
-    list.Append(i, Units::Store::GetName(i));
-
-  list.Sort();
-
-  /* let the user select */
-
-  int result = ComboPicker(UIGlobals::GetMainWindow(), _("Unit Presets"), list, NULL);
-  if (result >= 0) {
-    const UnitSetting& units = Units::Store::Read(list[result].DataFieldIndex);
+  int result = instance->GetValueInteger(UnitsPreset);
+  if (result > 0) {
+    // First selection means not to load any preset.
+    const UnitSetting& units = Units::Store::Read(result-1);
     instance->UpdateUnitFields(units);
 
-    Profile::Set(szProfileUnitsPresetName, list[result].StringValue);
-    instance->UpdateUnitsTitle();
+    Profile::Set(szProfileUnitsPresetName, Units::Store::GetName(result-1));
   }
 }
 
 void
-UnitsConfigPanel::OnFieldData(DataField::DataAccessKind_t mode)
+UnitsConfigPanel::SetToCustom(DataField::DataAccessKind_t mode)
 {
   switch (mode) {
   case DataField::daChange:
-    if (!loading)
-      Profile::Set(szProfileUnitsPresetName, _T("Custom"));
-    UpdateUnitsTitle();
+    if (!loading) {
+      Profile::Set(szProfileUnitsPresetName, custom_preset_label);
+      GetDataField(UnitsPreset).SetAsString(custom_preset_label);
+      GetControl(UnitsPreset).RefreshDisplay();
+    }
     break;
 
   case DataField::daSpecial:
@@ -186,97 +133,113 @@ UnitsConfigPanel::OnFieldData(DataField::DataAccessKind_t mode)
 static void
 OnFieldData(DataField *Sender, DataField::DataAccessKind_t Mode)
 {
-  instance->OnFieldData(Mode);
+  instance->SetToCustom(Mode);
 }
-
-gcc_constexpr_data CallBackTableEntry CallBackTable[] = {
-  DeclareCallBackEntry(OnLoadPreset),
-  DeclareCallBackEntry(OnFieldData),
-  DeclareCallBackEntry(NULL)
-};
 
 void
 UnitsConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
 {
+  const UnitSetting &config = CommonInterface::SetUISettings().units;
+
+  RowFormWidget::Prepare(parent, rc);
   instance = this;
-  LoadWindow(CallBackTable, parent,
-             Layout::landscape ? _T("IDR_XML_UNITSCONFIGPANEL") :
-                               _T("IDR_XML_UNITSCONFIGPANEL_L"));
 
-  loading = true;
+  static const TCHAR * preset_help = N_("Load a set of units.");
+  WndProperty *wp = AddEnum(_("Preset"), _T(""));
+  if (wp != NULL) {
+    DataFieldEnum &df = *(DataFieldEnum *)wp->GetDataField();
+    df.EnableItemHelp(true);
 
-  WndProperty *wp = (WndProperty*)form.FindByName(_T("prpUnitsSpeed"));
-  assert(wp != NULL);
-  DataFieldEnum *dfe = (DataFieldEnum*)wp->GetDataField();
-  dfe->addEnumText(_T("mph"));
-  dfe->addEnumText(_("knots"));
-  dfe->addEnumText(_T("km/h"));
-  wp->RefreshDisplay();
+    df.addEnumText(custom_preset_label, (unsigned)0, _("My individual set of units."));
+    unsigned len = Units::Store::Count();
+    for (unsigned i = 0; i < len; i++)
+      df.addEnumText(Units::Store::GetName(i), i+1, preset_help);
 
-  wp = (WndProperty*)form.FindByName(_T("prpUnitsLatLon"));
-  assert(wp != NULL);
-  dfe = (DataFieldEnum*)wp->GetDataField();
-  const TCHAR *const units_lat_lon[] = {
-    _T("DDMMSS"),
-    _T("DDMMSS.ss"),
-    _T("DDMM.mmm"),
-    _T("DD.dddd"),
-    NULL
-  };
+    TCHAR tmp_text[100];
+    Profile::Get(szProfileUnitsPresetName, tmp_text, 100);
+    df.SetAsString(tmp_text);
 
-  dfe->addEnumTexts(units_lat_lon);
+    wp->GetDataField()->SetDataAccessCallback(OnUnitsPreset);
+    wp->RefreshDisplay();
+  }
 
-  wp = (WndProperty*)form.FindByName(_T("prpUnitsTaskSpeed"));
-  assert(wp != NULL);
-  dfe = (DataFieldEnum*)wp->GetDataField();
-  dfe->addEnumText(_T("mph"));
-  dfe->addEnumText(_("knots"));
-  dfe->addEnumText(_T("km/h"));
-  wp->RefreshDisplay();
+  AddSpacer();
 
-  wp = (WndProperty*)form.FindByName(_T("prpUnitsDistance"));
-  assert(wp != NULL);
-  dfe = (DataFieldEnum*)wp->GetDataField();
-  dfe->addEnumText(_T("sm"));
-  dfe->addEnumText(_T("nm"));
-  dfe->addEnumText(_T("km"));
-  wp->RefreshDisplay();
-
-  wp = (WndProperty*)form.FindByName(_T("prpUnitsAltitude"));
-  assert(wp != NULL);
-  dfe = (DataFieldEnum*)wp->GetDataField();
-  dfe->addEnumText(_("foot"));
-  dfe->addEnumText(_("meter"));
-  wp->RefreshDisplay();
-
-  wp = (WndProperty*)form.FindByName(_T("prpUnitsTemperature"));
-  assert(wp != NULL);
-  dfe = (DataFieldEnum*)wp->GetDataField();
-  dfe->addEnumText(_T(DEG "C"));
-  dfe->addEnumText(_T(DEG "F"));
-  wp->RefreshDisplay();
-
-  wp = (WndProperty*)form.FindByName(_T("prpUnitsLift"));
-  assert(wp != NULL);
-  dfe = (DataFieldEnum*)wp->GetDataField();
-  dfe->addEnumText(_("knots"));
-  dfe->addEnumText(_T("m/s"));
-  dfe->addEnumText(_T("ft/min"));
-  wp->RefreshDisplay();
-
-  UpdateUnitFields(CommonInterface::GetUISettings().units);
-
-  static gcc_constexpr_data StaticEnumChoice pressure_labels_list[] = {
-    { unHectoPascal, _T("hPa") },
-    { unMilliBar, _T("mb") },
-    { unInchMercury, _T("inHg") },
+  // Should be all Expert items (TODO)
+  static const TCHAR * units_speed_help = N_("Units used for airspeed and ground speed.  "
+      "A separate unit is available for task speeds.");
+  static const StaticEnumChoice  units_speed_list[] = {
+    { unStatuteMilesPerHour,  _T("mph"), units_speed_help },
+    { unKnots,                N_("knots"), units_speed_help },
+    { unKiloMeterPerHour,     _T("km/h"), units_speed_help },
     { 0 }
   };
+  AddEnum(_("Aircraft/Wind speed"), _T(""), units_speed_list, (unsigned int)config.speed_unit, OnFieldData);
 
-  LoadFormProperty(form, _T("prpUnitsPressure"), pressure_labels_list,
-                   CommonInterface::GetUISettings().units.pressure_unit);
+  static const TCHAR *units_distance_help = _("Units used for horizontal distances e.g. "
+      "range to waypoint, distance to go.");
+  static const StaticEnumChoice  units_distance_list[] = {
+    { unStatuteMiles,  _T("sm"), units_distance_help },
+    { unNauticalMiles, _T("nm"), units_distance_help },
+    { unKiloMeter,     _T("km"), units_distance_help },
+    { 0 }
+  };
+  AddEnum(_("Distance"), _T(""), units_distance_list, config.distance_unit, OnFieldData);
 
-  loading = false;
+  static const TCHAR *units_lift_help = _("Units used for vertical speeds (variometer).");
+  static const StaticEnumChoice  units_lift_list[] = {
+    { unKnots,          N_("knots"), units_lift_help },
+    { unMeterPerSecond, _T("m/s"), units_lift_help },
+    { unFeetPerMinute,  _T("ft/min"), units_lift_help },
+    { 0 }
+  };
+  AddEnum(_("Lift"), _T(""), units_lift_list, config.vertical_speed_unit, OnFieldData);
+
+  static const TCHAR *units_altitude_help = _("Units used for altitude and heights.");
+  static const StaticEnumChoice  units_altitude_list[] = {
+    { unFeet,  N_("foot"), units_altitude_help },
+    { unMeter, N_("meter"), units_altitude_help },
+    { 0 }
+  };
+  AddEnum(_("Altitude"), _T(""), units_altitude_list, config.altitude_unit, OnFieldData);
+
+  static const TCHAR *units_temperature_help = _("Units used for temperature.");
+  static const StaticEnumChoice  units_temperature_list[] = {
+    { unGradCelcius,    _T(DEG "C"), units_temperature_help },
+    { unGradFahrenheit, _T(DEG "F"), units_temperature_help },
+    { 0 }
+  };
+  AddEnum(_("Temperature"), _T(""), units_temperature_list, config.temperature_unit, OnFieldData);
+
+  static const TCHAR *units_taskspeed_help = _("Units used for task speeds.");
+  static const StaticEnumChoice  units_taskspeed_list[] = {
+    { unStatuteMilesPerHour,  _T("mph"), units_taskspeed_help },
+    { unKnots,                N_("knots"), units_taskspeed_help },
+    { unKiloMeterPerHour,     _T("km/h"), units_taskspeed_help },
+    { 0 }
+  };
+  AddEnum(_("Task speed"), _T(""), units_taskspeed_list, config.task_speed_unit, OnFieldData);
+
+  static const TCHAR *units_pressure_help = _("Units used for pressures.");
+  static const StaticEnumChoice pressure_labels_list[] = {
+    { unHectoPascal, _T("hPa"), units_pressure_help },
+    { unMilliBar,    _T("mb"), units_pressure_help },
+    { unInchMercury, _T("inHg"), units_pressure_help },
+    { 0 }
+  };
+  AddEnum(_("Pressure"), _T(""), pressure_labels_list, config.pressure_unit, OnFieldData);
+
+  AddSpacer();
+
+  static const TCHAR *units_lat_lon_help = _("Units used for latitude and longitude.");
+  static const StaticEnumChoice units_lat_lon_list[] = {
+    { 0, _T("DDMMSS"), units_lat_lon_help },
+    { 1, _T("DDMMSS.ss"), units_lat_lon_help },
+    { 2, _T("DDMM.mmm"), units_lat_lon_help },
+    { 3, _T("DD.dddd"), units_lat_lon_help },
+    { 0 }
+  };
+  AddEnum(_("Lat./Lon."), _T(""), units_lat_lon_list, config.coordinate_format);
 }
 
 bool
@@ -289,130 +252,21 @@ UnitsConfigPanel::Save(bool &_changed, bool &_require_restart)
   /* the Units settings affect how other form values are read and translated
    * so changes to Units settings should be processed after all other form settings
    */
-  int tmp = GetFormValueInteger(form, _T("prpUnitsSpeed"));
-  if ((int)SpeedUnits != tmp) {
-    SpeedUnits = tmp;
-    Profile::Set(szProfileSpeedUnitsValue, SpeedUnits);
-    changed = true;
+  changed |= SaveValueEnum(UnitsSpeed, szProfileSpeedUnitsValue, config.speed_unit);
 
-    switch (SpeedUnits) {
-    case 0:
-      config.speed_unit = config.wind_speed_unit = unStatuteMilesPerHour;
-      break;
-    case 1:
-      config.speed_unit = config.wind_speed_unit = unKnots;
-      break;
-    case 2:
-    default:
-      config.speed_unit = config.wind_speed_unit = unKiloMeterPerHour;
-      break;
-    }
-  }
+  changed |= SaveValueEnum(UnitsDistance, szProfileDistanceUnitsValue, config.distance_unit);
 
-  tmp = GetFormValueInteger(form, _T("prpUnitsLatLon"));
-  if ((int)config.coordinate_format != tmp) {
-    config.coordinate_format = (CoordinateFormats)tmp;
-    Profile::Set(szProfileLatLonUnits, config.coordinate_format);
-    changed = true;
-  }
+  changed |= SaveValueEnum(UnitsLift, szProfileLiftUnitsValue, config.vertical_speed_unit);
 
-  tmp = GetFormValueInteger(form, _T("prpUnitsTaskSpeed"));
-  if ((int)TaskSpeedUnits != tmp) {
-    TaskSpeedUnits = tmp;
-    Profile::Set(szProfileTaskSpeedUnitsValue, TaskSpeedUnits);
-    changed = true;
+  changed |= SaveValueEnum(UnitsAltitude, szProfileAltitudeUnitsValue, config.altitude_unit);
 
-    switch (TaskSpeedUnits) {
-    case 0:
-      config.task_speed_unit = unStatuteMilesPerHour;
-      break;
-    case 1:
-      config.task_speed_unit = unKnots;
-      break;
-    case 2:
-    default:
-      config.task_speed_unit = unKiloMeterPerHour;
-      break;
-    }
-  }
+  changed |= SaveValueEnum(UnitsTemperature, szProfileTemperatureUnitsValue, config.temperature_unit);
 
-  tmp = GetFormValueInteger(form, _T("prpUnitsDistance"));
-  if ((int)DistanceUnits != tmp) {
-    DistanceUnits = tmp;
-    Profile::Set(szProfileDistanceUnitsValue, DistanceUnits);
-    changed = true;
+  changed |= SaveValueEnum(UnitsTaskSpeed, szProfileTaskSpeedUnitsValue, config.task_speed_unit);
 
-    switch (DistanceUnits) {
-    case 0:
-      config.distance_unit = unStatuteMiles;
-      break;
-    case 1:
-      config.distance_unit = unNauticalMiles;
-      break;
-    case 2:
-    default:
-      config.distance_unit = unKiloMeter;
-      break;
-    }
-  }
+  changed |= SaveValueEnum(UnitsPressure, szProfilePressureUnitsValue, config.pressure_unit);
 
-  tmp = GetFormValueInteger(form, _T("prpUnitsLift"));
-  if ((int)LiftUnits != tmp) {
-    LiftUnits = tmp;
-    Profile::Set(szProfileLiftUnitsValue, LiftUnits);
-    changed = true;
-
-    switch (LiftUnits) {
-    case 0:
-      config.vertical_speed_unit = unKnots;
-      break;
-    case 1:
-    default:
-      config.vertical_speed_unit = unMeterPerSecond;
-      break;
-    case 2:
-      config.vertical_speed_unit = unFeetPerMinute;
-      break;
-    }
-  }
-
-  changed |= SaveFormPropertyEnum(form, _T("prpUnitsPressure"),
-                                  szProfilePressureUnitsValue,
-                                  config.pressure_unit);
-
-  tmp = GetFormValueInteger(form, _T("prpUnitsAltitude"));
-  if ((int)AltitudeUnits != tmp) {
-    AltitudeUnits = tmp;
-    Profile::Set(szProfileAltitudeUnitsValue, AltitudeUnits);
-    changed = true;
-
-    switch (AltitudeUnits) {
-    case 0:
-      config.altitude_unit = unFeet;
-      break;
-    case 1:
-    default:
-      config.altitude_unit = unMeter;
-      break;
-    }
-  }
-
-  tmp = GetFormValueInteger(form, _T("prpUnitsTemperature"));
-  if ((int)TemperatureUnits != tmp) {
-    TemperatureUnits = tmp;
-    Profile::Set(szProfileTemperatureUnitsValue, TemperatureUnits);
-    changed = true;
-
-    switch (TemperatureUnits) {
-    case 0:
-      config.temperature_unit = unGradCelcius;
-      break;
-    case 1:
-    default:
-      config.temperature_unit = unGradFahrenheit;
-      break;
-    }
-  }
+  changed |= SaveValueEnum(UnitsLatLon, szProfileLatLonUnits, config.coordinate_format);
 
   _changed |= changed;
   _require_restart |= require_restart;
