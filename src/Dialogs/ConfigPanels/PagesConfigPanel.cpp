XCSoar/XCSoar

commit d7ed713ff2ee7b2fd6dd055a3368de6640c1837d

Converted PagesConfigPanel from XML to RowForm.

src/Dialogs/ConfigPanels/PagesConfigPanel.cpp
@@ -23,39 +23,28 @@ Copyright_License {
 
 #include "PagesConfigPanel.hpp"
 #include "DataField/Enum.hpp"
-#include "Form/Edit.hpp"
 #include "Pages.hpp"
+#include "Language/Language.hpp"
 #include "Profile/PageProfile.hpp"
 #include "Interface.hpp"
-#include "Form/Form.hpp"
-#include "Form/XMLWidget.hpp"
+#include "Form/RowFormWidget.hpp"
 #include "Screen/Layout.hpp"
+#include "UIGlobals.hpp"
 
 #include <stdio.h>
 
 using namespace Pages;
 
 
-class PagesConfigPanel : public XMLWidget {
+class PagesConfigPanel : public RowFormWidget {
+public:
+  PagesConfigPanel()
+    :RowFormWidget(UIGlobals::GetDialogLook(), Layout::Scale(50)) {}
 
 public:
   virtual void Prepare(ContainerWindow &parent, const PixelRect &rc);
   virtual bool Save(bool &changed, bool &require_restart);
-  virtual void Show(const PixelRect &rc);
-  virtual void Hide();
 };
-
-void
-PagesConfigPanel::Show(const PixelRect &rc)
-{
-  XMLWidget::Show(rc);
-}
-
-void
-PagesConfigPanel::Hide()
-{
-  XMLWidget::Hide();
-}
 
 static void
 UpdateComboBox(DataFieldEnum* dfe, unsigned page)
@@ -77,20 +66,27 @@ UpdateComboBox(DataFieldEnum* dfe, unsigned page)
   }
 }
 
+
+
 void
 PagesConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
 {
-  LoadWindow(NULL, parent,
-             Layout::landscape ? _T("IDR_XML_PAGESCONFIGPANEL") :
-                               _T("IDR_XML_PAGESCONFIGPANEL_L"));
+  static const StaticEnumChoice  empty_list[] = { { 0 }  };
 
-  StaticString<64> prpName;
-  for (unsigned i = 0; i < PageSettings::MAX_PAGES; i++) {
-    prpName.Format(_T("prpPageLayout%u"), i);
-    WndProperty* wp = (WndProperty*)form.FindByName(prpName);
+  RowFormWidget::Prepare(parent, rc);
+
+  for (unsigned i = 0; i < InfoBoxSettings::MAX_PANELS; i++) {
+    StaticString<32> caption;
+    caption.Format(_T("%s %u"), _("Page"), i+1);
+    WndProperty* wp = AddEnum(caption,
+                             _("Determines the information displayed on different screen pages."),
+                             empty_list, 0);
     if (wp) {
       DataFieldEnum* dfe = (DataFieldEnum*)wp->GetDataField();
       UpdateComboBox(dfe, i);
+// TODO     if (i>2 && !Expert) {
+//        wp->set_visible(false);
+//      }
       wp->RefreshDisplay();
     }
   }
@@ -100,16 +96,14 @@ bool
 PagesConfigPanel::Save(bool &_changed, bool &_require_restart)
 {
   bool changed = false, require_restart = false;
-  StaticString<64> prpName;
 
   PageSettings &settings = CommonInterface::SetUISettings().pages;
   for (unsigned int i = 0; i < PageSettings::MAX_PAGES; i++) {
-    prpName.Format(_T("prpPageLayout%u"), i);
-    WndProperty* wp = (WndProperty*)form.FindByName(prpName);
-    if (wp) {
+    unsigned int tmp_page_selection;
+    if (SaveValueEnum(i, tmp_page_selection)) {
       PageSettings::PageLayout *currentPL = &settings.pages[i];
       const PageSettings::PageLayout *setPL =
-        PossiblePageLayout(wp->GetDataField()->GetAsInteger());
+        PossiblePageLayout(tmp_page_selection);
       if (! (*currentPL == *setPL)) {
         SetLayout(i, *setPL);
 

Data/Dialogs/Configuration/PagesConfigPanel.xml
@@ -1,36 +1,0 @@
-<?xml version="1.0"?>
-
-<Panel Name="frmPages">
-  <Edit Name="prpPageLayout0" Caption="Page 1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout1" Caption="Page 2" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout2" Caption="Page 3" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout3" Caption="Page 4" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout4" Caption="Page 5" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout5" Caption="Page 6" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout6" Caption="Page 7" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout7" Caption="Page 8" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-</Panel>

Data/Dialogs/Configuration/PagesConfigPanel_L.xml
@@ -1,36 +1,0 @@
-<?xml version="1.0"?>
-
-<Panel Name="frmPages">
-  <Edit Name="prpPageLayout0" Caption="Page 1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout1" Caption="Page 2" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout2" Caption="Page 3" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout3" Caption="Page 4" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout4" Caption="Page 5" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout5" Caption="Page 6" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout6" Caption="Page 7" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-  <Edit Name="prpPageLayout7" Caption="Page 8" Expert="1" X="2" Width="-2" Height="22" CaptionWidth="50"
-     Help="Determines the information displayed on different screen pages.">
-    <DataField DataType="enum" />
-  </Edit>
-</Panel>

Data/XCSoar.rc
@@ -296,8 +296,6 @@ IDR_XML_LAYOUTCONFIGPANEL XMLDIALOG DISCARDABLE "../output/data/dialogs/Configur
 IDR_XML_LAYOUTCONFIGPANEL_L XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/LayoutConfigPanel_L.xml.gz"
 IDR_XML_LOGGERCONFIGPANEL XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/LoggerConfigPanel.xml.gz"
 IDR_XML_LOGGERCONFIGPANEL_L XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/LoggerConfigPanel_L.xml.gz"
-IDR_XML_PAGESCONFIGPANEL XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/PagesConfigPanel.xml.gz"
-IDR_XML_PAGESCONFIGPANEL_L XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/PagesConfigPanel_L.xml.gz"
 IDR_XML_POLARCONFIGPANEL XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/PolarConfigPanel.xml.gz"
 IDR_XML_POLARCONFIGPANEL_L XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/PolarConfigPanel_L.xml.gz"
 IDR_XML_ROUTECONFIGPANEL XMLDIALOG DISCARDABLE "../output/data/dialogs/Configuration/RouteConfigPanel.xml.gz"

commit 366a3f5d501226ead0e349d3e3e63ccd168c2762

Dialogs/ConfigPanels/PagesConfigPanel: Fix regression during RowForm conversion
Regression introduced in commit d7ed713ff2ee7b2fd6dd055a3368de6640c1837d.

src/Dialogs/ConfigPanels/PagesConfigPanel.cpp
@@ -99,19 +99,13 @@ PagesConfigPanel::Save(bool &_changed, bool &_require_restart)
 
   PageSettings &settings = CommonInterface::SetUISettings().pages;
   for (unsigned int i = 0; i < PageSettings::MAX_PAGES; i++) {
-    unsigned int tmp_page_selection;
-    if (SaveValueEnum(i, tmp_page_selection)) {
-      PageSettings::PageLayout *currentPL = &settings.pages[i];
-      const PageSettings::PageLayout *setPL =
-        PossiblePageLayout(tmp_page_selection);
-      if (! (*currentPL == *setPL)) {
-        SetLayout(i, *setPL);
-
-        *currentPL = *setPL;
-        Profile::Save(*currentPL, i);
-
-        changed = true;
-      }
+    PageSettings::PageLayout *currentPL = &settings.pages[i];
+    const PageSettings::PageLayout *setPL = PossiblePageLayout(GetValueInteger(i));
+    if (! (*currentPL == *setPL)) {
+      SetLayout(i, *setPL);
+      *currentPL = *setPL;
+      Profile::Save(*currentPL, i);
+      changed = true;
     }
   }
   _changed |= changed;