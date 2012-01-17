aebc1ada199898f510c6e75c6aa2ceece17c285a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-01-17 10:55:00 +0100

    GPSClock: Fixed delta_advance() function
    "dt" is also the name of an attribute which is supposed to be used in the
    second condition. By moving the local dt variable inside the scope
    of the condition, we only use the dt value for the return value.

--- src/GPSClock.hpp
+++ src/GPSClock.hpp
@@ -83,12 +83,12 @@ public:
   }
 
   fixed delta_advance(const fixed now) {
-    fixed dt=now-last;
     if (check_reverse(now)) {
       return fixed_minus_one;
     }
     // check if time has advanced past dt
     if (now-last>=dt) {
+      fixed dt=now-last;
       last= now;
       return dt;
     } else {
