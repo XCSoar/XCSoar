5f5f4f92fbff77a6202a3ff9e40a752f394bc3f7
Max Kellermann <max@duempel.org>
2012-01-30 21:01:54 +0100

    ProfileMap: re-add Vega comment that got lost during the merge

--- src/Profile/ProfileMap.cpp
+++ src/Profile/ProfileMap.cpp
@@ -76,6 +76,8 @@ ProfileMap::Export(KeyValueFileWriter &writer)
 {
   // Iterate through the profile maps
   for (auto it_str = map.begin(); it_str != map.end(); it_str++)
+    /* ignore the "Vega*" values; the Vega driver abuses the profile
+       to pass messages between the driver and the user interface */
     if (_tcsncmp(it_str->first.c_str(), _T("Vega"), 4) != 0)
       writer.Write(it_str->first.c_str(), it_str->second.c_str());
 }
