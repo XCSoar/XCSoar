XCSoar/XCSoar

commit 00241e4aa0b5fa11bd66bd8dbdfe2bf880265d81

AirspaceRoute: make RouteAirspaceIntersection a custom struct

src/Engine/Route/AirspaceRoute.cpp
@@ -125,7 +125,7 @@ AirspaceRoute::FirstIntersecting(const RouteLink& e) const
   m_airspaces.visit_intersecting(origin, v, visitor);
   const AIV::AIVResult res (visitor.get_nearest());
   count_airspace++;
-  return std::make_pair(res.first, res.second);
+  return RouteAirspaceIntersection(res.first, res.second);
 }
 
 const AbstractAirspace*
@@ -265,9 +265,9 @@ void
 AirspaceRoute::AddNearbyAirspace(const RouteAirspaceIntersection &inx,
                                    const RouteLink &e)
 {
-  const SearchPointVector& fat = inx.first->GetClearance();
+  const SearchPointVector& fat = inx.airspace->GetClearance();
   const ClearingPair p = GetPairs(fat, e.first, e.second);
-  const ClearingPair pb = GetBackupPairs(fat, e.first, inx.second);
+  const ClearingPair pb = GetBackupPairs(fat, e.first, inx.point);
 
   // process all options
   AddCandidate(RouteLinkBase(e.first, p.first));
@@ -279,9 +279,8 @@ AirspaceRoute::AddNearbyAirspace(const RouteAirspaceIntersection &inx,
 void
 AirspaceRoute::AddNearby(const RouteLink &e)
 {
-  RoutePoint ptmp = m_inx.second;
-  if (m_inx.first == NULL)
-    AddNearbyTerrain(ptmp, e);
+  if (m_inx.airspace == NULL)
+    AddNearbyTerrain(m_inx.point, e);
   else
     AddNearbyAirspace(m_inx, e);
 }
@@ -293,7 +292,7 @@ AirspaceRoute::CheckSecondary(const RouteLink &e)
     return true; // trivial
 
   m_inx = FirstIntersecting(e);
-  if (m_inx.first!=NULL)  {
+  if (m_inx.airspace != NULL)  {
     AddCandidate(e);
     return false;
   };
@@ -306,8 +305,8 @@ AirspaceRoute::CheckClearance(const RouteLink &e, RoutePoint& inp) const
   // attempt terrain clearance first
 
   if (!CheckClearanceTerrain(e, inp)) {
-    m_inx.first = NULL;
-    m_inx.second = inp;
+    m_inx.airspace = NULL;
+    m_inx.point = inp;
     return false;
   }
 
@@ -317,8 +316,8 @@ AirspaceRoute::CheckClearance(const RouteLink &e, RoutePoint& inp) const
   // passes terrain, so now check airspace clearance
 
   m_inx = FirstIntersecting(e);
-  if (m_inx.first!=NULL)  {
-    inp = m_inx.second;
+  if (m_inx.airspace != NULL)  {
+    inp = m_inx.point;
     return false;
   }
 

src/Engine/Route/AirspaceRoute.hpp
@@ -28,8 +28,19 @@
 class AirspaceRoute: public RoutePlanner {
   Airspaces m_airspaces;
 
-  typedef std::pair<const AbstractAirspace*,
-                    RoutePoint> RouteAirspaceIntersection;
+  struct RouteAirspaceIntersection {
+    const AbstractAirspace *airspace;
+
+    RoutePoint point;
+
+    RouteAirspaceIntersection() = default;
+
+    gcc_constexpr_ctor
+    RouteAirspaceIntersection(const AbstractAirspace *_airspace,
+                              RoutePoint _point)
+      :airspace(_airspace), point(_point) {}
+  };
+
   mutable RouteAirspaceIntersection m_inx;
 
 public:

commit e5875cc9e8224bb07544902494b68c820ff085a0

Engine/Route/AirspaceRoute: Fix regression in routing calculations
Regression introduced in commit 00241e4aa0b5fa11bd66bd8dbdfe2bf880265d81
(AirspaceRoute: make RouteAirspaceIntersection a custom struct)

Symptom was failing terrain routing if "Routing Mode" was set to "Both"

src/Engine/Route/AirspaceRoute.cpp
@@ -278,9 +278,12 @@ AirspaceRoute::AddNearbyAirspace(const RouteAirspaceIntersection &inx,
 void
 AirspaceRoute::AddNearby(const RouteLink &e)
 {
-  if (m_inx.airspace == NULL)
-    AddNearbyTerrain(m_inx.point, e);
-  else
+  if (m_inx.airspace == NULL) {
+    // NOTE: m_inx is "mutable" so that const in AddNearbyTerrain is ignored!!
+    // The copy is really needed!
+    RoutePoint ptmp = m_inx.point;
+    AddNearbyTerrain(ptmp, e);
+  } else
     AddNearbyAirspace(m_inx, e);
 }
 