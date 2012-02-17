XCSoar/XCSoar

commit e903b5a280b1ac4b66226f6696397ab7222f6073

AbstractTask: add attribute "mc_lpf_valid"
Keeps track of mc_lpf.  Now the polar isn't required anymore in
ResetAutoMC().

src/Engine/Task/Tasks/AbstractTask.cpp
@@ -39,6 +39,7 @@ AbstractTask::AbstractTask(enum Type _type, TaskEvents &te,
    mc_lpf(fixed(8)),
    ce_lpf(fixed(60)),
    em_lpf(fixed(60)),
+   mc_lpf_valid(false),
    trigger_auto(false)
 {
    stats.reset();
@@ -58,6 +59,7 @@ AbstractTask::UpdateAutoMC(GlidePolar &glide_polar,
 
   if (task_behaviour.auto_mc_mode == TaskBehaviour::AUTOMC_CLIMBAVERAGE) {
     stats.mc_best = mc_lpf.reset(fallback_mc);
+    mc_lpf_valid = false;
     trigger_auto = false;
     return false;
   }
@@ -81,7 +83,7 @@ AbstractTask::UpdateAutoMC(GlidePolar &glide_polar,
     trigger_auto = true;
   }
 
-  if (trigger_auto) {
+  if (trigger_auto && mc_lpf_valid) {
     // smooth out updates
     stats.mc_best = std::max(mc_lpf.update(mc_found), fixed_zero);
     glide_polar.SetMC(stats.mc_best);
@@ -280,7 +282,7 @@ AbstractTask::UpdateStatsTimes(const AircraftState &state)
 void
 AbstractTask::ResetAutoMC()
 {
-  stats.mc_best = mc_lpf.reset(glide_polar.GetMC());
+  mc_lpf_valid = false;
   trigger_auto = false;
 }
 

src/Engine/Task/Tasks/AbstractTask.hpp
@@ -68,6 +68,11 @@ private:
   /** low pass filter on effective MC calculations */
   Filter em_lpf;
 
+  /**
+   * True when #mc_lpf has been initialised.
+   */
+  bool mc_lpf_valid;
+
   /** whether auto MC has been triggered (above final glide) */
   bool trigger_auto;
 

commit 5d5efa38408d6342c77282e1c7d53db51b8aa512

Engine/Task/Tasks/AbstractTask: Fix final glide AutoMC calculation
Fixes trac issue #1784.

mc_lpf_valid was never set to true.

Regression was introduced in commit e903b5a280b1ac4b66226f6696397ab7222f6073.

src/Engine/Task/Tasks/AbstractTask.cpp
@@ -58,7 +58,7 @@ AbstractTask::UpdateAutoMC(GlidePolar &glide_polar,
 
   if (task_behaviour.auto_mc_mode == TaskBehaviour::AutoMCMode::CLIMBAVERAGE) {
     stats.mc_best = mc_lpf.reset(fallback_mc);
-    mc_lpf_valid = false;
+    mc_lpf_valid = true;
     trigger_auto = false;
     return false;
   }
@@ -89,6 +89,7 @@ AbstractTask::UpdateAutoMC(GlidePolar &glide_polar,
   } else {
     // reset lpf so will be smooth next time it becomes active
     stats.mc_best = mc_lpf.reset(fallback_mc);
+    mc_lpf_valid = true;
   }
 
   return trigger_auto;