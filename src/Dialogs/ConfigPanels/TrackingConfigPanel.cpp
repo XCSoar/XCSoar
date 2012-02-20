c436ed4fe006c0dc524dcb29a61acbcb3aef99f4
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-02-21 00:34:15 +0100

    TrackingConfigPanel: Added DHV tracking server
    Implements ticket #1757

--- src/Dialogs/ConfigPanels/TrackingConfigPanel.cpp
+++ src/Dialogs/ConfigPanels/TrackingConfigPanel.cpp
@@ -81,6 +81,7 @@ TrackingConfigPanel::OnModified(DataField &df)
 static const StaticEnumChoice  server_list[] = {
   { 0, _T("www.livetrack24.com") },
   { 1, _T("test.livetrack24.com") },
+  { 2, _T("livexc.dhv1.de") },
   { 0 },
 };
 
