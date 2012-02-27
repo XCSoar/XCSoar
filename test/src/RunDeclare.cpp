4fc652d0b57dcdf4fbe11ba07cc4056896bc9850
Max Kellermann <max@duempel.org>
2012-02-27 23:53:46 +0100

    test/RunDeclare: use the Args command line parser

--- test/src/RunDeclare.cpp
+++ test/src/RunDeclare.cpp
@@ -30,9 +30,11 @@ Copyright_License {
 #include "Plane/Plane.hpp"
 #include "Engine/Navigation/GeoPoint.hpp"
 #include "Engine/Waypoint/Waypoints.hpp"
-#include "OS/PathName.hpp"
 #include "Operation/ConsoleOperationEnvironment.hpp"
 #include "Profile/DeviceConfig.hpp"
+
+#define MORE_USAGE
+#include "OS/Args.hpp"
 
 #ifdef HAVE_POSIX
 #include "Device/Port/TTYPort.hpp"
@@ -41,6 +43,17 @@ Copyright_License {
 #endif
 
 #include <stdio.h>
+
+void
+PrintMoreUsage()
+{
+  fputs("Where DRIVER is one of:\n", stderr);
+
+  const struct DeviceRegister *driver;
+  for (unsigned i = 0; (driver = GetDriverByIndex(i)) != NULL; ++i)
+    if (driver->CanDeclare())
+      _ftprintf(stderr, _T("\t%s\n"), driver->name);
+}
 
 bool
 NMEAParser::ReadGeoPoint(NMEAInputLine &line, GeoPoint &value_r)
@@ -61,24 +74,18 @@ MakeWaypoint(const TCHAR *name, int altitude,
 
 int main(int argc, char **argv)
 {
-  if (argc != 4) {
-    fprintf(stderr, "Usage: %s DRIVER PORT BAUD\n"
-            "Where DRIVER is one of:\n", argv[0]);
+  Args args(argc, argv, "DRIVER PORT BAUD");
 
-    const struct DeviceRegister *driver;
-    for (unsigned i = 0; (driver = GetDriverByIndex(i)) != NULL; ++i)
-      if (driver->CanDeclare())
-        _ftprintf(stderr, _T("\t%s\n"), driver->name);
-
-    return EXIT_FAILURE;
-  }
-
-  PathName driver_name(argv[1]);
-  PathName port_name(argv[2]);
+  tstring _driver_name = args.ExpectNextT();
+  const TCHAR *driver_name = _driver_name.c_str();
+  tstring _port_name = args.ExpectNextT();
+  const TCHAR *port_name = _port_name.c_str();
+  const char *baud_string = args.ExpectNext();
+  args.ExpectEnd();
 
   DeviceConfig config;
   config.Clear();
-  config.baud_rate = atoi(argv[3]);
+  config.baud_rate = atoi(baud_string);
 
 #ifdef HAVE_POSIX
   TTYPort port(port_name, config.baud_rate, *(Port::Handler *)NULL);
@@ -108,12 +115,12 @@ int main(int argc, char **argv)
 
   const struct DeviceRegister *driver = FindDriverByName(driver_name);
   if (driver == NULL) {
-    fprintf(stderr, "No such driver: %s\n", argv[1]);
+    _ftprintf(stderr, _T("No such driver: %s\n"), driver_name);
     return EXIT_FAILURE;
   }
 
   if (!driver->CanDeclare()) {
-    fprintf(stderr, "Not a logger driver: %s\n", argv[1]);
+    _ftprintf(stderr, _T("Not a logger driver: %s\n"), driver_name);
     return EXIT_FAILURE;
   }
 
@@ -123,7 +130,7 @@ int main(int argc, char **argv)
 
   ConsoleOperationEnvironment env;
   if (!device->Open(env)) {
-    fprintf(stderr, "Failed to open driver: %s\n", argv[1]);
+    _ftprintf(stderr, _T("Failed to open driver: %s\n"), driver_name);
     return EXIT_FAILURE;
   }
 
