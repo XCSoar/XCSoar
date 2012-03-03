001e4c02775d715e6bed65b6d3210f819bbbc3d6
Max Kellermann <max@duempel.org>
2012-03-03 09:46:40 +0100

    ManageCAI302Dialog: use RowFormWidget

--- src/Dialogs/ManageCAI302Dialog.cpp
+++ src/Dialogs/ManageCAI302Dialog.cpp
@@ -22,132 +22,88 @@ Copyright_License {
 */
 
 #include "Dialogs/ManageCAI302Dialog.hpp"
+#include "Dialogs/WidgetDialog.hpp"
 #include "Dialogs/Message.hpp"
-#include "Form/Form.hpp"
-#include "Form/ButtonPanel.hpp"
-#include "Screen/Layout.hpp"
-#include "Screen/SingleWindow.hpp"
+#include "Form/RowFormWidget.hpp"
 #include "Language/Language.hpp"
 #include "Operation/MessageOperationEnvironment.hpp"
 #include "Device/Driver/CAI302/Internal.hpp"
 
-static WndForm *dialog;
-static CAI302Device *device;
+class ManageCAI302Widget : public RowFormWidget, private ActionListener {
+  enum Controls {
+    StartLogger,
+    StopLogger,
+    DeleteAllFlights,
+    Reboot,
+  };
 
-static void
-OnCloseClicked(gcc_unused WndButton &button)
+  CAI302Device &device;
+
+public:
+  ManageCAI302Widget(const DialogLook &look, CAI302Device &_device)
+    :RowFormWidget(look), device(_device) {}
+
+  /* virtual methods from Widget */
+  virtual void Prepare(ContainerWindow &parent, const PixelRect &rc);
+
+private:
+  /* virtual methods from ActionListener */
+  virtual void OnAction(int id);
+};
+
+void
+ManageCAI302Widget::Prepare(ContainerWindow &parent, const PixelRect &rc)
 {
-  dialog->SetModalResult(mrOK);
+  AddButton(_("Start Logger"), this, StartLogger);
+  AddButton(_("Stop Logger"), this, StopLogger);
+  AddButton(_("Delete all flights"), this, DeleteAllFlights);
+  AddButton(_("Reboot"), this, Reboot);
 }
 
-static void
-OnStartLogging(gcc_unused WndButton &button)
+void
+ManageCAI302Widget::OnAction(int id)
 {
-  MessageOperationEnvironment env;
-  device->StartLogging(env);
-}
+  switch (id) {
+  case StartLogger:
+    {
+      MessageOperationEnvironment env;
+      device.StartLogging(env);
+    }
+    break;
 
-static void
-OnStopLogging(gcc_unused WndButton &button)
-{
-  MessageOperationEnvironment env;
-  device->StopLogging(env);
-}
+  case StopLogger:
+    {
+      MessageOperationEnvironment env;
+      device.StopLogging(env);
+    }
+    break;
 
-static void
-OnClearLogClicked(gcc_unused WndButton &button)
-{
-  if (MessageBoxX(_("Do you really want to delete all flights from the device?"),
-                  _T("CAI 302"), MB_YESNO) != IDYES)
-    return;
+  case DeleteAllFlights:
+    {
+      if (MessageBoxX(_("Do you really want to delete all flights from the device?"),
+                      _T("CAI 302"), MB_YESNO) != IDYES)
+        return;
 
-  MessageOperationEnvironment env;
-  device->ClearLog(env);
-}
+      MessageOperationEnvironment env;
+      device.ClearLog(env);
+    }
+    break;
 
-static void
-OnRebootClicked(gcc_unused WndButton &button)
-{
-  MessageOperationEnvironment env;
-  device->Reboot(env);
+  case Reboot:
+    {
+      MessageOperationEnvironment env;
+      device.Reboot(env);
+    }
+    break;
+  }
 }
 
 void
 ManageCAI302Dialog(SingleWindow &parent, const DialogLook &look,
-                   Device &_device)
+                   Device &device)
 {
-  device = (CAI302Device *)&_device;
-
-  /* create the dialog */
-
-  WindowStyle dialog_style;
-  dialog_style.Hide();
-  dialog_style.ControlParent();
-
-  dialog = new WndForm(parent, look, parent.get_client_rect(),
-                       _T("CAI 302"), dialog_style);
-
-  ContainerWindow &client_area = dialog->GetClientAreaWindow();
-
-  /* create buttons */
-
-  ButtonPanel buttons(client_area, look);
-  buttons.Add(_("Close"), OnCloseClicked);
-
-  const PixelRect rc = buttons.UpdateLayout();
-
-  /* create the command buttons */
-
-  const UPixelScalar margin = Layout::Scale(2);
-  const UPixelScalar height = Layout::Scale(32);
-
-  ButtonWindowStyle button_style;
-  button_style.TabStop();
-
-  WndButton *button;
-
-  PixelRect brc = rc;
-  brc.left += margin;
-  brc.top += margin;
-  brc.right -= margin;
-  brc.bottom = brc.top + height;
-
-  button = new WndButton(client_area, look, _("Start Logger"),
-                         brc,
-                         button_style,
-                         OnStartLogging);
-  dialog->AddDestruct(button);
-
-  brc.top += height + margin;
-  brc.bottom += height + margin;
-
-  button = new WndButton(client_area, look, _("Stop Logger"),
-                         brc,
-                         button_style,
-                         OnStopLogging);
-  dialog->AddDestruct(button);
-
-  brc.top += height + margin;
-  brc.bottom += height + margin;
-
-  button = new WndButton(client_area, look, _("Delete all flights"),
-                         brc,
-                         button_style,
-                         OnClearLogClicked);
-  dialog->AddDestruct(button);
-
-  brc.top += height + margin;
-  brc.bottom += height + margin;
-
-  button = new WndButton(client_area, look, _("Reboot"),
-                         brc,
-                         button_style,
-                         OnRebootClicked);
-  dialog->AddDestruct(button);
-
-  /* run it */
-
-  dialog->ShowModal();
-
-  delete dialog;
+  WidgetDialog dialog(_T("CAI 302"),
+                      new ManageCAI302Widget(look, (CAI302Device &)device));
+  dialog.AddButton(_("Close"), mrCancel);
+  dialog.ShowModal();
 }
