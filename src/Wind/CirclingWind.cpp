99985ff2d0b79c8ae13d4b325b765fe25f1d0548
Max Kellermann <max@duempel.org>
2012-04-02 14:47:33 +0200

    CirclingWind: call Validity::Clear(), Reset() doesn't exist
    D'oh!

--- src/Wind/CirclingWind.cpp
+++ src/Wind/CirclingWind.cpp
@@ -68,8 +68,8 @@ a number of windmeasurements and calculates a weighted average based on quality.
 void
 CirclingWind::Reset()
 {
-  last_track_available.Reset();
-  last_ground_speed_available.Reset();
+  last_track_available.Clear();
+  last_ground_speed_available.Clear();
   circle_count = 0;
   active = false;
   circle_deg = 0;
