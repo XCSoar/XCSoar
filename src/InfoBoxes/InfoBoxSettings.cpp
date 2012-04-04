538b814888bc4afe50c9d1cd536eab33f36b351d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-04-04 15:54:32 +0200

    InfoBoxSettings: Changed infobox defaults to Nav Altitude instead of GPS

--- src/InfoBoxes/InfoBoxSettings.cpp
+++ src/InfoBoxes/InfoBoxSettings.cpp
@@ -61,11 +61,11 @@ InfoBoxSettings::SetDefaults()
   static gcc_constexpr_data unsigned DFLT_CONFIG_BOXES = 9;
   static gcc_constexpr_data unsigned DFLT_CONFIG_PANELS = 4;
   static gcc_constexpr_data t_InfoBox contents[DFLT_CONFIG_PANELS][DFLT_CONFIG_BOXES] = {
-    { e_WP_Distance,   e_TL_Avg,          e_HeightGPS,        e_HeightAGL,   e_TL_Gain,
+    { e_WP_Distance,   e_TL_Avg,          NavAltitude,        e_HeightAGL,   e_TL_Gain,
       e_MacCready,     e_TL_Time,         e_Thermal_30s,      e_TimeLocal },
-    { e_WP_Distance,   e_Alternate_1_GR,  e_HeightGPS,        e_HeightAGL,   e_WP_AltDiff,
+    { e_WP_Distance,   e_Alternate_1_GR,  NavAltitude,        e_HeightAGL,   e_WP_AltDiff,
       e_MacCready,     e_Speed_GPS,       e_LD_Avg,           e_LD_Cruise },
-    { e_WP_Distance,   e_Alternate_1_GR,  e_HeightGPS,        e_HeightAGL,   e_Fin_AltDiff,
+    { e_WP_Distance,   e_Alternate_1_GR,  NavAltitude,        e_HeightAGL,   e_Fin_AltDiff,
       e_MacCready,     e_Fin_LD,          e_LD_Avg,           e_Fin_Time },
     { e_WP_Name,       e_Fin_TimeLocal,   e_WP_Distance,      e_WP_Time,     e_Fin_Distance,
       e_Fin_Time,      e_TimeLocal,       e_TimeSinceTakeoff, e_CC_Speed }
