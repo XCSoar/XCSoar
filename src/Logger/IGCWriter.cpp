XCSoar/XCSoar

commit 6c86c4b573e4897ad2063920c2e0253da6fa228a

IGCWriter: Fixed Start/EndDeclaration() functions
According to the IGC GNSS specification (Appendix A 3.5.3):

"Two lines in the C-record of the IGC file are for the
takeoff airfield and landing airfield and are for general
information rather than being part of an IGC Flight Declaration.
They can be entered approximately or, if the co-ordinates are
difficult to obtain, the recorder shall default
to 00000000N000000000E for these two lines."

src/Logger/IGCWriter.cpp
@@ -217,13 +217,6 @@ IGCWriter::StartDeclaration(const BrokenDateTime &FirstDateTime,
 {
   assert(FirstDateTime.Plausible());
 
-  // TODO bug: this is causing problems with some analysis software
-  // maybe it's because the date and location fields are bogus
-
-  // JMW added task start declaration line
-
-  // LGCSTKF013945TAKEOFF DETECTED
-
   // IGC GNSS specification 3.6.1
   char buffer[100];
   sprintf(buffer, "C%02u%02u%02u%02u%02u%02u0000000000%02d",
@@ -240,7 +233,7 @@ IGCWriter::StartDeclaration(const BrokenDateTime &FirstDateTime,
 
   // takeoff line
   // IGC GNSS specification 3.6.3
-  writeln("C0000000N00000000ETAKEOFF");
+  writeln("C00000000N000000000ETAKEOFF");
 }
 
 void
@@ -248,7 +241,7 @@ IGCWriter::EndDeclaration(void)
 {
   // TODO bug: this is causing problems with some analysis software
   // maybe it's because the date and location fields are bogus
-  const char start[] = "C0000000N00000000ELANDING";
+  const char start[] = "C00000000N000000000ELANDING";
   writeln(start);
 }
 

test/src/TestLogger.cpp
@@ -69,11 +69,11 @@ static const char *const expect[] = {
   "HFDTM100Datum: WGS-84",
   "I023638FXA3940SIU  ",
   "C040910112233000000000001",
-  "C0000000N00000000ETAKEOFF",
+  "C00000000N000000000ETAKEOFF",
   "C5103117N00742367EBERGNEUSTADT",
   "C5037932N01043567ESUHL",
   "C5103117N00742367EBERGNEUSTADT",
-  "C0000000N00000000ELANDING",
+  "C00000000N000000000ELANDING",
   "B1122385103117N00742367EA004900048700000",
   "E112243my_event",
   "B1122435103117N00742367EA004900048700000",

commit c7d54c8d8c0cc3ef48b1ee11601f238be90dc37e

Logger/IGCWriter: Fix TAKEOFF/LANDING 'C' records
See trac issue #1993.

Regression caused by commit 6c86c4b573e4897ad2063920c2e0253da6fa228a.

src/Logger/IGCWriter.cpp
@@ -236,7 +236,7 @@ IGCWriter::StartDeclaration(const BrokenDateTime &FirstDateTime,
 
   // takeoff line
   // IGC GNSS specification 3.6.3
-  WriteLine("C00000000N000000000ETAKEOFF");
+  WriteLine("C0000000N00000000ETAKEOFF");
 }
 
 void
@@ -244,7 +244,7 @@ IGCWriter::EndDeclaration()
 {
   // TODO bug: this is causing problems with some analysis software
   // maybe it's because the date and location fields are bogus
-  WriteLine("C00000000N000000000ELANDING");
+  WriteLine("C0000000N00000000ELANDING");
 }
 
 void

test/src/TestLogger.cpp
@@ -70,11 +70,11 @@ static const char *const expect[] = {
   "HFDTM100Datum: WGS-84",
   "I023638FXA3940SIU",
   "C040910112233000000000001",
-  "C00000000N000000000ETAKEOFF",
+  "C0000000N00000000ETAKEOFF",
   "C5103117N00742367EBERGNEUSTADT",
   "C5037932N01043567ESUHL",
   "C5103117N00742367EBERGNEUSTADT",
-  "C00000000N000000000ELANDING",
+  "C0000000N00000000ELANDING",
   "B1122385103117N00742367EA004900048700000",
   "E112243my_event",
   "B1122435103117N00742367EA004900048700000",