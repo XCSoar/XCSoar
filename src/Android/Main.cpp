42357375b30aad6cc1c51b3df909f26de7324380
Max Kellermann <max@duempel.org>
2012-05-04 18:49:17 +0200

    Makefile: support Android/MIPS with NDK r8

--- src/Android/Main.cpp
+++ src/Android/Main.cpp
@@ -73,7 +73,7 @@ bool os_haptic_feedback_enabled;
 IOIOHelper *ioio_helper;
 #endif
 
-#ifdef __i386__
+#if defined(__i386__) || defined(__mips__)
 extern "C" {
   /* workaround for
      http://code.google.com/p/android/issues/detail?id=23203 copied
