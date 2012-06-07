824e66121ec3aa8ae26800c2237ed6c17d9165a6
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-06-07 22:50:29 +0200

    TestCSVLine: Added Test1/2() functions
    Gets rid of all those annoying curly braces

--- test/src/TestCSVLine.cpp
+++ test/src/TestCSVLine.cpp
@@ -25,90 +25,83 @@
 
 #include <cstring>
 
+static void
+Test1()
+{
+  CSVLine line("1,2,x,4,5,6,7,8,9,10");
+
+  // Test read(int)
+  ok1(line.Read(-1) == 1);
+
+  // Test rest()
+  ok1(strcmp(line.Rest(), "2,x,4,5,6,7,8,9,10") == 0);
+
+  // Test skip()
+  ok1(line.Skip() == 1);
+
+  // Test skip(int)
+  line.Skip(3);
+  ok1(line.Read(-1) == 6);
+
+  // Test read_first_char()
+  ok1(line.ReadFirstChar() == '7');
+
+  // Test read(char)
+  char temp[10];
+  line.Read(temp, 10);
+  ok1(strcmp(temp, "8") == 0);
+
+  // Test read_compare(char)
+  ok1(line.ReadCompare("9"));
+
+  // Test read(long)
+  ok1(line.Read(-1l) == 10l);
+
+  // Test default-value at line-end
+  ok1(line.Read(11) == 11);
+
+  // Test default-value at line-end
+  int temp_int;
+  ok1(!line.ReadChecked(temp_int));
+}
+
+static void
+Test2()
+{
+  CSVLine line("A0,4.5555,4.5555,42,0,1.337,42.42,42,xxx");
+
+  // Test read_hex()
+  ok1(line.ReadHex(-1) == 160);
+
+  // Test read(double)
+  ok1(equals(fixed(line.Read(0.0)), 4.5555));
+
+  // Test read(fixed)
+  ok1(equals(line.Read(fixed_zero), 4.5555));
+
+  // Test read(bool)
+  ok1(line.Read(false) == true);
+  ok1(line.Read(false) == false);
+
+  // Test read_checked()
+  double temp_double;
+  ok1(line.ReadChecked(temp_double) && equals(fixed(temp_double), 1.337));
+
+  fixed temp_fixed;
+  ok1(line.ReadChecked(temp_fixed) && equals(temp_fixed, 42.42));
+
+  int temp_int;
+  ok1(line.ReadChecked(temp_int) && temp_int == 42);
+  ok1(!line.ReadChecked(temp_int) && temp_int == 42);
+}
+
 int
 main(int argc, char **argv)
 {
   plan_tests(19);
 
-  {
-    CSVLine line("1,2,x,4,5,6,7,8,9,10");
-
-    {
-      // Test read(long)
-      int temp = line.Read(-1);
-      ok1(temp == 1);
-    }
-
-    // Test rest()
-    ok1(strcmp(line.Rest(), "2,x,4,5,6,7,8,9,10") == 0);
-
-    // Test skip()
-    ok1(line.Skip() == 1);
-
-    // Test skip(int)
-    line.Skip(3);
-    ok1(line.Read(-1) == 6);
-
-    // Test read_first_char()
-    ok1(line.ReadFirstChar() == '7');
-
-    {
-      // Test read(char)
-      char temp[10];
-      line.Read(temp, 10);
-      ok1(strcmp(temp, "8") == 0);
-    }
-
-    // Test read_compare(char)
-    ok1(line.ReadCompare("9"));
-
-    {
-      // Test read(long)
-      long temp = line.Read(-1);
-      ok1(temp == 10);
-    }
-
-    // Test default-value at line-end
-    ok1(line.Read(11) == 11);
-
-    {
-      // Test default-value at line-end
-      int temp;
-      ok1(!line.ReadChecked(temp));
-    }
-  }
-
-  {
-    CSVLine line("A0,4.5555,4.5555,42,0,1.337,42.42,42,xxx");
-
-    // Test read_hex()
-    ok1(line.ReadHex(-1) == 160);
-
-    // Test read(double)
-    ok1(equals(fixed(line.Read(0.0)), 4.5555));
-
-    // Test read(fixed)
-    ok1(equals(line.Read(fixed_zero), 4.5555));
-
-    // Test read(bool)
-    ok1(line.Read(false) == true);
-    ok1(line.Read(false) == false);
-
-    // Test read_checked()
-    {
-      double temp;
-      ok1(line.ReadChecked(temp) && equals(fixed(temp), 1.337));
-    }
-    {
-      fixed temp;
-      ok1(line.ReadChecked(temp) && equals(temp, 42.42));
-    }
-    {
-      int temp;
-      ok1(line.ReadChecked(temp) && temp == 42);
-      ok1(!line.ReadChecked(temp) && temp == 42);
-    }
-  }
+  Test1();
+  Test2();
 
   return exit_status();
 }
