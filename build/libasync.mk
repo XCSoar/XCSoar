b133516721ee0701e0531ef32b1099dc6e7ca507
Max Kellermann <max@duempel.org>
2012-07-03 09:20:15 +0200

    IO/Async/SocketThread: socket event thread for WIN32
    We can't use the I/O thread on WIN32 because it does not support
    poll(), and pipes cannot be used in select().  This new utility class
    allows using a similar method.

--- build/libasync.mk
+++ build/libasync.mk
@@ -6,6 +6,9 @@ ifeq ($(HAVE_POSIX),y)
 ASYNC_SOURCES += \
 	$(SRC)/IO/Async/IOThread.cpp \
 	$(SRC)/IO/Async/GlobalIOThread.cpp
+else
+ASYNC_SOURCES += \
+	$(SRC)/IO/Async/SocketThread.cpp
 endif
 
 $(eval $(call link-library,async,ASYNC))
