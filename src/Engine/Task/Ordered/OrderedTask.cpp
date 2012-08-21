XCSoar/XCSoar

commit e62452dc5f0babd7c123e8aa074f2c5dc237a73d

OrderedTask, ...: use range-based for

src/Engine/Task/Ordered/OrderedTask.cpp
@@ -140,9 +140,8 @@ OrderedTask::UpdateGeometry()
     (*i)->ScanProjection(task_projection);
   }
   // ... and optional start points
-  for (auto i = optional_start_points.cbegin(),
-         end = optional_start_points.cend(); i != end; ++i)
-    (*i)->ScanProjection(task_projection);
+  for (const OrderedTaskPoint *tp : optional_start_points)
+    tp->ScanProjection(task_projection);
 
   // projection can now be determined
   task_projection.Update();
@@ -153,12 +152,11 @@ OrderedTask::UpdateGeometry()
 
   // now that the task projection is stable, and oz is stable,
   // calculate the bounding box in projected coordinates
-  for (auto i = task_points.cbegin(), end = task_points.cend(); i != end; ++i)
-    (*i)->UpdateBoundingBox(task_projection);
+  for (const auto tp : task_points)
+    tp->UpdateBoundingBox(task_projection);
 
-  for (auto i = optional_start_points.cbegin(),
-         end = optional_start_points.cend(); i != end; ++i)
-    (*i)->UpdateBoundingBox(task_projection);
+  for (const auto tp : optional_start_points)
+    tp->UpdateBoundingBox(task_projection);
 
   // update stats so data can be used during task construction
   /// @todo this should only be done if not flying! (currently done with has_entered)
@@ -539,9 +537,8 @@ OrderedTask::SetNeighbours(unsigned position)
   task_points[position]->SetNeighbours(prev, next);
 
   if (position==0) {
-    for (auto i = optional_start_points.begin(),
-           end = optional_start_points.end(); i != end; ++i)
-      (*i)->SetNeighbours(prev, next);
+    for (const auto tp : optional_start_points)
+      tp->SetNeighbours(prev, next);
   }
 }
 
@@ -929,9 +926,9 @@ OrderedTask::CalcGradient(const AircraftState &state) const
 
   // Iterate through remaining turnpoints
   fixed distance = fixed_zero;
-  for (auto i = task_points.cbegin(), end = task_points.cend(); i != end; ++i)
+  for (const OrderedTaskPoint *tp : optional_start_points)
     // Sum up the leg distances
-    distance += (*i)->GetVectorRemaining(state.location).distance;
+    distance += tp->GetVectorRemaining(state.location).distance;
 
   if (!distance)
     return fixed_zero;
@@ -944,8 +941,8 @@ static void
 Visit(const OrderedTask::OrderedTaskPointVector &points,
       TaskPointConstVisitor &visitor)
 {
-  for (auto it = points.begin(), end = points.end(); it != end; ++it)
-    visitor.Visit(**it);
+  for (const TaskPoint *tp : points)
+    visitor.Visit(*tp);
 }
 
 void 
@@ -1121,8 +1118,8 @@ OrderedTask::GetFinishState() const
 bool
 OrderedTask::HasTargets() const
 {
-  for (auto i = task_points.cbegin(), end = task_points.cend(); i != end; ++i)
-    if ((*i)->HasTarget())
+  for (const OrderedTaskPoint *tp : task_points)
+    if (tp->HasTarget())
       return true;
 
   return false;
@@ -1163,12 +1160,11 @@ OrderedTask::Clone(const TaskBehaviour &tb) const
   new_task->ordered_behaviour = ordered_behaviour;
 
   new_task->SetFactory(factory_mode);
-  for (auto i = task_points.cbegin(), end = task_points.cend(); i != end; ++i)
-    new_task->Append(**i);
+  for (const OrderedTaskPoint *tp : task_points)
+    new_task->Append(*tp);
 
-  for (auto i = optional_start_points.cbegin(),
-         end = optional_start_points.cend(); i != end; ++i)
-    new_task->AppendOptionalStart(**i);
+  for (const OrderedTaskPoint *tp : optional_start_points)
+    new_task->AppendOptionalStart(*tp);
 
   new_task->active_task_point = active_task_point;
   new_task->UpdateGeometry();

src/Dialogs/XML.cpp
@@ -799,11 +799,11 @@ LoadChild(SubForm &form, ContainerWindow &parent, const PixelRect &parent_rc,
 
     window = tabbed;
 
-    for (auto i = node.begin(), end = node.end(); i != end; ++i) {
+    for (const auto &i : node) {
       // Load each child control from the child nodes
       Window *child = LoadChild(form, *tabbed, tabbed->GetClientRect(),
                                 lookup_table,
-                                *i);
+                                i);
       if (child != NULL)
         tabbed->AddClient(child);
     }

src/Engine/Task/Ordered/Points/OrderedTaskPoint.cpp
@@ -188,9 +188,8 @@ OrderedTaskPoint::ScanProjection(TaskProjection &task_projection) const
   task_projection.Scan(GetLocation());
   #define fixed_steps fixed(0.05)
 
-  const OZBoundary boundary = GetBoundary();
-  for (auto i = boundary.begin(), end = boundary.end(); i != end; ++i)
-    task_projection.Scan(*i);
+  for (const auto &i : GetBoundary())
+    task_projection.Scan(i);
 }
 
 void
@@ -198,9 +197,8 @@ OrderedTaskPoint::UpdateBoundingBox(const TaskProjection &task_projection)
 {
   flat_bb = FlatBoundingBox(task_projection.ProjectInteger(GetLocation()));
 
-  const OZBoundary boundary = GetBoundary();
-  for (auto i = boundary.begin(), end = boundary.end(); i != end; ++i)
-    flat_bb.Expand(task_projection.ProjectInteger(*i));
+  for (const auto &i : GetBoundary())
+    flat_bb.Expand(task_projection.ProjectInteger(i));
 
   flat_bb.ExpandByOne(); // add 1 to fix rounding
 }

src/Engine/Task/Points/SampledTaskPoint.cpp
@@ -89,11 +89,8 @@ SampledTaskPoint::UpdateOZ(const TaskProjection &projection)
   boundary_points.clear();
 
   if (boundary_scored) {
-    const OZBoundary boundary = GetBoundary();
-    for (auto i = boundary.begin(), end = boundary.end(); i != end; ++i) {
-      SearchPoint sp(*i);
+    for (const SearchPoint sp : GetBoundary())
       boundary_points.push_back(sp);
-    }
 
     boundary_points.PruneInterior();
   } else {

src/Engine/Task/Stats/TaskSummary.hpp
@@ -60,9 +60,9 @@ struct TaskSummary {
 
     p_remaining = d_remaining/d_planned;
     fixed p = fixed_zero;
-    for (auto it = pts.begin(); it != pts.end(); ++it) {
-      p+= it->d_planned / d_planned;
-      it->p = p;
+    for (auto &i : pts) {
+      p += i.d_planned / d_planned;
+      i.p = p;
     }
   }
 };

src/Engine/Task/Unordered/AbortTask.cpp
@@ -54,8 +54,8 @@ AbortTask::SetTaskBehaviour(const TaskBehaviour &tb)
 {
   UnorderedTask::SetTaskBehaviour(tb);
 
-  for (auto i = task_points.begin(), end = task_points.end(); i != end; ++i)
-    i->SetTaskBehaviour(tb);
+  for (auto &tp : task_points)
+    tp.SetTaskBehaviour(tb);
 }
 
 void 
@@ -301,8 +301,8 @@ AbortTask::CheckTransitions(const AircraftState &, const AircraftState&)
 void 
 AbortTask::AcceptTaskPointVisitor(TaskPointConstVisitor& visitor) const
 {
-  for (auto i = task_points.begin(), end = task_points.end(); i != end; ++i)
-    visitor.Visit((const TaskPoint &)*i);
+  for (const TaskPoint &tp : task_points)
+    visitor.Visit(tp);
 }
 
 void

src/Form/ButtonPanel.cpp
@@ -33,8 +33,8 @@ ButtonPanel::ButtonPanel(ContainerWindow &_parent, const DialogLook &_look)
 
 ButtonPanel::~ButtonPanel()
 {
-  for (auto it = buttons.begin(), end = buttons.end(); it != end; ++it)
-    delete *it;
+  for (const auto i : buttons)
+    delete i;
 }
 
 PixelRect

src/Form/DataField/ComboList.cpp
@@ -47,8 +47,8 @@ ComboList::Item::~Item()
 void
 ComboList::Clear()
 {
-  for (auto it = items.begin(), end = items.end(); it != end; ++it)
-    delete *it;
+  for (auto i : items)
+    delete i;
 
   items.clear();
 }

commit 9a62cfdc656eb2d7631009d88b5278f58a232037

Engine/Task/Ordered/OrderedTask: Fix final GR calculation
Regression introduced in commit e62452dc5f0babd7c123e8aa074f2c5dc237a73d.

Fixes trac issue #2256.

src/Engine/Task/Ordered/OrderedTask.cpp
@@ -926,7 +926,7 @@ OrderedTask::CalcGradient(const AircraftState &state) const
 
   // Iterate through remaining turnpoints
   fixed distance = fixed_zero;
-  for (const OrderedTaskPoint *tp : optional_start_points)
+  for (const OrderedTaskPoint *tp : task_points)
     // Sum up the leg distances
     distance += tp->GetVectorRemaining(state.location).distance;
 