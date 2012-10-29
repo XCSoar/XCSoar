4f8522874972d3bdd4515d98ca1ca8b54c790278
Max Kellermann <max@duempel.org>
2012-10-29 11:37:11 +0100

    LiftDatabaseComputer: use cast instead of floor()
    Casting a positive number does the same.

--- src/Computer/LiftDatabaseComputer.cpp
+++ src/Computer/LiftDatabaseComputer.cpp
@@ -66,7 +66,7 @@ heading_to_index(Angle &heading)
   static const Angle afive = Angle::Degrees(fixed(5));
 
   unsigned index = (unsigned)
-      floor((heading + afive).AsBearing().Degrees() / 10);
+      ((heading + afive).AsBearing().Degrees() / 10);
 
   return std::max(0u, std::min(35u, index));
 }
