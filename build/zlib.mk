cb3460dc6798b78dbbd38fd50ce7b3d68de0fe75
Max Kellermann <max@duempel.org>
2012-11-06 10:12:35 +0100

    zlib: remove unused sources

--- build/zlib.mk
+++ build/zlib.mk
@@ -27,15 +27,10 @@ else
 ZLIB_SRC_DIR = $(SRC)/zlib
 ZLIB_SOURCES = \
 	$(ZLIB_SRC_DIR)/adler32.c \
-	$(ZLIB_SRC_DIR)/compress.c \
 	$(ZLIB_SRC_DIR)/crc32.c \
-	$(ZLIB_SRC_DIR)/deflate.c \
-	$(ZLIB_SRC_DIR)/infback.c \
 	$(ZLIB_SRC_DIR)/inffast.c \
 	$(ZLIB_SRC_DIR)/inflate.c \
 	$(ZLIB_SRC_DIR)/inftrees.c \
-	$(ZLIB_SRC_DIR)/trees.c \
-	$(ZLIB_SRC_DIR)/uncompr.c \
 	$(ZLIB_SRC_DIR)/zutil.c
 
 ZLIB_LDLIBS =
