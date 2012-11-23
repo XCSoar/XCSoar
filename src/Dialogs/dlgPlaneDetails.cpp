XCSoar/XCSoar

commit 64590f45270999e5afea795a9b26b552319043e5

dlgPlaneDetails: use StringDataField instead of buttons

src/Dialogs/dlgPlaneDetails.cpp
@@ -24,10 +24,10 @@ Copyright_License {
 #include "Dialogs/Planes.hpp"
 #include "Dialogs/CallBackTable.hpp"
 #include "Dialogs/XML.hpp"
-#include "Dialogs/TextEntry.hpp"
 #include "Form/Form.hpp"
 #include "Form/Util.hpp"
 #include "Form/Button.hpp"
+#include "Form/DataField/String.hpp"
 #include "Screen/Layout.hpp"
 #include "Plane/Plane.hpp"
 #include "Language/Language.hpp"
@@ -41,7 +41,8 @@ static void
 UpdateCaption()
 {
   StaticString<128> tmp;
-  tmp.Format(_T("%s: %s"), _("Plane Details"), plane.registration.c_str());
+  tmp.Format(_T("%s: %s"), _("Plane Details"),
+             GetFormValueString(*dialog, _T("Registration")));
   dialog->SetCaption(tmp);
 }
 
@@ -56,11 +57,9 @@ UpdateButton(const TCHAR *button, const TCHAR *caption)
 static void
 Update()
 {
-  UpdateCaption();
-
-  UpdateButton(_T("RegistrationButton"), plane.registration);
-  UpdateButton(_T("CompetitionIDButton"), plane.competition_id);
-  UpdateButton(_T("TypeButton"), plane.type);
+  LoadFormProperty(*dialog, _T("Registration"), plane.registration);
+  LoadFormProperty(*dialog, _T("CompetitionID"), plane.competition_id);
+  LoadFormProperty(*dialog, _T("Type"), plane.type);
   UpdateButton(_T("PolarButton"), plane.polar_name);
 
   LoadFormProperty(*dialog, _T("HandicapEdit"), plane.handicap);
@@ -69,11 +68,15 @@ Update()
   LoadFormProperty(*dialog, _T("DumpTimeEdit"), plane.dump_time);
   LoadFormProperty(*dialog, _T("MaxSpeedEdit"),
                    UnitGroup::HORIZONTAL_SPEED, plane.max_speed);
+
+  UpdateCaption();
 }
 
 static void
 UpdatePlane()
 {
+  SaveFormProperty(*dialog, _T("Registration"), plane.registration);
+  SaveFormProperty(*dialog, _T("CompetitionID"), plane.competition_id);
   SaveFormProperty(*dialog, _T("HandicapEdit"), plane.handicap);
   SaveFormProperty(*dialog, _T("WingAreaEdit"), plane.wing_area);
   SaveFormProperty(*dialog, _T("MaxBallastEdit"), plane.max_ballast);
@@ -95,37 +98,10 @@ CancelClicked(gcc_unused WndButton &button)
 }
 
 static void
-RegistrationClicked(gcc_unused WndButton &button)
+OnRegistrationData(DataField *sender, DataField::DataAccessMode mode)
 {
-  if (!dlgTextEntryShowModal(*(SingleWindow *)dialog->GetRootOwner(),
-                             plane.registration.buffer(),
-                             plane.registration.MAX_SIZE))
-    return;
-
-  UpdateCaption();
-  UpdateButton(_T("RegistrationButton"), plane.registration);
-}
-
-static void
-CompetitionIDClicked(gcc_unused WndButton &button)
-{
-  if (!dlgTextEntryShowModal(*(SingleWindow *)dialog->GetRootOwner(),
-                             plane.competition_id.buffer(),
-                             plane.competition_id.MAX_SIZE))
-    return;
-
-  UpdateButton(_T("CompetitionIDButton"), plane.competition_id);
-}
-
-static void
-TypeClicked(gcc_unused WndButton &button)
-{
-  if (!dlgTextEntryShowModal(*(SingleWindow *)dialog->GetRootOwner(),
-                             plane.type.buffer(),
-                             plane.type.MAX_SIZE))
-    return;
-
-  UpdateButton(_T("TypeButton"), plane.type);
+  if (mode == DataField::daChange)
+    UpdateCaption();
 }
 
 static void
@@ -138,9 +114,7 @@ PolarClicked(gcc_unused WndButton &button)
 static constexpr CallBackTableEntry CallBackTable[] = {
   DeclareCallBackEntry(OKClicked),
   DeclareCallBackEntry(CancelClicked),
-  DeclareCallBackEntry(RegistrationClicked),
-  DeclareCallBackEntry(CompetitionIDClicked),
-  DeclareCallBackEntry(TypeClicked),
+  DeclareCallBackEntry(OnRegistrationData),
   DeclareCallBackEntry(PolarClicked),
   DeclareCallBackEntry(NULL)
 };

Data/Dialogs/dlgPlaneDetails.xml
@@ -1,20 +1,17 @@
 <?xml version="1.0"?>
 
 <Form Caption="Plane Details">
-  <Panel Height="26">
-    <Label Caption="Registration" X="2" Y="6" Height="-2" Width="88" />
-    <Button Name="RegistrationButton" X="90" Y="2" Width="-2" Height="-2" OnClick="RegistrationClicked"/>
-  </Panel>
+  <Edit Name="Registration" Caption="Registration" Height="22" CaptionWidth="88">
+    <DataField DataType="string" OnDataAccess="OnRegistrationData"/>
+  </Edit>
 
-  <Panel Height="26">
-    <Label Caption="Comp. ID" X="2" Y="6" Height="-2" Width="88" />
-    <Button Name="CompetitionIDButton" X="90" Y="2" Width="-2" Height="-2" OnClick="CompetitionIDClicked"/>
-  </Panel>
+  <Edit Name="CompetitionID" Caption="Comp. ID" Height="22" CaptionWidth="88">
+    <DataField DataType="string"/>
+  </Edit>
 
-  <Panel Height="26">
-    <Label Caption="Type" X="2" Y="6" Height="-2" Width="88" />
-    <Button Name="TypeButton" X="90" Y="2" Width="-2" Height="-2" OnClick="TypeClicked"/>
-  </Panel>
+  <Edit Name="Type" Caption="Type" Height="22" CaptionWidth="88">
+    <DataField DataType="string"/>
+  </Edit>
 
   <Panel Height="26">
     <Label Caption="Handicap" X="2" Y="6" Height="-2" Width="88" />

Data/Dialogs/dlgPlaneDetails_L.xml
@@ -2,16 +2,22 @@
 
 <Form Caption="Plane Details">
   <Panel Height="26">
-    <Label Caption="Registration" X="2" Y="6" Height="-2" Width="88" />
-    <Button Name="RegistrationButton" X="90" Y="2" Width="-112" Height="-2" OnClick="RegistrationClicked"/>
+    <Edit Name="Registration" Caption="Registration"
+          X="2" Y="2" Width="-112" Height="-2" CaptionWidth="88">
+      <DataField DataType="string" OnDataAccess="OnRegistrationData"/>
+    </Edit>
 
-    <Label Caption="Comp. ID" X="-107" Y="6" Height="-2" Width="-54" />
-    <Button Name="CompetitionIDButton" X="-52" Y="2" Width="-2" Height="-2" OnClick="CompetitionIDClicked"/>
+    <Edit Name="CompetitionID" Caption="Comp. ID"
+          X="-107" Y="2" Width="-2" Height="-2" CaptionWidth="53">
+      <DataField DataType="string"/>
+    </Edit>
   </Panel>
 
   <Panel Height="26">
-    <Label Caption="Type" X="2" Y="6" Height="-2" Width="88" />
-    <Button Name="TypeButton" X="90" Y="2" Width="-112" Height="-2" OnClick="TypeClicked"/>
+    <Edit Name="Type" Caption="Type"
+          X="2" Y="2" Width="-112" Height="-2" CaptionWidth="88">
+      <DataField DataType="string"/>
+    </Edit>
 
     <Label Caption="Handicap" X="-107" Y="6" Height="-2" Width="-54" />
     <Edit Name="HandicapEdit" X="-52" Y="2" Width="-2" Height="-2">

commit 9e67ab85fea1f3b8a25199815ec929c5beb0b13a

Dialogs/dlgPlaneDetails: Preserve modifications to non-polar data while changing polar
Fixes trac issue #2449.

Regression introduced in commit 64590f45270999e5afea795a9b26b552319043e5.

src/Dialogs/dlgPlaneDetails.cpp
@@ -108,6 +108,7 @@ OnRegistrationData(DataField *sender, DataField::DataAccessMode mode)
 static void
 PolarClicked()
 {
+  UpdatePlane();
   dlgPlanePolarShowModal(*(SingleWindow*)dialog->GetRootOwner(), plane);
   Update();
 }