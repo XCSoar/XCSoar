9b924c8933d11c453126765b2441e0065819e81d
Max Kellermann <max@duempel.org>
2012-11-29 02:43:20 +0100

    Makefile: disable -O4 when compiling LLVM bitcode
    clang insists on applying the gold plugin when passing -O4, which is
    not necessary when we link LLVM bitcode manually.

--- build/llvm.mk
+++ build/llvm.mk
@@ -12,7 +12,7 @@ endif
 
 DEPFLAGS = -MD -MF $(DEPFILE) -MT $@
 
-ifneq ($(DEBUG),y)
+ifeq ($(DEBUG)$(LLVM),yn)
 OPTIMIZE := -O4 -DNDEBUG -Wuninitialized
 AR += --plugin /usr/local/lib/LLVMgold.so
 endif
