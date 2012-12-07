XCSoar/XCSoar

commit cb56af7d9d5b4468c3b6d5ef91641ad8a158fc57

Dialogs/*: don't use ListControl::SetPaintItemCallback()
Use the ListItemRenderer interface instead.

src/Dialogs/Waypoint/dlgWaypointDetails.cpp
@@ -285,8 +285,16 @@ OnImagePaint(gcc_unused WndOwnerDrawFrame *Sender, Canvas &canvas)
 
 // TODO: support other platforms
 
-static void
-OnFileListEnter(gcc_unused unsigned i)
+class WaypointExternalFileListHandler : public ListControl::Handler {
+public:
+  virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
+                           unsigned idx) gcc_override;
+
+  virtual void OnCursorMoved(unsigned index) gcc_override;
+};
+
+void
+WaypointExternalFileListHandler::OnCursorMoved(unsigned i)
 {
   auto file = waypoint->files_external.begin();
   std::advance(file, i);
@@ -297,8 +305,10 @@ OnFileListEnter(gcc_unused unsigned i)
   native_view->openFile(path);
 }
 
-static void
-OnFileListItemPaint(Canvas &canvas, const PixelRect paint_rc, unsigned i)
+void
+WaypointExternalFileListHandler::OnPaintItem(Canvas &canvas,
+                                             const PixelRect paint_rc,
+                                             unsigned i)
 {
   auto file = waypoint->files_external.begin();
   std::advance(file, i);
@@ -389,12 +399,11 @@ dlgWaypointDetailsShowModal(SingleWindow &parent, const Waypoint &_waypoint,
   wDetailsText->SetText(waypoint->details.c_str());
 
 #ifdef ANDROID
+  WaypointExternalFileListHandler handler;
   int num_files = std::distance(waypoint->files_external.begin(),
                                 waypoint->files_external.end());
   if (num_files > 0) {
-    wFilesList->SetPaintItemCallback(OnFileListItemPaint);
-    wFilesList->SetCursorCallback(OnFileListEnter);
-    wFilesList->SetActivateCallback(OnFileListEnter);
+    wFilesList->SetHandler(&handler);
 
     unsigned list_height = wFilesList->GetItemHeight() * std::min(num_files, 5);
     wFilesList->Resize(wFilesList->GetWidth(), list_height);

src/Dialogs/Airspace/AirspaceList.cpp
@@ -55,6 +55,12 @@ Copyright_License {
 #include <stdlib.h>
 #include <stdio.h>
 
+class AirspaceListDialog : public ListItemRenderer {
+public:
+  virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
+                           unsigned idx) gcc_override;
+};
+
 /**
  * Special enum integer value for "filter disabled".
  */
@@ -203,8 +209,9 @@ AirspaceFilterListener::OnModified(DataField &df)
   UpdateList();
 }
 
-static void
-OnPaintListItem(Canvas &canvas, const PixelRect rc, unsigned i)
+void
+AirspaceListDialog::OnPaintItem(Canvas &canvas, const PixelRect rc,
+                                unsigned i)
 {
   if (airspace_list.empty()) {
     assert(i == 0);
@@ -349,7 +356,6 @@ PrepareAirspaceSelectDialog()
   airspace_list_control = (ListControl*)dialog->FindByName(_T("frmAirspaceList"));
   assert(airspace_list_control != NULL);
   airspace_list_control->SetActivateCallback(OnAirspaceListEnter);
-  airspace_list_control->SetPaintItemCallback(OnPaintListItem);
   airspace_list_control->SetItemHeight(AirspaceListRenderer::GetHeight(dialog_look));
 
   name_control = (WndProperty*)dialog->FindByName(_T("prpFltName"));
@@ -380,6 +386,9 @@ ShowAirspaceListDialog(const Airspaces &_airspaces,
   location = XCSoarInterface::Basic().location;
 
   PrepareAirspaceSelectDialog();
+
+  AirspaceListDialog dialog2;
+  airspace_list_control->SetItemRenderer(&dialog2);
 
   AirspaceFilterListener listener;
   name_control->GetDataField()->SetListener(&listener);

src/Dialogs/Airspace/dlgAirspace.cpp
@@ -48,13 +48,35 @@ Copyright_License {
 
 #include <assert.h>
 
-static ListControl *airspace_list = NULL;
+class AirspaceSettingsListHandler : public ListControl::Handler {
+  ListControl &airspace_list;
 
-static bool color_mode = false;
-static bool changed = false;
+  const bool color_mode;
+  bool changed;
 
-static void
-OnAirspacePaintListItem(Canvas &canvas, const PixelRect rc, unsigned i)
+public:
+  AirspaceSettingsListHandler(ListControl &_airspace_list,
+                              bool _color_mode)
+    :airspace_list(_airspace_list), color_mode(_color_mode),
+     changed(false) {}
+
+  bool IsModified() const {
+    return changed;
+  }
+
+  virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
+                           unsigned idx) gcc_override;
+
+  virtual bool CanActivateItem(unsigned index) const {
+    return true;
+  }
+
+  virtual void OnActivateItem(unsigned index) gcc_override;
+};
+
+void
+AirspaceSettingsListHandler::OnPaintItem(Canvas &canvas, const PixelRect rc,
+                                         unsigned i)
 {
   assert(i < AIRSPACECLASSCOUNT);
 
@@ -100,8 +122,8 @@ OnAirspacePaintListItem(Canvas &canvas, const PixelRect rc, unsigned i)
                          AirspaceFormatter::GetClass((AirspaceClass)i));
 }
 
-static void
-OnAirspaceListEnter(unsigned index)
+void
+AirspaceSettingsListHandler::OnActivateItem(unsigned index)
 {
   assert(index < AIRSPACECLASSCOUNT);
 
@@ -119,7 +141,7 @@ OnAirspaceListEnter(unsigned index)
 
     ActionInterface::SendMapSettings();
     look.Initialise(renderer);
-    airspace_list->Invalidate();
+    airspace_list.Invalidate();
   } else {
     renderer.classes[index].display = !renderer.classes[index].display;
     if (!renderer.classes[index].display)
@@ -130,7 +152,7 @@ OnAirspaceListEnter(unsigned index)
                              computer.warnings.class_warnings[index]);
     changed = true;
     ActionInterface::SendMapSettings();
-    airspace_list->Invalidate();
+    airspace_list.Invalidate();
   }
 }
 
@@ -146,29 +168,25 @@ static constexpr CallBackTableEntry CallBackTable[] = {
 };
 
 void
-dlgAirspaceShowModal(bool _color_mode)
+dlgAirspaceShowModal(bool color_mode)
 {
-  color_mode = _color_mode;
-
   WndForm *wf = LoadDialog(CallBackTable, UIGlobals::GetMainWindow(),
                            Layout::landscape
                            ? _T("IDR_XML_AIRSPACE_L")
                            : _T("IDR_XML_AIRSPACE"));
   assert(wf != NULL);
 
-  airspace_list = (ListControl*)wf->FindByName(_T("frmAirspaceList"));
-  assert(airspace_list != NULL);
-  airspace_list->SetActivateCallback(OnAirspaceListEnter);
-  airspace_list->SetPaintItemCallback(OnAirspacePaintListItem);
-  airspace_list->SetLength(AIRSPACECLASSCOUNT);
-
-  changed = false;
+  ListControl &airspace_list = *(ListControl *)
+    wf->FindByName(_T("frmAirspaceList"));
+  AirspaceSettingsListHandler handler(airspace_list, color_mode);
+  airspace_list.SetHandler(&handler);
+  airspace_list.SetLength(AIRSPACECLASSCOUNT);
 
   wf->ShowModal();
   delete wf;
 
   // now retrieve back the properties...
-  if (changed) {
+  if (handler.IsModified()) {
     if (!color_mode && glide_computer != NULL) {
       ProtectedAirspaceWarningManager::ExclusiveLease awm(glide_computer->GetAirspaceWarnings());
       awm->SetConfig(CommonInterface::SetComputerSettings().airspace.warnings);

src/Dialogs/Airspace/dlgAirspaceWarnings.cpp
@@ -69,6 +69,20 @@ struct WarningItem
   }
 };
 
+class AirspaceWarningListHandler : public ListControl::Handler {
+public:
+  virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
+                           unsigned idx) gcc_override;
+
+  virtual void OnCursorMoved(unsigned index);
+
+  virtual bool CanActivateItem(unsigned index) const {
+    return true;
+  }
+
+  virtual void OnActivateItem(unsigned index) gcc_override;
+};
+
 typedef TrivialArray<WarningItem, 64u> WarningList;
 
 static ProtectedAirspaceWarningManager *airspace_warnings;
@@ -139,8 +153,14 @@ AirspaceWarningCursorCallback(unsigned i)
   UpdateButtons();
 }
 
-static void
-OnAirspaceListEnter(gcc_unused unsigned i)
+void
+AirspaceWarningListHandler::OnCursorMoved(unsigned i)
+{
+  AirspaceWarningCursorCallback(i);
+}
+
+void
+AirspaceWarningListHandler::OnActivateItem(gcc_unused unsigned i)
 {
   if (!HasPointer())
     /* on platforms without a pointing device (e.g. ALTAIR), allow
@@ -295,8 +315,9 @@ OnKeyDown(unsigned key_code)
   }
 }
 
-static void
-OnAirspaceListItemPaint(Canvas &canvas, const PixelRect paint_rc, unsigned i)
+void
+AirspaceWarningListHandler::OnPaintItem(Canvas &canvas,
+                                        const PixelRect paint_rc, unsigned i)
 {
   TCHAR buffer[128];
 
@@ -511,9 +532,9 @@ dlgAirspaceWarningsShowModal(SingleWindow &parent,
   warning_list_frame =
     (ListControl*)dialog->FindByName(_T("frmAirspaceWarningList"));
   assert(warning_list_frame != NULL);
-  warning_list_frame->SetPaintItemCallback(OnAirspaceListItemPaint);
-  warning_list_frame->SetCursorCallback(AirspaceWarningCursorCallback);
-  warning_list_frame->SetActivateCallback(OnAirspaceListEnter);
+
+  AirspaceWarningListHandler handler;
+  warning_list_frame->SetHandler(&handler);
 
   auto_close = _auto_close;
   UpdateList();

src/Dialogs/WaypointList.cpp
@@ -129,6 +129,12 @@ struct WaypointListDialogState
   }
 };
 
+class WaypointListDialog : public ListItemRenderer {
+public:
+  virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
+                           unsigned idx) gcc_override;
+};
+
 static WaypointListDialogState dialog_state;
 static WaypointList waypoint_list;
 
@@ -282,8 +288,9 @@ FilterDataFieldListener::OnModified(DataField &df)
   UpdateList();
 }
 
-static void
-OnPaintListItem(Canvas &canvas, const PixelRect rc, unsigned i)
+void
+WaypointListDialog::OnPaintItem(Canvas &canvas, const PixelRect rc,
+                                unsigned i)
 {
   if (waypoint_list.empty()) {
     assert(i == 0);
@@ -402,10 +409,12 @@ ShowWaypointListDialog(SingleWindow &parent, const GeoPoint &_location,
 
   const DialogLook &dialog_look = UIGlobals::GetDialogLook();
 
+  WaypointListDialog dialog2;
+
   waypoint_list_control = (ListControl*)dialog->FindByName(_T("frmWaypointList"));
   assert(waypoint_list_control != NULL);
   waypoint_list_control->SetActivateCallback(OnWaypointListEnter);
-  waypoint_list_control->SetPaintItemCallback(OnPaintListItem);
+  waypoint_list_control->SetItemRenderer(&dialog2);
   waypoint_list_control->SetItemHeight(WaypointListRenderer::GetHeight(dialog_look));
 
   FilterDataFieldListener listener;

src/Dialogs/Weather/NOAAList.cpp
@@ -183,7 +183,6 @@ ListItemSelected(unsigned index)
 }
 
 static constexpr CallBackTableEntry CallBackTable[] = {
-  DeclareCallBackEntry(PaintListItem),
   DeclareCallBackEntry(ListItemSelected),
   DeclareCallBackEntry(AddClicked),
   DeclareCallBackEntry(UpdateClicked),
@@ -200,10 +199,12 @@ dlgNOAAListShowModal(SingleWindow &parent)
                   _T("IDR_XML_NOAA_LIST"));
   assert(wf != NULL);
 
+  FunctionListItemRenderer renderer(PaintListItem);
+
   station_list = (ListControl *)wf->FindByName(_T("StationList"));
   assert(station_list != NULL);
   station_list->SetItemHeight(NOAAListRenderer::GetHeight(UIGlobals::GetDialogLook()));
-  station_list->SetPaintItemCallback(PaintListItem);
+  station_list->SetItemRenderer(&renderer);
   station_list->SetActivateCallback(ListItemSelected);
 
   add_button = (WndButton *)wf->FindByName(_T("AddButton"));

src/Dialogs/dlgPlanes.cpp
@@ -49,6 +49,18 @@ Copyright_License {
 #include <assert.h>
 #include <windef.h> /* for MAX_PATH */
 
+class PlaneListHandler : public ListControl::Handler {
+public:
+  virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
+                           unsigned idx) gcc_override;
+
+  virtual bool CanActivateItem(unsigned index) const {
+    return true;
+  }
+
+  virtual void OnActivateItem(unsigned index) gcc_override;
+};
+
 static WndForm *dialog = NULL;
 static ListControl *plane_list = NULL;
 
@@ -111,8 +123,8 @@ UpdateList()
   b->SetEnabled(len > 0);
 }
 
-static void
-OnPlaneListPaint(Canvas &canvas, const PixelRect rc, unsigned i)
+void
+PlaneListHandler::OnPaintItem(Canvas &canvas, const PixelRect rc, unsigned i)
 {
   assert(i < list.size());
 
@@ -290,8 +302,8 @@ DeleteClicked()
   UpdateList();
 }
 
-static void
-ListItemSelected(unsigned i)
+void
+PlaneListHandler::OnActivateItem(unsigned i)
 {
   assert(i < list.size());
 
@@ -322,11 +334,12 @@ dlgPlanesShowModal(SingleWindow &parent)
 
   UPixelScalar item_height = GetRowHeight(UIGlobals::GetDialogLook());
 
+  PlaneListHandler handler;
+
   plane_list = (ListControl*)dialog->FindByName(_T("List"));
   assert(plane_list != NULL);
   plane_list->SetItemHeight(item_height);
-  plane_list->SetPaintItemCallback(OnPlaneListPaint);
-  plane_list->SetActivateCallback(ListItemSelected);
+  plane_list->SetHandler(&handler);
 
   UpdateList();
   dialog->ShowModal();

test/src/RunListControl.cpp
@@ -51,7 +51,9 @@ Main()
   style.TabStop();
   ListControl list(client_area, *dialog_look, list_rc,
                    style, normal_font.GetHeight() + 4);
-  list.SetPaintItemCallback(PaintItemCallback);
+
+  FunctionListItemRenderer renderer(PaintItemCallback);
+  list.SetItemRenderer(&renderer);
   list.SetLength(64);
   list.SetFocus();
 

commit 28852bc59069b87bdc7ba8f045aa4c5b902a567a

Dialogs/Waypoint/dlgWayPointDetails: Fix display of external files
Regression introduced in commit cb56af7d9d5b4468c3b6d5ef91641ad8a158fc57.

Fixes trac issue #2453.

src/Dialogs/Waypoint/dlgWaypointDetails.cpp
@@ -290,11 +290,15 @@ public:
   virtual void OnPaintItem(Canvas &canvas, const PixelRect rc,
                            unsigned idx) gcc_override;
 
-  virtual void OnCursorMoved(unsigned index) gcc_override;
+  virtual bool CanActivateItem(unsigned index) const {
+    return true;
+  }
+
+  virtual void OnActivateItem(unsigned index) gcc_override;
 };
 
 void
-WaypointExternalFileListHandler::OnCursorMoved(unsigned i)
+WaypointExternalFileListHandler::OnActivateItem(unsigned i)
 {
   auto file = waypoint->files_external.begin();
   std::advance(file, i);