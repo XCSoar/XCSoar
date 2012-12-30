b06e8c0e7ec65c728046064517b70a137cdcf30c
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-12-30 12:35:53 +0100

    AirspaceSorter: Replaced *Filter and *Compare classes by lambda functions

--- src/Engine/Airspace/AirspaceSorter.cpp
+++ src/Engine/Airspace/AirspaceSorter.cpp
@@ -54,148 +54,82 @@ AirspaceSorter::GetList() const
   return m_airspaces_all;
 }
 
-class AirspaceClassFilter
-{
-  AirspaceClass match_class;
-
-public:
-  AirspaceClassFilter(AirspaceClass _match_class): match_class(_match_class) {}
-
-  bool operator()(const AirspaceSelectInfo &info) {
-    return info.airspace->GetType() != match_class;
-  }
-};
-
 void
 AirspaceSorter::FilterByClass(AirspaceSelectInfoVector& vec,
                              const AirspaceClass match_class) const
 {
-  vec.erase(std::remove_if(vec.begin(), vec.end(),
-                           AirspaceClassFilter(match_class)), vec.end());
+  auto filter = [match_class] (const AirspaceSelectInfo &info) {
+    return info.airspace->GetType() != match_class;
+  };
+
+  vec.erase(std::remove_if(vec.begin(), vec.end(), filter),vec.end());
 }
-
-class AirspaceNameFilter
-{
-  unsigned char match_char;
-
-public:
-  AirspaceNameFilter(unsigned char _match_char): match_char(_match_char) {}
-
-  bool operator()(const AirspaceSelectInfo &info) {
-    return (((info.four_chars & 0xff000000) >> 24) != match_char);
-  }
-};
 
 void
 AirspaceSorter::FilterByName(AirspaceSelectInfoVector& vec,
                              const unsigned char match_char) const
 {
-  vec.erase(std::remove_if(vec.begin(), vec.end(),
-                           AirspaceNameFilter(match_char)), vec.end());
+  auto filter = [match_char] (const AirspaceSelectInfo &info) {
+    return (((info.four_chars & 0xff000000) >> 24) != match_char);
+  };
+
+  vec.erase(std::remove_if(vec.begin(), vec.end(), filter), vec.end());
 }
-
-class AirspaceNamePrefixFilter
-{
-  const TCHAR *prefix;
-
-public:
-  AirspaceNamePrefixFilter(const TCHAR *_prefix): prefix(_prefix) {}
-
-  bool operator()(const AirspaceSelectInfo &info) {
-    return !info.airspace->MatchNamePrefix(prefix);
-  }
-};
 
 void
 AirspaceSorter::FilterByNamePrefix(AirspaceSelectInfoVector &v,
                                    const TCHAR *prefix) const
 {
-  v.erase(std::remove_if(v.begin(), v.end(),
-                         AirspaceNamePrefixFilter(prefix)), v.end());
+  auto filter = [prefix] (const AirspaceSelectInfo &info) {
+    return !info.airspace->MatchNamePrefix(prefix);
+  };
+
+  v.erase(std::remove_if(v.begin(), v.end(), filter), v.end());
 }
-
-class AirspaceDirectionFilter
-{
-  Angle direction;
-  const TaskProjection &projection;
-  const GeoPoint &location;
-
-public:
-  AirspaceDirectionFilter(Angle _direction,
-      const TaskProjection &_projection, const GeoPoint &_location)
-    :direction(_direction), projection(_projection), location(_location) {}
-
-  bool operator()(const AirspaceSelectInfo &info) {
-    Angle bearing = info.GetVector(location, projection).bearing;
-    fixed direction_error = (bearing - direction).AsDelta().AbsoluteDegrees();
-    return direction_error > fixed(18);
-  }
-};
 
 void
 AirspaceSorter::FilterByDirection(AirspaceSelectInfoVector& vec,
                                  const Angle direction) const
 {
-  vec.erase(std::remove_if(vec.begin(), vec.end(),
-      AirspaceDirectionFilter(direction, projection, location)), vec.end());
+  auto filter = [&, direction] (const AirspaceSelectInfo &info) {
+    Angle bearing = info.GetVector(location, projection).bearing;
+    fixed direction_error = (bearing - direction).AsDelta().AbsoluteDegrees();
+    return direction_error > fixed(18);
+  };
+
+  vec.erase(std::remove_if(vec.begin(), vec.end(), filter), vec.end());
 }
-
-class AirspaceDistanceFilter
-{
-  fixed min_distance;
-  const TaskProjection &projection;
-  const GeoPoint &location;
-
-public:
-  AirspaceDistanceFilter(fixed _min_distance,
-      const TaskProjection &_projection, const GeoPoint &_location)
-    :min_distance(_min_distance), projection(_projection), location(_location) {}
-
-  bool operator()(const AirspaceSelectInfo &info) {
-    return info.GetVector(location, projection).distance > min_distance;
-  }
-};
 
 void
 AirspaceSorter::FilterByDistance(AirspaceSelectInfoVector& vec,
                                 const fixed distance) const
 {
-  vec.erase(std::remove_if(vec.begin(), vec.end(),
-      AirspaceDistanceFilter(distance, projection, location)), vec.end());
+  auto filter = [&, distance] (const AirspaceSelectInfo &info) {
+    return info.GetVector(location, projection).distance > distance;
+  };
+
+  vec.erase(std::remove_if(vec.begin(), vec.end(), filter), vec.end());
 }
-
-class AirspaceDistanceCompare
-{
-  const TaskProjection &projection;
-  const GeoPoint &location;
-
-public:
-  AirspaceDistanceCompare(
-      const TaskProjection &_projection, const GeoPoint &_location)
-    :projection(_projection), location(_location) {}
-
-  bool operator()(const AirspaceSelectInfo &elem1,
-                  const AirspaceSelectInfo &elem2) {
-    return elem1.GetVector(location, projection).distance <
-           elem2.GetVector(location, projection).distance;
-  }
-};
 
 void
 AirspaceSorter::SortByDistance(AirspaceSelectInfoVector& vec) const
 {
-  std::sort(vec.begin(), vec.end(), AirspaceDistanceCompare(projection, location));
-}
+  auto compare = [&] (const AirspaceSelectInfo &elem1,
+                      const AirspaceSelectInfo &elem2) {
+    return elem1.GetVector(location, projection).distance <
+           elem2.GetVector(location, projection).distance;
+  };
 
-static bool
-AirspaceNameCompare(const AirspaceSelectInfo& elem1,
-                    const AirspaceSelectInfo& elem2)
-{
-  return (elem1.four_chars < elem2.four_chars);
+  std::sort(vec.begin(), vec.end(), compare);
 }
 
 void
 AirspaceSorter::SortByName(AirspaceSelectInfoVector& vec) const
 {
-  std::sort(vec.begin(), vec.end(), AirspaceNameCompare);
+  auto compare = [&] (const AirspaceSelectInfo &elem1,
+                      const AirspaceSelectInfo &elem2) {
+    return elem1.four_chars < elem2.four_chars;
+  };
+
+  std::sort(vec.begin(), vec.end(), compare);
 }
