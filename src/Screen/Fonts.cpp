XCSoar/XCSoar

commit 4ee52479cfa59d08ebd9fa978a77273c08ebb94a

InfoBoxWindow: Used text-based UnitSymbolRenderer on high resolutions

src/Screen/Fonts.cpp
@@ -28,6 +28,7 @@ Copyright_License {
 /// values inside infoboxes  like numbers, etc.
 Font Fonts::InfoBox;
 Font Fonts::InfoBoxSmall;
+Font Fonts::InfoBoxUnits;
 /// Titles of infoboxes like Next, WP L/D etc.
 Font Fonts::Title;
 /// text names on the map
@@ -44,6 +45,7 @@ Font Fonts::MapLabelImportant;
 
 // these are the non-custom parameters
 LOGFONT LogInfoBox;
+LOGFONT LogInfoBoxUnits;
 LOGFONT LogTitle;
 LOGFONT LogMap;
 LOGFONT LogInfoBoxSmall;
@@ -82,6 +84,7 @@ static void
 LoadAltairLogFonts()
 {
   InitialiseLogfont(&LogInfoBox, _T("RasterGothicTwentyFourCond"), 24, true);
+  InitialiseLogfont(&LogInfoBoxUnits, _T("RasterGothicNineCond"), 10, true);
   InitialiseLogfont(&LogTitle, _T("RasterGothicNineCond"), 10);
   InitialiseLogfont(&LogCDI, _T("RasterGothicEighteenCond"), 19, true);
   InitialiseLogfont(&LogMapLabel, _T("RasterGothicTwelveCond"), 13);
@@ -160,6 +163,9 @@ InitialiseLogFonts()
 
   InitialiseLogfont(&LogInfoBoxSmall, Fonts::GetStandardFontFace(),
                     Layout::Scale(20));
+
+  InitialiseLogfont(&LogInfoBoxSmall, Fonts::GetStandardFontFace(),
+                    (int)(FontHeight * 0.56), true);
 }
 
 void
@@ -184,6 +190,15 @@ Fonts::SizeInfoboxFont(UPixelScalar control_width)
     SizeLogFont(lf, control_width, _T("1234m"));
   InfoBox.Set(lf);
 
+  if (!is_altair()) {
+    unsigned height = lf.lfHeight;
+    lf = LogInfoBoxUnits;
+    lf.lfHeight = (height * 2) / 5;
+  } else {
+    lf = LogInfoBoxUnits;
+  }
+  InfoBoxUnits.Set(lf);
+
   lf = LogInfoBoxSmall;
   if (!is_altair())
     SizeLogFont(lf, control_width, _T("12345m"));
@@ -195,6 +210,7 @@ Fonts::Deinitialize()
 {
   InfoBox.Reset();
   InfoBoxSmall.Reset();
+  InfoBoxUnits.Reset();
   Title.Reset();
   Map.Reset();
   MapBold.Reset();

src/InfoBoxes/InfoBoxWindow.cpp
@@ -26,6 +26,7 @@ Copyright_License {
 #include "Look/InfoBoxLook.hpp"
 #include "InputEvents.hpp"
 #include "Compatibility/string.h"
+#include "Renderer/UnitSymbolRenderer.hpp"
 #include "Screen/UnitSymbol.hpp"
 #include "Screen/Layout.hpp"
 #include "Screen/BufferCanvas.hpp"
@@ -240,6 +241,42 @@ InfoBoxWindow::PaintValue(Canvas &canvas)
     return;
 
   canvas.set_text_color(look.get_value_color(value_color));
+
+  // Do text-based unit rendering on higher resolutions
+  if (Layout::FastScale(10) > 18) {
+    canvas.select(*look.unit_font);
+    PixelSize unit_size = UnitSymbolRenderer::GetSize(canvas, value_unit);
+
+    canvas.select(*look.value.font);
+    UPixelScalar ascent_height = look.value.font->GetAscentHeight();
+
+    PixelSize tsize = canvas.text_size(value.c_str());
+    if (tsize.cx > value_rect.right - value_rect.left) {
+      canvas.select(*look.small_font);
+      ascent_height = look.small_font->GetAscentHeight();
+      tsize = canvas.text_size(value.c_str());
+    }
+
+    PixelScalar x = max(PixelScalar(0),
+                        PixelScalar((value_rect.left + value_rect.right - tsize.cx
+                                     - unit_size.cx) / 2));
+
+    PixelScalar y = (value_rect.top + value_rect.bottom - tsize.cy) / 2;
+
+    canvas.TextAutoClipped(x, y, value.c_str());
+
+    if (unit_size.cx != 0) {
+      UPixelScalar unit_height =
+          UnitSymbolRenderer::GetAscentHeight(*look.unit_font, value_unit);
+
+      canvas.select(*look.unit_font);
+      UnitSymbolRenderer::Draw(canvas,
+                               { PixelScalar(x + tsize.cx),
+                                 PixelScalar(y + ascent_height - unit_height) },
+                               value_unit);
+    }
+    return;
+  }
 
   canvas.select(*look.value.font);
   UPixelScalar ascent_height = look.value.font->GetAscentHeight();

src/Look/InfoBoxLook.cpp
@@ -50,6 +50,7 @@ InfoBoxLook::Initialise(bool _inverse, bool use_colors)
   title.font = &Fonts::Title;
   comment.font = &Fonts::Title;
   small_font = &Fonts::InfoBoxSmall;
+  unit_font = &Fonts::InfoBoxUnits;
 
   colors[0] = border_color;
   if (use_colors) {

src/Look/InfoBoxLook.hpp
@@ -44,6 +44,7 @@ struct InfoBoxLook {
   } title, value, comment;
 
   const Font *small_font;
+  const Font *unit_font;
 
   Color colors[6];
 

src/Screen/Fonts.hpp
@@ -34,6 +34,7 @@ class Font;
 namespace Fonts
 {
   extern Font InfoBox;
+  extern Font InfoBoxUnits;
   extern Font Title;
   extern Font Map;
   extern Font InfoBoxSmall;

commit 901da6569345ae0d0a5d1d09af2177eeca5afe62

Screen/Fonts: fix log_infobox_units initialisation
This fixes a data loss bug introduced by 4ee52479.  It was a
copy'n'paste error that initialisation log_infobox_small instead of
log_infobox_units.

src/Screen/Fonts.cpp
@@ -175,8 +175,10 @@ InitialiseLogFonts()
   InitialiseLogfont(&log_infobox_small, Fonts::GetStandardFontFace(),
                     Layout::Scale(20));
 
-  InitialiseLogfont(&log_infobox_small, Fonts::GetStandardFontFace(),
+#ifndef GNAV
+  InitialiseLogfont(&log_infobox_units, Fonts::GetStandardFontFace(),
                     (int)(FontHeight * 0.56), true);
+#endif
 
   InitialiseLogfont(&log_monospace, GetStandardMonospaceFontFace(),
                     UPixelScalar(FontHeight * 0.39), false, false, false);