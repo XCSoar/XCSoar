2c061db846cdb4703c9c700d331bbe3524cacfa6
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-01-02 22:57:00 +0100

    Updated copyright notice to 2013

--- src/XCSoarLaunch.c
+++ src/XCSoarLaunch.c
@@ -11,7 +11,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2012 The XCSoar Project
+  Copyright (C) 2000-2013 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
