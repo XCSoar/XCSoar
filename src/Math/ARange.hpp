XCSoar/XCSoar

commit c5936d74bf380709c33a32a83b4897a74d042d77

Math/AngleRange: new utility class
Used as the GeoBounds backend, to eliminate some duplicate code.
Since the GeoBounds ABI changes, RasterTileCache::CacheHeader::VERSION
must be incremented.

src/Math/ARange.hpp
@@ -1,0 +1,100 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2012 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_ANGLE_RANGE_HPP
+#define XCSOAR_ANGLE_RANGE_HPP
+
+#include "Angle.hpp"
+
+#include <type_traits>
+
+/**
+ * A range between two angles.  This class hosts a few algorithms that
+ * are safe against wraparound.
+ */
+struct AngleRange {
+  Angle start, end;
+
+  AngleRange() = default;
+
+  constexpr AngleRange(Angle _start, Angle _end)
+    :start(_start), end(_end) {}
+
+  constexpr bool IsEmpty() const {
+    return start == end;
+  }
+
+  /**
+   * Returns the length of the range.
+   */
+  gcc_pure
+  Angle GetLength() const {
+    return (end - start).AsBearing();
+  }
+
+  gcc_pure
+  Angle GetMiddle() const {
+    return start.Fraction(end, fixed(0.5));
+  }
+
+  /**
+   * Is the specified value within the range?
+   */
+  gcc_pure
+  bool IsInside(const Angle value) const {
+    return value.Between(start, end);
+  }
+
+  /**
+   * Do the two ranges overlap?
+   */
+  gcc_pure
+  bool Overlaps(const AngleRange &other) const {
+    return IsInside(other.start) || IsInside(other.end);
+  }
+
+  /**
+   * Is the specified range within this range?
+   */
+  gcc_pure
+  bool IsInside(const AngleRange &interior) const {
+    return IsInside(interior.start) && IsInside(interior.end);
+  }
+
+  /**
+   * Extend this range so the specified value is deemed inside.
+   */
+  void Extend(Angle value);
+
+  /**
+   * Set this object to the intersection of this and the other object.
+   *
+   * @return false if the two objects do not overlap; in this case,
+   * the object is left in an undefined state
+   */
+  bool IntersectWith(const AngleRange &other);
+};
+
+static_assert(std::is_trivial<AngleRange>::value, "type is not trivial");
+
+#endif

build/libmath.mk
@@ -4,6 +4,7 @@ MATH_SRC_DIR = $(SRC)/Math
 
 MATH_SOURCES = \
 	$(MATH_SRC_DIR)/Angle.cpp \
+	$(MATH_SRC_DIR)/ARange.cpp \
 	$(MATH_SRC_DIR)/FastMath.cpp \
 	$(MATH_SRC_DIR)/FastTrig.cpp \
 	$(MATH_SRC_DIR)/FastRotation.cpp \

gdb.py
@@ -55,6 +55,15 @@ class AnglePrinter:
     def to_string(self):
         return str(angle_value(self.value))
 
+class AngleRangePrinter:
+    def __init__(self, value):
+        self.value = value
+
+    def to_string(self):
+        start = AnglePrinter(self.value['start']).to_string()
+        end = AnglePrinter(self.value['end']).to_string()
+        return 'AngleRange(%s..%s)' % (start, end)
+
 class GeoPointPrinter:
     def __init__(self, value):
         self.value = value
@@ -72,13 +81,13 @@ class GeoBoundsPrinter:
         self.value = value
 
     def to_string(self):
-        if angle_value(self.value['north']) >= 180:
+        if angle_value(self.value['latitude']['end']) >= 180:
             return 'GeoBounds::INVALID'
 
-        west = AnglePrinter(self.value['west']).to_string()
-        east = AnglePrinter(self.value['east']).to_string()
-        south = AnglePrinter(self.value['south']).to_string()
-        north = AnglePrinter(self.value['north']).to_string()
+        west = AnglePrinter(self.value['longitude']['start']).to_string()
+        east = AnglePrinter(self.value['longitude']['end']).to_string()
+        south = AnglePrinter(self.value['latitude']['start']).to_string()
+        north = AnglePrinter(self.value['latitude']['end']).to_string()
         return 'GeoBounds([%s .. %s] [%s .. %s])' % (west, east, south, north)
 
 class GeoVectorPrinter:
@@ -203,6 +212,8 @@ def lookup_function(value):
         return FixedPrinter(value)
     elif typename == 'Angle':
         return AnglePrinter(value)
+    elif typename == 'AngleRange':
+        return AngleRangePrinter(value)
     elif typename == 'GeoPoint':
         return GeoPointPrinter(value)
     elif typename == 'GeoBounds':

src/Engine/Route/ReachFan.hpp
@@ -29,7 +29,7 @@
 
 class RoutePolars;
 class RasterMap;
-struct GeoBounds;
+class GeoBounds;
 struct ReachResult;
 
 class ReachFan

src/Engine/Task/Ordered/OrderedTask.hpp
@@ -44,7 +44,7 @@ struct Waypoint;
 class Waypoints;
 class AATPoint;
 class FlatBoundingBox;
-struct GeoBounds;
+class GeoBounds;
 struct TaskSummary;
 struct TaskFactoryConstraints;
 

src/Geo/Flat/TaskProjection.hpp
@@ -29,7 +29,7 @@
 struct FlatPoint;
 struct FlatGeoPoint;
 class FlatBoundingBox;
-struct GeoBounds;
+class GeoBounds;
 
 /**
  * Class for performing Lambert Conformal Conic projections from

src/Geo/GeoBounds.cpp
@@ -30,43 +30,18 @@ GeoBounds::Extend(const GeoPoint pt)
     return;
 
   if (IsValid()) {
-    if (pt.longitude < west)
-      west = pt.longitude;
-    if (pt.latitude > north)
-      north = pt.latitude;
-    if (pt.longitude > east)
-      east = pt.longitude;
-    if (pt.latitude < south)
-      south = pt.latitude;
+    longitude.Extend(pt.longitude);
+    latitude.Extend(pt.latitude);
   } else {
-    west = east = pt.longitude;
-    north = south = pt.latitude;
+    *this = GeoBounds(pt);
   }
-}
-
-static bool
-IntersectWith(Angle &a, Angle &b, const Angle other_a, const Angle other_b)
-{
-  bool result = false;
-
-  if (other_a.Between(a, b)) {
-    a = other_a;
-    result = true;
-  }
-
-  if (other_b.Between(a, b)) {
-    b = other_b;
-    result = true;
-  }
-
-  return result;
 }
 
 bool
 GeoBounds::IntersectWith(const GeoBounds &other)
 {
-  return ::IntersectWith(west, east, other.west, other.east) &&
-    ::IntersectWith(south, north, other.south, other.north);
+  return longitude.IntersectWith(other.longitude) &&
+    latitude.IntersectWith(other.latitude);
 }
 
 GeoPoint
@@ -75,8 +50,7 @@ GeoBounds::GetCenter() const
   if (!IsValid())
     return GeoPoint::Invalid();
 
-  return GeoPoint(west.Fraction(east, fixed(0.5)),
-                  south.Fraction(north, fixed(0.5)));
+  return GeoPoint(longitude.GetMiddle(), latitude.GetMiddle());
 }
 
 GeoBounds
@@ -86,15 +60,15 @@ GeoBounds::Scale(fixed factor) const
     return Invalid();
 
   Angle diff_lat_half =
-    (north - south).AsBearing() / fixed(2) * (factor - fixed(1));
+    GetHeight() / 2 * (factor - fixed(1));
   Angle diff_lon_half =
-    (east - west).AsBearing() / fixed(2) * (factor - fixed(1));
+    GetWidth() / 2 * (factor - fixed(1));
 
   GeoBounds br = *this;
-  br.east += diff_lon_half;
-  br.west -= diff_lon_half;
-  br.north += diff_lat_half;
-  br.south -= diff_lat_half;
+  br.longitude.end += diff_lon_half;
+  br.longitude.start -= diff_lon_half;
+  br.latitude.end += diff_lat_half;
+  br.latitude.start -= diff_lat_half;
 
   return br;
 }

src/Geo/GeoBounds.hpp
@@ -24,6 +24,7 @@ Copyright_License {
 #ifndef XCSOAR_GEO_RECT_HPP
 #define XCSOAR_GEO_RECT_HPP
 
+#include "Math/ARange.hpp"
 #include "GeoPoint.hpp"
 #include "Compiler.h"
 
@@ -33,20 +34,29 @@ Copyright_License {
  * goal is to perform fast overlap checks, e.g. to determine if an
  * object is visible on the screen.
  */
-struct GeoBounds {
-  Angle west, north, east, south;
+class GeoBounds {
+  /**
+   * The range from west to east.
+   */
+  AngleRange longitude;
 
+  /**
+   * The range from south to north.
+   */
+  AngleRange latitude;
+
+public:
   GeoBounds() = default;
 
   constexpr
   GeoBounds(const GeoPoint pt)
-    :west(pt.longitude), north(pt.latitude),
-     east(pt.longitude), south(pt.latitude) {}
+    :longitude(pt.longitude, pt.longitude),
+     latitude(pt.latitude, pt.latitude) {}
 
   constexpr
-  GeoBounds(const GeoPoint _north_west, const GeoPoint _south_east)
-    :west(_north_west.longitude), north(_north_west.latitude),
-     east(_south_east.longitude), south(_south_east.latitude) {}
+  GeoBounds(const GeoPoint north_west, const GeoPoint south_east)
+    :longitude(north_west.longitude, south_east.longitude),
+     latitude(south_east.latitude, north_west.latitude) {}
 
   /**
    * Construct an instance that is "invalid", i.e. IsValid() will
@@ -63,23 +73,39 @@ struct GeoBounds {
    * The return value must not be used in any calculation.
    */
   void SetInvalid() {
-    north = Angle::FullCircle();
+    latitude.end = Angle::FullCircle();
+  }
+
+  constexpr Angle GetWest() const {
+    return longitude.start;
+  }
+
+  constexpr Angle GetEast() const {
+    return longitude.end;
+  }
+
+  constexpr Angle GetSouth() const {
+    return latitude.start;
+  }
+
+  constexpr Angle GetNorth() const {
+    return latitude.end;
   }
 
   constexpr GeoPoint GetNorthWest() const {
-    return GeoPoint(west, north);
+    return GeoPoint(GetWest(), GetNorth());
   }
 
   constexpr GeoPoint GetNorthEast() const {
-    return GeoPoint(east, north);
+    return GeoPoint(GetEast(), GetNorth());
   }
 
   constexpr GeoPoint GetSouthWest() const {
-    return GeoPoint(west, south);
+    return GeoPoint(GetWest(), GetSouth());
   }
 
   constexpr GeoPoint GetSouthEast() const {
-    return GeoPoint(east, south);
+    return GeoPoint(GetEast(), GetSouth());
   }
 
   /**
@@ -90,19 +116,19 @@ struct GeoBounds {
    */
   constexpr
   bool IsValid() const {
-    return north <= Angle::HalfCircle();
+    return latitude.end <= Angle::HalfCircle();
   }
 
-  bool IsEmpty() const {
-    return west == east && north == south;
+  constexpr bool IsEmpty() const {
+    return longitude.IsEmpty() || latitude.IsEmpty();
   }
 
   Angle GetWidth() const {
-    return (east - west).AsBearing();
+    return longitude.GetLength();
   }
 
   Angle GetHeight() const {
-    return (north - south).AsBearing();
+    return latitude.GetLength();
   }
 
   /**
@@ -111,8 +137,9 @@ struct GeoBounds {
    */
   gcc_pure
   fixed GetGeoWidth() const {
-    const Angle latitude = south.Fraction(north, fixed(0.5));
-    return GeoPoint(west, latitude).Distance(GeoPoint(east, latitude));
+    const Angle middle_latitude = latitude.GetMiddle();
+    return GeoPoint(GetWest(), middle_latitude)
+      .Distance(GeoPoint(GetEast(), middle_latitude));
   }
 
   /**
@@ -126,8 +153,8 @@ struct GeoBounds {
 
   void Extend(const GeoPoint pt);
 
-  bool IsInside(Angle longitude, Angle latitude) const {
-    return longitude.Between(west, east) && latitude.Between(south, north);
+  bool IsInside(Angle _longitude, Angle _latitude) const {
+    return longitude.IsInside(_longitude) && latitude.IsInside(_latitude);
   }
 
   bool IsInside(const GeoPoint pt) const {
@@ -135,27 +162,17 @@ struct GeoBounds {
   }
 
   bool IsInside(const GeoBounds &interior) const {
-    return IsInside(interior.west, interior.north) &&
-      IsInside(interior.east, interior.south);
+    return longitude.IsInside(interior.longitude) &&
+      latitude.IsInside(interior.latitude);
   }
 
-protected:
-  /**
-   * Does the range a1..a2 overlap with b1..b2?
-   */
-  gcc_const
-  static bool Overlaps(Angle a1, Angle a2, Angle b1, Angle b2) {
-    return a1.Between(b1, b2) || b1.Between(a1, a2);
-  }
-
-public:
   /**
    * Does this GeoBounds instance overlap with the specified one?
    */
   gcc_pure
   bool Overlaps(const GeoBounds &other) const {
-    return Overlaps(west, east, other.west, other.east) &&
-      Overlaps(south, north, other.south, other.north);
+    return longitude.Overlaps(other.longitude) &&
+      latitude.Overlaps(other.latitude);
   }
 
   /**

commit 0eb61b1dc123323842d345cc29ae989e25e6b1c8

Math/ARange: Fix Overlaps() test
Fixes trac issue #2561.

Regression introduced in commit c5936d74bf380709c33a32a83b4897a74d042d77.

src/Math/ARange.hpp
@@ -70,7 +70,7 @@ struct AngleRange {
    */
   gcc_pure
   bool Overlaps(const AngleRange &other) const {
-    return IsInside(other.start) || IsInside(other.end);
+    return IsInside(other.start) || other.IsInside(start);
   }
 
   /**