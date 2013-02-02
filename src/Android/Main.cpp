14065325eb932902ee5285989791466b18217f43
Max Kellermann <max@duempel.org>
2013-02-02 09:53:45 +0100

    Android/Main: don't send pause/resume to TopWindow before it gets created
    Fixes potential nullptr dereference (similar to TRAC #2575).

--- src/Android/Main.cpp
+++ src/Android/Main.cpp
@@ -253,7 +253,7 @@ gcc_visibility_default
 JNIEXPORT void JNICALL
 Java_org_xcsoar_NativeView_pauseNative(JNIEnv *env, jobject obj)
 {
-  if (event_queue == NULL)
+  if (event_queue == nullptr || CommonInterface::main_window == nullptr)
     /* pause before we have initialized the event subsystem does not
        work - let's bail out, nothing is lost anyway */
     exit(0);
@@ -268,7 +268,7 @@ gcc_visibility_default
 JNIEXPORT void JNICALL
 Java_org_xcsoar_NativeView_resumeNative(JNIEnv *env, jobject obj)
 {
-  if (event_queue == NULL)
+  if (event_queue == nullptr || CommonInterface::main_window == nullptr)
     /* there is nothing here yet which can be resumed */
     exit(0);
 
