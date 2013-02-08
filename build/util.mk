d564dc9de4d75401b7bec187818df6b30a1a1832
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-02-08 23:40:57 +0100

    build/util: Added create-directory macro
    This standard Windows mkdir.exe does not know about the -p option. The
    GnuWin32 mkdir.exe understands it but needs to be called explicitly.

--- build/util.mk
+++ build/util.mk
@@ -1,5 +1,5 @@
 %/dirstamp:
-	@mkdir -p $(abspath $(@D))
+	@$(call create-directory,$(@D))
 	@touch $(abspath $@)
 
 
@@ -12,4 +12,18 @@ ifeq ($(WINHOST),y)
 slash-subst = $(subst /,\\,$(1))
 else
 slash-subst = $(1)
+
+
+# Creates a new directory.
+# mkdir -p on UNIX platforms.
+#
+# Example: $(call create-directory,foobar)
+#
+# Arguments: FOLDER
+ifeq ($(WINHOST),y)
+MKDIR ?= "c:\Program Files\GnuWin32\bin\mkdir"
+create-directory = $(MKDIR) -p "$(1)"
+else
+MKDIR ?= mkdir
+create-directory = $(MKDIR) -p $(abspath $(1))
 endif
