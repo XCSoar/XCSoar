d58285762813e25c6864817bb454a11f16ce23f7
Max Kellermann <max@duempel.org>
2013-02-15 22:58:41 +0100

    Makefile: add new Raspberry Pi include directory
    Add /opt/vc/include/interface/vmcs_host/linux, where vchost_config.h
    resides, to be included by
    /opt/vc/include/interface/vmcs_host/vcgencmd.h.  Adapts to a change in
    libraspberrypi-dev 1.20130207-1.

--- build/egl.mk
+++ build/egl.mk
@@ -19,6 +19,7 @@ ifeq ($(TARGET_IS_PI),y)
 # Raspberry Pi detected
 EGL_CPPFLAGS += -DUSE_VIDEOCORE
 EGL_CPPFLAGS += -isystem $(PI)/opt/vc/include -isystem $(PI)/opt/vc/include/interface/vcos/pthreads
+EGL_CPPFLAGS += -isystem $(PI)/opt/vc/include/interface/vmcs_host/linux
 EGL_LDLIBS += -L$(PI)/opt/vc/lib -lvchostif -lvchiq_arm -lvcos -lbcm_host
 else
 EGL_CPPFLAGS += -DUSE_X11
