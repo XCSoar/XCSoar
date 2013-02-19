d2e89165df362b2a050859838893a92e3ec94424
Henrik Bieler <Henrik.Bieler@nospamgmx.de>
2013-02-19 18:29:12 +0100

    vlapi:Replace new/delete by std::vector
    The line with delete[] caused a segmentation fault.
    The new/delete[] was replaced by switching to
    std::vector.
    This allows us to do some more cleanup:
    -DIRECTORY structure obsolete.
    -Since we have conv_dir() now using std::vector.
    The first "count only" call of the function is
    no longer necessary.
    The original use was to determine the count of
    entries so the dynamic array could be initialized
    with the correct size. std::vector makes that
    obsolete.
    -int number_of_flights used in conv_dir() is
    no longer necessary

--- src/Device/Driver/Volkslogger/Logger.cpp
+++ src/Device/Driver/Volkslogger/Logger.cpp
@@ -54,25 +54,23 @@ ParseTime(const tm &time,BrokenTime &btime)
 }
 
 static bool
-ConvertDirectoryToRecordedFlightList(const DIRECTORY &dir,
+ConvertDirectoryToRecordedFlightList(const std::vector<DIRENTRY> &dir,
                                      RecordedFlightList &flight_list)
 {
   RecordedFlightInfo flight_info;
-  DIRENTRY *pflight = dir.flights;
-  for (int i=0; (i < dir.nflights) && !flight_list.full(); i++) {
+  for (unsigned i=0; (i < dir.size()) && !flight_list.full(); i++) {
+    const DIRENTRY &flight = dir[i];
     /*
      * Only show logs with a takeoff detected
      */
-    if  (pflight->takeoff == 1) {
-      if (!ParseDate(pflight->firsttime, flight_info.date) ||
-          !ParseTime(pflight->firsttime, flight_info.start_time) ||
-          !ParseTime(pflight->lasttime, flight_info.end_time) )
+    if  (flight.takeoff == 1) {
+      if (!ParseDate(flight.firsttime, flight_info.date) ||
+          !ParseTime(flight.firsttime, flight_info.start_time) ||
+          !ParseTime(flight.lasttime, flight_info.end_time) )
         return false;
       flight_info.internal.volkslogger = i;
       flight_list.append(flight_info);
     }
-
-    pflight++;
   }
   return true;
 }
