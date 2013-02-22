7e3c926ab71762bd9a7ba589eeddae44195fe29f
Max Kellermann <max@duempel.org>
2013-02-22 22:26:24 +0100

    Makefile: split target "install"

--- build/install.mk
+++ build/install.mk
@@ -3,13 +3,20 @@ ifeq ($(TARGET),UNIX)
 DESTDIR =
 prefix = $(DESTDIR)/usr
 
-install: all mo manual
-	install -d -m 0755 $(prefix)/bin $(prefix)/share/doc/xcsoar
-	install -m 0755 $(TARGET_BIN_DIR)/xcsoar $(TARGET_BIN_DIR)/vali-xcs $(prefix)/bin
+install-mo: mo
 	install -d -m 0755 $(patsubst %,$(prefix)/share/locale/%/LC_MESSAGES,$(LINGUAS))
 	for i in $(LINGUAS); do \
 		install -m 0644 $(OUT)/po/$$i.mo $(prefix)/share/locale/$$i/LC_MESSAGES/xcsoar.mo; \
 	done
+
+install-bin: all
+	install -d -m 0755 $(prefix)/bin
+	install -m 0755 $(TARGET_BIN_DIR)/xcsoar $(TARGET_BIN_DIR)/vali-xcs $(prefix)/bin
+
+install-manual: manual
+	install -d -m 0755 $(prefix)/share/doc/xcsoar
 	install -m 0644 $(MANUAL_PDF) $(prefix)/share/doc/xcsoar
+
+install: install-bin install-mo install-manual
 
 endif
