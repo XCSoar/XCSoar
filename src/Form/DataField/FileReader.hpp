a428546142b2867cf2b1a9a148e6892c81e43ccf
Max Kellermann <max@duempel.org>
2013-02-22 10:35:40 +0100

    DataField/FileReader: add constant MAX_FILES

--- src/Form/DataField/FileReader.hpp
+++ src/Form/DataField/FileReader.hpp
@@ -51,10 +51,12 @@ public:
   };
 
 private:
+  static constexpr unsigned MAX_FILES = 100;
+
   /** Index of the active file */
   unsigned int mValue;
   /** FileList item array */
-  StaticArray<Item, 100> files;
+  StaticArray<Item, MAX_FILES> files;
 
   /**
    * Has the file list already been loaded?  This class tries to
