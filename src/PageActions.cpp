XCSoar/XCSoar

commit e236e55b1b2f340e2844c8289a21094d39d0b3af

PageSettings: add method Compress()
Always ensure that the page array is contiguous.  This simplifies a
lot of page switching code.

src/PageActions.cpp
@@ -48,13 +48,7 @@ GetCurrentLayout()
 void
 PageActions::Update()
 {
-  const PageSettings &settings = CommonInterface::GetUISettings().pages;
-  UIState &ui_state = CommonInterface::SetUIState();
-
-  if (!settings.pages[ui_state.page_index].IsDefined())
-    ui_state.page_index = NextIndex();
-
-  OpenLayout(settings.pages[ui_state.page_index]);
+  OpenLayout(GetCurrentLayout());
 }
 
 
@@ -64,11 +58,7 @@ PageActions::NextIndex()
   const PageSettings &settings = CommonInterface::GetUISettings().pages;
   const UIState &ui_state = CommonInterface::SetUIState();
 
-  unsigned i = ui_state.page_index;
-  do {
-    i = (i + 1) % PageSettings::MAX_PAGES;
-  } while (!settings.pages[i].IsDefined());
-  return i;
+  return (ui_state.page_index + 1) % settings.n_pages;
 }
 
 
@@ -88,11 +78,8 @@ PageActions::PrevIndex()
   const PageSettings &settings = CommonInterface::GetUISettings().pages;
   const UIState &ui_state = CommonInterface::SetUIState();
 
-  unsigned i = ui_state.page_index;
-  do {
-    i = (i == 0) ? PageSettings::MAX_PAGES - 1 : i - 1;
-  } while (!settings.pages[i].IsDefined());
-  return i;
+  return (ui_state.page_index + PageSettings::MAX_PAGES - 1)
+    % settings.n_pages;
 }
 
 

src/Dialogs/Settings/Panels/PagesConfigPanel.cpp
@@ -252,14 +252,9 @@ void
 PageListWidget::Initialise(ContainerWindow &parent, const PixelRect &rc)
 {
   settings = CommonInterface::GetUISettings().pages;
-  auto end = std::remove_if(settings.pages.begin(), settings.pages.end(),
-                            [](const PageLayout &layout) {
-                              return !layout.IsDefined();
-                            });
-  unsigned n = std::distance(settings.pages.begin(), end);
 
   CreateList(parent, UIGlobals::GetDialogLook(),
-             rc, Layout::Scale(18)).SetLength(n);
+             rc, Layout::Scale(18)).SetLength(settings.n_pages);
 
   CreateButtons(buttons->GetButtonPanel());
   UpdateButtons();
@@ -278,7 +273,8 @@ PageListWidget::Save(bool &_changed, gcc_unused bool &require_restart)
 {
   bool changed = false;
 
-  std::fill(settings.pages.begin() + GetList().GetLength(),
+  settings.n_pages = GetList().GetLength();
+  std::fill(settings.pages.begin() + settings.n_pages,
             settings.pages.end(),
             PageLayout::Undefined());
 
@@ -287,13 +283,15 @@ PageListWidget::Save(bool &_changed, gcc_unused bool &require_restart)
     PageLayout &dest = _settings.pages[i];
     const PageLayout &src = settings.pages[i];
     if (src != dest) {
-      dest = src;
       Profile::Save(src, i);
       changed = true;
     }
   }
 
-  PageActions::Update();
+  if (changed) {
+    _settings = settings;
+    PageActions::Update();
+  }
 
   _changed |= changed;
   return true;

src/PageSettings.cpp
@@ -25,6 +25,8 @@ Copyright_License {
 #include "InfoBoxes/InfoBoxSettings.hpp"
 #include "Language/Language.hpp"
 
+#include <algorithm>
+
 #include <stdio.h>
 
 void
@@ -99,4 +101,17 @@ PageSettings::SetDefaults()
   pages[1] = PageLayout::FullScreen();
 
   std::fill(pages.begin() + 2, pages.end(), PageLayout::Undefined());
+
+  n_pages = 2;
+}
+
+void
+PageSettings::Compress()
+{
+  auto last = std::remove_if(pages.begin(), pages.end(),
+                             [](const PageLayout &layout) {
+                               return !layout.IsDefined();
+                             });
+  std::fill(last, pages.end(), PageLayout::Undefined());
+  n_pages = std::distance(pages.begin(), last);
 }

src/PageSettings.hpp
@@ -171,7 +171,14 @@ struct PageSettings {
 
   std::array<PageLayout, MAX_PAGES> pages;
 
+  unsigned n_pages;
+
   void SetDefaults();
+
+  /**
+   * Eliminate empty pages to make the array contiguous.
+   */
+  void Compress();
 };
 
 static_assert(std::is_trivial<PageSettings>::value, "type is not trivial");

src/Profile/PageProfile.cpp
@@ -87,6 +87,8 @@ Profile::Load(PageSettings &settings)
 {
   for (unsigned i = 0; i < PageSettings::MAX_PAGES; ++i)
     ::Load(settings.pages[i], i);
+
+  settings.Compress();
 }
 
 void

commit bb31f3a8a735ed666050cc5d7e8e10dc8b4a77d9

PageActions: fix PrevIndex() formula
Regression by e236e55b.

src/PageActions.cpp
@@ -101,7 +101,7 @@ PageActions::PrevIndex()
        the last configured page */
     return ui_state.page_index;
 
-  return (ui_state.page_index + PageSettings::MAX_PAGES - 1)
+  return (ui_state.page_index + settings.n_pages - 1)
     % settings.n_pages;
 }
 