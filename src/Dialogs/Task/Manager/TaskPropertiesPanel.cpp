e069100a4c44ab256da88a5908233b628cf4f28c
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-03-12 22:03:35 +0100

    Dialogs/TaskPropertiesPanel: hidde FAI finish height if not a FAI task

--- src/Dialogs/Task/Manager/TaskPropertiesPanel.cpp
+++ src/Dialogs/Task/Manager/TaskPropertiesPanel.cpp
@@ -46,6 +46,20 @@ enum Controls {
   TASK_TYPE,
 };
 
+/**
+ * returns true if task is an FAI type
+ * @param ftype. task type being checked
+ */
+constexpr
+static bool
+IsFai(TaskFactoryType ftype)
+{
+  return ftype == TaskFactoryType::FAI_GENERAL ||
+    ftype == TaskFactoryType::FAI_GOAL ||
+    ftype == TaskFactoryType::FAI_OR ||
+    ftype == TaskFactoryType::FAI_TRIANGLE;
+}
+
 TaskPropertiesPanel::TaskPropertiesPanel(TaskManagerDialog &_dialog,
                                          OrderedTask **_active_task,
                                          bool *_task_modified)
@@ -86,6 +100,7 @@ TaskPropertiesPanel::RefreshView()
   SetRowVisible(FINISH_HEIGHT_REF, !fai_start_finish);
   LoadValueEnum(FINISH_HEIGHT_REF, p.finish_constraints.min_height_ref);
 
+  SetRowVisible(FAI_FINISH_HEIGHT, IsFai(ftype));
   LoadValue(FAI_FINISH_HEIGHT, fai_start_finish);
 
   LoadValueEnum(TASK_TYPE, ftype);
