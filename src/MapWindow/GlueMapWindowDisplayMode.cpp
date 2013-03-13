XCSoar/XCSoar

commit 337a60908c21e7dbd308d3d21be5a7308a1012fa

GlueMapWindowDisplayMode: Check availability of heading for DisplayMode::HEADING_UP
Fixes trac issue #2662, #2663.

src/MapWindow/GlueMapWindowDisplayMode.cpp
@@ -188,7 +188,8 @@ GlueMapWindow::UpdateScreenAngle()
     visible_projection.SetScreenAngle(calculated.task_stats.current_leg.
                                       vector_remaining.bearing);
   else if (orientation == DisplayOrientation::HEADING_UP)
-    visible_projection.SetScreenAngle(basic.attitude.heading);
+    visible_projection.SetScreenAngle(
+      basic.attitude.heading_available ? basic.attitude.heading : Angle::Zero());
   else if (orientation == DisplayOrientation::NORTH_UP ||
            !basic.track_available)
     visible_projection.SetScreenAngle(Angle::Zero());

commit 54fb652bba7f010375b8373be40d379d1822cfba

GlueMapWindowDisplayMode: Use AttitudeState::IsHeadingUsable()
Fixes a regression introduced in
commit 337a60908c21e7dbd308d3d21be5a7308a1012fa

src/MapWindow/GlueMapWindowDisplayMode.cpp
@@ -189,7 +189,7 @@ GlueMapWindow::UpdateScreenAngle()
                                       vector_remaining.bearing);
   else if (orientation == DisplayOrientation::HEADING_UP)
     visible_projection.SetScreenAngle(
-      basic.attitude.heading_available ? basic.attitude.heading : Angle::Zero());
+      basic.attitude.IsHeadingUseable() ? basic.attitude.heading : Angle::Zero());
   else if (orientation == DisplayOrientation::NORTH_UP)
     visible_projection.SetScreenAngle(Angle::Zero());
   else if (orientation == DisplayOrientation::WIND_UP &&