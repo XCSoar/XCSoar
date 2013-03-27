0922135c7d2a502e4e59c3239f37edb7592366d9
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-03-27 19:30:25 +0100

    android.mk: Pass cache setting to submakes

--- build/android.mk
+++ build/android.mk
@@ -187,7 +187,7 @@ $$(ANDROID_BUILD)/libs/$(2)/lib$(1).so: $$(OUT)/$(3)/bin/lib$(1).so | $$(ANDROID
 	$$(Q)cp $$< $$@
 
 $$(OUT)/$(3)/bin/lib$(1).so:
-	$$(Q)$$(MAKE) TARGET=$(3) DEBUG=$$(DEBUG) IOIOLIB_DIR=$$(IOIOLIB_DIR) $$@
+	$$(Q)$$(MAKE) TARGET=$(3) DEBUG=$$(DEBUG) IOIOLIB_DIR=$$(IOIOLIB_DIR) USE_CCACHE=$$(USE_CCACHE) $$@
 
 endef
 
