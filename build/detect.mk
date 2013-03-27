535c567b29bca1e8f2c71dd7812ce96039dfe40e
Max Kellermann <max@duempel.org>
2013-03-27 21:40:48 +0100

    Makefile: auto-detect host ARMv6, ARMv7, NEON
    Compile with NEON enabled on the Cubieboard.

--- build/detect.mk
+++ build/detect.mk
@@ -10,7 +10,16 @@ HOST_IS_MINGW := $(call string_contains,$(UNAME_S),MINGW)
 HOST_IS_WIN32 := $(call bool_or,$(HOST_IS_CYGWIN),$(HOST_IS_MINGW))
 HOST_IS_UNIX := $(call bool_not,$(HOST_IS_WIN32))
 
-ifeq ($(HOST_IS_LINUX),y)
+HOST_IS_ARMV6 := $(call string_equals,$(UNAME_M),armv6l)
+HOST_IS_ARMV7 := $(call string_equals,$(UNAME_M),armv7l)
+
+ifeq ($(HOST_IS_ARMV7),y)
+HOST_HAS_NEON := $(call string_contains,$(shell grep -E ^Features /proc/cpuinfo),neon)
+else
+HOST_HAS_NEON := n
+endif
+
+ifeq ($(HOST_IS_LINUX)$(HOST_IS_ARMV6),yy)
 # Check for VideoCore headers present on a Raspberry Pi
 HOST_IS_PI := $(call string_equals,$(shell test -f /opt/vc/include/interface/vmcs_host/vc_dispmanx.h && echo y),y)
 else
--- build/targets.mk
+++ build/targets.mk
@@ -181,6 +181,9 @@ ifeq ($(TARGET),UNIX)
   TCPREFIX := $(LOCAL_TCPREFIX)
   TCSUFFIX := $(LOCAL_TCSUFFIX)
   TARGET_IS_PI = $(HOST_IS_PI)
+  ARMV6 = $(HOST_IS_ARMV6)
+  ARMV7 = $(HOST_IS_ARMV7)
+  NEON = $(HOST_HAS_NEON)
 endif
 
 ifeq ($(TARGET),UNIX32)
@@ -199,6 +202,7 @@ ifeq ($(TARGET),PI)
   PI ?= /opt/pi/root
   TARGET_ARCH += -march=armv6j -mfpu=vfp -mfloat-abi=hard
   TARGET_IS_PI = y
+  ARMV6 = y
 endif
 
 ifeq ($(TARGET),KOBO)
@@ -216,6 +220,18 @@ ifeq ($(TARGET),UNIX)
   HAVE_WIN32 := n
   HAVE_MSVCRT := n
   HAVE_VASPRINTF := y
+
+  ifeq ($(ARMV6),y)
+    TARGET_ARCH += -march=armv6
+  endif
+
+  ifeq ($(ARMV7),y)
+    TARGET_ARCH += -march=armv7-a
+  endif
+
+  ifeq ($(NEON),y)
+    TARGET_ARCH += -mfpu=neon -mfpu=vfpv3-d16
+  endif
 endif
 
 ifeq ($(filter $(TARGET),UNIX WINE),$(TARGET))
