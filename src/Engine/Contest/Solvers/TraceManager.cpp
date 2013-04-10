XCSoar/XCSoar

commit 70df027dee869c3979b7c3a1c468962ba94ff5ad

ContestDijkstra: split Dijkstra specific code and trace management code
Create new class TraceManager which manages the trace point vector.
To be used later.

src/Engine/Contest/Solvers/TraceManager.cpp
@@ -1,0 +1,142 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2013 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#include "TraceManager.hpp"
+#include "Trace/Trace.hpp"
+
+#include <assert.h>
+
+TraceManager::TraceManager(const Trace &_trace)
+  :trace_master(_trace),
+   predicted(TracePoint::Invalid())
+{
+}
+
+
+bool
+TraceManager::SetPredicted(const TracePoint &_predicted)
+{
+  TracePoint n(_predicted);
+  if (n.IsDefined() && !trace_master.empty())
+    n.Project(trace_master.GetProjection());
+
+  bool result = false;
+  if (predicted.IsDefined() && n_points > 0) {
+    if (n.IsDefined() && !trace_master.empty() &&
+        n.GetFlatLocation() == predicted.GetFlatLocation()) {
+      /* no change since last call, but update time stamp */
+      predicted = n;
+      return false;
+    }
+
+    /* the predicted location has changed, and the solver must be
+       restarted */
+    Reset();
+    result = true;
+  }
+
+  predicted = n;
+  return result;
+}
+
+bool
+TraceManager::IsMasterUpdated(bool continuous) const
+{
+  /* disabled assertion, TraceManager doesn't know about stages */
+  // assert(num_stages <= MAX_STAGES);
+
+  if (CheckMasterSerial())
+    return true;
+
+  if (continuous)
+    return false;
+
+  /* TODO: disabled check, move it to ContestDijkstra */
+  //if (n_points < num_stages)
+  //  return true;
+
+  // find min distance and time step within this trace
+  const unsigned threshold_delta_t_trace = trace_master.GetAverageDeltaTime();
+  const unsigned threshold_distance_trace = trace_master.GetAverageDeltaDistance();
+
+  const TracePoint &last_master = trace_master.back();
+  const TracePoint &last_point = *trace.back();
+
+  // update trace if time and distance are greater than significance thresholds
+
+  return last_master.GetTime() > last_point.GetTime() + threshold_delta_t_trace &&
+    last_master.FlatDistanceTo(last_point) > threshold_distance_trace;
+}
+
+void
+TraceManager::ClearTrace()
+{
+  append_serial = modify_serial = Serial();
+  trace_dirty = true;
+  trace.clear();
+  n_points = 0;
+  predicted = TracePoint::Invalid();
+}
+
+void
+TraceManager::UpdateTraceFull()
+{
+  trace.reserve(trace_master.GetMaxSize());
+  trace_master.GetPoints(trace);
+  n_points = trace.size();
+
+  if (n_points > 0 && predicted.IsDefined())
+    predicted.Project(trace_master.GetProjection());
+
+  append_serial = trace_master.GetAppendSerial();
+  modify_serial = trace_master.GetModifySerial();
+}
+
+bool
+TraceManager::UpdateTraceTail()
+{
+  /* the following assertions were disabled because this method doesn't
+     get the "force" and "continuous" parameter */
+  //assert(continuous);
+  //assert(incremental == finished || force);
+  assert(modify_serial == trace_master.GetModifySerial());
+
+  if (!trace_master.SyncPoints(trace))
+    /* no new points */
+    return false;
+
+  n_points = trace.size();
+
+  if (n_points > 0 && predicted.IsDefined())
+    predicted.Project(trace_master.GetProjection());
+
+  append_serial = trace_master.GetAppendSerial();
+  return true;
+}
+
+void
+TraceManager::UpdateTrace(bool force)
+{
+}
+
+

build/hot.mk
@@ -25,7 +25,8 @@ HOT_SOURCES = \
 	$(ENGINE_SRC_DIR)/Route/RoutePolar.cpp \
 	$(ENGINE_SRC_DIR)/Route/RouteLink.cpp \
 	$(ENGINE_SRC_DIR)/Route/RoutePolars.cpp \
-	$(ENGINE_SRC_DIR)/Contest/Solvers/ContestDijkstra.cpp
+	$(ENGINE_SRC_DIR)/Contest/Solvers/ContestDijkstra.cpp \
+	$(ENGINE_SRC_DIR)/Contest/Solvers/TraceManager.cpp
 
 $(call SRC_TO_OBJ,$(HOT_SOURCES)): OPTIMIZE += -O3
 

build/libcontest.mk
@@ -5,6 +5,7 @@ CONTEST_SOURCES = \
 	$(CONTEST_SRC_DIR)/ContestManager.cpp \
 	$(CONTEST_SRC_DIR)/Solvers/Contests.cpp \
 	$(CONTEST_SRC_DIR)/Solvers/AbstractContest.cpp \
+	$(CONTEST_SRC_DIR)/Solvers/TraceManager.cpp \
 	$(CONTEST_SRC_DIR)/Solvers/ContestDijkstra.cpp \
 	$(CONTEST_SRC_DIR)/Solvers/DMStQuad.cpp \
 	$(CONTEST_SRC_DIR)/Solvers/OLCLeague.cpp \

src/Engine/Contest/Solvers/ContestDijkstra.cpp
@@ -39,123 +39,21 @@ ContestDijkstra::ContestDijkstra(const Trace &_trace,
                                  const unsigned finish_alt_diff)
   :AbstractContest(finish_alt_diff),
    NavDijkstra(n_legs + 1),
-   trace_master(_trace),
+   TraceManager(_trace),
    continuous(_continuous),
-   incremental(false),
-   predicted(TracePoint::Invalid())
+   incremental(false)
 {
   assert(num_stages <= MAX_STAGES);
 
   std::fill(stage_weights, stage_weights + num_stages - 1, 5);
 }
 
-bool
-ContestDijkstra::SetPredicted(const TracePoint &_predicted)
-{
-  TracePoint n(_predicted);
-  if (n.IsDefined() && !trace_master.empty())
-    n.Project(trace_master.GetProjection());
-
-  bool result = false;
-  if (predicted.IsDefined() && n_points > 0) {
-    if (n.IsDefined() && !trace_master.empty() &&
-        n.GetFlatLocation() == predicted.GetFlatLocation()) {
-      /* no change since last call, but update time stamp */
-      predicted = n;
-      return false;
-    }
-
-    /* the predicted location has changed, and the solver must be
-       restarted */
-    Reset();
-    result = true;
-  }
-
-  predicted = n;
-  return result;
-}
-
-bool
-ContestDijkstra::IsMasterUpdated() const
-{
-  assert(num_stages <= MAX_STAGES);
-
-  if (modify_serial != trace_master.GetModifySerial())
-    return true;
-
-  if (continuous)
-    return false;
-
-  if (n_points < num_stages)
-    return true;
-
-  // find min distance and time step within this trace
-  const unsigned threshold_delta_t_trace = trace_master.GetAverageDeltaTime();
-  const unsigned threshold_distance_trace = trace_master.GetAverageDeltaDistance();
-
-  const TracePoint &last_master = trace_master.back();
-  const TracePoint &last_point = *trace.back();
-
-  // update trace if time and distance are greater than significance thresholds
-
-  return last_master.GetTime() > last_point.GetTime() + threshold_delta_t_trace &&
-    last_master.FlatDistanceTo(last_point) > threshold_distance_trace;
-}
-
-void
-ContestDijkstra::ClearTrace()
-{
-  append_serial = modify_serial = Serial();
-  trace_dirty = true;
-  finished = false;
-  trace.clear();
-  n_points = 0;
-}
-
-void
-ContestDijkstra::UpdateTraceFull()
-{
-  trace.reserve(trace_master.GetMaxSize());
-  trace_master.GetPoints(trace);
-  n_points = trace.size();
-
-  if (n_points > 0 && predicted.IsDefined())
-    predicted.Project(trace_master.GetProjection());
-
-  append_serial = trace_master.GetAppendSerial();
-  modify_serial = trace_master.GetModifySerial();
-}
-
-bool
-ContestDijkstra::UpdateTraceTail()
-{
-  assert(continuous);
-  /* the following assertion was disabled because this method doesn't
-     get the "force" parameter */
-  //assert(incremental == finished || force);
-  assert(modify_serial == trace_master.GetModifySerial());
-
-  if (!trace_master.SyncPoints(trace))
-    /* no new points */
-    return false;
-
-  n_points = trace.size();
-
-  if (n_points > 0 && predicted.IsDefined())
-    predicted.Project(trace_master.GetProjection());
-
-  append_serial = trace_master.GetAppendSerial();
-  return true;
-}
-
 void
 ContestDijkstra::UpdateTrace(bool force)
 {
-  if (append_serial == trace_master.GetAppendSerial())
-    /* unmodified */
-    return;
+  if (IsMasterAppended()) return; /* unmodified */
 
-  if (IsMasterUpdated()) {
+  if (IsMasterUpdated(continuous)) {
     UpdateTraceFull();
 
     trace_dirty = true;
@@ -194,6 +92,7 @@ ContestDijkstra::Solve(bool exhaustive)
   if (trace_master.size() < num_stages) {
     /* not enough data in master trace */
     ClearTrace();
+    finished = false;
     return SolverResult::FAILED;
   }
 
@@ -207,7 +106,7 @@ ContestDijkstra::Solve(bool exhaustive)
     if (!trace_dirty && !finished)
       return SolverResult::FAILED;
   } else if (exhaustive || n_points < num_stages ||
-             modify_serial != trace_master.GetModifySerial()) {
+             CheckMasterSerial()) {
     UpdateTrace(exhaustive);
     if (n_points < num_stages)
       return SolverResult::FAILED;
@@ -250,7 +149,7 @@ ContestDijkstra::Reset()
 {
   dijkstra.Clear();
   ClearTrace();
-  predicted = TracePoint::Invalid();
+  finished = false;
 
   AbstractContest::Reset();
 }
@@ -267,8 +166,8 @@ ContestDijkstra::SaveSolution()
          ? predicted
          /* fallback, just in case somebody has deleted the prediction
             meanwhile */
-         : GetPoint(n_points - 1))
-      : GetPoint(NavDijkstra::solution[i]);
+         : TraceManager::GetPoint(n_points - 1))
+      : TraceManager::GetPoint(NavDijkstra::solution[i]);
 
   return AbstractContest::SaveSolution();
 }
@@ -310,7 +209,7 @@ ContestDijkstra::AddStartEdges()
   assert(n_points > 0);
 
   const int max_altitude = incremental
-    ? GetMaximumStartAltitude(GetPoint(n_points - 1))
+    ? GetMaximumStartAltitude(TraceManager::GetPoint(n_points - 1))
     : 0;
 
   for (ScanTaskPoint destination(0, 0), end(0, n_points);

src/Engine/Contest/Solvers/ContestDijkstra.hpp
@@ -28,6 +28,7 @@ Copyright_License {
 #include "AbstractContest.hpp"
 #include "PathSolvers/NavDijkstra.hpp"
 #include "Trace/Vector.hpp"
+#include "TraceManager.hpp"
 
 #include <assert.h>
 
@@ -41,25 +42,7 @@ class Trace;
  *
  *
  */
-class ContestDijkstra : public AbstractContest, protected NavDijkstra {
-protected:
-  const Trace &trace_master;
-
-private:
-  /**
-   * This attribute tracks Trace::GetAppendSerial().  It is updated
-   * when appnew copy of the master Trace is obtained, and is used to
-   * check if that copy should be replaced with a new one.
-   */
-  Serial append_serial;
-
-  /**
-   * This attribute tracks Trace::GetModifySerial().  It is updated
-   * when a new copy of the master Trace is obtained, and is used to
-   * check if that copy should be replaced with a new one.
-   */
-  Serial modify_serial;
-
+class ContestDijkstra : public AbstractContest, protected NavDijkstra, public TraceManager {
   /**
    * Is this a contest that allows continuous analysis?
    */
@@ -72,8 +55,6 @@ private:
    */
   bool incremental;
 
-  bool trace_dirty;
-
   /**
    * Did the last Dijkstra search finish (even if without a valid
    * solution)?  This means the Dijkstra object still contains valid
@@ -83,25 +64,11 @@ private:
   bool finished;
 
   /**
-   * Working trace for solver.  This contains pointers to trace_master
-   * records, which get Invalidated when the trace gets thinned.  Be
-   * careful!
-   */
-  TracePointerVector trace;
-
-  /**
    * The last solution.  Use only if Solve() has returned VALID.
    */
   ContestTraceVector solution;
 
-  TracePoint predicted;
-
-  static constexpr unsigned predicted_index = 0xffff;
-
 protected:
-  /** Number of points in current trace set */
-  unsigned n_points;
-
   /**
    * The index of the first finish candidate.  During incremental
    * scan, only the new trace points are considered.
@@ -128,46 +95,15 @@ public:
     incremental = _incremental;
   }
 
-  /**
-   * Sets the location of the "predicted" finish location.  If
-   * defined, then the algorithm will assume that you will reach it.
-   * Pass an "invalid" #TracePoint to disable this prediction (see
-   * TracePoint::Invalid()).
-   *
-   * @return true if the object was reset
-   */
-  bool SetPredicted(const TracePoint &_predicted);
-
 protected:
   bool IsIncremental() const {
     return incremental;
   }
 
   gcc_pure
-  const TracePoint &GetPoint(unsigned i) const {
-    assert(i < n_points);
-
-    return *trace[i];
-  }
-
-  gcc_pure
   const TracePoint &GetPoint(const ScanTaskPoint sp) const {
-    return GetPoint(sp.GetPointIndex());
+    return TraceManager::GetPoint(sp.GetPointIndex());
   }
-
-  void ClearTrace();
-
-  /**
-   * Obtain a new #Trace copy.
-   */
-  void UpdateTraceFull();
-
-  /**
-   * Copy points that were added to the end of the master Trace.
-   *
-   * @return true if new points were added
-   */
-  bool UpdateTraceTail();
 
   void AddEdges(ScanTaskPoint origin, unsigned first_point);
 
@@ -212,9 +148,6 @@ protected:
   }
 
 private:
-  gcc_pure
-  bool IsMasterUpdated() const;
-
   bool SaveSolution();
 
 protected:

src/Engine/Contest/Solvers/OLCSprint.cpp
@@ -62,12 +62,12 @@ OLCSprint::FindStart() const
   assert(n_points >= 2);
 
   unsigned start_index = 0;
-  const auto end_time = GetPoint(n_points - 1).GetTime();
+  const auto end_time = TraceManager::GetPoint(n_points - 1).GetTime();
   if (end_time > 9000) {
     // fast forward to 2.5 hours before finish
     const unsigned start_time = end_time-9000;
     assert(start_index < n_points);
-    while (GetPoint(start_index).GetTime() < start_time) {
+    while (TraceManager::GetPoint(start_index).GetTime() < start_time) {
       ++start_index;
       assert(start_index < n_points);
     }
@@ -83,7 +83,7 @@ OLCSprint::AddStartEdges()
   assert(num_stages > 0);
   assert(n_points > 0);
 
-  const int max_altitude = GetMaximumStartAltitude(GetPoint(n_points - 1));
+  const int max_altitude = GetMaximumStartAltitude(TraceManager::GetPoint(n_points - 1));
 
   const ScanTaskPoint start(0, FindStart());
 

src/Engine/Contest/Solvers/OLCTriangle.cpp
@@ -101,7 +101,7 @@ OLCTriangle::IsPathClosed() const
   // note this may fail if resolution of sampled trace is too low
   assert(n_points > 0);
 
-  const GeoPoint end_location = GetPoint(n_points - 1).GetLocation();
+  const GeoPoint end_location = TraceManager::GetPoint(n_points - 1).GetLocation();
 
   fixed d_min(-1);
 
@@ -109,7 +109,7 @@ OLCTriangle::IsPathClosed() const
 
   for (unsigned start_index = 0; start_index <= first_tp; ++start_index) {
     const fixed d_this =
-      GetPoint(start_index).GetLocation().Distance(end_location);
+      TraceManager::GetPoint(start_index).GetLocation().Distance(end_location);
 
     if (!positive(d_min) || d_this < d_min)
       d_min = d_this;
@@ -133,14 +133,14 @@ OLCTriangle::ClosingPoint(unsigned first_index, unsigned last_index,
   // if old closing point is prior to tp3 reset it.
   if (closing < last_index) closing = last_index;
 
-  const SearchPoint first = GetPoint(first_index);
+  const SearchPoint first = TraceManager::GetPoint(first_index);
 
   unsigned alternate = 0;
   while(true) {
     if (closing + alternate < n_points) {
-      const SearchPoint last = GetPoint(closing + alternate);
+      const SearchPoint last = TraceManager::GetPoint(closing + alternate);
 
-      if (GetPoint(closing + alternate).GetIntegerAltitude() >= min_altitude &&
+      if (TraceManager::GetPoint(closing + alternate).GetIntegerAltitude() >= min_altitude &&
           first.FlatDistanceTo(last) < max_range &&
           first.GetLocation().Distance(last.GetLocation()) < max_distance) {
         // closing pair found. return
@@ -153,9 +153,9 @@ OLCTriangle::ClosingPoint(unsigned first_index, unsigned last_index,
     }
 
     if (signed(closing) - signed(alternate) >= signed(last_index)) {
-      const SearchPoint last = GetPoint(closing - alternate);
+      const SearchPoint last = TraceManager::GetPoint(closing - alternate);
 
-      if (GetPoint(closing - alternate).GetIntegerAltitude() >= min_altitude &&
+      if (TraceManager::GetPoint(closing - alternate).GetIntegerAltitude() >= min_altitude &&
           first.FlatDistanceTo(last) < max_range &&
           first.GetLocation().Distance(last.GetLocation()) < max_distance) {
         // closing pair found. return

src/Engine/Contest/Solvers/TraceManager.hpp
@@ -1,0 +1,134 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2013 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef TRACE_MANAGER_HPP
+#define TRACE_MANAGER_HPP
+
+#include "Util/Serial.hpp"
+#include "Trace/Trace.hpp"
+#include "Trace/Vector.hpp"
+#include "Trace/Point.hpp"
+
+class TraceManager {
+protected:
+  const Trace &trace_master;
+
+private:
+  /**
+   * This attribute tracks Trace::GetAppendSerial().  It is updated
+   * when appnew copy of the master Trace is obtained, and is used to
+   * check if that copy should be replaced with a new one.
+   */
+  Serial append_serial;
+
+  /**
+   * This attribute tracks Trace::GetModifySerial().  It is updated
+   * when a new copy of the master Trace is obtained, and is used to
+   * check if that copy should be replaced with a new one.
+   */
+  Serial modify_serial;
+
+protected:
+  /**
+   * Working trace for solver.  This contains pointers to trace_master
+   * records, which get Invalidated when the trace gets thinned.  Be
+   * careful!
+   */
+  TracePointerVector trace;
+
+  /** Number of points in current trace set */
+  unsigned n_points;
+
+  TracePoint predicted;
+
+  static constexpr unsigned predicted_index = 0xffff;
+
+  bool trace_dirty;
+
+public:
+  /**
+   * Constructor
+   *
+   * @param _trace Trace object reference to use for solving
+   */
+  TraceManager(const Trace &_trace);
+
+  /**
+   * Sets the location of the "predicted" finish location.  If
+   * defined, then the algorithm will assume that you will reach it.
+   * Pass an "invalid" #TracePoint to disable this prediction (see
+   * TracePoint::Invalid()).
+   *
+   * @return true if the object was reset
+   */
+  bool SetPredicted(const TracePoint &_predicted);
+
+protected:
+  void ClearTrace();
+
+  /**
+   * Obtain a new #Trace copy.
+   */
+  void UpdateTraceFull();
+
+  /**
+   * Copy points that were added to the end of the master Trace.
+   *
+   * @return true if new points were added
+   */
+  bool UpdateTraceTail();
+
+  gcc_pure
+  const TracePoint &GetPoint(unsigned i) const {
+    assert(i < n_points);
+
+    return *trace[i];
+  }
+
+  gcc_pure
+  bool IsMasterUpdated(bool continuous) const;
+
+  gcc_pure
+  bool CheckMasterSerial() const {
+    return modify_serial != trace_master.GetModifySerial();
+  }
+
+  gcc_pure
+  bool IsMasterAppended() const {
+    return append_serial == trace_master.GetAppendSerial();
+  }
+
+protected:
+  /**
+   * Update working trace from master.
+   *
+   * @param force disable lazy updates, force the trace to be up to
+   * date before returning
+   */
+  virtual void UpdateTrace(bool force);
+
+  virtual void Reset() = 0;
+};
+
+#endif /* TRACE_MANAGER_HPP */
+

commit 03e5e9644321ba24c8bce14cdf7dfe79ca8b8f5f

Contest/TraceManager: fix crash when local copy is empty
A regression by commit 70df027d removed the "n_points<num_stages"
check and thus caused a call to back() on an empty vector.  Naturally,
this must crash.  A new check for an empty vector replaces the old
check.

src/Engine/Contest/Solvers/TraceManager.cpp
@@ -71,6 +71,9 @@ TraceManager::IsMasterUpdated(bool continuous) const
   if (continuous)
     return false;
 
+  if (trace.empty())
+    return true;
+
   /* TODO: disabled check, move it to ContestDijkstra */
   //if (n_points < num_stages)
   //  return true;