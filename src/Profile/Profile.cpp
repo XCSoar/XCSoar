XCSoar/XCSoar

commit e2c5daee54b58640e8fcc53f5314722c75ae1cc5

Profile: use narrow strings by default
Convert from UTF-8 to TCHAR only when needed.  This reduces overhead
on Windows.

src/Profile/Profile.cpp
@@ -66,7 +66,7 @@ Profile::LoadFile(const TCHAR *szFile)
   if (StringIsEmpty(szFile))
     return;
 
-  FileLineReader reader(szFile);
+  FileLineReaderA reader(szFile);
   if (reader.error())
     return;
 
@@ -148,18 +148,18 @@ Profile::SetFiles(const TCHAR *override_path)
 }
 
 bool
-Profile::GetPath(const TCHAR *key, TCHAR *value)
+Profile::GetPath(const char *key, TCHAR *value)
 {
-  const TCHAR *p = Get(key);
-  if (p == NULL || StringIsEmpty(p))
-    return false;
+  TCHAR buffer[MAX_PATH];
+  if (!Get(key, buffer, ARRAY_SIZE(buffer)))
+      return false;
 
-  ExpandLocalPath(value, p);
+  ExpandLocalPath(value, buffer);
   return true;
 }
 
 bool
-Profile::GetPathIsEqual(const TCHAR *key, const TCHAR *value)
+Profile::GetPathIsEqual(const char *key, const TCHAR *value)
 {
   TCHAR saved[MAX_PATH];
   if (!GetPath(key, saved))
@@ -169,12 +169,13 @@ Profile::GetPathIsEqual(const TCHAR *key, const TCHAR *value)
 }
 
 const TCHAR *
-Profile::GetPathBase(const TCHAR *key)
+Profile::GetPathBase(const char *key)
 {
-  const TCHAR *p = Get(key);
-  if (p == NULL)
-    return NULL;
+  TCHAR buffer[MAX_PATH];
+  if (!Get(key, buffer, ARRAY_SIZE(buffer)))
+      return nullptr;
 
+  const TCHAR *p = buffer;
   if (DIR_SEPARATOR != '\\') {
     const TCHAR *backslash = _tcsrchr(p, _T('\\'));
     if (backslash != NULL)
@@ -185,7 +186,7 @@ Profile::GetPathBase(const TCHAR *key)
 }
 
 void
-Profile::SetPath(const TCHAR *key, const TCHAR *value)
+Profile::SetPath(const char *key, const TCHAR *value)
 {
   TCHAR path[MAX_PATH];
 

src/Dialogs/Plane/PlaneListDialog.cpp
@@ -194,7 +194,7 @@ PlaneListWidget::OnPaintItem(Canvas &canvas, const PixelRect rc, unsigned i)
 
   canvas.Select(name_font);
 
-  if (Profile::GetPathIsEqual(_T("PlanePath"), list[i].path)) {
+  if (Profile::GetPathIsEqual("PlanePath", list[i].path)) {
     StaticString<256> buffer;
     buffer.Format(_T("%s - %s"), list[i].name.c_str(), _("Active"));
     canvas.DrawClippedText(rc.left + Layout::FastScale(2),
@@ -218,7 +218,7 @@ LoadFile(const TCHAR *path)
   if (!PlaneGlue::ReadFile(settings.plane, path))
     return false;
 
-  Profile::SetPath(_T("PlanePath"), path);
+  Profile::SetPath("PlanePath", path);
   PlaneGlue::Synchronize(settings.plane, settings,
                          settings.polar.glide_polar_task);
   if (protected_task_manager != NULL)
@@ -336,14 +336,14 @@ PlaneListWidget::EditClicked()
 
       File::Delete(old_path);
       PlaneGlue::WriteFile(plane, path);
-      if (Profile::GetPathIsEqual(_T("PlanePath"), old_path)) {
+      if (Profile::GetPathIsEqual("PlanePath", old_path)) {
         list[index].path = path;
         list[index].name = filename;
         Load(index);
       }
     } else {
       PlaneGlue::WriteFile(plane, old_path);
-      if (Profile::GetPathIsEqual(_T("PlanePath"), old_path))
+      if (Profile::GetPathIsEqual("PlanePath", old_path))
         Load(index);
     }
 

src/Dialogs/Settings/Panels/TrackingConfigPanel.cpp
@@ -207,7 +207,7 @@ TrackingConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
 
 #ifdef HAVE_SKYLINES_TRACKING
 static bool
-SaveKey(const RowFormWidget &form, unsigned idx, const TCHAR *profile_key,
+SaveKey(const RowFormWidget &form, unsigned idx, const char *profile_key,
         uint64_t &value_r)
 {
   const TCHAR *const s = form.GetValueString(idx);

src/Dialogs/Settings/dlgConfigFonts.cpp
@@ -64,7 +64,7 @@ extern LOGFONT log_map_label_important;
 static bool changed = false;
 
 class FontPreviewAndButton : public NullWidget, private ActionListener {
-  const TCHAR *const profile_key;
+  const char *const profile_key;
   const TCHAR *const text;
 
   const LOGFONT defaults;
@@ -78,7 +78,7 @@ class FontPreviewAndButton : public NullWidget, private ActionListener {
   WndButton *button;
 
 public:
-  FontPreviewAndButton(const TCHAR *_key, const TCHAR *_text,
+  FontPreviewAndButton(const char *_key, const TCHAR *_text,
                        const LOGFONT _defaults,
                        bool _enabled)
     :profile_key(_key), text(_text), defaults(_defaults),
@@ -205,7 +205,7 @@ private:
 };
 
 static const struct {
-  const TCHAR *registry_key;
+  const char *registry_key;
   const TCHAR *text;
   const LOGFONT &defaults;
 } customisable_fonts[] = {

src/Dialogs/Waypoint/dlgWaypointDetails.cpp
@@ -357,7 +357,7 @@ UpdateCaption()
   StaticString<256> buffer;
   buffer.Format(_T("%s: %s"), _("Waypoint"), waypoint->name.c_str());
 
-  const TCHAR *key = nullptr;
+  const char *key = nullptr;
   switch (waypoint->file_num) {
   case 1:
     key = ProfileKeys::WaypointFile;

src/FLARM/FlarmId.cpp
@@ -40,9 +40,18 @@ FlarmId::Parse(const TCHAR *input, TCHAR **endptr_r)
 }
 #endif
 
+const char *
+FlarmId::Format(char *buffer) const
+{
+  sprintf(buffer, "%lX", (unsigned long)value);
+  return buffer;
+}
+
+#ifdef _UNICODE
 const TCHAR *
 FlarmId::Format(TCHAR *buffer) const
 {
   _stprintf(buffer, _T("%lX"), (unsigned long)value);
   return buffer;
 }
+#endif

src/FLARM/FlarmId.hpp
@@ -27,7 +27,10 @@ Copyright_License {
 #include "Compiler.h"
 
 #include <stdint.h>
+
+#ifdef _UNICODE
 #include <tchar.h>
+#endif
 
 /**
  * The identification number of a FLARM traffic.
@@ -69,7 +72,10 @@ public:
   static FlarmId Parse(const TCHAR *input, TCHAR **endptr_r);
 #endif
 
+  const char *Format(char *buffer) const;
+#ifdef _UNICODE
   const TCHAR *Format(TCHAR *buffer) const;
+#endif
 };
 
 #endif

src/FLARM/NameDatabase.hpp
@@ -30,6 +30,7 @@ Copyright_License {
 #include "Compiler.h"
 
 #include <assert.h>
+#include <tchar.h>
 
 class FlarmNameDatabase {
 public:

commit 7be0554318f468783f9060da0daffe0236c05fa6

Profile/Profile: Consider empty ("") paths as not set
Regression introduced in commit e2c5daee54b58640e8fcc53f5314722c75ae1cc5

Fixes trac issue #2803.

src/Profile/Profile.cpp
@@ -155,6 +155,9 @@ Profile::GetPath(const char *key, TCHAR *value)
   if (!Get(key, buffer, ARRAY_SIZE(buffer)))
       return false;
 
+  if (StringIsEmpty(buffer))
+    return false;
+
   ExpandLocalPath(value, buffer);
   return true;
 }