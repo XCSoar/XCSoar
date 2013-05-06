XCSoar/XCSoar

commit 7b530132fab2934619e21cdbf110e7f8e654e89d

DeviceDescriptor: check cancellation state in OpenOnPort()
Abort the whole operation if the user has pressed "cancel".

src/Device/Descriptor.cpp
@@ -225,7 +225,7 @@ DeviceDescriptor::OpenOnPort(Port &_port, OperationEnvironment &env)
     port->StartRxThread();
 
   EnableNMEA(env);
-  return true;
+  return !env.IsCancelled();
 }
 
 bool

commit d9461c185e08cc8833fdf7c109a6d6618f2da33f

Device/Descriptor: fix double free() bug in OpenOnPort()
When the user cancels the operation, clean up.  Fixes a regression by
commit 7b530132.

src/Device/Descriptor.cpp
@@ -230,7 +230,16 @@ DeviceDescriptor::OpenOnPort(Port *_port, OperationEnvironment &env)
     port->StartRxThread();
 
   EnableNMEA(env);
-  return !env.IsCancelled();
+
+  if (env.IsCancelled()) {
+    /* the caller is responsible for freeing the port on error */
+    port = nullptr;
+    delete device;
+    device = nullptr;
+    return false;
+  }
+
+  return true;
 }
 
 bool