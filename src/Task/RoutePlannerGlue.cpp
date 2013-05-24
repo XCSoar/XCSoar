d2173d79058027cdc4f19d7c1ec47716a848aeb8
Max Kellermann <max@duempel.org>
2013-05-24 18:04:58 +0200

    RoutePlannerGlue: include cleanup

--- src/Task/RoutePlannerGlue.cpp
+++ src/Task/RoutePlannerGlue.cpp
@@ -21,11 +21,7 @@
  */
 
 #include "RoutePlannerGlue.hpp"
-#include "Thread/Guard.hpp"
 #include "Terrain/RasterTerrain.hpp"
-#include "Geo/SpeedVector.hpp"
-#include "NMEA/Derived.hpp"
-#include <assert.h>
 
 void
 RoutePlannerGlue::SetTerrain(const RasterTerrain *_terrain)
--- src/Task/RoutePlannerGlue.hpp
+++ src/Task/RoutePlannerGlue.hpp
@@ -19,6 +19,7 @@
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 }
  */
+
 #ifndef ROUTE_PLANNER_GLUE_HPP
 #define ROUTE_PLANNER_GLUE_HPP
 
