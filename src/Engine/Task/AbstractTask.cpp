XCSoar/XCSoar

commit ca0b747007d6165d9e9a3fec4a6606d630704fe3

Elementstat: split time_remaining into two attributes
This distinction is necessary because "remaining_effective" is based
on the planned task distance, while "remaining" includes the distance
to be travelled until reaching the start point.  Calculating the speed
of "remaining_effective" rendered wrong values before the task start
because the wrong time_remaining value was used (TRAC #2876).

src/Engine/Task/AbstractTask.cpp
@@ -286,8 +286,9 @@ void
 AbstractTask::UpdateStatsTimes(const AircraftState &state)
 {
   if (!stats.task_finished) {
-    stats.total.SetTimes(ScanTotalStartTime(), state);
-    stats.current_leg.SetTimes(ScanLegStartTime(),state);
+    stats.current_leg.SetTimes(fixed(0), ScanLegStartTime(), state);
+    stats.total.SetTimes(stats.current_leg.time_remaining_now,
+                         ScanTotalStartTime(), state);
   }
 }
 

src/Computer/ConditionMonitor/ConditionMonitorAATTime.cpp
@@ -41,7 +41,7 @@ ConditionMonitorAATTime::CheckCondition(const NMEAInfo &basic,
       calculated.ordered_task_stats.task_finished)
     return false;
 
-  return calculated.ordered_task_stats.total.time_remaining <
+  return calculated.ordered_task_stats.total.time_remaining_now <
     calculated.common_stats.aat_time_remaining;
 }
 

src/Computer/TaskComputer.cpp
@@ -140,7 +140,7 @@ Predicted(const ContestSettings &settings,
   /* predict that the next task point will be reached, using the
      calculated remaining time and the minimum arrival altitude */
   return TracePoint(current_leg.location_remaining,
-                    unsigned(basic.time + current_leg.time_remaining),
+                    unsigned(basic.time + current_leg.time_remaining_now),
                     current_leg.solution_remaining.min_arrival_altitude,
                     fixed(0), 0);
 }

src/Dialogs/StatusPanels/TaskStatusPanel.cpp
@@ -62,7 +62,7 @@ TaskStatusPanel::Refresh()
   FormatSignedTimeHHMM(Temp, ete_time);
   SetText(ETETime, Temp);
 
-  FormatSignedTimeHHMM(Temp, (int)task_stats.total.time_remaining);
+  FormatSignedTimeHHMM(Temp, (int)task_stats.total.time_remaining_now);
   SetText(RemainingTime, Temp);
 
   if (task_stats.total.planned.IsDefined()) {

src/Engine/Task/Computer/ElementStatComputer.cpp
@@ -43,8 +43,9 @@ ElementStatComputer::Reset(ElementStat &data)
 void 
 ElementStatComputer::CalcSpeeds(ElementStat &data, const fixed time)
 {
-  remaining_effective.CalcSpeed(data.remaining_effective, data.time_remaining);
-  remaining.CalcSpeed(data.remaining, data.time_remaining);
+  remaining_effective.CalcSpeed(data.remaining_effective,
+                                data.time_remaining_start);
+  remaining.CalcSpeed(data.remaining, data.time_remaining_now);
   planned.CalcSpeed(data.planned, data.time_planned);
   travelled.CalcSpeed(data.travelled, data.time_elapsed);
   pirker.CalcSpeed(data.pirker, data.time_elapsed);

src/Engine/Task/Stats/ElementStat.cpp
@@ -32,7 +32,7 @@ ElementStat::Reset()
   next_leg_vector = GeoVector::Invalid();
 
   time_started = fixed(-1);
-  time_elapsed = time_remaining = time_planned = fixed(0);
+  time_elapsed = time_remaining_now = time_remaining_start = time_planned = fixed(0);
   gradient = fixed(0);
 
   remaining_effective.Reset();
@@ -50,7 +50,8 @@ ElementStat::Reset()
 }
 
 void
-ElementStat::SetTimes(const fixed ts, const AircraftState& state)
+ElementStat::SetTimes(const fixed until_start_s, const fixed ts,
+                      const AircraftState& state)
 {
   time_started = ts;
   if (negative(time_started))
@@ -60,9 +61,11 @@ ElementStat::SetTimes(const fixed ts, const AircraftState& state)
     time_elapsed = std::max(state.time - fixed(ts), fixed(0));
 
   if (solution_remaining.IsOk()) {
-    time_remaining = solution_remaining.time_elapsed;
-    time_planned = time_elapsed + time_remaining;
+    time_remaining_now = solution_remaining.time_elapsed;
+    time_remaining_start = std::max(time_remaining_now - until_start_s,
+                                    fixed(0));
+    time_planned = time_elapsed + time_remaining_start;
   } else {
-    time_remaining = time_planned = fixed(0);
+    time_remaining_now = time_remaining_start = time_planned = fixed(0);
   }
 }

src/Engine/Task/Stats/ElementStat.hpp
@@ -64,8 +64,19 @@ struct ElementStat
   fixed time_started;
   /** Time (s) since element was started */
   fixed time_elapsed;
-  /** Time (s) to element completion */
-  fixed time_remaining;
+
+  /**
+   * Time (s) remaining to element completion from now, including the
+   * time to reach the start point (if task was not yet started).
+   */
+  fixed time_remaining_now;
+
+  /**
+   * Time (s) remaining to element completion, counted from the start
+   * of the task.
+   */
+  fixed time_remaining_start;
+
   /** Time (s) of overall element */
   fixed time_planned;
 
@@ -101,10 +112,12 @@ struct ElementStat
   /**
    * Calculate element times
    *
+   * @param until_start_s the estimated time until the task start will
+   * be reached [s]; zero if the task has already started
    * @param ts Start time of this element (s)
    * @param state Aircraft state (to access time)
    */
-  void SetTimes(const fixed ts, const AircraftState& state);
+  void SetTimes(fixed until_start_s, fixed ts, const AircraftState &state);
 
   /**
    * Determine whether the task (or subtask) is able to be finished

src/Engine/Task/Stats/TaskStats.hpp
@@ -77,7 +77,7 @@ public:
   WindowStats last_hour;
 
   fixed GetEstimatedTotalTime() const {
-    return total.time_elapsed + total.time_remaining;
+    return total.time_elapsed + total.time_remaining_start;
   }
 
   /**

commit d5a34528d6d2f348bff62f6847a105caa0c89e8c

AbstractTask: pass current_leg.time_remaining only for ordered start point
Fixes a miscalculation of the total remaining time, regression by
commit ca0b7470.

src/Engine/Task/AbstractTask.cpp
@@ -287,8 +287,17 @@ AbstractTask::UpdateStatsTimes(const AircraftState &state)
 {
   if (!stats.task_finished) {
     stats.current_leg.SetTimes(fixed(0), ScanLegStartTime(), state);
-    stats.total.SetTimes(stats.current_leg.time_remaining_now,
-                         ScanTotalStartTime(), state);
+
+    const fixed until_start_s = GetType() == TaskType::ORDERED &&
+      GetActiveTaskPointIndex() == 0
+      /* flying towards the start point in an ordered task: pass
+         current_leg.time_remaining, which is the estimated time to
+         reach the start point */
+      ? stats.current_leg.time_remaining_now
+      /* already beyond the start point (or no start point) */
+      : fixed(0);
+
+    stats.total.SetTimes(until_start_s, ScanTotalStartTime(), state);
   }
 }
 