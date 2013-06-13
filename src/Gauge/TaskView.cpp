dc836e76448226f4666041a769d1603e784873af
Max Kellermann <max@duempel.org>
2013-06-13 20:09:20 +0200

    Gauge/TaskView: transparent FAI triangle areas on SDL

--- src/Gauge/TaskView.cpp
+++ src/Gauge/TaskView.cpp
@@ -136,8 +136,10 @@ PaintTask(Canvas &canvas, const WindowProjection &projection,
 
   if (fai_sectors && IsFAITriangleApplicable(task)) {
     static constexpr Color fill_color = COLOR_YELLOW;
+#if defined(ENABLE_OPENGL) || defined(ENABLE_SDL)
 #ifdef ENABLE_OPENGL
     const GLBlend blend(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+#endif
 
     canvas.Select(Brush(fill_color.WithAlpha(40)));
     canvas.Select(Pen(1, COLOR_BLACK.WithAlpha(80)));
