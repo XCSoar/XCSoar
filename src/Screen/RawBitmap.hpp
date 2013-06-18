XCSoar/XCSoar

commit 6a95a1d09dfccbe58ca9e5f5c942733be463c91e

Screen/RawBitmap: use standard integers instead of PixelScalar
Less overhead.

src/Screen/RawBitmap.hpp
@@ -11,8 +11,6 @@
 #ifndef XCSOAR_SCREEN_RAW_BITMAP_HPP
 #define XCSOAR_SCREEN_RAW_BITMAP_HPP
 
-#include "Screen/Point.hpp"
-
 #ifdef ENABLE_OPENGL
 #include "Screen/OpenGL/Surface.hpp"
 #include "Screen/OpenGL/Features.hpp"
@@ -20,6 +18,11 @@
 
 #ifdef ENABLE_SDL
 #include <SDL_endian.h>
+#endif
+
+#ifdef USE_GDI
+#include <windef.h>
+#include <wingdi.h>
 #endif
 
 #include <stdint.h>
@@ -106,9 +109,9 @@ class RawBitmap
 #endif
 {
 protected:
-  const UPixelScalar width;
-  const UPixelScalar height;
-  const UPixelScalar corrected_width;
+  const unsigned width;
+  const unsigned height;
+  const unsigned corrected_width;
   BGRColor *buffer;
 
 #ifdef ENABLE_OPENGL
@@ -144,7 +147,7 @@ public:
    * @param nHeight Height of the buffer
    * @param clr Fill color of the buffer
    */
-  RawBitmap(UPixelScalar width, UPixelScalar height);
+  RawBitmap(unsigned width, unsigned height);
 
 #ifdef ENABLE_OPENGL
   virtual
@@ -194,7 +197,7 @@ public:
    * points array directly (using GetPointsArray function).
    * @return Real width of the screen buffer
    */
-  UPixelScalar GetCorrectedWidth() const {
+  unsigned GetCorrectedWidth() const {
     return corrected_width;
   }
 
@@ -202,7 +205,7 @@ public:
    * Returns the screen buffer width
    * @return The screen buffer width
    */
-  UPixelScalar GetWidth() const {
+  unsigned GetWidth() const {
     return width;
   }
 
@@ -210,7 +213,7 @@ public:
    * Returns screen buffer height
    * @return The screen buffer height
    */
-  UPixelScalar GetHeight() const {
+  unsigned GetHeight() const {
     return height;
   }
 
@@ -223,8 +226,8 @@ public:
   GLTexture &BindAndGetTexture() const;
 #endif
 
-  void StretchTo(UPixelScalar width, UPixelScalar height, Canvas &dest_canvas,
-                 UPixelScalar dest_width, UPixelScalar dest_height) const;
+  void StretchTo(unsigned width, unsigned height, Canvas &dest_canvas,
+                 unsigned dest_width, unsigned dest_height) const;
 
 #ifdef ENABLE_OPENGL
 private:

src/Screen/GDI/RawBitmap.cpp
@@ -30,13 +30,13 @@ Copyright_License {
  * Returns minimum width that is greater then the given width and
  * that is acceptable as image width (not all numbers are acceptable)
  */
-static inline UPixelScalar
-CorrectedWidth(UPixelScalar nWidth)
+static inline unsigned
+CorrectedWidth(unsigned nWidth)
 {
   return ((nWidth + 3) / 4) * 4;
 }
 
-RawBitmap::RawBitmap(UPixelScalar nWidth, UPixelScalar nHeight)
+RawBitmap::RawBitmap(unsigned nWidth, unsigned nHeight)
   :width(nWidth), height(nHeight),
    corrected_width(CorrectedWidth(nWidth))
 {
@@ -93,9 +93,9 @@ RawBitmap::~RawBitmap()
 }
 
 void
-RawBitmap::StretchTo(UPixelScalar width, UPixelScalar height,
+RawBitmap::StretchTo(unsigned width, unsigned height,
                      Canvas &dest_canvas,
-                     UPixelScalar dest_width, UPixelScalar dest_height) const
+                     unsigned dest_width, unsigned dest_height) const
 {
 #if defined(_WIN32_WCE) && _WIN32_WCE < 0x0400
   /* StretchDIBits() is bugged on PPC2002, workaround follows */

src/Screen/OpenGL/RawBitmap.cpp
@@ -33,13 +33,13 @@ Copyright_License {
  * Returns minimum width that is greater then the given width and
  * that is acceptable as image width (not all numbers are acceptable)
  */
-static inline UPixelScalar
-CorrectedWidth(UPixelScalar nWidth)
+static inline unsigned
+CorrectedWidth(unsigned nWidth)
 {
   return ((nWidth + 3) / 4) * 4;
 }
 
-RawBitmap::RawBitmap(UPixelScalar nWidth, UPixelScalar nHeight)
+RawBitmap::RawBitmap(unsigned nWidth, unsigned nHeight)
   :width(nWidth), height(nHeight),
    corrected_width(CorrectedWidth(nWidth)),
    texture(new GLTexture(CorrectedWidth(nWidth), nHeight)),
@@ -104,9 +104,9 @@ RawBitmap::BindAndGetTexture() const
 }
 
 void
-RawBitmap::StretchTo(UPixelScalar width, UPixelScalar height,
+RawBitmap::StretchTo(unsigned width, unsigned height,
                      Canvas &dest_canvas,
-                     UPixelScalar dest_width, UPixelScalar dest_height) const
+                     unsigned dest_width, unsigned dest_height) const
 {
   GLTexture &texture = BindAndGetTexture();
 

src/Screen/SDL/RawBitmap.cpp
@@ -30,13 +30,13 @@ Copyright_License {
  * Returns minimum width that is greater then the given width and
  * that is acceptable as image width (not all numbers are acceptable)
  */
-static inline UPixelScalar
-CorrectedWidth(UPixelScalar nWidth)
+static inline unsigned
+CorrectedWidth(unsigned nWidth)
 {
   return ((nWidth + 3) / 4) * 4;
 }
 
-RawBitmap::RawBitmap(UPixelScalar nWidth, UPixelScalar nHeight)
+RawBitmap::RawBitmap(unsigned nWidth, unsigned nHeight)
   :width(nWidth), height(nHeight),
    corrected_width(CorrectedWidth(nWidth))
 {
@@ -74,9 +74,9 @@ RawBitmap::~RawBitmap()
 }
 
 void
-RawBitmap::StretchTo(UPixelScalar width, UPixelScalar height,
+RawBitmap::StretchTo(unsigned width, unsigned height,
                      Canvas &dest_canvas,
-                     UPixelScalar dest_width, UPixelScalar dest_height) const
+                     unsigned dest_width, unsigned dest_height) const
 {
   Canvas src_canvas(surface);
   dest_canvas.Stretch(0, 0, dest_width, dest_height,

commit d19434dc00ec685fdfcd5ca8e3a3cf870d6c67a7

Screen/RawBitmap.hpp: Fix compilation issue with gcc 4.6.2
Regression introduced in commit 6a95a1d09dfccbe58ca9e5f5c942733be463c91e

src/Screen/RawBitmap.hpp
@@ -26,6 +26,7 @@
 #endif
 
 #include <stdint.h>
+#include "Compiler.h"
 
 class Canvas;
 