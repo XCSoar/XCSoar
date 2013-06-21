XCSoar/XCSoar

commit a83c42a5b4942812907fb6af7e7fe9d51367af4c

Makefile: enable glibcxx debugging on DEBUG=y mode

build/flags.mk
@@ -26,6 +26,10 @@ C_FEATURES += -ffunction-sections
 TARGET_LDFLAGS += -Wl,--gc-sections
 endif
 
+ifeq ($(DEBUG),n)
+CXX_FEATURES += -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+endif
+
 ALL_CPPFLAGS = $(TARGET_INCLUDES) $(INCLUDES) $(TARGET_CPPFLAGS) $(CPPFLAGS) $(EXTRA_CPPFLAGS)
 ALL_CXXFLAGS = $(OPTIMIZE) $(FLAGS_PROFILE) $(CXX_FEATURES) $(CXXFLAGS) $(EXTRA_CXXFLAGS)
 ALL_CFLAGS = $(OPTIMIZE) $(FLAGS_PROFILE) $(C_FEATURES) $(CFLAGS) $(EXTRA_CFLAGS)

commit f77657f7be1a4772806e10cf6e944faf9593fdbb

Makefile: fix flipped DEBUG check for glibcxx debugging
Regression from commit a83c42a5.

build/flags.mk
@@ -26,7 +26,7 @@ C_FEATURES += -ffunction-sections
 TARGET_LDFLAGS += -Wl,--gc-sections
 endif
 
-ifeq ($(DEBUG),n)
+ifeq ($(DEBUG),y)
 CXX_FEATURES += -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
 endif
 