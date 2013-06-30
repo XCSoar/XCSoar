cce8ebe848a42ca04ef4d1a554d6580bd98d5cdf
Max Kellermann <max@duempel.org>
2013-06-30 20:54:18 +0200

    Screen/Dither: eliminate offset from err_dist_l1

--- src/Screen/Memory/Dither.cpp
+++ src/Screen/Memory/Dither.cpp
@@ -46,7 +46,7 @@ void Dither::dither_luminosity8_to_uint16(const uint8_t *gcc_restrict src,
     ErrorDistType *gcc_restrict err_dist_l0 =
       error_dist_buffer + ((height & 1) ? width_2 : 0) + 1;
     ErrorDistType *gcc_restrict err_dist_l1 =
-      error_dist_buffer + ((height & 1) ? 0 : width_2) + 1;
+      error_dist_buffer + ((height & 1) ? 0 : width_2);
 
     /* scan the line and convert the Y8 to BW */
     for (unsigned col = width; col > 0; --col) {
@@ -64,8 +64,8 @@ void Dither::dither_luminosity8_to_uint16(const uint8_t *gcc_restrict src,
       bwPix >>= 1;
       *(++err_dist_l0) += bwPix;
       bwPix >>= 1;
-      *(err_dist_l1 - 1) += bwPix;
-      *err_dist_l1++ = bwPix;
+      *err_dist_l1 += bwPix;
+      *++err_dist_l1 = bwPix;
 
     }
 
