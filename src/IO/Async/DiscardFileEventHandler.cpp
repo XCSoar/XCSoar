008ae0607fc53324ada1989f04f8e92e663446a8
Max Kellermann <max@duempel.org>
2013-07-02 19:49:37 +0200

    IO/Async/DiscardFileEventHandler: new helper class

---
+++ src/IO/Async/DiscardFileEventHandler.cpp
@@ -0,0 +1,34 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2013 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#include "DiscardFileEventHandler.hpp"
+
+#include <unistd.h>
+
+bool
+DiscardFileEventHandler::OnFileEvent(int fd, unsigned mask)
+{
+  char buffer[256];
+  ssize_t nbytes = read(fd, buffer, sizeof(buffer));
+  return nbytes > 0;
+}
