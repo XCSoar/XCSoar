XCSoar/XCSoar

commit be70dffb36d457f3e8bcf994c388537ee2637612

TaskDijkstra{Min,Max}: improved support for optimising the start point
Add TaskDijkstra::AddStartEdges(), which adds all points of the first
stage, instead of just adding the first point.

src/Engine/Task/PathSolvers/TaskDijkstra.cpp
@@ -72,6 +72,17 @@ TaskDijkstra::AddEdges(const ScanTaskPoint curNode)
     Link(destination, curNode, CalcDistance(curNode, destination));
 }
 
+void
+TaskDijkstra::AddZeroStartEdges()
+{
+  ScanTaskPoint destination(active_stage, 0);
+  const unsigned dsize = GetStageSize(active_stage);
+
+  for (const ScanTaskPoint end(active_stage, dsize);
+       destination != end; destination.IncrementPointIndex())
+    LinkStart(destination, 0);
+}
+
 void 
 TaskDijkstra::AddStartEdges(const SearchPoint &currentLocation)
 {

src/Engine/Task/PathSolvers/TaskDijkstra.hpp
@@ -98,6 +98,11 @@ protected:
   }
 
   /**
+   * Add a zero-length start edge to each point in the first stage.
+   */
+  void AddZeroStartEdges();
+
+  /**
    * Add a start edge from the given location to each point in the
    * first stage.
    */

src/Engine/Task/PathSolvers/TaskDijkstraMax.cpp
@@ -30,6 +30,6 @@ TaskDijkstraMax::DistanceMax(const OrderedTask &task)
 
   dijkstra.Reserve(256);
   dijkstra.Clear();
-  LinkStart(ScanTaskPoint(0, 0));
+  AddZeroStartEdges();
   return Run();
 }

src/Engine/Task/PathSolvers/TaskDijkstraMin.cpp
@@ -35,7 +35,7 @@ TaskDijkstraMin::DistanceMin(const OrderedTask &task,
   if (currentLocation.IsValid()) {
     AddStartEdges(currentLocation);
   } else {
-    LinkStart(ScanTaskPoint(0, 0));
+    AddZeroStartEdges();
   }
 
   return Run();

commit e188159f1f677c153285d197919fa0c15393b17c

TaskDijkstra: start at stage zero in AddZeroStartEdges()
Fixes a buffer overflow (TRAC #2970), regression by commit be70dffb.

src/Engine/Task/PathSolvers/TaskDijkstra.cpp
@@ -75,10 +75,11 @@ TaskDijkstra::AddEdges(const ScanTaskPoint curNode)
 void
 TaskDijkstra::AddZeroStartEdges()
 {
-  ScanTaskPoint destination(active_stage, 0);
-  const unsigned dsize = GetStageSize(active_stage);
+  const unsigned stage = 0;
+  ScanTaskPoint destination(stage, 0);
+  const unsigned dsize = GetStageSize(stage);
 
-  for (const ScanTaskPoint end(active_stage, dsize);
+  for (const ScanTaskPoint end(stage, dsize);
        destination != end; destination.IncrementPointIndex())
     LinkStart(destination, 0);
 }