c94e282ae708e3822b47ed8a39f0dd6238066b87
Max Kellermann <max@duempel.org>
2013-07-15 11:25:19 +0200

    Makefile: reduce KoboMenu dependencies

--- build/kobo.mk
+++ build/kobo.mk
@@ -2,22 +2,16 @@ KOBO_MENU_SOURCES = \
 	$(SRC)/Formatter/HexColor.cpp \
 	$(SRC)/Hardware/Display.cpp \
 	$(SRC)/Screen/Layout.cpp \
-	$(SRC)/Compatibility/fmode.c \
 	$(SRC)/ResourceLoader.cpp \
-	$(SRC)/NMEA/Checksum.cpp \
 	$(SRC)/Screen/TerminalWindow.cpp \
 	$(SRC)/Look/TerminalLook.cpp \
 	$(SRC)/Look/DialogLook.cpp \
 	$(SRC)/Look/ButtonLook.cpp \
 	$(SRC)/Dialogs/DialogSettings.cpp \
-	$(SRC)/IO/Async/IOLoop.cpp \
-	$(SRC)/IO/Async/DiscardFileEventHandler.cpp \
 	$(TEST_SRC_DIR)/Fonts.cpp \
-	$(TEST_SRC_DIR)/FakeAsset.cpp \
-	$(TEST_SRC_DIR)/FakeBlank.cpp \
 	$(SRC)/Kobo/KoboMenu.cpp
 KOBO_MENU_LDADD = $(FAKE_LIBS)
-KOBO_MENU_DEPENDS = FORM SCREEN EVENT OS THREAD MATH UTIL
+KOBO_MENU_DEPENDS = FORM SCREEN EVENT ASYNC OS THREAD MATH UTIL
 
 $(eval $(call link-program,KoboMenu,KOBO_MENU))
 
