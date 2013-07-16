ae86a6b8370241d3432d17b52b27a88047953a1f
Max Kellermann <max@duempel.org>
2013-07-16 16:12:35 +0200

    Makefile: add rule to generate KoboRoot.tgz

--- build/kobo.mk
+++ build/kobo.mk
@@ -20,3 +20,17 @@ $(eval $(call link-program,KoboMenu,KOBO_MENU))
 ifeq ($(TARGET),UNIX)
 OPTIONAL_OUTPUTS += $(KOBO_MENU_BIN)
 endif
+
+ifeq ($(TARGET_IS_KOBO),y)
+
+# /mnt/onboard/.kobo/KoboRoot.tgz is a file that is picked up by
+# /etc/init.d/rcS, extracted to / on each boot; we can use it to
+# install XCSoar
+$(TARGET_OUTPUT_DIR)/KoboRoot.tgz: $(KOBO_MENU_BIN) $(XCSOAR_BIN)
+	@$(NQ)echo "  TAR     $@"
+	$(Q)rm -rf $(@D)/KoboRoot
+	$(Q)install -m 0755 -d $(@D)/KoboRoot/opt/xcsoar/bin
+	$(Q)install -m 0755 $(KOBO_MENU_BIN) $(XCSOAR_BIN) $(@D)/KoboRoot/opt/xcsoar/bin
+	$(Q)fakeroot tar czfC $@ $(@D)/KoboRoot .
+
+endif
