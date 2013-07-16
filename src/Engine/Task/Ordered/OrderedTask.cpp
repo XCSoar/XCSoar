d9546231a5d01560d5f2894a3484ade2fb90a8dd
Max Kellermann <max@duempel.org>
2013-07-16 23:59:55 +0200

    OrderedTask: reduce number of TaskDijkstraMin calls
    Call TaskDijkstraMin only if the distance to the active task point has
    changed by 5% or more.  With less than that, no relevant change will
    result.  This commit saves a big amount of CPU cycles.

--- src/Engine/Task/Ordered/OrderedTask.cpp
+++ src/Engine/Task/Ordered/OrderedTask.cpp
@@ -183,6 +183,25 @@ OrderedTask::ScanLegStartTime()
 inline fixed
 OrderedTask::ScanDistanceMin(const GeoPoint &location, bool full)
 {
+  if (full && location.IsValid() && last_min_location.IsValid()) {
+    const TaskWaypoint *active = GetActiveTaskPoint();
+    if (active != nullptr) {
+      const GeoPoint &target = active->GetWaypoint().location;
+      const unsigned last_distance =
+        (unsigned)last_min_location.Distance(target);
+      const unsigned cur_distance =
+        (unsigned)location.Distance(target);
+
+      /* skip this call if the distance to the active task point has
+         changed by less than 5%, because we don't expect any relevant
+         changes */
+      if (last_distance >= 2000 && cur_distance >= 2000 &&
+          last_distance * 20 < cur_distance * 21 &&
+          cur_distance * 20 < last_distance * 21)
+        full = false;
+    }
+  }
+
   if (full) {
     if (dijkstra_min == NULL)
       dijkstra_min = new TaskDijkstraMin();
