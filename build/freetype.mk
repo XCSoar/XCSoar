28f6db42aef30c4e5d9397c78bff72457ef377c2
Max Kellermann <max@duempel.org>
2013-07-16 21:02:26 +0200

    Makefile: use pkg-config for Kobo
    Remove more unnecessary special cases.

--- build/freetype.mk
+++ build/freetype.mk
@@ -2,12 +2,7 @@ FREETYPE ?= n
 
 ifeq ($(FREETYPE),y)
 
-ifeq ($(TARGET_IS_KOBO),y)
-FREETYPE_CPPFLAGS = -isystem $(KOBO)/include/freetype2
-FREETYPE_LDLIBS = -lfreetype
-else
 $(eval $(call pkg-config-library,FREETYPE,freetype2))
-endif
 
 FREETYPE_CPPFLAGS += -DUSE_FREETYPE
 
--- build/libpng.mk
+++ build/libpng.mk
@@ -2,10 +2,6 @@ LIBPNG ?= n
 
 ifeq ($(LIBPNG),y)
 
-ifeq ($(TARGET_IS_KOBO),y)
-LIBPNG_LDLIBS = -lpng
-else
 $(eval $(call pkg-config-library,LIBPNG,libpng))
-endif
 
 endif
