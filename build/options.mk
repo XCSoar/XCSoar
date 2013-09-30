82da1f368a580ce6394b1649e29dc723b4f9b6d6
Max Kellermann <max@duempel.org>
2013-09-30 18:33:21 +0200

    release v6.7

--- build/options.mk
+++ build/options.mk
@@ -48,7 +48,7 @@ endif
 # a red Activity icon, to allow simultaneous installation of "stable"
 # and "testing".
 # In the stable branch, this should default to "n".
-TESTING = y
+TESTING = n
 
 ifeq ($(TESTING),y)
   TARGET_CPPFLAGS += -DTESTING
