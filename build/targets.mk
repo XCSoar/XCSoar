484c09d6945b4f54bfa52babdee159c96239d2ea
Max Kellermann <max@duempel.org>
2013-12-03 21:22:44 +0100

    Makefile: make clang the default compiler on Android
    clang/LLVM support is now mature enough in the Android NDK.  clang
    gives us better diagnostics.  One day, we'll enable link-time
    optimisation via LLVM bitcode.

--- build/targets.mk
+++ build/targets.mk
@@ -393,6 +393,9 @@ ifeq ($(TARGET),ANDROID)
 
   ANDROID_GCC_TOOLCHAIN_NAME = $(ANDROID_ABI2)-$(ANDROID_GCC_VERSION)
 
+  # clang is the default compiler on Android
+  CLANG ?= y
+
   ifeq ($(CLANG),y)
     ANDROID_TOOLCHAIN_NAME = llvm-3.4
   else
