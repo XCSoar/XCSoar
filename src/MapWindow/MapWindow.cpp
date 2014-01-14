XCSoar/XCSoar

commit a539688fd3c293c60e4bccc49d3802b072bf194c

DerivedInfo: require IsPlausible() checks before using date_time_local
There was no formal requirement to check the validity of
date_time_local.  Most users checked NMEAInfo::time_available or
NMEAInfo::date_available, but NMEAInfo is not guaranteed to be in sync
with DerivedInfo.

src/MapWindow/MapWindow.cpp
@@ -161,7 +161,7 @@ MapWindow::UpdateWeather()
   // always service weather even if it's not used by the map,
   // because it's potentially used by other calculations
 
-  if (weather == NULL)
+  if (weather == NULL || Calculated().date_time_local.IsTimePlausible())
     return false;
 
   QuietOperationEnvironment operation;

src/Computer/GlideComputer.cpp
@@ -88,16 +88,20 @@ GlideComputer::ProcessGPS(bool force)
 
   const bool last_flying = calculated.flight.flying;
 
-  /* use UTC offset to calculate local time */
-  const int utc_offset_s = settings.utc_offset.AsSeconds();
-  calculated.date_time_local = basic.date_available
-    /* known date: apply UTC offset to BrokenDateTime, which may
-       increment/decrement date */
-    ? basic.date_time_utc + utc_offset_s
-    /* unknown date: apply UTC offset only to BrokenTime, leave the
-       BrokenDate part invalid as it was */
-    : BrokenDateTime(BrokenDate::Invalid(),
-                     ((const BrokenTime &)basic.date_time_utc) + utc_offset_s);
+  if (basic.time_available) {
+    /* use UTC offset to calculate local time */
+    const int utc_offset_s = settings.utc_offset.AsSeconds();
+
+    calculated.date_time_local = basic.date_available
+      /* known date: apply UTC offset to BrokenDateTime, which may
+         increment/decrement date */
+      ? basic.date_time_utc + utc_offset_s
+      /* unknown date: apply UTC offset only to BrokenTime, leave the
+         BrokenDate part invalid as it was */
+      : BrokenDateTime(BrokenDate::Invalid(),
+                       ((const BrokenTime &)basic.date_time_utc) + utc_offset_s);
+  } else
+    calculated.date_time_local = BrokenDateTime::Invalid();
 
   calculated.Expire(basic.clock);
 

src/InfoBoxes/Content/Task.cpp
@@ -285,12 +285,14 @@ UpdateInfoBoxNextETA(InfoBoxData &data)
   // use proper non-terminal next task stats
 
   const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
-  if (!task_stats.task_valid || !task_stats.current_leg.IsAchievable()) {
+  const BrokenTime &now_local = CommonInterface::Calculated().date_time_local;
+
+  if (!task_stats.task_valid || !task_stats.current_leg.IsAchievable() ||
+      !now_local.IsPlausible()) {
     data.SetInvalid();
     return;
   }
 
-  const BrokenTime &now_local = CommonInterface::Calculated().date_time_local;
   const BrokenTime t = now_local +
     unsigned(task_stats.current_leg.solution_remaining.time_elapsed);
 
@@ -437,12 +439,14 @@ void
 UpdateInfoBoxFinalETA(InfoBoxData &data)
 {
   const TaskStats &task_stats = CommonInterface::Calculated().task_stats;
-  if (!task_stats.task_valid || !task_stats.total.IsAchievable()) {
+  const BrokenTime &now_local = CommonInterface::Calculated().date_time_local;
+
+  if (!task_stats.task_valid || !task_stats.total.IsAchievable() ||
+      !now_local.IsPlausible()) {
     data.SetInvalid();
     return;
   }
 
-  const BrokenTime &now_local = CommonInterface::Calculated().date_time_local;
   const BrokenTime t = now_local +
     unsigned(task_stats.total.solution_remaining.time_elapsed);
 

src/NMEA/Derived.cpp
@@ -54,6 +54,8 @@ AirspaceWarningsInfo::Clear()
 void
 DerivedInfo::Reset()
 {
+  date_time_local = BrokenDateTime::Invalid();
+
   VarioInfo::Clear();
   ClimbInfo::Clear();
   CirclingInfo::Clear();

src/NMEA/Derived.hpp
@@ -138,7 +138,12 @@ struct DerivedInfo:
   public TerrainInfo,
   public TeamInfo
 {
-  /** GPS date and time (local) */
+  /**
+   * GPS date and time (local).
+   *
+   * Check IsPlausible(), IsDatePlausible() or IsTimePlausible()
+   * before using this attribute.
+   */
   BrokenDateTime date_time_local;
 
   /** Speed to fly block/dolphin (m/s) */

commit 4a963ed8c4a61067ad957b46be9c645c92a605d1

MapWindow: fix inverted BrokenDateTime::IsPlausible() check
Fixes RASP display (regression by commit a539688f).

src/MapWindow/MapWindow.cpp
@@ -163,7 +163,7 @@ MapWindow::UpdateWeather()
   // always service weather even if it's not used by the map,
   // because it's potentially used by other calculations
 
-  if (weather == NULL || Calculated().date_time_local.IsTimePlausible())
+  if (weather == NULL || !Calculated().date_time_local.IsTimePlausible())
     return false;
 
   QuietOperationEnvironment operation;