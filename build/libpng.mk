d647ae2d660a867fe128b8b546de18ec9097c310
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-02-08 12:42:20 +0100

    libpng: Fixed build errors
    /usr/include/libpng12/png.h:2659:31: error: invalid suffix on literal; C++11 requires a space between literal and identifier [-Werror=literal-suffix]
            fprintf(PNG_DEBUG_FILE,"%s"m PNG_STRING_NEWLINE,(num_tabs==1 ? "\t" : \
                                   ^
    
    This seems to happen with newer versions of gcc. For example gcc 4.8.1-10
    which is shipped as default with Ubuntu 13.10.

--- build/libpng.mk
+++ build/libpng.mk
@@ -7,4 +7,13 @@ $(eval $(call pkg-config-library,LIBPNG,libpng))
 LIBPNG_LDADD += $(ZLIB_LDADD)
 LIBPNG_LDLIBS += $(ZLIB_LDLIBS)
 
+ifneq ($(CLANG),y)
+# this option disables a C++11 warning/error in libpng due to a missing space
+# in a debug macro. unfortunately a GCC bug is producing the warning even
+# though the code should be disabled by the preprocessor.
+# (see http://gcc.gnu.org/bugzilla//show_bug.cgi?id=58155)
+
+LIBPNG_CPPFLAGS += -Wno-error=literal-suffix
+endif
+
 endif
