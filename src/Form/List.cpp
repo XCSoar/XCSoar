XCSoar/XCSoar

commit f6728bedf751008ca3609d75f550b511acd80b8e

Form/List: use GLCanvasScissor instead of GLScissor
Let GLCanvasScissor figure out the projection.

src/Form/List.cpp
@@ -30,7 +30,7 @@ Copyright_License {
 #include "Asset.hpp"
 
 #ifdef ENABLE_OPENGL
-#include "Screen/OpenGL/Scope.hpp"
+#include "Screen/OpenGL/Scissor.hpp"
 #include "Screen/OpenGL/Globals.hpp"
 #elif defined(USE_GDI)
 #include "Screen/WindowCanvas.hpp"
@@ -149,9 +149,9 @@ ListControl::DrawItems(Canvas &canvas, unsigned start, unsigned end) const
 
 #ifdef ENABLE_OPENGL
   /* enable clipping */
-  GLScissor scissor(OpenGL::translate.x,
-                    OpenGL::screen_size.y - OpenGL::translate.y - canvas.GetHeight(),
-                    scroll_bar.GetLeft(GetSize()), canvas.GetHeight());
+  const PixelRect scissor_rc(0, 0, canvas.GetHeight(),
+                             scroll_bar.GetLeft(GetSize()));
+  GLCanvasScissor scissor(scissor_rc);
 #endif
 
   unsigned last_item = std::min(length, end);

commit 53465157964abe89470ef959389d5aad185c4b84

Form/List: fix swapped right/bottom in scissor area
Fixes rendering bug in the list control (regression by commit
f6728bed).

src/Form/List.cpp
@@ -149,8 +149,8 @@ ListControl::DrawItems(Canvas &canvas, unsigned start, unsigned end) const
 
 #ifdef ENABLE_OPENGL
   /* enable clipping */
-  const PixelRect scissor_rc(0, 0, canvas.GetHeight(),
-                             scroll_bar.GetLeft(GetSize()));
+  const PixelRect scissor_rc(0, 0, scroll_bar.GetLeft(GetSize()),
+                             canvas.GetHeight());
   GLCanvasScissor scissor(scissor_rc);
 #endif
 