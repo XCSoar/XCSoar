XCSoar/XCSoar

commit 8cfe3228304c05aa443f27f133b3b893528a068e

OpenGL/Shaders: add new shader "alpha"
Useful for drawing colored text and StretchMono().

src/Screen/OpenGL/Canvas.cpp
@@ -629,11 +629,9 @@ Canvas::CalcTextSize(const TCHAR *text) const
 static void
 PrepareColoredAlphaTexture(Color color)
 {
-  color.Set();
-
 #ifdef HAVE_GLES2
-  // TODO: implement using shader
-  OpenGL::texture_shader->Use();
+  OpenGL::alpha_shader->Use();
+  color.Uniform(OpenGL::alpha_color);
 #else
   if (color == COLOR_BLACK) {
     /* GL_ALPHA textures have black RGB - this is easy */
@@ -642,6 +640,8 @@ PrepareColoredAlphaTexture(Color color)
   } else {
     /* use GL_COMBINE to replace the texture color (black) with the
        specified one */
+
+    color.Set();
 
     OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
 
@@ -867,8 +867,8 @@ Canvas::StretchMono(int dest_x, int dest_y,
      again */
 
 #ifdef HAVE_GLES2
-  // TODO: implement
-  OpenGL::texture_shader->Use();
+  OpenGL::alpha_shader->Use();
+  fg_color.Uniform(OpenGL::alpha_color);
 #else
   OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
 

src/Screen/OpenGL/Shaders.cpp
@@ -35,6 +35,9 @@ namespace OpenGL {
 
   GLProgram *texture_shader;
   GLint texture_projection, texture_texture;
+
+  GLProgram *alpha_shader;
+  GLint alpha_projection, alpha_texture, alpha_color;
 }
 
 static constexpr char solid_vertex_shader[] =
@@ -70,6 +73,16 @@ static constexpr char texture_fragment_shader[] =
   "varying vec2 texcoordvar;"
   "void main() {"
   "  gl_FragColor = texture2D(texture, texcoordvar);"
+  "}";
+
+static const char *const alpha_vertex_shader = texture_vertex_shader;
+static constexpr char alpha_fragment_shader[] =
+  "precision mediump float;"
+  "uniform vec4 color;"
+  "uniform sampler2D texture;"
+  "varying vec2 texcoordvar;"
+  "void main() {"
+  "  gl_FragColor = vec4(color.rgb, texture2D(texture, texcoordvar).a);"
   "}";
 
 static void
@@ -147,6 +160,19 @@ OpenGL::InitShaders()
   texture_shader->Use();
   glUniform1i(texture_texture, 0);
 
+  alpha_shader = CompileProgram(alpha_vertex_shader, alpha_fragment_shader);
+  alpha_shader->BindAttribLocation(Attribute::TRANSLATE, "translate");
+  alpha_shader->BindAttribLocation(Attribute::POSITION, "position");
+  alpha_shader->BindAttribLocation(Attribute::TEXCOORD, "texcoord");
+  LinkProgram(*alpha_shader);
+
+  alpha_projection = alpha_shader->GetUniformLocation("projection");
+  alpha_texture = alpha_shader->GetUniformLocation("texture");
+  alpha_color = alpha_shader->GetUniformLocation("color");
+
+  alpha_shader->Use();
+  glUniform1i(alpha_texture, 0);
+
   glVertexAttrib4f(Attribute::TRANSLATE, 0, 0, 0, 0);
 }
 
@@ -160,6 +186,10 @@ OpenGL::DeinitShaders()
 void
 OpenGL::UpdateShaderProjectionMatrix()
 {
+  alpha_shader->Use();
+  glUniformMatrix4fv(alpha_projection, 1, GL_FALSE,
+                     glm::value_ptr(projection_matrix));
+
   texture_shader->Use();
   glUniformMatrix4fv(texture_projection, 1, GL_FALSE,
                      glm::value_ptr(projection_matrix));

src/Screen/OpenGL/Shaders.hpp
@@ -47,6 +47,13 @@ namespace OpenGL {
   extern GLProgram *texture_shader;
   extern GLint texture_projection, texture_texture;
 
+  /**
+   * A shader that copies the texture's alpha channel, but replaces
+   * the color.
+   */
+  extern GLProgram *alpha_shader;
+  extern GLint alpha_projection, alpha_texture, alpha_color;
+
   void InitShaders();
   void DeinitShaders();
 

commit 89facd07a2b582f415f0dcbe0ea6f812cf30da1f

OpenGL/Canvas: fix black text color
Regression by commit 8cfe3228

src/Screen/OpenGL/Canvas.cpp
@@ -633,6 +633,8 @@ PrepareColoredAlphaTexture(Color color)
   OpenGL::alpha_shader->Use();
   color.Uniform(OpenGL::alpha_color);
 #else
+  color.Set();
+
   if (color == COLOR_BLACK) {
     /* GL_ALPHA textures have black RGB - this is easy */
 
@@ -640,9 +642,6 @@ PrepareColoredAlphaTexture(Color color)
   } else {
     /* use GL_COMBINE to replace the texture color (black) with the
        specified one */
-
-    color.Set();
-
     OpenGL::glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
 
     /* replace the texture color with the selected text color */