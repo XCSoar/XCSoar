e6f96bcd0ede5591fab30e4935b2efcc9fef10fb
Felix Hädicke <felixhaedicke@web.de>
2014-03-27 23:53:44 +0100

    Add HOST_IS_X86_32 and HOST_IS_X86_64 variables

--- build/detect.mk
+++ build/detect.mk
@@ -10,6 +10,9 @@ HOST_IS_MINGW := $(call string_contains,$(UNAME_S),MINGW)
 HOST_IS_WIN32 := $(call bool_or,$(HOST_IS_CYGWIN),$(HOST_IS_MINGW))
 HOST_IS_UNIX := $(call bool_not,$(HOST_IS_WIN32))
 
+HOST_IS_X86_32 := $(call bool_or,$(call string_contains,$(UNAME_M),i386),$(call string_contains,$(UNAME_M),i686))
+HOST_IS_X86_64 := $(call bool_or,$(call string_contains,$(UNAME_M),x86_64),$(call string_contains,$(UNAME_M),amd64))
+
 HOST_IS_ARM := $(call string_contains,$(UNAME_M),armv)
 HOST_IS_ARMV6 := $(call string_equals,$(UNAME_M),armv6l)
 HOST_IS_ARMV7 := $(call string_equals,$(UNAME_M),armv7l)
