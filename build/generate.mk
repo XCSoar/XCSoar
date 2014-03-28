XCSoar/XCSoar

commit b094ce14852d3dc120ea1612d7de127a5fb78948

Use cc -E instead of cpp for generating .rc files
The $(HOSTCPP) call always failed when compiling on Mac OS X using the
Clang cpp which is shipped with Xcode 5. Using cc -E instead works, at
least if the source is read from stdin.

build/generate.mk
@@ -63,7 +63,7 @@ ifeq ($(HAVE_WIN32),n)
 
 $(TARGET_OUTPUT_DIR)/XCSoar.rc: Data/XCSoar.rc $(OUT)/include/resource.h | $(TARGET_OUTPUT_DIR)/dirstamp
 	@$(NQ)echo "  CPP     $@"
-	$(Q)$(HOSTCPP) -o $@ $< -I$(OUT)/include $(TARGET_CPPFLAGS) $(OPENGL_CPPFLAGS)
+	$(Q)cat $< | $(HOSTCC) -E -o $@ -I$(OUT)/include $(TARGET_CPPFLAGS) $(OPENGL_CPPFLAGS) -
 
 $(TARGET_OUTPUT_DIR)/include/resource_data.h: $(TARGET_OUTPUT_DIR)/XCSoar.rc \
 	$(RESOURCE_FILES) \
@@ -74,7 +74,7 @@ $(TARGET_OUTPUT_DIR)/include/resource_data.h: $(TARGET_OUTPUT_DIR)/XCSoar.rc \
 
 $(TARGET_OUTPUT_DIR)/XCSoar-drawable.rc: Data/XCSoar.rc $(OUT)/include/resource.h | $(TARGET_OUTPUT_DIR)/dirstamp
 	@$(NQ)echo "  CPP     $@"
-	$(Q)$(HOSTCPP) -o $@ $< -I$(OUT)/include $(TARGET_CPPFLAGS) -DANDROID_DRAWABLE
+	$(Q)cat $< | $(HOSTCC) -o $@ $< -I$(OUT)/include $(TARGET_CPPFLAGS) -DANDROID_DRAWABLE -
 
 $(TARGET_OUTPUT_DIR)/include/android_drawable.h: $(TARGET_OUTPUT_DIR)/XCSoar-drawable.rc \
 	$(RESOURCE_FILES) \

commit 2b1d58dc24f587ae96f5d0c4108a4051048c2d1b

build/generate.mk: Add "-E" option to fix Android build
Fixes regression introduced in commit b094ce14852d3dc120ea1612d7de127a5fb7

build/generate.mk
@@ -74,7 +74,7 @@ $(TARGET_OUTPUT_DIR)/include/resource_data.h: $(TARGET_OUTPUT_DIR)/XCSoar.rc \
 
 $(TARGET_OUTPUT_DIR)/XCSoar-drawable.rc: Data/XCSoar.rc $(OUT)/include/resource.h | $(TARGET_OUTPUT_DIR)/dirstamp
 	@$(NQ)echo "  CPP     $@"
-	$(Q)cat $< | $(HOSTCC) -o $@ $< -I$(OUT)/include $(TARGET_CPPFLAGS) -DANDROID_DRAWABLE -
+	$(Q)cat $< | $(HOSTCC) -E -o $@ $< -I$(OUT)/include $(TARGET_CPPFLAGS) -DANDROID_DRAWABLE -
 
 $(TARGET_OUTPUT_DIR)/include/android_drawable.h: $(TARGET_OUTPUT_DIR)/XCSoar-drawable.rc \
 	$(RESOURCE_FILES) \