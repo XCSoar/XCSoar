6ffab00f6ef563d2d6a4e3f2b88ece366ca6ed86
Max Kellermann <max@duempel.org>
2014-04-10 23:54:45 +0200

    AirspaceActivity: use bool instead of integer

--- src/Engine/Airspace/AirspaceActivity.hpp
+++ src/Engine/Airspace/AirspaceActivity.hpp
@@ -26,13 +26,13 @@
 class AirspaceActivity {
   struct Days
   {
-    unsigned sunday:1;
-    unsigned monday:1;
-    unsigned tuesday:1;
-    unsigned wednesday:1;
-    unsigned thursday:1;
-    unsigned friday:1;
-    unsigned saturday:1;
+    bool sunday:1;
+    bool monday:1;
+    bool tuesday:1;
+    bool wednesday:1;
+    bool thursday:1;
+    bool friday:1;
+    bool saturday:1;
   };
 
   union
@@ -55,25 +55,25 @@ public:
     mask.value = 0;
     switch(day_of_week) {
     case 0:
-      mask.days.sunday = 1;
+      mask.days.sunday = true;
       break;
     case 1:
-      mask.days.monday = 1;
+      mask.days.monday = true;
       break;
     case 2:
-      mask.days.tuesday = 1;
+      mask.days.tuesday = true;
       break;
     case 3:
-      mask.days.wednesday = 1;
+      mask.days.wednesday = true;
       break;
     case 4:
-      mask.days.thursday = 1;
+      mask.days.thursday = true;
       break;
     case 5:
-      mask.days.friday = 1;
+      mask.days.friday = true;
       break;
     case 6:
-      mask.days.saturday = 1;
+      mask.days.saturday = true;
       break;
     default:
       break;
@@ -86,17 +86,17 @@ public:
 
   void SetWeekdays() {
     mask.value = 0;
-    mask.days.monday = 1;
-    mask.days.tuesday = 1;
-    mask.days.wednesday = 1;
-    mask.days.thursday = 1;
-    mask.days.friday = 1;
+    mask.days.monday = true;
+    mask.days.tuesday = true;
+    mask.days.wednesday = true;
+    mask.days.thursday = true;
+    mask.days.friday = true;
   }
 
   void SetWeekend() {
     mask.value = 0;
-    mask.days.saturday = 1;
-    mask.days.sunday = 1;
+    mask.days.saturday = true;
+    mask.days.sunday = true;
   }
 
   bool Matches(AirspaceActivity _mask) const {
