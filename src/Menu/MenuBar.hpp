7f7e9ab0ce721361434e7cc949322f3d4002cede
Max Kellermann <max@duempel.org>
2014-04-10 21:57:00 +0200

    MenuBar: use constexpr instead of anonymous enum

--- src/Menu/MenuBar.hpp
+++ src/Menu/MenuBar.hpp
@@ -35,9 +35,7 @@ class ContainerWindow;
  */
 class MenuBar {
 public:
-  enum {
-    MAX_BUTTONS = 32,
-  };
+  static constexpr unsigned MAX_BUTTONS = 32;
 
 protected:
   class Button : public ButtonWindow {
