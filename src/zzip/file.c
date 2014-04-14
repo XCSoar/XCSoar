8de046e36c4b63f9c9730ddcfd1762192ff58cc7
Max Kellermann <max@duempel.org>
2014-04-14 17:44:44 +0200

    zzip/file: remove pointless tells() call

--- src/zzip/file.c
+++ src/zzip/file.c
@@ -1126,7 +1126,6 @@ zzip_seek(ZZIP_FILE * fp, zzip_off_t offset, int whence)
 
     if (fp->method == 0)
     {                           /* unstore, just lseek relatively */
-        ofs = fp->io->fd.tells(dir->fd);
         ofs = fp->io->fd.seeks(dir->fd, read_size, SEEK_CUR);
         if (ofs > 0)
         {                       /* readjust from beginning of file */
