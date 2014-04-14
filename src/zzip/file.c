cfa68b9c4b3201a5163e4a1e0e4bb37c390f4d03
Max Kellermann <max@duempel.org>
2014-04-14 17:37:22 +0200

    zzip: disable dead "err" assignments

--- src/zzip/file.c
+++ src/zzip/file.c
@@ -236,7 +236,7 @@ zzip_file_open(ZZIP_DIR * dir, zzip_char_t * name, int o_mode)
             case 8:            /* inflate */
                 break;
             default:
-                { err = ZZIP_UNSUPP_COMPR; goto error; }
+                { /*err = ZZIP_UNSUPP_COMPR; */ goto error; }
             }
 
             if (dir->cache.locked == NULL)
@@ -250,7 +250,7 @@ zzip_file_open(ZZIP_DIR * dir, zzip_char_t * name, int o_mode)
             } else
             {
                 if (! (fp = (ZZIP_FILE *) calloc(1, sizeof(*fp))))
-                    { err =  ZZIP_OUTOFMEM; goto error; }
+                    { /* err =  ZZIP_OUTOFMEM; */ goto error; }
             }
 
             fp->dir = dir;
@@ -264,7 +264,7 @@ zzip_file_open(ZZIP_DIR * dir, zzip_char_t * name, int o_mode)
             } else
             {
                 if (! (fp->buf32k = (char *) malloc(ZZIP_32K)))
-                    { err = ZZIP_OUTOFMEM; goto error; }
+                    { /* err = ZZIP_OUTOFMEM; */ goto error; }
             }
 
             if (dir->cache.locked == &self)
@@ -276,13 +276,13 @@ zzip_file_open(ZZIP_DIR * dir, zzip_char_t * name, int o_mode)
              */
 
             if (zzip_file_saveoffset(dir->currentfp) < 0)
-                { err = ZZIP_DIR_SEEK; goto error; }
+                { /* err = ZZIP_DIR_SEEK; */ goto error; }
 
             fp->offset = hdr->d_off;
             dir->currentfp = fp;
 
             if (dir->io->fd.seeks(dir->fd, hdr->d_off, SEEK_SET) < 0)
-                { err = ZZIP_DIR_SEEK; goto error; }
+                { /* err = ZZIP_DIR_SEEK; */ goto error; }
 
             {
                 /* skip local header - should test tons of other info,
@@ -292,14 +292,14 @@ zzip_file_open(ZZIP_DIR * dir, zzip_char_t * name, int o_mode)
 
                 dataoff = dir->io->fd.read(dir->fd, (void *) p, sizeof(*p));
                 if (dataoff < (zzip_ssize_t) sizeof(*p))
-                    { err = ZZIP_DIR_READ;  goto error; }
+                    { /* err = ZZIP_DIR_READ; */ goto error; }
                 if (! zzip_file_header_check_magic(p))   /* PK\3\4 */
-                    { err = ZZIP_CORRUPTED; goto error; }
+                    { /* err = ZZIP_CORRUPTED; */ goto error; }
 
                 dataoff = zzip_file_header_sizeof_tail(p);
 
                 if (dir->io->fd.seeks(dir->fd, dataoff, SEEK_CUR) < 0)
-                    { err = ZZIP_DIR_SEEK; goto error; }
+                    { /* err = ZZIP_DIR_SEEK; */ goto error; }
 
                 fp->dataoffset = dir->io->fd.tells(dir->fd);
                 fp->usize = hdr->d_usize;
