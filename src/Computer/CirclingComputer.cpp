XCSoar/XCSoar

commit 55807f789bc8d73f9ce36ad21363f8a78a483020

Computer/CirclingComputer: Improve circling detection when using some external NMEA devices
Work around trac issues #3360 and #3372.

For position devices providing several position sources (in this case
GPS and Glonass) and sending the data via the same physical port a proper
solution would need to separate the individual streams to "virtual"
ports. This would allow proper prioritization fallback while also working
in the case that both streams actually send differing data (the device
where the problem occurred actually simply seems to mirror identical
data to different GPS and Glonass NMEA sentences).

src/Computer/CirclingComputer.cpp
@@ -95,10 +95,10 @@ CirclingComputer::TurnRate(CirclingInfo &circling_info,
     smoothed = LowPassFilter(circling_info.turn_rate_heading_smoothed.Native(),
                              turn_rate.Native(), fixed(0.3));
     circling_info.turn_rate_heading_smoothed = Angle::Native(smoothed);
-  }
 
-  last_track = basic.track;
-  last_heading = basic.attitude.heading;
+    last_track = basic.track;
+    last_heading = basic.attitude.heading;
+  }
 }
 
 void

commit fdacd0569727b29e892d9c1e1f3ee4e7d76a6844

Computer/CirclingComputer: Fix initialization of turnrate calculation
Fixes regression introduced in commit 55807f789bc8d73f9ce36ad21363f8a78a483020

==6525== Conditional jump or move depends on uninitialised value(s)
==6525==    at 0x433508: CirclingComputer::Turning(CirclingInfo&, MoreData const&, FlyingState const&, CirclingSettings const&) (CirclingComputer.cpp:143)
==6525==    by 0x43B0CD: GlideComputerAirData::Turning(MoreData const&, DerivedInfo&, ComputerSettings const&) (GlideComputerAirData.cpp:296)
==6525==    by 0x43A096: GlideComputerAirData::ProcessVertical(MoreData const&, DerivedInfo&, ComputerSettings const&) (GlideComputerAirData.cpp:96)
==6525==    by 0x437C2E: GlideComputer::ProcessGPS(bool) (GlideComputer.cpp:132)
==6525==    by 0x4B7366: CalculationThread::Tick() (CalculationThread.cpp:97)
==6525==    by 0x670953: WorkerThread::Run() (WorkerThread.cpp:66)
==6525==    by 0x670244: Thread::ThreadProc(void*) (Thread.cpp:145)
==6525==    by 0x4E3A0DA: start_thread (in /lib64/libpthread-2.18.so)
==6525==    by 0x6D2158C: clone (in /lib64/libc-2.18.so)
==6525==  Uninitialised value was created by a heap allocation
==6525==    at 0x4C27D49: operator new(unsigned long) (in /usr/lib64/valgrind/vgpreload_memcheck-amd64-linux.so)
==6525==    by 0x4E7513: Startup() (Startup.cpp:321)
==6525==    by 0x5541E1: main (XCSoar.cpp:142)

src/Computer/CirclingComputer.cpp
@@ -59,6 +59,8 @@ CirclingComputer::TurnRate(CirclingInfo &circling_info,
     circling_info.turn_rate_heading = Angle::Zero();
     circling_info.turn_rate_smoothed = Angle::Zero();
     circling_info.turn_rate_heading_smoothed = Angle::Zero();
+    last_track = basic.track;
+    last_heading = basic.attitude.heading;
     return;
   }
 
@@ -69,6 +71,8 @@ CirclingComputer::TurnRate(CirclingInfo &circling_info,
     circling_info.turn_rate_heading = Angle::Zero();
     circling_info.turn_rate_smoothed = Angle::Zero();
     circling_info.turn_rate_heading_smoothed = Angle::Zero();
+    last_track = basic.track;
+    last_heading = basic.attitude.heading;
     return;
   }
 