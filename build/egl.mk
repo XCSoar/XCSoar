063cb87080e9db1e40e59435b6c0f1d4f34e69a1
Felix Hädicke <felixhaedicke@web.de>
2014-10-02 21:08:38 +0200

    Add support for Mesa KMS
    This makes it possible to use KMS based EGL contexts for Mesa drivers.
    The libdrm and libgbm libraries are being used.

--- build/egl.mk
+++ build/egl.mk
@@ -4,6 +4,9 @@ EGL ?= y
 else ifeq ($(TARGET)$(TARGET_HAS_MALI),UNIXy)
 # auto-enable EGL on the Cubieboard.
 EGL ?= y
+else ifeq ($(ENABLE_MESA_KMS),y)
+# if Mesa KMS is explicitly enabled, we also need to enable EGL
+EGL ?= y
 else
 EGL ?= n
 endif
@@ -27,6 +30,13 @@ EGL_CPPFLAGS += -isystem $(PI)/opt/vc/include/interface/vmcs_host/linux
 EGL_LDLIBS += -L$(PI)/opt/vc/lib -lvchostif -lvchiq_arm -lvcos -lbcm_host
 else ifeq ($(TARGET_HAS_MALI),y)
 EGL_CPPFLAGS += -DHAVE_MALI
+else ifeq ($(ENABLE_MESA_KMS),y)
+$(eval $(call pkg-config-library,DRM,libdrm))
+$(eval $(call pkg-config-library,GBM,gbm))
+DRM_CPPFLAGS := $(patsubst -I%,-isystem %,$(DRM_CPPFLAGS))
+GBM_CPPFLAGS := $(patsubst -I%,-isystem %,$(GBM_CPPFLAGS))
+EGL_CPPFLAGS += -DMESA_KMS $(DRM_CPPFLAGS) $(GBM_CPPFLAGS)
+EGL_LDLIBS += $(DRM_LDLIBS) $(GBM_LDLIBS)
 else
 EGL_CPPFLAGS += -DUSE_X11
 EGL_LDLIBS += -lX11
