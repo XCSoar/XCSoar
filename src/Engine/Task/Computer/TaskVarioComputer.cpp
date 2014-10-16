d9d21d6e467666709857b505abda5f5a0f66cf9d
Max Kellermann <max@duempel.org>
2014-10-16 20:29:59 +0200

    TaskVarioComputer: use TaskVario::Reset()

--- src/Engine/Task/Computer/TaskVarioComputer.cpp
+++ src/Engine/Task/Computer/TaskVarioComputer.cpp
@@ -42,5 +42,5 @@ TaskVarioComputer::reset(TaskVario &data, const GlideResult& solution)
 {
   v_lpf.Reset(fixed(0));
   df.Reset(solution.altitude_difference, fixed(0));
-  data.value = fixed(0);
+  data.Reset();
 }
