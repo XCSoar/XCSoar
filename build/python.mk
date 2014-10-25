fa39781049ffdb9ab9dac30e0c1ba1cc269070f4
Tobias Bieniek <tobias.bieniek@qsc.de>
2014-10-25 12:29:00 +0200

    python.mk: Use link-shared-library instead of link-python-library

--- build/python.mk
+++ build/python.mk
@@ -1,68 +1,5 @@
 name-to-so = $(patsubst %,$(TARGET_BIN_DIR)/%.so,$(1))
 
-# Generates a shared library linking rule for python.
-#
-# Example: $(eval $(call link-shared-library,Foo,FOO))
-#
-# Arguments: NAME, PREFIX
-#
-# NAME is the name of the library binary, without the path, without
-# the prefix (lib) and without the suffix (.exe).
-#
-# PREFIX is a prefix for variables that will hold detailed information
-# about what is linked, and now.  These must be set before this
-# generator function is called.  The following variables will be used:
-#
-#  _SOURCES: a list of source files
-#
-#  _CPPFLAGS: preprocessor flags for the compiler
-#
-#  _LDADD: a list of static libraries that will be linked into the binary
-#
-#  _LDFLAGS: linker flags
-#
-#  _DEPENDS: a list of library names this executable depends on; it
-#  will use its CPPFLAGS, LDADD and LDFLAGS
-#
-#  _STRIP: if "y", then the library will be stripped
-#
-define link-python-library
-
-$(2)_BIN = $$(TARGET_BIN_DIR)/$(1).so
-
-ifeq ($$($(2)_STRIP),y)
-$(2)_NOSTRIP = $$(TARGET_BIN_DIR)/$(1)-ns.so
-else
-$(2)_NOSTRIP = $$($(2)_BIN)
-endif
-
-$(2)_LDADD += $(patsubst %,$$(%_LDADD),$($(2)_DEPENDS))
-$(2)_LDLIBS += $(patsubst %,$$(%_LDLIBS),$($(2)_DEPENDS))
-
-# Compile
-$(2)_OBJS = $$(call SRC_TO_OBJ,$$($(2)_SOURCES))
-$$($(2)_OBJS): CPPFLAGS += $$($(2)_CPPFLAGS)
-$$($(2)_OBJS): CPPFLAGS += $(patsubst %,$$(%_CPPFLAGS),$($(2)_DEPENDS))
-
-# Link the unstripped binary
-$$($(2)_NOSTRIP): LDFLAGS += -Wl,-shared,-Bsymbolic
-ifeq ($$(TARGET),ANDROID)
-$$($(2)_NOSTRIP): LDFLAGS += -nostdlib
-endif
-
-$$($(2)_NOSTRIP): $$($(2)_OBJS) $$($(2)_LDADD) $$(TARGET_LDADD) | $$(TARGET_BIN_DIR)/dirstamp
-	@$$(NQ)echo "  LINK    $$@"
-	$$(Q)$$(LINK) $$(ld-flags) -o $$@ $$^ $$(ld-libs) $$($(2)_LDLIBS)
-
-# Strip the binary (optional)
-ifeq ($$($(2)_STRIP),y)
-$$($(2)_BIN): $$($(2)_NOSTRIP)
-	@$$(NQ)echo "  STRIP   $$@"
-	$$(Q)$$(STRIP) $$< -o $$@
-endif
-
-endef
-
 python: $(call name-to-so,xcsoar)
 
 PYTHON_SOURCES = \
@@ -122,4 +59,5 @@ PYTHON_DEPENDS = CONTEST WAYPOINT UTIL ZZIP GEO MATH TIME
 PYTHON_CPPFLAGS = -I/usr/include/python2.7 \
 	-I$(TEST_SRC_DIR) -Wno-write-strings
 PYTHON_FILTER_FLAGS = -Wwrite-strings
-$(eval $(call link-python-library,xcsoar,PYTHON))
+PYTHON_NO_LIB_PREFIX = y
+$(eval $(call link-shared-library,xcsoar,PYTHON))
