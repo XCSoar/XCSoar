7aada15dd2df22e6d2586c0348cb8bbd81b80930
Max Kellermann <max@duempel.org>
2015-01-10 15:10:22 +0100

    Copyright year 2015

--- src/Computer/ConditionMonitor/ConditionMonitorWind.hpp
+++ src/Computer/ConditionMonitor/ConditionMonitorWind.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2014 The XCSoar Project
+  Copyright (C) 2000-2015 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
--- tools/FixHeaders.pl
+++ tools/FixHeaders.pl
@@ -5,7 +5,7 @@ use strict;
 my $header = q{
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2014 The XCSoar Project
+  Copyright (C) 2000-2015 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
