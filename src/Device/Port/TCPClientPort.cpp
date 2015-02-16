XCSoar/XCSoar

commit ea917c7c478fa4f0ea29adc17f5c3ac32fa72879

Net/SocketDescriptor: make class "trivial"
Prepare to use it everywhere instead of "int" (without overhead), and
prepare for using SOCKET on Windows.

src/Device/Port/TCPClientPort.cpp
@@ -33,7 +33,7 @@ Copyright_License {
 TCPClientPort::~TCPClientPort()
 {
   if (connecting.IsDefined())
-    io_thread->LockRemove(connecting.Get());
+    io_thread->LockRemove(connecting.Steal());
 }
 
 #endif

src/Device/Port/TCPPort.cpp
@@ -35,7 +35,7 @@ TCPPort::~TCPPort()
 
 #ifdef HAVE_POSIX
   if (listener.IsDefined())
-    io_thread->LockRemove(listener.Get());
+    io_thread->LockRemove(listener.Steal());
 #else
   if (thread.IsDefined()) {
     thread.BeginStop();

src/Device/Port/SocketPort.hpp
@@ -52,7 +52,8 @@ public:
    * port
    */
   SocketPort(PortListener *_listener, DataHandler &_handler)
-    :BufferedPort(_listener, _handler)
+    :BufferedPort(_listener, _handler),
+     socket(SocketDescriptor::Undefined())
 #ifndef HAVE_POSIX
     , thread(socket, *this)
 #endif
@@ -66,7 +67,7 @@ public:
   /**
    * Make the object use the specified socket.
    */
-  void Set(SocketDescriptor &&socket);
+  void Set(SocketDescriptor &&_socket);
 
 protected:
   /**

src/Device/Port/TCPClientPort.hpp
@@ -42,7 +42,11 @@ class TCPClientPort final
 
 public:
   TCPClientPort(PortListener *_listener, DataHandler &_handler)
-    :SocketPort(_listener, _handler) {}
+    :SocketPort(_listener, _handler)
+#ifdef HAVE_POSIX
+    , connecting(SocketDescriptor::Undefined())
+#endif
+  {}
 
 #ifdef HAVE_POSIX
   virtual ~TCPClientPort();

src/Device/Port/TCPPort.hpp
@@ -51,7 +51,8 @@ public:
    * port
    */
   TCPPort(PortListener *_listener, DataHandler &_handler)
-    :SocketPort(_listener, _handler)
+    :SocketPort(_listener, _handler),
+     listener(SocketDescriptor::Undefined())
 #ifndef HAVE_POSIX
     , thread(listener, *this)
 #endif

src/IO/Async/SocketThread.hpp
@@ -25,19 +25,19 @@ Copyright_License {
 #define XCSOAR_SOCKET_THREAD_HPP
 
 #include "Thread/StoppableThread.hpp"
+#include "Net/SocketDescriptor.hpp"
 
-class SocketDescriptor;
 class FileEventHandler;
 
 /**
  * An adapter between a #SocketDescriptor and a #FileEventHandler.
  */
 class SocketThread : public StoppableThread {
-  SocketDescriptor &socket;
+  SocketDescriptor socket;
   FileEventHandler &handler;
 
 public:
-  SocketThread(SocketDescriptor &_socket, FileEventHandler &_handler)
+  SocketThread(SocketDescriptor _socket, FileEventHandler &_handler)
     :StoppableThread("SocketThread"), socket(_socket), handler(_handler) {}
 
 protected:

src/Kobo/WPASupplicant.cpp
@@ -56,7 +56,8 @@ WPASupplicant::Close()
     local_path[0] = 0;
   }
 
-  fd.Close();
+  if (fd.IsDefined())
+    fd.Close();
 }
 
 bool

src/Kobo/WPASupplicant.hpp
@@ -62,6 +62,8 @@ class WPASupplicant {
   char local_path[32];
 
 public:
+  WPASupplicant():fd(SocketDescriptor::Undefined()) {}
+
   ~WPASupplicant() {
     Close();
   }

commit 99395f94b6c1dd77ad7751a591174dffc156b2d5

Device/Port/TCP*Port: close the SocketDescriptor explicitly
The method Steal() does not close the socket.  Regression by commit
ea917c7c

src/Device/Port/TCPClientPort.cpp
@@ -32,8 +32,10 @@ Copyright_License {
 
 TCPClientPort::~TCPClientPort()
 {
-  if (connecting.IsDefined())
-    io_thread->LockRemove(connecting.Steal());
+  if (connecting.IsDefined()) {
+    io_thread->LockRemove(connecting.Get());
+    connecting.Close();
+  }
 }
 
 #endif

src/Device/Port/TCPPort.cpp
@@ -33,15 +33,19 @@ TCPPort::~TCPPort()
 {
   SocketPort::Close();
 
-#ifdef HAVE_POSIX
-  if (listener.IsDefined())
-    io_thread->LockRemove(listener.Steal());
-#else
+#ifndef HAVE_POSIX
   if (thread.IsDefined()) {
     thread.BeginStop();
     thread.Join();
   }
 #endif
+
+  if (listener.IsDefined()) {
+#ifdef HAVE_POSIX
+    io_thread->LockRemove(listener.Get());
+#endif
+    listener.Close();
+  }
 }
 
 bool