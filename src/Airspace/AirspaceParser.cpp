7af5920b8570bf17940e6bb56cad90553446e20d
Max Kellermann <max@duempel.org>
2015-02-19 22:12:07 +0100

    AirspaceParser: reset base and top altitude
    Don't leak altitude data from the previous airspace.
    
    Fixes the second part of TRAC #3468.

--- src/Airspace/AirspaceParser.cpp
+++ src/Airspace/AirspaceParser.cpp
@@ -145,6 +145,7 @@ struct TempAirspaceType
     days_of_operation.SetAll();
     radio = _T("");
     type = OTHER;
+    base = top = AirspaceAltitude();
     points.clear();
     center.longitude = Angle::Zero();
     center.latitude = Angle::Zero();
