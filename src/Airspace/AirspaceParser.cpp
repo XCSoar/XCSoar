3598ede734911ebd48ded760171376781433d6ca
Max Kellermann <max@duempel.org>
2015-02-19 22:25:08 +0100

    AirspaceParser: set fallback altitudes
    Set fallback values for FL and AGL, just in case we have no QNH and no
    terrain.
    
    Fixes the second part of TRAC #3468.

--- src/Airspace/AirspaceParser.cpp
+++ src/Airspace/AirspaceParser.cpp
@@ -328,6 +328,9 @@ ReadAltitude(const TCHAR *buffer, AirspaceAltitude &altitude)
   case FL:
     altitude.reference = AltitudeReference::STD;
     altitude.flight_level = value;
+
+    /* prepare fallback, just in case we have no terrain */
+    altitude.altitude = Units::ToSysUnit(value, Unit::FLIGHT_LEVEL);
     return;
 
   case UNLIMITED:
@@ -338,6 +341,9 @@ ReadAltitude(const TCHAR *buffer, AirspaceAltitude &altitude)
   case SFC:
     altitude.reference = AltitudeReference::AGL;
     altitude.altitude_above_terrain = fixed(-1);
+
+    /* prepare fallback, just in case we have no terrain */
+    altitude.altitude = fixed(0);
     return;
 
   default:
@@ -355,11 +361,17 @@ ReadAltitude(const TCHAR *buffer, AirspaceAltitude &altitude)
   case AGL:
     altitude.reference = AltitudeReference::AGL;
     altitude.altitude_above_terrain = value;
+
+    /* prepare fallback, just in case we have no terrain */
+    altitude.altitude = value;
     return;
 
   case STD:
     altitude.reference = AltitudeReference::STD;
     altitude.flight_level = Units::ToUserUnit(value, Unit::FLIGHT_LEVEL);
+
+    /* prepare fallback, just in case we have no QNH */
+    altitude.altitude = value;
     return;
 
   default:
