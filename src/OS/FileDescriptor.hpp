fce5045900c3aef0892fdbdddc874d59c3f298cf
Max Kellermann <max@duempel.org>
2015-03-03 22:29:33 +0100

    OS/FileDescriptor: add "mode" parameter to Open()

--- src/OS/FileDescriptor.hpp
+++ src/OS/FileDescriptor.hpp
@@ -93,7 +93,7 @@ public:
     return FileDescriptor(-1);
   }
 
-  bool Open(const char *pathname, int flags);
+  bool Open(const char *pathname, int flags, mode_t mode=0666);
   bool OpenReadOnly(const char *pathname);
 
 #ifdef HAVE_POSIX
