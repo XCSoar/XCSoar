XCSoar/XCSoar

commit 16d0364027bd24562305d1d382cb0a1511e1e228

Device/Driver: pass RadioFrequency to PutActiveFrequency()
No inaccurate floating point.  That method isn't used currently.

src/Device/Driver/XCOM760.cpp
@@ -36,8 +36,8 @@ public:
 
 public:
   virtual bool PutVolume(int volume);
-  virtual bool PutActiveFrequency(double frequency);
-  virtual bool PutStandbyFrequency(double frequency);
+  virtual bool PutActiveFrequency(RadioFrequency frequency);
+  virtual bool PutStandbyFrequency(RadioFrequency frequency);
 };
 
 bool
@@ -50,20 +50,23 @@ XCOM760Device::PutVolume(int Volume)
 }
 
 bool
-XCOM760Device::PutActiveFrequency(double Freq)
+XCOM760Device::PutActiveFrequency(RadioFrequency frequency)
 {
   char szTmp[32];
-  sprintf(szTmp, "$TXAF=%.3f\r\n", Freq);
+  sprintf(szTmp, "$TXAF=%u.%03u\r\n",
+          frequency.GetKiloHertz() / 1000,
+          frequency.GetKiloHertz() % 1000);
   port->Write(szTmp);
   return true;
 }
 
 bool
-XCOM760Device::PutStandbyFrequency(double Freq)
+XCOM760Device::PutStandbyFrequency(RadioFrequency frequency)
 {
   char szTmp[32];
-  sprintf(szTmp, "$TXSF=%.3f\r\n", Freq);
-  port->Write(szTmp);
+  sprintf(szTmp, "$TXSF=%u.%03u\r\n",
+          frequency.GetKiloHertz() / 1000,
+          frequency.GetKiloHertz() % 1000);
   return true;
 }
 

src/Device/All.cpp
@@ -82,7 +82,7 @@ AllDevicesPutVolume(int volume)
 }
 
 void
-AllDevicesPutActiveFrequency(double frequency)
+AllDevicesPutActiveFrequency(RadioFrequency frequency)
 {
   if (is_simulator())
     return;
@@ -92,7 +92,7 @@ AllDevicesPutActiveFrequency(double frequency)
 }
 
 void
-AllDevicesPutStandbyFrequency(double frequency)
+AllDevicesPutStandbyFrequency(RadioFrequency frequency)
 {
   if (is_simulator())
     return;

src/Device/All.hpp
@@ -26,6 +26,7 @@ Copyright_License {
 
 #include "Math/fixed.hpp"
 #include "Compiler.h"
+#include "RadioFrequency.hpp"
 
 #include <tchar.h>
 
@@ -39,8 +40,12 @@ void AllDevicesPutMacCready(fixed MacCready);
 void AllDevicesPutBugs(fixed bugs);
 void AllDevicesPutBallast(fixed ballast);
 void AllDevicesPutVolume(int volume);
-void AllDevicesPutActiveFrequency(double frequency);
-void AllDevicesPutStandbyFrequency(double frequency);
+
+void
+AllDevicesPutActiveFrequency(RadioFrequency frequency);
+
+void
+AllDevicesPutStandbyFrequency(RadioFrequency frequency);
 
 void
 AllDevicesPutQNH(const AtmosphericPressure &pres,

src/Device/Descriptor.cpp
@@ -186,13 +186,13 @@ DeviceDescriptor::PutVolume(int volume)
 }
 
 bool
-DeviceDescriptor::PutActiveFrequency(double frequency)
+DeviceDescriptor::PutActiveFrequency(RadioFrequency frequency)
 {
   return device != NULL ? device->PutActiveFrequency(frequency) : true;
 }
 
 bool
-DeviceDescriptor::PutStandbyFrequency(double frequency)
+DeviceDescriptor::PutStandbyFrequency(RadioFrequency frequency)
 {
   return device != NULL ? device->PutStandbyFrequency(frequency) : true;
 }

src/Device/Descriptor.hpp
@@ -26,6 +26,7 @@ Copyright_License {
 
 #include "Device/Port.hpp"
 #include "Device/Parser.hpp"
+#include "RadioFrequency.hpp"
 
 #include <tchar.h>
 #include <stdio.h>
@@ -123,8 +124,8 @@ public:
   bool PutBugs(fixed bugs);
   bool PutBallast(fixed ballast);
   bool PutVolume(int volume);
-  bool PutActiveFrequency(double frequency);
-  bool PutStandbyFrequency(double frequency);
+  bool PutActiveFrequency(RadioFrequency frequency);
+  bool PutStandbyFrequency(RadioFrequency frequency);
   bool PutQNH(const AtmosphericPressure &pres,
               const DERIVED_INFO &calculated);
   bool PutVoice(const TCHAR *sentence);

src/Device/Driver.cpp
@@ -80,13 +80,13 @@ AbstractDevice::PutVolume(int volume)
 }
 
 bool
-AbstractDevice::PutActiveFrequency(double frequency)
+AbstractDevice::PutActiveFrequency(RadioFrequency frequency)
 {
   return true;
 }
 
 bool
-AbstractDevice::PutStandbyFrequency(double frequency)
+AbstractDevice::PutStandbyFrequency(RadioFrequency frequency)
 {
   return true;
 }

src/Device/Driver.hpp
@@ -25,6 +25,7 @@ Copyright_License {
 #define XCSOAR_DEVICE_DRIVER_HPP
 
 #include "Device/Declaration.hpp"
+#include "RadioFrequency.hpp"
 
 #include <tchar.h>
 
@@ -54,8 +55,8 @@ public:
                       const DERIVED_INFO &calculated) = 0;
   virtual bool PutVoice(const TCHAR *sentence) = 0;
   virtual bool PutVolume(int volume) = 0;
-  virtual bool PutActiveFrequency(double frequency) = 0;
-  virtual bool PutStandbyFrequency(double frequency) = 0;
+  virtual bool PutActiveFrequency(RadioFrequency frequency) = 0;
+  virtual bool PutStandbyFrequency(RadioFrequency frequency) = 0;
 
   virtual bool Declare(const struct Declaration *declaration,
                        OperationEnvironment &env) = 0;
@@ -83,8 +84,8 @@ public:
                       const DERIVED_INFO &calculated);
   virtual bool PutVoice(const TCHAR *sentence);
   virtual bool PutVolume(int volume);
-  virtual bool PutActiveFrequency(double frequency);
-  virtual bool PutStandbyFrequency(double frequency);
+  virtual bool PutActiveFrequency(RadioFrequency frequency);
+  virtual bool PutStandbyFrequency(RadioFrequency frequency);
 
   virtual bool Declare(const struct Declaration *declaration,
                        OperationEnvironment &env);

commit 412056f0ee969e9ca4350915cf262c2943ae8a39

Device: fix broken XCOM760Device::PutStandbyFrequency()
Regression introduced by 16d0364027bd24562305d1d382cb0a1511e1e228

src/Device/Driver/XCOM760.cpp
@@ -76,6 +76,7 @@ XCOM760Device::PutStandbyFrequency(RadioFrequency frequency,
   sprintf(szTmp, "$TXSF=%u.%03u\r\n",
           frequency.GetKiloHertz() / 1000,
           frequency.GetKiloHertz() % 1000);
+  port.Write(szTmp);
   return true;
 }
 