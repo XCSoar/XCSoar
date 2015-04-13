ce43fa5347846e27d51cba7d2d04ec947b005760
Max Kellermann <max@duempel.org>
2015-04-13 13:32:06 +0200

    Makefile: remove unused variable THIRDPARTY_DLLS
    This has been obsolete for a number of years.

--- build/cab.mk
+++ build/cab.mk
@@ -3,12 +3,8 @@ CABWIZ = wine 'c:\cabwiz\cabwiz.exe'
 $(TARGET_BIN_DIR)/XCSoar.inf: build/cab.inf $(TARGET_BIN_DIR)/dirstamp
 	$(Q)cp $< $@
 
-$(addprefix $(TARGET_BIN_DIR)/,$(THIRDPARTY_DLLS)): $(TARGET_BIN_DIR)/%.dll: $(THIRDPARTY_DLL_DIR)/%.dll
-	$(Q)cp $< $@
-
 $(TARGET_BIN_DIR)/XCSoar.$(PCPU).CAB: $(TARGET_BIN_DIR)/XCSoar.inf $(XCSOAR_BIN) \
-	$(XCSOARSETUP_DLL) $(XCSOARLAUNCH_DLL) \
-	$(addprefix $(TARGET_BIN_DIR)/,$(THIRDPARTY_DLLS))
+	$(XCSOARSETUP_DLL) $(XCSOARLAUNCH_DLL)
 	@$(NQ)echo "  CAB     $@"
 	$(Q)cd $(TARGET_BIN_DIR) && $(CABWIZ) XCSoar.inf /cpu $(PCPU)
 
