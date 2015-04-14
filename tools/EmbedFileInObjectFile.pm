bbaa12bbb23bdc2ea4822e84a2de6cbeb9ee85e8
Max Kellermann <max@duempel.org>
2015-04-14 12:58:10 +0200

    tools/EmbedFileInObjectFile.pm: move .globl after .set
    Fixes a failure with the clang internal assembler ("symbol already
    defined").

--- tools/EmbedFileInObjectFile.pm
+++ tools/EmbedFileInObjectFile.pm
@@ -8,19 +8,19 @@ sub embed_file_in_object_file($$$$$$) {
     my $asm = "";
     $asm .= ".p2align 3\n";
     $asm .= ".data\n";
-    $asm .= ".globl ${start_sym_name}\n";
     $asm .= "${start_sym_name}:\n";
+    $asm .= ".globl ${start_sym_name}\n";
     $asm .= ".incbin \"${input}\"\n";
-    $asm .= ".globl ${end_sym_name}\n";
     $asm .= ".set ${end_sym_name}, \.\n";
-    $asm .= ".globl ${size_sym_name}\n";
+    $asm .= ".globl ${end_sym_name}\n";
     $asm .= ".set ${size_sym_name}, ${input_file_size}\n";
-    $asm .= ".globl _${start_sym_name}\n";
+    $asm .= ".globl ${size_sym_name}\n";
     $asm .= ".set _${start_sym_name}, ${start_sym_name}\n";
-    $asm .= ".globl _${end_sym_name}\n";
+    $asm .= ".globl _${start_sym_name}\n";
     $asm .= ".set _${end_sym_name}, ${end_sym_name}\n";
-    $asm .= ".globl _${size_sym_name}\n";
+    $asm .= ".globl _${end_sym_name}\n";
     $asm .= ".set _${size_sym_name}\, ${size_sym_name}\n";
+    $asm .= ".globl _${size_sym_name}\n";
 
     my $asm_echo = $asm;
     $asm_echo =~ s,\n,\\n,g;
