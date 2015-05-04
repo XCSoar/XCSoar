eea6a84ee989150bc8813b6fcdcaacd2f1d5806d
Max Kellermann <max@duempel.org>
2015-05-04 13:54:52 +0200

    Dialogs/Task: add API documentation

--- src/Dialogs/Task/TaskDialogs.hpp
+++ src/Dialogs/Task/TaskDialogs.hpp
@@ -29,9 +29,21 @@ class OrderedTask;
 void
 dlgTaskManagerShowModal();
 
+/**
+ * Show a dialog that lets the user edit a task point (and lets him
+ * navigate to other task points).
+ *
+ * @return true if the task was modified
+ */
 bool
 dlgTaskPointShowModal(OrderedTask **task, const unsigned index);
 
+/**
+ * Show a dialog that lets the user mutate one task point to another
+ * type.
+ *
+ * @return true if the task was modified
+ */
 bool
 dlgTaskPointType(OrderedTask **task, unsigned index);
 
