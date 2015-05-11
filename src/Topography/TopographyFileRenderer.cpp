XCSoar/XCSoar

commit 7dc33c3d399abafcca23c7cd37c5a35052e1930a

TopographyFileRenderer: add struct TopographyLook
Use a Look class instead of accessing the Fonts::* global variables.

src/Topography/TopographyFileRenderer.cpp
@@ -24,7 +24,7 @@ Copyright_License {
 #include "Topography/TopographyFileRenderer.hpp"
 #include "Topography/TopographyFile.hpp"
 #include "Topography/XShape.hpp"
-#include "Look/GlobalFonts.hpp"
+#include "Look/TopographyLook.hpp"
 #include "Renderer/LabelBlock.hpp"
 #include "Projection/WindowProjection.hpp"
 #include "Screen/Canvas.hpp"
@@ -54,8 +54,10 @@ Copyright_License {
 #include <numeric>
 #include <set>
 
-TopographyFileRenderer::TopographyFileRenderer(const TopographyFile &_file)
-  :file(_file), pen(file.GetPenWidth(), file.GetColor()),
+TopographyFileRenderer::TopographyFileRenderer(const TopographyFile &_file,
+                                               const TopographyLook &_look)
+  :file(_file), look(_look),
+   pen(file.GetPenWidth(), file.GetColor()),
 #ifdef ENABLE_OPENGL
    array_buffer(nullptr)
 #else
@@ -457,8 +459,9 @@ TopographyFileRenderer::PaintLabels(Canvas &canvas,
   if (visible_labels.empty())
     return;
 
-  canvas.Select(file.IsLabelImportant(map_scale) ?
-                Fonts::map_label_important : Fonts::map_label);
+  canvas.Select(file.IsLabelImportant(map_scale)
+                ? *look.regular_label_font
+                : *look.important_label_font);
   canvas.SetTextColor(file.IsLabelImportant(map_scale) ?
                 COLOR_BLACK : COLOR_DARK_GRAY);
   canvas.SetBackgroundTransparent();

src/Dialogs/Task/TargetDialog.cpp
@@ -59,9 +59,10 @@ public:
                         const AirspaceLook &airspace_look,
                         const TrailLook &trail_look,
                         const TaskLook &task_look,
-                        const AircraftLook &aircraft_look)
+                        const AircraftLook &aircraft_look,
+                        const TopographyLook &topography_look)
     :TargetMapWindow(waypoint_look, airspace_look, trail_look,
-                     task_look, aircraft_look),
+                     task_look, aircraft_look, topography_look),
      widget(_widget) {}
 
 protected:
@@ -127,7 +128,8 @@ public:
      rate_limited_bl(*this, 1800, 300),
      map(*this,
          map_look.waypoint, map_look.airspace,
-         map_look.trail, map_look.task, map_look.aircraft),
+         map_look.trail, map_look.task, map_look.aircraft,
+         map_look.topography),
      range(dialog_look),
      radial(dialog_look),
      ete(dialog_look),

src/Look/Look.cpp
@@ -44,7 +44,8 @@ Look::InitialiseConfigured(const UISettings &settings,
                            const Font &dialog_bold_font,
                            const Font &dialog_small_font,
                            const Font &map_font, const Font &map_bold_font,
-                           const Font &map_label_font,
+                           const Font &regular_topography_font,
+                           const Font &important_topography_font,
                            const Font &cdi_font,
                            const Font &monospace_font,
                            const Font &infobox_value_font,
@@ -79,6 +80,7 @@ Look::InitialiseConfigured(const UISettings &settings,
   thermal_assistant_gauge.Initialise(true, settings.info_boxes.inverse);
   final_glide_bar.Initialise(map_font);
   vario_bar.Initialise(map_font);
-  map.Initialise(settings.map, map_font, map_bold_font);
+  map.Initialise(settings.map, map_font, map_bold_font,
+                 regular_topography_font, important_topography_font);
   icon.Initialise();
 }

src/Look/Look.hpp
@@ -77,7 +77,8 @@ struct Look {
                             const Font &dialog_bold_font,
                             const Font &dialog_small_font,
                             const Font &map_font, const Font &map_bold_font,
-                            const Font &map_label_font,
+                            const Font &regular_topography_font,
+                            const Font &important_topography_font,
                             const Font &cdi_font,
                             const Font &monospace_font,
                             const Font &infobox_value_font,

src/Look/MapLook.cpp
@@ -28,7 +28,9 @@ Copyright_License {
 
 void
 MapLook::Initialise(const MapSettings &settings,
-                    const Font &font, const Font &bold_font)
+                    const Font &font, const Font &bold_font,
+                    const Font &regular_topography_font,
+                    const Font &important_topography_font)
 {
   waypoint.Initialise(settings.waypoint, font, bold_font);
   airspace.Initialise(settings.airspace, font);
@@ -90,4 +92,6 @@ MapLook::Initialise(const MapSettings &settings,
   no_gps_icon.LoadResource(IDB_GPSSTATUS2, IDB_GPSSTATUS2_HD, false);
 
   overlay_font = &bold_font;
+
+  topography.Initialise(regular_topography_font, important_topography_font);
 }

src/Look/MapLook.hpp
@@ -32,6 +32,7 @@ Copyright_License {
 #include "TrailLook.hpp"
 #include "WaveLook.hpp"
 #include "WindArrowLook.hpp"
+#include "TopographyLook.hpp"
 #include "Screen/Icon.hpp"
 #include "Screen/Bitmap.hpp"
 #include "Screen/Pen.hpp"
@@ -93,8 +94,12 @@ struct MapLook {
 
   const Font *overlay_font;
 
+  TopographyLook topography;
+
   void Initialise(const MapSettings &settings,
-                  const Font &font, const Font &bold_font);
+                  const Font &font, const Font &bold_font,
+                  const Font &regular_topography_font,
+                  const Font &important_topography_font);
 };
 
 #endif

src/Look/TopographyLook.hpp
@@ -1,0 +1,39 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2015 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef XCSOAR_TOPOGRAPHY_LOOK_HPP
+#define XCSOAR_TOPOGRAPHY_LOOK_HPP
+
+class Font;
+
+struct TopographyLook {
+  const Font *regular_label_font, *important_label_font;
+
+  void Initialise(const Font &_regular_label_font,
+                  const Font &_important_label_font) {
+    regular_label_font = &_regular_label_font;
+    important_label_font = &_important_label_font;
+  }
+};
+
+#endif

src/MainWindow.cpp
@@ -249,7 +249,9 @@ MainWindow::InitialiseConfigured()
   look->InitialiseConfigured(CommonInterface::GetUISettings(),
                              Fonts::dialog, Fonts::dialog_bold,
                              Fonts::dialog_small,
-                             Fonts::map, Fonts::map_bold, Fonts::map_label,
+                             Fonts::map, Fonts::map_bold,
+                             Fonts::map_label,
+                             Fonts::map_label_important,
                              Fonts::cdi, Fonts::monospace,
                              Fonts::infobox, Fonts::infobox_small,
 #ifndef GNAV

commit 27c19e83c0419b3d3f72b58db27e8581a272836a

TopographyFileRenderer: Fix swapped label fonts
Fixes regression introduced in 7dc33c3d399abafcca23c7cd37c5a35052e1930a .

src/Topography/TopographyFileRenderer.cpp
@@ -460,8 +460,8 @@ TopographyFileRenderer::PaintLabels(Canvas &canvas,
     return;
 
   canvas.Select(file.IsLabelImportant(map_scale)
-                ? *look.regular_label_font
-                : *look.important_label_font);
+                ? *look.important_label_font
+                : *look.regular_label_font);
   canvas.SetTextColor(file.IsLabelImportant(map_scale) ?
                 COLOR_BLACK : COLOR_DARK_GRAY);
   canvas.SetBackgroundTransparent();