58902443384d79fa8e913ea945f9e4d4dbef142a
Felix Hädicke <felixhaedicke@web.de>
2015-05-18 23:27:46 +0200

    Makefile: Rename Cydia-Deb App Bundle Identifier
    Do not use the technical names "org.xcsoar" / "org.xcsoar.testing", but
    XCSoar / XCSoar-testing as the App Bundle Indentifier. This indentifier
    is shown as the display name in Cydia.

--- build/ios.mk
+++ build/ios.mk
@@ -11,14 +11,14 @@ CYDIA_DEB_NAME = xcsoar-testing.deb
 IOS_LOGO_SVG = $(topdir)/Data/graphics/logo_red.svg
 IOS_ICON_SVG = $(topdir)/Data/iOS/iOS-Icon_red.svg
 IOS_APP_DIR_NAME = XCSoar.testing.app
-IOS_APP_BUNDLE_INENTIFIER = org.xcsoar.testing
+IOS_APP_BUNDLE_INENTIFIER = XCSoar-testing
 IOS_APP_DISPLAY_NAME = XCSoar Testing
 else
 CYDIA_DEB_NAME = xcsoar.deb
 IOS_LOGO_SVG = $(topdir)/Data/graphics/logo.svg
 IOS_ICON_SVG = $(topdir)/Data/iOS/iOS-Icon.svg
 IOS_APP_DIR_NAME = XCSoar.app
-IOS_APP_BUNDLE_INENTIFIER = org.xcsoar
+IOS_APP_BUNDLE_INENTIFIER = XCSoar
 IOS_APP_DISPLAY_NAME = XCSoar
 endif
 
