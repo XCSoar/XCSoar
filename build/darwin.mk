b664edd01e42352ef93fc1e09cd8de86cfed8dd9
Felix Hädicke <felixhaedicke@web.de>
2015-05-18 22:29:31 +0200

    Makefile: Move CoreLocation framework reference from TARGET_LDFLAGS to XCSOAR_LDLIBS

--- build/darwin.mk
+++ build/darwin.mk
@@ -3,8 +3,6 @@ ifeq ($(TARGET_IS_DARWIN),y)
 TARGET_CPPFLAGS += -isystem $(DARWIN_LIBS)/include
 TARGET_CXXFLAGS += -ObjC++
 
-TARGET_LDFLAGS += -framework CoreLocation
-
 TARGET_ARCH += -isysroot $(DARWIN_SDK)
 
 endif
