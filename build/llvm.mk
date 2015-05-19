ea124711980b3302a6f2cea04db72a0b45c35eda
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2015-05-19 20:12:52 +0200

    build/llvm: Simplify CCACHE usage
    There is no need to export this all the time. Defining it just for the call
    is enough.

--- build/llvm.mk
+++ build/llvm.mk
@@ -17,7 +17,7 @@ ifeq ($(USE_CCACHE),y)
   # ccache will not use the optimisation of avoiding the 2nd call to the
   # pre-processor by compiling the pre-processed output that was used for
   # finding the hash in the case of a cache miss.
-  CCACHE := export CCACHE_CPP2=yes && $(CCACHE)
+  CCACHE := CCACHE_CPP2=yes $(CCACHE)
 endif
 
 ifneq ($(LLVM_TARGET),)
