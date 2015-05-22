XCSoar/XCSoar

commit 17d86c64481eb07d6b0ce8928d2d3815cfbcd0cf

Dialogs/Task/Calculator: move to dlgStatus
There are quite a few redundant lines; merge those and eliminate the
TaskCalculator panel.  The TaskCalculator panel is confusing because
it does not consider unsaved data, therefore it does not belong in the
task editor.

src/Dialogs/Task/Manager/TaskManagerDialog.cpp
@@ -23,7 +23,6 @@ Copyright_License {
 
 #include "Internal.hpp"
 #include "TaskMapWindow.hpp"
-#include "TaskCalculatorPanel.hpp"
 #include "TaskEditPanel.hpp"
 #include "TaskPropertiesPanel.hpp"
 #include "TaskMiscPanel.hpp"
@@ -168,9 +167,6 @@ TaskManagerDialog::Initialise(ContainerWindow &parent, const PixelRect &rc)
   TaskClosePanel *wClose = new TaskClosePanel(*this, &modified,
                                               UIGlobals::GetDialogLook());
 
-  TaskCalculatorPanel *wCalculator =
-    new TaskCalculatorPanel(UIGlobals::GetDialogLook(), &modified);
-
   const MapLook &look = UIGlobals::GetMapLook();
   Widget *wEdit = CreateTaskEditPanel(*this, look.task, look.airspace,
                                       &task, &modified);
@@ -181,12 +177,9 @@ TaskManagerDialog::Initialise(ContainerWindow &parent, const PixelRect &rc)
     CommonInterface::GetUISettings().dialog.tab_style
     == DialogSettings::TabStyle::Icon;
   const IconLook &icons = UIGlobals::GetIconLook();
-  const Bitmap *CalcIcon = enable_icons ? &icons.hBmpTabCalculator : nullptr;
   const Bitmap *TurnPointIcon = enable_icons ? &icons.hBmpTabTask : nullptr;
   const Bitmap *BrowseIcon = enable_icons ? &icons.hBmpTabWrench : nullptr;
   const Bitmap *PropertiesIcon = enable_icons ? &icons.hBmpTabSettings : nullptr;
-
-  tab_bar->AddTab(wCalculator, _("Calculator"), CalcIcon);
 
   if (layout.vertical) {
     tab_bar->AddTab(wEdit, _("Turn Points"), TurnPointIcon);

build/main.mk
@@ -123,7 +123,6 @@ DIALOG_SOURCES = \
 	$(SRC)/Dialogs/Task/Manager/TaskMiscPanel.cpp \
 	$(SRC)/Dialogs/Task/Manager/TaskActionsPanel.cpp \
 	$(SRC)/Dialogs/Task/Manager/TaskListPanel.cpp \
-	$(SRC)/Dialogs/Task/Manager/TaskCalculatorPanel.cpp \
 	$(SRC)/Dialogs/Task/OptionalStartsDialog.cpp \
 	$(SRC)/Dialogs/Task/TaskPointDialog.cpp \
 	$(SRC)/Dialogs/Task/MutateTaskPointDialog.cpp \

src/Dialogs/StatusPanels/TaskStatusPanel.cpp
@@ -22,8 +22,10 @@ Copyright_License {
 */
 
 #include "TaskStatusPanel.hpp"
+#include "Form/DataField/Float.hpp"
 #include "Util/Macros.hpp"
-#include "Interface.hpp"
+#include "ActionInterface.hpp"
+#include "Units/Units.hpp"
 #include "Formatter/UserUnits.hpp"
 #include "Formatter/TimeFormatter.hpp"
 #include "Components.hpp"
@@ -38,7 +40,25 @@ enum Controls {
   RemainingDistance,
   EstimatedSpeed,
   AverageSpeed,
+
+  MC,
+  RANGE,
+  SPEED_REMAINING,
+  EFFECTIVE_MC,
+  SPEED_ACHIEVED,
+  CRUISE_EFFICIENCY,
 };
+
+void
+TaskStatusPanel::OnModified(DataField &df)
+{
+  if (IsDataField(MC, df)) {
+    const DataFieldFloat &dff = (const DataFieldFloat &)df;
+    fixed mc = Units::ToSysVSpeed(dff.GetAsFixed());
+    ActionInterface::SetManualMacCready(mc);
+    Refresh();
+  }
+}
 
 void
 TaskStatusPanel::Refresh()
@@ -81,6 +101,38 @@ TaskStatusPanel::Refresh()
             FormatUserTaskSpeed(task_stats.total.travelled.GetSpeed()));
   else
     ClearText(AverageSpeed);
+
+  LoadValue(MC,
+            CommonInterface::GetComputerSettings().polar.glide_polar_task.GetMC(),
+            UnitGroup::VERTICAL_SPEED);
+
+  if (task_stats.total.planned.IsDefined()) {
+    fixed rMax = task_stats.distance_max;
+    fixed rMin = task_stats.distance_min;
+
+    if (rMin < rMax) {
+      fixed range = Double((task_stats.total.remaining.GetDistance() - rMin) / (rMax - rMin)) - fixed(1);
+      LoadValue(RANGE, range * 100);
+    } else
+      ClearValue(RANGE);
+  } else
+    ClearValue(RANGE);
+
+  if (task_stats.total.remaining_effective.IsDefined())
+    LoadValue(SPEED_REMAINING, task_stats.total.remaining_effective.GetSpeed(),
+              UnitGroup::TASK_SPEED);
+  else
+    ClearValue(SPEED_REMAINING);
+
+  LoadValue(EFFECTIVE_MC, task_stats.effective_mc, UnitGroup::VERTICAL_SPEED);
+
+  if (task_stats.total.travelled.IsDefined())
+    LoadValue(SPEED_ACHIEVED, task_stats.total.travelled.GetSpeed(),
+              UnitGroup::TASK_SPEED);
+  else
+    ClearValue(SPEED_ACHIEVED);
+
+  LoadValue(CRUISE_EFFICIENCY, task_stats.cruise_efficiency * 100);
 }
 
 void
@@ -93,4 +145,36 @@ TaskStatusPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
   AddReadOnly(_("Remaining distance"));
   AddReadOnly(_("Speed estimated"));
   AddReadOnly(_("Speed average"));
+
+  AddFloat(_("Set MacCready"),
+           _("Adjusts MC value used in the calculator.  "
+             "Use this to determine the effect on estimated task time due to changes in conditions.  "
+             "This value will not affect the main computer's setting if the dialog is exited with the Cancel button."),
+           _T("%.1f %s"), _T("%.1f"),
+           fixed(0), Units::ToUserVSpeed(fixed(5)),
+           GetUserVerticalSpeedStep(), false, fixed(0),
+           this);
+  DataFieldFloat &mc_df = (DataFieldFloat &)GetDataField(MC);
+  mc_df.SetFormat(GetUserVerticalSpeedFormat(false, false));
+
+  AddReadOnly(_("AAT range"),
+              /* xgettext:no-c-format */
+              _("For AAT tasks, this value tells you how far based on the targets of your task you will fly relative to the minimum and maximum possible tasks. -100% indicates the minimum AAT distance.  0% is the nominal AAT distance.  +100% is maximum AAT distance."),
+              _T("%.0f %%"), fixed(0));
+
+  AddReadOnly(_("Speed remaining"), nullptr, _T("%.0f %s"),
+              UnitGroup::TASK_SPEED, fixed(0));
+
+  AddReadOnly(_("Achieved MacCready"), nullptr, _T("%.1f %s"),
+              UnitGroup::VERTICAL_SPEED, fixed(0));
+  DataFieldFloat &emc_df = (DataFieldFloat &)GetDataField(EFFECTIVE_MC);
+  emc_df.SetFormat(GetUserVerticalSpeedFormat(false, false));
+
+  AddReadOnly(_("Achieved speed"), nullptr, _T("%.0f %s"),
+              UnitGroup::TASK_SPEED, fixed(0));
+
+  AddReadOnly(_("Cruise efficiency"),
+              _("Efficiency of cruise.  100 indicates perfect MacCready performance, greater than 100 indicates better than MacCready performance is achieved through flying in streets.  Less than 100 is appropriate if you fly considerably off-track.  This value estimates your cruise efficiency according to the current flight history with the set MC value.  Calculation begins after task is started."),
+              _T("%.0f %%"),
+              fixed(0));
 }

src/Dialogs/StatusPanels/TaskStatusPanel.hpp
@@ -25,8 +25,9 @@ Copyright_License {
 #define XCSOAR_TASK_STATUS_PANEL_HPP
 
 #include "StatusPanel.hpp"
+#include "Form/DataField/Listener.hpp"
 
-class TaskStatusPanel : public StatusPanel {
+class TaskStatusPanel : public StatusPanel, DataFieldListener {
 public:
   TaskStatusPanel(const DialogLook &look):StatusPanel(look) {}
 
@@ -35,6 +36,10 @@ public:
 
   /* virtual methods from class Widget */
   void Prepare(ContainerWindow &parent, const PixelRect &rc) override;
+
+private:
+  /* virtual methods from DataFieldListener */
+  void OnModified(DataField &df) override;
 };
 
 #endif

src/Dialogs/Task/Manager/TaskCalculatorPanel.cpp
@@ -1,196 +1,0 @@
-/*
-Copyright_License {
-
-  XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2015 The XCSoar Project
-  A detailed list of copyright holders can be found in the file "AUTHORS".
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-}
-*/
-
-#include "TaskCalculatorPanel.hpp"
-#include "Internal.hpp"
-#include "../TaskDialogs.hpp"
-#include "Widget/TextWidget.hpp"
-#include "Interface.hpp"
-#include "ActionInterface.hpp"
-#include "Units/Units.hpp"
-#include "Formatter/UserUnits.hpp"
-#include "Formatter/TimeFormatter.hpp"
-#include "Form/Draw.hpp"
-#include "Form/Button.hpp"
-#include "Form/DataField/Float.hpp"
-#include "Task/ProtectedTaskManager.hpp"
-#include "Components.hpp"
-#include "Screen/Canvas.hpp"
-#include "Screen/Layout.hpp"
-#include "Screen/Icon.hpp"
-#include "UIGlobals.hpp"
-#include "Look/DialogLook.hpp"
-#include "Look/MapLook.hpp"
-#include "Language/Language.hpp"
-
-enum Controls {
-  WARNING,
-  AAT_TIME,
-  AAT_ESTIMATED,
-  DISTANCE,
-  MC,
-  RANGE,
-  SPEED_REMAINING,
-  EFFECTIVE_MC,
-  SPEED_ACHIEVED,
-  CRUISE_EFFICIENCY,
-};
-
-void
-TaskCalculatorPanel::Refresh()
-{
-  const auto &calculated = CommonInterface::Calculated();
-  const TaskStats &task_stats = calculated.ordered_task_stats;
-
-  SetRowVisible(AAT_TIME, task_stats.has_targets);
-  if (task_stats.has_targets)
-    SetText(AAT_TIME,
-            FormatTimespanSmart((int)protected_task_manager->GetOrderedTaskSettings().aat_min_time, 2));
-
-  SetText(AAT_ESTIMATED,
-          FormatTimespanSmart((int)task_stats.GetEstimatedTotalTime(), 2));
-
-  fixed rPlanned = task_stats.total.solution_planned.IsDefined()
-    ? task_stats.total.solution_planned.vector.distance
-    : fixed(0);
-
-  if (positive(rPlanned))
-    LoadValue(DISTANCE, rPlanned, UnitGroup::DISTANCE);
-  else
-    ClearValue(DISTANCE);
-
-  LoadValue(MC, CommonInterface::GetComputerSettings().polar.glide_polar_task.GetMC(),
-            UnitGroup::VERTICAL_SPEED);
-  LoadValue(EFFECTIVE_MC, emc, UnitGroup::VERTICAL_SPEED);
-
-  if (positive(rPlanned)) {
-    fixed rMax = task_stats.distance_max;
-    fixed rMin = task_stats.distance_min;
-
-    if (rMin < rMax) {
-      fixed range = Double((rPlanned - rMin) / (rMax - rMin)) - fixed(1);
-      LoadValue(RANGE, range * 100);
-    } else
-      ClearValue(RANGE);
-  } else
-    ClearValue(RANGE);
-
-  if (task_stats.total.remaining_effective.IsDefined())
-    LoadValue(SPEED_REMAINING, task_stats.total.remaining_effective.GetSpeed(),
-              UnitGroup::TASK_SPEED);
-  else
-    ClearValue(SPEED_REMAINING);
-
-  if (task_stats.total.travelled.IsDefined())
-    LoadValue(SPEED_ACHIEVED, task_stats.total.travelled.GetSpeed(),
-              UnitGroup::TASK_SPEED);
-  else
-    ClearValue(SPEED_ACHIEVED);
-
-  LoadValue(CRUISE_EFFICIENCY, task_stats.cruise_efficiency * 100);
-}
-
-void
-TaskCalculatorPanel::OnModified(DataField &df)
-{
-  if (IsDataField(MC, df)) {
-    const DataFieldFloat &dff = (const DataFieldFloat &)df;
-    fixed mc = Units::ToSysVSpeed(dff.GetAsFixed());
-    ActionInterface::SetManualMacCready(mc);
-    Refresh();
-  }
-}
-
-void
-TaskCalculatorPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
-{
-  assert(protected_task_manager != nullptr);
-
-  Add(new TextWidget());
-  SetRowVisible(WARNING, false);
-
-  AddReadOnly(_("Assigned task time"));
-  AddReadOnly(_("Estimated task time"));
-  AddReadOnly(_("Task distance"), nullptr, _T("%.0f %s"),
-              UnitGroup::DISTANCE, fixed(0));
-
-  AddFloat(_("Set MacCready"),
-           _("Adjusts MC value used in the calculator.  "
-             "Use this to determine the effect on estimated task time due to changes in conditions.  "
-             "This value will not affect the main computer's setting if the dialog is exited with the Cancel button."),
-           _T("%.1f %s"), _T("%.1f"),
-           fixed(0), Units::ToUserVSpeed(fixed(5)),
-           GetUserVerticalSpeedStep(), false, fixed(0),
-           this);
-  DataFieldFloat &mc_df = (DataFieldFloat &)GetDataField(MC);
-  mc_df.SetFormat(GetUserVerticalSpeedFormat(false, false));
-
-  AddReadOnly(_("AAT range"),
-              /* xgettext:no-c-format */
-              _("For AAT tasks, this value tells you how far based on the targets of your task you will fly relative to the minimum and maximum possible tasks. -100% indicates the minimum AAT distance.  0% is the nominal AAT distance.  +100% is maximum AAT distance."),
-              _T("%.0f %%"), fixed(0));
-
-  AddReadOnly(_("Speed remaining"), nullptr, _T("%.0f %s"),
-              UnitGroup::TASK_SPEED, fixed(0));
-
-  AddReadOnly(_("Achieved MacCready"), nullptr, _T("%.1f %s"),
-              UnitGroup::VERTICAL_SPEED, fixed(0));
-  DataFieldFloat &emc_df = (DataFieldFloat &)GetDataField(EFFECTIVE_MC);
-  emc_df.SetFormat(GetUserVerticalSpeedFormat(false, false));
-
-  AddReadOnly(_("Achieved speed"), nullptr, _T("%.0f %s"),
-              UnitGroup::TASK_SPEED, fixed(0));
-
-  AddReadOnly(_("Cruise efficiency"),
-              _("Efficiency of cruise.  100 indicates perfect MacCready performance, greater than 100 indicates better than MacCready performance is achieved through flying in streets.  Less than 100 is appropriate if you fly considerably off-track.  This value estimates your cruise efficiency according to the current flight history with the set MC value.  Calculation begins after task is started."),
-              _T("%.0f %%"),
-              fixed(0));
-}
-
-void
-TaskCalculatorPanel::Show(const PixelRect &rc)
-{
-  emc = CommonInterface::Calculated().ordered_task_stats.effective_mc;
-
-  Refresh();
-
-  CommonInterface::GetLiveBlackboard().AddListener(*this);
-
-  RowFormWidget::Show(rc);
-
-  const bool modified = IsTaskModified();
-  SetRowVisible(WARNING, modified);
-  if (modified) {
-    TextWidget &widget = (TextWidget &)GetRowWidget(WARNING);
-    widget.SetText(_("Calculator excludes unsaved task changes!"));
-    widget.SetColor(COLOR_RED);
-  }
-}
-
-void
-TaskCalculatorPanel::Hide()
-{
-  CommonInterface::GetLiveBlackboard().RemoveListener(*this);
-
-  RowFormWidget::Hide();
-}

src/Dialogs/Task/Manager/TaskCalculatorPanel.hpp
@@ -1,68 +1,0 @@
-/*
-Copyright_License {
-
-  XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2015 The XCSoar Project
-  A detailed list of copyright holders can be found in the file "AUTHORS".
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-}
-*/
-
-#ifndef XCSOAR_TASK_CALCULATOR_PANEL_HPP
-#define XCSOAR_TASK_CALCULATOR_PANEL_HPP
-
-#include "Widget/RowFormWidget.hpp"
-#include "Form/DataField/Listener.hpp"
-#include "Blackboard/BlackboardListener.hpp"
-#include "Math/fixed.hpp"
-
-class WndForm;
-
-class TaskCalculatorPanel final
-  : public RowFormWidget,
-    private DataFieldListener,
-    private NullBlackboardListener {
-  const bool *task_modified;
-
-  fixed emc;
-
-public:
-  TaskCalculatorPanel(const DialogLook &look, const bool *_task_modified)
-    :RowFormWidget(look),
-     task_modified(_task_modified) {}
-
-  bool IsTaskModified() const {
-    return *task_modified;
-  }
-
-  void Refresh();
-
-  /* virtual methods from Widget */
-  void Prepare(ContainerWindow &parent, const PixelRect &rc) override;
-  void Show(const PixelRect &rc) override;
-  void Hide() override;
-
-private:
-  /* virtual methods from DataFieldListener */
-  void OnModified(DataField &df) override;
-
-  /* virtual methods from NullBlackboardListener */
-  void OnCalculatedUpdate(const MoreData &, const DerivedInfo &) override {
-    Refresh();
-  }
-};
-
-#endif

commit 27158a752b3af74f1674f013c5ae52e3ab321372

Dialogs/Task/Manager: fix "Close" page index
Fallout from commit 17d86c64

src/Dialogs/Task/Manager/TaskManagerDialog.cpp
@@ -90,9 +90,9 @@ TaskManagerDialog::KeyPress(unsigned key_code)
       /* close the dialog immediately if nothing was modified */
       return false;
 
-    if (tab_bar->GetCurrentPage() != 4) {
+    if (tab_bar->GetCurrentPage() != 3) {
       /* switch to "close" page instead of closing the dialog */
-      tab_bar->SetCurrentPage(4);
+      tab_bar->SetCurrentPage(3);
       tab_bar->FocusCurrentWidget();
       return true;
     }