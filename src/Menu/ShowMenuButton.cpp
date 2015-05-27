12e987046a6b538ed732ba1b95dbc6deefaa243b
Max Kellermann <max@duempel.org>
2015-05-27 08:48:49 +0200

    Renderer/TextInBox, Menu/ShowMenuButton: use PtScale() for arc

--- src/Menu/ShowMenuButton.cpp
+++ src/Menu/ShowMenuButton.cpp
@@ -67,7 +67,7 @@ ShowMenuButtonRenderer::DrawButton(Canvas &canvas, const PixelRect &rc,
 
   canvas.Select(Pen(pen_width, COLOR_BLACK));
   canvas.DrawRoundRectangle(rc.left, rc.top, rc.right - 1, rc.bottom - 1,
-                            Layout::SmallScale(8), Layout::SmallScale(8));
+                            Layout::PtScale(8), Layout::PtScale(8));
 
   const RasterPoint m[] = {
     RasterPoint(rc.left + padding, rc.bottom - padding),
