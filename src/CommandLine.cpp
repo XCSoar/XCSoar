d753fa3786910af322b959d39730e6d768accd57
Max Kellermann <max@duempel.org>
2015-06-17 20:49:18 +0200

    CommandLine: simplify curly braces

--- src/CommandLine.cpp
+++ src/CommandLine.cpp
@@ -75,19 +75,16 @@ CommandLine::Parse(Args &args)
       s += 10;
       PathName convert(s);
       SetPrimaryDataPath(convert);
-    }
 #ifdef SIMULATOR_AVAILABLE
-    else if (StringIsEqual(s, "-simulator")) {
+    } else if (StringIsEqual(s, "-simulator")) {
       global_simulator_flag = true;
       sim_set_in_cmd_line_flag = true;
-    }
-    else if (StringIsEqual(s, "-fly")) {
+    } else if (StringIsEqual(s, "-fly")) {
       global_simulator_flag=false;
       sim_set_in_cmd_line_flag=true;
-    }
 #endif
 #if !defined(_WIN32_WCE)
-    else if (isdigit(s[1])) {
+    } else if (isdigit(s[1])) {
       char *p;
       width = ParseUnsigned(s + 1, &p);
       if (*p != 'x' && *p != 'X')
@@ -96,33 +93,27 @@ CommandLine::Parse(Args &args)
       height = ParseUnsigned(s + 1, &p);
       if (*p != '\0')
         args.UsageError();
-    }
-    else if (StringIsEqual(s, "-portrait")) {
+    } else if (StringIsEqual(s, "-portrait")) {
       width = 480;
       height = 640;
-    }
-    else if (StringIsEqual(s, "-square")) {
+    } else if (StringIsEqual(s, "-square")) {
       width = 480;
       height = 480;
-    }
-    else if (StringIsEqual(s, "-small")) {
+    } else if (StringIsEqual(s, "-small")) {
       width = 320;
       height = 240;
-    }
 #endif
 #ifdef HAVE_CMDLINE_FULLSCREEN
-    else if (StringIsEqual(s, "-fullscreen")) {
+    } else if (StringIsEqual(s, "-fullscreen")) {
       full_screen = true;
-    }
 #endif
 #if defined(_WIN32) && !defined(_WIN32_WCE) && !defined(__WINE__)
-    else if (StringIsEqual(s, "-console")) {
+    } else if (StringIsEqual(s, "-console")) {
       AllocConsole();
       freopen("CONOUT$", "wb", stdout);
-    }
 #endif
 #if !defined(ANDROID) && !defined(_WIN32_WCE)
-    else if (StringIsEqual(s, "-dpi=", 5)) {
+    } else if (StringIsEqual(s, "-dpi=", 5)) {
       unsigned x_dpi, y_dpi;
       char *p;
       x_dpi = ParseUnsigned(s + 5, &p);
@@ -138,19 +129,18 @@ CommandLine::Parse(Args &args)
         args.UsageError();
 
       Display::SetDPI(x_dpi, y_dpi);
-    }
 #endif
 #ifdef __APPLE__
-    else if (StringStartsWith(s, "-psn")) {
+    } else if (StringStartsWith(s, "-psn")) {
       /* The OS X launcher always supplies some process number argument.
          Just ignore it.
       */
-    }
 #endif
+    } else {
 #ifndef _WIN32
-    else
       args.UsageError();
 #endif
+    }
   }
 
 #if !defined(_WIN32_WCE)
