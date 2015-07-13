5bfcba37242367e065f223d7eaffc18c13efc1b4
Max Kellermann <max@duempel.org>
2015-07-13 20:00:17 +0200

    Waypoints: remove redundant file_num assignment
    Already initialised by constructor.

--- src/Engine/Waypoint/Waypoints.cpp
+++ src/Engine/Waypoint/Waypoints.cpp
@@ -375,7 +375,6 @@ Waypoints::GenerateTakeoffPoint(const GeoPoint& location,
   // fallback: create a takeoff point
   Waypoint to_point(location);
   to_point.elevation = terrain_alt;
-  to_point.file_num = -1;
   to_point.name = _T("(takeoff)");
   to_point.type = Waypoint::Type::OUTLANDING;
   return to_point;
