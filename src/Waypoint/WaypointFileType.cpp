bbd83e8b32992a4984fad455d275a7db5c169818
Max Kellermann <max@duempel.org>
2015-07-15 19:38:01 +0200

    Waypoint/FileType: move code to template function VerifyFormat()

--- src/Waypoint/WaypointFileType.cpp
+++ src/Waypoint/WaypointFileType.cpp
@@ -31,7 +31,19 @@ Copyright_License {
 #include "IO/TextFile.hpp"
 #include "IO/LineReader.hpp"
 
-#include <memory>
+template<class R>
+gcc_pure
+static bool
+VerifyFormat(const TCHAR *path)
+{
+  auto reader = OpenTextFile(path);
+  if (reader == nullptr)
+    return false;
+
+  bool result = R::VerifyFormat(*reader);
+  delete reader;
+  return result;
+}
 
 WaypointFileType
 DetermineWaypointFileType(const TCHAR *path)
@@ -51,17 +63,13 @@ DetermineWaypointFileType(const TCHAR *path)
 
   // If FS waypoint file -> save type and return true
   if (MatchesExtension(path, _T(".wpt"))) {
-
-    std::unique_ptr<TLineReader> reader(OpenTextFile(path));
-    if (reader && WaypointReaderFS::VerifyFormat(*reader))
+    if (VerifyFormat<WaypointReaderFS>(path))
       return WaypointFileType::FS;
 
-    reader.reset(OpenTextFile(path));
-    if (reader && WaypointReaderOzi::VerifyFormat(*reader))
+    if (VerifyFormat<WaypointReaderOzi>(path))
       return WaypointFileType::OZI_EXPLORER;
 
-    reader.reset(OpenTextFile(path));
-    if (reader && WaypointReaderCompeGPS::VerifyFormat(*reader))
+    if (VerifyFormat<WaypointReaderCompeGPS>(path))
       return WaypointFileType::COMPE_GPS;
   }
 
