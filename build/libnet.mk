192f96231c6dad6e4daf1061d03affb7317ba945
Max Kellermann <max@duempel.org>
2015-07-21 14:16:20 +0200

    Net/AllocatedSocketAddress: new sockaddr wrapper class

--- build/libnet.mk
+++ build/libnet.mk
@@ -4,6 +4,7 @@ LIBNET_SOURCES = \
 	$(SRC)/Net/State.cpp \
 	$(SRC)/Net/IPv4Address.cxx \
 	$(SRC)/Net/StaticSocketAddress.cxx \
+	$(SRC)/Net/AllocatedSocketAddress.cxx \
 	$(SRC)/Net/SocketAddress.cxx \
 	$(SRC)/Net/SocketDescriptor.cpp
 
