8335fc4747819aa78fa13b4a0d76f5eef6e12c61
Max Kellermann <max@duempel.org>
2015-07-28 10:38:27 +0200

    Android/XCSoar: move setSystemUiVisibility() call to new class
    By moving to a separate class, we avoid the verifier error, observed
    on Android 1.6.

--- android/src/XCSoar.java
+++ android/src/XCSoar.java
@@ -215,15 +215,8 @@ public class XCSoar extends Activity {
 
   private void enableImmersiveModeIfSupported() {
     // Set / Reset the System UI visibility flags for Immersive Full Screen Mode, if supported
-    if (android.os.Build.VERSION.SDK_INT >= 19) {
-      getWindow().getDecorView().setSystemUiVisibility(
-          View.SYSTEM_UI_FLAG_FULLSCREEN|
-          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
-          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
-          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
-          View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
-          View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-    }
+    if (android.os.Build.VERSION.SDK_INT >= 19)
+      ImmersiveFullScreenMode.enable(getWindow().getDecorView());
   }
 
   @Override protected void onResume() {
