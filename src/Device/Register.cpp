9350ccad7ee77f739050c9aaadc8fe4d1d45f86a
Max Kellermann <max@duempel.org>
2015-08-03 16:54:43 +0200

    Driver/KRT2: remove unmaintained buggy driver
    Deleted due to tickets #3585 (crash) and #3586 (corrupt data), see
    commit 79835289.  No maintainer was found, and the author didn't reply
    for 4 weeks.

--- src/Device/Register.cpp
+++ src/Device/Register.cpp
@@ -54,7 +54,6 @@ Copyright_License {
 #include "Device/Driver/BlueFlyVario.hpp"
 #include "Device/Driver/OpenVario.hpp"
 #include "Device/Driver/Vaulter.hpp"
-//#include "Device/Driver/KRT2.hpp"
 #include "Device/Driver/ATR833.hpp"
 #include "Util/Macros.hpp"
 #include "Util/StringAPI.hpp"
@@ -98,7 +97,6 @@ static const struct DeviceRegister *const driver_list[] = {
   &vaulter_driver,
   /* disabled due to http://bugs.xcsoar.org/ticket/3585 and
      http://bugs.xcsoar.org/ticket/3586 - scheduled for deletion */
-  //&krt2_driver,
   &atr833_driver,
   nullptr
 };
