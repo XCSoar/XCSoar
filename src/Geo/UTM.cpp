XCSoar/XCSoar

commit eae4c12ba937cd706f7a5605e27e5417eb7130bc

Geo/UTM: use "unsigned" instead of "unsigned char"
Cast to "unsigned char" adds overhead.

src/Geo/UTM.cpp
@@ -42,7 +42,7 @@ CalculateZoneLetter(const Angle latitude)
   return (index < ARRAY_SIZE(letters)) ? letters[index] : '\0';
 }
 
-static unsigned char
+static unsigned
 CalculateZoneNumber(const GeoPoint &p)
 {
   if (p.latitude <= Angle::Degrees(64) &&
@@ -68,7 +68,7 @@ CalculateZoneNumber(const GeoPoint &p)
 }
 
 static Angle
-GetCentralMeridian(unsigned char zone_number)
+GetCentralMeridian(unsigned zone_number)
 {
   return Angle::Degrees(fixed((zone_number - 1) * 6 - 180 + 3));
 }

commit 00c5815e8b08a7b822737fb05aab37caca5cf232

Geo/UTM: cast zone number to signed integer
Regression from commit eae4c12b: previously, "unsigned char" was
implicitly cast to a signed integer, but an "unsigned" stays
unsigned.  Now if we subtract 180 from that number, we easily make it
negative, which results in a very large unsigned integer.

src/Geo/UTM.cpp
@@ -73,7 +73,7 @@ gcc_const
 static Angle
 GetCentralMeridian(unsigned zone_number)
 {
-  return Angle::Degrees(fixed((zone_number - 1) * 6 - 180 + 3));
+  return Angle::Degrees(fixed(int(zone_number - 1) * 6 - 180 + 3));
 }
 
 UTM