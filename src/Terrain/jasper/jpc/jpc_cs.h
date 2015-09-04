6b33534656c4929e2229d9a8bd649ef80d1a46ec
Max Kellermann <max@duempel.org>
2015-09-04 15:00:45 +0200

    Terrain/jasper/jpc_cs: remove more writing core

--- src/Terrain/jasper/jpc/jpc_cs.h
+++ src/Terrain/jasper/jpc/jpc_cs.h
@@ -698,8 +698,10 @@ typedef struct jpc_msops_s {
 	/* Get the marker segment parameters from a stream. */
 	int (*getparms)(jpc_ms_t *ms, jpc_cstate_t *cstate, jas_stream_t *in);
 
+#ifdef ENABLE_JASPER_ENCODE
 	/* Put the marker segment parameters to a stream. */
 	int (*putparms)(jpc_ms_t *ms, jpc_cstate_t *cstate, jas_stream_t *out);
+#endif /* ENABLE_JASPER_ENCODE */
 
 } jpc_msops_t;
 
