9aeb665e0bd0fb4c4b75ee1ed8784d0ff39cf96a
Max Kellermann <max@duempel.org>
2015-09-18 00:31:40 +0200

    Event/Linux/Translate: translate digits and letters (#3611)

--- src/Event/Poll/Linux/Translate.hpp
+++ src/Event/Poll/Linux/Translate.hpp
@@ -35,6 +35,46 @@ static constexpr struct {
   /* the Kobo Touch "home" button shall open the menu */
   { KEY_HOME, KEY_MENU },
 #endif
+
+  /* TODO: the ASCII codes overlap with many of the Linux key codes,
+     and we should fix that */
+  { KEY_0, '0' },
+  { KEY_1, '1' },
+  { KEY_2, '2' },
+  { KEY_3, '3' },
+  { KEY_4, '4' },
+  { KEY_5, '5' },
+  { KEY_6, '6' },
+  { KEY_7, '7' },
+  { KEY_8, '8' },
+  { KEY_9, '9' },
+
+  { KEY_A, 'A' },
+  { KEY_B, 'B' },
+  { KEY_C, 'C' },
+  { KEY_D, 'D' },
+  { KEY_E, 'E' },
+  { KEY_F, 'F' },
+  { KEY_G, 'G' },
+  { KEY_H, 'H' },
+  { KEY_I, 'I' },
+  { KEY_J, 'J' },
+  { KEY_K, 'K' },
+  { KEY_L, 'L' },
+  { KEY_M, 'M' },
+  { KEY_N, 'N' },
+  { KEY_O, 'O' },
+  { KEY_P, 'P' },
+  { KEY_Q, 'Q' },
+  { KEY_R, 'R' },
+  { KEY_S, 'S' },
+  { KEY_T, 'T' },
+  { KEY_U, 'U' },
+  { KEY_V, 'V' },
+  { KEY_W, 'W' },
+  { KEY_X, 'X' },
+  { KEY_Y, 'Y' },
+  { KEY_Z, 'Z' },
 };
 
 gcc_const
