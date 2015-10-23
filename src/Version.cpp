1962b7b03b0aae1c953b0633b7871a298e8bae3e
Felix Hädicke <felixhaedicke@web.de>
2015-10-23 20:57:06 +0200

    Version: Use target string "OS X" for OS X
    The OS is no longer called "Mac OSX" but "OS X".

--- src/Version.cpp
+++ src/Version.cpp
@@ -40,7 +40,7 @@ Copyright_License {
   #if TARGET_OS_IPHONE
     #define TARGET "iOS"
   #else
-    #define TARGET "MacOSX"
+    #define TARGET "OSX"
   #endif
 #elif !defined(WIN32)
   #define TARGET "UNIX"
