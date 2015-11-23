fe2aa59d0a67013619611039ab2b275316731b2d
Max Kellermann <max@duempel.org>
2015-11-23 12:28:56 +0100

    build/thirdparty.py: pass $(TARGET_CPPFLAGS) to build script

--- build/thirdparty.py
+++ build/thirdparty.py
@@ -3,11 +3,11 @@
 import os, os.path
 import sys
 
-if len(sys.argv) != 9:
-    print("Usage: build.py TARGET_OUTPUT_DIR TARGET HOST_TRIPLET ARCH_CFLAGS CC CXX AR STRIP", file=sys.stderr)
+if len(sys.argv) != 10:
+    print("Usage: build.py TARGET_OUTPUT_DIR TARGET HOST_TRIPLET ARCH_CFLAGS CPPFLAGS CC CXX AR STRIP", file=sys.stderr)
     sys.exit(1)
 
-target_output_dir, target, host_triplet, arch_cflags, cc, cxx, ar, strip = sys.argv[1:]
+target_output_dir, target, host_triplet, arch_cflags, cppflags, cc, cxx, ar, strip = sys.argv[1:]
 
 # the path to the XCSoar sources
 xcsoar_path = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]) or '.', '..'))
@@ -29,7 +29,9 @@ if 'MAKEFLAGS' in os.environ:
     del os.environ['MAKEFLAGS']
 
 class Toolchain:
-    def __init__(self, tarball_path, src_path, build_path, install_prefix, arch, arch_cflags, cc, cxx, ar, strip):
+    def __init__(self, tarball_path, src_path, build_path, install_prefix,
+                 arch, arch_cflags, cppflags,
+                 cc, cxx, ar, strip):
         self.tarball_path = tarball_path
         self.src_path = src_path
         self.build_path = build_path
@@ -44,7 +46,7 @@ class Toolchain:
         common_flags = '-Os -g -ffunction-sections -fdata-sections -fvisibility=hidden ' + arch_cflags
         self.cflags = common_flags
         self.cxxflags = common_flags
-        self.cppflags = '-isystem ' + os.path.join(install_prefix, 'include') + ' -DNDEBUG'
+        self.cppflags = '-isystem ' + os.path.join(install_prefix, 'include') + ' -DNDEBUG ' + cppflags
         self.ldflags = '-L' + os.path.join(install_prefix, 'lib')
         self.libs = ''
 
@@ -77,7 +79,7 @@ else:
 
 # build the third-party libraries
 toolchain = Toolchain(tarball_path, src_path, build_path, install_prefix,
-                      host_triplet, arch_cflags, cc, cxx, ar, strip)
+                      host_triplet, arch_cflags, cppflags, cc, cxx, ar, strip)
 for x in thirdparty_libs:
     if not x.is_installed(toolchain):
         x.build(toolchain)
