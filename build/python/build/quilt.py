26f614d30df1850046aaaaedd886d90e0b304c50
Felix Hädicke <felixhaedicke@web.de>
2015-11-30 22:55:19 +0100

    build/python: do not use absolute path for quilt

--- build/python/build/quilt.py
+++ build/python/build/quilt.py
@@ -3,7 +3,7 @@ import os.path, subprocess, shutil
 def run_quilt(toolchain, cwd, patches_path, *args):
     env = dict(toolchain.env)
     env['QUILT_PATCHES'] = patches_path
-    subprocess.check_call(['/usr/bin/quilt'] + list(args), cwd=cwd, env=env)
+    subprocess.check_call(['quilt'] + list(args), cwd=cwd, env=env)
 
 def push_all(toolchain, src_path, patches_path):
     run_quilt(toolchain, src_path, patches_path, 'push', '-a')
