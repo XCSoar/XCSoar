ab5eee19dd941c92022bc234ccd4a6031e715479
Max Kellermann <max@duempel.org>
2016-01-08 12:40:15 +0100

    Makefile: move libfreetype flags to SCREEN_CPPFLAGS_INTERNAL

--- build/freetype.mk
+++ build/freetype.mk
@@ -5,6 +5,6 @@ ifeq ($(FREETYPE),y)
 $(eval $(call pkg-config-library,FREETYPE,freetype2))
 FREETYPE_CPPFLAGS := $(patsubst -I%,-isystem %,$(FREETYPE_CPPFLAGS))
 
-FREETYPE_CPPFLAGS += -DUSE_FREETYPE
+FREETYPE_FEATURE_CPPFLAGS = -DUSE_FREETYPE
 
 endif
