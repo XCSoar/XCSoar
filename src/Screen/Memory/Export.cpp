516614b82ef8828ab58e5c014e1957e307c3b0aa
Max Kellermann <max@duempel.org>
2016-01-12 09:44:05 +0100

    Screen/Memory/Export: expose pixel conversion functions

--- src/Screen/Memory/Export.cpp
+++ src/Screen/Memory/Export.cpp
@@ -23,51 +23,12 @@ Copyright_License {
 
 #include "Export.hpp"
 #include "Buffer.hpp"
-#include "Screen/PortableColor.hpp"
 
 #ifdef DITHER
 #include "../Memory/Dither.hpp"
 #endif
 
 #ifdef GREYSCALE
-
-#ifndef DITHER
-
-static uint32_t
-GreyscaleToRGB8(Luminosity8 luminosity)
-{
-  const unsigned value = luminosity.GetLuminosity();
-
-  return value | (value << 8) | (value << 16) | (value << 24);
-}
-
-static void
-CopyGreyscaleToRGB8(uint32_t *gcc_restrict dest,
-                     const Luminosity8 *gcc_restrict src,
-                     unsigned width)
-{
-  for (unsigned i = 0; i < width; ++i)
-    *dest++ = GreyscaleToRGB8(*src++);
-}
-
-static RGB565Color
-GreyscaleToRGB565(Luminosity8 luminosity)
-{
-  const unsigned value = luminosity.GetLuminosity();
-
-  return RGB565Color(value, value, value);
-}
-
-static void
-CopyGreyscaleToRGB565(RGB565Color *gcc_restrict dest,
-                      const Luminosity8 *gcc_restrict src,
-                      unsigned width)
-{
-  for (unsigned i = 0; i < width; ++i)
-    *dest++ = GreyscaleToRGB565(*src++);
-}
-
-#endif /* !DITHER */
 
 #ifdef KOBO
 
@@ -147,19 +108,6 @@ CopyFromGreyscale(
 }
 
 #else /* GREYSCALE */
-
-static RGB565Color
-ToRGB565(BGRA8Color c)
-{
-  return RGB565Color(c.Red(), c.Green(), c.Blue());
-}
-
-static void
-BGRAToRGB565(RGB565Color *dest, const BGRA8Color *src, unsigned n)
-{
-  for (unsigned i = 0; i < n; ++i)
-    dest[i] = ToRGB565(src[i]);
-}
 
 void
 CopyFromBGRA(void *_dest_pixels, unsigned _dest_pitch, unsigned dest_bpp,
--- src/Screen/Memory/Export.hpp
+++ src/Screen/Memory/Export.hpp
@@ -25,6 +25,7 @@ Copyright_License {
 #define XCSOAR_SCREEN_MEMORY_EXPORT_HPP
 
 #include "PixelTraits.hpp"
+#include "Screen/PortableColor.hpp"
 
 #ifdef DITHER
 class Dither;
@@ -32,6 +33,53 @@ class Dither;
 
 template<typename PixelTraits>
 struct ConstImageBuffer;
+
+static inline uint32_t
+GreyscaleToRGB8(Luminosity8 luminosity)
+{
+  const unsigned value = luminosity.GetLuminosity();
+
+  return value | (value << 8) | (value << 16) | (value << 24);
+}
+
+static inline void
+CopyGreyscaleToRGB8(uint32_t *gcc_restrict dest,
+                     const Luminosity8 *gcc_restrict src,
+                     unsigned width)
+{
+  for (unsigned i = 0; i < width; ++i)
+    *dest++ = GreyscaleToRGB8(*src++);
+}
+
+static inline RGB565Color
+GreyscaleToRGB565(Luminosity8 luminosity)
+{
+  const unsigned value = luminosity.GetLuminosity();
+
+  return RGB565Color(value, value, value);
+}
+
+static inline void
+CopyGreyscaleToRGB565(RGB565Color *gcc_restrict dest,
+                      const Luminosity8 *gcc_restrict src,
+                      unsigned width)
+{
+  for (unsigned i = 0; i < width; ++i)
+    *dest++ = GreyscaleToRGB565(*src++);
+}
+
+static constexpr inline RGB565Color
+ToRGB565(BGRA8Color c)
+{
+  return RGB565Color(c.Red(), c.Green(), c.Blue());
+}
+
+static inline void
+BGRAToRGB565(RGB565Color *dest, const BGRA8Color *src, unsigned n)
+{
+  for (unsigned i = 0; i < n; ++i)
+    dest[i] = ToRGB565(src[i]);
+}
 
 #ifdef GREYSCALE
 
