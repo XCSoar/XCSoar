XCSoar/XCSoar

commit 26c716344a427ae47422f94ab0f885b2d4415cf7

Route/FlatTriangleFan: simplify IsInside() formula

src/Engine/Route/FlatTriangleFan.cpp
@@ -57,7 +57,10 @@ FlatTriangleFan::IsInside(const FlatGeoPoint &p) const
     if ((i->y > p.y) == (j->y > p.y))
       continue;
 
-    if (p.x < (j->x - i->x) * (p.y - i->y) / (j->y - i->y) + i->x)
+    const FlatGeoPoint ji = *j - *i;
+    const FlatGeoPoint pi = p - *i;
+
+    if (pi.x * ji.y < ji.x * pi.y)
       inside = !inside;
   }
 

commit 90d5d3448e6b30abe392a7c05be73fdb0a794e53

Route/FlatTriangleFan: fix sign error in IsInside() formula
Fix a regression from commit 26c71634 (TRAC #3707): when I
"simplified" the formula, I multiplied it by "ji.y", which however may
need to flip the comparison symbol.  Therefore, the migration to
CrossProduct() was not legal.  This commit reverts to the old formula,
but keeps the variables "ji" and "pi".

src/Engine/Route/FlatTriangleFan.cpp
@@ -60,7 +60,7 @@ FlatTriangleFan::IsInside(FlatGeoPoint p) const
     const FlatGeoPoint ji = *j - *i;
     const FlatGeoPoint pi = p - *i;
 
-    if (pi.CrossProduct(ji) < 0)
+    if (0 < ji.x * pi.y / ji.y - pi.x)
       inside = !inside;
   }
 