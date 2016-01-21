e42c54c093dd7b85fae5b48e17afd7535e57bc11
Max Kellermann <max@duempel.org>
2016-01-21 12:28:55 +0100

    Dialogs/Settings/TaskRules: fix swapped altitude reference help text
    TRAC #3706.

--- src/Dialogs/Settings/Panels/TaskRulesConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/TaskRulesConfigPanel.cpp
@@ -88,9 +88,9 @@ TaskRulesConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
 
   static constexpr StaticEnumChoice altitude_reference_list[] = {
     { (unsigned)AltitudeReference::AGL, N_("AGL"),
-      N_("Reference is altitude above mean sea level."), },
-    { (unsigned)AltitudeReference::MSL, N_("MSL"),
       N_("Reference is the height above the task point."), },
+    { (unsigned)AltitudeReference::MSL, N_("MSL"),
+      N_("Reference is altitude above mean sea level."), },
     { 0 }
   };
 
