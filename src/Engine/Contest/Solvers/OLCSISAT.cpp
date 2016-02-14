b09caae2c8eb37c26c28237a70fac89453ea6611
Robert Kohel <robert@kohel.at>
2016-02-14 17:24:11 +0100

    update SIS-AT contest
    Update for the Austrian online glider contest.
    
    The additional zick-zack is scored now with 1 km = 0.5 p.
    
    (http://sis-at.streckenflug.at/2016/pdf/sis-at-ausschreibung.pdf page 4)

--- src/Engine/Contest/Solvers/OLCSISAT.cpp
+++ src/Engine/Contest/Solvers/OLCSISAT.cpp
@@ -34,10 +34,10 @@ OLCSISAT::OLCSISAT(const Trace &_trace)
   R = remainder distance
 
   V: 1.00 point per km
-  Z: 0.75 points per km
-  total score = V + Z*0.75
-              = V + 0.75*(S-V)
-              = (V + 3*S)/4
+  Z: 0.50 points per km
+  total score = V + Z*0.50
+              = V + 0.50*(S-V)
+              = (V + S)/2
 */
 
 ContestResult
@@ -69,7 +69,6 @@ OLCSISAT::CalculateResult(const ContestTraceVector &solution) const
 
   // S = total distance
   ContestResult result = ContestDijkstra::CalculateResult(solution);
-  result.score = ApplyHandicap((V + 3 * result.distance) / 4000);
+  result.score = ApplyHandicap((V + result.distance) / 2000);
   return result;
 }
-
