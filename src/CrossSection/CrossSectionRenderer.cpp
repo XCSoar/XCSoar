3288575cee9c3e2db5f31e7f2cc7697c18ee5851
John Wharington <jwharington@gmail.com>
2016-04-21 19:40:46 +1000

    CrossSectionRenderer: don't shade region if invalid

--- src/CrossSection/CrossSectionRenderer.cpp
+++ src/CrossSection/CrossSectionRenderer.cpp
@@ -62,17 +62,18 @@ CrossSectionRenderer::Paint(Canvas &canvas, const PixelRect rc) const
 {
   canvas.Clear(COLOR_WHITE);
   ChartRenderer chart(chart_look, canvas, rc);
+
+  if (!vec.IsValid() || !start.IsValid()) {
+    chart.DrawNoData(_("Not moving"));
+    return;
+  }
+
   DrawVerticalGradient(canvas, chart.GetChartRect(),
                        look.sky_color, look.background_color,
                        look.background_color);
 
   canvas.SetTextColor(look.text_color);
   canvas.Select(*look.grid_font);
-
-  if (!vec.IsValid() || !start.IsValid()) {
-    chart.DrawNoData(_("Not moving"));
-    return;
-  }
 
   const auto nav_altitude = gps_info.NavAltitudeAvailable()
     ? gps_info.nav_altitude
