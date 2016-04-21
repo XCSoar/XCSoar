bcc0178af20a5b1ccfa6a97bf7dab437ce867b54
John Wharington <jwharington@gmail.com>
2016-04-21 23:49:26 +1000

    VarioHistogramRenderer: better label location

--- src/Renderer/VarioHistogramRenderer.cpp
+++ src/Renderer/VarioHistogramRenderer.cpp
@@ -97,7 +97,7 @@ RenderVarioHistogram(Canvas &canvas, const PixelRect rc,
   // draw labels and other overlays
   chart.DrawYGrid(Units::ToSysVSpeed(1), 1, ChartRenderer::UnitFormat::NUMERIC);
 
-  const double tref = chart.GetXMin()*0.2+chart.GetXMax()*0.8;
+  const double tref = chart.GetXMin()*0.1+chart.GetXMax()*0.9;
   chart.DrawLabel(_T("MC"), tref, mc);
   chart.DrawLabel(_T("S cruise"), tref, s);
 
