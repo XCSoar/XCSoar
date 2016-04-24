3c3051daf84a965d06a74f0338b744ac65193df1
John Wharington <jwharington@gmail.com>
2016-04-25 00:23:54 +1000

    UnitsStore: Australian units for weight are kg

--- src/Units/UnitsStore.cpp
+++ src/Units/UnitsStore.cpp
@@ -79,8 +79,8 @@ static constexpr UnitStoreItem Presets[] =
     Unit::KNOTS,
     Unit::KILOMETER_PER_HOUR,
     Unit::HECTOPASCAL,
-    Unit::LB_PER_FT2,
-    Unit::LB,
+    Unit::KG_PER_M2,
+    Unit::KG,
   } }
 };
 
