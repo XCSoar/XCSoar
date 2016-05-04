cd8f9d4ed98aaf8b5ef640dab84affaf9cb05108
John Wharington <jwharington@gmail.com>
2016-05-05 09:40:04 +1000

    GlueMapWindowOverlays: slightly wider thermal band on-screen
    With the higher resolution and more information in the widget now, it
    should be a little larger so to improve readability.

--- src/MapWindow/GlueMapWindowOverlays.cpp
+++ src/MapWindow/GlueMapWindowOverlays.cpp
@@ -389,7 +389,7 @@ GlueMapWindow::DrawThermalBand(Canvas &canvas, const PixelRect &rc) const
 
   PixelRect tb_rect;
   tb_rect.left = rc.left;
-  tb_rect.right = rc.left+Layout::Scale(20);
+  tb_rect.right = rc.left+Layout::Scale(25);
   tb_rect.top = Layout::Scale(2);
   tb_rect.bottom = (rc.bottom-rc.top)/5 - Layout::Scale(2);
 
