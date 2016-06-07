XCSoar/XCSoar

commit e14193d36d01a6cbfff4a49e3404c0054643b630

Weather/Settings: move username/password to struct Credentials
Prepare for adding FTP credentials.

src/Dialogs/Weather/PCMetDialog.cpp
@@ -160,7 +160,7 @@ Widget *
 CreatePCMetWidget()
 {
   const auto &settings = CommonInterface::GetComputerSettings().weather.pcmet;
-  if (settings.username.empty() || settings.password.empty())
+  if (settings.www_credentials.IsDefined())
     return new LargeTextWidget(UIGlobals::GetDialogLook(),
                                _T("No account was configured."));
 

src/Dialogs/Settings/Panels/WeatherConfigPanel.cpp
@@ -56,8 +56,10 @@ WeatherConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
 
   RowFormWidget::Prepare(parent, rc);
 
-  AddText(_T("pc_met Username"), _T(""), settings.pcmet.username);
-  AddPassword(_T("pc_met Password"), _T(""), settings.pcmet.password);
+  AddText(_T("pc_met Username"), _T(""),
+          settings.pcmet.www_credentials.username);
+  AddPassword(_T("pc_met Password"), _T(""),
+              settings.pcmet.www_credentials.password);
 }
 
 bool
@@ -69,10 +71,10 @@ WeatherConfigPanel::Save(bool &_changed)
 
 #ifdef HAVE_PCMET
   changed |= SaveValue(PCMET_USER, ProfileKeys::PCMetUsername,
-                       settings.pcmet.username);
+                       settings.pcmet.www_credentials.username);
 
   changed |= SaveValue(PCMET_PASSWORD, ProfileKeys::PCMetPassword,
-                       settings.pcmet.password);
+                       settings.pcmet.www_credentials.password);
 #endif
 
   _changed |= changed;

src/Profile/WeatherProfile.cpp
@@ -30,8 +30,8 @@ Copyright_License {
 
 namespace Profile {
   static void Load(const ProfileMap &map, PCMetSettings &settings) {
-    map.Get(ProfileKeys::PCMetUsername, settings.username);
-    map.Get(ProfileKeys::PCMetPassword, settings.password);
+    map.Get(ProfileKeys::PCMetUsername, settings.www_credentials.username);
+    map.Get(ProfileKeys::PCMetPassword, settings.www_credentials.password);
   }
 }
 

src/Weather/PCMet.cpp
@@ -103,8 +103,8 @@ PCMet::DownloadLatestImage(const char *type, const char *area,
                            const PCMetSettings &settings,
                            JobRunner &runner)
 {
-  const WideToUTF8Converter username(settings.username);
-  const WideToUTF8Converter password(settings.password);
+  const WideToUTF8Converter username(settings.www_credentials.username);
+  const WideToUTF8Converter password(settings.www_credentials.password);
 
   char url[256];
   snprintf(url, sizeof(url),

src/Weather/Settings.hpp
@@ -31,12 +31,27 @@ Copyright_License {
 #include "Util/StaticString.hxx"
 
 struct PCMetSettings {
-  StaticString<64> username;
-  StaticString<64> password;
+  struct Credentials {
+    StaticString<64> username;
+    StaticString<64> password;
+
+    bool IsDefined() const {
+      return !username.empty() && !password.empty();
+    }
+
+    void SetDefaults() {
+      username.clear();
+      password.clear();
+    }
+  };
+
+  /**
+   * Credentials for https://www.flugwetter.de/
+   */
+  Credentials www_credentials;
 
   void SetDefaults() {
-    username.clear();
-    password.clear();
+    www_credentials.SetDefaults();
   }
 };
 

commit 18a6d99fab1d17e99a79e1720aac1e2c4a06b257

Dialogs/Weather/PCMet: fix credentials breakage
Regression from commit e14193d3

src/Dialogs/Weather/PCMetDialog.cpp
@@ -160,7 +160,7 @@ Widget *
 CreatePCMetWidget()
 {
   const auto &settings = CommonInterface::GetComputerSettings().weather.pcmet;
-  if (settings.www_credentials.IsDefined())
+  if (!settings.www_credentials.IsDefined())
     return new LargeTextWidget(UIGlobals::GetDialogLook(),
                                _T("No account was configured."));
 