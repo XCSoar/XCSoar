XCSoar/XCSoar

commit 34e856cbb40beb0ea3802d84bd2d0e95f38550d7

Profile/Weather: fix typo

src/Profile/WeatherProfile.cpp
@@ -31,7 +31,7 @@ Copyright_License {
 namespace Profile {
   static void Load(const ProfileMap &map, PCMetSettings &settings) {
     map.Get(ProfileKeys::PCMetUsername, settings.www_credentials.username);
-    map.Get(ProfileKeys::PCMetPassword, settings.www_credentials.password);
+    map.Get(ProfileKeys::PCMetPassword, settings.ftp_credentials.password);
   }
 }
 

commit 20e9479c5131d614a5f8f4b645e5e065b3335d1a

Profile/WeatherProfile: fix www/ftp breakage
Broken by 34e856cb which tried to fix 94a75882.  Ouch.

src/Profile/WeatherProfile.cpp
@@ -31,7 +31,9 @@ Copyright_License {
 namespace Profile {
   static void Load(const ProfileMap &map, PCMetSettings &settings) {
     map.Get(ProfileKeys::PCMetUsername, settings.www_credentials.username);
-    map.Get(ProfileKeys::PCMetPassword, settings.ftp_credentials.password);
+    map.Get(ProfileKeys::PCMetPassword, settings.www_credentials.password);
+    map.Get(ProfileKeys::PCMetFtpUsername, settings.ftp_credentials.username);
+    map.Get(ProfileKeys::PCMetFtpPassword, settings.ftp_credentials.password);
   }
 }
 