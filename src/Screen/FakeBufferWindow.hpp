b1e96c145c022193d04e5c5efc35a503f230eb75
Max Kellermann <max@duempel.org>
2016-06-12 11:00:05 +0200

    Screen/AntiFlickerWindow: move code to FakeBufferWindow
    Prepare for reusing it.

--- src/Screen/FakeBufferWindow.hpp
+++ src/Screen/FakeBufferWindow.hpp
@@ -21,35 +21,30 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_SCREEN_ANTI_FLICKER_WINDOW_HXX
-#define XCSOAR_SCREEN_ANTI_FLICKER_WINDOW_HXX
+#ifndef XCSOAR_SCREEN_FAKE_BUFFER_WINDOW_HXX
+#define XCSOAR_SCREEN_FAKE_BUFFER_WINDOW_HXX
 
-#if defined(ENABLE_OPENGL) || defined(USE_MEMORY_CANVAS)
-
-#include "FakeBufferWindow.hpp"
+#include "PaintWindow.hpp"
 
 /**
- * A #PaintWindow implementation that avoids flickering.  Some
- * platforms such as Windows draw directly to the screen, which may
- * expose the window before drawing has finished.  On these,
- * #AntiFlickerWindow will be buffered.  On OpenGL/SDL, which both have
- * full-screen double-buffering, this class is a simple #PaintWindow
- * without extra buffering.
- *
- * Note that this class is not supposed to reduce the number of
- * redraws when this is expensive.  Use it only when flicker avoidance
- * is the goal.
+ * Emulation of the #BufferWindow API without actually buffering.
  */
-class AntiFlickerWindow : public FakeBufferWindow {
+class FakeBufferWindow : public PaintWindow {
+protected:
+  /**
+   * Determines whether this class maintains a persistent buffer which
+   * allows incremental drawing in each frame.
+   */
+  static constexpr bool IsPersistent() {
+    return false;
+  }
+
+  virtual void OnPaintBuffer(Canvas &canvas) = 0;
+
+  /* virtual methods from class Window */
+  void OnPaint(Canvas &canvas) override {
+    OnPaintBuffer(canvas);
+  }
 };
-
-#else
-
-#include "BufferWindow.hpp"
-
-class AntiFlickerWindow : public BufferWindow {
-};
-
-#endif
 
 #endif
