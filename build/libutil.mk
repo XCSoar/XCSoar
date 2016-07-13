b614f69fad05b23aa110cfc0d140592522fbcf97
Max Kellermann <max@duempel.org>
2016-07-13 11:13:10 +0200

    Util/TruncateString: new library for truncating TCHAR strings

--- build/libutil.mk
+++ build/libutil.mk
@@ -6,6 +6,7 @@ UTIL_SOURCES = \
 	$(UTIL_SRC_DIR)/CRC.cpp \
 	$(UTIL_SRC_DIR)/tstring.cpp \
 	$(UTIL_SRC_DIR)/UTF8.cpp \
+	$(UTIL_SRC_DIR)/TruncateString.cpp \
 	$(UTIL_SRC_DIR)/EscapeBackslash.cpp \
 	$(UTIL_SRC_DIR)/ExtractParameters.cpp \
 	$(UTIL_SRC_DIR)/ConvertString.cpp \
