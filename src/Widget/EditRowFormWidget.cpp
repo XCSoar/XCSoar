67df50f966ad0a49be5f47bb0576acb708441e84
Max Kellermann <max@duempel.org>
2016-07-15 14:40:32 +0200

    Widget/RowFormWidget: use CopyTruncateString() instead of CopyString()

--- src/Widget/EditRowFormWidget.cpp
+++ src/Widget/EditRowFormWidget.cpp
@@ -37,6 +37,7 @@ Copyright_License {
 #include "Language/Language.hpp"
 #include "Math/Angle.hpp"
 #include "Util/StringAPI.hpp"
+#include "Util/TruncateString.hpp"
 
 #include <assert.h>
 
@@ -467,6 +468,6 @@ RowFormWidget::SaveValue(unsigned i, TCHAR *string, size_t max_size) const
   if (StringIsEqual(string, new_value))
     return false;
 
-  CopyString(string, new_value, max_size);
+  CopyTruncateString(string, max_size, new_value);
   return true;
 }
