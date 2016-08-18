1a67f405e154ef885b9cc61a0412dda31d623254
Max Kellermann <max.kellermann@gmail.com>
2016-08-18 20:30:16 +0200

    Driver/CAI302: use std::unique_ptr

--- src/Device/Driver/CAI302/Logger.cpp
+++ src/Device/Driver/CAI302/Logger.cpp
@@ -29,6 +29,8 @@ Copyright_License {
 #include "OS/Path.hpp"
 #include "IO/BinaryWriter.hpp"
 
+#include <memory>
+
 #include <stdlib.h>
 
 static void
@@ -123,36 +125,31 @@ DownloadFlightInner(Port &port, const RecordedFlightInfo &flight,
   env.SetProgressRange(num_blocks);
 
   unsigned allocated_size = sizeof(CAI302::FileData) + bytes_per_block;
-  void *allocated = malloc(allocated_size);
-  CAI302::FileData *header = (CAI302::FileData *)allocated;
+  std::unique_ptr<uint8_t> allocated(new uint8_t[allocated_size]);
+  // TODO: alignment?
+  CAI302::FileData *header = (CAI302::FileData *)(void *)allocated.get();
   void *data = header + 1;
 
   unsigned current_block = 0;
   unsigned valid_bytes;
   do {
     int i = UploadFileData(port, true, header, allocated_size, env);
-    if (i < (int)sizeof(*header)) {
-      free(allocated);
+    if (i < (int)sizeof(*header))
       return false;
-    }
 
     i -= sizeof(*header);
 
     valid_bytes = FromBE16(header->valid_bytes);
-    if ((unsigned)i < valid_bytes) {
-      free(allocated);
+    if ((unsigned)i < valid_bytes)
       return false;
-    }
 
-    if (!writer.Write(data, 1, valid_bytes)) {
-      free(allocated);
+    if (!writer.Write(data, 1, valid_bytes))
       return false;
-    }
 
     env.SetProgressPosition(current_block++);
   } while (valid_bytes == bytes_per_block);
 
-  free(allocated);
+  allocated.reset();
 
   CAI302::FileSignatureASCII signature;
   if (!CAI302::UploadFileSignatureASCII(port, signature, env))
