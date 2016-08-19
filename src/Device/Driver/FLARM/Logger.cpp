3b2871f6b66c05a3be39a3936433a25ae3f26dc7
Max Kellermann <max.kellermann@gmail.com>
2016-08-19 14:26:40 +0200

    Driver/FLARM: use class FileOutputStream instead of BinaryWriter

--- src/Device/Driver/FLARM/Logger.cpp
+++ src/Device/Driver/FLARM/Logger.cpp
@@ -23,8 +23,8 @@
 
 #include "Device.hpp"
 #include "Device/RecordedFlight.hpp"
-#include "IO/BinaryWriter.hpp"
-#include "OS/FileUtil.hpp"
+#include "IO/FileOutputStream.hxx"
+#include "IO/BufferedOutputStream.hxx"
 #include "OS/Path.hpp"
 #include "Operation/Operation.hpp"
 
@@ -304,8 +304,10 @@ FlarmDevice::ReadFlightList(RecordedFlightList &flight_list,
 bool
 FlarmDevice::DownloadFlight(Path path, OperationEnvironment &env)
 {
-  BinaryWriter writer(path);
-  if (writer.HasError() || env.IsCancelled())
+  FileOutputStream fos(path);
+  BufferedOutputStream os(fos);
+
+  if (env.IsCancelled())
     return false;
 
   env.SetProgressRange(100);
@@ -342,11 +344,14 @@ FlarmDevice::DownloadFlight(Path path, OperationEnvironment &env)
 
     // Read IGC data
     const char *igc_data = (const char *)data.begin() + 3;
-    writer.Write(igc_data, sizeof(igc_data[0]), length);
+    os.Write(igc_data, length);
 
     if (is_last_packet)
       break;
   }
+
+  os.Flush();
+  fos.Commit();
 
   return true;
 }
@@ -365,11 +370,15 @@ FlarmDevice::DownloadFlight(const RecordedFlightInfo &flight,
   if (ack_result != FLARM::MT_ACK || env.IsCancelled())
     return false;
 
-  if (DownloadFlight(path, env))
-    return true;
+  try {
+    if (DownloadFlight(path, env))
+      return true;
+  } catch (...) {
+    mode = Mode::UNKNOWN;
+    throw;
+  }
 
   mode = Mode::UNKNOWN;
-  File::Delete(path);
 
   return false;
 }
