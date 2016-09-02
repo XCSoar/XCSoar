1bfcb1b53c46d7e87c5005e49a4667b66a12439e
Max Kellermann <max.kellermann@gmail.com>
2016-09-02 11:18:27 +0200

    IO/StdioOutputStream: import another IO class from MPD

---
+++ src/IO/StdioOutputStream.hxx
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003-2016 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MPD_STDIO_OUTPUT_STREAM_HXX
+#define MPD_STDIO_OUTPUT_STREAM_HXX
+
+#include "OutputStream.hxx"
+
+#include <stdio.h>
+
+class StdioOutputStream final : public OutputStream {
+	FILE *const file;
+
+public:
+	StdioOutputStream(FILE *_file):file(_file) {}
+
+	/* virtual methods from class OutputStream */
+	void Write(const void *data, size_t size) override {
+		fwrite(data, 1, size, file);
+
+		/* this class is debug-only and ignores errors */
+	}
+};
+
+#endif
