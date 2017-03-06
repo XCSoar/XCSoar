e03b8478088644af12a44e9db87fee56619b0ab9
Max Kellermann <max.kellermann@gmail.com>
2017-03-06 08:52:11 +0100

    build/python: move class AutotoolsProject to library

--- kobo/build.py
+++ kobo/build.py
@@ -17,6 +17,7 @@ xcsoar_path = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]) or '.', 
 sys.path[0] = os.path.join(xcsoar_path, 'build/python')
 from build.project import Project
 from build.zlib import ZlibProject
+from build.autotools import AutotoolsProject
 
 output_path = os.path.abspath('output')
 tarball_path = os.path.join(output_path, 'download')
@@ -71,54 +72,6 @@ class Toolchain:
         # redirect pkg-config to use our root directory instead of the
         # default one on the build host
         self.env['PKG_CONFIG_LIBDIR'] = os.path.join(install_prefix, 'lib/pkgconfig')
-
-class AutotoolsProject(Project):
-    def __init__(self, url, md5, installed, configure_args=[],
-                 autogen=False,
-                 cppflags='',
-                 **kwargs):
-        Project.__init__(self, url, md5, installed, **kwargs)
-        self.configure_args = configure_args
-        self.autogen = autogen
-        self.cppflags = cppflags
-
-    def configure(self, toolchain):
-        src = self.unpack(toolchain)
-        if self.autogen:
-            subprocess.check_call(['/usr/bin/aclocal'], cwd=src)
-            subprocess.check_call(['/usr/bin/automake', '--add-missing', '--force-missing', '--foreign'], cwd=src)
-            subprocess.check_call(['/usr/bin/autoconf'], cwd=src)
-            subprocess.check_call(['/usr/bin/libtoolize', '--force'], cwd=src)
-
-        build = self.make_build_path(toolchain)
-
-        configure = [
-            os.path.join(src, 'configure'),
-            'CC=' + toolchain.cc,
-            'CXX=' + toolchain.cxx,
-            'CFLAGS=' + toolchain.cflags,
-            'CXXFLAGS=' + toolchain.cxxflags,
-            'CPPFLAGS=' + toolchain.cppflags + ' ' + self.cppflags,
-            'LDFLAGS=' + toolchain.ldflags,
-            'LIBS=' + toolchain.libs,
-            'AR=' + toolchain.ar,
-            'RANLIB=' + toolchain.ranlib,
-            'STRIP=' + toolchain.strip,
-            '--host=' + toolchain.arch,
-            '--prefix=' + toolchain.install_prefix,
-            '--enable-silent-rules',
-        ] + self.configure_args
-
-        subprocess.check_call(configure, cwd=build, env=toolchain.env)
-        return build
-
-    def build(self, toolchain):
-        build = self.configure(toolchain)
-
-        subprocess.check_call(['/usr/bin/make', '--quiet', '-j12'],
-                              cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', 'install'],
-                              cwd=build, env=toolchain.env)
 
 class FreeTypeProject(AutotoolsProject):
     def configure(self, toolchain):
