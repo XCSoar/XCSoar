79663255b074baa30ebe3fadbdf1ba80739fa6a8
Max Kellermann <max.kellermann@gmail.com>
2017-08-06 19:52:59 +0200

    Makefile: use Android NDK r15c

--- build/targets.mk
+++ build/targets.mk
@@ -435,7 +435,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r14b
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r15c
 
   ANDROID_SDK_PLATFORM = android-22
   ANDROID_NDK_PLATFORM = android-19
