XCSoar/XCSoar

commit 6f981ceb7a3a7fc5cbbb58b32211ffdea7f8cc40

Route/Polars: clip ReachIntercept() results
See code comment.

src/Engine/Route/RoutePolars.cpp
@@ -256,5 +256,22 @@ RoutePolars::ReachIntercept(const int index, const AFlatGeoPoint &flat_origin,
   if (!p.IsValid())
     return flat_dest;
 
-  return proj.ProjectInteger(p);
+  FlatGeoPoint fp = proj.ProjectInteger(p);
+
+  /* when there's an obstacle very nearby and our intersection is
+     right next to our origin, the intersection may be deformed due to
+     terrain raster rounding errors; the following code applies
+     clipping to avoid degenerate polygons */
+  FlatGeoPoint delta1 = flat_dest - (FlatGeoPoint)flat_origin;
+  FlatGeoPoint delta2 = fp - (FlatGeoPoint)flat_origin;
+
+  if (delta1.x * delta2.x < 0)
+    /* intersection is on the wrong horizontal side */
+    fp.x = flat_origin.x;
+
+  if (delta1.y * delta2.y < 0)
+    /* intersection is on the wrong vertical side */
+    fp.x = flat_origin.y;
+
+  return fp;
 }

commit 1981faa186d7782a1b3ee5bdccb750804f77f959

Route/Polars: fix typo in clipping code
Fixes Coverity issue 167044, caused by commit 6f981ceb7a3a

src/Engine/Route/RoutePolars.cpp
@@ -276,7 +276,7 @@ RoutePolars::ReachIntercept(const int index, const AFlatGeoPoint &flat_origin,
 
   if (delta1.y * delta2.y < 0)
     /* intersection is on the wrong vertical side */
-    fp.x = flat_origin.y;
+    fp.y = flat_origin.y;
 
   return fp;
 }