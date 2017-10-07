2aa688b603c5a40357d83102b52b49a1afc9200d
Max Kellermann <max.kellermann@gmail.com>
2017-10-07 10:41:48 +0200

    Terrain/jasper/jpc_dec: simplify formula in calcstepsizes()

--- src/Terrain/jasper/jpc/jpc_dec.c
+++ src/Terrain/jasper/jpc/jpc_dec.c
@@ -1806,8 +1806,7 @@ static int calcstepsizes(uint_fast16_t refstepsize, int numrlvls,
 	numbands = 3 * numrlvls - 2;
 	for (bandno = 0; bandno < numbands; ++bandno) {
 //jas_eprintf("DEBUG %d %d %d %d %d\n", bandno, expn, numrlvls, bandno, ((numrlvls - 1) - (numrlvls - 1 - ((bandno > 0) ? ((bandno + 2) / 3) : (0)))));
-		stepsizes[bandno] = JPC_QCX_MANT(mant) | JPC_QCX_EXPN(expn +
-		  (numrlvls - 1) - (numrlvls - 1 - ((bandno > 0) ? ((bandno + 2) / 3) : (0))));
+		stepsizes[bandno] = JPC_QCX_MANT(mant) | JPC_QCX_EXPN(expn + (bandno + 2) / 3);
 	}
 	return 0;
 }
