9c9157a13905a3ce84a55e4bee2cd18c9386a3e2
Max Kellermann <max.kellermann@gmail.com>
2018-08-21 10:57:21 +0200

    Net/SocketAddress: add GetSteadyPart()

--- src/Net/SocketAddress.cxx
+++ src/Net/SocketAddress.cxx
@@ -38,6 +38,14 @@
 #include <sys/un.h>
 #endif
 
+#ifdef HAVE_TCP
+#ifdef _WIN32
+#include <ws2tcpip.h>
+#else
+#include <netinet/in.h>
+#endif
+#endif
+
 bool
 SocketAddress::operator==(SocketAddress other) const noexcept
 {
@@ -71,7 +79,7 @@ SocketAddress::GetLocalRaw() const noexcept
 bool
 SocketAddress::IsV6Any() const noexcept
 {
-    return GetFamily() == AF_INET6 && IPv6Address(*this).IsAny();
+	return GetFamily() == AF_INET6 && IPv6Address(*this).IsAny();
 }
 
 bool
@@ -98,4 +106,41 @@ SocketAddress::GetPort() const noexcept
 	}
 }
 
+static constexpr ConstBuffer<void>
+GetSteadyPart(const struct sockaddr_in &address) noexcept
+{
+	return {&address.sin_addr, sizeof(address.sin_addr)};
+}
+
+static constexpr ConstBuffer<void>
+GetSteadyPart(const struct sockaddr_in6 &address) noexcept
+{
+	return {&address.sin6_addr, sizeof(address.sin6_addr)};
+}
+
 #endif
+
+ConstBuffer<void>
+SocketAddress::GetSteadyPart() const noexcept
+{
+	if (IsNull())
+		return nullptr;
+
+	switch (GetFamily()) {
+#ifdef HAVE_UN
+	case AF_LOCAL:
+		return GetLocalRaw().ToVoid();
+#endif
+
+#ifdef HAVE_TCP
+	case AF_INET:
+		return ::GetSteadyPart(*(const struct sockaddr_in *)(const void *)GetAddress());
+
+	case AF_INET6:
+		return ::GetSteadyPart(*(const struct sockaddr_in6 *)(const void *)GetAddress());
+#endif
+
+	default:
+		return nullptr;
+	}
+}
