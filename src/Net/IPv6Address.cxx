e9ab172bc8259258a1f011af3742e35fc46f2b20
Max Kellermann <max.kellermann@gmail.com>
2018-08-21 20:20:08 +0200

    net/IPv6Address: rewrite constructor code

--- src/Net/IPv6Address.cxx
+++ src/Net/IPv6Address.cxx
@@ -53,3 +53,31 @@ IPv6Address::IsAny() const noexcept
 	return memcmp(&address.sin6_addr,
 		      &in6addr_any, sizeof(in6addr_any)) == 0;
 }
+
+template<typename T>
+static void
+BitwiseAndT(T *dest, const T *a, const T *b, size_t n)
+{
+	while (n-- > 0)
+		*dest++ = *a++ & *b++;
+}
+
+static void
+BitwiseAnd32(void *dest, const void *a, const void *b, size_t n)
+{
+	using value_type = uint32_t;
+	using pointer = value_type *;
+	using const_pointer = const value_type *;
+
+	BitwiseAndT(pointer(dest), const_pointer(a), const_pointer(b),
+		    n / sizeof(value_type));
+}
+
+IPv6Address
+IPv6Address::operator&(const IPv6Address &other) const
+{
+	IPv6Address result;
+	BitwiseAnd32(&result, this, &other,
+		     sizeof(result));
+	return result;
+}
