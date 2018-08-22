53cceed0564becd93f1b93032d172f46684eb144
Max Kellermann <max.kellermann@gmail.com>
2018-08-22 13:43:31 +0200

    build/vali.mk: remove duplicate OS/FileDescriptor.cpp, already in libos.a

--- build/vali.mk
+++ build/vali.mk
@@ -1,7 +1,6 @@
 # Rules for VALI-XCS.exe, the non-interactive G record validation tool
 
 VALI_XCS_SOURCES = \
-	$(SRC)/OS/FileDescriptor.cpp \
 	$(SRC)/Logger/GRecord.cpp \
 	$(SRC)/Logger/MD5.cpp \
 	$(SRC)/Version.cpp \
