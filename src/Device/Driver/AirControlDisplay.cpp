b94947e05bcc08fcaa5dd367479ff13accbb9284
Stefan Schumann <stefan.schumann@posteo.de>
2019-02-09 20:34:45 +0100

    provide GPS data to ACD
    Air Control Display can make use of GPS data (e.g. for the NEAREST
    function of the radio)

--- src/Device/Driver/AirControlDisplay.cpp
+++ src/Device/Driver/AirControlDisplay.cpp
@@ -27,6 +27,8 @@ Copyright_License {
 #include "NMEA/Info.hpp"
 #include "NMEA/InputLine.hpp"
 #include "NMEA/Checksum.hpp"
+#include "NMEA/MoreData.hpp"
+#include "Operation/Operation.hpp"
 #include "Atmosphere/Pressure.hpp"
 #include "RadioFrequency.hpp"
 #include "Units/System.hpp"
@@ -79,6 +81,8 @@ public:
   bool PutStandbyFrequency(RadioFrequency frequency,
                            const TCHAR *name,
                            OperationEnvironment &env) override;
+  void OnCalculatedUpdate(const MoreData &basic,
+                          const DerivedInfo &calculated) override;
 };
 
 bool
@@ -121,6 +125,226 @@ ACDDevice::ParseNMEA(const char *_line, NMEAInfo &info)
     return ParsePAAVS(line, info);
   else
     return false;
+}
+
+static void
+FormatLatitude(char *buffer, size_t buffer_size, Angle latitude )
+{
+  // Calculate Latitude sign
+  char sign = latitude.IsNegative() ? 'S' : 'N';
+
+  double mlat(latitude.AbsoluteDegrees());
+
+  int dd = (int)mlat;
+  // Calculate minutes
+  double mins = (mlat - dd) * 60.0;
+
+  // Save the string to the buffer
+  snprintf(buffer, buffer_size, "%02d%06.3f,%c", dd, mins, sign);
+}
+
+static void
+FormatLongitude(char *buffer, size_t buffer_size, Angle longitude)
+{
+  // Calculate Longitude sign
+  char sign = longitude.IsNegative() ? 'W' : 'E';
+
+  double mlong(longitude.AbsoluteDegrees());
+
+  int dd = (int)mlong;
+  // Calculate minutes
+  double mins = (mlong - dd) * 60.0;
+  // Save the string to the buffer
+  snprintf(buffer, buffer_size, "%02d%06.3f,%c", dd, mins, sign);
+}
+
+/*
+ * $GPRMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,xxxx,x.x,a,m,*hh
+ *
+ * Field Number:
+ *  1) UTC Time
+ *  2) Status, V=Navigation receiver warning A=Valid
+ *  3) Latitude
+ *  4) N or S
+ *  5) Longitude
+ *  6) E or W
+ *  7) Speed over ground, knots
+ *  8) Track made good, degrees true
+ *  9) Date, ddmmyy
+ * 10) Magnetic Variation, degrees
+ * 11) E or W
+ * 12) FAA mode indicator (NMEA 2.3 and later)
+ * 13) Checksum
+ */
+static bool
+FormatGPRMC(char *buffer, size_t buffer_size, const MoreData &info)
+{
+  char lat_buffer[20];
+  char long_buffer[20];
+
+  const GeoPoint location = info.location_available
+    ? info.location
+    : GeoPoint::Zero();
+
+  FormatLatitude(lat_buffer, sizeof(lat_buffer), location.latitude);
+  FormatLongitude(long_buffer, sizeof(long_buffer), location.longitude);
+
+  const BrokenDateTime now = info.time_available &&
+    info.date_time_utc.IsDatePlausible()
+    ? info.date_time_utc
+    : BrokenDateTime::NowUTC();
+
+  snprintf(buffer, buffer_size,
+           "GPRMC,%02u%02u%02u,%c,%s,%s,%05.1f,%05.1f,%02u%02u%02u,,",
+           now.hour, now.minute, now.second,
+           info.location.IsValid() ? 'A' : 'V',
+           lat_buffer,
+           long_buffer,
+           (double)Units::ToUserUnit(info.ground_speed, Unit::KNOTS),
+           (double)info.track.Degrees(),
+           now.day, now.month, now.year % 100);
+
+  return true;
+}
+
+/*
+ * $GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+ *
+ * Field Number:
+ *  1) Universal Time Coordinated (UTC)
+ *  2) Latitude
+ *  3) N or S (North or South)
+ *  4) Longitude
+ *  5) E or W (East or West)
+ *  6) GPS Quality Indicator,
+ *     0 - fix not available,
+ *     1 - GPS fix,
+ *     2 - Differential GPS fix
+ *     (values above 2 are 2.3 features)
+ *     3 = PPS fix
+ *     4 = Real Time Kinematic
+ *     5 = Float RTK
+ *     6 = estimated (dead reckoning)
+ *     7 = Manual input mode
+ *     8 = Simulation mode
+ *  7) Number of satellites in view, 00 - 12
+ *  8) Horizontal Dilution of precision (meters)
+ *  9) Antenna Altitude above/below mean-sea-level (geoid) (in meters)
+ * 10) Units of antenna altitude, meters
+ * 11) Geoidal separation, the difference between the WGS-84 earth
+ *     ellipsoid and mean-sea-level (geoid), "-" means mean-sea-level
+ *     below ellipsoid
+ * 12) Units of geoidal separation, meters
+ * 13) Age of differential GPS data, time in seconds since last SC104
+ *     type 1 or 9 update, null field when DGPS is not used
+ * 14) Differential reference station ID, 0000-1023
+ * 15) Checksum
+ */
+static bool
+FormatGPGGA(char *buffer, size_t buffer_size, const MoreData &info)
+{
+  char lat_buffer[20];
+  char long_buffer[20];
+
+  const GeoPoint location = info.location_available
+    ? info.location
+    : GeoPoint::Zero();
+
+  FormatLatitude(lat_buffer, sizeof(lat_buffer), location.latitude);
+  FormatLongitude(long_buffer, sizeof(long_buffer), location.longitude);
+
+  const BrokenDateTime now = info.time_available &&
+    info.date_time_utc.IsDatePlausible()
+    ? info.date_time_utc
+    : BrokenDateTime::NowUTC();
+
+  snprintf(buffer, buffer_size,
+           "GPGGA,%02u%02u%02u,%c,%s,%s,%u,%2u,%.1f,%.1f,%c,%.1f,%c,,",
+           now.hour, now.minute, now.second,
+           info.location.IsValid() ? 'A' : 'V',
+           lat_buffer,
+           long_buffer,
+           (int)info.gps.fix_quality,
+           info.gps.satellites_used,
+           info.gps.hdop,
+           info.gps_altitude,
+           'M',
+           info.gps_altitude,
+           'M');
+
+  return true;
+}
+
+/*
+ * $GPGSA,a,a,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x.x,x.x,x.x*hh
+ *
+ * Field Number:
+ *  1) Selection mode
+ *         M=Manual, forced to operate in 2D or 3D
+ *         A=Automatic, 3D/2D
+ *  2) Mode (1 = no fix, 2 = 2D fix, 3 = 3D fix)
+ *  3) ID of 1st satellite used for fix
+ *  4) ID of 2nd satellite used for fix
+ *  ...
+ *  14) ID of 12th satellite used for fix
+ *  15) PDOP
+ *  16) HDOP
+ *  17) VDOP
+ *  18) checksum
+ */
+static bool
+FormatGPGSA(char *buffer, size_t buffer_size, const MoreData &info)
+{
+  // don't format GPGSA string if no sat id's are available
+  if(!info.gps.satellite_ids_available)
+    return false;
+
+  int gps_status;
+
+  if (!info.alive || !info.location_available)
+    gps_status = 1;
+  else if (!info.gps_altitude_available)
+    gps_status = 2;
+  else
+    gps_status = 3;
+
+  snprintf(buffer, buffer_size,
+           "GPGSA,A,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%.1f,%.1f,%.1f",
+           gps_status,
+           info.gps.satellite_ids[0],
+           info.gps.satellite_ids[1],
+           info.gps.satellite_ids[2],
+           info.gps.satellite_ids[3],
+           info.gps.satellite_ids[4],
+           info.gps.satellite_ids[5],
+           info.gps.satellite_ids[6],
+           info.gps.satellite_ids[7],
+           info.gps.satellite_ids[8],
+           info.gps.satellite_ids[9],
+           info.gps.satellite_ids[10],
+           info.gps.satellite_ids[11],
+           info.gps.pdop,
+           info.gps.hdop,
+           info.gps.vdop);
+
+  return true;
+}
+
+void
+ACDDevice::OnCalculatedUpdate(const MoreData &basic,
+                        const DerivedInfo &calculated)
+{
+  NullOperationEnvironment env;
+  char buffer[100];
+
+  if (FormatGPRMC(buffer, sizeof(buffer), basic))
+    PortWriteNMEA(port, buffer, env);
+
+  if (FormatGPGSA(buffer, sizeof(buffer), basic))
+    PortWriteNMEA(port, buffer, env);
+
+  if (FormatGPGGA(buffer, sizeof(buffer), basic))
+    PortWriteNMEA(port, buffer, env);
 }
 
 static Device *
