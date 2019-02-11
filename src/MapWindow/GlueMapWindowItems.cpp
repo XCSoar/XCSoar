ed3a8a85f6f6f1e10c0dad0a84a08503fefc345b
Thomas Hanika <thomas@hanika-co.de>
2019-02-11 11:34:59 +0100

    reformating sources and better code-dokumentations

--- src/MapWindow/GlueMapWindowItems.cpp
+++ src/MapWindow/GlueMapWindowItems.cpp
@@ -1,25 +1,25 @@
 /*
-Copyright_License {
+ Copyright_License {
 
-  XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2016 The XCSoar Project
-  A detailed list of copyright holders can be found in the file "AUTHORS".
+ XCSoar Glide Computer - http://www.xcsoar.org/
+ Copyright (C) 2000-2016 The XCSoar Project
+ A detailed list of copyright holders can be found in the file "AUTHORS".
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-}
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ }
+ */
 
 #include "GlueMapWindow.hpp"
 #include "Items/List.hpp"
@@ -42,105 +42,126 @@ GlueMapWindow::ShowMapItems(const GeoPoint &location,
                             bool show_empty_message) const
 {
   /* not using MapWindowBlackboard here because this method is called
-     by the main thread */
-  const ComputerSettings &computer_settings =
-    CommonInterface::GetComputerSettings();
+   by the main thread */
+  const ComputerSettings &computer_settings = CommonInterface::GetComputerSettings();
   const MapSettings &settings = CommonInterface::GetMapSettings();
   const MoreData &basic = CommonInterface::Basic();
   const DerivedInfo &calculated = CommonInterface::Calculated();
 
-  bool doRefreshMapList = false;
-
-  do{
-
-  bool only_landable = false;
-  int rangedisplaypercent = settings.item_list.range_of_nearest_map_items_in_percent_of_displaysize;
-  auto range = 0.0;
-  if(rangedisplaypercent == 0) range = visible_projection.DistancePixelsToMeters(Layout::GetHitRadius());
-  else {
-    range = visible_projection.DistancePixelsToMeters(Layout::min_screen_pixels/2*rangedisplaypercent/100);
-    range = std::max(250.0,range);     // minimal circle of 250m
-    range = std::min(50000.0,range);   // maximal circle of 50km, no sense to find whole Europe
-    only_landable = (range > settings.item_list.rangefilter_all_waypoint_up_to_km*1000.0); // list only landable mapitems
-  }
-
   MapItemList list;
-  MapItemListBuilder builder(list, location, range);
 
-  if (settings.item_list.add_location)
+/*
+ * loop for rebuild of MapItemList
+ */
+  do {
+    bool only_landable = false;
+    int rangedisplaypercent = settings.item_list.range_of_nearest_map_items_in_percent_of_displaysize;
+    double range_m = 0.0;
+
+    list.clear();
+
+    if (rangedisplaypercent == 0)
+      /*
+       * old behavior
+       */
+      range_m = visible_projection.DistancePixelsToMeters(
+          Layout::GetHitRadius());
+    else {
+      /*
+       * calculate range in m based on the display size and zoomfactor
+       */
+      range_m = visible_projection.DistancePixelsToMeters(
+          Layout::min_screen_pixels / 2 * rangedisplaypercent / 100);
+      /*
+       * set the best min and max range values
+       */
+      range_m = std::max(250.0, range_m);   // minimal circle of 250m, for
+      range_m = std::min(50000.0, range_m); // maximal circle of 50km, no sense to find whole Europe
+      only_landable = (range_m
+          > settings.item_list.rangefilter_all_waypoint_up_to_km * 1000.0); // list only landable mapitems
+    }
+
+    MapItemListBuilder builder(list, location, range_m);
+
+    if (settings.item_list.add_location)
       builder.AddLocation(basic, terrain);
 
-  if (settings.item_list.add_arrival_altitude && route_planner)
-    builder.AddArrivalAltitudes(*route_planner, terrain,
-                                computer_settings.task.safety_height_arrival);
+    if (settings.item_list.add_arrival_altitude && route_planner)
+      builder.AddArrivalAltitudes(
+          *route_planner, terrain,
+          computer_settings.task.safety_height_arrival);
 
-  if (basic.location_available)
-    builder.AddSelfIfNear(basic.location, basic.attitude.heading);
+    if (basic.location_available)
+      builder.AddSelfIfNear(basic.location, basic.attitude.heading);
 
-  if (task)
-    builder.AddTaskOZs(*task);
+    if (task)
+      builder.AddTaskOZs(*task);
 
-  if(settings.item_list.add_airspace) {
-    const Airspaces *airspace_database = airspace_renderer.GetAirspaces();
-    if (airspace_database)
-      builder.AddVisibleAirspace(*airspace_database,
-                                 airspace_renderer.GetWarningManager(),
-                                 computer_settings.airspace,
-                                 settings.airspace, basic,
-                                 calculated);
-  }
+    if (settings.item_list.add_airspace) {
+      const Airspaces *airspace_database = airspace_renderer.GetAirspaces();
+      if (airspace_database)
+        builder.AddVisibleAirspace(*airspace_database,
+                                   airspace_renderer.GetWarningManager(),
+                                   computer_settings.airspace,
+                                   settings.airspace, basic, calculated);
+    }
 
-  if (visible_projection.GetMapScale() <= 4000)
-    builder.AddThermals(calculated.thermal_locator, basic, calculated);
+    if (visible_projection.GetMapScale() <= 4000)
+      builder.AddThermals(calculated.thermal_locator, basic, calculated);
 
-  // flarm-traffic could be more interested than waypoints...if no interest, disable it
-  if(settings.item_list.add_traffic)
-    builder.AddTraffic(basic.flarm.traffic);
+    // flarm-traffic could be more interested than waypoints...if no interest, disable it
+    if (settings.item_list.add_traffic)
+      builder.AddTraffic(basic.flarm.traffic);
 
-  if (waypoints)
-    builder.AddWaypoints(*waypoints,only_landable);
+    if (waypoints)
+      builder.AddWaypoints(*waypoints, only_landable);
 
 #ifdef HAVE_NOAA
-  if (noaa_store)
-    builder.AddWeatherStations(*noaa_store);
+    if (noaa_store)
+      builder.AddWeatherStations(*noaa_store);
 #endif
 
 #ifdef HAVE_SKYLINES_TRACKING
-  builder.AddSkyLinesTraffic();
+    builder.AddSkyLinesTraffic();
 #endif
 
 #ifdef ENABLE_OPENGL
-  if (!list.full() && overlay && overlay->IsInside(location))
+    if (!list.full() && overlay && overlay->IsInside(location))
     list.push_back(new OverlayMapItem(*overlay));
 #endif
 
-  if (!list.full()) {
+    if (!list.full()) {
 #ifndef ENABLE_OPENGL
-    const ScopeLock protect(mutex);
+      const ScopeLock protect(mutex);
 #endif
 
-    if (rasp_renderer && rasp_renderer->IsInside(location))
-      list.push_back(new RaspMapItem(rasp_renderer->GetLabel()));
-  }
+      if (rasp_renderer && rasp_renderer->IsInside(location))
+        list.push_back(new RaspMapItem(rasp_renderer->GetLabel()));
+    }
 
-  // Sort the list of map items
-  // Sortierung aufheben, es wird nach entfernung sortiert list.Sort();
+    // Sort the list of map items
+    // is sorted by distance in the builder list.Sort();
 
-  // Show the list dialog
-  if (list.empty()) {
-    if (show_empty_message)
-      ShowMessageBox(_("There is nothing interesting near this location."),
-                  _("Map elements at this location"), MB_OK | MB_ICONINFORMATION);
+    // Show the list dialog
+    if (list.empty()) {
+      if (show_empty_message)
+        ShowMessageBox(_("There is nothing interesting near this location."),
+                       _("Map elements at this location"),
+                       MB_OK | MB_ICONINFORMATION);
 
-    return false;
-  }
+      return false;
+    }
 
-  doRefreshMapList = ShowMapItemListDialog(list,
-                        UIGlobals::GetDialogLook(), look, traffic_look,
-                        final_glide_bar_renderer.GetLook(), settings,
-                        glide_computer != nullptr
-                        ? &glide_computer->GetAirspaceWarnings() : nullptr);
-  }while(doRefreshMapList);
+  } while (MAPITEMLIST_REBUILD
+      == ShowMapItemListDialog(
+          list,
+          UIGlobals::GetDialogLook(),
+          look,
+          traffic_look,
+          final_glide_bar_renderer.GetLook(),
+          settings,
+          glide_computer != nullptr ? &glide_computer->GetAirspaceWarnings() :
+                                      nullptr));
 
   return true;
 }
--- src/MapWindow/Items/Builder.cpp
+++ src/MapWindow/Items/Builder.cpp
@@ -1,25 +1,25 @@
 /*
-Copyright_License {
+ Copyright_License {
 
-  XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2016 The XCSoar Project
-  A detailed list of copyright holders can be found in the file "AUTHORS".
+ XCSoar Glide Computer - http://www.xcsoar.org/
+ Copyright (C) 2000-2016 The XCSoar Project
+ A detailed list of copyright holders can be found in the file "AUTHORS".
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-}
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ }
+ */
 
 #include "Builder.hpp"
 #include "MapItem.hpp"
@@ -36,49 +36,85 @@ Copyright_License {
 #include "NMEA/Info.hpp"
 #include "Terrain/RasterTerrain.hpp"
 
-class WaypointListBuilderVisitor:
-  public WaypointVisitor
-{
+class WaypointListBuilderVisitor: public WaypointVisitor {
   MapItemList &list;
+
+  /*
+   * only_landable filters landable waypoints
+   */
   bool only_landable;
+
+  /*
+   * location is the hitpoint of touch, mouseclick
+   */
   GeoPoint location;
 
-  // distance waypoint vector, for sorting on distance in destructor
-  std::vector<std::pair<double,std::shared_ptr<const Waypoint>> > tempMIvect;
+  /*
+   * distanced_map_items: mapitems with saved distances to location (sorted at the end)
+   */
+  std::vector<std::pair<double, std::shared_ptr<const Waypoint>> > distanced_map_items;
 
 public:
-  WaypointListBuilderVisitor(MapItemList &_list, bool _only_landable = false, GeoPoint _location = GeoPoint().Invalid() ):list(_list),only_landable(_only_landable),location(_location) {}
+  WaypointListBuilderVisitor(MapItemList &_list, bool _only_landable = false,
+                             GeoPoint _location = GeoPoint().Invalid())
+      : list(_list), only_landable(_only_landable), location(_location)
+  {
+  }
 
-  void Visit(const WaypointPtr & waypointptr) override {
-    if(location.IsValid()){
-      double distance = waypointptr->location.Distance( location ); // calculate distance
-      std::pair<double,std::shared_ptr<const Waypoint>> elemptr = std::make_pair(distance,waypointptr); // save pair distance and waypoint
-      tempMIvect.push_back(elemptr);
+  void
+  Visit(const WaypointPtr & waypointptr) override
+  {
+    if (location.IsValid()) {
+      /*
+       * a valid location allows calculating the distance
+       */
+      double distance = waypointptr->location.Distance(location); // calculate distance
+      std::pair<double, std::shared_ptr<const Waypoint>> elemptr = std::make_pair(
+          distance, waypointptr); // save pair distance and waypoint
+      distanced_map_items.emplace_back(elemptr);
     } else {
-      if (!list.full()){
-        if(only_landable){
-          if(waypointptr->IsLandable()) list.append(new WaypointMapItem(waypointptr));
-        }else{
+      /*
+       * failback, if location is invalid
+       */
+      if (!list.full()) {
+        if (only_landable) {
+          if (waypointptr->IsLandable())
+            list.append(new WaypointMapItem(waypointptr));
+        } else {
           list.append(new WaypointMapItem(waypointptr));
         }
       }
     }
   }
 
-  virtual ~WaypointListBuilderVisitor() {
-    if(location.IsValid()){ // sort on distance available
-      std::sort(tempMIvect.begin(),tempMIvect.end());
-      for(unsigned int i = 0; i < tempMIvect.size(); i++){
-        std::shared_ptr<const Waypoint> WPptr = tempMIvect[i].second;
-        if (!list.full()){
-          if(only_landable){
-            if((WPptr->IsLandable()) || (tempMIvect[i].first < 250.0)) list.append(new WaypointMapItem(WPptr)); // list also this item, it was clicked!!!
-          }else{
+  ~WaypointListBuilderVisitor()
+  {
+    if (location.IsValid()) {
+      /*
+       * the location is valid, all visitor are in distance_map_items listed, now fill the list
+       */
+      std::sort(distanced_map_items.begin(), distanced_map_items.end());
+      for (unsigned int i = 0; i < distanced_map_items.size(); i++) {
+        std::shared_ptr<const Waypoint> WPptr = distanced_map_items[i].second;
+        if (!list.full()) {
+          if (only_landable) {
+            if (WPptr->IsLandable()) {
+              list.append(new WaypointMapItem(WPptr));
+            } else {
+              if (distanced_map_items[i].first < 250.0) {
+                /*
+                 * If you click near a mapitem, it should be displayed.
+                 */
+                list.append(new WaypointMapItem(WPptr));
+              }
+            }
+          } else {
             list.append(new WaypointMapItem(WPptr));
           }
-        } else break;
+        } else
+          break; // list full
       }
-      tempMIvect.clear();
+      distanced_map_items.clear();
     }
   }
 };
@@ -98,16 +134,16 @@ MapItemListBuilder::AddLocation(const NMEAInfo &basic,
 
   double elevation = LocationMapItem::UNKNOWN_ELEVATION;
   if (terrain != nullptr)
-    elevation = terrain->GetTerrainHeight(location)
-      .ToDouble(LocationMapItem::UNKNOWN_ELEVATION);
+    elevation = terrain->GetTerrainHeight(location).ToDouble(
+        LocationMapItem::UNKNOWN_ELEVATION);
 
   list.append(new LocationMapItem(vector, elevation));
 }
 
 void
 MapItemListBuilder::AddArrivalAltitudes(
-    const ProtectedRoutePlanner &route_planner,
-    const RasterTerrain *terrain, double safety_height)
+    const ProtectedRoutePlanner &route_planner, const RasterTerrain *terrain,
+    double safety_height)
 {
   if (list.full())
     return;
@@ -115,8 +151,8 @@ MapItemListBuilder::AddArrivalAltitudes(
   // Calculate terrain elevation if possible
   double elevation = LocationMapItem::UNKNOWN_ELEVATION;
   if (terrain != nullptr)
-    elevation = terrain->GetTerrainHeight(location)
-      .ToDouble(LocationMapItem::UNKNOWN_ELEVATION);
+    elevation = terrain->GetTerrainHeight(location).ToDouble(
+        LocationMapItem::UNKNOWN_ELEVATION);
 
   // Calculate target altitude
   double target_elevation = 0;
@@ -144,9 +180,11 @@ MapItemListBuilder::AddSelfIfNear(const GeoPoint &self, Angle bearing)
 }
 
 void
-MapItemListBuilder::AddWaypoints(const Waypoints &waypoints, bool only_landable)
+MapItemListBuilder::AddWaypoints(const Waypoints &waypoints,
+                                 bool only_landable)
 {
-  WaypointListBuilderVisitor waypoint_list_builder(list, only_landable, location );
+  WaypointListBuilderVisitor waypoint_list_builder(list, only_landable,
+                                                   location);
   waypoints.VisitWithinRange(location, range, waypoint_list_builder);
 }
 
@@ -171,7 +209,8 @@ MapItemListBuilder::AddTaskOZs(const ProtectedTaskManager &task)
       continue;
 
     const ObservationZonePoint &oz = task_point.GetObservationZone();
-    list.append(new TaskOZMapItem(i, oz, task_point.GetType(),
-                                  task_point.GetWaypointPtr()));
+    list.append(
+        new TaskOZMapItem(i, oz, task_point.GetType(),
+                          task_point.GetWaypointPtr()));
   }
 }
