cca2cfa1632198464edd08dbe6597f68b3f6f753
Kai Horstmann <horstmannkai@hotmail.com>
2019-04-26 16:12:41 +0200

    Android: Load legacy library on Android < 5 (Lollipop), API < 21
    Load legacy library android_legacy when
    - the Android version running is < API 21
    - the standard library does not exist because the APK is a pure legacy
    build

--- android/src/Loader.java
+++ android/src/Loader.java
@@ -29,12 +29,41 @@ public class Loader {
   private static final String TAG = "XCSoar";
 
   public static boolean loaded = false;
+  public static boolean isLegacy = false;
   public static String error;
 
   static {
     try {
-      System.loadLibrary("xcsoar");
-      loaded = true;
+      /* Depending on the Android version we load the fully functional library
+       * or the functional slightly restricted version which is compatible down to
+       * API level 12 (Honeycomb), and implements some work-arounds for missing functions
+       * in the Bionic C library
+       */
+
+      if (android.os.Build.VERSION.SDK_INT >= 21) {
+        System.loadLibrary("xcsoar");
+        Log.i(TAG, "Loaded native library xcsoar");
+        loaded = true;
+      }
+    } catch (UnsatisfiedLinkError e) {
+      Log.w(TAG, "Native library xcsoar cannot be loaded. Load error = ");
+      Log.w(TAG, e.getMessage());
+      Log.w(TAG, "You may ignore the load error when this is a legacy-only build.");
+    }
+
+    /* Library xcsoar.so was not be loaded because
+     * - It is running on an older Android version < API 21
+     * - It is a legacy-only build of the APK
+     * - Something with loading of the library xcsoar simply went wrong.
+     * Try the same with the library xcsoar_legacy. If that does not work give up.
+     */
+    try {
+      if (!loaded) {
+        System.loadLibrary("xcsoar_legacy");
+        Log.i(TAG, "Loaded native library xcsoar_legacy");
+        isLegacy = true;
+        loaded = true;
+      }
     } catch (UnsatisfiedLinkError e) {
       Log.e(TAG, e.getMessage());
       error = e.getMessage();
