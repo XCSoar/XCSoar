XCSoar/XCSoar

commit 8e1548b901e14e5c5e8601de7c5a416966fd46e6

Makefile: use Android NDK r16

build/targets.mk
@@ -438,7 +438,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r15c
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r16
 
   ANDROID_SDK_PLATFORM = android-22
   ANDROID_NDK_PLATFORM = android-19
@@ -491,6 +491,7 @@ ifeq ($(TARGET),ANDROID)
     ANDROID_NDK_PLATFORM = $(ANDROID_NDK_PLATFORM_64)
   endif
 
+  ANDROID_SYSROOT = $(ANDROID_NDK)/sysroot
   ANDROID_NDK_PLATFORM_DIR = $(ANDROID_NDK)/platforms/$(ANDROID_NDK_PLATFORM)
   ANDROID_TARGET_ROOT = $(ANDROID_NDK_PLATFORM_DIR)/arch-$(ANDROID_ARCH)
 
@@ -673,8 +674,10 @@ ifeq ($(TARGET_IS_KOBO),y)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  TARGET_CPPFLAGS += --sysroot=$(ANDROID_TARGET_ROOT)
+  TARGET_CPPFLAGS += --sysroot=$(ANDROID_SYSROOT)
+  TARGET_CPPFLAGS += -isystem $(ANDROID_SYSROOT)/usr/include/$(ANDROID_ABI2)
   TARGET_CPPFLAGS += -DANDROID
+  TARGET_CPPFLAGS += -D__ANDROID_API__=13
   CXXFLAGS += -D__STDC_VERSION__=199901L
 
   ifeq ($(X86),y)

doc/manual/en/compiling.tex
@@ -123,7 +123,7 @@ For Android, you need:
 
 \begin{itemize}
 \item \href{http://developer.android.com/sdk/}{Android SDK level 22}
-\item \href{http://developer.android.com/sdk/ndk/}{Android NDK r14b}
+\item \href{http://developer.android.com/sdk/ndk/}{Android NDK r16}
 \item \href{http://www.vorbis.com/}{Ogg Vorbis}
 \item {Java JDK 
 \begin{verbatim*}
@@ -133,7 +133,7 @@ sudo apt-get install default-jdk-headless ant vorbis-tools
 
 The \texttt{Makefile} assumes that the Android SDK is installed in
 \verb|~/opt/android-sdk-linux| and the NDK is installed in
-\verb|~/opt/android-ndk-r14b|.  You can use the options
+\verb|~/opt/android-ndk-r16|.  You can use the options
 \verb|ANDROID_SDK| and \verb|ANDROID_NDK| to override these paths.
 
 After installing Java you will have to install at least one Android Platform SDK using the Android SDK Manager:

doc/manual/en/developsetuplinux.tex
@@ -127,9 +127,9 @@ ln -s android-sdk-linux_x86 android-sdk-linux
 
 # Download NDK installer
 wget "http://dl.google.com/android/\
-repository/android-ndk-r14b-linux-x86_64.zip"
+repository/android-ndk-r16-linux-x86_64.zip"
 
-unzip android-ndk-r14b-linux-x86_64.zip
+unzip android-ndk-r16-linux-x86_64.zip
 \end{verbatim}
 
 We need to add the paths by appending the following lines to \texttt{\~/.profile} and \texttt{/~/.bashrc} --- here relative paths work.
@@ -141,7 +141,7 @@ export PATH="$PATH:$ANDROID_SDK_HOME/tools"
 export PATH="$PATH:$ANDROID_SDK_HOME/platform-tools"
 
 # Android NDK
-export ANDROID_NDK_HOME="~/opt/android-ndk-r14b"
+export ANDROID_NDK_HOME="~/opt/android-ndk-r16"
 export PATH="$PATH:$ANDROID_NDK_HOME"
 \end{verbatim}
 

commit 9bb2c1e1ac32f025b5cf3580991c4bb4a339bcff

build/targets.mk: downgrade Android NDK to r15c
This reverts commit 8e1548b901e1 which broke support for older Android
versions because it added a dependency on symbol "posix_memalign".

build/targets.mk
@@ -438,7 +438,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r16b
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r15c
 
   ANDROID_SDK_PLATFORM = android-26
   ANDROID_NDK_PLATFORM = android-19

doc/manual/en/compiling.tex
@@ -133,7 +133,7 @@ sudo apt-get install default-jdk-headless ant vorbis-tools
 
 The \texttt{Makefile} assumes that the Android SDK is installed in
 \verb|~/opt/android-sdk-linux| and the NDK is installed in
-\verb|~/opt/android-ndk-r16b|.  You can use the options
+\verb|~/opt/android-ndk-r15c|.  You can use the options
 \verb|ANDROID_SDK| and \verb|ANDROID_NDK| to override these paths.
 
 After installing Java you will have to install at least one Android Platform SDK using the Android SDK Manager:

doc/manual/en/developsetuplinux.tex
@@ -127,9 +127,9 @@ ln -s android-sdk-linux_x86 android-sdk-linux
 
 # Download NDK installer
 wget "http://dl.google.com/android/\
-repository/android-ndk-r16b-linux-x86_64.zip"
+repository/android-ndk-r15c-linux-x86_64.zip"
 
-unzip android-ndk-r16b-linux-x86_64.zip
+unzip android-ndk-r15c-linux-x86_64.zip
 \end{verbatim}
 
 We need to add the paths by appending the following lines to \texttt{\~/.profile} and \texttt{/~/.bashrc} --- here relative paths work.
@@ -141,7 +141,7 @@ export PATH="$PATH:$ANDROID_SDK_HOME/tools"
 export PATH="$PATH:$ANDROID_SDK_HOME/platform-tools"
 
 # Android NDK
-export ANDROID_NDK_HOME="~/opt/android-ndk-r16b"
+export ANDROID_NDK_HOME="~/opt/android-ndk-r15c"
 export PATH="$PATH:$ANDROID_NDK_HOME"
 \end{verbatim}
 