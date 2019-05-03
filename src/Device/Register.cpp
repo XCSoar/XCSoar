719d8d66456cd7f5d466349371d387d375f9083e
eku60 <50234402+eku60@users.noreply.github.com>
2019-05-03 15:43:24 +0200

    Registers the AR62xx driver for use in XCSoar
    The file registers the driver for use with XCSoar. It can be selected in the Device settings tab

--- src/Device/Register.cpp
+++ src/Device/Register.cpp
@@ -58,6 +58,7 @@ Copyright_License {
 #include "Device/Driver/ATR833.hpp"
 #include "Device/Driver/XCTracer.hpp"
 #include "Device/Driver/KRT2.hpp"
+#include "Device/Driver/AR62xx.hpp"
 #include "Util/Macros.hpp"
 #include "Util/StringAPI.hxx"
 
@@ -101,6 +102,7 @@ static const struct DeviceRegister *const driver_list[] = {
   &xctracer_driver,
   &thermalexpress_driver,
   &acd_driver,
+  &ar62xx_driver,
   nullptr
 };
 
