81eb346ab5830bbf810fe3ee16039f93c8fd025c
Max Kellermann <max.kellermann@gmail.com>
2019-05-05 19:42:39 +0200

    build/python/libstdcxxmuslheaders: kludge to enable POSIX threading

--- build/python/build/libstdcxxmuslheaders.py
+++ build/python/build/libstdcxxmuslheaders.py
@@ -9,7 +9,22 @@ from build.makeproject import MakeProject
 # source tarball.
 class LibstdcxxMuslHeadersProject(AutotoolsProject):
     def build(self, toolchain):
-        build = self.configure(toolchain)
+        src = self.unpack(toolchain)
+        top_build = self.make_build_path(toolchain)
+        build = os.path.join(top_build, 'libstdc++-v3')
+        os.mkdir(build)
+
+        # libstdc++'s configure requires a configured libgcc or it
+        # will disable threading support; but instead of configuring
+        # libgcc, this kludge just creates the "gthr-default.h"
+        # symlink, which is enough to enable multi-threading
+        libgcc = os.path.join(top_build, 'libgcc')
+        os.mkdir(libgcc)
+        os.symlink(os.path.join(src, 'libgcc', 'gthr-posix.h'),
+                   os.path.join(libgcc, 'gthr-default.h'))
+
+        build = self.configure(toolchain, src=src, build=build)
+
         incdir_param = 'gxx_include_dir=' + toolchain.install_prefix + '/include/libstdc++'
         self.make(toolchain, os.path.join(build, 'libsupc++'), [incdir_param, 'install-bitsHEADERS', 'install-stdHEADERS'])
         self.make(toolchain, os.path.join(build, 'include'), [incdir_param, 'install-headers'])
