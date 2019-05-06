XCSoar/XCSoar

commit 439cd89c103b325f9f88338641c677ae1cd475cd

Thread/Cond: pass std::chrono::duration to wait_for()

src/Device/Driver/KRT2.cpp
@@ -44,7 +44,7 @@ Copyright_License {
  * for the protocol specification.
  */
 class KRT2Device final : public AbstractDevice {
-  static constexpr unsigned CMD_TIMEOUT = 250; //!< Command timeout in ms.
+  static constexpr auto CMD_TIMEOUT = 250; //!< Command timeout in ms.
   static constexpr unsigned NR_RETRIES = 3; //!< Number of tries to send a command.
 
   static constexpr char STX = 0x02; //!< Command start character.
@@ -178,7 +178,7 @@ KRT2Device::Send(const uint8_t *msg, unsigned msg_size,
     uint8_t _response;
     {
       const std::lock_guard<Mutex> lock(response_mutex);
-      rx_cond.wait_for(response_mutex, CMD_TIMEOUT);
+      rx_cond.wait_for(response_mutex, std::chrono::milliseconds(CMD_TIMEOUT));
       _response = response;
     }
 

src/Device/Driver/BlueFly/Settings.cpp
@@ -72,7 +72,7 @@ BlueFlyDevice::WaitForSettings(unsigned int timeout)
 {
   const std::lock_guard<Mutex> lock(mutex_settings);
   if (!settings_ready)
-    settings_cond.wait_for(mutex_settings, timeout);
+    settings_cond.wait_for(mutex_settings, std::chrono::milliseconds(timeout));
   return settings_ready;
 }
 

src/Device/Port/BufferedPort.cpp
@@ -109,7 +109,7 @@ BufferedPort::WaitRead(unsigned timeout_ms)
     if (remaining_ms <= 0)
       return WaitResult::TIMEOUT;
 
-    cond.wait_for(mutex, remaining_ms);
+    cond.wait_for(mutex, std::chrono::milliseconds(remaining_ms));
   }
 
   return WaitResult::READY;

src/Device/SettingsMap.hpp
@@ -110,7 +110,7 @@ public:
       if (remaining <= 0)
         return end();
 
-      cond.wait_for(*this, remaining);
+      cond.wait_for(*this, std::chrono::milliseconds(remaining));
     }
   }
 

src/Event/Android/Queue.cpp
@@ -77,7 +77,7 @@ EventQueue::Wait(Event &event)
     if (timeout < std::chrono::steady_clock::duration::zero())
       cond.wait(mutex);
     else
-      cond.wait_for(mutex, (std::chrono::duration_cast<std::chrono::microseconds>(timeout).count() + 999) / 1000);
+      cond.wait_for(mutex, timeout);
 
     FlushClockCaches();
   }

src/Operation/ThreadedOperationEnvironment.cpp
@@ -40,7 +40,7 @@ ThreadedOperationEnvironment::Sleep(unsigned ms)
 {
   const std::lock_guard<Mutex> lock(mutex);
   if (!cancel_flag)
-    cancel_cond.wait_for(mutex, ms);
+    cancel_cond.wait_for(mutex, std::chrono::milliseconds(ms));
 }
 
 void

src/Thread/PosixCond.hxx
@@ -32,6 +32,8 @@
 
 #include "PosixMutex.hxx"
 
+#include <chrono>
+
 #include <sys/time.h>
 
 /**
@@ -72,13 +74,14 @@ public:
 		pthread_cond_wait(&cond, &mutex.mutex);
 	}
 
-	bool wait_for(PosixMutex &mutex, unsigned timeout_ms) noexcept {
+private:
+	bool wait_for(PosixMutex &mutex, uint_least32_t timeout_us) noexcept {
 		struct timeval now;
 		gettimeofday(&now, nullptr);
 
 		struct timespec ts;
-		ts.tv_sec = now.tv_sec + timeout_ms / 1000;
-		ts.tv_nsec = (now.tv_usec + (timeout_ms % 1000) * 1000) * 1000;
+		ts.tv_sec = now.tv_sec + timeout_us / 1000000;
+		ts.tv_nsec = (now.tv_usec + (timeout_us % 1000000)) * 1000;
 		// Keep tv_nsec < 1E9 to prevent return of EINVAL
 		if (ts.tv_nsec >= 1000000000) {
 			ts.tv_nsec -= 1000000000;
@@ -86,6 +89,18 @@ public:
 		}
 
 		return pthread_cond_timedwait(&cond, &mutex.mutex, &ts) == 0;
+	}
+
+public:
+	bool wait_for(PosixMutex &mutex,
+		      std::chrono::steady_clock::duration timeout) noexcept {
+		auto timeout_us = std::chrono::duration_cast<std::chrono::microseconds>(timeout).count();
+		if (timeout_us < 0)
+			timeout_us = 0;
+		else if (timeout_us > std::numeric_limits<uint_least32_t>::max())
+			timeout_us = std::numeric_limits<uint_least32_t>::max();
+
+		return wait_for(mutex, timeout_us);
 	}
 };
 

src/Thread/SuspensibleThread.cpp
@@ -166,7 +166,7 @@ SuspensibleThread::_WaitForStopped(unsigned timeout_ms)
   suspended = true;
 
   if (!stop_received)
-    command_trigger.wait_for(mutex, timeout_ms);
+    command_trigger.wait_for(mutex, std::chrono::milliseconds(timeout_ms));
 
   if (!stop_received && suspend_received) {
     client_trigger.notify_one();

commit 015d20aa6c21591c60845247867e6f91de8e44a8

Driver/KRT2: fix initializer of CMD_TIMEOUT
Fixes the regression introduced by 439cd89c103b325f9f88338641c677ae1cd475cd
(issue #192).

src/Device/Driver/KRT2.cpp
@@ -44,7 +44,7 @@ Copyright_License {
  * for the protocol specification.
  */
 class KRT2Device final : public AbstractDevice {
-  static constexpr auto CMD_TIMEOUT = 250; //!< Command timeout in ms.
+  static constexpr auto CMD_TIMEOUT = 250ll; //!< Command timeout in ms.
   static constexpr unsigned NR_RETRIES = 3; //!< Number of tries to send a command.
 
   static constexpr char STX = 0x02; //!< Command start character.