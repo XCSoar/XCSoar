baf343766a722221812411bb1594a114ced2817f
Max Kellermann <max.kellermann@gmail.com>
2019-05-08 15:05:21 +0200

    Time/Calendar: indent with tabs

--- src/Time/Calendar.hxx
+++ src/Time/Calendar.hxx
@@ -35,25 +35,25 @@ gcc_const
 static inline bool
 IsLeapYear(unsigned y)
 {
-    y += 1900;
-    return (y % 4) == 0 && ((y % 100) != 0 || (y % 400) == 0);
+	y += 1900;
+	return (y % 4) == 0 && ((y % 100) != 0 || (y % 400) == 0);
 }
 
 gcc_const
 static inline unsigned
 DaysInFebruary(unsigned year)
 {
-  return IsLeapYear(year) ? 29 : 28;
+	return IsLeapYear(year) ? 29 : 28;
 }
 
 gcc_const
 static inline unsigned
 DaysInMonth(unsigned month, unsigned year)
 {
-  if (month == 4 || month == 6 || month == 9 || month == 11)
-    return 30;
-  else if (month != 2)
-    return 31;
-  else
-    return DaysInFebruary(year);
+	if (month == 4 || month == 6 || month == 9 || month == 11)
+		return 30;
+	else if (month != 2)
+		return 31;
+	else
+		return DaysInFebruary(year);
 }
