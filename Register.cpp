804630e893698513dbfeb11f2b83bd1035e1100d
eku60 <50234402+eku60@users.noreply.github.com>
2019-05-15 20:13:43 +0200

    Corrected files with changes from XCSoar-master

---
+++ Register.cpp
@@ -0,0 +1,141 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2016 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#include "Device/Register.hpp"
+#include "Device/Driver.hpp"
+#include "Device/Driver/AirControlDisplay.hpp"
+#include "Device/Driver/CAI302.hpp"
+#include "Device/Driver/CaiGpsNav.hpp"
+#include "Device/Driver/CaiLNav.hpp"
+#include "Device/Driver/EW.hpp"
+#include "Device/Driver/Eye.hpp"
+#include "Device/Driver/AltairPro.hpp"
+#include "Device/Driver/Generic.hpp"
+#include "Device/Driver/Vega.hpp"
+#include "Device/Driver/NmeaOut.hpp"
+#include "Device/Driver/PosiGraph.hpp"
+#include "Device/Driver/BorgeltB50.hpp"
+#include "Device/Driver/Volkslogger.hpp"
+#include "Device/Driver/EWMicroRecorder.hpp"
+#include "Device/Driver/LX.hpp"
+#include "Device/Driver/IMI.hpp"
+#include "Device/Driver/Zander.hpp"
+#include "Device/Driver/FlymasterF1.hpp"
+#include "Device/Driver/XCOM760.hpp"
+#include "Device/Driver/Condor.hpp"
+#include "Device/Driver/Leonardo.hpp"
+#include "Device/Driver/Flytec.hpp"
+#include "Device/Driver/ILEC.hpp"
+#include "Device/Driver/Westerboer.hpp"
+#include "Device/Driver/FLARM.hpp"
+#include "Device/Driver/FlyNet.hpp"
+#include "Device/Driver/ThermalExpress.hpp"
+#include "Device/Driver/CProbe.hpp"
+#include "Device/Driver/LevilAHRS_G.hpp"
+#include "Device/Driver/BlueFlyVario.hpp"
+#include "Device/Driver/OpenVario.hpp"
+#include "Device/Driver/Vaulter.hpp"
+#include "Device/Driver/ATR833.hpp"
+#include "Device/Driver/XCTracer.hpp"
+#include "Device/Driver/KRT2.hpp"
+#include "Device/Driver/AR62xx.hpp"
+#include "Util/Macros.hpp"
+#include "Util/StringAPI.hxx"
+
+#include <assert.h>
+
+/** nullptr terminated array of available device drivers. */
+static const struct DeviceRegister *const driver_list[] = {
+  // IMPORTANT: ADD NEW ONES TO BOTTOM OF THIS LIST
+  &generic_driver, // MUST BE FIRST
+  &cai302_driver,
+  &ew_driver,
+  &altair_pro_driver,
+  &vega_driver,
+  &gps_nav_driver,
+  &nmea_out_driver,
+  &posigraph_driver,
+  &b50_driver,
+  &volkslogger_driver,
+  &ew_microrecorder_driver,
+  &lx_driver,
+  &zander_driver,
+  &flymaster_f1_driver,
+  &xcom760_driver,
+  &condor_driver,
+  &leonardo_driver,
+  &flytec_driver,
+  &ilec_driver,
+  &westerboer_driver,
+  &imi_driver,
+  &flarm_driver,
+  &flynet_driver,
+  &c_probe_driver,
+  &levil_driver,
+  &eye_driver,
+  &bluefly_driver,
+  &cai_lnav_driver,
+  &open_vario_driver,
+  &vaulter_driver,
+  &krt2_driver,
+  &atr833_driver,
+  &xctracer_driver,
+  &thermalexpress_driver,
+  &acd_driver,
+  &ar62xx_driver,
+  nullptr
+};
+
+const struct DeviceRegister *
+GetDriverByIndex(unsigned i)
+{
+  assert(i < ARRAY_SIZE(driver_list));
+
+  return driver_list[i];
+}
+
+const struct DeviceRegister *
+FindDriverByName(const TCHAR *name)
+{
+  for (auto i = driver_list; *i != nullptr; ++i) {
+    const DeviceRegister &driver = **i;
+    if (StringIsEqual(driver.name, name))
+      return &driver;
+  }
+
+  return driver_list[0];
+}
+
+const TCHAR *
+FindDriverDisplayName(const TCHAR *name)
+{
+  assert(name != nullptr);
+
+  for (auto i = driver_list; *i != nullptr; ++i) {
+    const DeviceRegister &driver = **i;
+    if (StringIsEqual(driver.name, name))
+      return driver.display_name;
+  }
+
+  return name;
+}
---
+++ driver.mk
@@ -0,0 +1,131 @@
+DRIVER_SRC_DIR = $(SRC)/Device/Driver
+
+VOLKSLOGGER_SOURCES = \
+	$(DRIVER_SRC_DIR)/Volkslogger/Register.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/Parser.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/Protocol.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/Declare.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/Database.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/Util.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/dbbconv.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/grecord.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/vlapi2.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/vlapihlp.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/vlconv.cpp \
+	$(DRIVER_SRC_DIR)/Volkslogger/Logger.cpp
+
+CAI302_SOURCES = \
+	$(DRIVER_SRC_DIR)/CAI302/Protocol.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/PocketNav.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Mode.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Parser.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Settings.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Declare.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Logger.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Manage.cpp \
+	$(DRIVER_SRC_DIR)/CAI302/Register.cpp
+
+IMI_SOURCES = \
+	$(DRIVER_SRC_DIR)/IMI/Protocol/MessageParser.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Protocol/Communication.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Protocol/Checksum.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Protocol/Conversion.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Protocol/IGC.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Protocol/Protocol.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Declare.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Internal.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Logger.cpp \
+	$(DRIVER_SRC_DIR)/IMI/Register.cpp
+
+LX_SOURCES = \
+	$(DRIVER_SRC_DIR)/LX/NanoLogger.cpp \
+	$(DRIVER_SRC_DIR)/LX/NanoDeclare.cpp \
+	$(DRIVER_SRC_DIR)/LX/Protocol.cpp \
+	$(DRIVER_SRC_DIR)/LX/Mode.cpp \
+	$(DRIVER_SRC_DIR)/LX/Parser.cpp \
+	$(DRIVER_SRC_DIR)/LX/Settings.cpp \
+	$(DRIVER_SRC_DIR)/LX/Declare.cpp \
+	$(DRIVER_SRC_DIR)/LX/Logger.cpp \
+	$(DRIVER_SRC_DIR)/LX/Convert.cpp \
+	$(DRIVER_SRC_DIR)/LX/LXN.cpp \
+	$(DRIVER_SRC_DIR)/LX/Register.cpp
+
+FLARM_SOURCES = \
+	$(DRIVER_SRC_DIR)/FLARM/Device.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/Register.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/Mode.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/Parser.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/StaticParser.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/Settings.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/Declare.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/Logger.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/CRC16.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/BinaryProtocol.cpp \
+	$(DRIVER_SRC_DIR)/FLARM/TextProtocol.cpp
+
+FLYTEC_SOURCES = \
+	$(DRIVER_SRC_DIR)/Flytec/Register.cpp \
+	$(DRIVER_SRC_DIR)/Flytec/Parser.cpp \
+	$(DRIVER_SRC_DIR)/Flytec/Logger.cpp
+
+VEGA_SOURCES = \
+	$(DRIVER_SRC_DIR)/Vega/Misc.cpp \
+	$(DRIVER_SRC_DIR)/Vega/Parser.cpp \
+	$(DRIVER_SRC_DIR)/Vega/Settings.cpp \
+	$(DRIVER_SRC_DIR)/Vega/Volatile.cpp \
+	$(DRIVER_SRC_DIR)/Vega/Register.cpp
+
+BLUEFLY_SOURCES = \
+	$(DRIVER_SRC_DIR)/BlueFly/Misc.cpp \
+	$(DRIVER_SRC_DIR)/BlueFly/Parser.cpp \
+	$(DRIVER_SRC_DIR)/BlueFly/Settings.cpp \
+	$(DRIVER_SRC_DIR)/BlueFly/Register.cpp
+
+XCTRACER_SOURCES = \
+	$(DRIVER_SRC_DIR)/XCTracer/Parser.cpp \
+	$(DRIVER_SRC_DIR)/XCTracer/Register.cpp
+
+THERMALEXPRESS_SOURCES = \
+	$(DRIVER_SRC_DIR)/ThermalExpress/Driver.cpp
+ 
+DRIVER_SOURCES = \
+	$(SRC)/Device/Driver.cpp \
+	$(SRC)/Device/Register.cpp \
+	$(VOLKSLOGGER_SOURCES) \
+	$(CAI302_SOURCES) \
+	$(IMI_SOURCES) \
+	$(LX_SOURCES) \
+	$(FLARM_SOURCES) \
+	$(FLYTEC_SOURCES) \
+	$(VEGA_SOURCES) \
+	$(BLUEFLY_SOURCES) \
+	$(XCTRACER_SOURCES) \
+	$(THERMALEXPRESS_SOURCES) \
+	$(DRIVER_SRC_DIR)/AltairPro.cpp \
+	$(DRIVER_SRC_DIR)/BorgeltB50.cpp \
+	$(DRIVER_SRC_DIR)/CaiGpsNav.cpp \
+	$(DRIVER_SRC_DIR)/CaiLNav.cpp \
+	$(DRIVER_SRC_DIR)/Condor.cpp \
+	$(DRIVER_SRC_DIR)/CProbe.cpp \
+	$(DRIVER_SRC_DIR)/EW.cpp \
+	$(DRIVER_SRC_DIR)/EWMicroRecorder.cpp \
+	$(DRIVER_SRC_DIR)/Eye.cpp \
+	$(DRIVER_SRC_DIR)/FlymasterF1.cpp \
+	$(DRIVER_SRC_DIR)/FlyNet.cpp \
+	$(DRIVER_SRC_DIR)/Generic.cpp \
+	$(DRIVER_SRC_DIR)/LevilAHRS_G.cpp \
+	$(DRIVER_SRC_DIR)/Leonardo.cpp \
+	$(DRIVER_SRC_DIR)/NmeaOut.cpp \
+	$(DRIVER_SRC_DIR)/OpenVario.cpp \
+	$(DRIVER_SRC_DIR)/PosiGraph.cpp \
+	$(DRIVER_SRC_DIR)/XCOM760.cpp \
+	$(DRIVER_SRC_DIR)/ILEC.cpp \
+	$(DRIVER_SRC_DIR)/Westerboer.cpp \
+	$(DRIVER_SRC_DIR)/Zander.cpp \
+	$(DRIVER_SRC_DIR)/Vaulter.cpp \
+	$(DRIVER_SRC_DIR)/KRT2.cpp \
+	$(DRIVER_SRC_DIR)/AirControlDisplay.cpp \
+	$(DRIVER_SRC_DIR)/AR62xx.cpp \
+	$(DRIVER_SRC_DIR)/ATR833.cpp
+
+$(eval $(call link-library,driver,DRIVER))
