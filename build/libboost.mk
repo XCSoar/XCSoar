f3dc64f5ebb8fb1186e8626211f45a16e2ea5bbc
andreas <andreas.luethi@gmx.net>
2020-03-28 13:53:57 +0100

    HTTP Error 403: Forbidden on boost url: change the BOOST_ALTERNATIVE_URL.

--- build/libboost.mk
+++ build/libboost.mk
@@ -1,5 +1,5 @@
 BOOST_URL = https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.bz2
-BOOST_ALTERNATIVE_URL = https://fossies.org/linux/misc/boost_1_70_0.tar.bz2
+BOOST_ALTERNATIVE_URL = https://sourceforge.net/projects/boost/files/boost/1.70.0/boost_1_70_0.tar.bz2/download
 BOOST_MD5 = 430ae8354789de4fd19ee52f3b1f739e1fba576f0aded0897c3c2bc00fb38778
 
 BOOST_TARBALL_NAME = $(notdir $(BOOST_URL))
