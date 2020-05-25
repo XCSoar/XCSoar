XCSoar/XCSoar

commit 234ce75d68c95b2a13241a872ec897b54427b56a

Net/HTTP/Request: pass response body to DataHandler instance
Eliminate the method Read().  Prepare for moving all I/O to the
IOThread.

src/Net/HTTP/ToFile.cpp
@@ -23,12 +23,59 @@ Copyright_License {
 
 #include "ToFile.hpp"
 #include "Request.hpp"
+#include "Handler.hpp"
 #include "Operation/Operation.hpp"
 #include "OS/FileUtil.hpp"
 #include "Logger/MD5.hpp"
 
 #include <assert.h>
 #include <stdio.h>
+
+class CancelDownloadToFile {};
+
+class DownloadToFileHandler final : public Net::ResponseHandler {
+  FILE *file;
+
+  MD5 md5;
+
+  size_t received = 0;
+
+  OperationEnvironment &env;
+
+  bool do_md5;
+
+public:
+  DownloadToFileHandler(FILE *_file, bool _do_md5, OperationEnvironment &_env)
+    :file(_file), env(_env), do_md5(_do_md5) {
+    if (do_md5)
+      md5.Initialise();
+  }
+
+  void GetDigest(char *digest) {
+    assert(do_md5);
+
+    md5.Finalize();
+    md5.GetDigest(digest);
+  }
+
+  void ResponseReceived(int64_t content_length) override {
+    if (content_length > 0)
+      env.SetProgressRange(content_length);
+  };
+
+  void DataReceived(const void *data, size_t length) override {
+    if (do_md5)
+      md5.Append(data, length);
+
+    size_t written = fwrite(data, 1, length, file);
+    if (written != (size_t)length)
+      throw CancelDownloadToFile();
+
+    received += length;
+
+    env.SetProgressRange(received);
+  };
+};
 
 static bool
 DownloadToFile(Net::Session &session, const char *url,
@@ -39,43 +86,19 @@ DownloadToFile(Net::Session &session, const char *url,
   assert(url != nullptr);
   assert(file != nullptr);
 
-  Net::Request request(session, url);
+  DownloadToFileHandler handler(file, md5_digest != nullptr, env);
+  Net::Request request(session, handler, url);
   if (username != nullptr)
     request.SetBasicAuth(username, password);
-  request.Send(10000);
 
-  int64_t total = request.GetLength();
-  if (total >= 0)
-    env.SetProgressRange(total);
-  total = 0;
-
-  MD5 md5;
-  md5.Initialise();
-
-  uint8_t buffer[4096];
-  while (true) {
-    if (env.IsCancelled())
-      return false;
-
-    size_t nbytes = request.Read(buffer, sizeof(buffer), 5000);
-    if (nbytes == 0)
-      break;
-
-    if (md5_digest != nullptr)
-      md5.Append(buffer, nbytes);
-
-    size_t written = fwrite(buffer, 1, nbytes, file);
-    if (written != (size_t)nbytes)
-      return false;
-
-    total += nbytes;
-    env.SetProgressPosition(total);
+  try {
+    request.Send(10000);
+  } catch (CancelDownloadToFile) {
+    return false;
   }
 
-  if (md5_digest != nullptr) {
-    md5.Finalize();
-    md5.GetDigest(md5_digest);
-  }
+  if (md5_digest != nullptr)
+    handler.GetDigest(md5_digest);
 
   return true;
 }

src/Net/HTTP/Handler.hpp
@@ -1,0 +1,44 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2016 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#ifndef NET_HTTP_RESPONSE_HANDLER_HPP
+#define NET_HTTP_RESPONSE_HANDLER_HPP
+
+#include "IO/DataHandler.hpp"
+
+#include <stdint.h>
+
+namespace Net {
+  class ResponseHandler : public DataHandler {
+  public:
+    /**
+     * Response metadata (status and headers) has been received.
+     *
+     * @param content_length the total length of the response body or -1 if
+     * the server did not announce a length
+     */
+    virtual void ResponseReceived(int64_t content_length) = 0;
+  };
+}
+
+#endif

src/Net/HTTP/Request.cpp
@@ -23,11 +23,13 @@ Copyright_License {
 
 #include "Request.hpp"
 #include "Session.hpp"
+#include "Handler.hpp"
 #include "Version.hpp"
 #include "Util/ConvertString.hpp"
 
-Net::Request::Request(Session &_session, const char *url)
-  :session(_session)
+Net::Request::Request(Session &_session, ResponseHandler &_handler,
+                      const char *url)
+  :session(_session), handler(_handler)
 {
   char user_agent[32];
   snprintf(user_agent, 32, "XCSoar/%s",
@@ -68,17 +70,10 @@ Net::Request::SetBasicAuth(const char *username, const char *password)
 size_t
 Net::Request::ResponseData(const uint8_t *ptr, size_t size)
 {
-  auto range = buffer.Write();
-  if (range.size < size) {
-    buffer.Shift();
-    range = buffer.Write();
-    if (range.size < size)
-      /* buffer is full, pause CURL */
-      return CURL_WRITEFUNC_PAUSE;
-  }
+  if (!submitted)
+    SubmitResponse();
 
-  std::copy_n(ptr, size, range.data);
-  buffer.Append(size);
+  handler.DataReceived(ptr, size);
   return size;
 }
 
@@ -99,46 +94,12 @@ Net::Request::Send(unsigned _timeout_ms)
   const int timeout_ms = _timeout_ms == INFINITE ? -1 : _timeout_ms;
 
   CURLMcode mcode = CURLM_CALL_MULTI_PERFORM;
-  while (buffer.IsEmpty()) {
-    CURLcode code = session.InfoRead(handle.GetHandle());
-    if (code != CURLE_AGAIN) {
-      if (code != CURLE_OK)
-        throw std::runtime_error(curl_easy_strerror(code));
-      return;
-    }
-
-    if (mcode != CURLM_CALL_MULTI_PERFORM)
-      session.Select(timeout_ms);
-
-    mcode = session.Perform();
-    if (mcode != CURLM_OK && mcode != CURLM_CALL_MULTI_PERFORM)
-      throw std::runtime_error(curl_multi_strerror(mcode));
-  }
-}
-
-int64_t
-Net::Request::GetLength() const
-{
-  return handle.GetContentLength();
-}
-
-size_t
-Net::Request::Read(void *_buffer, size_t buffer_size, unsigned _timeout_ms)
-{
-  const int timeout_ms = _timeout_ms == INFINITE ? -1 : _timeout_ms;
-
-  Buffer::Range range;
-  CURLMcode mcode = CURLM_CALL_MULTI_PERFORM;
   while (true) {
-    range = buffer.Read();
-    if (!range.IsEmpty())
-      break;
-
     CURLcode code = session.InfoRead(handle.GetHandle());
     if (code != CURLE_AGAIN) {
       if (code != CURLE_OK)
         throw std::runtime_error(curl_easy_strerror(code));
-      return 0;
+      break;
     }
 
     if (mcode != CURLM_CALL_MULTI_PERFORM)
@@ -149,16 +110,16 @@ Net::Request::Read(void *_buffer, size_t buffer_size, unsigned _timeout_ms)
       throw std::runtime_error(curl_multi_strerror(mcode));
   }
 
-  --buffer_size;
-  if (buffer_size > range.size)
-    buffer_size = range.size;
+  if (!submitted)
+    SubmitResponse();
+}
 
-  uint8_t *p = (uint8_t *)_buffer;
-  std::copy_n(range.data, buffer_size, p);
-  p[buffer_size] = 0;
+void
+Net::Request::SubmitResponse()
+{
+  assert(!submitted);
 
-  buffer.Consume(buffer_size);
-  handle.Unpause();
+  submitted = true;
 
-  return buffer_size;
+  handler.ResponseReceived(handle.GetContentLength());
 }

src/Net/HTTP/Request.hpp
@@ -24,17 +24,16 @@ Copyright_License {
 #ifndef NET_REQUEST_HPP
 #define NET_REQUEST_HPP
 
-#include "Util/StaticFifoBuffer.hpp"
 #include "Easy.hpp"
 
 #include <curl/curl.h>
 
 #include <stddef.h>
 #include <stdint.h>
-#include <sys/types.h>
 
 namespace Net {
   class Session;
+  class ResponseHandler;
 
   class Request {
 #ifndef WIN32
@@ -47,8 +46,13 @@ namespace Net {
 
     struct curl_slist *request_headers = nullptr;
 
-    typedef StaticFifoBuffer<uint8_t, CURL_MAX_WRITE_SIZE> Buffer;
-    Buffer buffer;
+    ResponseHandler &handler;
+
+    /**
+     * Was the response metadata already submitted to
+     * ResponseHandler::ResponseReceived().
+     */
+    bool submitted = false;
 
   public:
     /**
@@ -57,7 +61,8 @@ namespace Net {
      * @param url the absolute URL of the request
      * @param timeout_ms Timeout used for creating this request
      */
-    Request(Session &session, const char *url);
+    Request(Session &session, ResponseHandler &_handler,
+            const char *url);
 
     ~Request();
 
@@ -97,6 +102,9 @@ namespace Net {
      */
     size_t Read(void *buffer, size_t buffer_size,
                 unsigned timeout_ms=INFINITE);
+
+  private:
+    void SubmitResponse();
   };
 }
 

src/Net/HTTP/ToBuffer.cpp
@@ -23,9 +23,50 @@ Copyright_License {
 
 #include "ToBuffer.hpp"
 #include "Request.hpp"
+#include "Handler.hpp"
 #include "Operation/Operation.hpp"
 
 #include <stdint.h>
+#include <string.h>
+
+class CancelDownloadToBuffer {};
+
+class DownloadToBufferHandler final : public Net::ResponseHandler {
+  uint8_t *buffer;
+  const size_t max_size;
+
+  size_t received = 0;
+
+  OperationEnvironment &env;
+
+public:
+  DownloadToBufferHandler(void *_buffer, size_t _max_size,
+                          OperationEnvironment &_env)
+    :buffer((uint8_t *)_buffer), max_size(_max_size), env(_env) {}
+
+  size_t GetReceived() const {
+    return received;
+  }
+
+  void ResponseReceived(int64_t content_length) override {
+    if (content_length > 0)
+      env.SetProgressRange(content_length);
+  };
+
+  void DataReceived(const void *data, size_t length) override {
+    size_t remaining = max_size - received;
+    if (remaining == 0 || env.IsCancelled())
+      throw CancelDownloadToBuffer();
+
+    if (length > remaining)
+      length = remaining;
+
+    memcpy(buffer + received, data, length);
+    received += length;
+
+    env.SetProgressRange(received);
+  };
+};
 
 size_t
 Net::DownloadToBuffer(Session &session, const char *url,
@@ -33,32 +74,19 @@ Net::DownloadToBuffer(Session &session, const char *url,
                       void *_buffer, size_t max_length,
                       OperationEnvironment &env)
 {
-  Request request(session, url);
+  DownloadToBufferHandler handler(_buffer, max_length, env);
+  Request request(session, handler, url);
   if (username != nullptr)
     request.SetBasicAuth(username, password);
-  request.Send(10000);
 
-  int64_t total = request.GetLength();
-  if (total >= 0)
-    env.SetProgressRange(total);
-  total = 0;
-
-  uint8_t *buffer = (uint8_t *)_buffer, *p = buffer, *end = buffer + max_length;
-  while (p != end) {
+  try {
+    request.Send(10000);
+  } catch (CancelDownloadToBuffer) {
     if (env.IsCancelled())
       return -1;
-
-    size_t nbytes = request.Read(p, end - p, 5000);
-    if (nbytes == 0)
-      break;
-
-    p += nbytes;
-
-    total += nbytes;
-    env.SetProgressPosition(total);
   }
 
-  return p - buffer;
+  return handler.GetReceived();
 }
 
 void

test/src/DownloadFile.cpp
@@ -23,6 +23,7 @@ Copyright_License {
 
 #include "Net/HTTP/Session.hpp"
 #include "Net/HTTP/Request.hpp"
+#include "Net/HTTP/Handler.hpp"
 #include "Net/HTTP/Init.hpp"
 #include "OS/ConvertPathName.hpp"
 #include "Util/PrintException.hxx"
@@ -35,6 +36,23 @@ Copyright_License {
 
 using namespace std;
 
+class MyResponseHandler final : public Net::ResponseHandler {
+  FILE *const file;
+
+public:
+  explicit MyResponseHandler(FILE *_file):file(_file) {}
+
+  void ResponseReceived(int64_t content_length) override {
+  }
+
+  void DataReceived(const void *data, size_t length) override {
+    fwrite(data, 1, length, stdout);
+
+    if (file != nullptr)
+      fwrite(data, 1, length, file);
+  }
+};
+
 static void
 Download(const char *url, Path path)
 {
@@ -43,28 +61,16 @@ Download(const char *url, Path path)
   cout << "done" << endl;
 
   cout << "Creating Request ... ";
-  Net::Request request(session, url);
-  request.Send();
-
-  cout << "done" << endl;
-
-  cout << "Reading Response:" << endl;
-  cout << "-------------------------------------------------" << endl;
 
   FILE *file = path != nullptr ? _tfopen(path.c_str(), _T("wb")) : nullptr;
+  MyResponseHandler handler(file);
+  Net::Request request(session, handler, url);
+  cout << "done" << endl;
 
-  char buffer[256];
-  while (request.Read(buffer, sizeof(buffer))) {
-    cout << buffer;
-
-    if (file != NULL)
-      fputs(buffer, file);
-  }
+  request.Send();
 
   if (file != NULL)
     fclose(file);
-
-  cout << "-------------------------------------------------" << endl;
 }
 
 int

commit e6bfdfcb7b39c74b349b2f03e8e62de379b3254d

Net/HTTP/ToFile: fix progress display by calling SetProgressPosition()
Dumb typo, introduced in commit 234ce75d68c95b2a13241a872ec897b54427b56a

src/Net/HTTP/ToFile.cpp
@@ -72,7 +72,7 @@ public:
 
     received += length;
 
-    env.SetProgressRange(received);
+    env.SetProgressPosition(received);
   };
 };
 