4bb04a756769631674bb5c83bcc28b5ed09559a6
Max Kellermann <max.kellermann@gmail.com>
2020-06-04 10:42:39 +0200

    build/python/libs: update OpenSSL to 3.0.0-alpha2
    OpenSSL 3 switched to Apache2 license, which is compatible with GPLv3.

--- build/python/build/libs.py
+++ build/python/build/libs.py
@@ -54,9 +54,9 @@ libstdcxx_musl_headers = LibstdcxxMuslHeadersProject(
 )
 
 openssl = OpenSSLProject(
-    'https://www.openssl.org/source/openssl-1.0.2k.tar.gz',
-    'ftp://ftp.kfki.hu/pub/packages/security/openssl/openssl-1.0.2k.tar.gz',
-    '6b3977c61f2aedf0f96367dcfb5c6e578cf37e7b8d913b4ecb6643c3cb88d8c0',
+    'https://www.openssl.org/source/openssl-3.0.0-alpha2.tar.gz',
+    'ftp://ftp.cert.dfn.de/pub/tools/net/openssl/source/openssl-3.0.0-alpha2.tar.gz',
+    '9077d53d889f9708c261ee8a698df10575e2fd191de6924d89136b97dc8bc0c0',
     'include/openssl/ossl_typ.h',
 )
 
--- build/python/build/openssl.py
+++ build/python/build/openssl.py
@@ -5,23 +5,51 @@ from build.makeproject import MakeProject
 class OpenSSLProject(MakeProject):
     def __init__(self, url, alternative_url, md5, installed,
                  **kwargs):
-        MakeProject.__init__(self, url, alternative_url, md5, installed, install_target='install_sw', **kwargs)
-
-    def get_simultaneous_jobs(self):
-        return 1
+        MakeProject.__init__(self, url, alternative_url, md5, installed, install_target='install_dev', **kwargs)
 
     def get_make_args(self, toolchain):
         return MakeProject.get_make_args(self, toolchain) + [
             'CC=' + toolchain.cc,
-            'AR=' + toolchain.ar + ' r',
-            'RANLIB=arm-linux-gnueabihf-ranlib'
+            'CFLAGS=' + toolchain.cflags,
+            'CPPFLAGS=' + toolchain.cppflags,
+            'AR=' + toolchain.ar,
+            'RANLIB=' + toolchain.ranlib,
+            'build_libs',
         ]
 
     def build(self, toolchain):
         src = self.unpack(toolchain, out_of_tree=False)
 
-        subprocess.check_call(['/bin/sh', './Configure',
-                               'dist', 'no-dso', 'no-krb5',
+        # OpenSSL has a weird target architecture scheme with lots of
+        # hard-coded architectures; this table translates between our
+        # "toolchain_arch" (HOST_TRIPLET) and the OpenSSL target
+        openssl_archs = {
+            # not using "android-*" because those OpenSSL targets want
+            # to know where the SDK is, but our own build scripts
+            # prepared everything already to look like a regular Linux
+            # build
+            'arm-linux-androideabi': 'linux-generic32',
+            'aarch64-linux-android': 'linux-aarch64',
+            'i686-linux-android': 'linux-x86-clang',
+            'x86_64-linux-android': 'linux-x86_64-clang',
+
+            # Kobo
+            'arm-linux-gnueabihf': 'linux-generic32',
+
+            # Windows
+            'i686-w64-mingw32': 'mingw',
+            'x86_64-w64-mingw32': 'mingw64',
+        }
+
+        openssl_arch = openssl_archs[toolchain.toolchain_arch]
+
+        subprocess.check_call(['./Configure',
+                               'no-shared',
+                               'no-module', 'no-engine', 'no-static-engine',
+                               'no-async',
+                               'no-tests',
+                               'no-asm', # "asm" causes build failures on Windows
+                               openssl_arch,
                                '--prefix=' + toolchain.install_prefix],
                               cwd=src, env=toolchain.env)
-        MakeProject.build(self, toolchain, wd)
+        MakeProject.build(self, toolchain, src)
