a4e41fe8895d5ac3f9d60f0378e04fed22680117
Max Kellermann <max.kellermann@gmail.com>
2020-06-06 12:41:09 +0200

    build/detect.mk: add Raspberry Pi 4 detection

--- build/detect.mk
+++ build/detect.mk
@@ -29,8 +29,10 @@ endif
 ifeq ($(HOST_IS_LINUX)$(HOST_IS_ARM_OR_AARCH64),yy)
 # Check for VideoCore headers present on a Raspberry Pi
 HOST_IS_PI := $(call string_contains,$(shell cat /sys/firmware/devicetree/base/model 2>/dev/null),Raspberry)
+HOST_IS_PI4 := $(call string_contains,$(shell cat /sys/firmware/devicetree/base/model 2>/dev/null),Raspberry Pi 4)
 else
 HOST_IS_PI := n
+HOST_IS_PI4 := n
 endif
 
 ifeq ($(HOST_IS_LINUX)$(HOST_IS_ARMV7),yy)
