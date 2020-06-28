12bd99ce5751555be9551063479acbca38572d5b
Max Kellermann <max.kellermann@gmail.com>
2020-06-28 15:15:02 +0200

    build/targets.mk: use Android NDK r21d

--- build/targets.mk
+++ build/targets.mk
@@ -320,7 +320,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r21b
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r21d
 
   ANDROID_SDK_PLATFORM = android-26
   ANDROID_NDK_API = 21
