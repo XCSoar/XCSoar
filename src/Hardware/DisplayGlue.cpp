0fd357a47b0ba13ab24613f160e6ef57559dbc95
Andrey Lebedev <andrey@lebedev.lt>
2020-07-10 00:02:30 +0300

    Autodetect display orientation for DRM/KMS mode
    When XCSoar is running in KMS mode, assume that linux console is
    oriented the same way as physical display.

--- src/Hardware/DisplayGlue.cpp
+++ src/Hardware/DisplayGlue.cpp
@@ -27,6 +27,8 @@ Copyright_License {
 #include "LogFile.hpp"
 #include "Interface.hpp"
 #include "MainWindow.hpp"
+#include "OS/Path.hpp"
+#include "OS/FileUtil.hpp"
 
 #ifdef KOBO
 #include "Event/Globals.hpp"
@@ -92,5 +94,21 @@ Display::RestoreOrientation()
 DisplayOrientation
 Display::DetectInitialOrientation()
 {
-  return DisplayOrientation::DEFAULT;
+  auto orientation = DisplayOrientation::DEFAULT;
+
+#ifdef MESA_KMS
+  // When running in DRM/KMS mode, infer the display orientation from the linux
+  // console rotation.
+  char buf[3];
+  auto rotatepath = Path("/sys/class/graphics/fbcon/rotate");
+  if (File::ReadString(rotatepath, buf, sizeof(buf))) {
+    switch (*buf) {
+    case '0': orientation = DisplayOrientation::LANDSCAPE; break;
+    case '1': orientation = DisplayOrientation::REVERSE_PORTRAIT; break;
+    case '2': orientation = DisplayOrientation::REVERSE_LANDSCAPE; break;
+    case '3': orientation = DisplayOrientation::PORTRAIT; break;
+    }
+  }
+#endif
+  return orientation;
 }
