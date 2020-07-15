073a79d40dbd8281e55ef5265bd847bcf3ae27ab
Max Kellermann <max.kellermann@gmail.com>
2020-07-15 16:27:26 +0200

    Task/Shapes/FAITriangleRules: no indent inside namespace

--- src/Engine/Task/Shapes/FAITriangleRules.hpp
+++ src/Engine/Task/Shapes/FAITriangleRules.hpp
@@ -29,58 +29,59 @@ Copyright_License {
 struct GeoPoint;
 struct FAITriangleSettings;
 
-namespace FAITriangleRules
-{
-  /**
-   * The minimum leg percentage for "small FAI triangles".
-   */
-  static constexpr double SMALL_MIN_LEG(0.28);
+namespace FAITriangleRules {
 
-  /**
-   * The maximum leg percentage for "small FAI triangles".  This is a
-   * derived value, assuming the other two legs are as short as
-   * possible.
-   */
-  static constexpr double SMALL_MAX_LEG(1 - 2 * SMALL_MIN_LEG);
+/**
+ * The minimum leg percentage for "small FAI triangles".
+ */
+static constexpr double SMALL_MIN_LEG(0.28);
 
-  /**
-   * The threshold which allows applying the "large FAI triangle"
-   * rules [m].
-   *
-   * Don't use this variable.  Use FAITriangleSettings::GetThreshold()
-   * instead.
-   *
-   * @see FAITriangleSettings::Threshold::FAI
-   */
-  static constexpr double LARGE_THRESHOLD_FAI(750000);
+/**
+ * The maximum leg percentage for "small FAI triangles".  This is a
+ * derived value, assuming the other two legs are as short as
+ * possible.
+ */
+static constexpr double SMALL_MAX_LEG(1 - 2 * SMALL_MIN_LEG);
 
-  /**
-   * Relaxed threshold used by some contests such as OLC and DMSt.
-   *
-   * Don't use this variable.  Use FAITriangleSettings::GetThreshold()
-   * instead.
-   *
-   * @see FAITriangleSettings::Threshold::KM500
-   */
-  static constexpr double LARGE_THRESHOLD_500(500000);
+/**
+ * The threshold which allows applying the "large FAI triangle"
+ * rules [m].
+ *
+ * Don't use this variable.  Use FAITriangleSettings::GetThreshold()
+ * instead.
+ *
+ * @see FAITriangleSettings::Threshold::FAI
+ */
+static constexpr double LARGE_THRESHOLD_FAI(750000);
 
-  /**
-   * The minimum leg percentage for "large FAI triangles".
-   */
-  static constexpr double LARGE_MIN_LEG(0.25);
+/**
+ * Relaxed threshold used by some contests such as OLC and DMSt.
+ *
+ * Don't use this variable.  Use FAITriangleSettings::GetThreshold()
+ * instead.
+ *
+ * @see FAITriangleSettings::Threshold::KM500
+ */
+static constexpr double LARGE_THRESHOLD_500(500000);
 
-  /**
-   * The maximum leg percentage for "large FAI triangles".
-   */
-  static constexpr double LARGE_MAX_LEG(0.45);
+/**
+ * The minimum leg percentage for "large FAI triangles".
+ */
+static constexpr double LARGE_MIN_LEG(0.25);
 
-  gcc_pure
-  bool TestDistances(double d1, double d2, double d3,
-                     const FAITriangleSettings &settings) noexcept;
+/**
+ * The maximum leg percentage for "large FAI triangles".
+ */
+static constexpr double LARGE_MAX_LEG(0.45);
 
-  gcc_pure
-  bool TestDistances(const GeoPoint &a, const GeoPoint &b, const GeoPoint &c,
-                     const FAITriangleSettings &settings) noexcept;
-}
+gcc_pure
+bool TestDistances(double d1, double d2, double d3,
+                   const FAITriangleSettings &settings) noexcept;
+
+gcc_pure
+bool TestDistances(const GeoPoint &a, const GeoPoint &b, const GeoPoint &c,
+                   const FAITriangleSettings &settings) noexcept;
+
+} // namespace FAITriangleRules
 
 #endif
