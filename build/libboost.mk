4a10f59a74bc2fd499976f9643417153e814d796
Max Kellermann <max.kellermann@gmail.com>
2020-09-22 14:48:02 +0200

    build/libboost.mk: update Boost to 1.74.0

--- build/libboost.mk
+++ build/libboost.mk
@@ -1,6 +1,6 @@
-BOOST_URL = https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2
-BOOST_ALTERNATIVE_URL = https://sourceforge.net/projects/boost/files/boost/1.73.0/boost_1_73_0.tar.bz2/download
-BOOST_MD5 = 4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402
+BOOST_URL = https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2
+BOOST_ALTERNATIVE_URL = https://sourceforge.net/projects/boost/files/boost/1.74.0/boost_1_74_0.tar.bz2/download
+BOOST_MD5 = 83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1
 
 BOOST_TARBALL_NAME = $(notdir $(BOOST_URL))
 BOOST_TARBALL = $(DOWNLOAD_DIR)/$(BOOST_TARBALL_NAME)
