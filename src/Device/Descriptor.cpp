XCSoar/XCSoar

commit ff51c9507de8503d4f1a5e6b4723a326c2cf3b2b

Device/Port/Configured: return std::unique_ptr

src/Device/Descriptor.cpp
@@ -126,6 +126,11 @@ DeviceDescriptor::DeviceDescriptor(boost::asio::io_context &_io_context,
 #endif
 }
 
+DeviceDescriptor::~DeviceDescriptor() noexcept
+{
+  assert(!IsOccupied());
+}
+
 void
 DeviceDescriptor::SetConfig(const DeviceConfig &_config)
 {
@@ -235,8 +240,8 @@ DeviceDescriptor::CancelAsync()
   open_job = nullptr;
 }
 
-bool
-DeviceDescriptor::OpenOnPort(DumpPort *_port, OperationEnvironment &env)
+inline bool
+DeviceDescriptor::OpenOnPort(std::unique_ptr<DumpPort> &&_port, OperationEnvironment &env)
 {
   assert(port == nullptr);
   assert(device == nullptr);
@@ -257,7 +262,7 @@ DeviceDescriptor::OpenOnPort(DumpPort *_port, OperationEnvironment &env)
   settings_received.Clear();
   was_alive = false;
 
-  port = _port;
+  port = std::move(_port);
 
   parser.Reset();
   parser.SetReal(!StringIsEqual(driver->name, _T("Condor")));
@@ -461,7 +466,7 @@ try {
 
   reopen_clock.Update();
 
-  Port *port;
+  std::unique_ptr<Port> port;
   try {
     port = OpenPort(io_context, config, this, *this);
   } catch (...) {
@@ -497,14 +502,13 @@ try {
     return false;
   }
 
-  DumpPort *dump_port = new DumpPort(port);
+  auto dump_port = std::make_unique<DumpPort>(std::move(port));
   dump_port->Disable();
 
-  if (!port->WaitConnected(env) || !OpenOnPort(dump_port, env)) {
+  if (!port->WaitConnected(env) || !OpenOnPort(std::move(dump_port), env)) {
     if (!env.IsCancelled())
       ++n_failures;
 
-    delete dump_port;
     return false;
   }
 
@@ -588,9 +592,7 @@ DeviceDescriptor::Close()
   delete second_device;
   second_device = nullptr;
 
-  Port *old_port = port;
-  port = nullptr;
-  delete old_port;
+  port.reset();
 
   ticker = false;
 
@@ -781,7 +783,7 @@ DeviceDescriptor::ForwardLine(const char *line)
      any thread, and if the Port gets closed, bad things happen */
 
   if (IsNMEAOut() && port != nullptr) {
-    Port *p = port;
+    Port *p = port.get();
     p->Write(line);
     p->Write("\r\n");
   }

src/Device/Descriptor.hpp
@@ -42,6 +42,7 @@ Copyright_License {
 #include "Android/GliderLink.hpp"
 
 #include <chrono>
+#include <memory>
 
 #include <cassert>
 #include <tchar.h>
@@ -113,7 +114,7 @@ class DeviceDescriptor final : PortListener, PortLineSplitter {
    * The #Port used by this device.  This is not applicable to some
    * devices, and is nullptr in that case.
    */
-  DumpPort *port;
+  std::unique_ptr<DumpPort> port;
 
   /**
    * A handler that will receive all data, to display it on the
@@ -238,9 +239,7 @@ class DeviceDescriptor final : PortListener, PortLineSplitter {
 public:
   DeviceDescriptor(boost::asio::io_context &_io_context,
                    unsigned index, PortListener *port_listener);
-  ~DeviceDescriptor() {
-    assert(!IsOccupied());
-  }
+  ~DeviceDescriptor() noexcept;
 
   unsigned GetIndex() const {
     return index;
@@ -331,7 +330,7 @@ private:
    * Port object.
    */
   gcc_nonnull_all
-  bool OpenOnPort(DumpPort *port, OperationEnvironment &env);
+  bool OpenOnPort(std::unique_ptr<DumpPort> &&port, OperationEnvironment &env);
 
   bool OpenInternalSensors();
 

src/Device/Port/AndroidBluetoothPort.cpp
@@ -28,7 +28,7 @@ Copyright_License {
 
 #include <cassert>
 
-Port *
+std::unique_ptr<Port>
 OpenAndroidBluetoothPort(const TCHAR *address, PortListener *listener,
                          DataHandler &handler)
 {
@@ -38,15 +38,15 @@ OpenAndroidBluetoothPort(const TCHAR *address, PortListener *listener,
   if (bridge == nullptr)
     return nullptr;
 
-  return new AndroidPort(listener, handler, bridge);
+  return std::make_unique<AndroidPort>(listener, handler, bridge);
 }
 
-Port *
+std::unique_ptr<Port>
 OpenAndroidBluetoothServerPort(PortListener *listener, DataHandler &handler)
 {
   PortBridge *bridge = BluetoothHelper::createServer(Java::GetEnv());
   if (bridge == nullptr)
     return nullptr;
 
-  return new AndroidPort(listener, handler, bridge);
+  return std::make_unique<AndroidPort>(listener, handler, bridge);
 }

src/Device/Port/AndroidBluetoothPort.hpp
@@ -26,19 +26,19 @@ Copyright_License {
 
 #include "util/Compiler.h"
 
+#include <memory>
+
 #include <tchar.h>
 
 class Port;
 class PortListener;
 class DataHandler;
 
-gcc_malloc
-Port *
+std::unique_ptr<Port>
 OpenAndroidBluetoothPort(const TCHAR *address, PortListener *_listener,
                          DataHandler &_handler);
 
-gcc_malloc
-Port *
+std::unique_ptr<Port>
 OpenAndroidBluetoothServerPort(PortListener *_listener, DataHandler &_handler);
 
 #endif

src/Device/Port/AndroidIOIOUartPort.cpp
@@ -29,7 +29,7 @@ Copyright_License {
 
 #include <cassert>
 
-Port *
+std::unique_ptr<Port>
 OpenAndroidIOIOUartPort(unsigned uart_id, unsigned baud_rate,
                         PortListener *listener, DataHandler &handler)
 {
@@ -40,5 +40,5 @@ OpenAndroidIOIOUartPort(unsigned uart_id, unsigned baud_rate,
   if (bridge == nullptr)
     return nullptr;
 
-  return new AndroidPort(listener, handler, bridge);
+  return std::make_unique<AndroidPort>(listener, handler, bridge);
 }

src/Device/Port/AndroidIOIOUartPort.hpp
@@ -26,6 +26,8 @@ Copyright_License {
 
 #include "util/Compiler.h"
 
+#include <memory>
+
 #include <tchar.h>
 
 class Port;
@@ -52,8 +54,7 @@ namespace AndroidIOIOUartPort
   }
 }
 
-gcc_malloc
-Port *
+std::unique_ptr<Port>
 OpenAndroidIOIOUartPort(unsigned uart_id, unsigned baud_rate,
                         PortListener *listener, DataHandler &handler);
 

src/Device/Port/ConfiguredPort.cpp
@@ -66,22 +66,23 @@ DetectGPS(TCHAR *path, size_t path_max_size)
   return false;
 }
 
-static Port *
+static std::unique_ptr<Port>
 WrapPort(const DeviceConfig &config, PortListener *listener,
-         DataHandler &handler, Port *port)
+         DataHandler &handler, std::unique_ptr<Port> port) noexcept
 {
   if (config.k6bt && config.MaybeBluetooth())
-    port = new K6BtPort(port, config.baud_rate, listener, handler);
+    port = std::make_unique<K6BtPort>(std::move(port), config.baud_rate,
+                                      listener, handler);
 
 #ifndef NDEBUG
   if (config.dump_port)
-    port = new DumpPort(port);
+    port = std::make_unique<DumpPort>(std::move(port));
 #endif
 
   return port;
 }
 
-static Port *
+static std::unique_ptr<Port>
 OpenPortInternal(boost::asio::io_context &io_context,
                  const DeviceConfig &config, PortListener *listener,
                  DataHandler &handler)
@@ -150,21 +151,20 @@ OpenPortInternal(boost::asio::io_context &io_context,
     if (!ip_address.IsValid())
       throw std::runtime_error("No IP address configured");
 
-    auto port = new TCPClientPort(io_context, listener, handler);
-    if (!port->Connect(ip_address, config.tcp_port)) {
-      delete port;
+    auto port = std::make_unique<TCPClientPort>(io_context, listener, handler);
+    if (!port->Connect(ip_address, config.tcp_port))
       return nullptr;
-    }
 
     return port;
   }
 
   case DeviceConfig::PortType::TCP_LISTENER:
-    return new TCPPort(io_context, config.tcp_port,
-                       listener, handler);
+    return std::make_unique<TCPPort>(io_context, config.tcp_port,
+                                     listener, handler);
 
   case DeviceConfig::PortType::UDP_LISTENER:
-    return new UDPPort(io_context, config.tcp_port, listener, handler);
+    return std::make_unique<UDPPort>(io_context, config.tcp_port,
+                                     listener, handler);
 
   case DeviceConfig::PortType::PTY: {
 #if defined(HAVE_POSIX) && !defined(ANDROID)
@@ -176,12 +176,10 @@ OpenPortInternal(boost::asio::io_context &io_context,
                                               std::system_category()),
                               "Failed to delete pty");
 
-    TTYPort *port = new TTYPort(io_context, listener, handler);
+    auto port = std::make_unique<TTYPort>(io_context, listener, handler);
     const char *slave_path = port->OpenPseudo();
-    if (slave_path == nullptr) {
-      delete port;
+    if (slave_path == nullptr)
       return nullptr;
-    }
 
     if (symlink(slave_path, config.path.c_str()) < 0)
       throw std::system_error(std::error_code(errno,
@@ -199,25 +197,23 @@ OpenPortInternal(boost::asio::io_context &io_context,
     throw std::runtime_error("No port path configured");
 
 #ifdef HAVE_POSIX
-  TTYPort *port = new TTYPort(io_context, listener, handler);
+  auto port = std::make_unique<TTYPort>(io_context, listener, handler);
 #else
-  SerialPort *port = new SerialPort(listener, handler);
+  auto port = std::make_unique<SerialPort>(listener, handler);
 #endif
-  if (!port->Open(path, config.baud_rate)) {
-    delete port;
+  if (!port->Open(path, config.baud_rate))
     return nullptr;
-  }
 
   return port;
 }
 
-Port *
+std::unique_ptr<Port>
 OpenPort(boost::asio::io_context &io_context,
          const DeviceConfig &config, PortListener *listener,
          DataHandler &handler)
 {
-  Port *port = OpenPortInternal(io_context, config, listener, handler);
+  auto port = OpenPortInternal(io_context, config, listener, handler);
   if (port != nullptr)
-    port = WrapPort(config, listener, handler, port);
+    port = WrapPort(config, listener, handler, std::move(port));
   return port;
 }

src/Device/Port/ConfiguredPort.hpp
@@ -24,6 +24,8 @@ Copyright_License {
 #ifndef XCSOAR_DEVICE_CONFIGURED_PORT_HPP
 #define XCSOAR_DEVICE_CONFIGURED_PORT_HPP
 
+#include <memory>
+
 class Port;
 class PortListener;
 class DataHandler;
@@ -34,7 +36,7 @@ namespace boost { namespace asio { class io_context; }}
  * Open the port described by #DeviceConfig.  On error, throws a
  * #std::runtime_error or returns nullptr.
  */
-Port *
+std::unique_ptr<Port>
 OpenPort(boost::asio::io_context &io_context,
          const DeviceConfig &config, PortListener *listener,
          DataHandler &handler);

commit de93b779a0948337e3c497eee429c65f173d2179

Device/Descriptor: fix use-after-move bug
Regression by commit ff51c9507de8503d4f1a5e6b4723a326c2cf3b2b

src/Device/Descriptor.cpp
@@ -502,10 +502,17 @@ try {
     return false;
   }
 
+  if (!port->WaitConnected(env)) {
+    if (!env.IsCancelled())
+      ++n_failures;
+
+    return false;
+  }
+
   auto dump_port = std::make_unique<DumpPort>(std::move(port));
   dump_port->Disable();
 
-  if (!port->WaitConnected(env) || !OpenOnPort(std::move(dump_port), env)) {
+  if (!OpenOnPort(std::move(dump_port), env)) {
     if (!env.IsCancelled())
       ++n_failures;
 