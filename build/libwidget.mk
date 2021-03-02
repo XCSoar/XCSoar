1b0cf2d0e8e63608bc8e0e4091356589473d0925
Max Kellermann <max.kellermann@gmail.com>
2021-03-02 22:28:48 +0100

    Widget/DockWindow: remove unused class

--- build/libwidget.mk
+++ build/libwidget.mk
@@ -26,8 +26,7 @@ WIDGET_SOURCES = \
 	$(SRC)/Widget/ButtonWidget.cpp \
 	$(SRC)/Widget/QuestionWidget.cpp \
 	$(SRC)/Widget/KeyboardWidget.cpp \
-	$(SRC)/Widget/ViewImageWidget.cpp \
-	$(SRC)/Widget/DockWindow.cpp
+	$(SRC)/Widget/ViewImageWidget.cpp
 
 WIDGET_CPPFLAGS_INTERNAL = $(SCREEN_CPPFLAGS)
 
