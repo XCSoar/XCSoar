90c8af5eb1977e7aaae8191f882e203e3221d414
Ronald Niederhagen <ronald_niederhagen@freenet.de>
2021-03-04 11:22:00 +0100

    Revert "Update ProtectedTaskManager.cpp"
    This reverts commit f043a6ac2b9ce23a7a21fa617e61a723ad3f35b4.

--- src/Task/ProtectedTaskManager.cpp
+++ src/Task/ProtectedTaskManager.cpp
@@ -25,7 +25,8 @@ Copyright_License {
 #include "Task/RoutePlannerGlue.hpp"
 #include "Engine/Task/TaskManager.hpp"
 #include "Engine/Task/Ordered/OrderedTask.hpp"
-#include "Engine/Task/Ordered/Points/IntermediatePoint.hpp"
+#include "Engine/Task/Ordered/Points/ASTPoint.hpp"
+#include "Engine/Task/Ordered/Points/AATPoint.hpp"
 #include "Engine/Task/Points/TaskWaypoint.hpp"
 #include "Engine/Route/ReachResult.hpp"
 
@@ -85,13 +86,14 @@ ProtectedTaskManager::IncrementActiveTaskPointArm(int offset)
 {
   ExclusiveLease lease(*this);
   TaskAdvance &advance = lease->SetTaskAdvance();
-  IntermediateTaskPoint *nextwp = nullptr;
+  TaskWaypoint *twp = nullptr;
 
   switch (advance.GetState()) {
   case TaskAdvance::MANUAL:
-  case TaskAdvance::AUTO:
-    lease->IncrementActiveTaskPoint(offset);
-    nextwp = (IntermediateTaskPoint *)lease->GetActiveTaskPoint();
+  case TaskAdvance::AUTO: {
+      lease->IncrementActiveTaskPoint(offset);
+      twp = lease->GetActiveTaskPoint();
+    }
     break;
   case TaskAdvance::START_DISARMED:
   case TaskAdvance::TURN_DISARMED:
@@ -99,21 +101,35 @@ ProtectedTaskManager::IncrementActiveTaskPointArm(int offset)
       advance.SetArmed(true);
     } else {
       lease->IncrementActiveTaskPoint(offset);
-      nextwp = (IntermediateTaskPoint *)lease->GetActiveTaskPoint();
+      twp = lease->GetActiveTaskPoint();
     }
     break;
   case TaskAdvance::START_ARMED:
   case TaskAdvance::TURN_ARMED:
     if (offset>0) {
       lease->IncrementActiveTaskPoint(offset);
-      nextwp = (IntermediateTaskPoint *)lease->GetActiveTaskPoint();
+      twp = lease->GetActiveTaskPoint();
     } else {
       advance.SetArmed(false);
     }
     break;
   }
-
-  if(nextwp) nextwp->Reset();
+  if (twp) {
+    switch (twp->GetType()) {
+    case TaskPointType::AST : {
+      ASTPoint *p = (ASTPoint *)twp;
+      if (p->HasEntered()) p->Reset();
+      }
+      break;
+    case TaskPointType::AAT : {
+      AATPoint *p = (AATPoint *)twp;
+      if (p->HasEntered()) p->Reset();
+      }
+      break;
+    default:
+      break;
+    }
+  }
 }
 
 bool 
