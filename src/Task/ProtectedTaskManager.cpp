a9452c35e8e66eccbd2c80d6670dd5d06222801d
Ronald Niederhagen <ronald_niederhagen@freenet.de>
2021-03-04 11:30:38 +0100

    Fix adjust to previous task point
    When manually adjusting to the previous turn point it must forget that it had visited that task point before. If it finds this TP had been visited before it auto-increments to the next TP. Rev2

--- src/Task/ProtectedTaskManager.cpp
+++ src/Task/ProtectedTaskManager.cpp
@@ -25,8 +25,7 @@ Copyright_License {
 #include "Task/RoutePlannerGlue.hpp"
 #include "Engine/Task/TaskManager.hpp"
 #include "Engine/Task/Ordered/OrderedTask.hpp"
-#include "Engine/Task/Ordered/Points/ASTPoint.hpp"
-#include "Engine/Task/Ordered/Points/AATPoint.hpp"
+#include "Engine/Task/Ordered/Points/IntermediatePoint.hpp"
 #include "Engine/Task/Points/TaskWaypoint.hpp"
 #include "Engine/Route/ReachResult.hpp"
 
@@ -86,14 +85,13 @@ ProtectedTaskManager::IncrementActiveTaskPointArm(int offset)
 {
   ExclusiveLease lease(*this);
   TaskAdvance &advance = lease->SetTaskAdvance();
-  TaskWaypoint *twp = nullptr;
+  IntermediateTaskPoint *nextwp = nullptr;
 
   switch (advance.GetState()) {
   case TaskAdvance::MANUAL:
-  case TaskAdvance::AUTO: {
-      lease->IncrementActiveTaskPoint(offset);
-      twp = lease->GetActiveTaskPoint();
-    }
+  case TaskAdvance::AUTO:
+    lease->IncrementActiveTaskPoint(offset);
+    nextwp = (IntermediateTaskPoint *)lease->GetActiveTaskPoint();
     break;
   case TaskAdvance::START_DISARMED:
   case TaskAdvance::TURN_DISARMED:
@@ -101,35 +99,21 @@ ProtectedTaskManager::IncrementActiveTaskPointArm(int offset)
       advance.SetArmed(true);
     } else {
       lease->IncrementActiveTaskPoint(offset);
-      twp = lease->GetActiveTaskPoint();
+      nextwp = (IntermediateTaskPoint *)lease->GetActiveTaskPoint();
     }
     break;
   case TaskAdvance::START_ARMED:
   case TaskAdvance::TURN_ARMED:
     if (offset>0) {
       lease->IncrementActiveTaskPoint(offset);
-      twp = lease->GetActiveTaskPoint();
+      nextwp = (IntermediateTaskPoint *)lease->GetActiveTaskPoint();
     } else {
       advance.SetArmed(false);
     }
     break;
   }
-  if (twp) {
-    switch (twp->GetType()) {
-    case TaskPointType::AST : {
-      ASTPoint *p = (ASTPoint *)twp;
-      if (p->HasEntered()) p->Reset();
-      }
-      break;
-    case TaskPointType::AAT : {
-      AATPoint *p = (AATPoint *)twp;
-      if (p->HasEntered()) p->Reset();
-      }
-      break;
-    default:
-      break;
-    }
-  }
+
+  if(nextwp) nextwp->Reset();
 }
 
 bool 
