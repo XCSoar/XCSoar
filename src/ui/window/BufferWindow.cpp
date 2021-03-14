XCSoar/XCSoar

commit aa01a7c94d9f94491dc114c6c949e760b7cc1000

ui/window/BufferWindow: create the BufferCanvas lazily

src/ui/window/BufferWindow.cpp
@@ -27,29 +27,6 @@ Copyright_License {
 #include "ui/canvas/WindowCanvas.hpp"
 #endif
 
-#ifndef ENABLE_OPENGL
-
-void
-BufferWindow::OnCreate()
-{
-  PaintWindow::OnCreate();
-
-  dirty = true;
-
-  WindowCanvas a_canvas(*this);
-  buffer.Create(a_canvas);
-}
-
-void
-BufferWindow::OnDestroy()
-{
-  PaintWindow::OnDestroy();
-
-  buffer.Destroy();
-}
-
-#endif
-
 void
 BufferWindow::OnResize(PixelSize new_size)
 {
@@ -66,12 +43,12 @@ BufferWindow::OnResize(PixelSize new_size)
 void
 BufferWindow::OnPaint(Canvas &canvas)
 {
-#ifdef ENABLE_OPENGL
   if (!buffer.IsDefined()) {
-    buffer.Create(canvas.GetSize());
+    buffer.Create(canvas);
     dirty = true;
   }
 
+#ifdef ENABLE_OPENGL
   if (dirty) {
     dirty = false;
     buffer.Begin(canvas);

src/ui/window/BufferWindow.hpp
@@ -65,11 +65,6 @@ protected:
 
 protected:
   /* virtual methods from class Window */
-#ifndef ENABLE_OPENGL
-  void OnCreate() override;
-  void OnDestroy() override;
-#endif
-
   void OnResize(PixelSize new_size) override;
 
   /* virtual methods from class PaintWindow */

commit aa11b96f0543b1b1f2e6c8533b84f41a3d5438cf

ui/window/BufferWindow: add check to OnResize()
Fixes assertion failure introduced by commit aa01a7c94d9f9449

src/ui/window/BufferWindow.cpp
@@ -30,12 +30,14 @@ Copyright_License {
 void
 BufferWindow::OnResize(PixelSize new_size)
 {
+  if (buffer.IsDefined()) {
 #ifdef ENABLE_OPENGL
-  buffer.Destroy();
+    buffer.Destroy();
 #else
-  buffer.Resize(new_size);
-  Invalidate();
+    buffer.Resize(new_size);
+    Invalidate();
 #endif
+  }
 
   PaintWindow::OnResize(new_size);
 }