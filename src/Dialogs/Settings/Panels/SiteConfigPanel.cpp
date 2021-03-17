d063bdb07db456b4d8225c90b2ed82a6c7f6d738
CptFrikadel <adkupper@gmail.com>
2021-03-17 18:21:11 +0100

    Change SiteConfigPanel to use AddMultipleFiles for airspaces.
    Use a single AddMultipleFiles field instead of the two 'airspace'
    and 'more airspace' fields.

--- src/Dialogs/Settings/Panels/SiteConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/SiteConfigPanel.cpp
@@ -38,8 +38,7 @@ enum ControlIndex {
   WaypointFile,
   AdditionalWaypointFile,
   WatchedWaypointFile,
-  AirspaceFile,
-  AdditionalAirspaceFile,
+  AirspaceFiles,
   AirfieldFile,
   FlarmFile
 };
@@ -90,14 +89,11 @@ SiteConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc) noexcept
           FileType::WAYPOINT);
   SetExpertRow(WatchedWaypointFile);
 
-  AddFile(_("Airspaces"), _("The file name of the primary airspace file."),
-          ProfileKeys::AirspaceFile, _T("*.txt\0*.air\0*.sua\0"),
-          FileType::AIRSPACE);
-
-  AddFile(_("More airspaces"), _("The file name of the secondary airspace file."),
-          ProfileKeys::AdditionalAirspaceFile, _T("*.txt\0*.air\0*.sua\0"),
-          FileType::AIRSPACE);
-  SetExpertRow(AdditionalAirspaceFile);
+  AddMultipleFiles(_("Airspace Files"),
+		  _("List of active airspace files. Use the Add and Remove buttons to activate or deactivate "
+			  " airspace files respectively. Supported file types are: Openair (.txt /.air), and Tim Newport-Pearce (.sua). "),
+		  ProfileKeys::AirspaceFileList, _T("*.txt\0*.air\0*.sua\0"),
+		  FileType::AIRSPACE, false);
 
   AddFile(_("Waypoint details"),
           _("The file may contain extracts from enroute supplements or other contributed "
@@ -124,8 +120,7 @@ SiteConfigPanel::Save(bool &_changed) noexcept
   WaypointFileChanged |= SaveValueFileReader(AdditionalWaypointFile, ProfileKeys::AdditionalWaypointFile);
   WaypointFileChanged |= SaveValueFileReader(WatchedWaypointFile, ProfileKeys::WatchedWaypointFile);
 
-  AirspaceFileChanged = SaveValueFileReader(AirspaceFile, ProfileKeys::AirspaceFile);
-  AirspaceFileChanged |= SaveValueFileReader(AdditionalAirspaceFile, ProfileKeys::AdditionalAirspaceFile);
+  AirspaceFileChanged |= SaveValueMultiFileReader(AirspaceFiles, ProfileKeys::AirspaceFileList);
 
   FlarmFileChanged = SaveValueFileReader(FlarmFile, ProfileKeys::FlarmFile);
 
