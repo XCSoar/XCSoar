faa31c2455c5054d067d0f0a87f4d958d9f309c2
Max Kellermann <max.kellermann@gmail.com>
2021-03-23 21:55:24 +0100

    Terrain/jasper: update to 2.0.25

--- src/Terrain/jasper/base/jas_stream.c
+++ src/Terrain/jasper/base/jas_stream.c
@@ -557,7 +557,7 @@ jas_stream_t *jas_stream_fdopen(int fd, const char *mode)
 	/* Parse the mode string. */
 	stream->openmode_ = jas_strtoopenmode(mode);
 
-#if defined(WIN32)
+#if defined(WIN32) || defined(JAS_HAVE_IO_H)
 	/* Argh!!!  Someone ought to banish text mode (i.e., O_TEXT) to the
 	  greatest depths of purgatory! */
 	/* Ensure that the file descriptor is in binary mode, if the caller
