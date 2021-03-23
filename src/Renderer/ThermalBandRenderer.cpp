6e1c9798284b662c415a228a1c3c3c61ef29f5e5
Max Kellermann <max.kellermann@gmail.com>
2021-03-23 18:14:16 +0100

    Renderer/ThermalBand: worst-case allocation, no std::vector

--- src/Renderer/ThermalBandRenderer.cpp
+++ src/Renderer/ThermalBandRenderer.cpp
@@ -31,6 +31,7 @@ Copyright_License {
 #include "Units/Units.hpp"
 #include "Screen/Layout.hpp"
 #include "Math/LeastSquares.hpp"
+#include "util/StaticArray.hxx"
 
 #ifdef ENABLE_OPENGL
 #include "ui/canvas/opengl/Scope.hpp"
@@ -78,8 +79,7 @@ ThermalBandRenderer::DrawThermalProfile(const ThermalBand &thermal_band,
   const unsigned renderable = thermal_band.size();
   // position of thermal band
 
-  std::vector<std::pair<double, double>> thermal_profile;
-  thermal_profile.reserve(renderable);
+  StaticArray<std::pair<double, double>, ThermalBand::max_size()> thermal_profile;
 
   // add elements, with smoothing filter
   for (unsigned i = 0; i < renderable; ++i) {
