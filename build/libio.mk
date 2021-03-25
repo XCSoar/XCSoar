36d687b8bd24849d45cbc47e5427571439c161e9
Max Kellermann <max.kellermann@gmail.com>
2021-03-25 16:20:45 +0100

    io/ConvertLineReader: move code to class StringConverter

--- build/libio.mk
+++ build/libio.mk
@@ -15,6 +15,7 @@ IO_SOURCES = \
 	$(IO_SRC_DIR)/FileCache.cpp \
 	$(IO_SRC_DIR)/ZipArchive.cpp \
 	$(IO_SRC_DIR)/ZipReader.cpp \
+	$(IO_SRC_DIR)/StringConverter.cpp \
 	$(IO_SRC_DIR)/ConvertLineReader.cpp \
 	$(IO_SRC_DIR)/FileLineReader.cpp \
 	$(IO_SRC_DIR)/KeyValueFileReader.cpp \
--- src/io/ConvertLineReader.cpp
+++ src/io/ConvertLineReader.cpp
@@ -22,32 +22,13 @@ Copyright_License {
 */
 
 #include "ConvertLineReader.hpp"
-#include "util/UTF8.hpp"
-
-#include <string.h>
-
-#ifdef _UNICODE
-#include <windows.h>
-#endif
 
 ConvertLineReader::ConvertLineReader(std::unique_ptr<LineReader<char>> &&_source,
                                      Charset cs)
   :source(std::move(_source)),
-   charset(cs)
+   converter(cs)
 {
 }
-
-#ifdef _UNICODE
-
-static void
-iso_latin_1_to_tchar(TCHAR *dest, const char *src)
-{
-    do {
-      *dest++ = (unsigned char)*src;
-    } while (*src++ != '\0');
-}
-
-#endif
 
 TCHAR *
 ConvertLineReader::ReadLine()
@@ -57,78 +38,7 @@ ConvertLineReader::ReadLine()
   if (narrow == nullptr)
     return nullptr;
 
-  // Check if there is byte order mark in front
-  if (narrow[0] == (char)0xEF &&
-      narrow[1] == (char)0xBB &&
-      narrow[2] == (char)0xBF &&
-      (charset == Charset::AUTO || charset == Charset::UTF8)) {
-    // -> if so, skip it
-    narrow += 3;
-
-    /* if it was "AUTO", then explicitly switch to UTF-8 now */
-    charset = Charset::UTF8;
-  }
-
-  if (charset == Charset::AUTO && !ValidateUTF8(narrow))
-    /* invalid UTF-8 sequence detected: switch to ISO-Latin-1 */
-    charset = Charset::ISO_LATIN_1;
-
-#ifdef _UNICODE
-  size_t narrow_length = strlen(narrow);
-
-  TCHAR *t = tbuffer.get(narrow_length + 1);
-  if (t == nullptr)
-    return nullptr;
-
-  if (narrow_length == 0) {
-    t[0] = _T('\0');
-    return t;
-  }
-
-  switch (charset) {
-  case Charset::ISO_LATIN_1:
-    iso_latin_1_to_tchar(t, narrow);
-    break;
-
-  default:
-    int length = MultiByteToWideChar(CP_UTF8, 0, narrow, narrow_length,
-                                     t, narrow_length);
-    if (length == 0)
-      return nullptr;
-
-    t[length] = _T('\0');
-
-    break;
-  }
-
-  return t;
-#else
-  switch (charset) {
-    size_t buffer_size;
-    const char *utf8;
-
-  case Charset::ISO_LATIN_1:
-    buffer_size = strlen(narrow) * 2 + 1;
-    utf8 = Latin1ToUTF8(narrow, tbuffer.get(buffer_size), buffer_size);
-    if (utf8 == nullptr)
-      return narrow;
-    return const_cast<char *>(utf8);
-
-  case Charset::UTF8:
-    if (!ValidateUTF8(narrow))
-      /* abort on invalid UTF-8 sequence */
-      return nullptr;
-
-    /* fall through ... */
-    gcc_fallthrough;
-
-  case Charset::AUTO:
-    return narrow;
-  }
-
-  /* unreachable */
-  gcc_unreachable();
-#endif
+  return converter.Convert(narrow);
 }
 
 long
