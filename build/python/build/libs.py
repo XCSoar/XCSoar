fb42157ccbec0815d9bcf75bf2383179ddd4e9da
Max Kellermann <max.kellermann@gmail.com>
2021-03-25 20:01:26 +0100

    build/python/libs: update libstdc++ to 10.2.0

--- build/python/build/libs.py
+++ build/python/build/libs.py
@@ -23,9 +23,9 @@ musl = AutotoolsProject(
 )
 
 libstdcxx_musl_headers = LibstdcxxMuslHeadersProject(
-    'https://ftp.gnu.org/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz',
-    'http://mirrors.ibiblio.org/gnu/ftp/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz',
-    '64baadfe6cc0f4947a84cb12d7f0dfaf45bb58b7e92461639596c21e02d97d2c',
+    'https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz',
+    'http://mirrors.ibiblio.org/gnu/ftp/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz',
+    'b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c',
     'include/libstdc++/algorithm',
     [
         '--enable-clocale=generic',
