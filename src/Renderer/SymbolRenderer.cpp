XCSoar/XCSoar

commit f143a2e61f809ab56f2276e9273149bee5ef23aa

ui/canvas/Canvas: pass PixelRect to DrawRectangle()

src/Renderer/SymbolRenderer.cpp
@@ -59,14 +59,12 @@ void
 SymbolRenderer::DrawSign(Canvas &canvas, PixelRect rc, bool plus)
 {
   unsigned size = std::min(rc.GetWidth(), rc.GetHeight()) / 5;
-  auto center = rc.GetCenter();
+  const auto r = PixelRect{rc.GetCenter()}.WithMargin({size, size / 3});
 
   // Draw horizontal bar
-  canvas.Rectangle(center.x - size, center.y - size / 3,
-                   center.x + size, center.y + size / 3);
+  canvas.DrawRectangle(r);
 
   if (plus)
     // Draw vertical bar
-    canvas.Rectangle(center.x - size / 3, center.y - size,
-                     center.x + size / 3, center.y + size);
+    canvas.DrawRectangle(r);
 }

src/CrossSection/AirspaceXSRenderer.cpp
@@ -115,19 +115,19 @@ AirspaceIntersectionVisitorSlice::RenderBox(const PixelRect rc,
       border.Grow(-(int)border_width);
 
       // Left border
-      canvas.Rectangle(rc.left, rc.top, border.left, rc.bottom);
+      canvas.DrawRectangle({rc.left, rc.top, border.left, rc.bottom});
 
       // Right border
-      canvas.Rectangle(border.right, rc.top, rc.right, rc.bottom);
+      canvas.DrawRectangle({border.right, rc.top, rc.right, rc.bottom});
 
       // Bottom border
-      canvas.Rectangle(border.left, border.bottom, border.right, rc.bottom);
+      canvas.DrawRectangle({border.left, border.bottom, border.right, rc.bottom});
 
       // Top border
-      canvas.Rectangle(border.left, rc.top, border.right, border.top);
+      canvas.DrawRectangle({border.left, rc.top, border.right, border.top});
     } else {
       // .. or fill the entire rect if the outlines would overlap
-      canvas.Rectangle(rc.left, rc.top, rc.right, rc.bottom);
+      canvas.DrawRectangle(rc);
     }
 
     AirspacePreviewRenderer::UnprepareFill(canvas);
@@ -136,7 +136,7 @@ AirspaceIntersectionVisitorSlice::RenderBox(const PixelRect rc,
   // Use transparent brush and type-dependent pen for the outlines
   if (AirspacePreviewRenderer::PrepareOutline(canvas, type, airspace_look,
                                               settings))
-    canvas.Rectangle(rc.left, rc.top, rc.right, rc.bottom);
+    canvas.DrawRectangle(rc);
 }
 
 inline void

src/Dialogs/Airspace/dlgAirspace.cpp
@@ -100,20 +100,16 @@ AirspaceSettingsListWidget::OnPaintItem(Canvas &canvas, PixelRect rc,
   if (color_mode) {
     int second_x = row_renderer.NextColumn(canvas, rc, name);
 
-    const unsigned padding = Layout::GetTextPadding();
+    const int padding = Layout::GetTextPadding();
 
     if (AirspacePreviewRenderer::PrepareFill(
         canvas, (AirspaceClass)i, look, renderer)) {
-      canvas.Rectangle(second_x, rc.top + padding,
-                       rc.right - padding,
-                       rc.bottom - padding);
+      canvas.DrawRectangle({second_x, rc.top + padding, rc.right - padding, rc.bottom - padding});
       AirspacePreviewRenderer::UnprepareFill(canvas);
     }
     if (AirspacePreviewRenderer::PrepareOutline(
         canvas, (AirspaceClass)i, look, renderer)) {
-      canvas.Rectangle(second_x, rc.top + padding,
-                       rc.right - padding,
-                       rc.bottom - padding);
+      canvas.DrawRectangle({second_x, rc.top + padding, rc.right - padding, rc.bottom - padding});
     }
   } else {
     rc.right = renderer.classes[i].display

src/Dialogs/Airspace/dlgAirspacePatterns.cpp
@@ -52,8 +52,7 @@ public:
     canvas.SetBackgroundTransparent();
     canvas.Select(look.brushes[i]);
     canvas.SetTextColor(COLOR_BLACK);
-    canvas.Rectangle(rc.left + padding, rc.top + padding,
-                     rc.right - padding, rc.bottom - padding);
+    canvas.DrawRectangle(rc.WithPadding(padding));
   }
 };
 

src/Dialogs/ColorListDialog.cpp
@@ -51,7 +51,7 @@ OnPaintListItem(Canvas &canvas, const PixelRect rc, unsigned i) noexcept
 #endif
 
   canvas.SelectBlackPen();
-  canvas.Rectangle(rc2.left, rc2.top, rc2.right, rc2.bottom);
+  canvas.DrawRectangle(rc2);
 }
 
 bool

src/Dialogs/Settings/dlgConfigInfoboxes.cpp
@@ -414,7 +414,7 @@ InfoBoxPreview::OnPaint(Canvas &canvas)
 
   canvas.SelectHollowBrush();
   canvas.SelectBlackPen();
-  canvas.Rectangle(0, 0, canvas.GetWidth() - 1, canvas.GetHeight() - 1);
+  canvas.DrawRectangle(PixelRect{PixelSize{canvas.GetWidth() - 1, canvas.GetHeight() - 1}});
 
   InfoBoxFactory::Type type = parent->GetContents(i);
   const TCHAR *caption = type < InfoBoxFactory::NUM_TYPES

src/Dialogs/Traffic/TrafficList.cpp
@@ -639,10 +639,7 @@ TrafficListWidget::OnPaintItem(Canvas &canvas, PixelRect rc,
     canvas.SelectHollowBrush();
 
     const PixelSize size = canvas.CalcTextSize(tmp);
-    canvas.Rectangle(rc.left + row_renderer.GetX() - frame_padding,
-                     rc.top + row_renderer.GetFirstY() - frame_padding,
-                     rc.left + row_renderer.GetX() + size.width + frame_padding,
-                     rc.top + row_renderer.GetFirstY() + size.height + frame_padding);
+    canvas.DrawRectangle(PixelRect{{rc.left + row_renderer.GetX(), rc.top + row_renderer.GetFirstY()}, size}.WithMargin(frame_padding));
   }
 
   row_renderer.DrawFirstRow(canvas, rc, tmp);

src/Form/CheckBox.cpp
@@ -185,11 +185,12 @@ CheckBoxControl::OnPaint(Canvas &canvas)
           : cb_look.standard))
     : cb_look.disabled;
 
-  unsigned size = canvas.GetHeight() - 4;
+  const unsigned padding = 2;
+  unsigned size = canvas.GetHeight() - 2 * padding;
 
   canvas.Select(state_look.box_brush);
   canvas.Select(state_look.box_pen);
-  canvas.Rectangle(2, 2, size, size);
+  canvas.DrawRectangle(PixelRect{PixelSize{canvas.GetHeight()}}.WithPadding(padding));
 
   if (checked) {
     canvas.Select(state_look.check_brush);

commit 3b02a163c45adfa8624f147a38962f0607a5f472

Fix plus symbol rendering in SymbolRenderer This bug was introduced in f143a2e6 which make plus symbol rendered as minus symbol

src/Renderer/SymbolRenderer.cpp
@@ -59,12 +59,16 @@ void
 SymbolRenderer::DrawSign(Canvas &canvas, PixelRect rc, bool plus)
 {
   unsigned size = std::min(rc.GetWidth(), rc.GetHeight()) / 5;
-  const auto r = PixelRect{rc.GetCenter()}.WithMargin({size, size / 3});
+  const auto horizontal_rect = PixelRect{rc.GetCenter()}
+    .WithMargin({size, size / 3});
 
   // Draw horizontal bar
-  canvas.DrawRectangle(r);
+  canvas.DrawRectangle(horizontal_rect);
 
-  if (plus)
+  if (plus) {
     // Draw vertical bar
-    canvas.DrawRectangle(r);
+    const auto vertical_rect = PixelRect{rc.GetCenter()}
+      .WithMargin({size / 3, size});
+    canvas.DrawRectangle(vertical_rect);
+  }
 }