XCSoar/XCSoar

commit 12754933538c422d4f53ca3e4e48ae5952f1a31f

ui/dim/Point: add method At()

src/Renderer/ChartRenderer.cpp
@@ -183,7 +183,7 @@ ChartRenderer::DrawLabel(const TCHAR *text, const double xv, const double yv)
     const PixelSize rect_size = tsize + PixelSize{padding_text * 2, padding_text * 2};
     canvas.DrawRectangle(PixelRect::Centered(pt, rect_size));
   }
-  canvas.DrawText({pt.x - tsize.width / 2, pt.y - tsize.height / 2}, text);
+  canvas.DrawText(pt + tsize / 2u, text);
 }
 
 void
@@ -511,7 +511,7 @@ ChartRenderer::DrawYGrid(double tic_step, double unit_step, UnitFormat unit_form
             TCHAR unit_text[MAX_PATH];
             FormatTicText(unit_text, yval * unit_step / tic_step, unit_step, unit_format);
             const auto c = canvas.CalcTextSize(unit_text);
-            canvas.DrawText({std::max(x - (int)c.width, rc.left + padding_text), ymin - c.height / 2}, unit_text);
+            canvas.DrawText({std::max(x - (int)c.width, rc.left + padding_text), ymin - (int)c.height / 2}, unit_text);
           }
         }
       }

src/Dialogs/Airspace/dlgAirspaceWarnings.cpp
@@ -430,7 +430,7 @@ AirspaceWarningListWidget::OnPaintItem(Canvas &canvas,
 
   if (state_text != NULL) {
     // -- status text will be centered inside its table cell:
-    canvas.DrawText({paint_rc.left + left2 + padding + (status_width / 2) - (canvas.CalcTextWidth(state_text) / 2),
+    canvas.DrawText({paint_rc.left + left2 + (int)padding + (int)(status_width / 2) - (int)(canvas.CalcTextWidth(state_text) / 2),
         (paint_rc.bottom + paint_rc.top - (int)state_text_size.height) / 2},
                     state_text);
   }

src/Dialogs/Device/DeviceListDialog.cpp
@@ -357,7 +357,7 @@ DeviceListWidget::OnPaintItem(Canvas &canvas, const PixelRect rc,
   }
 
   canvas.Select(*look.list.font);
-  canvas.DrawText({rc.left + margin, rc.top + margin}, text);
+  canvas.DrawText(rc.GetTopLeft() + PixelSize{margin, margin}, text);
 
   /* show a list of features that are available in the second row */
 
@@ -433,7 +433,7 @@ DeviceListWidget::OnPaintItem(Canvas &canvas, const PixelRect rc,
   }
 
   canvas.Select(look.small_font);
-  canvas.DrawText({rc.left + margin, rc.top + 2 * margin + font_height},
+  canvas.DrawText(rc.GetTopLeft() + PixelSize{margin, 2 * margin + font_height},
                   status);
 }
 

src/Form/CheckBox.cpp
@@ -217,5 +217,5 @@ CheckBoxControl::OnPaint(Canvas &canvas)
   canvas.Select(*cb_look.font);
   canvas.SetTextColor(state_look.text_color);
   canvas.SetBackgroundTransparent();
-  canvas.DrawText({canvas.GetHeight() + 2, 2}, caption.c_str());
+  canvas.DrawText({(int)canvas.GetHeight() + 2, 2}, caption.c_str());
 }

src/Form/Form.cpp
@@ -518,14 +518,14 @@ WndForm::OnPaint(Canvas &canvas)
                      look.caption.background_bitmap);
 
       // Draw titlebar text
-      canvas.DrawText({title_rect.left + Layout::GetTextPadding(), title_rect.top},
+      canvas.DrawText(title_rect.GetTopLeft().At(Layout::GetTextPadding(), 0),
                       caption.c_str());
     } else {
 #endif
       canvas.SetBackgroundColor(is_active
                                 ? look.caption.background_color
                                 : look.caption.inactive_background_color);
-      canvas.DrawOpaqueText({title_rect.left + Layout::GetTextPadding(), title_rect.top},
+      canvas.DrawOpaqueText(title_rect.GetTopLeft().At(Layout::GetTextPadding(), 0),
                             title_rect, caption.c_str());
 #ifdef EYE_CANDY
     }

src/Gauge/BigTrafficWidget.cpp
@@ -304,7 +304,7 @@ FlarmTrafficControl::PaintClimbRate(Canvas &canvas, PixelRect rc,
   // Paint label
   canvas.Select(look.info_labels_font);
   const unsigned label_width = canvas.CalcTextSize(_("Vario")).width;
-  canvas.DrawText({rc.right - label_width, rc.top}, _("Vario"));
+  canvas.DrawText(rc.GetTopRight().At(-(int)label_width, 0), _("Vario"));
 
   // Format climb rate
   TCHAR buffer[20];
@@ -328,14 +328,18 @@ FlarmTrafficControl::PaintClimbRate(Canvas &canvas, PixelRect rc,
   const int max_height = std::max(unit_height, value_height);
   const int y = rc.top + look.info_units_font.GetHeight() + max_height;
 
+  const int unit_x = rc.right - unit_width;
+  const int unit_y = y - unit_height;
+
+  const int value_x = unit_x - space_width - value_width;
+  const int value_y = y - value_height;
+
   // Paint value
-  canvas.DrawText({rc.right - unit_width - space_width - value_width, y - value_height},
-                  buffer);
+  canvas.DrawText({value_x, value_y}, buffer);
 
   // Paint unit
   canvas.Select(look.info_units_font);
-  UnitSymbolRenderer::Draw(canvas,
-                           PixelPoint(rc.right - unit_width, y - unit_height),
+  UnitSymbolRenderer::Draw(canvas, {unit_x, unit_y},
                            unit, look.unit_fraction_pen);
 }
 
@@ -363,20 +367,22 @@ FlarmTrafficControl::PaintDistance(Canvas &canvas, PixelRect rc,
   // Calculate positions
   const unsigned max_height = std::max(unit_height, value_height);
 
+  const auto p0 = rc.GetBottomLeft();
+
   // Paint value
-  canvas.DrawText({rc.left, rc.bottom - value_height}, buffer);
+  canvas.DrawText(p0.At(0, -(int)value_height), buffer);
 
   // Paint unit
   canvas.Select(look.info_units_font);
   UnitSymbolRenderer::Draw(canvas,
-                           PixelPoint(rc.left + value_width + space_width,
-                                      rc.bottom - unit_height),
+                           p0.At(value_width + space_width,
+                                 -(int)unit_height),
                            unit, look.unit_fraction_pen);
 
 
   // Paint label
   canvas.Select(look.info_labels_font);
-  canvas.DrawText({rc.left, rc.bottom - max_height - look.info_labels_font.GetHeight()},
+  canvas.DrawText(p0.At(0, -int(max_height + look.info_labels_font.GetHeight())),
                   _("Distance"));
 }
 
@@ -405,22 +411,24 @@ FlarmTrafficControl::PaintRelativeAltitude(Canvas &canvas, PixelRect rc,
   // Calculate positions
   const unsigned max_height = std::max(unit_height, value_height);
 
+  const auto p0 = rc.GetBottomRight();
+
   // Paint value
-  canvas.DrawText({rc.right - unit_width - space_width - value_width, rc.bottom - value_height},
+  canvas.DrawText(p0.At(-int(unit_width + space_width + value_width),
+                        -(int)value_height),
                   buffer);
 
   // Paint unit
   canvas.Select(look.info_units_font);
   UnitSymbolRenderer::Draw(canvas,
-                           PixelPoint(rc.right - unit_width,
-                                      rc.bottom - unit_height),
+                           p0.At(-(int)unit_width, -(int)unit_height),
                            unit, look.unit_fraction_pen);
 
 
   // Paint label
   canvas.Select(look.info_labels_font);
   const unsigned label_width = canvas.CalcTextSize(_("Rel. Alt.")).width;
-  canvas.DrawText({rc.right - label_width, rc.bottom - max_height - look.info_labels_font.GetHeight()},
+  canvas.DrawText(p0.At(-(int)label_width,  -int(max_height + look.info_labels_font.GetHeight())),
                   _("Rel. Alt."));
 }
 

src/Gauge/FlarmTrafficWindow.cpp
@@ -498,7 +498,7 @@ FlarmTrafficWindow::PaintTargetInfoSmall(
   unsigned dist = Layout::FastScale(traffic.HasAlarm() ? 12 : 8);
 
   // Draw string
-  canvas.DrawText({sc[i].x + dist, sc[i].y - tsize.height / 2}, relalt_s);
+  canvas.DrawText(sc[i].At(dist, -int(tsize.height / 2)), relalt_s);
 
   // Set target_brush for the up/down arrow
   canvas.Select(arrow_brush);
@@ -523,8 +523,8 @@ FlarmTrafficWindow::PaintTargetInfoSmall(
     triangle[j].x = Layout::FastScale(triangle[j].x);
     triangle[j].y = Layout::FastScale(triangle[j].y);
 
-    triangle[j].x = sc[i].x + dist + triangle[j].x + tsize.width / 2;
-    triangle[j].y = sc[i].y + flip * (triangle[j].y  - tsize.height / 2);
+    triangle[j] = sc[i].At(dist + triangle[j].x + int(tsize.width / 2),
+                           flip * (triangle[j].y - int(tsize.height / 2)));
   }
   triangle[3].x = triangle[0].x;
   triangle[3].y = triangle[0].y;

src/Gauge/GaugeVario.cpp
@@ -181,7 +181,7 @@ GaugeVario::OnPaintBuffer(Canvas &canvas)
   if (!IsPersistent() || background_dirty) {
     canvas.Stretch(rc.GetTopLeft(), rc.GetSize(),
                    look.background_bitmap,
-                   {look.background_x, 0}, {58, 120});
+                   {(int)look.background_x, 0}, {58, 120});
 
     background_dirty = false;
   }
@@ -435,7 +435,7 @@ GaugeVario::RenderValue(Canvas &canvas, const LabelValueGeometry &g,
     canvas.Select(*look.text_font);
     const unsigned width = canvas.CalcTextSize(label).width;
 
-    const PixelPoint text_position{g.label_right - width, g.label_y};
+    const PixelPoint text_position{g.label_right - (int)width, g.label_y};
 
     if (IsPersistent()) {
       PixelRect rc;
@@ -461,7 +461,7 @@ GaugeVario::RenderValue(Canvas &canvas, const LabelValueGeometry &g,
     canvas.Select(look.value_font);
     const unsigned width = canvas.CalcTextSize(buffer).width;
 
-    const PixelPoint text_position{g.value_right - width, g.value_y};
+    const PixelPoint text_position{g.value_right - (int)width, g.value_y};
 
     if (IsPersistent()) {
       PixelRect rc;

commit 24da41b833dfeeca6c2a7a47082d50645387d7ab

Renderer/Chart: fix sign error in DrawLabel()
Regression by commit 12754933538c422d4f53ca3e4e48ae5952f1a31f

src/Renderer/ChartRenderer.cpp
@@ -238,7 +238,7 @@ ChartRenderer::DrawLabel(const TCHAR *text,
     const PixelSize rect_size = tsize + PixelSize{Layout::GetTextPadding() * 2};
     canvas.DrawRectangle(PixelRect::Centered(pt, rect_size));
   }
-  canvas.DrawText(pt + tsize / 2u, text);
+  canvas.DrawText(pt - tsize / 2u, text);
 }
 
 void