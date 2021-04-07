e41d5c42085c4db56cecb3dc1e4c8a71121f4bfe
Max Kellermann <max.kellermann@gmail.com>
2021-04-07 15:21:50 +0200

    Kobo/PowerOff: adjust to Canvas API changes
    Forgot to adjust those after commit fbc390f142183
    
    Closes https://github.com/XCSoar/XCSoar/issues/571

--- src/Kobo/PowerOff.cpp
+++ src/Kobo/PowerOff.cpp
@@ -46,15 +46,14 @@ Copyright_License {
 static void
 DrawBanner(Canvas &canvas, PixelRect &rc)
 {
-  const unsigned padding = 2;
+  const int padding = 2;
   const Bitmap logo(IDB_LOGO);
   const unsigned banner_height = logo.GetHeight();
 
   /* draw the XCSoar logo */
   int x = rc.left + padding;
-  canvas.Copy(x, rc.top + padding,
-              logo.GetWidth(), logo.GetHeight(),
-              logo, 0, 0);
+  canvas.Copy({x, rc.top + padding}, logo.GetSize(),
+              logo, {0, 0});
 
   x += logo.GetWidth() + 8;
 
@@ -66,28 +65,28 @@ DrawBanner(Canvas &canvas, PixelRect &rc)
   Font large_font;
   large_font.LoadFile("/opt/xcsoar/share/fonts/VeraBd.ttf", 40);
   canvas.Select(large_font);
-  const unsigned name_y = rc.top
+  const int name_y = rc.top
     + (banner_height - large_font.GetHeight()) / 2;
 
   const TCHAR *const name1 = _T("XC");
-  canvas.DrawText(x, name_y, name1);
+  canvas.DrawText({x, name_y}, name1);
   x += canvas.CalcTextWidth(name1);
 
   const TCHAR *const name2 = _T("Soar");
   canvas.SetTextColor(COLOR_GRAY);
-  canvas.DrawText(x, name_y, name2);
+  canvas.DrawText({x, name_y}, name2);
   canvas.SetTextColor(COLOR_BLACK);
   x += canvas.CalcTextWidth(name2) + 30;
 
   /* some more text */
   const TCHAR *const website = _T("www.xcsoar.org");
   canvas.Select(normal_font);
-  canvas.DrawText(x, rc.top + (banner_height - normal_font.GetHeight()) / 2,
+  canvas.DrawText({x, rc.top + int(banner_height - normal_font.GetHeight()) / 2},
                   website);
 
   const TCHAR *const comment = _T("powered off");
-  canvas.DrawText(rc.right - canvas.CalcTextWidth(comment) - padding,
-                  rc.top + padding, comment);
+  canvas.DrawText({rc.right - (int)canvas.CalcTextWidth(comment) - padding, rc.top + padding},
+                  comment);
 
   rc.top += banner_height + 8;
 }
