37369891d418ca996064468eb573cdbc97acc565
Max Kellermann <max.kellermann@gmail.com>
2021-04-08 19:58:51 +0200

    build/debug.mk: indent

--- build/options.mk
+++ build/options.mk
@@ -6,8 +6,8 @@ FUZZER ?= n
 # shall we paint with some eye candy?
 EYE_CANDY ?= $(call bool_not,$(TARGET_IS_KOBO))
 ifeq ($(EYE_CANDY),y)
-TARGET_CPPFLAGS += -DEYE_CANDY
-WINDRESFLAGS += -DEYE_CANDY
+  TARGET_CPPFLAGS += -DEYE_CANDY
+  WINDRESFLAGS += -DEYE_CANDY
 endif
 
 ifeq ($(DEBUG)$(HAVE_WIN32)$(TARGET_IS_DARWIN),nnn)
@@ -22,26 +22,26 @@ SANITIZE ?= n
 # show map renderer times?
 STOP_WATCH ?= n
 ifeq ($(STOP_WATCH),y)
-TARGET_CPPFLAGS += -DSTOP_WATCH
+  TARGET_CPPFLAGS += -DSTOP_WATCH
 endif
 
 # compile without UI?
 HEADLESS ?= n
 
 ifeq ($(TARGET_IS_KOBO),y)
-DITHER ?= y
+  DITHER ?= y
 else
-DITHER ?= n
+  DITHER ?= n
 endif
 
 ifeq ($(DITHER),y)
-TARGET_CPPFLAGS += -DDITHER
+  TARGET_CPPFLAGS += -DDITHER
 endif
 
 GREYSCALE ?= $(DITHER)
 
 ifeq ($(GREYSCALE),y)
-TARGET_CPPFLAGS += -DGREYSCALE
+  TARGET_CPPFLAGS += -DGREYSCALE
 endif
 
 # When enabled, the Androidpackage org.xcsoar.testing is created, with
