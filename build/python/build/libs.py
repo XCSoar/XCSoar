2a7d583789d2980d0d282d4a5793df3a8e6f0b7f
Philipp Wollschlegel <folken@kabelsalat.ch>
2021-04-11 17:34:03 +0200

    Update libs.py

--- build/python/build/libs.py
+++ build/python/build/libs.py
@@ -39,6 +39,7 @@ libstdcxx_musl_headers = LibstdcxxMuslHeadersProject(
 openssl = OpenSSLProject(
     'https://www.openssl.org/source/openssl-3.0.0-alpha13.tar.gz',
     'ftp://ftp.cert.dfn.de/pub/tools/net/openssl/source/openssl-3.0.0-alpha13.tar.gz',
+    'https://github.com/openssl/openssl/archive/refs/tags/openssl-3.0.0-alpha13.tar.gz',
     'c88cbb9d330b4daa3dbb5af1ed511d5062253291a56e09fd17e9ac013a20f8a3',
     'include/openssl/ossl_typ.h',
 )
