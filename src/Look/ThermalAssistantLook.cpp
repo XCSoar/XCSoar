25761fbc40cadc3740398311b95d15c0983192a4
Max Kellermann <max.kellermann@gmail.com>
2021-04-15 21:30:21 +0200

    Look/ThermalAssistant: make several fields static/constexpr

--- src/Look/ThermalAssistantLook.cpp
+++ src/Look/ThermalAssistantLook.cpp
@@ -28,12 +28,6 @@
 void
 ThermalAssistantLook::Initialise(bool small, bool inverse)
 {
-  background_color = COLOR_WHITE;
-  circle_color = Color(0xB0, 0xB0, 0xB0);
-  text_color = Color(0x00, 0x00, 0x00);
-  polygon_fill_color = Color(0xCC, 0xCC, 0xFF);
-  polygon_border_color = Color(0x00, 0x00, 0xFF);
-
 #ifdef ENABLE_OPENGL
   polygon_brush.Create(polygon_fill_color.WithAlpha(128));
 #else /* !OPENGL */
