1d990a537b5de3469df1f60a72336ae9c699013c
Max Kellermann <max.kellermann@gmail.com>
2021-04-24 07:40:42 +0200

    ui/canvas/opengl/Color: add `noexcept`

--- src/ui/canvas/opengl/Color.hpp
+++ src/ui/canvas/opengl/Color.hpp
@@ -40,11 +40,11 @@ class Color {
 #ifdef HAVE_GLES
   typedef GLfixed Component;
 
-  static constexpr Component Import(uint8_t value) {
+  static constexpr Component Import(uint8_t value) noexcept {
     return value << 8;
   }
 
-  static constexpr uint8_t Export(Component value) {
+  static constexpr uint8_t Export(Component value) noexcept {
     return value >> 8;
   }
 
@@ -52,18 +52,18 @@ class Color {
 #else
   typedef GLubyte Component;
 
-  static constexpr Component Import(uint8_t value) {
+  static constexpr Component Import(uint8_t value) noexcept {
     return value;
   }
 
-  static constexpr uint8_t Export(Component value) {
+  static constexpr uint8_t Export(Component value) noexcept {
     return value;
   }
 
   static constexpr Component MAX = 0xff;
 #endif
 
-  static constexpr GLfloat ExportFloat(Component value) {
+  static constexpr GLfloat ExportFloat(Component value) noexcept {
     return GLfloat(value) / GLfloat(MAX);
   }
 
@@ -72,20 +72,21 @@ class Color {
   struct Internal {};
 
   constexpr Color(Internal,
-                  Component _r, Component _g, Component _b, Component _a)
+                  Component _r, Component _g,
+                  Component _b, Component _a) noexcept
     :r(_r), g(_g), b(_b), a(_a) {}
 
 public:
-  constexpr Color(uint8_t _r, uint8_t _g, uint8_t _b)
+  constexpr Color(uint8_t _r, uint8_t _g, uint8_t _b) noexcept
     :r(Import(_r)), g(Import(_g)), b(Import(_b)), a(MAX) {}
-  constexpr Color(GLubyte _r, GLubyte _g, GLubyte _b, GLubyte _a)
+  constexpr Color(GLubyte _r, GLubyte _g, GLubyte _b, GLubyte _a) noexcept
     :r(Import(_r)), g(Import(_g)), b(Import(_b)), a(Import(_a)) {}
 
   explicit constexpr Color(RGB8Color other)
     :r(Import(other.Red())), g(Import(other.Green())), b(Import(other.Blue())),
      a(MAX) {}
 
-  Color() = default;
+  Color() noexcept = default;
 
 #ifdef HAVE_GLES
   static constexpr GLenum TYPE = GL_FIXED;
@@ -97,10 +98,7 @@ public:
    * Returns the red part of the color
    * @return The red part of the color (0-255)
    */
-  constexpr
-  uint8_t
-  Red() const
-  {
+  constexpr uint8_t Red() const noexcept {
     return Export(r);
   }
 
@@ -108,10 +106,7 @@ public:
    * Returns the green part of the color
    * @return The green part of the color (0-255)
    */
-  constexpr
-  uint8_t
-  Green() const
-  {
+  constexpr uint8_t Green() const noexcept {
     return Export(g);
   }
 
@@ -119,10 +114,7 @@ public:
    * Returns the blue part of the color
    * @return The blue part of the color (0-255)
    */
-  constexpr
-  uint8_t
-  Blue() const
-  {
+  constexpr uint8_t Blue() const noexcept {
     return Export(b);
   }
 
@@ -130,20 +122,15 @@ public:
    * Returns the alpha part of the color
    * @return The alpha part of the color (0-255)
    */
-  constexpr
-  uint8_t
-  Alpha() const
-  {
+  constexpr uint8_t Alpha() const noexcept {
     return Export(a);
   }
 
-  constexpr
-  Color
-  WithAlpha(uint8_t alpha) const {
+  constexpr Color WithAlpha(uint8_t alpha) const noexcept {
     return Color(Internal(), r, g, b, Import(alpha));
   }
 
-  constexpr bool IsOpaque() const {
+  constexpr bool IsOpaque() const noexcept {
 #ifdef HAVE_GLES
     return a >= 0xff00;
 #else
@@ -151,24 +138,21 @@ public:
 #endif
   }
 
-  constexpr bool IsTransparent() const {
+  constexpr bool IsTransparent() const noexcept {
     return a == 0;
   }
 
   /**
    * Construct a #Color object that is transparent.
    */
-  static constexpr Color Transparent() {
+  static constexpr Color Transparent() noexcept {
     return Color(0, 0, 0, 0);
   }
 
   /**
    * Returns the highlighted version of this color.
    */
-  constexpr
-  Color
-  Highlight() const
-  {
+  constexpr Color Highlight() const noexcept {
 #ifdef HAVE_GLES
     return Color((r + 3) / 4., (g + 3) / 4., (b + 3) / 4.);
 #else
@@ -179,19 +163,19 @@ public:
   /**
    * Returns the shadowed version of this color.
    */
-  constexpr Color Shadow() const {
+  constexpr Color Shadow() const noexcept {
     return Color(Red() * 15u / 16u,
                  Green() * 15u / 16u,
                  Blue() * 15u / 16u,
                  Alpha());
   }
 
-  void Uniform(GLint location) const {
+  void Uniform(GLint location) const noexcept {
     glUniform4f(location, ExportFloat(r), ExportFloat(g),
                 ExportFloat(b), ExportFloat(a));
   }
 
-  void VertexAttrib(GLint index) const {
+  void VertexAttrib(GLint index) const noexcept {
     glVertexAttrib4f(index, ExportFloat(r), ExportFloat(g),
                      ExportFloat(b), ExportFloat(a));
   }
@@ -199,7 +183,7 @@ public:
   /**
    * Configures this color in the OpenGL context.
    */
-  void Bind() const {
+  void Bind() const noexcept {
     VertexAttrib(OpenGL::Attribute::COLOR);
   }
 
@@ -209,9 +193,7 @@ public:
    * @param b Color 2
    * @return True if colors match, False otherwise
    */
-  constexpr
-  bool operator ==(const Color other) const
-  {
+  constexpr bool operator ==(const Color other) const noexcept {
     return r == other.r
       && g == other.g
       && b == other.b;
@@ -223,21 +205,19 @@ public:
    * @param b Color 2
    * @return True if color do not match, False otherwise
    */
-  constexpr
-  bool operator !=(const Color other) const
-  {
+  constexpr bool operator !=(const Color other) const noexcept {
     return !(*this == other);
   }
 };
 
 struct ScopeColorPointer {
-  ScopeColorPointer(const Color *p) {
+  ScopeColorPointer(const Color *p) noexcept {
     glEnableVertexAttribArray(OpenGL::Attribute::COLOR);
     glVertexAttribPointer(OpenGL::Attribute::COLOR, 4, Color::TYPE,
                           GL_FALSE, 0, p);
   }
 
-  ~ScopeColorPointer() {
+  ~ScopeColorPointer() noexcept {
     glDisableVertexAttribArray(OpenGL::Attribute::COLOR);
   }
 };
