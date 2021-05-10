067a3a1d29a322425b8791fe63c09edb022cd440
Philipp Wollschlegel <folken@kabelsalat.ch>
2021-05-10 19:38:47 +0200

    ide/provisioning: quote variables, uppercase uniformly

--- ide/provisioning/install-android-tools.sh
+++ ide/provisioning/install-android-tools.sh
@@ -11,49 +11,49 @@ ANDROID_REPO_URL=https://dl.google.com/android/repository
 ANDROID_SDK_DIR=~/opt/android-sdk-linux
 ANDROID_NDK_DIR=~/opt/android-ndk-${ANDROID_NDK_VERSION}
 
-if [ -d ${ANDROID_SDK_DIR}\build-tools\${ANDROID_BUILD_TOOLS_VERSION} ]
+if [ -d "${ANDROID_SDK_DIR}"\\build-tools\\"${ANDROID_BUILD_TOOLS_VERSION}" ]
 then
-  echo Not installing Android SDK, because ${ANDROID_SDK_DIR}\build-tools\${ANDROID_BUILD_TOOLS_VERSION} exists already
+  echo "Not installing Android SDK, because ${ANDROID_SDK_DIR}\\build-tools\${ANDROID_BUILD_TOOLS_VERSION} exists already"
 else
-  echo Installing Android SDK to ${ANDROID_SDK_DIR}...
+  echo "Installing Android SDK to ${ANDROID_SDK_DIR}..."
 
-  android_sdk_tmp_zip=$(mktemp)
+  ANDROID_SDK_TMP_ZIP="$(mktemp)"
   wget --progress=bar:force:noscroll \
       ${ANDROID_REPO_URL}/commandlinetools-linux-${ANDROID_SDK_TOOLS_VERSION}.zip \
-      -O ${android_sdk_tmp_zip}
+      -O "${ANDROID_SDK_TMP_ZIP}"
 
-  mkdir -p ${ANDROID_SDK_DIR}/licenses
-  cd ${ANDROID_SDK_DIR}
-  unzip ${android_sdk_tmp_zip}
+  mkdir -p "${ANDROID_SDK_DIR}"/licenses
+  cd "${ANDROID_SDK_DIR}"
+  unzip "${ANDROID_SDK_TMP_ZIP}"
 
-  rm ${android_sdk_tmp_zip}
+  rm "${ANDROID_SDK_TMP_ZIP}"
 
   echo 24333f8a63b6825ea9c5514f83c2829b004d1fee > licenses/android-sdk-license
 fi
 echo
 
-cd ${ANDROID_SDK_DIR}
+cd "${ANDROID_SDK_DIR}"
 echo Installing Android SDK packages...
-echo cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_SDK_DIR} \
+echo cmdline-tools/bin/sdkmanager --sdk_root="${ANDROID_SDK_DIR}" \
     "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" "platforms;android-${ANDROID_PLATFORM_VERSION}"
 cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_SDK_DIR} \
     "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" "platforms;android-${ANDROID_PLATFORM_VERSION}"
 echo
 
-if [ -d ${ANDROID_NDK_DIR} ]
+if [ -d "${ANDROID_NDK_DIR}" ]
 then
   echo Not installing Android NDK, because ${ANDROID_NDK_DIR} exists already
 else
   echo Installing Android NDK to ${ANDROID_NDK_DIR}...
 
-  android_ndk_tmp_zip=$(mktemp)
+  ANDROID_NDK_TMP_ZIP=$(mktemp)
   wget --progress=bar:force:noscroll \
       ${ANDROID_REPO_URL}/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip \
-      -O ${android_ndk_tmp_zip}
+      -O "${ANDROID_NDK_TMP_ZIP}"
 
   cd ~/opt
-  unzip ${android_ndk_tmp_zip}
+  unzip "${ANDROID_NDK_TMP_ZIP}"
 
-  rm ${android_ndk_tmp_zip}
+  rm "${ANDROID_NDK_TMP_ZIP}"
 fi
 echo
