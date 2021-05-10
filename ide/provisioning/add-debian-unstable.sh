9f96b998c8bea7d1ef7c341dfb557bf5ee1481ca
Philipp Wollschlegel <folken@kabelsalat.ch>
2021-05-10 22:46:24 +0200

    ide: upgrade to java-11

--- ide/provisioning/add-debian-unstable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Adds stretch sources list to debian for jdk8 dependency
-echo 'deb http://deb.debian.org/debian stretch main' > /etc/apt/sources.list.d/unstable.list
-echo 'APT::Default-Release "stable";' > /etc/apt/apt.conf.d/99defaultrelease
