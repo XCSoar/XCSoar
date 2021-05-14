d13ab2c3c3ec342f8d46edf3b960a70a2f147d49
August2111 <Info@FlapsOnline.de>
2021-05-14 16:10:17 +0200

    test/src/DumpVario.cpp - With MSVC compiler there is no header file unistd.h the function declarations are in windows.h
    I don't know if that is also better for PC and WIN64 (_WIN32) target too

--- test/src/DumpVario.cpp
+++ test/src/DumpVario.cpp
@@ -28,7 +28,11 @@ Copyright_License {
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#if defined(_MSC_VER)
+# include <windows.h>
+#else
+# include <unistd.h>
+#endif
 
 int
 main(int argc, char **argv)
