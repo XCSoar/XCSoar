XCSoar/XCSoar

commit b8ecc71f4476e2e506d2fd5f666e40017d13fd43

Terrain/RasterTile: use struct Point2D

src/Terrain/RasterBuffer.cpp
@@ -29,11 +29,12 @@ Copyright_License {
 #include <stdlib.h>
 
 void
-RasterBuffer::Resize(unsigned _width, unsigned _height) noexcept
+RasterBuffer::Resize(RasterLocation _size) noexcept
 {
-  assert(_width > 0 && _height > 0);
+  assert(_size.x > 0);
+  assert(_size.y > 0);
 
-  data.GrowDiscard(_width, _height);
+  data.GrowDiscard(_size.x, _size.y);
 }
 
 TerrainHeight
@@ -41,15 +42,15 @@ RasterBuffer::GetInterpolated(unsigned lx, unsigned ly,
                               unsigned ix, unsigned iy) const noexcept
 {
   assert(IsDefined());
-  assert(lx < GetWidth());
-  assert(ly < GetHeight());
+  assert(lx < GetSize().x);
+  assert(ly < GetSize().y);
   assert(ix < 0x100);
   assert(iy < 0x100);
 
   // perform piecewise linear interpolation
-  const unsigned int dx = (lx == GetWidth() - 1) ? 0 : 1;
-  const unsigned int dy = (ly == GetHeight() - 1) ? 0 : GetWidth();
-  const TerrainHeight *tm = GetDataAt(lx, ly);
+  const unsigned int dx = (lx == GetSize().x - 1) ? 0 : 1;
+  const unsigned int dy = (ly == GetSize().y - 1) ? 0 : GetSize().x;
+  const TerrainHeight *tm = GetDataAt({lx, ly});
 
   if (tm->IsSpecial() || tm[dx].IsSpecial() ||
       tm[dy].IsSpecial() || tm[dx + dy].IsSpecial())
@@ -65,19 +66,19 @@ RasterBuffer::GetInterpolated(unsigned lx, unsigned ly,
 }
 
 TerrainHeight
-RasterBuffer::GetInterpolated(unsigned lx, unsigned ly) const noexcept
+RasterBuffer::GetInterpolated(RasterLocation p) const noexcept
 {
   // check x in range, and decompose fraction part
-  const unsigned int ix = CombinedDivAndMod(lx);
-  if (lx >= GetWidth())
+  const unsigned int ix = CombinedDivAndMod(p.x);
+  if (p.x >= GetSize().x)
     return TerrainHeight::Invalid();
 
   // check y in range, and decompose fraction part
-  const unsigned int iy = CombinedDivAndMod(ly);
-  if (ly >= GetHeight())
+  const unsigned int iy = CombinedDivAndMod(p.y);
+  if (p.y >= GetSize().y)
     return TerrainHeight::Invalid();
 
-  return GetInterpolated(lx, ly, ix, iy);
+  return GetInterpolated(p.x, p.y, ix, iy);
 }
 
 /**
@@ -122,15 +123,17 @@ RasterBuffer::ScanHorizontalLine(unsigned ax, unsigned bx, unsigned y,
                                  TerrainHeight *gcc_restrict buffer, unsigned size,
                                  bool interpolate) const noexcept
 {
-  assert(ax < GetFineWidth());
-  assert(bx < GetFineWidth());
-  assert(y < GetFineHeight());
+  assert(ax < GetFineSize().x);
+  assert(bx < GetFineSize().x);
+  assert(y < GetFineSize().y);
   assert(buffer != nullptr);
   assert(size > 0);
 
   if (size == 1) {
-    *buffer = Get(ax >> RasterTraits::SUBPIXEL_BITS,
-                  y >> RasterTraits::SUBPIXEL_BITS);
+    *buffer = Get({
+        ax >> RasterTraits::SUBPIXEL_BITS,
+        y >> RasterTraits::SUBPIXEL_BITS,
+      });
     return;
   }
 
@@ -157,8 +160,10 @@ RasterBuffer::ScanHorizontalLine(unsigned ax, unsigned bx, unsigned y,
     /* no interpolation needed, forward scan */
 
     const TerrainHeight *gcc_restrict src =
-      GetDataAt(ax >> RasterTraits::SUBPIXEL_BITS,
-                y >> RasterTraits::SUBPIXEL_BITS);
+      GetDataAt({
+          ax >> RasterTraits::SUBPIXEL_BITS,
+          y >> RasterTraits::SUBPIXEL_BITS,
+        });
 
     PixelIterator iterator(dx >> RasterTraits::SUBPIXEL_BITS, size);
     TerrainHeight *gcc_restrict end = buffer + size;
@@ -172,7 +177,7 @@ RasterBuffer::ScanHorizontalLine(unsigned ax, unsigned bx, unsigned y,
     /* no interpolation needed */
 
     const TerrainHeight *gcc_restrict src =
-      GetDataAt(0, y >> RasterTraits::SUBPIXEL_BITS);
+      GetDataAt({0, y >> RasterTraits::SUBPIXEL_BITS});
 
     --size;
     for (int i = 0; (unsigned)i <= size; ++i) {
@@ -184,41 +189,40 @@ RasterBuffer::ScanHorizontalLine(unsigned ax, unsigned bx, unsigned y,
 }
 
 void
-RasterBuffer::ScanLine(unsigned ax, unsigned ay, unsigned bx, unsigned by,
+RasterBuffer::ScanLine(RasterLocation a, RasterLocation b,
                        TerrainHeight *gcc_restrict buffer,
                        unsigned size, bool interpolate) const noexcept
 {
-  assert(ax < GetFineWidth());
-  assert(ay < GetFineHeight());
-  assert(bx < GetFineWidth());
-  assert(by < GetFineHeight());
+  assert(a.x < GetFineSize().x);
+  assert(a.y < GetFineSize().y);
+  assert(b.x < GetFineSize().x);
+  assert(b.y < GetFineSize().y);
   assert(buffer != nullptr);
   assert(size > 0);
 
-  if (ay == by) {
-    ScanHorizontalLine(ax, bx, ay, buffer, size, interpolate);
+  if (a.y == b.y) {
+    ScanHorizontalLine(a.x, b.x, a.y, buffer, size, interpolate);
     return;
   }
 
   if (size == 1) {
-    *buffer = Get(ax >> RasterTraits::SUBPIXEL_BITS,
-                  ay >> RasterTraits::SUBPIXEL_BITS);
+    *buffer = Get(a >> RasterTraits::SUBPIXEL_BITS);
     return;
   }
 
   --size;
-  const int dx = bx - ax, dy = by - ay;
+  const IntPoint2D d(b.x - a.x, b.y - a.y);
   /* disable interpolation when an output pixel is larger than two
      pixels in our buffer; the factor of two should account for the Y
      axis, which can have a different scale, making the factor some
      sort of ugly kludge to avoid horizontal shading stripes */
   if (interpolate &&
-      (unsigned)(abs(dx) + abs(dy)) < (2 * size << RasterTraits::SUBPIXEL_BITS)) {
+      (unsigned)(abs(d.x) + abs(d.y)) < (2 * size << RasterTraits::SUBPIXEL_BITS)) {
     /* interpolate */
 
     for (int i = 0; (unsigned)i <= size; ++i) {
-      unsigned cx = ax + (i * dx) / (int)size;
-      unsigned cy = ay + (i * dy) / (int)size;
+      unsigned cx = a.x + (i * d.x) / (int)size;
+      unsigned cy = a.y + (i * d.y) / (int)size;
 
       const unsigned int ix = CombinedDivAndMod(cx);
       const unsigned int iy = CombinedDivAndMod(cy);
@@ -229,34 +233,32 @@ RasterBuffer::ScanLine(unsigned ax, unsigned ay, unsigned bx, unsigned by,
     /* no interpolation needed */
 
     for (int i = 0; (unsigned)i <= size; ++i) {
-      unsigned cx = ax + (i * dx) / (int)size;
-      unsigned cy = ay + (i * dy) / (int)size;
+      const RasterLocation c(a.x + (i * d.x) / (int)size,
+                             a.y + (i * d.y) / (int)size);
 
-      *buffer++ = Get(cx >> RasterTraits::SUBPIXEL_BITS,
-                      cy >> RasterTraits::SUBPIXEL_BITS);
+      *buffer++ = Get(c);
     }
   }
 }
 
 void
-RasterBuffer::ScanLineChecked(unsigned ax, unsigned ay,
-                              unsigned bx, unsigned by,
+RasterBuffer::ScanLineChecked(RasterLocation a, RasterLocation b,
                               TerrainHeight *buffer, unsigned size,
                               bool interpolate) const noexcept
 {
-  if (ax >= GetFineWidth())
-    ax = GetFineWidth() - 1;
+  if (a.x >= GetFineSize().x)
+    a.x = GetFineSize().x - 1;
 
-  if (ay >= GetFineHeight())
-    ay = GetFineHeight() - 1;
+  if (a.y >= GetFineSize().y)
+    a.y = GetFineSize().y - 1;
 
-  if (bx >= GetFineWidth())
-    bx = GetFineWidth() - 1;
+  if (b.x >= GetFineSize().x)
+    b.x = GetFineSize().x - 1;
 
-  if (by >= GetFineHeight())
-    by = GetFineHeight() - 1;
+  if (b.y >= GetFineSize().y)
+    b.y = GetFineSize().y - 1;
 
-  ScanLine(ax, ay, bx, by, buffer, size, interpolate);
+  ScanLine(a, b, buffer, size, interpolate);
 }
 
 TerrainHeight

src/Terrain/Intersection.cpp
@@ -47,7 +47,7 @@ RasterTileCache::FirstIntersection(const SignedRasterLocation origin,
     // origin is outside overall bounds
     return false;
 
-  const TerrainHeight h_origin2 = GetFieldDirect(origin.x, origin.y).first;
+  const TerrainHeight h_origin2 = GetFieldDirect(location).first;
   if (h_origin2.IsInvalid()) {
     _location = location;
     _h = h_origin;
@@ -115,7 +115,7 @@ RasterTileCache::FirstIntersection(const SignedRasterLocation origin,
       if (!IsInside(location))
         break; // outside bounds
 
-      const auto field_direct = GetFieldDirect(location.x, location.y);
+      const auto field_direct = GetFieldDirect(location);
       if (field_direct.first.IsInvalid())
         break;
 
@@ -225,31 +225,29 @@ RasterTileCache::FirstIntersection(const SignedRasterLocation origin,
 }
 
 inline std::pair<TerrainHeight, bool>
-RasterTileCache::GetFieldDirect(const unsigned px,
-                                const unsigned py) const noexcept
+RasterTileCache::GetFieldDirect(RasterLocation p) const noexcept
 {
-  assert(px < size.x);
-  assert(py < size.y);
+  assert(p.x < size.x);
+  assert(p.y < size.y);
 
-  const RasterTile &tile = tiles.Get(px / tile_size.x, py / tile_size.y);
+  const RasterTile &tile = tiles.Get(p.x / tile_size.x, p.y / tile_size.y);
   if (tile.IsEnabled())
-    return std::make_pair(tile.GetHeight(px, py), true);
+    return std::make_pair(tile.GetHeight(p), true);
 
   // still not found, so go to overview
 
   // The overview might not cover the whole tile, if width or height are not
   // a multiple of 2^OVERVIEW_BITS.
-  unsigned x_overview = px >> OVERVIEW_BITS;
-  unsigned y_overview = py >> OVERVIEW_BITS;
-  assert(x_overview <= overview.GetWidth());
-  assert(y_overview <= overview.GetHeight());
+  auto p_overview = p >> OVERVIEW_BITS;
+  assert(p_overview.x <= overview.GetSize().x);
+  assert(p_overview.y <= overview.GetSize().y);
 
-  if (x_overview == overview.GetWidth())
-    x_overview--;
-  if (y_overview == overview.GetHeight())
-    y_overview--;
+  if (p_overview.x == overview.GetSize().x)
+    --p_overview.x;
+  if (p_overview.y == overview.GetSize().y)
+    --p_overview.y;
 
-  return std::make_pair(overview.Get(x_overview, y_overview), false);
+  return std::make_pair(overview.Get(p_overview), false);
 }
 
 SignedRasterLocation
@@ -305,7 +303,7 @@ RasterTileCache::Intersection(const SignedRasterLocation origin,
       if (!IsInside(location))
         break;
 
-      const auto field_direct = GetFieldDirect(location.x, location.y);
+      const auto field_direct = GetFieldDirect(location);
       if (field_direct.first.IsInvalid())
         break;
 

src/Terrain/RasterBuffer.hpp
@@ -25,6 +25,7 @@ Copyright_License {
 #define XCSOAR_RASTER_BUFFER_HPP
 
 #include "RasterTraits.hpp"
+#include "RasterLocation.hpp"
 #include "Height.hpp"
 #include "util/AllocatedGrid.hxx"
 #include "util/Compiler.h"
@@ -46,20 +47,12 @@ public:
     return data.IsDefined();
   }
 
-  unsigned GetWidth() const noexcept {
-    return data.GetWidth();
+  RasterLocation GetSize() const noexcept {
+    return {data.GetWidth(), data.GetHeight()};
   }
 
-  unsigned GetHeight() const noexcept {
-    return data.GetHeight();
-  }
-
-  unsigned GetFineWidth() const noexcept {
-    return GetWidth() << RasterTraits::SUBPIXEL_BITS;
-  }
-
-  unsigned GetFineHeight() const noexcept {
-    return GetHeight() << RasterTraits::SUBPIXEL_BITS;
+  RasterLocation GetFineSize() const noexcept {
+    return GetSize() << RasterTraits::SUBPIXEL_BITS;
   }
 
   TerrainHeight *GetData() noexcept {
@@ -70,26 +63,26 @@ public:
     return data.begin();
   }
 
-  const TerrainHeight *GetDataAt(unsigned x, unsigned y) const noexcept {
-    return data.GetPointerAt(x, y);
+  const TerrainHeight *GetDataAt(RasterLocation p) const noexcept {
+    return data.GetPointerAt(p.x, p.y);
   }
 
   void Reset() noexcept {
     data.Reset();
   }
 
-  void Resize(unsigned _width, unsigned _height) noexcept;
+  void Resize(RasterLocation _size) noexcept;
 
   gcc_pure
   TerrainHeight GetInterpolated(unsigned lx, unsigned ly,
                                 unsigned ix, unsigned iy) const noexcept;
 
   gcc_pure
-  TerrainHeight GetInterpolated(unsigned lx, unsigned ly) const noexcept;
+  TerrainHeight GetInterpolated(RasterLocation p) const noexcept;
 
   gcc_pure
-  TerrainHeight Get(unsigned x, unsigned y) const noexcept {
-    return *GetDataAt(x, y);
+  TerrainHeight Get(RasterLocation p) const noexcept {
+    return *GetDataAt(p);
   }
 
 protected:
@@ -102,13 +95,13 @@ protected:
 
 
 public:
-  void ScanLine(unsigned ax, unsigned ay, unsigned bx, unsigned by,
+  void ScanLine(RasterLocation a, RasterLocation b,
                 TerrainHeight *buffer, unsigned size, bool interpolate) const noexcept;
 
   /**
    * Wrapper for ScanLine() with basic range checks.
    */
-  void ScanLineChecked(unsigned ax, unsigned ay, unsigned bx, unsigned by,
+  void ScanLineChecked(RasterLocation a, RasterLocation b,
                        TerrainHeight *buffer, unsigned size,
                        bool interpolate) const noexcept;
 

src/Terrain/RasterMap.cpp
@@ -45,14 +45,14 @@ TerrainHeight
 RasterMap::GetHeight(const GeoPoint &location) const
 {
   const auto pt = projection.ProjectCoarse(location);
-  return raster_tile_cache.GetHeight(pt.x, pt.y);
+  return raster_tile_cache.GetHeight(pt);
 }
 
 TerrainHeight
 RasterMap::GetInterpolatedHeight(const GeoPoint &location) const
 {
   const auto pt = projection.ProjectFine(location);
-  return raster_tile_cache.GetInterpolatedHeight(pt.x, pt.y);
+  return raster_tile_cache.GetInterpolatedHeight(pt);
 }
 
 void

src/Terrain/RasterTile.cpp
@@ -54,7 +54,7 @@ RasterTile::CopyFrom(const struct jas_matrix &m) noexcept
   if (!IsDefined())
     return;
 
-  buffer.Resize(size.x, size.y);
+  buffer.Resize(size);
 
   auto *gcc_restrict dest = buffer.GetData();
   assert(dest != nullptr);
@@ -70,17 +70,16 @@ RasterTile::CopyFrom(const struct jas_matrix &m) noexcept
 }
 
 TerrainHeight
-RasterTile::GetHeight(unsigned x, unsigned y) const noexcept
+RasterTile::GetHeight(RasterLocation p) const noexcept
 {
   assert(IsEnabled());
 
-  x -= start.x;
-  y -= start.y;
+  p -= start;
 
-  assert(x < size.x);
-  assert(y < size.y);
+  assert(p.x < size.x);
+  assert(p.y < size.y);
 
-  return buffer.Get(x, y);
+  return buffer.Get(p);
 }
 
 TerrainHeight

src/Terrain/RasterTile.hpp
@@ -120,7 +120,7 @@ public:
    * @param y the pixel row within the tile; may be out of range
    */
   gcc_pure
-  TerrainHeight GetHeight(unsigned x, unsigned y) const noexcept;
+  TerrainHeight GetHeight(RasterLocation p) const noexcept;
 
   /**
    * Determine the interpolated height at the specified sub-pixel
@@ -137,13 +137,11 @@ public:
 
   bool VisibilityChanged(IntPoint2D view, unsigned view_radius) noexcept;
 
-  void ScanLine(unsigned ax, unsigned ay, unsigned bx, unsigned by,
+  void ScanLine(RasterLocation a, RasterLocation b,
                 TerrainHeight *dest, unsigned dest_size,
                 bool interpolate) const noexcept {
-    buffer.ScanLine(ax - (start.x << RasterTraits::SUBPIXEL_BITS),
-                    ay - (start.y << RasterTraits::SUBPIXEL_BITS),
-                    bx - (start.x << RasterTraits::SUBPIXEL_BITS),
-                    by - (start.y << RasterTraits::SUBPIXEL_BITS),
+    buffer.ScanLine(a - (start << RasterTraits::SUBPIXEL_BITS),
+                    b - (start << RasterTraits::SUBPIXEL_BITS),
                     dest, dest_size, interpolate);
   }
 };

src/Terrain/RasterTileCache.cpp
@@ -52,20 +52,20 @@ RasterTileCache::PutOverviewTile(unsigned index,
 {
   tiles.GetLinear(index).Set(start, end);
 
-  const unsigned dest_pitch = overview.GetWidth();
+  const unsigned dest_pitch = overview.GetSize().x;
 
   start.x = RasterTraits::ToOverview(start.x);
   start.y = RasterTraits::ToOverview(start.y);
 
-  if (start.x >= overview.GetWidth() || start.y >= overview.GetHeight())
+  if (start.x >= overview.GetSize().x || start.y >= overview.GetSize().y)
     return;
 
   unsigned width = RasterTraits::ToOverviewCeil(m.numcols_);
-  if (start.x + width > overview.GetWidth())
-    width = overview.GetWidth() - start.x;
+  if (start.x + width > overview.GetSize().x)
+    width = overview.GetSize().x - start.x;
   unsigned height = RasterTraits::ToOverviewCeil(m.numrows_);
-  if (start.y + height > overview.GetHeight())
-    height = overview.GetHeight() - start.y;
+  if (start.y + height > overview.GetSize().y)
+    height = overview.GetSize().y - start.y;
 
   const unsigned skip = 1 << OVERVIEW_BITS;
 
@@ -165,29 +165,28 @@ RasterTileCache::PollTiles(int x, int y, unsigned radius) noexcept
 }
 
 TerrainHeight
-RasterTileCache::GetHeight(unsigned px, unsigned py) const noexcept
+RasterTileCache::GetHeight(RasterLocation p) const noexcept
 {
-  if (px >= size.x || py >= size.y)
+  if (p.x >= size.x || p.y >= size.y)
     // outside overall bounds
     return TerrainHeight::Invalid();
 
-  const RasterTile &tile = tiles.Get(px / tile_size.x, py / tile_size.y);
+  const RasterTile &tile = tiles.Get(p.x / tile_size.x, p.y / tile_size.y);
   if (tile.IsEnabled())
-    return tile.GetHeight(px, py);
+    return tile.GetHeight(p);
 
   // still not found, so go to overview
-  return overview.GetInterpolated(px << (RasterTraits::SUBPIXEL_BITS - RasterTraits::OVERVIEW_BITS),
-                                  py << (RasterTraits::SUBPIXEL_BITS - RasterTraits::OVERVIEW_BITS));
+  return overview.GetInterpolated(p << (RasterTraits::SUBPIXEL_BITS - RasterTraits::OVERVIEW_BITS));
 }
 
 TerrainHeight
-RasterTileCache::GetInterpolatedHeight(unsigned lx, unsigned ly) const noexcept
+RasterTileCache::GetInterpolatedHeight(RasterLocation l) const noexcept
 {
-  if (lx >= overview_size_fine.x || ly >= overview_size_fine.y)
+  if (l.x >= overview_size_fine.x || l.y >= overview_size_fine.y)
     // outside overall bounds
     return TerrainHeight::Invalid();
 
-  unsigned px = lx, py = ly;
+  unsigned px = l.x, py = l.y;
   const unsigned int ix = CombinedDivAndMod(px);
   const unsigned int iy = CombinedDivAndMod(py);
 
@@ -196,8 +195,7 @@ RasterTileCache::GetInterpolatedHeight(unsigned lx, unsigned ly) const noexcept
     return tile.GetInterpolatedHeight(px, py, ix, iy);
 
   // still not found, so go to overview
-  return overview.GetInterpolated(RasterTraits::ToOverview(lx),
-                                  RasterTraits::ToOverview(ly));
+  return overview.GetInterpolated({RasterTraits::ToOverview(l.x), RasterTraits::ToOverview(l.y)});
 }
 
 void
@@ -210,8 +208,7 @@ RasterTileCache::SetSize(UnsignedPoint2D _size,
 
   /* round the overview size up, because PutOverviewTile() does the
      same */
-  overview.Resize(RasterTraits::ToOverviewCeil(size.x),
-                  RasterTraits::ToOverviewCeil(size.y));
+  overview.Resize({RasterTraits::ToOverviewCeil(size.x), RasterTraits::ToOverviewCeil(size.y)});
   overview_size_fine = size << RasterTraits::SUBPIXEL_BITS;
 
   tiles.GrowDiscard(_n_tiles.x, _n_tiles.y);
@@ -307,7 +304,7 @@ RasterTileCache::SaveCache(BufferedOutputStream &os) const
   os.Write(&i, sizeof(i));
 
   /* save overview */
-  size_t overview_size = overview.GetWidth() * overview.GetHeight();
+  size_t overview_size = overview.GetSize().Area();
   os.Write(overview.GetData(), sizeof(*overview.GetData()) * overview_size);
 }
 
@@ -358,7 +355,7 @@ RasterTileCache::LoadCache(BufferedReader &r)
   }
 
   /* load overview */
-  size_t overview_size = overview.GetWidth() * overview.GetHeight();
+  size_t overview_size = overview.GetSize().Area();
   r.ReadFull({
       overview.GetData(),
       sizeof(*overview.GetData()) * overview_size,

src/Terrain/RasterTileCache.hpp
@@ -164,22 +164,19 @@ public:
    * Determine the non-interpolated height at the specified pixel
    * location.
    *
-   * @param x the pixel column within the map; may be out of range
-   * @param y the pixel row within the map; may be out of range
+   * @param x the pixel position within the map; may be out of range
    */
   gcc_pure
-  TerrainHeight GetHeight(unsigned x, unsigned y) const noexcept;
+  TerrainHeight GetHeight(RasterLocation p) const noexcept;
 
   /**
    * Determine the interpolated height at the specified sub-pixel
    * location.
    *
-   * @param lx the sub-pixel column within the map; may be out of range
-   * @param ly the sub-pixel row within the map; may be out of range
+   * @param p the sub-pixel position within the map; may be out of range
    */
   gcc_pure
-  TerrainHeight GetInterpolatedHeight(unsigned lx,
-                                      unsigned ly) const noexcept;
+  TerrainHeight GetInterpolatedHeight(RasterLocation p) const noexcept;
 
   /**
    * Scan a straight line and fill the buffer with the specified
@@ -212,15 +209,13 @@ public:
 private:
   /**
    * Get field (not interpolated) directly, without bringing tiles to front.
-   * @param px X position/256
-   * @param px Y position/256
+   * @param p position/256
    * @param tile_index Remember position of active tile, or -1 for overview
    * @return the terrain altitude and a flag that is true when the
    * value was loaded from a "fine" tile
    */
   gcc_pure
-  std::pair<TerrainHeight, bool> GetFieldDirect(unsigned px,
-                                                unsigned py) const noexcept;
+  std::pair<TerrainHeight, bool> GetFieldDirect(RasterLocation p) const noexcept;
 
 public:
   /**

commit ec2a0e08fb3d10096f84c53da8cb6bb8e7a0871f

Terrain/RasterBuffer: apply subpixel shift on un-interpolated scan
Regression by commit b8ecc71f4476e2e506d2fd5f666e40017d13fd43

Closes https://github.com/XCSoar/XCSoar/issues/654

src/Terrain/RasterBuffer.cpp
@@ -237,7 +237,7 @@ RasterBuffer::ScanLine(RasterLocation a, RasterLocation b,
       const RasterLocation c(a.x + (i * d.x) / (int)size,
                              a.y + (i * d.y) / (int)size);
 
-      *buffer++ = Get(c);
+      *buffer++ = Get(c >> RasterTraits::SUBPIXEL_BITS);
     }
   }
 }