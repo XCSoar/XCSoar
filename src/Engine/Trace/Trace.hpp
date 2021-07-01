XCSoar/XCSoar

commit 5bc4b63b717fd8d98f4114979ee891b4f73bc2e3

Trace: use class boost::intrusive::list instead of ListHead

src/Engine/Trace/Trace.hpp
@@ -27,11 +27,11 @@ Copyright_License {
 #include "Point.hpp"
 #include "Util/NonCopyable.hpp"
 #include "Util/SliceAllocator.hpp"
-#include "Util/ListHead.hpp"
-#include "Util/CastIterator.hpp"
 #include "Util/Serial.hpp"
 #include "Geo/Flat/TaskProjection.hpp"
 #include "Compiler.h"
+
+#include <boost/intrusive/list.hpp>
 
 #include <set>
 #include <assert.h>
@@ -52,7 +52,8 @@ class TracePointerVector;
  */
 class Trace : private NonCopyable
 {
-  struct TraceDelta : public ListHead {
+  struct TraceDelta
+    : public boost::intrusive::list_base_hook<boost::intrusive::link_mode<boost::intrusive::normal_link>> {
     /**
      * Function used to points for sorting by deltas.
      * Ranking is primarily by distance delta; for equal distances, rank by
@@ -126,22 +127,6 @@ class Trace : private NonCopyable
       return elim_time == null_time;
     }
 
-    TraceDelta &GetPrevious() {
-      return *(TraceDelta *)ListHead::GetPrevious();
-    }
-
-    TraceDelta &GetNext() {
-      return *(TraceDelta *)ListHead::GetNext();
-    }
-
-    const TraceDelta &GetPrevious() const {
-      return *(const TraceDelta *)ListHead::GetPrevious();
-    }
-
-    const TraceDelta &GetNext() const {
-      return *(const TraceDelta *)ListHead::GetNext();
-    }
-
     void Update(const TracePoint &p_last, const TracePoint &p_next) {
       elim_time = TimeMetric(p_last, point, p_next);
       elim_distance = DistanceMetric(p_last, point, p_next);
@@ -185,10 +170,11 @@ class Trace : private NonCopyable
     }
   };
 
-  typedef CastIterator<const TraceDelta, ListHead::const_iterator> ChronologicalConstIterator;
+  typedef boost::intrusive::list<TraceDelta,
+                                 boost::intrusive::constant_time_size<false>> ChronologicalList;
 
   TraceDelta::List delta_list;
-  ListHead chronological_list;
+  ChronologicalList chronological_list;
   unsigned cached_size;
 
   TaskProjection task_projection;
@@ -387,13 +373,13 @@ public:
   const TracePoint &front() const {
     assert(!empty());
 
-    return static_cast<const TraceDelta *>(chronological_list.GetNext())->point;
+    return chronological_list.front().point;
   }
 
   const TracePoint &back() const {
     assert(!empty());
 
-    return static_cast<const TraceDelta *>(chronological_list.GetPrevious())->point;
+    return chronological_list.back().point;
   }
 
 private:
@@ -423,13 +409,13 @@ private:
   TraceDelta &GetFront() {
     assert(!empty());
 
-    return *static_cast<TraceDelta *>(chronological_list.GetNext());
+    return chronological_list.front();
   }
 
   TraceDelta &GetBack() {
     assert(!empty());
 
-    return *static_cast<TraceDelta *>(chronological_list.GetPrevious());
+    return chronological_list.back();
   }
 
   gcc_pure
@@ -452,17 +438,15 @@ public:
   }
 
 public:
-  class const_iterator {
+  class const_iterator : public ChronologicalList::const_iterator {
     friend class Trace;
 
-    ListHead::const_iterator iterator;
-
-    const_iterator(ListHead::const_iterator _iterator)
-      :iterator(_iterator) {}
+    const_iterator(ChronologicalList::const_iterator &&_iterator)
+      :ChronologicalList::const_iterator(std::move(_iterator)) {}
 
   public:
-    typedef ListHead::const_iterator::iterator_category iterator_category;
-    typedef ptrdiff_t difference_type;
+    using ChronologicalList::const_iterator::iterator_category;
+    using ChronologicalList::const_iterator::difference_type;
     typedef const TracePoint value_type;
     typedef const TracePoint *pointer;
     typedef const TracePoint &reference;
@@ -470,58 +454,28 @@ public:
     const_iterator() = default;
 
     const TracePoint &operator*() const {
-      const TraceDelta &td = (const TraceDelta &)*iterator;
+      const TraceDelta &td = ChronologicalList::const_iterator::operator*();
       return td.point;
     }
 
     const TracePoint *operator->() const {
-      const TraceDelta &td = (const TraceDelta &)*iterator;
+      const TraceDelta &td = ChronologicalList::const_iterator::operator*();
       return &td.point;
-    }
-
-    const_iterator &operator++() {
-      ++iterator;
-      return *this;
-    }
-
-    const_iterator operator++(int) {
-      const_iterator old = *this;
-      iterator++;
-      return old;
-    }
-
-    const_iterator &operator--() {
-      --iterator;
-      return *this;
-    }
-
-    const_iterator operator--(int) {
-      const_iterator old = *this;
-      iterator--;
-      return old;
     }
 
     const_iterator &NextSquareRange(unsigned sq_resolution,
                                     const const_iterator &end) {
-      const TracePoint &previous = ((const TraceDelta &)*iterator).point;
+      const TracePoint &previous = **this;
       while (true) {
-        ++iterator;
+        ++*this;
 
-        if (iterator == end.iterator)
+        if (*this == end)
           return *this;
 
-        const TraceDelta &td = (const TraceDelta &)*iterator;
+        const TraceDelta &td = (const TraceDelta &)**this;
         if (td.point.FlatSquareDistanceTo(previous) >= sq_resolution)
           return *this;
       }
-    }
-
-    bool operator==(const const_iterator &other) const {
-      return iterator == other.iterator;
-    }
-
-    bool operator!=(const const_iterator &other) const {
-      return iterator != other.iterator;
     }
   };
 
@@ -531,72 +485,6 @@ public:
 
   const_iterator end() const {
     return chronological_list.end();
-  }
-
-  class const_reverse_iterator {
-    friend class Trace;
-
-    ListHead::const_reverse_iterator iterator;
-
-    const_reverse_iterator(ListHead::const_reverse_iterator _iterator)
-      :iterator(_iterator) {}
-
-  public:
-    typedef std::forward_iterator_tag iterator_category;
-    typedef ptrdiff_t difference_type;
-    typedef const TracePoint value_type;
-    typedef const TracePoint *pointer;
-    typedef const TracePoint &reference;
-
-    const_reverse_iterator() = default;
-
-    const TracePoint &operator*() const {
-      const TraceDelta &td = (const TraceDelta &)*iterator;
-      return td.point;
-    }
-
-    const TracePoint *operator->() const {
-      const TraceDelta &td = (const TraceDelta &)*iterator;
-      return &td.point;
-    }
-
-    const_reverse_iterator &operator++() {
-      ++iterator;
-      return *this;
-    }
-
-    const_reverse_iterator operator++(int) {
-      const_reverse_iterator old = *this;
-      iterator++;
-      return old;
-    }
-
-    const_reverse_iterator &operator--() {
-      --iterator;
-      return *this;
-    }
-
-    const_reverse_iterator operator--(int) {
-      const_reverse_iterator old = *this;
-      iterator--;
-      return old;
-    }
-
-    bool operator==(const const_reverse_iterator &other) const {
-      return iterator == other.iterator;
-    }
-
-    bool operator!=(const const_reverse_iterator &other) const {
-      return iterator != other.iterator;
-    }
-  };
-
-  const_reverse_iterator rbegin() const {
-    return chronological_list.rbegin();
-  }
-
-  const_reverse_iterator rend() const {
-    return chronological_list.rend();
   }
 
   const TaskProjection &GetProjection() const {

src/Engine/Trace/Trace.cpp
@@ -29,8 +29,7 @@ Copyright_License {
 
 Trace::Trace(const unsigned _no_thin_time, const unsigned max_time,
              const unsigned max_size)
-  :chronological_list(ListHead::empty()),
-   cached_size(0),
+  :cached_size(0),
    max_time(max_time),
    no_thin_time(_no_thin_time),
    max_size(max_size),
@@ -43,17 +42,17 @@ void
 Trace::clear()
 {
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
 
   average_delta_distance = 0;
   average_delta_time = 0;
 
   delta_list.clear();
-  chronological_list.Clear();
+  chronological_list.clear();
   cached_size = 0;
 
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
 
   ++modify_serial;
   ++append_serial;
@@ -76,18 +75,21 @@ void
 Trace::UpdateDelta(TraceDelta &td)
 {
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
 
-  if (chronological_list.IsEdge(td))
+  if (&td == &chronological_list.front() ||
+      &td == &chronological_list.back())
     return;
 
-  const TraceDelta &previous = td.GetPrevious();
-  const TraceDelta &next = td.GetNext();
+  const auto ci = chronological_list.iterator_to(td);
+  const TraceDelta &previous = *std::prev(ci);
+  const TraceDelta &next = *std::next(ci);
 
   TraceDelta temp_td = td;
-  temp_td.SetDisconnected();
 
-  td.Replace(temp_td);
+  auto old_i = chronological_list.iterator_to(td);
+  chronological_list.insert(old_i, temp_td);
+  chronological_list.erase(old_i);
 
   // erase old one
   auto i = delta_list.find(td);
@@ -97,8 +99,10 @@ Trace::UpdateDelta(TraceDelta &td)
   // insert new in sorted position
   temp_td.Update(previous.point, next.point);
   TraceDelta &new_td = Insert(temp_td);
-  new_td.SetDisconnected();
-  temp_td.Replace(new_td);
+
+  auto temp_i = chronological_list.iterator_to(temp_td);
+  chronological_list.insert(temp_i, new_td);
+  chronological_list.erase(temp_i);
 }
 
 void
@@ -106,17 +110,18 @@ Trace::EraseInside(TraceDelta::iterator it)
 {
   assert(cached_size > 0);
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
   assert(it != delta_list.end());
 
   const TraceDelta &td = *it;
   assert(!td.IsEdge());
 
-  TraceDelta &previous = const_cast<TraceDelta &>(td.GetPrevious());
-  TraceDelta &next = const_cast<TraceDelta &>(td.GetNext());
+  const auto ci = chronological_list.iterator_to(td);
+  TraceDelta &previous = const_cast<TraceDelta &>(*std::prev(ci));
+  TraceDelta &next = const_cast<TraceDelta &>(*std::next(ci));
 
   // now delete the item
-  td.RemoveConst();
+  chronological_list.erase(chronological_list.iterator_to(td));
   delta_list.erase(it);
   --cached_size;
 
@@ -129,7 +134,7 @@ bool
 Trace::EraseDelta(const unsigned target_size, const unsigned recent)
 {
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
 
   if (size() < 2)
     return false;
@@ -162,8 +167,9 @@ Trace::EraseEarlierThan(const unsigned p_time)
     return false;
 
   do {
-    TraceDelta &td = GetFront();
-    td.Remove();
+    auto ci = chronological_list.begin();
+    TraceDelta &td = *ci;
+    chronological_list.erase(ci);
 
     auto i = delta_list.find(td);
     assert(i != delta_list.end());
@@ -191,7 +197,7 @@ Trace::EraseLaterThan(const unsigned min_time)
   while (!empty() && GetBack().point.GetTime() > min_time) {
     TraceDelta &td = GetBack();
 
-    td.Remove();
+    chronological_list.erase(chronological_list.iterator_to(td));
 
     auto i = delta_list.find(td);
     assert(i != delta_list.end());
@@ -222,8 +228,9 @@ Trace::Insert(const TraceDelta &td) {
 void
 Trace::EraseStart(TraceDelta &td_start) {
   TraceDelta temp_td = td_start;
-  temp_td.SetDisconnected();
-  td_start.Replace(temp_td);
+  auto old_i = chronological_list.iterator_to(td_start);
+  chronological_list.insert(old_i, temp_td);
+  chronological_list.erase(old_i);
 
   auto i_start = delta_list.find(td_start);
   assert(i_start != delta_list.end());
@@ -233,15 +240,17 @@ Trace::EraseStart(TraceDelta &td_start) {
   temp_td.elim_time = null_time;
 
   TraceDelta &new_td = Insert(temp_td);
-  new_td.SetDisconnected();
-  temp_td.Replace(new_td);
+
+  auto temp_i = chronological_list.iterator_to(temp_td);
+  chronological_list.insert(temp_i, new_td);
+  chronological_list.erase(temp_i);
 }
 
 void
 Trace::push_back(const TracePoint &point)
 {
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
 
   if (empty()) {
     // first point determines origin for flat projection
@@ -272,12 +281,12 @@ Trace::push_back(const TracePoint &point)
 
   TraceDelta &td = Insert(TraceDelta(point));
   td.point.Project(task_projection);
-  td.InsertBefore(chronological_list);
+  chronological_list.push_back(td);
 
   ++cached_size;
 
-  if (!chronological_list.IsFirst(td))
-    UpdateDelta(td.GetPrevious());
+  if (&td != &chronological_list.front())
+    UpdateDelta(*std::prev(chronological_list.iterator_to(td)));
 
   ++append_serial;
 }
@@ -289,8 +298,8 @@ Trace::CalcAverageDeltaDistance(const unsigned no_thin) const
   unsigned acc = 0;
   unsigned counter = 0;
 
-  const ChronologicalConstIterator end = chronological_list.end();
-  for (ChronologicalConstIterator it = chronological_list.begin();
+  const auto end = chronological_list.end();
+  for (auto it = chronological_list.begin();
        it != end && it->point.GetTime() < r; ++it, ++counter)
     acc += it->delta_distance;
 
@@ -307,8 +316,8 @@ Trace::CalcAverageDeltaTime(const unsigned no_thin) const
   unsigned counter = 0;
 
   /* find the last item before the "r" timestamp */
-  const ChronologicalConstIterator end = chronological_list.end();
-  ChronologicalConstIterator it;
+  const auto end = chronological_list.end();
+  ChronologicalList::const_iterator it;
   for (it = chronological_list.begin(); it != end && it->point.GetTime() < r; ++it)
     ++counter;
 
@@ -361,7 +370,7 @@ void
 Trace::Thin()
 {
   assert(cached_size == delta_list.size());
-  assert(cached_size == chronological_list.Count());
+  assert(cached_size == chronological_list.size());
   assert(size() == max_size);
 
   Thin2();

commit 8a56df80ff7d7735a2120ee584fa0e75ee04354b

Engine/Trace: fix wrong cast in NextSquareRange()
It is illegal to cast TracePoint to TraceDelta, because the two are
not derived from each others and have a non-zero offset.

This is a 7 year old regression from commit 5bc4b63b717f, so this
piece of code has been broken for a VERY long time.  D'oh!

src/Engine/Trace/Trace.hpp
@@ -511,7 +511,7 @@ public:
         if (*this == end)
           return *this;
 
-        const TraceDelta &td = (const TraceDelta &)**this;
+        const TraceDelta &td = ChronologicalList::const_iterator::operator*();
         if (td.point.FlatSquareDistanceTo(previous) >= sq_resolution)
           return *this;
       }