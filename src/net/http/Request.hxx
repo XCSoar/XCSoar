6fb9a605a1b2ff11b049638eb07aea1be1b631bb
Max Kellermann <max.kellermann@gmail.com>
2021-07-02 19:49:10 +0200

    curl/Request: move API documentation from constructor to class

--- src/net/http/Request.hxx
+++ src/net/http/Request.hxx
@@ -39,6 +39,9 @@ struct StringView;
 class CurlGlobal;
 class CurlResponseHandler;
 
+/**
+ * To start sending the request, call Start().
+ */
 class CurlRequest final {
 	CurlGlobal &global;
 
@@ -61,9 +64,6 @@ class CurlRequest final {
 	bool registered = false;
 
 public:
-	/**
-	 * To start sending the request, call Start().
-	 */
 	CurlRequest(CurlGlobal &_global,
 		    CurlResponseHandler &_handler);
 
