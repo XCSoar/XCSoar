82402aee85b1601b354f2f8b6521b28ead964227
August2111 <Info@FlapsOnline.de>
2021-07-05 20:49:54 +0200

    WeGlide Form/DataField/Integer.hpp different behaviour for very big Lists!

--- src/Form/DataField/Integer.hpp
+++ src/Form/DataField/Integer.hpp
@@ -46,7 +46,10 @@ public:
   DataFieldInteger(const TCHAR *edit_format, const TCHAR *display_format,
                    int _min, int _max, int _value, int _step,
                    DataFieldListener *listener=nullptr) noexcept
-    :NumberDataField(Type::INTEGER, true, edit_format, display_format, listener),
+    :NumberDataField(Type::INTEGER,
+                     /* no list picker for very big lists */
+                     ((_max - _min) / _step) < 500,
+                     edit_format, display_format, listener),
      value(_value), min(_min), max(_max), step(_step) {}
 
   void Set(int _value) noexcept {
