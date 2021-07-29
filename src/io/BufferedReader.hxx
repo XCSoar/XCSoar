61280e57ece1492a1f157a1fdba320c70f5a4182
Max Kellermann <max.kellermann@gmail.com>
2021-07-29 17:18:44 +0200

    io/BufferedReader: use std::size_t

--- src/io/BufferedReader.hxx
+++ src/io/BufferedReader.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 Max Kellermann <max.kellermann@gmail.com>
+ * Copyright 2014-2021 Max Kellermann <max.kellermann@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -73,9 +73,9 @@ public:
 	 * it).  Throws std::runtime_error if not enough data is
 	 * available.
 	 */
-	void *ReadFull(size_t size);
+	void *ReadFull(std::size_t size);
 
-	void Consume(size_t n) noexcept {
+	void Consume(std::size_t n) noexcept {
 		buffer.Consume(n);
 	}
 
@@ -83,7 +83,7 @@ public:
 	 * Read (and consume) data from the input buffer into the
 	 * given buffer.  Does not attempt to refill the buffer.
 	 */
-	size_t ReadFromBuffer(WritableBuffer<void> dest) noexcept;
+	std::size_t ReadFromBuffer(WritableBuffer<void> dest) noexcept;
 
 	/**
 	 * Read data into the given buffer and consume it from our
