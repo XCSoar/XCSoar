f16cec796711655d34cfdd3408eb3bf0b88413bc
Max Kellermann <max.kellermann@gmail.com>
2021-08-10 13:05:00 +0200

    util/OverwritingRingBuffer: add `noexcept`

--- src/util/OverwritingRingBuffer.hpp
+++ src/util/OverwritingRingBuffer.hpp
@@ -51,27 +51,29 @@ public:
 		const TrivialOverwritingRingBuffer &buffer;
 		unsigned i;
 
-		const_iterator(const TrivialOverwritingRingBuffer<T, size> &_buffer, unsigned _i)
-			:buffer(_buffer), i(_i) {
+		const_iterator(const TrivialOverwritingRingBuffer<T, size> &_buffer,
+			       unsigned _i) noexcept
+			:buffer(_buffer), i(_i)
+		{
 			assert(i < size);
 		}
 
 	public:
-		const T &operator*() const {
+		const T &operator*() const noexcept {
 			return buffer.data[i];
 		}
 
-		typename TrivialOverwritingRingBuffer::const_iterator &operator++() {
+		typename TrivialOverwritingRingBuffer::const_iterator &operator++() noexcept {
 			i = buffer.next(i);
 			return *this;
 		}
 
-		bool operator==(const const_iterator &other) const {
+		bool operator==(const const_iterator &other) const noexcept {
 			assert(&buffer == &other.buffer);
 			return i == other.i;
 		}
 
-		bool operator!=(const const_iterator &other) const {
+		bool operator!=(const const_iterator &other) const noexcept {
 			assert(&buffer == &other.buffer);
 			return i != other.i;
 		}
@@ -89,12 +91,13 @@ public:
 	TrivialOverwritingRingBuffer() = default;
 
 protected:
-	static unsigned next(unsigned i) {
+	static unsigned next(unsigned i) noexcept {
 		assert(i < size);
 
 		return (i + 1) % size;
 	}
-	static unsigned previous(unsigned i) {
+
+	static unsigned previous(unsigned i) noexcept {
 		assert(i < size);
 		if (i>0)
 			return (i-1);
@@ -103,30 +106,30 @@ protected:
 	}
 
 public:
-	bool empty() const {
+	bool empty() const noexcept {
 		assert(head < size);
 		assert(tail < size);
 
 		return head == tail;
 	}
 
-	void clear() {
+	void clear() noexcept {
 		head = tail = 0;
 	}
 
 	// returns last value added
-	const T &last() const {
+	const T &last() const noexcept {
 		assert(!empty());
 		return data[previous(tail)];
 	}
 
-	const T &peek() const {
+	const T &peek() const noexcept {
 		assert(!empty());
 
 		return data[head];
 	}
 
-	const T &shift() {
+	const T &shift() noexcept {
 		/* this returns a reference to an item which is being
 		   Invalidated - but that's okay, because it won't get
 		   purged yet */
@@ -155,7 +158,7 @@ public:
 	/**
 	 * Returns a pointer to the oldest item.
 	 */
-	const_iterator begin() const {
+	const_iterator begin() const noexcept {
 		return const_iterator(*this, head);
 	}
 
@@ -163,11 +166,11 @@ public:
 	 * Returns a pointer to end of the buffer (one item after the
 	 * newest item).
 	 */
-	const_iterator end() const {
+	const_iterator end() const noexcept {
 		return const_iterator(*this, tail);
 	}
 
-	unsigned capacity() const {
+	static constexpr unsigned capacity() noexcept {
 		return size;
 	}
 };
