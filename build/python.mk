4ec919fa86707ba0264f84d7d501b017b76bb4c5
Max Kellermann <max.kellermann@gmail.com>
2021-08-10 18:09:43 +0200

    build/python.mk: call python3-config instead of python-config
    python-config is deprecated.

--- build/python.mk
+++ build/python.mk
@@ -50,9 +50,9 @@ PYTHON_SOURCES = \
 	$(ENGINE_SRC_DIR)/Airspace/AirspaceAircraftPerformance.cpp \
 	$(SRC)/NMEA/Aircraft.cpp
 PYTHON_LDADD = $(DEBUG_REPLAY_LDADD)
-PYTHON_LDLIBS = $(shell python-config --ldflags)
+PYTHON_LDLIBS = $(shell python3-config --ldflags)
 PYTHON_DEPENDS = CONTEST WAYPOINT UTIL ZZIP GEO MATH TIME
-PYTHON_CPPFLAGS = $(shell python-config --includes) \
+PYTHON_CPPFLAGS = $(shell python3-config --includes) \
 	-I$(TEST_SRC_DIR) -Wno-write-strings
 PYTHON_NO_LIB_PREFIX = y
 $(eval $(call link-shared-library,xcsoar,PYTHON))
