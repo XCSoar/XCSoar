XCSoar/XCSoar

commit affdd9fc32dc59a7aa5b9c63c2250b63300b4eac

android/XCSoar: disable full-screen mode in multi-window mode
In multi-window mode, a white bar appears on top of XCSoar where the
top status bar should be, but it's disabled because XCSoar requested
it to be disabled.

Closes https://github.com/XCSoar/XCSoar/issues/691

android/src/WindowUtil.java
@@ -56,8 +56,18 @@ class WindowUtil {
                                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
   }
 
+  static void disableImmersiveMode(Window window) {
+    View decorView = window.getDecorView();
+    decorView.setSystemUiVisibility(0);
+  }
+
   static void enterFullScreenMode(Window window) {
     window.addFlags(FULL_SCREEN_WINDOW_FLAGS);
     enableImmersiveMode(window);
+  }
+
+  static void leaveFullScreenMode(Window window) {
+    disableImmersiveMode(window);
+    window.clearFlags(FULL_SCREEN_WINDOW_FLAGS);
   }
 }

android/src/XCSoar.java
@@ -144,7 +144,7 @@ public class XCSoar extends Activity {
   };
 
   boolean wantFullScreen() {
-    return Loader.loaded;
+    return Loader.loaded && !isInMultiWindowMode();
   }
 
   public void initSDL() {
@@ -306,6 +306,15 @@ public class XCSoar extends Activity {
       WindowUtil.enableImmersiveMode(getWindow());
 
     super.onWindowFocusChanged(hasFocus);
+  }
+
+  @Override
+  public void onMultiWindowModeChanged(boolean isInMultiWindowMode) {
+    final Window window = getWindow();
+    if (wantFullScreen())
+      WindowUtil.enterFullScreenMode(window);
+    else
+      WindowUtil.leaveFullScreenMode(window);
   }
 
   @Override public boolean dispatchTouchEvent(final MotionEvent ev) {

commit 268c52096a7672022c386126e418275ca1583b67

android/XCSoar: always use FLAG_KEEP_SCREEN_ON
We need this flag always (while holding the wake lock), even if we're
not full screen.

Fixes a regression by commit affdd9fc32dc59a7aa5b9c63c2250b63300b4eac
which happened because I thought all those flags were relevant for
full-screen mode, but in fact this one is not.

android/src/WindowUtil.java
@@ -32,7 +32,6 @@ import android.view.View;
 class WindowUtil {
   static final int FULL_SCREEN_WINDOW_FLAGS =
     WindowManager.LayoutParams.FLAG_FULLSCREEN|
-    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
 
     /* Workaround for layout problems in Android KitKat with immersive full
        screen mode: Sometimes the content view was not initialized with the

android/src/XCSoar.java
@@ -29,6 +29,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.KeyEvent;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.os.Build;
 import android.os.Environment;
@@ -140,6 +141,9 @@ public class XCSoar extends Activity {
   };
 
   private void acquireWakeLock() {
+    final Window window = getWindow();
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
     if (wakeLock != null)
       return;
 