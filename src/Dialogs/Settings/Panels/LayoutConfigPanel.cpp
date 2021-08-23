XCSoar/XCSoar

commit 1d83a9ad949942607322d14a24a3314bacf3c967

Android: make full-screen mode optional
Closes https://github.com/XCSoar/XCSoar/issues/659

src/Dialogs/Settings/Panels/LayoutConfigPanel.cpp
@@ -37,12 +37,18 @@ Copyright_License {
 #include "Menu/ShowMenuButton.hpp"
 #include "ActionInterface.hpp"
 
+#ifdef ANDROID
+#include "Android/Main.hpp"
+#include "Android/NativeView.hpp"
+#endif
+
 #ifdef USE_POLL_EVENT
 #include "ui/event/Globals.hpp"
 #include "ui/event/Queue.hpp"
 #endif
 
 enum ControlIndex {
+  FullScreen,
   MapOrientation,
   AppInfoBoxGeom,
   AppFlarmLocation,
@@ -180,6 +186,11 @@ LayoutConfigPanel::Prepare(ContainerWindow &parent,
 
   RowFormWidget::Prepare(parent, rc);
 
+#ifdef ANDROID
+  AddBoolean(_("Full screen"), _("Run XCSoar in full screen mode"),
+             ui_settings.display.full_screen);
+#endif
+
   if (Display::RotateSupported())
     AddEnum(_("Display orientation"), _("Rotate the display on devices that support it."),
             display_orientation_list, (unsigned)ui_settings.display.orientation);
@@ -240,6 +251,12 @@ LayoutConfigPanel::Save(bool &_changed) noexcept
   bool changed = false;
 
   UISettings &ui_settings = CommonInterface::SetUISettings();
+
+#ifdef ANDROID
+  changed |= SaveValue(FullScreen, ProfileKeys::FullScreen,
+                       ui_settings.display.full_screen);
+  native_view->SetFullScreen(ui_settings.display.full_screen);
+#endif
 
   bool orientation_changed = false;
 

android/src/NativeView.java
@@ -276,6 +276,13 @@ class NativeView extends SurfaceView
     config = null;
   }
 
+  /**
+   * Called from native code.
+   */
+  void setFullScreen(boolean fullScreen) {
+    fullScreenHandler.sendEmptyMessage(fullScreen ? 1 : 0);
+  }
+
   private boolean setRequestedOrientation(int requestedOrientation) {
     try {
       ((Activity)getContext()).setRequestedOrientation(requestedOrientation);
@@ -337,8 +344,6 @@ class NativeView extends SurfaceView
              but we're about to be resumed, which means we're in
              foreground... */
         }
-
-        fullScreenHandler.sendEmptyMessage(1);
 
         try {
           runNative();

src/Android/NativeView.cpp
@@ -33,6 +33,7 @@ Copyright_License {
 Java::TrivialClass NativeView::cls;
 jfieldID NativeView::textureNonPowerOfTwo_field;
 jmethodID NativeView::init_surface_method, NativeView::deinit_surface_method;
+jmethodID NativeView::setFullScreen_method;
 jmethodID NativeView::setRequestedOrientationID;
 jmethodID NativeView::loadResourceBitmap_method;
 jmethodID NativeView::loadFileBitmap_method;
@@ -55,6 +56,10 @@ NativeView::Initialise(JNIEnv *env)
     env->GetStaticFieldID(cls, "textureNonPowerOfTwo", "Z");
   init_surface_method = env->GetMethodID(cls, "initSurface", "()Z");
   deinit_surface_method = env->GetMethodID(cls, "deinitSurface", "()V");
+
+  setFullScreen_method =
+    env->GetMethodID(cls, "setFullScreen", "(Z)V");
+
   setRequestedOrientationID =
     env->GetMethodID(cls, "setRequestedOrientation", "(I)Z");
 

src/Android/NativeView.hpp
@@ -45,6 +45,7 @@ class NativeView {
   static Java::TrivialClass cls;
   static jfieldID textureNonPowerOfTwo_field;
   static jmethodID init_surface_method, deinit_surface_method;
+  static jmethodID setFullScreen_method;
   static jmethodID setRequestedOrientationID;
   static jmethodID loadResourceBitmap_method;
   static jmethodID loadFileBitmap_method;
@@ -104,6 +105,10 @@ public:
 
   void deinitSurface() {
     env->CallVoidMethod(obj, deinit_surface_method);
+  }
+
+  void SetFullScreen(bool full_screen) const noexcept {
+    return env->CallVoidMethod(obj, setFullScreen_method, full_screen);
   }
 
   bool setRequestedOrientation(ScreenOrientation so) {

src/DisplaySettings.cpp
@@ -29,4 +29,5 @@ DisplaySettings::SetDefaults()
   orientation = DisplayOrientation::DEFAULT;
   cursor_size = 1;
   invert_cursor_colors = false;
+  full_screen = true;
 }

src/DisplaySettings.hpp
@@ -35,6 +35,7 @@ struct DisplaySettings {
   DisplayOrientation orientation;
   uint8_t cursor_size;
   bool invert_cursor_colors;
+  bool full_screen;
 
   void SetDefaults();
 };

src/Profile/ProfileKeys.cpp
@@ -25,6 +25,7 @@ Copyright_License {
 
 namespace ProfileKeys {
 
+const char FullScreen[] = "FullScreen";
 const char UIScale[] = "UIScale";
 const char CustomDPI[] = "CustomDPI";
 const char Password[] = "Password";

src/Profile/ProfileKeys.hpp
@@ -27,6 +27,7 @@ Copyright_License {
 namespace ProfileKeys {
 
 extern const char ShowMenuButton[];
+extern const char FullScreen[];
 extern const char UIScale[];
 extern const char CustomDPI[];
 extern const char Password[];

commit 24d96eefa6a573fabe6c6742217d494797b16f44

Dialogs/Settings/Panels/Layout: disable ControlIndex::FullScreen if unavailable
Fixes crash bug introduced by commit 1d83a9ad949942607322d14a24a3314bacf3c967

Closes https://github.com/XCSoar/XCSoar/issues/698

src/Dialogs/Settings/Panels/LayoutConfigPanel.cpp
@@ -48,7 +48,9 @@ Copyright_License {
 #endif
 
 enum ControlIndex {
+#ifdef ANDROID
   FullScreen,
+#endif
   MapOrientation,
   AppInfoBoxGeom,
   AppFlarmLocation,