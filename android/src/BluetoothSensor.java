XCSoar/XCSoar

commit ae9a597d1d76517b7aa898e37c115a6f35d3549e

android/InternalGPS: protect onSensorStateChanged() calls

android/src/InternalGPS.java
@@ -135,6 +135,21 @@ public class InternalGPS
     return state;
   }
 
+  private void setStateSafe(int _state) {
+    if (_state == state)
+      return;
+
+    state = _state;
+
+    if (!safeDestruct.increment()) {
+      try {
+        listener.onSensorStateChanged();
+      } finally {
+        safeDestruct.decrement();
+      }
+    }
+  }
+
   private void sendLocation(Location location) {
     Bundle extras = location.getExtras();
 
@@ -187,20 +202,18 @@ public class InternalGPS
                                         Bundle extras) {
     switch (status) {
     case LocationProvider.OUT_OF_SERVICE:
-      state = STATE_FAILED;
       setConnectedSafe(0); // not connected
-      listener.onSensorStateChanged();
+      setStateSafe(STATE_FAILED);
       break;
 
     case LocationProvider.TEMPORARILY_UNAVAILABLE:
-      state = STATE_LIMBO;
       setConnectedSafe(1); // waiting for fix
-      listener.onSensorStateChanged();
+      setStateSafe(STATE_LIMBO);
       break;
 
     case LocationProvider.AVAILABLE:
-      state = STATE_READY;
       listener.onSensorStateChanged();
+      setStateSafe(STATE_READY);
       break;
     }
   }

commit e6637fdd7409f5f43796af306891e1d146160387

android/{BluetoothSensor,InternalGPS}: fix inverted checks causing deadlock
Regressions introduced by commits ae9a597d1d76 and f5bbfe872bd4

android/src/BluetoothSensor.java
@@ -92,7 +92,7 @@ public final class BluetoothSensor
 
     state = _state;
 
-    if (!safeDestruct.increment()) {
+    if (safeDestruct.increment()) {
       try {
         listener.onSensorStateChanged();
       } finally {

android/src/InternalGPS.java
@@ -115,7 +115,7 @@ public class InternalGPS
 
     state = _state;
 
-    if (!safeDestruct.increment()) {
+    if (safeDestruct.increment()) {
       try {
         listener.onSensorStateChanged();
       } finally {