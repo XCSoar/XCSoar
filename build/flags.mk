XCSoar/XCSoar

commit 6c99c24ff88d7e8b990dfb820b77324ca506988b

build/flags.mk: switch from C99 to C11

build/flags.mk
@@ -6,7 +6,7 @@ ifeq ($(CLANG),n)
 CXX_FEATURES += -fconserve-space -fno-operator-names
 endif
 
-C_FEATURES = -std=gnu99
+C_FEATURES = -std=c11
 
 # produce position independent code when compiling the python library
 ifeq ($(MAKECMDGOALS),python)

commit 834e90015bd7e48a50829a8c81cf305f234ee67d

build/flags.mk: compile C in GNU mode
Fixes build failures caused by 6c99c24ff88d7e8b990dfb820b77324ca506988b

build/flags.mk
@@ -6,7 +6,8 @@ ifeq ($(CLANG),n)
 CXX_FEATURES += -fconserve-space -fno-operator-names
 endif
 
-C_FEATURES = -std=c11
+# Need GNU extensions for sources generated by tools/BinToC.pm
+C_FEATURES = -std=gnu11
 
 # produce position independent code when compiling the python library
 ifeq ($(MAKECMDGOALS),python)