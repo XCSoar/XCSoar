7e251061b7f2476138ef9b414daeb8a504526d8f
Max Kellermann <max.kellermann@gmail.com>
2021-09-02 21:30:25 +0200

    Operation/PluggableOperationEnvironment: new class

--- build/liboperation.mk
+++ build/liboperation.mk
@@ -1,5 +1,6 @@
 OPERATION_SOURCES := \
 	$(SRC)/Operation/Operation.cpp \
+	$(SRC)/Operation/PluggableOperationEnvironment.cpp \
 	$(SRC)/Operation/ProxyOperationEnvironment.cpp \
 	$(SRC)/Operation/NoCancelOperationEnvironment.cpp \
 	$(SRC)/Operation/ThreadedOperationEnvironment.cpp
