XCSoar/XCSoar

commit 550a3c7ddda4f7ea7df648df23b807e386f8f3a7

lib/main.mk: move some build rules to liboperation.mk

build/liboperation.mk
@@ -1,0 +1,7 @@
+OPERATION_SOURCES := \
+	$(SRC)/Operation/Operation.cpp \
+	$(SRC)/Operation/ProxyOperationEnvironment.cpp \
+	$(SRC)/Operation/NoCancelOperationEnvironment.cpp \
+	$(SRC)/Operation/ThreadedOperationEnvironment.cpp
+
+$(eval $(call link-library,liboperation,OPERATION))

Makefile
@@ -137,6 +137,7 @@ include $(topdir)/build/libcomputer.mk
 include $(topdir)/build/libos.mk
 include $(topdir)/build/libtime.mk
 include $(topdir)/build/libprofile.mk
+include $(topdir)/build/liboperation.mk
 include $(topdir)/build/libnet.mk
 include $(topdir)/build/libhttp.mk
 include $(topdir)/build/libcoroutines.mk

build/main.mk
@@ -219,12 +219,8 @@ XCSOAR_SOURCES := \
 	$(SRC)/Renderer/AirspaceRendererSettings.cpp \
 	$(SRC)/Renderer/GeoBitmapRenderer.cpp \
 	\
-	$(SRC)/Operation/Operation.cpp \
-	$(SRC)/Operation/ProxyOperationEnvironment.cpp \
-	$(SRC)/Operation/NoCancelOperationEnvironment.cpp \
 	$(SRC)/Operation/PopupOperationEnvironment.cpp \
 	$(SRC)/Operation/MessageOperationEnvironment.cpp \
-	$(SRC)/Operation/ThreadedOperationEnvironment.cpp \
 	$(SRC)/Operation/VerboseOperationEnvironment.cpp \
 	$(SRC)/Atmosphere/Pressure.cpp \
 	$(SRC)/Atmosphere/AirDensity.cpp \
@@ -687,6 +683,7 @@ XCSOAR_DEPENDS = \
 	LIBHTTP CO IO ASYNC TASK CONTEST ROUTE GLIDE WAYPOINT AIRSPACE \
 	LUA \
 	SHAPELIB ZZIP \
+	OPERATION \
 	LIBNET TIME OS THREAD \
 	UTIL GEO MATH
 

commit 64148106067de851676fd46bb9c2be3cee8b9490

build/liboperation.mk: add $(POLL_EVENT_CPPFLAGS)
Fixes an obscure crash caused by 550a3c7ddda4f7ea7df648df23b807e386f8f3a7

build/liboperation.mk
@@ -5,4 +5,9 @@ OPERATION_SOURCES := \
 	$(SRC)/Operation/NoCancelOperationEnvironment.cpp \
 	$(SRC)/Operation/ThreadedOperationEnvironment.cpp
 
+# This is necessary because ThreadedOperationEnvironment depends on
+# class UI::DelayedNotify, which embeds a UI::Timer, which has a
+# different implementation based on macro USE_POLL_EVENT
+OPERATION_CPPFLAGS = $(POLL_EVENT_CPPFLAGS)
+
 $(eval $(call link-library,liboperation,OPERATION))