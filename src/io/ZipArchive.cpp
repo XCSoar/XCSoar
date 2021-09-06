2590c5df552830ac79ce6f972696d92730a95688
Max Kellermann <max.kellermann@gmail.com>
2021-09-06 18:00:02 +0200

    io/ZipArchive: add `noexcept`

--- src/io/ZipArchive.cpp
+++ src/io/ZipArchive.cpp
@@ -35,21 +35,21 @@ ZipArchive::ZipArchive(Path path)
     throw std::runtime_error(std::string("Failed to open ZIP archive ") + (const char *)NarrowPathName(path));
 }
 
-ZipArchive::~ZipArchive()
+ZipArchive::~ZipArchive() noexcept
 {
   if (dir != nullptr)
     zzip_dir_close(dir);
 }
 
 bool
-ZipArchive::Exists(const char *name) const
+ZipArchive::Exists(const char *name) const noexcept
 {
   ZZIP_STAT st;
   return zzip_dir_stat(dir, name, &st, 0) == 0;
 }
 
 std::string
-ZipArchive::NextName()
+ZipArchive::NextName() noexcept
 {
   ZZIP_DIRENT e;
   return zzip_dir_read(dir, &e)
