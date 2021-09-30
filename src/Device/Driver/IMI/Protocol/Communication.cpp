XCSoar/XCSoar

commit 391398c7899eda74f7ac5fd88cc3712b5d4eadf7

Time/PeriodClock: pass duration as std::chrono::duration

src/Device/Driver/IMI/Protocol/Communication.cpp
@@ -85,8 +85,8 @@ IMI::Receive(Port &port, OperationEnvironment &env,
     /* fallback for timeout calculation */
     baudrate = 9600;
 
-  const TimeoutClock timeout(extraTimeout + 10000 *
-                             (expectedPayloadSize + sizeof(IMICOMM_MSG_HEADER_SIZE) + 10) / baudrate);
+  const TimeoutClock timeout(std::chrono::milliseconds(extraTimeout) + 10 *
+                             std::chrono::seconds((expectedPayloadSize + sizeof(IMICOMM_MSG_HEADER_SIZE) + 10) / baudrate));
 
   // wait for the message
   while (true) {

build/screen.mk
@@ -19,6 +19,7 @@ SCREEN_SOURCES = \
 	$(SCREEN_SRC_DIR)/SingleWindow.cpp
 
 SCREEN_CUSTOM_SOURCES = \
+	$(SCREEN_SRC_DIR)/Custom/DoubleClick.cpp \
 	$(SCREEN_SRC_DIR)/Custom/GeoBitmap.cpp \
 	$(SCREEN_SRC_DIR)/Custom/Pen.cpp \
 	$(SCREEN_SRC_DIR)/Custom/Timer.cpp \

src/BatteryTimer.hpp
@@ -32,7 +32,7 @@ class BatteryTimer {
 
   static constexpr unsigned BATTERY_WARNING = 10;
   static constexpr unsigned BATTERY_EXIT = 5;
-  static constexpr unsigned BATTERY_REMINDER = 5 * 60 * 1000;
+  static constexpr auto BATTERY_REMINDER = std::chrono::minutes(5);
 
   PeriodClock last_warning;
 

src/Computer/GlideComputer.cpp
@@ -152,7 +152,7 @@ GlideComputer::ProcessGPS(bool force)
   // Update the ConditionMonitors
   ConditionMonitorsUpdate(Basic(), Calculated(), settings);
 
-  return idle_clock.CheckUpdate(500);
+  return idle_clock.CheckUpdate(std::chrono::milliseconds(500));
 }
 
 void
@@ -205,7 +205,7 @@ GlideComputer::CalculateOwnTeamCode()
     return;
 
   // Only calculate every 10sec otherwise cancel calculation
-  if (!last_team_code_update.CheckUpdate(10000))
+  if (!last_team_code_update.CheckUpdate(std::chrono::seconds(10)))
     return;
 
   // Get bearing and distance to the reference waypoint

src/Device/Descriptor.cpp
@@ -624,7 +624,7 @@ DeviceDescriptor::AutoReopen(OperationEnvironment &env)
       !config.IsAvailable() ||
       !ShouldReopen() ||
       /* attempt to reopen a failed device every 30 seconds */
-      !reopen_clock.CheckUpdate(30000))
+      !reopen_clock.CheckUpdate(std::chrono::seconds(30)))
     return;
 
   TCHAR buffer[64];

src/Device/Driver/AltairPro.cpp
@@ -227,7 +227,7 @@ AltairProDevice::PropertySetGet(char *Buffer, size_t size,
 
   port.Flush();
 
-  TimeoutClock timeout(5000);
+  TimeoutClock timeout(std::chrono::seconds(5));
 
   // eg $PDVSC,S,FOO,BAR*<cr>\r\n
   if (!PortWriteNMEA(port, Buffer, env))

src/Device/Driver/EWMicroRecorder.cpp
@@ -111,7 +111,7 @@ TryConnect(Port &port, char *user_data, size_t max_user_data,
 
   unsigned user_size = 0;
 
-  TimeoutClock timeout(8000);
+  TimeoutClock timeout(std::chrono::seconds(8));
 
   while (true) {
     const size_t nbytes = port.WaitAndRead(user_data + user_size,

src/Device/Driver/Flytec/Logger.cpp
@@ -298,7 +298,7 @@ FlytecDevice::DownloadFlight(const RecordedFlightInfo &flight,
     if (StringIsEmpty(buffer))
       break;
 
-    if (status_clock.CheckUpdate(250) &&
+    if (status_clock.CheckUpdate(std::chrono::milliseconds(250)) &&
         *buffer == 'B') {
       // Parse the fix time
       BrokenTime time;

commit 4b139f6335848b488f8fe3d77469d7b59ffbc161

Driver/IMI: fix rounding error in timeout formula
Regression from commit 391398c7899

src/Device/Driver/IMI/Protocol/Communication.cpp
@@ -85,8 +85,8 @@ IMI::Receive(Port &port, OperationEnvironment &env,
     /* fallback for timeout calculation */
     baudrate = 9600;
 
-  const TimeoutClock timeout(std::chrono::milliseconds(extraTimeout) + 10 *
-                             std::chrono::seconds((expectedPayloadSize + sizeof(IMICOMM_MSG_HEADER_SIZE) + 10) / baudrate));
+  const TimeoutClock timeout(std::chrono::milliseconds(extraTimeout) +
+                             std::chrono::milliseconds(10000 * (expectedPayloadSize + sizeof(IMICOMM_MSG_HEADER_SIZE) + 10) / baudrate));
 
   // wait for the message
   while (true) {