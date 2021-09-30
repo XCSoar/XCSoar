ef569d3be3e4b37cba458ddbbab5e1335a021a88
Max Kellermann <max.kellermann@gmail.com>
2021-09-30 21:45:32 +0200

    Driver/IMI: remove duplicate CRC16 code

--- src/Device/Driver/IMI/Protocol/Checksum.cpp
+++ src/Device/Driver/IMI/Protocol/Checksum.cpp
@@ -22,24 +22,11 @@ Copyright_License {
 */
 
 #include "Checksum.hpp"
+#include "util/CRC.hpp"
 
 IMI::IMIWORD IMI::CRC16Checksum(const void *message, unsigned bytes)
 {
-  const IMIBYTE *pData = (const IMIBYTE *)message;
-
-  IMIWORD crc = 0xFFFF;
-  for(;bytes; bytes--) {
-    crc  = (IMIBYTE)(crc >> 8) | (crc << 8);
-    crc ^= *pData++;
-    crc ^= (IMIBYTE)(crc & 0xff) >> 4;
-    crc ^= (crc << 8) << 4;
-    crc ^= ((crc & 0xff) << 4) << 1;
-  }
-
-  if (crc == 0xFFFF)
-    crc = 0xAAAA;
-
-  return crc;
+  return UpdateCRC16CCITT(message, bytes, 0xffff);
 }
 
 IMI::IMIBYTE IMI::FixChecksum(const void *message, unsigned bytes)
