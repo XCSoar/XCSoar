XCSoar/XCSoar

commit e7888fe0bec731c007de60131dd564fd799e0515

Geo/GeoPoint: add isfinite() checks to Check()
Fixes crash bug after reading malformed topography file.

src/Geo/GeoPoint.hpp
@@ -100,8 +100,10 @@ struct GeoPoint {
    * Check if both longitude and latitude are in the allowed range.
    */
   constexpr bool Check() const noexcept {
-    return longitude >= -Angle::HalfCircle() &&
+    return std::isfinite(longitude.Native()) &&
+      longitude >= -Angle::HalfCircle() &&
       longitude <= Angle::HalfCircle() &&
+      std::isfinite(latitude.Native()) &&
       latitude >= -Angle::QuarterCircle() &&
       latitude <= Angle::QuarterCircle();
   }

commit 29f6f2a04b00255b8ab8bcfe776a851277ed9b55

Geo/GeoPoint: use __builtin_isfinite()
Fix build breakage with clang after commit e7888fe0bec because
std::isfinite() is not constexpr.

src/Geo/GeoPoint.hpp
@@ -26,6 +26,7 @@ Copyright_License {
 #define XCSOAR_GeoPoint_HPP
 
 #include "Math/Angle.hpp"
+#include "Math/Classify.hpp"
 
 #include <type_traits>
 
@@ -100,10 +101,10 @@ struct GeoPoint {
    * Check if both longitude and latitude are in the allowed range.
    */
   constexpr bool Check() const noexcept {
-    return std::isfinite(longitude.Native()) &&
+    return IsFinite(longitude.Native()) &&
       longitude >= -Angle::HalfCircle() &&
       longitude <= Angle::HalfCircle() &&
-      std::isfinite(latitude.Native()) &&
+      IsFinite(latitude.Native()) &&
       latitude >= -Angle::QuarterCircle() &&
       latitude <= Angle::QuarterCircle();
   }

src/Math/Classify.hpp
@@ -1,0 +1,36 @@
+/*
+Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2021 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+
+#pragma once
+
+/**
+ * A constexpr wrapper for std::isfinite().  This uses the
+ * non-standard __builtin_isfinite() function (specific to GCC and
+ * clang) because the C++ standard library is not "constexpr".
+ */
+template<typename T>
+constexpr bool
+IsFinite(T value) noexcept
+{
+  return __builtin_isfinite(value);
+}