a08413a84d025a29b7bb4f37f044c0f8d4cf669f
Max Kellermann <max.kellermann@gmail.com>
2021-11-02 09:27:47 +0100

    Math/Point2D: use C++20 constraints instead of std::enable_if

--- src/Math/Point2D.hpp
+++ src/Math/Point2D.hpp
@@ -28,6 +28,7 @@ Copyright_License {
 #include <cmath>
 
 template<typename T, typename PT=T>
+requires(std::is_arithmetic_v<T> && std::is_arithmetic_v<PT>)
 struct Point2D {
   using scalar_type = T;
 
@@ -126,22 +127,20 @@ struct FloatPoint2D : Point2D<float> {
 static_assert(std::is_trivial<FloatPoint2D>::value, "type is not trivial");
 
 template<typename P>
-using IsPoint2D = std::is_base_of<Point2D<typename P::scalar_type,
-                                          typename P::product_type>, P>;
+concept IsPoint2D = std::is_base_of_v<Point2D<typename P::scalar_type,
+                                              typename P::product_type>,
+                                      P>;
 
-template<typename P>
-using EnableIfPoint2D = std::enable_if_t<IsPoint2D<P>::value>;
-
-template<typename P, typename RT=typename P::product_type,
-         typename=EnableIfPoint2D<P>>
+template<typename P, typename RT=typename P::product_type>
+requires(IsPoint2D<P>)
 constexpr P
 operator+(P a, P b) noexcept
 {
   return P(a.x + b.x, a.y + b.y);
 }
 
-template<typename P, typename RT=typename P::product_type,
-         typename=EnableIfPoint2D<P>>
+template<typename P, typename RT=typename P::product_type>
+requires(IsPoint2D<P>)
 constexpr P
 operator-(P a, P b) noexcept
 {
@@ -149,9 +148,8 @@ operator-(P a, P b) noexcept
 }
 
 template<typename P, typename RT=typename P::product_type,
-         typename=EnableIfPoint2D<P>,
-         typename Z,
-         typename=std::enable_if_t<std::is_arithmetic<Z>::value>>
+         typename Z>
+requires(IsPoint2D<P> && std::is_arithmetic_v<Z>)
 constexpr P
 operator*(P a, Z z) noexcept
 {
@@ -159,25 +157,24 @@ operator*(P a, Z z) noexcept
 }
 
 template<typename P, typename RT=typename P::product_type,
-         typename=EnableIfPoint2D<P>,
-         typename Z,
-         typename=std::enable_if_t<std::is_arithmetic<Z>::value>>
+         typename Z>
+requires(IsPoint2D<P> && std::is_arithmetic_v<Z>)
 constexpr P
 operator/(P a, Z z) noexcept
 {
   return P(a.x / z, a.y / z);
 }
 
-template<typename P, typename RT=typename P::product_type,
-         typename=EnableIfPoint2D<P>>
+template<typename P, typename RT=typename P::product_type>
+requires(IsPoint2D<P>)
 constexpr RT
 DotProduct(P a, P b) noexcept
 {
   return RT(a.x) * RT(b.x) + RT(a.y) * RT(b.y);
 }
 
-template<typename P, typename RT=typename P::product_type,
-         typename=EnableIfPoint2D<P>>
+template<typename P, typename RT=typename P::product_type>
+requires(IsPoint2D<P>)
 constexpr RT
 CrossProduct(P a, P b) noexcept
 {
@@ -187,8 +184,8 @@ CrossProduct(P a, P b) noexcept
 /**
  * Calculates the "manhattan distance" or "taxicab distance".
  */
-template<typename P, typename RT=typename P::scalar_type,
-         typename=EnableIfPoint2D<P>>
+template<typename P, typename RT=typename P::scalar_type>
+requires(IsPoint2D<P>)
 constexpr RT
 ManhattanDistance(P a, P b) noexcept
 {
