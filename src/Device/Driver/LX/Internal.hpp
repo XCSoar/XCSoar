XCSoar/XCSoar

commit 70f845617c6aaf27eb08b9c7a653f06099fc94c3

Device/Driver/LX: Added detection of S8x, S10x, and NANO4 products

src/Device/Driver/LX/Internal.hpp
@@ -28,6 +28,7 @@ Copyright_License {
 #include "Device/Driver.hpp"
 #include "Device/SettingsMap.hpp"
 #include "thread/Mutex.hxx"
+#include "util/StaticString.hxx"
 
 #include <atomic>
 #include <cstdint>
@@ -60,6 +61,11 @@ class LXDevice: public AbstractDevice
    * Was a LXNAV V7 detected?
    */
   bool is_v7;
+
+  /**
+   * Was a LXNAV S series vario detected?
+   */
+  bool is_sVario;
 
   /**
    * Was a LXNAV Nano detected?
@@ -113,10 +119,31 @@ public:
   }
 
   /**
+   * Was a LXNAV S series vario detected?
+   */
+  bool IsSVario() const {
+    return is_sVario;
+  }
+
+  /**
+   * Was a LXNAV vario device detected?
+   */
+  bool IsLXNAVVario() const {
+    return IsV7() || IsSVario();
+  }
+
+  /**
    * Was a LXNAV Nano detected?
    */
   bool IsNano() const {
     return port_is_nano || is_nano || is_forwarded_nano;
+  }
+
+  /**
+   * Was an LXNAV logger device detected?
+   */
+  bool IsLXNAVLogger() const {
+    return IsNano() || IsSVario();
   }
 
   /**
@@ -127,7 +154,15 @@ public:
   }
 
   void ResetDeviceDetection() {
-    is_v7 = is_nano = is_lx16xx = is_forwarded_nano = false;
+    is_v7 = is_sVario = is_nano = is_lx16xx = is_forwarded_nano = false;
+  }
+
+  void IdDeviceByName(NarrowString<16> productName)
+  {
+    is_v7 = productName.equals("V7");
+    is_sVario = productName.equals("NINC") || productName.equals("S8x");
+    is_nano = productName.equals("NANO") || productName.equals("NANO3") || productName.equals("NANO4");
+    is_lx16xx = productName.equals("1606") || productName.equals("1600");
   }
 
   /**

src/Device/Driver/LX/Parser.cpp
@@ -357,9 +357,12 @@ LXDevice::ParseNMEA(const char *String, NMEAInfo &info)
       : info.device;
     LXWP1(line, device_info);
 
+    const bool saw_sVario = device_info.product.equals("NINC") || 
+                            device_info.product.equals("S8x");
     const bool saw_v7 = device_info.product.equals("V7");
     const bool saw_nano = device_info.product.equals("NANO") ||
-                            device_info.product.equals("NANO3");
+                            device_info.product.equals("NANO3") || 
+                            device_info.product.equals("NANO4");
     const bool saw_lx16xx = device_info.product.equals("1606") ||
                              device_info.product.equals("1600");
 
@@ -368,16 +371,18 @@ LXDevice::ParseNMEA(const char *String, NMEAInfo &info)
          because the V7 is still there, even though it's "hidden"
          currently */
       is_v7 |= saw_v7;
+      is_sVario |= saw_sVario;
       is_nano |= saw_nano;
       is_lx16xx |= saw_lx16xx;
       is_forwarded_nano = saw_nano;
     } else {
       is_v7 = saw_v7;
+      is_sVario = saw_sVario;
       is_nano = saw_nano;
       is_lx16xx = saw_lx16xx;
     }
 
-    if (saw_v7 || saw_nano || saw_lx16xx)
+    if (saw_v7 || saw_sVario || saw_nano || saw_lx16xx)
       is_colibri = false;
 
     return true;
@@ -400,7 +405,11 @@ LXDevice::ParseNMEA(const char *String, NMEAInfo &info)
     is_colibri = false;
     PLXVC(line, info.device, info.secondary_device, nano_settings);
     is_forwarded_nano = info.secondary_device.product.equals("NANO") ||
-                          info.secondary_device.product.equals("NANO3");
+                          info.secondary_device.product.equals("NANO3") ||
+                          info.secondary_device.product.equals("NANO4");
+
+    LXDevice::IdDeviceByName(info.device.product);
+
     return true;
   }
 

commit 9ceb0c30d5d7f5efcdddad36661980deaf3fca5f

Driver/LX: add missing initialiser for "is_sVario"
Fixes regression caused by commit 70f845617c6aaf27eb08b9c7a653f06099fc94c3

src/Device/Driver/LX/Internal.hpp
@@ -70,7 +70,7 @@ class LXDevice: public AbstractDevice
   /**
    * Was a LXNAV S series vario detected?
    */
-  bool is_sVario;
+  bool is_sVario = false;
 
   /**
    * Was a LXNAV Nano detected?