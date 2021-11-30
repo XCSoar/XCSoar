d255f03f72739d24bfc3916f1ad009208473ecfe
Max Kellermann <max.kellermann@gmail.com>
2021-11-30 09:54:58 +0100

    Dialogs/Error: add overload with message prefix

--- src/Dialogs/Error.cpp
+++ src/Dialogs/Error.cpp
@@ -25,10 +25,24 @@ Copyright_License {
 #include "Message.hpp"
 #include "util/ConvertString.hpp"
 #include "util/Exception.hxx"
+#include "util/StaticString.hxx"
 
 void
 ShowError(std::exception_ptr e, const TCHAR *caption) noexcept
 {
   ShowMessageBox(UTF8ToWideConverter(GetFullMessage(e).c_str()), caption,
+                 MB_OK|MB_ICONEXCLAMATION);
+}
+
+void
+ShowError(const TCHAR *msg, std::exception_ptr e,
+          const TCHAR *caption) noexcept
+{
+  StaticString<1024> buffer;
+  buffer.Format(_T("%s\n%s"), msg,
+                UTF8ToWideConverter(GetFullMessage(e).c_str()).c_str());
+  buffer.CropIncompleteUTF8();
+
+  ShowMessageBox(msg, caption,
                  MB_OK|MB_ICONEXCLAMATION);
 }
