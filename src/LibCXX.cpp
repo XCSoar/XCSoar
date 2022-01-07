88adae8507da3fe4914760d72b492e2458576ede
Max Kellermann <max.kellermann@gmail.com>
2022-01-07 14:31:49 +0100

    build/libcxx.mk: drop support for custom libc++ build

--- src/LibCXX.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-_LIBCPP_BEGIN_NAMESPACE_STD
-template class __vector_base_common<true>;
-_LIBCPP_END_NAMESPACE_STD
-
-#ifdef ANDROID
-extern "C" __attribute__((noreturn)) void __cxa_pure_virtual() { abort(); }
-#endif
