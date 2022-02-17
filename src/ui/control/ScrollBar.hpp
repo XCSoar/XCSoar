2dfbe9faef9f141e38a2a6ca8df46f270f429218
Max Kellermann <max.kellermann@gmail.com>
2022-02-17 17:51:25 +0100

    ui/control/ScrollBar: add `noexcept`

--- src/ui/control/ScrollBar.hpp
+++ src/ui/control/ScrollBar.hpp
@@ -45,25 +45,25 @@ protected:
 
 public:
   /** Constructor of the ScrollBar class */
-  explicit ScrollBar(const ButtonLook &button_look);
+  explicit ScrollBar(const ButtonLook &button_look) noexcept;
 
   /** Returns the width of the ScrollBar */
-  int GetWidth() const {
+  int GetWidth() const noexcept {
     return rc.GetWidth();
   }
 
   /** Returns the height of the ScrollBar */
-  int GetHeight() const {
+  int GetHeight() const noexcept {
     return rc.GetHeight();
   }
 
   /** Returns the height of the slider */
-  int GetSliderHeight() const {
+  int GetSliderHeight() const noexcept {
     return rc_slider.GetHeight();
   }
 
   /** Returns the height of the scrollable area of the ScrollBar */
-  int GetNettoHeight() const {
+  int GetNettoHeight() const noexcept {
     return std::max(GetHeight() - 2 * GetWidth() - 1, 0);
   }
 
@@ -71,7 +71,7 @@ public:
    * Returns the height of the visible scroll area of the ScrollBar
    * (the area thats not covered with the slider)
    */
-  int GetScrollHeight() const {
+  int GetScrollHeight() const noexcept {
     return std::max(GetNettoHeight() - GetSliderHeight(), 1);
   }
 
@@ -80,7 +80,7 @@ public:
    * @return True if the ScrollBar is defined,
    * False if it has to be set up first
    */
-  bool IsDefined() const {
+  bool IsDefined() const noexcept {
     return GetWidth() > 0;
   }
 
@@ -90,7 +90,7 @@ public:
    * @param size Size of the client area including the ScrollBar
    * @return The x-Coordinate of the ScrollBar
    */
-  unsigned GetLeft(const PixelSize size) const {
+  unsigned GetLeft(const PixelSize size) const noexcept {
     return IsDefined() ? rc.left : size.width;
   }
 
@@ -100,7 +100,7 @@ public:
    * @return True if the given PixelPoint is in the ScrollBar area,
    * False otherwise
    */
-  bool IsInside(const PixelPoint &pt) const {
+  bool IsInside(const PixelPoint &pt) const noexcept {
     return rc.Contains(pt);
   }
 
@@ -110,7 +110,7 @@ public:
    * @return True if the given PixelPoint is in the slider area,
    * False otherwise
    */
-  bool IsInsideSlider(const PixelPoint pt) const {
+  bool IsInsideSlider(const PixelPoint pt) const noexcept {
     return rc_slider.Contains(pt);
   }
 
@@ -120,7 +120,7 @@ public:
    * @return True if the given y-Coordinate is on the up arrow,
    * False otherwise
    */
-  bool IsInsideUpArrow(int y) const {
+  bool IsInsideUpArrow(int y) const noexcept {
     return y < rc.top + GetWidth();
   }
 
@@ -130,7 +130,7 @@ public:
    * @return True if the given y-Coordinate is on the down arrow,
    * False otherwise
    */
-  bool IsInsideDownArrow(int y) const {
+  bool IsInsideDownArrow(int y) const noexcept {
     return y >= rc.bottom - GetWidth();
   }
 
@@ -140,7 +140,7 @@ public:
    * @return True if the given y-Coordinate is above the slider area,
    * False otherwise
    */
-  bool IsAboveSlider(int y) const {
+  bool IsAboveSlider(int y) const noexcept {
     return y < rc_slider.top;
   }
 
@@ -150,7 +150,7 @@ public:
    * @return True if the given y-Coordinate is below the slider area,
    * False otherwise
    */
-  bool IsBelowSlider(int y) const {
+  bool IsBelowSlider(int y) const noexcept {
     return y >= rc_slider.bottom;
   }
 
@@ -159,25 +159,25 @@ public:
    * (actually just the height, width is automatically set)
    * @param size Size of the Control the ScrollBar is used with
    */
-  void SetSize(const PixelSize size);
+  void SetSize(const PixelSize size) noexcept;
 
   /** Resets the ScrollBar (undefines it) */
-  void Reset();
+  void Reset() noexcept;
 
   /** Calculates the size and position of the slider */
-  void SetSlider(unsigned size, unsigned view_size, unsigned origin);
+  void SetSlider(unsigned size, unsigned view_size, unsigned origin) noexcept;
 
   /** Calculates the new origin out of the given y-Coordinate of the drag */
-  unsigned ToOrigin(unsigned size, unsigned view_size, int y) const;
+  unsigned ToOrigin(unsigned size, unsigned view_size, int y) const noexcept;
 
   /** Paints the ScollBar */
-  void Paint(Canvas &canvas) const;
+  void Paint(Canvas &canvas) const noexcept;
 
   /**
    * Returns whether the slider is currently being dragged
    * @return True if the slider is currently being dragged, False otherwise
    */
-  bool IsDragging() const {
+  bool IsDragging() const noexcept {
     return dragging;
   }
 
@@ -187,14 +187,14 @@ public:
    * @param w The Window object the ScrollBar is belonging to
    * @param y y-Coordinate
    */
-  void DragBegin(PaintWindow *w, unsigned y);
+  void DragBegin(PaintWindow *w, unsigned y) noexcept;
 
   /**
    * Should be called when stopping to drag
    * (Called by ListControl::OnMouseUp)
    * @param w The Window object the ScrollBar is belonging to
    */
-  void DragEnd(PaintWindow *w);
+  void DragEnd(PaintWindow *w) noexcept;
 
   /**
    * Should be called while dragging
@@ -203,5 +203,5 @@ public:
    * @param y y-Coordinate
    * @return "Value" of the ScrollBar
    */
-  unsigned DragMove(unsigned  size, unsigned view_size, int y) const;
+  unsigned DragMove(unsigned  size, unsigned view_size, int y) const noexcept;
 };
