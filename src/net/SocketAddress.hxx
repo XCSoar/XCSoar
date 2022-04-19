9ddd68b00d4735a21f81b262820ab1b370c07572
Max Kellermann <mk@cm4all.com>
2022-04-19 17:29:48 +0200

    net/SocketAddress: GetSteadyPart() returns std::span

--- src/net/SocketAddress.hxx
+++ src/net/SocketAddress.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 Max Kellermann <max.kellermann@gmail.com>
+ * Copyright 2012-2022 Max Kellermann <max.kellermann@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,16 +32,19 @@
 
 #include "Features.hxx"
 
-#include <cstddef>
-
 #ifdef _WIN32
 #include <winsock2.h> // IWYU pragma: export
 #else
 #include <sys/socket.h> // IWYU pragma: export
 #endif
 
-template<typename T> struct ConstBuffer;
-struct StringView;
+#include <cstddef>
+#include <span>
+
+#ifdef HAVE_UN
+#include <string_view>
+#endif
+
 class IPv4Address;
 
 /**
@@ -118,7 +121,7 @@ public:
 	 * nullptr if not applicable.
 	 */
 	[[gnu::pure]]
-	StringView GetLocalRaw() const noexcept;
+	std::string_view GetLocalRaw() const noexcept;
 
 	/**
 	 * Returns the local socket path or nullptr if not applicable
@@ -162,7 +165,7 @@ public:
 	 * not supported.
 	 */
 	[[gnu::pure]]
-	ConstBuffer<void> GetSteadyPart() const noexcept;
+	std::span<const std::byte> GetSteadyPart() const noexcept;
 
 	[[gnu::pure]]
 	bool operator==(const SocketAddress other) const noexcept;
