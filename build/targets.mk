3674cca8dbdd061f1380b79358b5cde1ab56d6e5
Max Kellermann <max.kellermann@gmail.com>
2022-04-26 21:29:08 +0200

    Fix targets.mk reset android sdk

--- build/targets.mk
+++ build/targets.mk
@@ -313,7 +313,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= /home/volkmar/Android/Sdk/ndk/23.1.7779620
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r24
 
   ANDROID_SDK_PLATFORM = android-30
   ANDROID_NDK_API = 21
