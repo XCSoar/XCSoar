a64b0fbe64742f269bbd325bd1c6fa976ef1265f
Max Kellermann <max.kellermann@gmail.com>
2022-06-08 14:17:14 +0200

    util/SpanCast: conditional "const" for the void* cast

--- src/util/SpanCast.hxx
+++ src/util/SpanCast.hxx
@@ -47,11 +47,11 @@ FromBytesFloor(std::span<CopyConst<T, std::byte>> other) noexcept
 {
 	static_assert(sizeof(T) > 0, "Empty base type");
 
-	/* TODO: the "const void *" cast suppresses alignment
+	/* TODO: the "void *" cast suppresses alignment
 	   warnings, but should we really suppress them? */
 
 	return {
-		reinterpret_cast<T *>(reinterpret_cast<const void *>(other.data())),
+		reinterpret_cast<T *>(reinterpret_cast<CopyConst<T, void> *>(other.data())),
 		other.size() / sizeof(T),
 	};
 }
