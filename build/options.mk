b290387d70cbf08644ff8c3121443abaf784164b
Honza Liška <jan.liska@cleverlance.com>
2022-06-17 08:50:13 +0200

    Build prepnuty na std. verzi

--- build/options.mk
+++ build/options.mk
@@ -50,8 +50,8 @@ endif
 # a red Activity icon, to allow simultaneous installation of "stable"
 # and "testing".
 # In the stable branch, this should default to "n".
-# TESTING = n
-TESTING = y
+TESTING = n
+# TESTING = y
 
 ifeq ($(TESTING),y)
   TARGET_CPPFLAGS += -DXCSOAR_TESTING
