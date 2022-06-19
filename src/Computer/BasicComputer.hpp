3f6f36edd729f5f42f2b69d8db5ab191acb20f36
Peter F Bradshaw <pfb@exadios.com>
2022-06-19 13:44:08 +0800

    Documentation: BasicComputer
    Apparently the name of the NmeaInfo class has been changed sometime in
    the past. Comment updated to make link whole again.

--- src/Computer/BasicComputer.hpp
+++ src/Computer/BasicComputer.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -33,7 +33,7 @@ struct FeaturesSettings;
 struct ComputerSettings;
 
 /**
- * A computer which adds missing values to #NMEA_INFO.  It performs
+ * A computer which adds missing values to #NEMAInfo.  It performs
  * simple and fast calculations after every GPS update, cheap enough
  * to run outside of the #CalculationThread.
  */
