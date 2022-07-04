919b73bd81682f7508c7bfee5817926adf1e4866
Christian Mötzing <christian@moetzing.net>
2022-07-04 10:41:02 +0200

    IDE: add missing package for OpenGL math

--- ide/provisioning/install-debian-packages.sh
+++ ide/provisioning/install-debian-packages.sh
@@ -47,7 +47,7 @@ apt-get install ${APTOPTS[*]} make g++ \
   libsdl2-dev \
   librsvg2-bin xsltproc \
   imagemagick gettext \
-  mesa-common-dev libgl1-mesa-dev libegl1-mesa-dev \
+  mesa-common-dev libgl1-mesa-dev libegl1-mesa-dev libglm-dev \
   fonts-dejavu \
   xz-utils
 echo
