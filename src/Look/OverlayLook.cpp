3195969d2cf5538b91992efa4268753547b40eb5
Philipp Wollschlegel <folken@kabelsalat.ch>
2022-07-05 02:26:08 +0200

    look: crosshair pen and alias look with dpi scaling

--- src/Look/OverlayLook.cpp
+++ src/Look/OverlayLook.cpp
@@ -31,5 +31,6 @@ OverlayLook::Initialise(const Font &font, const Font &bold_font)
   map_scale_left_icon.LoadResource(IDB_MAPSCALE_LEFT, IDB_MAPSCALE_LEFT_HD, false);
   map_scale_right_icon.LoadResource(IDB_MAPSCALE_RIGHT, IDB_MAPSCALE_RIGHT_HD, false);
   overlay_font = &bold_font;
-  crosshair_pen.Create(Pen::DASH2, 1, COLOR_DARK_GRAY);
+  crosshair_pen.Create(Pen::SOLID, Layout::ScalePenWidth(2), COLOR_DARK_GRAY);
+  crosshair_pen_alias.Create(Pen::SOLID, Layout::ScalePenWidth(2), COLOR_WHITE);
 }
