6d7ed7a8609b16e1a50bdebb7fb2836c4856b2cb
Max Kellermann <max.kellermann@gmail.com>
2022-07-22 08:20:01 +0200

    build/python/build/libs.py: update OpenSSL to 3.0.5

--- build/python/build/libs.py
+++ build/python/build/libs.py
@@ -96,9 +96,9 @@ musl = MuslProject(
 )
 
 openssl = OpenSSLProject(
-    'https://www.openssl.org/source/openssl-3.0.0.tar.gz',
-    'ftp://ftp.cert.dfn.de/pub/tools/net/openssl/source/openssl-3.0.0.tar.gz',
-    '59eedfcb46c25214c9bd37ed6078297b4df01d012267fe9e9eee31f61bc70536',
+    'https://www.openssl.org/source/openssl-3.0.5.tar.gz',
+    'ftp://ftp.cert.dfn.de/pub/tools/net/openssl/source/openssl-3.0.5.tar.gz',
+    'aa7d8d9bef71ad6525c55ba11e5f4397889ce49c2c9349dcea6d3e4f0b024a7a',
     'include/openssl/ossl_typ.h',
 )
 
