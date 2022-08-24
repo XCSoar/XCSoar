b3723e0762c24a6e38eb603dbc607fa668878046
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-24 14:56:34 +0200

    Skylines/Glue: use [[gnu::pure]]

--- src/Tracking/SkyLines/Glue.hpp
+++ src/Tracking/SkyLines/Glue.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_TRACKING_SKYLINES_GLUE_HPP
-#define XCSOAR_TRACKING_SKYLINES_GLUE_HPP
+#pragma once
 
 #include "Client.hpp"
 #include "time/GPSClock.hpp"
@@ -70,7 +69,7 @@ public:
   }
 
 private:
-  gcc_pure
+  [[gnu::pure]]
   bool IsConnected() const;
 
   void SendFixes(const NMEAInfo &basic);
@@ -78,5 +77,3 @@ private:
 };
 
 } /* namespace SkyLinesTracking */
-
-#endif
