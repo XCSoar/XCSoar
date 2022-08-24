2baebccf8b46ac1cdd58cfde97d1501d132c05f7
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-24 14:52:58 +0200

    src/Units/: use #pragma once

--- src/Units/UnitsStore.hpp
+++ src/Units/UnitsStore.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_UNITS_STORE_HPP
-#define XCSOAR_UNITS_STORE_HPP
+#pragma once
 
 #include <tchar.h>
 
@@ -54,5 +53,3 @@ unsigned
 EqualsPresetUnits(const UnitSetting &config) noexcept;
 
 } // namespace Units::Store
-
-#endif
