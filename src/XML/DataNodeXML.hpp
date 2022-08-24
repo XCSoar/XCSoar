964a68f0ee3109d7d4ffd9a7ba9a110432545d1d
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-24 14:51:51 +0200

    XML/DataNodeXML: use #pragma once

--- src/XML/DataNodeXML.hpp
+++ src/XML/DataNodeXML.hpp
@@ -1,7 +1,7 @@
 /* Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -20,8 +20,7 @@
 }
  */
 
-#ifndef DATANODE_XML_HPP
-#define DATANODE_XML_HPP
+#pragma once
 
 #include "DataNode.hpp"
 
@@ -73,5 +72,3 @@ public:
   std::unique_ptr<WritableDataNode> AppendChild(const TCHAR *name) noexcept override;
   void SetAttribute(const TCHAR *name, const TCHAR *value) noexcept override;
 };
-
-#endif
