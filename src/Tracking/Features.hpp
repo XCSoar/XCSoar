288a42372cdfd3d1f88f356ff66b06fe6e309661
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-24 14:57:26 +0200

    src/Tracking/: use #pragma once

--- src/Tracking/Features.hpp
+++ src/Tracking/Features.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_TRACKING_FEATURES_HPP
-#define XCSOAR_TRACKING_FEATURES_HPP
+#pragma once
 
 #include "net/http/Features.hpp"
 #include "Tracking/SkyLines/Features.hpp"
@@ -34,6 +33,4 @@ Copyright_License {
 
 #if defined(HAVE_SKYLINES_TRACKING) || defined(HAVE_LIVETRACK24)
 #define HAVE_TRACKING
-#endif
-
 #endif
