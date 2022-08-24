cef4cd6f3f55696316611cb4ce631803c677a428
Max Kellermann <max.kellermann@gmail.com>
2022-08-24 20:06:30 +0200

    MapWindow/Thermal: call GetComputerSettings(), not ComputerSettings()
    ComputerSettings() constructs a default-/zero-initialized
    ComputerSettings instance.  This broke thermalmap.info icon display.
    D'oh, what a stupid bug!
    
    Closes https://github.com/XCSoar/XCSoar/issues/922

--- src/MapWindow/MapWindowThermal.cpp
+++ src/MapWindow/MapWindowThermal.cpp
@@ -69,7 +69,7 @@ MapWindow::DrawThermalEstimate(Canvas &canvas) const
                      calculated.wind_available
                      ? calculated.wind : SpeedVector::Zero());
 
-  const auto &cloud_settings = ComputerSettings().tracking.skylines.cloud;
+  const auto &cloud_settings = GetComputerSettings().tracking.skylines.cloud;
   if (cloud_settings.show_thermals && skylines_data != nullptr) {
     std::lock_guard<Mutex> lock(skylines_data->mutex);
     for (auto &i : skylines_data->thermals) {
@@ -79,7 +79,7 @@ MapWindow::DrawThermalEstimate(Canvas &canvas) const
     }
   }
 
-  if (tim_glue != nullptr && ComputerSettings().weather.enable_tim)
+  if (tim_glue != nullptr && GetComputerSettings().weather.enable_tim)
     for (const auto &i : tim_glue->Get())
       if (auto p = render_projection.GeoToScreenIfVisible(i.location))
         look.thermal_source_icon.Draw(canvas, *p);
