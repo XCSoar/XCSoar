d06e771f141cfd05f50cde90536e23ae594fe4a1
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-25 13:20:58 +0200

    src/Repository/: use #pragma once

--- src/Repository/AvailableFile.hpp
+++ src/Repository/AvailableFile.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_AVAILABLE_FILE_HPP
-#define XCSOAR_AVAILABLE_FILE_HPP
+#pragma once
 
 #include "util/StaticString.hxx"
 #include "FileType.hpp"
@@ -98,5 +97,3 @@ struct AvailableFile {
     return area;
   }
 };
-
-#endif
