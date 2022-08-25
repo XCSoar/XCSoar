1a091c76cd0062c27dc0463fae2af7e53d299177
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-25 13:19:46 +0200

    src/Screen/: use #pragma once

--- src/Screen/Layout.hpp
+++ src/Screen/Layout.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_SCREEN_LAYOUT_HPP
-#define XCSOAR_SCREEN_LAYOUT_HPP
+#pragma once
 
 #include "ui/dim/Size.hpp"
 
@@ -302,5 +301,3 @@ GetHitRadius() noexcept
 }
 
 } // namespace Layout
-
-#endif
--- src/Screen/StopWatch.hpp
+++ src/Screen/StopWatch.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_SCREEN_STOP_WATCH_HPP
-#define XCSOAR_SCREEN_STOP_WATCH_HPP
+#pragma once
 
 #ifdef STOP_WATCH
 
@@ -160,5 +159,3 @@ public:
   void Finish() {}
 #endif /* !STOP_WATCH */
 };
-
-#endif
