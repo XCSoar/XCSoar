607e5bd5f6612cb3a6ade87004795335bbe14819
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-25 13:22:03 +0200

    src/Replay/: use #pragma once

--- src/Replay/NmeaReplay.hpp
+++ src/Replay/NmeaReplay.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef NMEA_REPLAY_HPP
-#define NMEA_REPLAY_HPP
+#pragma once
 
 #include "AbstractReplay.hpp"
 #include "time/ReplayClock.hpp"
@@ -59,5 +58,3 @@ protected:
 private:
   bool ReadUntilRMC(NMEAInfo &data);
 };
-
-#endif
