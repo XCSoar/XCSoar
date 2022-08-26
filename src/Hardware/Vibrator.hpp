2773f62dd3ed085d952c0732c4f5462e541aeab4
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-26 11:16:11 +0200

    Vibrator.hpp: use [[gnu::const]]

--- src/Hardware/Vibrator.hpp
+++ src/Hardware/Vibrator.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,12 +21,9 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_HARDWARE_VIBRATOR_HPP
-#define XCSOAR_HARDWARE_VIBRATOR_HPP
+#pragma once
 
 #ifdef ANDROID
-
-#include "util/Compiler.h"
 
 /**
  * This macro specifies whether this platform has support for a
@@ -38,7 +35,7 @@ Copyright_License {
 /**
  * Check whether this device has a vibrator.
  */
-gcc_const
+[[gnu::const]]
 bool
 HaveVibrator();
 
@@ -57,7 +54,5 @@ HaveVibrator()
 {
   return false;
 }
-
-#endif
 
 #endif
