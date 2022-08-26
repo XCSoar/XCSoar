139cf5f1cd438d0a75c47010f1ff7d4eac517fa6
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-26 11:09:19 +0200

    src/json/: use #pragma once

--- src/json/Geo.hpp
+++ src/json/Geo.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
  */
 
-#ifndef XCSOAR_JSON_GEO_HPP
-#define XCSOAR_JSON_GEO_HPP
+#pragma once
 
 #include "Geo/GeoPoint.hpp"
 
@@ -44,5 +43,3 @@ tag_invoke(boost::json::value_from_tag, boost::json::value &jv,
     {"latitude", request.latitude},
   };
 }
-
-#endif
