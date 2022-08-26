4d9181eca745d991797844e90290d4231e18183f
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-26 11:11:03 +0200

    src/Logger/: use #pragma once

--- src/Logger/GlueFlightLogger.hpp
+++ src/Logger/GlueFlightLogger.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_GLUE_FLIGHT_LOGGER_HPP
-#define XCSOAR_GLUE_FLIGHT_LOGGER_HPP
+#pragma once
 
 #include "Blackboard/BlackboardListener.hpp"
 #include "FlightLogger.hpp"
@@ -58,5 +57,3 @@ private:
   virtual void OnCalculatedUpdate(const MoreData &basic,
                                   const DerivedInfo &calculated);
 };
-
-#endif
--- src/Logger/LoggerImpl.hpp
+++ src/Logger/LoggerImpl.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#if !defined(XCSOAR_LOGGER_IMPL_HPP)
-#define XCSOAR_LOGGER_IMPL_HPP
+#pragma once
 
 #include "LoggerFRecord.hpp"
 #include "time/BrokenDateTime.hpp"
@@ -143,5 +142,3 @@ private:
   void LogPointToBuffer(const NMEAInfo &gps_info) noexcept;
   void WritePoint(const NMEAInfo &gps_info);
 };
-
-#endif
