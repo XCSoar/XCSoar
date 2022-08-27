3b2179d6be562c9decef65b12b80481e6d9c8260
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-27 10:57:04 +0200

    ActivePredicate.hpp: use [[gnu::pure]]

--- src/Airspace/ActivePredicate.hpp
+++ src/Airspace/ActivePredicate.hpp
@@ -1,7 +1,7 @@
 /* Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -20,10 +20,7 @@
 }
 */
 
-#ifndef XCSOAR_ACTIVE_AIRSPACE_PREDICATE_HPP
-#define XCSOAR_ACTIVE_AIRSPACE_PREDICATE_HPP
-
-#include "util/Compiler.h"
+#pragma once
 
 class ProtectedAirspaceWarningManager;
 class AbstractAirspace;
@@ -44,8 +41,6 @@ public:
   ActiveAirspacePredicate(const ProtectedAirspaceWarningManager *_warnings)
     :warnings(_warnings) {}
 
-  gcc_pure
+  [[gnu::pure]]
   bool operator()(const AbstractAirspace &airspace) const;
 };
-
-#endif
