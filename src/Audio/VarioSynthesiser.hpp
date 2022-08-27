fb268be4bbb51adeca404bce44d61cbf110de41f
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-27 10:53:58 +0200

    VarioSynthesiser use [[gnu::const]]

--- src/Audio/VarioSynthesiser.hpp
+++ src/Audio/VarioSynthesiser.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,12 +21,10 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_AUDIO_VARIO_SYNTHESISER_HPP
-#define XCSOAR_AUDIO_VARIO_SYNTHESISER_HPP
+#pragma once
 
 #include "ToneSynthesiser.hpp"
 #include "thread/Mutex.hxx"
-#include "util/Compiler.h"
 
 /**
  * This class generates vario sound.
@@ -158,12 +156,10 @@ private:
    *
    * @param ivario the current vario value [cm/s]
    */
-  gcc_const
+  [[gnu::const]]
   unsigned VarioToFrequency(int ivario);
 
   bool InDeadBand(int ivario) {
     return ivario >= min_dead && ivario <= max_dead;
   }
 };
-
-#endif
