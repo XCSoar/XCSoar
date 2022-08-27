f8cf67edcd7addc035c3f18512acf1afee3bb59c
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-27 10:54:33 +0200

    src/Atmosphere/: use #pragma once

--- src/Atmosphere/CuSonde.hpp
+++ src/Atmosphere/CuSonde.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_ATMOSPHERE_CUSONDE_HPP
-#define XCSOAR_ATMOSPHERE_CUSONDE_HPP
+#pragma once
 
 #include "Atmosphere/Temperature.hpp"
 
@@ -99,5 +98,3 @@ public:
   void FindThermalHeight(unsigned short level) noexcept;
   void SetForecastTemperature(Temperature temperature) noexcept;
 };
-
-#endif
--- src/Atmosphere/Temperature.hpp
+++ src/Atmosphere/Temperature.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_ATMOSPHERE_TEMPERATURE_HPP
-#define XCSOAR_ATMOSPHERE_TEMPERATURE_HPP
+#pragma once
 
 #include <cmath>
 #include <compare> // for the defaulted spaceship operator
@@ -139,5 +138,3 @@ public:
   static Temperature FromUser(double value) noexcept;
   double ToUser() const noexcept;
 };
-
-#endif
