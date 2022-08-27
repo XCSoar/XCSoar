ae4e21816330f50d2dc74a44f3f12780a3e58620
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-27 10:47:29 +0200

    src/CrossSection/: use #pragma once

--- src/CrossSection/CrossSectionWidget.hpp
+++ src/CrossSection/CrossSectionWidget.hpp
@@ -2,7 +2,7 @@
   Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@
 }
 */
 
-#ifndef XCSOAR_CROSS_SECTION_WIDGET_HPP
-#define XCSOAR_CROSS_SECTION_WIDGET_HPP
+#pragma once
 
 #include "Widget/WindowWidget.hpp"
 #include "Blackboard/BlackboardListener.hpp"
@@ -45,5 +44,3 @@ private:
   virtual void OnCalculatedUpdate(const MoreData &basic,
                                   const DerivedInfo &calculated) override;
 };
-
-#endif
--- src/CrossSection/CrossSectionWindow.hpp
+++ src/CrossSection/CrossSectionWindow.hpp
@@ -2,7 +2,7 @@
   Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@
 }
 */
 
-#ifndef CROSS_SECTION_WINDOW_HPP
-#define CROSS_SECTION_WINDOW_HPP
+#pragma once
 
 #include "ui/window/BufferWindow.hpp"
 #include "CrossSectionRenderer.hpp"
@@ -115,5 +114,3 @@ protected:
   /* virtual methods from AntiFlickerWindow */
   virtual void OnPaintBuffer(Canvas &canvas) noexcept override;
 };
-
-#endif
