8930bd150132afa4e2a8f24342d8026cab4a1213
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-27 10:50:41 +0200

    ComputerSettingsBlackboard: use [[gnu::pure]]

--- src/Blackboard/ComputerSettingsBlackboard.hpp
+++ src/Blackboard/ComputerSettingsBlackboard.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,11 +21,9 @@ Copyright_License {
 }
 */
 
-#ifndef COMPUTER_SETTINGS_BLACKBOARD_HPP
-#define COMPUTER_SETTINGS_BLACKBOARD_HPP
+#pragma once
 
 #include "Computer/Settings.hpp"
-#include "util/Compiler.h"
 
 class ComputerSettingsBlackboard
 {
@@ -33,10 +31,8 @@ protected:
   ComputerSettings computer_settings;
 
 public:
-  gcc_const
+  [[gnu::const]]
   const ComputerSettings& GetComputerSettings() const {
     return computer_settings;
   }
 };
-
-#endif
