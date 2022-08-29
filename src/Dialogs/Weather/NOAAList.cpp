ba04a09aa3bf2318dda3b79c48a1408a42495e95
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:08:11 +0200

    NOAAList: use [[gnu::pure]]

--- src/Dialogs/Weather/NOAAList.cpp
+++ src/Dialogs/Weather/NOAAList.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -67,7 +67,7 @@ class NOAAListWidget final
     StaticString<5> code;
     NOAAStore::iterator iterator;
 
-    gcc_pure
+    [[gnu::pure]]
     bool operator<(const ListItem &i2) const {
       return StringCollate(code, i2.code) < 0;
     }
