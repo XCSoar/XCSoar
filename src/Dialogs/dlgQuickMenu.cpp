d7af23e338335da3e291a53fffd9937906fbe17a
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:09:31 +0200

    dlgQuickMenu: use [[gnu::pure]]

--- src/Dialogs/dlgQuickMenu.cpp
+++ src/Dialogs/dlgQuickMenu.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -59,7 +59,7 @@ public:
     text_renderer.SetControl();
   }
 
-  gcc_pure
+  [[gnu::pure]]
   unsigned GetMinimumButtonWidth() const noexcept override;
 
   void DrawButton(Canvas &canvas, const PixelRect &rc,
