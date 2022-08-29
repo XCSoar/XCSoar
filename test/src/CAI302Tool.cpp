eebc3e9d6cfb2fcc209192a5feac7a8bcc7ffd4c
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 12:58:03 +0200

    test/src/CAI302Tool: use [[gnu::pure]]

--- test/src/CAI302Tool.cpp
+++ test/src/CAI302Tool.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -36,7 +36,7 @@ Copyright_License {
 
 #include <stdio.h>
 
-gcc_pure
+[[gnu::pure]]
 static int
 StringBufferLength(const char *buffer, size_t size)
 {
