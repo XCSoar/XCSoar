fb2396fb67fba94b30925d44755f39def0f82c6f
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:08:41 +0200

    TaskListPanel: use [[gnu::pure]]

--- src/Dialogs/Task/Manager/TaskListPanel.cpp
+++ src/Dialogs/Task/Manager/TaskListPanel.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -115,7 +115,7 @@ public:
 protected:
   const OrderedTask *get_cursor_task();
 
-  gcc_pure
+  [[gnu::pure]]
   const TCHAR *get_cursor_name();
 
 private:
