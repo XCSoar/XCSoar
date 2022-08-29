741e3ea020eb43f220ad6bc833edf9a6aa968a3c
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:10:43 +0200

    AirspaceList: use [[gnu::pure]]

--- src/Dialogs/Airspace/AirspaceList.cpp
+++ src/Dialogs/Airspace/AirspaceList.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -316,7 +316,7 @@ AirspaceListWidget::OnPaintItem(Canvas &canvas, const PixelRect rc,
       CommonInterface::GetMapSettings().airspace);
 }
 
-gcc_pure
+[[gnu::pure]]
 static const TCHAR *
 GetHeadingString(TCHAR *buffer)
 {
