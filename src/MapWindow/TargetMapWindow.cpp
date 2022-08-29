f55be2b6a9df7b370d939df42ae6efce3320d116
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:04:45 +0200

    TargetMapWindow: use [[gnu::pure]]

--- src/MapWindow/TargetMapWindow.cpp
+++ src/MapWindow/TargetMapWindow.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -268,7 +268,7 @@ TargetMapWindow::SetTopograpgy(TopographyStore *topography)
     : nullptr;
 }
 
-gcc_pure
+[[gnu::pure]]
 static double
 GetRadius(const ObservationZonePoint &oz)
 {
@@ -292,7 +292,7 @@ GetRadius(const ObservationZonePoint &oz)
   return 1;
 }
 
-gcc_pure
+[[gnu::pure]]
 static double
 GetRadius(const OrderedTaskPoint &tp)
 {
