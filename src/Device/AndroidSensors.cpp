90ece78561362b1de75e74c1aee03c5d12e0af27
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:12:40 +0200

    AndroidSensors: use [[gnu::pure]]

--- src/Device/AndroidSensors.cpp
+++ src/Device/AndroidSensors.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -164,7 +164,7 @@ DeviceDescriptor::OnMagneticFieldSensor(float h_x, float h_y,
  * t, then taking the derivative with respect to t. The dP(t)/dt term
  * is the pressure change rate.
  */
-gcc_pure
+[[gnu::pure]]
 static inline double
 ComputeNoncompVario(const double pressure, const double d_pressure)
 {
