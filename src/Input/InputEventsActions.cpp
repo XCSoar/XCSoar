1be0bc1c496b0dfe77179094d39727ead9086d2c
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:06:33 +0200

    InputEventsActions: use [[gnu::pure]]

--- src/Input/InputEventsActions.cpp
+++ src/Input/InputEventsActions.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -107,7 +107,7 @@ doc/html/advanced/input/ALL		http://xcsoar.sourceforge.net/advanced/input/
 /**
  * Determine the reference location of the current map display.
  */
-gcc_pure
+[[gnu::pure]]
 static GeoPoint
 GetVisibleLocation()
 {
