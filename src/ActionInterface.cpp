cff83664c3a575f0440f6d67ddc5ca5e6ac1653c
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:12:59 +0200

    ActionInterface: use [[gnu::pure]]

--- src/ActionInterface.cpp
+++ src/ActionInterface.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -250,7 +250,7 @@ ActionInterface::SendUIState(const bool trigger_draw)
     main_window->FullRedraw();
 }
 
-gcc_pure
+[[gnu::pure]]
 static unsigned
 GetPanelIndex(const UIState &ui_state)
 {
