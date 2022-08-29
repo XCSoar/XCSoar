98512f2dfbfd105cad289d0649f2c25b648592f0
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:07:56 +0200

    TriangleContest: use [[gnu::pure]]

--- src/Engine/Contest/Solvers/TriangleContest.cpp
+++ src/Engine/Contest/Solvers/TriangleContest.cpp
@@ -1,7 +1,7 @@
 /* Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -89,7 +89,7 @@ TriangleContest::ResetBranchAndBound() noexcept
   branch_and_bound.clear();
 }
 
-gcc_pure
+[[gnu::pure]]
 static double
 CalcLegDistance(const ContestTraceVector &solution,
                 const unsigned index) noexcept
@@ -483,7 +483,7 @@ TriangleContest::CalculateResult() const noexcept
   return result;
 }
 
-gcc_pure
+[[gnu::pure]]
 static bool
 IsInRange(const SearchPoint &a, const SearchPoint &b,
           unsigned half_max_range_sq, double max_distance) noexcept
@@ -508,12 +508,12 @@ TriangleContest::FindClosingPairs(unsigned old_size) noexcept
   };
 
   struct TracePointNodeAccessor {
-    gcc_pure
+    [[gnu::pure]]
     int GetX(const TracePointNode &node) const noexcept {
       return node.point->GetFlatLocation().x;
     }
 
-    gcc_pure
+    [[gnu::pure]]
     int GetY(const TracePointNode &node) const noexcept {
       return node.point->GetFlatLocation().y;
     }
