5e0157fb7cb92cc93e87219e8a3bdf68132be422
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:09:52 +0200

    VegaParametersWidget: use [[gnu::pure]]

--- src/Dialogs/Device/Vega/VegaParametersWidget.cpp
+++ src/Dialogs/Device/Vega/VegaParametersWidget.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -92,7 +92,7 @@ VegaParametersWidget::AddParameter(const StaticParameter &p)
   }
 }
 
-gcc_pure
+[[gnu::pure]]
 static bool
 SettingExists(VegaDevice &device, const char *name)
 {
