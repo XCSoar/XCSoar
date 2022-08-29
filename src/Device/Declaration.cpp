6134016a0662f98f5fafa9191be5b3596ecd08e4
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:12:28 +0200

    Declaration: use [[gnu::pure]]

--- src/Device/Declaration.cpp
+++ src/Device/Declaration.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -29,7 +29,7 @@ Copyright_License {
 #include "Engine/Task/Ordered/OrderedTask.hpp"
 #include "Engine/Task/Ordered/Points/OrderedTaskPoint.hpp"
 
-gcc_pure
+[[gnu::pure]]
 static Declaration::TurnPoint::Shape
 get_shape(const OrderedTaskPoint &tp)
 {
@@ -49,7 +49,7 @@ get_shape(const OrderedTaskPoint &tp)
   }
 }
 
-gcc_pure
+[[gnu::pure]]
 static unsigned
 get_radius(const OrderedTaskPoint &tp)
 {
