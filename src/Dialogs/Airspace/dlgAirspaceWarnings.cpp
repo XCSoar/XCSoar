551ef2d2912917d578720153807f160b84fb7a15
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 13:10:32 +0200

    AirspaceWarnings: use [[gnu::pure]]

--- src/Dialogs/Airspace/dlgAirspaceWarnings.cpp
+++ src/Dialogs/Airspace/dlgAirspaceWarnings.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -94,10 +94,10 @@ public:
   void UpdateList();
   void UpdateButtons();
 
-  gcc_pure
+  [[gnu::pure]]
   const AbstractAirspace *GetSelectedAirspace() const;
 
-  gcc_pure
+  [[gnu::pure]]
   bool HasWarning() const;
 
   void Ack();
