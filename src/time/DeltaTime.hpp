ec22189a06ef61e137975726d2e5a11032605a6c
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 12:59:51 +0200

    DeltaTime: use [[gnu::pure]]

--- src/time/DeltaTime.hpp
+++ src/time/DeltaTime.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_DELTA_TIME_HPP
-#define XCSOAR_DELTA_TIME_HPP
+#pragma once
 
 #include "Stamp.hpp"
 #include "util/Compiler.h"
@@ -43,7 +42,7 @@ public:
     last_time = TimeStamp::Undefined();
   }
 
-  gcc_pure
+  [[gnu::pure]]
   bool IsDefined() const {
     return last_time.IsDefined();
   }
@@ -64,5 +63,3 @@ public:
                        FloatDuration min_delta,
                        FloatDuration warp_tolerance) noexcept;
 };
-
-#endif
