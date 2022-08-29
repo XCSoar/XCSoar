4ec65c9a4fc1c2c74fd01739c00b05e5e09a9b09
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-29 12:59:06 +0200

    RaspRenderer: use [[gnu::pure]]

--- src/Weather/Rasp/RaspRenderer.cpp
+++ src/Weather/Rasp/RaspRenderer.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -30,7 +30,7 @@ Copyright_License {
 #include "Projection/WindowProjection.hpp"
 #include "util/StringAPI.hxx"
 
-gcc_pure
+[[gnu::pure]]
 static const RaspStyle &
 LookupWeatherTerrainStyle(const TCHAR *name)
 {
