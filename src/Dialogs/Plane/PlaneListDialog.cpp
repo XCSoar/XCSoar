bab978dff5cb340ad7901e358a0d57f17ff78f15
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 09:36:54 +0200

    PlaneListDialog: use [[maybe_unused]]

--- src/Dialogs/Plane/PlaneListDialog.cpp
+++ src/Dialogs/Plane/PlaneListDialog.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -108,7 +108,7 @@ protected:
                    unsigned idx) noexcept override;
 
   /* virtual methods from ListCursorHandler */
-  bool CanActivateItem(gcc_unused unsigned index) const noexcept override {
+  bool CanActivateItem([[maybe_unused]] unsigned index) const noexcept override {
     return true;
   }
 
