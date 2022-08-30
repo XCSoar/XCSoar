b1af647e1bdc3278b098b96b2aa58dbed12115ee
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 09:36:40 +0200

    TaskEditPanel: use [[maybe_unused]]

--- src/Dialogs/Task/Manager/TaskEditPanel.cpp
+++ src/Dialogs/Task/Manager/TaskEditPanel.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -294,7 +294,7 @@ TaskEditPanel::OnEditTurnpointClicked()
 }
 
 bool
-TaskEditPanel::CanActivateItem(gcc_unused unsigned index) const noexcept
+TaskEditPanel::CanActivateItem([[maybe_unused]] unsigned index) const noexcept
 {
   return true;
 }
@@ -342,7 +342,7 @@ TaskEditPanel::OnActivateItem(unsigned index) noexcept
 }
 
 void
-TaskEditPanel::OnCursorMoved(gcc_unused unsigned index) noexcept
+TaskEditPanel::OnCursorMoved([[maybe_unused]] unsigned index) noexcept
 {
   UpdateButtons();
 }
