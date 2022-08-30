9ed9c226883a21bb334c707eb84cdb88c7ca64b3
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 09:35:27 +0200

    BigThermalAssistantWindow: use [[maybe_unused]]

--- src/Gauge/BigThermalAssistantWindow.cpp
+++ src/Gauge/BigThermalAssistantWindow.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -60,7 +60,7 @@ BigThermalAssistantWindow::OnMouseUp(PixelPoint p)
 }
 
 bool
-BigThermalAssistantWindow::OnMouseMove(PixelPoint p, gcc_unused unsigned keys)
+BigThermalAssistantWindow::OnMouseMove(PixelPoint p, [[maybe_unused]] unsigned keys)
 {
   if (dragging)
     gestures.Update(p);
