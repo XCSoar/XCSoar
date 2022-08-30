162940930d0b4edae3b8f7f9ad2eaa5693df19cf
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 09:33:04 +0200

    XCSoar.cpp: use [[maybe_unused]]

--- src/XCSoar.cpp
+++ src/XCSoar.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -138,7 +138,7 @@ int main(int argc, char **argv)
 #else
 int WINAPI
 WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-        gcc_unused LPSTR lpCmdLine2,
+        [[maybe_unused]] LPSTR lpCmdLine2,
         int nCmdShow)
 #endif
 try {
