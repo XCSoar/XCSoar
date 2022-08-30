ed31bb510da4eff638e129979bfe67a5c9e58207
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 09:35:13 +0200

    BigTrafficWidget: use [[maybe_unused]]

--- src/Gauge/BigTrafficWidget.cpp
+++ src/Gauge/BigTrafficWidget.cpp
@@ -2,7 +2,7 @@
   Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -34,7 +34,6 @@
 #include "PageActions.hpp"
 #include "Look/Look.hpp"
 #include "Profile/Profile.hpp"
-#include "util/Compiler.h"
 #include "FLARM/Friends.hpp"
 #include "Look/FlarmTrafficLook.hpp"
 #include "Gauge/FlarmTrafficWindow.hpp"
@@ -781,7 +780,7 @@ TrafficWidget::Update() noexcept
 }
 
 bool
-FlarmTrafficControl::OnMouseMove(PixelPoint p, gcc_unused unsigned keys)
+FlarmTrafficControl::OnMouseMove(PixelPoint p, [[maybe_unused]] unsigned keys)
 {
   if (dragging)
     gestures.Update(p);
