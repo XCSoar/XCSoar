06f4d14bdc5506e1688036212b65034971722dc3
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 09:38:11 +0200

    LX/Settings: use [[maybe_unused]]

--- src/Device/Driver/LX/Settings.cpp
+++ src/Device/Driver/LX/Settings.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -147,7 +147,7 @@ LXDevice::GetNanoSetting(const char *name) const noexcept
 }
 
 bool
-LXDevice::PutBallast(gcc_unused double fraction, double overload,
+LXDevice::PutBallast([[maybe_unused]] double fraction, double overload,
                      OperationEnvironment &env)
 {
   if (!EnableNMEA(env))
