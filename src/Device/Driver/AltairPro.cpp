d70a9f9497ef1705537adfab15edd72e04b9f207
Fabian Berstecher <F.Berstecher@gmx.de>
2022-08-30 10:01:59 +0200

    Driver/AltairPro: use [[maybe_unused]]

--- src/Device/Driver/AltairPro.cpp
+++ src/Device/Driver/AltairPro.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -142,7 +142,7 @@ AltairProDevice::ParseNMEA(const char *String, NMEAInfo &info)
 
 bool
 AltairProDevice::Declare(const struct Declaration &declaration,
-                         gcc_unused const Waypoint *home,
+                         [[maybe_unused]] const Waypoint *home,
                          OperationEnvironment &env)
 {
   port.StopRxThread();
