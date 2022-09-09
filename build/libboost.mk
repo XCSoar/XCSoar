03d850d18422377494b9ffae965b8874707328ba
Max Kellermann <max.kellermann@gmail.com>
2022-09-09 10:54:02 +0200

    build/libboost.mk: update Boost to 1.80.0

--- build/libboost.mk
+++ build/libboost.mk
@@ -1,6 +1,6 @@
-BOOST_URL = https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.bz2
-BOOST_ALTERNATIVE_URL = https://sourceforge.net/projects/boost/files/boost/1.79.0/boost_1_79_0.tar.bz2/download
-BOOST_MD5 = 475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39
+BOOST_URL = https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2
+BOOST_ALTERNATIVE_URL = https://sourceforge.net/projects/boost/files/boost/1.80.0/boost_1_80_0.tar.bz2/download
+BOOST_MD5 = 1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0
 
 BOOST_TARBALL_NAME = $(notdir $(BOOST_URL))
 BOOST_TARBALL = $(DOWNLOAD_DIR)/$(BOOST_TARBALL_NAME)
