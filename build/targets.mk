d1df1ae1c00069c3115d91cc54d62b802db4f4be
Max Kellermann <max.kellermann@gmail.com>
2022-09-16 10:30:51 +0200

    build/targets.mk: use Android NDK r25b

--- build/targets.mk
+++ build/targets.mk
@@ -316,7 +316,7 @@ ifeq ($(TARGET),UNIX)
 endif
 
 ifeq ($(TARGET),ANDROID)
-  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r25
+  ANDROID_NDK ?= $(HOME)/opt/android-ndk-r25b
 
   ANDROID_SDK_PLATFORM = android-30
   ANDROID_NDK_API = 21
