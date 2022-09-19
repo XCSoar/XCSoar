beeb565f2b4087cedd9beed06bda26f68ab67c34
Eckhard Völlm <e.voellm@mail.de>
2022-09-19 19:09:41 +0200

    Device/XCVario: Add protocol for absolute ballast exchange
    The current way sent ballast as fraction of max ballast to the device plus
    another fraction (ballast overweight) from the device leads to issue #824.
    The issue can be fixed sending an absolute value in litres to the device,
    addressed with the new protocol version 2. In order to remain backward
    compatible, XCSoar and XCVario will negotiate furthermore the protocol version.
    This is initiated by XCSoar what will send its version as soon the link is
    up and $PXCV messages are getting in. The additional $PXCV is needed
    as an application layer check on top of the link state, to ensure that
    the device is talking XCVario protocol and ready to respond.
    The device has to reply with its own protocol version and upon reception
    XCSoar increases its version to the minimum protocol both can do.
    If nothing is received within timeout, legacy protocol version 1 is selected.
    
    Closes #824

--- src/Device/Driver/XCVario.cpp
+++ src/Device/Driver/XCVario.cpp
@@ -31,33 +31,54 @@ Copyright_License {
 #include "NMEA/InputLine.hpp"
 #include "util/Clamp.hpp"
 #include "Atmosphere/Pressure.hpp"
+#include "Operation/Operation.hpp"
 #include <math.h>
+
 
 class XVCDevice : public AbstractDevice {
   Port &port;
+  enum {
+    XCV_VERSION_UNKNOWN,  // as long device has not yet answered and no timeout
+    XCV_VERSION_1,        // exchange legacy format with fractional ballast in $PXCV plus CAI302 !g command
+    XCV_VERSION_2         // exchange absolute ballast in liters by !xc[s|v] messages
+  };
 
-public:
-  XVCDevice(Port &_port):port(_port) {}
-
+  public:
+  XVCDevice(Port &_port):port(_port) {};
   // virtual methods from class Device
   bool ParseNMEA(const char *line, struct NMEAInfo &info) override;
-
   bool PutMacCready(double mc, OperationEnvironment &env) override;
   bool PutBugs(double bugs, OperationEnvironment &env) override;
   bool PutBallast(double fraction, double overload, OperationEnvironment &env) override;
   bool PutQNH(const AtmosphericPressure &pres, OperationEnvironment &env) override;
+  bool EnableNMEA(OperationEnvironment &env) override;
+  void OnSysTicker() override;
+  void LinkTimeout() override;
+  bool SendNMEAItem(const char *which, unsigned int value);
+
+private:
+  unsigned int protocol_negotiation_timer{5};         // timer to supervise $PXCV protocol talking peer on application level
+  unsigned int protocol_version{XCV_VERSION_UNKNOWN}; // the protocol version negotiated between XCSoar and XCVario
+  bool nmea_enable{false};                            // EnableNMEA() even has been received
+  bool xcvario_protocol_up{false};                    // application level XCVario protocol $PXCV msg received
+
+  bool PXCV(NMEAInputLine &line, NMEAInfo &info);
+  bool XCV(NMEAInputLine &line, NMEAInfo &info);
+  bool PutProtocolVersion(int version);
 };
 
 /*
   For a complete documentation of the protocol refer to https://xcvario.de/handbuch and
   search for NMEA Protokoll.
-  
-  Sentence has following format:
+
+  Protocol XCVario
+
+  Vario Sensor Data Sentence has following format:
   $PXCV,
   BBB.B  = Vario, -30 to +30 m/s, negative sign for sink,
   C.C    = MacCready 0 to 10 m/s
   EE     = bugs degradation, 0 = clean to 30 %,
-  F.FF   = Ballast 1.00 to 1.60,
+  F.FF  = Ballast 1.00 to 1.60 ( for protocol version 1, empty in protocol version 2)
   G      = 0 in climb, 1 in cruise,
   HH.H   = Outside airtemp in degrees celcius ( may have leading negative sign ) e.g. 24.4,
   QQQQ.Q = QNH in hectoPascal e.g. 1013.2,
@@ -67,11 +88,17 @@ public:
   X.XX   = acceleration in X-Axis (multiples of G),
   Y.YY   = acceleration in Y-Axis,
   Z.ZZ   = acceleration in Z-Axis,
-  *,
-  CHK    = standard NMEA checksum, CR,LF
+  *
+  CHK    = standard NMEA checksum
+  CR,LF
+
+  Exchange of Ballast and protocol version:
+  !xc[s|v],bal-water,NNN*CHK   // exchange ballast NNN (integer) of glider in litres [s|v]: sender, s=XCSoar, v=XCVario
+  !xc[s|v],version,N*CHK       // xcv protocol version; N=1 for legacy, N=2 for new ballast protocol
+
  */
-static bool
-PXCV(NMEAInputLine &line, NMEAInfo &info)
+bool
+XVCDevice::PXCV(NMEAInputLine &line, NMEAInfo &info)
 {
   // Format as defined above.
   double value;
@@ -94,12 +121,16 @@ PXCV(NMEAInputLine &line, NMEAInfo &info)
     info.settings.ProvideBugs(1 - Clamp(value, 0., 30.) / 100.,
                               info.clock);
 
-  // Ballast overload value in %
-  double ballast_overload;
-  if (line.ReadChecked(ballast_overload))
-    info.settings.ProvideBallastOverload(ballast_overload, info.clock);
+  // legacy reading of fractional water ballast in protocol version 1
+  if (protocol_version == XCV_VERSION_1) {
+    double ballast_overload;
+    if (line.ReadChecked(ballast_overload))   // Parse ballast overload value
+      info.settings.ProvideBallastOverload(ballast_overload, info.clock);
+  } else {
+    line.Read(-1);    // starting protocol version 2 we ignore this field
+  }
 
-  // inclimb/incruise 1=cruise,0=climb, OAT
+  // inclimb/incruise 1=cruise,0=climb
   switch (line.Read(-1)) {
   case 0:
     info.switch_state.flight_mode = SwitchState::FlightMode::CRUISE;
@@ -110,7 +141,7 @@ PXCV(NMEAInputLine &line, NMEAInfo &info)
     break;
   }
 
-  // Outside air temperature
+  // Outside air temperature (OAT)
   info.temperature_available = line.ReadChecked(value);
   if (info.temperature_available)
     info.temperature = Temperature::FromCelsius(value);
@@ -138,9 +169,43 @@ PXCV(NMEAInputLine &line, NMEAInfo &info)
     info.attitude.pitch_angle = Angle::Degrees(value);
   }
   // Space diagonal acceleration in X,Y,Z axes measure
-  if ( line.ReadChecked(x) && line.ReadChecked(y) && line.ReadChecked(z) )
+  if (line.ReadChecked(x) && line.ReadChecked(y) && line.ReadChecked(z))
     info.acceleration.ProvideGLoad(SpaceDiagonal(x, y, z));
 
+  return true;
+}
+
+bool
+XVCDevice::XCV(NMEAInputLine &line, NMEAInfo &info)
+{
+  char topic[16];
+  line.Read(topic, sizeof(topic));
+  if (StringIsEqual(topic, "bal-water")) {
+    double value;
+    if (line.ReadChecked(value)) {
+      info.settings.ProvideBallastLitres(value, info.clock);
+    }
+  }
+  else if (StringIsEqual(topic, "version")) {
+    unsigned int value;
+    if (line.ReadChecked(value)) {
+      protocol_version = std::min(value, (unsigned int)XCV_VERSION_2);  // switch protocol version to the minimum version both can do
+    }
+  }
+  return true;
+}
+
+void XVCDevice::LinkTimeout()
+{
+  protocol_version = XCV_VERSION_UNKNOWN;   // on link timeout protocol negotiation is restarted
+  protocol_negotiation_timer = 5;                           // link timeout ticks = 5 seconds
+  xcvario_protocol_up = false;
+  nmea_enable = false;
+}
+
+bool XVCDevice::EnableNMEA([[maybe_unused]] OperationEnvironment &env)
+{
+  nmea_enable = true;
   return true;
 }
 
@@ -149,31 +214,52 @@ XVCDevice::ParseNMEA(const char *String, NMEAInfo &info)
 {
   if (!VerifyNMEAChecksum(String))
     return false;
-
   NMEAInputLine line(String);
   char type[16];
-  line.Read(type, 16);
-
-  if (StringIsEqual(type, "$PXCV"))
-    return PXCV(line, info);
-  else
-    return false;
+  line.Read(type, sizeof(type));
+  if (StringIsEqual(type, "$PXCV")) {                // cyclic data from device useful for channel supervision
+    xcvario_protocol_up = true;
+    if (protocol_version != XCV_VERSION_UNKNOWN) {   // only parse NMEA once protocol version is set
+      return PXCV(line, info);
+    }
+    return true;
+  }
+  else if (StringIsEqual(type, "!xcv")) {
+    return XCV(line, info);
+  }
+  return false;
 }
 
 // For documentation refer to chapter 10.1.3 Device Driver/XCVario in mulilingual handbook: https://xcvario.de/handbuch
+// Schedule sending of new protocol version 2; peer shall answer with the same message with its version
+// Upon reception of XCV's version, the protocol will be switched, atfer timeout we assume legacy version 1
+void
+XVCDevice::OnSysTicker()
+{
+  // repeat sending protocol version every second until timeout ( 5 seconds )
+  if (protocol_negotiation_timer && nmea_enable && protocol_version == XCV_VERSION_UNKNOWN && xcvario_protocol_up) {
+    protocol_negotiation_timer--;
+    if (protocol_version == XCV_VERSION_UNKNOWN) { // send our version until negotiation is finished
+      PutProtocolVersion(XCV_VERSION_2);
+    }
+  }
+  if (protocol_negotiation_timer == 0 && protocol_version == XCV_VERSION_UNKNOWN) {  // timeout waiting for device sending version: legacy version 1
+    // after timer expiry, we work in legacy version 1 mode
+    protocol_version = XCV_VERSION_1;
+  }
+}
 
+// For documentation refer to chapter 10.1.3 Device Driver/XCVario in multilingual handbook: https://xcvario.de/handbuch
 bool 
 XVCDevice::PutQNH(const AtmosphericPressure &pres, OperationEnvironment &env)
 {
-  /* the XCVario understands "!g,q<NNNN>" command for QNH updates with recent builds */
+  /* the XCVario understands "!g,q<NNNN>" command for QNH updates */
   char buffer[32];
   unsigned qnh = uround(pres.GetHectoPascal());
   int msg_len = sprintf(buffer,"!g,q%u\r", std::min(qnh,(unsigned)2000));
-  port.FullWrite(buffer, msg_len, env, std::chrono::seconds(2) );
+  port.FullWrite(buffer, msg_len, env, std::chrono::seconds(2));
   return true;
 }
-
-
 
 bool
 XVCDevice::PutMacCready(double mac_cready, OperationEnvironment &env)
@@ -201,9 +287,30 @@ XVCDevice::PutBallast(double fraction, [[maybe_unused]] double overload,
      float precision */
    char buffer[32];
    double ballast = fraction * 10.;
-   int msg_len = sprintf(buffer,"!g,b%.3f\r", ballast );
-   port.FullWrite(buffer, msg_len, env, std::chrono::seconds(2) );
+   int msg_len = sprintf(buffer,"!g,b%.3f\r", ballast);
+   port.FullWrite(buffer, msg_len, env, std::chrono::seconds(2));
   return true;
+}
+
+bool
+XVCDevice::SendNMEAItem(const char *which, unsigned int value)
+{
+  /* the new xcs protocol for various int items e.g. with absolute weights or protocol version including the NMEA checksum
+   * We are using Port::Write() as do no need reliable and maybe blocking transmission here, there is retry on application layer
+   * */
+  char line[32];
+  sprintf(line,"xcs,%s,%u", which, value);
+  port.Write('!');
+  port.Write(line);
+  char checksum[16];
+  sprintf(checksum, "*%02X\r\n", NMEAChecksum(line));
+  port.Write(checksum);
+  return true;
+}
+
+bool XVCDevice::PutProtocolVersion(int version)
+{
+  return SendNMEAItem("version", version );
 }
 
 static Device *
