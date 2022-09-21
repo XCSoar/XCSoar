976784c111cb008f8d1a5eb92c38c09e770dc3e8
Fabian Berstecher <F.Berstecher@gmx.de>
2022-09-21 14:58:05 +0200

    use #pragma once

--- src/Engine/Task/TaskManager.hpp
+++ src/Engine/Task/TaskManager.hpp
@@ -1,7 +1,7 @@
 /* Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -20,8 +20,7 @@
 }
 */
 
-#ifndef TASKMANAGER_H
-#define TASKMANAGER_H
+#pragma once
 
 #include "util/NonCopyable.hpp"
 #include "Stats/TaskStats.hpp"
@@ -462,5 +461,3 @@ private:
   void UpdateCommonStatsWaypoints(const AircraftState &state);
   void UpdateCommonStatsPolar(const AircraftState &state);
 };
-
-#endif
--- src/NMEA/Derived.hpp
+++ src/NMEA/Derived.hpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -21,8 +21,7 @@ Copyright_License {
 }
 */
 
-#ifndef XCSOAR_NMEA_DERIVED_H
-#define XCSOAR_NMEA_DERIVED_H
+#pragma once
 
 #include "Geo/GeoPoint.hpp"
 #include "Geo/SpeedVector.hpp"
@@ -290,6 +289,3 @@ struct DerivedInfo:
 };
 
 static_assert(std::is_trivial_v<DerivedInfo>, "type is not trivial");
-
-#endif
-
