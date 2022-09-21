abe11f175823345bef2a63fb9fff97b82eb81ae9
Fabian Berstecher <F.Berstecher@gmx.de>
2022-09-21 14:59:48 +0200

    system/PathName: use [[gnu::pure]]

--- src/system/PathName.cpp
+++ src/system/PathName.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2021 The XCSoar Project
+  Copyright (C) 2000-2022 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -24,7 +24,7 @@ Copyright_License {
 #include "system/PathName.hpp"
 #include "util/StringAPI.hxx"
 
-gcc_pure
+[[gnu::pure]]
 static const TCHAR *
 LastSeparator(const TCHAR *path)
 {
@@ -37,7 +37,7 @@ LastSeparator(const TCHAR *path)
   return p;
 }
 
-gcc_pure
+[[gnu::pure]]
 static TCHAR *
 LastSeparator(TCHAR *path)
 {
