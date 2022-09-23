3b53c80caac980fa01bf6e6aa906e5c21b0223a9
Max Kellermann <max.kellermann@gmail.com>
2022-09-23 17:34:43 +0200

    IGC/String: add `noexcept`

--- src/IGC/IGCString.hpp
+++ src/IGC/IGCString.hpp
@@ -36,9 +36,8 @@ Copyright_License {
  *
  * @see IGC specification, section A6
  */
-constexpr
-static inline bool
-IsReservedIGCChar(char ch)
+static constexpr bool
+IsReservedIGCChar(char ch) noexcept
 {
   return ch == '$' || ch == '*' || ch == '!' || ch == '\\' ||
     ch == '^' || ch == '~';
@@ -49,17 +48,15 @@ IsReservedIGCChar(char ch)
  *
  * @see IGC specification, section A6
  */
-constexpr
-static inline bool
-IsValidIGCChar(char ch)
+static constexpr bool
+IsValidIGCChar(char ch) noexcept
 {
   return ch >= 0x20 && ch <= 0x7e && !IsReservedIGCChar(ch);
 }
 
 #ifdef _UNICODE
-constexpr
-static inline bool
-IsValidIGCChar(TCHAR ch)
+static constexpr bool
+IsValidIGCChar(TCHAR ch) noexcept
 {
   return ch >= 0x20 && ch <= 0x7e && !IsReservedIGCChar(char(ch));
 }
@@ -71,9 +68,9 @@ IsValidIGCChar(TCHAR ch)
  * The destination buffer will not be null-terminated.
  */
 char *
-CopyIGCString(char *dest, char *dest_limit, const char *src);
+CopyIGCString(char *dest, char *dest_limit, const char *src) noexcept;
 
 #ifdef _UNICODE
 char *
-CopyIGCString(char *dest, char *dest_limit, const TCHAR *src);
+CopyIGCString(char *dest, char *dest_limit, const TCHAR *src) noexcept;
 #endif
