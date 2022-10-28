XCSoar/XCSoar

commit b77ba983422c12a2944a8c90873b1c875a53a200

Route/Terrain: move the ReachFan instances out of the class
Convert to return value, stored in ProtectedRoutePlanner and protected
with a dedicated mutex, to reduce lock contention while the route
planner is running.

src/Task/ProtectedRoutePlanner.cpp
@@ -22,6 +22,7 @@ Copyright_License {
 */
 
 #include "ProtectedRoutePlanner.hpp"
+#include "Engine/Route/ReachResult.hpp"
 
 void
 ProtectedRoutePlanner::SetTerrain(const RasterTerrain *terrain) noexcept
@@ -68,13 +69,51 @@ ProtectedRoutePlanner::SolveReach(const AGeoPoint &origin,
                                   const int h_ceiling,
                                   const bool do_solve) noexcept
 {
-  ExclusiveLease lease(*this);
-  lease->SolveReach(origin, config, h_ceiling, do_solve);
+  /* these local variables help avoid locking both mutexes at the same
+     time */
+  ReachFan rt, rw;
+
+  {
+    ExclusiveLease lease(*this);
+    rt = lease->SolveReach(origin, config, h_ceiling, do_solve, false);
+    rw = lease->SolveReach(origin, config, h_ceiling, do_solve, true);
+    rpolars_reach = lease->GetReachPolar();
+  }
+
+  /* we lock this mutex not during the expensive reach calculation,
+     but only for moving the result to the mutex-protected fields */
+  const std::scoped_lock lock{reach_mutex};
+  reach_terrain = std::move(rt);
+  reach_working = std::move(rw);
 }
 
 const FlatProjection
 ProtectedRoutePlanner::GetTerrainReachProjection() const noexcept
 {
-  Lease lease(*this);
-  return lease->GetTerrainReachProjection();
+  const std::scoped_lock lock{reach_mutex};
+  return reach_terrain.GetProjection();
+}
+
+std::optional<ReachResult>
+ProtectedRoutePlanner::FindPositiveArrival(const AGeoPoint &dest) const noexcept
+{
+  const std::scoped_lock lock{reach_mutex};
+  return reach_terrain.FindPositiveArrival(dest, rpolars_reach);
+}
+
+void
+ProtectedRoutePlanner::AcceptInRange(const GeoBounds &bounds,
+                                     FlatTriangleFanVisitor &visitor,
+                                     bool working) const noexcept
+{
+  const std::scoped_lock lock{reach_mutex};
+  const auto &reach = working ? reach_terrain : reach_working;
+  reach.AcceptInRange(bounds, visitor);
+}
+
+int
+ProtectedRoutePlanner::GetTerrainBase() const noexcept
+{
+  const std::scoped_lock lock{reach_mutex};
+  return reach_terrain.GetTerrainBase();
 }

src/Computer/RouteComputer.cpp
@@ -148,7 +148,7 @@ RouteComputer::Reach(const MoreData &basic, DerivedInfo &calculated,
     protected_route_planner.SolveReach(start, config, h_ceiling, do_solve);
 
     if (do_solve) {
-      calculated.terrain_base = route_planner.GetTerrainBase();
+      calculated.terrain_base = protected_route_planner.GetTerrainBase();
       calculated.terrain_base_valid = true;
     }
   }

src/Computer/RouteComputer.hpp
@@ -54,14 +54,6 @@ public:
   RouteComputer(const Airspaces &airspace_database,
                 const ProtectedAirspaceWarningManager *warnings);
 
-  /**
-   * Returns a reference to the unprotected route planner object,
-   * which must not be used outside of the calculation thread.
-   */
-  const RoutePlannerGlue &GetRoutePlanner() const {
-    return route_planner;
-  }
-
   const ProtectedRoutePlanner &GetProtectedRoutePlanner() const {
     return protected_route_planner;
   }

src/Computer/TaskComputer.cpp
@@ -46,7 +46,7 @@ TaskComputer::TaskComputer(ProtectedTaskManager &_task,
    route(airspace_database, warnings),
    contest(trace.GetFull(), trace.GetContest(), trace.GetSprint())
 {
-  task.SetRoutePlanner(&route.GetRoutePlanner());
+  task.SetRoutePlanner(&route.GetProtectedRoutePlanner());
 }
 
 void

src/Engine/Route/ReachFan.hpp
@@ -54,10 +54,22 @@ public:
   bool Solve(const AGeoPoint origin, const RoutePolars &rpolars,
              const RasterMap *terrain, const bool do_solve = true) noexcept;
 
+  /**
+   * Find arrival height at destination.
+   *
+   * Requires solve_reach() to have been called for positive results.
+   *
+   * @param dest Destination location
+   * @param arrival_height_reach height at arrival (terrain reach) or -1 if out of reach
+   * @param arrival_height_direct height at arrival (pure glide reach) or -1 if out of reach
+   *
+   * @return true if check was successful
+   */
   [[gnu::pure]]
   std::optional<ReachResult> FindPositiveArrival(const AGeoPoint dest,
                                                  const RoutePolars &rpolars) const noexcept;
 
+  /** Visit reach (working or terrain reach) */
   void AcceptInRange(const GeoBounds &bounds,
                      FlatTriangleFanVisitor &visitor) const noexcept;
 

src/Engine/Route/TerrainRoute.cpp
@@ -22,21 +22,8 @@
 
 #include "TerrainRoute.hpp"
 #include "ReachResult.hpp"
+#include "ReachFan.hpp"
 #include "Terrain/RasterMap.hpp"
-
-void
-TerrainRoute::ClearReach() noexcept
-{
-  reach_terrain.Reset();
-  reach_working.Reset();
-}
-
-void
-TerrainRoute::Reset() noexcept
-{
-  RoutePlanner::Reset();
-  ClearReach();
-}
 
 void
 TerrainRoute::UpdatePolar(const GlideSettings &settings,
@@ -64,32 +51,19 @@ TerrainRoute::UpdatePolar(const GlideSettings &settings,
                                    height_min_working);
 }
 
-bool
-TerrainRoute::SolveReachTerrain(const AGeoPoint &origin,
-                                const RoutePlannerConfig &config,
-                                const int h_ceiling,
-                                const bool do_solve) noexcept
+ReachFan
+TerrainRoute::SolveReach(const AGeoPoint &origin,
+                         const RoutePlannerConfig &config,
+                         const int h_ceiling,
+                         const bool do_solve,
+                         const bool working) noexcept
 {
-  rpolars_reach.SetConfig(config, origin.altitude, h_ceiling);
+  auto &rpolars = working ? rpolars_reach_working : rpolars_reach;
+  rpolars.SetConfig(config, origin.altitude, h_ceiling);
 
-  return reach_terrain.Solve(origin, rpolars_reach, terrain, do_solve);
-}
-
-bool
-TerrainRoute::SolveReachWorking(const AGeoPoint &origin,
-                                const RoutePlannerConfig &config,
-                                const int h_ceiling,
-                                const bool do_solve) noexcept
-{
-  rpolars_reach_working.SetConfig(config, origin.altitude, h_ceiling);
-
-  return reach_working.Solve(origin, rpolars_reach_working, terrain, do_solve);
-}
-
-std::optional<ReachResult>
-TerrainRoute::FindPositiveArrival(const AGeoPoint &dest) const noexcept
-{
-  return reach_terrain.FindPositiveArrival(dest, rpolars_reach);
+  ReachFan reach;
+  reach.Solve(origin, rpolars, terrain, do_solve);
+  return reach;
 }
 
 /*
@@ -105,17 +79,6 @@ TerrainRoute::FindPositiveArrival(const AGeoPoint &dest) const noexcept
     acknowledged in the airspace warning manager.
   - more documentation
  */
-
-void
-TerrainRoute::AcceptInRange(const GeoBounds &bounds,
-                            FlatTriangleFanVisitor &visitor,
-                            bool working) const noexcept
-{
-  if (working)
-    reach_working.AcceptInRange(bounds, visitor);
-  else
-    reach_terrain.AcceptInRange(bounds, visitor);
-}
 
 GeoPoint
 TerrainRoute::Intersection(const AGeoPoint &origin,

src/Engine/Route/TerrainRoute.hpp
@@ -23,7 +23,8 @@
 #pragma once
 
 #include "RoutePlanner.hpp"
-#include "ReachFan.hpp"
+
+class ReachFan;
 
 /**
  * Specialization of #RoutePlanner which implements terrain avoidance.
@@ -43,9 +44,6 @@ class TerrainRoute: public RoutePlanner
   /** Aircraft performance model for reach to working floor */
   RoutePolars rpolars_reach_working;
 
-  ReachFan reach_terrain;
-  ReachFan reach_working;
-
   mutable RoutePoint m_inx_terrain;
 
 public:
@@ -59,24 +57,9 @@ public:
     terrain = _terrain;
   }
 
-  bool IsTerrainReachEmpty() const noexcept {
-    return reach_terrain.IsEmpty();
+  const auto &GetReachPolar() const noexcept {
+    return rpolars_reach;
   }
-
-  const FlatProjection &GetTerrainReachProjection() const noexcept {
-    return reach_terrain.GetProjection();
-  }
-
-  int GetTerrainBase() const noexcept {
-    return reach_terrain.GetTerrainBase();
-  }
-
-  /**
-   * Delete all reach fans.
-   */
-  void ClearReach() noexcept;
-
-  void Reset() noexcept override;
 
   void UpdatePolar(const GlideSettings &settings,
                    const RoutePlannerConfig &config,
@@ -86,45 +69,16 @@ public:
                    int height_min_working=0) noexcept;
 
   /**
-   * Solve reach footprint to terrain
+   * Solve reach footprint to terrain or working height.
    *
    * @param origin The start of the search (current aircraft location)
    * @param do_solve actually solve or just perform minimal calculations
-   *
-   * @return True if reach was scanned
-   */
-  bool SolveReachTerrain(const AGeoPoint &origin, const RoutePlannerConfig &config,
-                         int h_ceiling, bool do_solve=true) noexcept;
-
-  /**
-   * Solve reach footprint to working height
-   *
-   * @param origin The start of the search (current aircraft location)
-   * @param do_solve actually solve or just perform minimal calculations
-   *
-   * @return True if reach was scanned
-   */
-  bool SolveReachWorking(const AGeoPoint &origin, const RoutePlannerConfig &config,
-                         int h_ceiling, bool do_solve=true) noexcept;
-
-  /**
-   * Find arrival height at destination.
-   *
-   * Requires solve_reach() to have been called for positive results.
-   *
-   * @param dest Destination location
-   * @param arrival_height_reach height at arrival (terrain reach) or -1 if out of reach
-   * @param arrival_height_direct height at arrival (pure glide reach) or -1 if out of reach
-   *
-   * @return true if check was successful
    */
   [[gnu::pure]]
-  std::optional<ReachResult> FindPositiveArrival(const AGeoPoint &dest) const noexcept;
-
-  /** Visit reach (working or terrain reach) */
-  void AcceptInRange(const GeoBounds &bounds,
-                     FlatTriangleFanVisitor &visitor,
-                     bool working) const noexcept;
+  ReachFan SolveReach(const AGeoPoint &origin,
+                      const RoutePlannerConfig &config,
+                      int h_ceiling, bool do_solve,
+                      bool working) noexcept;
 
   /**
    * Determine if intersection with terrain occurs in forwards direction from

src/MapWindow/Items/Builder.cpp
@@ -79,18 +79,8 @@ MapItemListBuilder::AddArrivalAltitudes(
   const AGeoPoint destination(location, target_elevation);
 
   // Calculate arrival altitudes
-  ReachResult reach;
-
-  {
-    ProtectedRoutePlanner::Lease leased_route_planner(route_planner);
-    auto _reach = leased_route_planner->FindPositiveArrival(destination);
-    if (!_reach)
-      return;
-
-    reach = *_reach;
-  }
-
-  list.append(new ArrivalAltitudeMapItem(elevation, reach, safety_height));
+  if (auto reach = route_planner.FindPositiveArrival(destination))
+    list.append(new ArrivalAltitudeMapItem(elevation, *reach, safety_height));
 }
 
 void

commit 486f2e1a77ca9a894801f9928aace68d817384cc

Task/ProtectedRoutePlanner: fix inverted check
This bug was introduced by commit
b77ba983422c12a2944a8c90873b1c875a53a200 and caused the wrong reach
fan to be rendered.

Closes https://github.com/XCSoar/XCSoar/issues/1068

src/Task/ProtectedRoutePlanner.cpp
@@ -99,7 +99,7 @@ ProtectedRoutePlanner::AcceptInRange(const GeoBounds &bounds,
                                      bool working) const noexcept
 {
   const std::scoped_lock lock{reach_mutex};
-  const auto &reach = working ? reach_terrain : reach_working;
+  const auto &reach = working ? reach_working : reach_terrain;
   reach.AcceptInRange(bounds, visitor);
 }
 