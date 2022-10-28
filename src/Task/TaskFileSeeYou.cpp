XCSoar/XCSoar

commit 12627e8ee9a4fe0404dd1de71679f1bef54ea0e4

Task/TaskFileSeeYou: use StringAfterPrefix()

src/Task/TaskFileSeeYou.cpp
@@ -150,72 +150,60 @@ ParseOptions(SeeYouTaskInformation *task_info, const TCHAR *params[],
 {
   // Iterate through available task options
   for (unsigned i = 1; i < n_params; i++) {
-    if (StringIsEqual(params[i], _T("WpDis"), 5)) {
+    if (auto wp_dis = StringAfterPrefix(params[i], _T("WpDis="))) {
       // Parse WpDis option
-      if (_tcslen(params[i]) > 6 &&
-          StringIsEqual(params[i] + 6, _T("False"), 5))
+      if (StringIsEqual(wp_dis, _T("False")))
         task_info->wp_dis = false;
-    } else if (StringIsEqual(params[i], _T("TaskTime"), 8)) {
+    } else if (auto task_time = StringAfterPrefix(params[i], _T("TaskTime="))) {
       // Parse TaskTime option
-      if (_tcslen(params[i]) > 9)
-        task_info->task_time = ParseTaskTime(params[i] + 9);
+      if (!StringIsEmpty(task_time))
+        task_info->task_time = ParseTaskTime(task_time);
     }
   }
 }
 
 /**
- * Parses one ObsZone line from the See You task file
- * @param turnpoint_infos Updated with the OZ info
  * @param params Input array of parameters preparsed from See You task file
  * @param n_params Number parameters in the line
- * @return OZ index from CU (0 to n-1) or -1 if no OZ found
  */
-static int
-ParseOZs(SeeYouTurnpointInformation turnpoint_infos[], const TCHAR *params[],
+static void
+ParseOZs(SeeYouTurnpointInformation &tp_info, const TCHAR *params[],
          unsigned n_params)
 {
-  // Read OZ index
-  TCHAR* end;
-  const std::size_t oz_index = _tcstol(params[0] + 8, &end, 10);
-  if (params[0] + 8 == end || oz_index >= CUP_MAX_TPS)
-    return -1;
-
-  turnpoint_infos[oz_index].valid = true;
+  tp_info.valid = true;
   // Iterate through available OZ options
   for (unsigned i = 1; i < n_params; i++) {
     const TCHAR *pair = params[i];
-    SeeYouTurnpointInformation &tp_info = turnpoint_infos[oz_index];
 
-    if (StringIsEqual(pair, _T("Style"), 5)) {
-      if (_tcslen(pair) > 6)
-        tp_info.style = ParseStyle(pair + 6);
-    } else if (StringIsEqual(pair, _T("R1="), 3)) {
-      if (_tcslen(pair) > 3)
-        tp_info.radius1 = ParseRadius(pair + 3);
-    } else if (StringIsEqual(pair, _T("A1="), 3)) {
-      if (_tcslen(pair) > 3)
-        tp_info.angle1 = ParseAngle(pair + 3);
-    } else if (StringIsEqual(pair, _T("R2="), 3)) {
-      if (_tcslen(pair) > 3)
-        tp_info.radius2 = ParseRadius(pair + 3);
-    } else if (StringIsEqual(pair, _T("A2="), 3)) {
-      if (_tcslen(pair) > 3)
-        tp_info.angle2 = ParseAngle(pair + 3);
-    } else if (StringIsEqual(pair, _T("A12="), 4)) {
-      if (_tcslen(pair) > 3)
-        tp_info.angle12 = ParseAngle(pair + 4);
-    } else if (StringIsEqual(pair, _T("MaxAlt="), 7)) {
-      if (_tcslen(pair) > 7)
-        tp_info.max_altitude = ParseMaxAlt(pair + 7);
-      } else if (StringIsEqual(pair, _T("Line"), 4)) {
-      if (_tcslen(pair) > 5 && pair[5] == _T('1'))
+    if (auto style = StringAfterPrefix(pair, _T("Style="))) {
+      if (!StringIsEmpty(style))
+        tp_info.style = ParseStyle(style);
+    } else if (auto r1 = StringAfterPrefix(pair, _T("R1="))) {
+      if (!StringIsEmpty(r1))
+        tp_info.radius1 = ParseRadius(r1);
+    } else if (auto a1 = StringAfterPrefix(pair, _T("A1="))) {
+      if (!StringIsEmpty(a1))
+        tp_info.angle1 = ParseAngle(a1);
+    } else if (auto r2 = StringAfterPrefix(pair, _T("R2="))) {
+      if (!StringIsEmpty(r2))
+        tp_info.radius2 = ParseRadius(r2);
+    } else if (auto a2 = StringAfterPrefix(pair, _T("A2="))) {
+      if (!StringIsEmpty(a2))
+        tp_info.angle2 = ParseAngle(a2);
+    } else if (auto a12 = StringAfterPrefix(pair, _T("A12="))) {
+      if (!StringIsEmpty(a12))
+        tp_info.angle12 = ParseAngle(a12);
+    } else if (auto max_altitude = StringAfterPrefix(pair, _T("MaxAlt="))) {
+      if (!StringIsEmpty(max_altitude))
+        tp_info.max_altitude = ParseMaxAlt(max_altitude);
+    } else if (auto line = StringAfterPrefix(pair, _T("Line="))) {
+      if (*line == _T('1'))
         tp_info.is_line = true;
-    } else if (StringIsEqual(pair, _T("Reduce"), 6)) {
-      if (_tcslen(pair) > 7 && pair[7] == _T('1'))
+    } else if (auto reduce = StringAfterPrefix(pair, _T("Reduce="))) {
+      if (*reduce == _T('1'))
         tp_info.reduce = true;
     }
   }
-  return oz_index;
 }
 
 /**
@@ -232,7 +220,6 @@ ParseCUTaskDetails(TLineReader &reader, SeeYouTaskInformation *task_info,
   TCHAR params_buffer[1024];
   const TCHAR *params[20];
   TCHAR *line;
-  int TPIndex = 0;
   const unsigned int max_params = ARRAY_SIZE(params);
   while ((line = reader.ReadLine()) != nullptr &&
          line[0] != _T('\"') && line[0] != _T(',')) {
@@ -243,12 +230,17 @@ ParseCUTaskDetails(TLineReader &reader, SeeYouTaskInformation *task_info,
       // Options line found
       ParseOptions(task_info, params, n_params);
 
-    } else if (StringIsEqual(params[0], _T("ObsZone"), 7)) {
+    } else if (auto obs_zone = StringAfterPrefix(params[0], _T("ObsZone="))) {
       // Observation zone line found
-      if (_tcslen(params[0]) <= 8)
+      if (StringIsEmpty(obs_zone))
         continue;
 
-      TPIndex = ParseOZs(turnpoint_infos, params, n_params);
+      TCHAR *end;
+      const std::size_t TPIndex = _tcstol(obs_zone, &end, 10);
+      if (end == obs_zone || TPIndex >= CUP_MAX_TPS)
+        continue;
+
+      ParseOZs(turnpoint_infos[TPIndex], params + 1, n_params - 1);
       if (TPIndex == 0)
         task_info->max_start_altitude = turnpoint_infos[TPIndex].max_altitude;
     }

commit 09f4e34d2201c94d6803dd4ab591ca39cef2a0dc

Task/TaskFileSeeYou: fix off-by-one bug
Regression from commit 12627e8ee9a4fe0; that commit introduced a "+1"
to the ParseOZs() call, but did not adjust the start index inside that
function.

Closes https://github.com/XCSoar/XCSoar/issues/1073

src/Task/TaskFileSeeYou.cpp
@@ -172,7 +172,7 @@ ParseOZs(SeeYouTurnpointInformation &tp_info, const TCHAR *params[],
 {
   tp_info.valid = true;
   // Iterate through available OZ options
-  for (unsigned i = 1; i < n_params; i++) {
+  for (unsigned i = 0; i < n_params; i++) {
     const TCHAR *pair = params[i];
 
     if (auto style = StringAfterPrefix(pair, _T("Style="))) {