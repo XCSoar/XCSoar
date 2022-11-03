c72c150b95811a58bd802d2bb26e9deb70c9381e
Max Kellermann <max.kellermann@gmail.com>
2022-11-03 10:43:39 +0100

    build/python/build/libs.py: update OpenSSL to 3.0.7
    Punycode hooray!

--- build/python/build/libs.py
+++ build/python/build/libs.py
@@ -96,9 +96,9 @@ musl = MuslProject(
 )
 
 openssl = OpenSSLProject(
-    'https://www.openssl.org/source/openssl-3.0.5.tar.gz',
-    'ftp://ftp.cert.dfn.de/pub/tools/net/openssl/source/openssl-3.0.5.tar.gz',
-    'aa7d8d9bef71ad6525c55ba11e5f4397889ce49c2c9349dcea6d3e4f0b024a7a',
+    'https://www.openssl.org/source/openssl-3.0.7.tar.gz',
+    'ftp://ftp.cert.dfn.de/pub/tools/net/openssl/source/openssl-3.0.7.tar.gz',
+    '83049d042a260e696f62406ac5c08bf706fd84383f945cf21bd61e9ed95c396e',
     'include/openssl/ossl_typ.h',
 )
 
