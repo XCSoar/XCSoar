1dcd2b3341e343da00831e690547f2d9d541e5da
August2111 <Info@FlapsOnline.de>
2023-02-01 16:46:18 +0100

    Install - package name with CamelCase

--- build/install.mk
+++ build/install.mk
@@ -6,16 +6,17 @@ prefix = $(DESTDIR)/usr
 install-mo: mo
 	install -d -m 0755 $(patsubst %,$(prefix)/share/locale/%/LC_MESSAGES,$(LINGUAS))
 	for i in $(LINGUAS); do \
-		install -m 0644 $(OUT)/po/$$i.mo $(prefix)/share/locale/$$i/LC_MESSAGES/xcsoar.mo; \
+		install -m 0644 $(OUT)/po/$$i.mo $(prefix)/share/locale/$$i/LC_MESSAGES/$(PROGRAM_NAME_LC).mo; \
 	done
 
 install-bin: all
+	@$(NQ)echo "  INSTALL   Passiert hier irgend etwas????"
 	install -d -m 0755 $(prefix)/bin
-	install -m 0755 $(TARGET_BIN_DIR)/$(PROGRAM_NAME_LC) $(TARGET_BIN_DIR)/vali-xcs $(prefix)/bin
+	install -m 0755 $(TARGET_BIN_DIR)/$(PROGRAM_NAME) $(TARGET_BIN_DIR)/vali-xcs $(prefix)/bin
 
 install-manual: manual
-	install -d -m 0755 $(prefix)/share/doc/$(PROGRAM_NAME_LC)
-	install -m 0644 $(MANUAL_PDF) $(prefix)/share/doc/$(PROGRAM_NAME_LC)
+	install -d -m 0755 $(prefix)/share/doc/$(PROGRAM_NAME)
+	install -m 0644 $(MANUAL_PDF) $(prefix)/share/doc/$(PROGRAM_NAME)
 
 install: install-bin install-mo install-manual
 
