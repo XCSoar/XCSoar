6b64099770fddb19c422c26a614194192902bbc5
August2111 <Info@FlapsOnline.de>
2023-02-10 23:45:31 +0100

    util/Compiler.h - SSIZE_T only at Windows-Clang needed

--- src/util/Compiler.h
+++ src/util/Compiler.h
@@ -37,7 +37,9 @@ Copyright_License {
 
 #ifdef __clang__
 #  define CLANG_VERSION GCC_MAKE_VERSION(__clang_major__, __clang_minor__, __clang_patchlevel__)
-typedef SSIZE_T ssize_t;
+#  ifdef _WIN32  // TODO(August2111): is there a better solution?
+     typedef SSIZE_T ssize_t;
+#  endif
 #else
 #  define CLANG_VERSION 0
 #endif
