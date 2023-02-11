c8dc4276ecb90ce3a97ee350f2c54846644f7225
August2111 <Info@FlapsOnline.de>
2023-02-11 22:53:13 +0100

    Cmd: Rework Licux - Commands

--- build/cmake/MakeAll.sh
+++ build/cmake/MakeAll.sh
@@ -1,3 +1,8 @@
+echo Make Win64:
+make DEBUG=n TARGET=WIN64
+echo Make Linux:
+make DEBUG=n TARGET=UNIX
+
 echo Make Android v7a (32 bit):
 make DEBUG=n TARGET=ANDROID
 
@@ -9,3 +14,4 @@ make DEBUG=n TARGET=ANDROIDX64
 
 echo Make Android x86:
 make DEBUG=n TARGET=ANDROID86
+
--- build/cmake/MakeLinux.sh
+++ build/cmake/MakeLinux.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
-git fetch --all
+git fetch OpenSoaring_OpenSoar
 # git reset --hard august/weglide-tmp 
-git reset --hard flaps3/cmake
-make
+# git reset --hard flaps3/cmake
+git reset --hard OpenSoaring_OpenSoar/dev-branch
+sudo chmod 757 -R ./build/cmake
+make TARGET=UNIX DEBUG=n
 output/UNIX/bin/xcsoar -fly -1000x500
 
