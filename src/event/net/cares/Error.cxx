4f00e72227dfb8fd1bd39de69808479f3cffb19d
Max Kellermann <mk@cm4all.com>
2023-03-12 20:07:14 +0100

    event: use libfmt

--- src/event/net/cares/Error.cxx
+++ src/event/net/cares/Error.cxx
@@ -3,15 +3,15 @@
 // author: Max Kellermann <mk@cm4all.com>
 
 #include "Error.hxx"
-#include "util/RuntimeError.hxx"
+#include "lib/fmt/RuntimeError.hxx"
 
 #include <ares.h>
 
 namespace Cares {
 
 Error::Error(int _code, const char *msg) noexcept
-	:std::runtime_error(FormatRuntimeError("%s: %s",
-					       msg, ares_strerror(_code))),
+	:std::runtime_error(FmtRuntimeError("{}: {}",
+					    msg, ares_strerror(_code))),
 	 code(_code) {}
 
 } // namespace Cares
