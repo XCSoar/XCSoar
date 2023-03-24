752668a96152305b543769799837cb67bc3cb1d9
August2111 <Info@FlapsOnline.de>
2023-03-24 14:19:34 +0100

    event/CMakeSource.cmake - removed IdleEvent.cxx
    This is done in XCSoar
    Besides that: code cleanup

--- src/event/CMakeSource.cmake
+++ src/event/CMakeSource.cmake
@@ -2,46 +2,21 @@ set(_SOURCES
     Loop.cxx
     Call.cxx
     DeferEvent.cxx
-    IdleEvent.cxx
     InjectEvent.cxx
     SocketEvent.cxx
     SignalMonitor.cxx
     TimerWheel.cxx
     TimerList.cxx
     CoarseTimerEvent.cxx
-#   TimerEvent.cxx
 
     net/ConnectSocket.cxx
     net/cares/Error.cxx
     net/cares/Init.cxx
     net/cares/Channel.cxx
     net/cares/SimpleResolver.cxx
-#    Backend.hxx
-#    BackendEvents.hxx
-#    Call.cxx
-#    Call.hxx
-#    Chrono.hxx
-#    DeferEvent.cxx
-#    DeferEvent.hxx
-#    EpollBackend.hxx
-#    EpollEvents.hxx
-#    Features.h
-#    IdleEvent.cxx
-#    IdleEvent.hxx
-#    InjectEvent.cxx
-#    InjectEvent.hxx
-#    Loop.cxx
-#    Loop.hxx
-#    SignalMonitor.cxx
-#    SignalMonitor.hxx
-#    SocketEvent.cxx
-#    SocketEvent.hxx
-#    TimerEvent.cxx
-#    TimerEvent.hxx
-#    WakeFD.hxx
 
     ../io/async/AsioThread.cpp
-      ../io/async/GlobalAsioThread.cpp
+    ../io/async/GlobalAsioThread.cpp
 )
 
 if(UNIX)
