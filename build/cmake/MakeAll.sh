27bf8b73e44668a60042dcba82853387b8e8caea
August2111 <Info@FlapsOnline.de>
2023-03-27 11:22:30 +0200

    bugfix build/cmake/MakeAll.sh - use inverted comma

--- build/cmake/MakeAll.sh
+++ build/cmake/MakeAll.sh
@@ -1,17 +1,17 @@
-echo Make Win64:
+echo "Make Win64:"
 make DEBUG=n TARGET=WIN64
-echo Make Linux:
+echo "Make Linux:"
 make DEBUG=n TARGET=UNIX
 
-echo Make Android v7a (32 bit):
+echo "Make Android v7a (32 bit):"
 make DEBUG=n TARGET=ANDROID
 
-echo Make Android v8a (64 bit):
+echo "Make Android v8a (64 bit):"
 make DEBUG=n TARGET=ANDROIDAARCH64
 
-echo Make Android x64:
+echo "Make Android x64:"
 make DEBUG=n TARGET=ANDROIDX64
 
-echo Make Android x86:
+echo "Make Android x86:"
 make DEBUG=n TARGET=ANDROID86
 
