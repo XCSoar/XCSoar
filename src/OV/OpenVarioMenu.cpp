21bb94768af6fa1c59d4d26eb2b1110c40141a1e
Blaubart <Blaubart@gmx.de>
2023-04-20 19:03:43 +0200

    The "Files" submenu has been revised and adapted to the new backup/restore script

--- src/OV/OpenVarioMenu.cpp
+++ src/OV/OpenVarioMenu.cpp
@@ -66,38 +66,74 @@ private:
   /* virtual methods from class Widget */
   void Prepare(ContainerWindow &parent,
                const PixelRect &rc) noexcept override;
+
 };
 
 void
 FileMenuWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
                         [[maybe_unused]] const PixelRect &rc) noexcept
 {
-  AddButton("Download XCSoar IGC files to USB", [this](){
-    const UI::ScopeDropMaster drop_master{display};
-    const UI::ScopeSuspendEventQueue suspend_event_queue{event_queue};
-    Run("/usr/bin/download-igc.sh");
+  AddButton("Download XCSoar IGC files to USB", [](){
+    static constexpr const char *argv[] = {
+      "/usr/bin/download-igc.sh", nullptr
+    };
+    RunProcessDialog(UIGlobals::GetMainWindow(),
+                     UIGlobals::GetDialogLook(),
+                     "Download IGC Files", argv);
   });
 
-  AddButton("Download XCSoar to USB", [](){
+  AddButton("Update Maps", [](){
     static constexpr const char *argv[] = {
-      "/usr/bin/download-all.sh", nullptr
+      "/usr/bin/update-maps.sh", nullptr
     };
 
     RunProcessDialog(UIGlobals::GetMainWindow(),
                      UIGlobals::GetDialogLook(),
-                     "Downloading files", argv);
+                     "Update Maps", argv);
   });
 
-  AddButton("Upload files from USB to XCSoar", [](){
+  AddButton("Update or upload XCSoar files from USB", [](){
     static constexpr const char *argv[] = {
       "/usr/bin/upload-xcsoar.sh", nullptr
     };
 
     RunProcessDialog(UIGlobals::GetMainWindow(),
                      UIGlobals::GetDialogLook(),
-                     "Uploading files", argv);
+                     "Update/Upload files", argv);
+  });
+
+  AddButton("System Backup: OpenVario and XCSoar settings to USB", [](){
+    static constexpr const char *argv[] = {
+      "/usr/bin/backup-system.sh", nullptr
+    };
+
+    RunProcessDialog(UIGlobals::GetMainWindow(),
+                     UIGlobals::GetDialogLook(),
+                     "Backup System", argv);
+  });
+
+  AddButton("System Restore: OpenVario and XCSoar settings from USB", [](){
+    static constexpr const char *argv[] = {
+      "/usr/bin/restore-system.sh", nullptr
+    };
+
+    RunProcessDialog(UIGlobals::GetMainWindow(),
+                     UIGlobals::GetDialogLook(),
+                     "Restore XCSoar and System", argv);
+    Display::Rotate(Display::DetectInitialOrientation());
+  });
+
+  AddButton("XCSoar Restore: Only XCSoar settings from USB", [](){
+    static constexpr const char *argv[] = {
+      "/usr/bin/restore-xcsoar.sh", nullptr
+    };
+
+    RunProcessDialog(UIGlobals::GetMainWindow(),
+                     UIGlobals::GetDialogLook(),
+                     "Restore XCSoar", argv);
   });
 }
+
 
 class ScreenRotationWidget final
   : public RowFormWidget
