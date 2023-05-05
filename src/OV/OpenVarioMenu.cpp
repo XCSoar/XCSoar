2b93fd5a86e727ff3b0558651c59f80728674d49
Blaubart <Blaubart@gmx.de>
2023-05-06 01:02:04 +0200

    Revert "The "Files" submenu has been revised and adapted to the new backup/restore script"
    This reverts commit 21bb94768af6fa1c59d4d26eb2b1110c40141a1e.

--- src/OV/OpenVarioMenu.cpp
+++ src/OV/OpenVarioMenu.cpp
@@ -118,71 +118,36 @@ private:
   /* virtual methods from class Widget */
   void Prepare(ContainerWindow &parent,
                const PixelRect &rc) noexcept override;
-
 };
 
 void
 FileMenuWidget::Prepare([[maybe_unused]] ContainerWindow &parent,
                         [[maybe_unused]] const PixelRect &rc) noexcept
 {
-  AddButton("Download XCSoar IGC files to USB", [](){
-    static constexpr const char *argv[] = {
-      "/usr/bin/download-igc.sh", nullptr
-    };
-    RunProcessDialog(UIGlobals::GetMainWindow(),
-                     UIGlobals::GetDialogLook(),
-                     "Download IGC Files", argv);
+  AddButton("Download XCSoar IGC files to USB", [this](){
+    const UI::ScopeDropMaster drop_master{display};
+    const UI::ScopeSuspendEventQueue suspend_event_queue{event_queue};
+    Run("/usr/bin/download-igc.sh");
   });
 
-  AddButton("Update Maps", [](){
+  AddButton("Download XCSoar to USB", [](){
     static constexpr const char *argv[] = {
-      "/usr/bin/update-maps.sh", nullptr
+      "/usr/bin/download-all.sh", nullptr
     };
 
     RunProcessDialog(UIGlobals::GetMainWindow(),
                      UIGlobals::GetDialogLook(),
-                     "Update Maps", argv);
+                     "Downloading files", argv);
   });
 
-  AddButton("Update or upload XCSoar files from USB", [](){
+  AddButton("Upload files from USB to XCSoar", [](){
     static constexpr const char *argv[] = {
       "/usr/bin/upload-xcsoar.sh", nullptr
     };
 
     RunProcessDialog(UIGlobals::GetMainWindow(),
                      UIGlobals::GetDialogLook(),
-                     "Update/Upload files", argv);
-  });
-
-  AddButton("System Backup: OpenVario and XCSoar settings to USB", [](){
-    static constexpr const char *argv[] = {
-      "/usr/bin/backup-system.sh", nullptr
-    };
-
-    RunProcessDialog(UIGlobals::GetMainWindow(),
-                     UIGlobals::GetDialogLook(),
-                     "Backup System", argv);
-  });
-
-  AddButton("System Restore: OpenVario and XCSoar settings from USB", [](){
-    static constexpr const char *argv[] = {
-      "/usr/bin/restore-system.sh", nullptr
-    };
-
-    RunProcessDialog(UIGlobals::GetMainWindow(),
-                     UIGlobals::GetDialogLook(),
-                     "Restore XCSoar and System", argv);
-    Display::Rotate(Display::DetectInitialOrientation());
-  });
-
-  AddButton("XCSoar Restore: Only XCSoar settings from USB", [](){
-    static constexpr const char *argv[] = {
-      "/usr/bin/restore-xcsoar.sh", nullptr
-    };
-
-    RunProcessDialog(UIGlobals::GetMainWindow(),
-                     UIGlobals::GetDialogLook(),
-                     "Restore XCSoar", argv);
+                     "Uploading files", argv);
   });
 }
 
