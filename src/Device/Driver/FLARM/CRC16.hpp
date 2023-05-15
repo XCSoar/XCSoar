951b9f0b9a2976eaf3cc2cf7031ca3efcdf1b2df
Max Kellermann <max.kellermann@gmail.com>
2023-05-15 17:40:51 +0200

    Driver/FLARM/CRC16: no namespace indent

--- src/Device/Driver/FLARM/CRC16.hpp
+++ src/Device/Driver/FLARM/CRC16.hpp
@@ -7,16 +7,18 @@
 #include <cstddef>
 
 namespace FLARM {
-  struct FrameHeader;
 
-  /**
-   * Calculates the CRC value of the FrameHeader and an optional payload
-   * @param header FrameHeader to calculate the CRC for
-   * @param data Optional pointer to the first byte of the payload
-   * @param length Optional length of the payload
-   * @return CRC value
-   */
-  [[gnu::pure]]
-  uint16_t CalculateCRC(const FrameHeader &header, const void *data = nullptr,
-                        size_t length = 0);
-};
+struct FrameHeader;
+
+/**
+ * Calculates the CRC value of the FrameHeader and an optional payload
+ * @param header FrameHeader to calculate the CRC for
+ * @param data Optional pointer to the first byte of the payload
+ * @param length Optional length of the payload
+ * @return CRC value
+ */
+[[gnu::pure]]
+uint16_t CalculateCRC(const FrameHeader &header, const void *data = nullptr,
+                      size_t length = 0);
+
+} // namespace FLARM
