XCSoar/XCSoar

commit 35f47267854ccfeddeea12ee4d1e38edc2668f43

Terrain/HeightMatrix: use UnsignedPoint2D

src/Terrain/HeightMatrix.cpp
@@ -21,37 +21,40 @@ HeightMatrix::SetSize(std::size_t _size) noexcept
 }
 
 void
-HeightMatrix::SetSize(unsigned _width, unsigned _height) noexcept
+HeightMatrix::SetSize(UnsignedPoint2D _size) noexcept
 {
-  width = _width;
-  height = _height;
+  size = _size;
 
-  SetSize(width * height);
+  SetSize(std::size_t{size.x} * std::size_t{size.y});
 }
 
 void
-HeightMatrix::SetSize(unsigned width, unsigned height,
+HeightMatrix::SetSize(UnsignedPoint2D _size,
                       unsigned quantisation_pixels) noexcept
 {
-  SetSize((width + quantisation_pixels - 1) / quantisation_pixels,
-          (height + quantisation_pixels - 1) / quantisation_pixels);
+  const UnsignedPoint2D round_up{
+    quantisation_pixels - 1,
+    quantisation_pixels - 1,
+  };
+
+  SetSize((_size + round_up) / quantisation_pixels);
 }
 
 #ifdef ENABLE_OPENGL
 
 void
 HeightMatrix::Fill(const RasterMap &map, const GeoBounds &bounds,
-                   unsigned width, unsigned height, bool interpolate) noexcept
+                   const UnsignedPoint2D _size, bool interpolate) noexcept
 {
-  SetSize(width, height);
+  SetSize(_size);
 
-  const Angle delta_y = bounds.GetHeight() / height;
+  const Angle delta_y = bounds.GetHeight() / _size.y;
   Angle latitude = bounds.GetNorth();
-  for (auto p = data.data(), end = p + width * height;
-       p != end; p += width, latitude -= delta_y) {
+  for (auto p = data.data(), end = p + std::size_t{_size.x} * std::size_t{_size.y};
+       p != end; p += _size.x, latitude -= delta_y) {
     map.ScanLine(GeoPoint(bounds.GetWest(), latitude),
                  GeoPoint(bounds.GetEast(), latitude),
-                 p, width, interpolate);
+                 p, _size.x, interpolate);
   }
 }
 
@@ -63,15 +66,14 @@ HeightMatrix::Fill(const RasterMap &map, const WindowProjection &projection,
 {
   const auto screen_size = projection.GetScreenSize();
 
-  SetSize((screen_size.width + quantisation_pixels - 1) / quantisation_pixels,
-          (screen_size.height + quantisation_pixels - 1) / quantisation_pixels);
+  SetSize((UnsignedPoint2D)screen_size, quantisation_pixels);
 
   auto p = data.data();
-  for (unsigned y = 0; y < screen_size.height;
-       y += quantisation_pixels, p += width) {
+  for (unsigned y = 0; y < size.y;
+       y += quantisation_pixels, p += size.x) {
     map.ScanLine(projection.ScreenToGeo({0, (int)y}),
                  projection.ScreenToGeo({(int)screen_size.width, (int)y}),
-                 p, width, interpolate);
+                 p, size.x, interpolate);
   }
 }
 

src/Terrain/HeightMatrix.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "Height.hpp"
+#include "Math/Point2D.hpp"
 #include "util/AllocatedArray.hxx"
 
 class RasterMap;
@@ -16,7 +17,7 @@ class WindowProjection;
 
 class HeightMatrix {
   AllocatedArray<TerrainHeight> data;
-  unsigned width = 0, height = 0;
+  UnsignedPoint2D size;
 
 public:
   HeightMatrix() noexcept = default;
@@ -26,8 +27,8 @@ public:
 
 protected:
   void SetSize(std::size_t _size) noexcept;
-  void SetSize(unsigned width, unsigned height) noexcept;
-  void SetSize(unsigned width, unsigned height,
+  void SetSize(UnsignedPoint2D _size) noexcept;
+  void SetSize(UnsignedPoint2D _size,
                unsigned quantisation_pixels) noexcept;
 
 public:
@@ -36,7 +37,7 @@ public:
    * Copy values from the #RasterMap to the buffer, north-up only.
    */
   void Fill(const RasterMap &map, const GeoBounds &bounds,
-            unsigned _width, unsigned _height, bool interpolate) noexcept;
+            UnsignedPoint2D _size, bool interpolate) noexcept;
 #else
   /**
    * @param interpolate true enables interpolation of sub-pixel values
@@ -45,12 +46,8 @@ public:
             unsigned quantisation_pixels, bool interpolate) noexcept;
 #endif
 
-  unsigned GetWidth() const noexcept {
-    return width;
-  }
-
-  unsigned GetHeight() const noexcept {
-    return height;
+  UnsignedPoint2D GetSize() const noexcept {
+    return size;
   }
 
   const TerrainHeight *GetData() const noexcept {
@@ -58,10 +55,10 @@ public:
   }
 
   const TerrainHeight *GetRow(unsigned y) const noexcept {
-    return GetData() + y * width;
+    return GetData() + y * size.x;
   }
 
   const TerrainHeight *GetDataEnd() const noexcept {
-    return GetRow(height);
+    return GetRow(size.y);
   }
 };

src/Terrain/RasterRenderer.cpp
@@ -158,8 +158,7 @@ RasterRenderer::ScanMap(const RasterMap &map,
   bounds.IntersectWith(map.GetBounds());
 
   height_matrix.Fill(map, bounds,
-                     projection.GetScreenSize().width / quantisation_pixels,
-                     projection.GetScreenSize().height / quantisation_pixels,
+                     (UnsignedPoint2D)projection.GetScreenSize() / quantisation_pixels,
                      true);
 
   last_quantisation_pixels = quantisation_pixels;
@@ -176,13 +175,13 @@ RasterRenderer::GenerateImage(bool do_shading,
                               bool do_contour) noexcept
 {
   if (image == nullptr ||
-      height_matrix.GetWidth() > image->GetSize().width ||
-      height_matrix.GetHeight() > image->GetSize().height) {
+      height_matrix.GetSize().x > image->GetSize().width ||
+      height_matrix.GetSize().y > image->GetSize().height) {
     delete image;
-    image = new RawBitmap({height_matrix.GetWidth(), height_matrix.GetHeight()});
+    image = new RawBitmap(PixelSize{height_matrix.GetSize()});
 
     delete[] contour_column_base;
-    contour_column_base = new unsigned char[height_matrix.GetWidth()];
+    contour_column_base = new unsigned char[height_matrix.GetSize().x];
   }
 
   if (quantisation_effective == 0) {
@@ -211,14 +210,14 @@ RasterRenderer::GenerateUnshadedImage(const unsigned height_scale,
   const RawColor *oColorBuf = color_table + 64 * 256;
   RawColor *dest = image->GetTopRow();
 
-  for (unsigned y = height_matrix.GetHeight(); y > 0; --y) {
+  for (unsigned y = height_matrix.GetSize().y; y > 0; --y) {
     RawColor *p = dest;
     dest = image->GetNextRow(dest);
 
     unsigned contour_row_base = ContourInterval(*src, contour_height_scale);
     unsigned char *contour_this_column_base = contour_column_base;
 
-    for (unsigned x = height_matrix.GetWidth(); x > 0; --x) {
+    for (unsigned x = height_matrix.GetSize().x; x > 0; --x) {
       const auto e = *src++;
       if (gcc_likely(!e.IsSpecial())) {
         unsigned h = std::max(0, (int)e.GetValue());
@@ -279,11 +278,8 @@ RasterRenderer::GenerateSlopeImage(unsigned height_scale,
 {
   assert(quantisation_effective > 0);
 
-  PixelRect border;
-  border.left = quantisation_effective;
-  border.top = quantisation_effective;
-  border.right = height_matrix.GetWidth() - quantisation_effective;
-  border.bottom = height_matrix.GetHeight() - quantisation_effective;
+  const auto border = PixelRect{PixelSize{height_matrix.GetSize()}}
+    .WithPadding(quantisation_effective);
 
   const unsigned height_slope_factor =
     Clamp((unsigned)pixel_size, 1u,
@@ -297,15 +293,15 @@ RasterRenderer::GenerateSlopeImage(unsigned height_scale,
 
   RawColor *dest = image->GetTopRow();
 
-  for (unsigned y = 0; y < height_matrix.GetHeight(); ++y) {
+  for (unsigned y = 0; y < height_matrix.GetSize().y; ++y) {
     const unsigned row_plus_index = y < (unsigned)border.bottom
       ? quantisation_effective
-      : height_matrix.GetHeight() - 1 - y;
-    const unsigned row_plus_offset = height_matrix.GetWidth() * row_plus_index;
+      : height_matrix.GetSize().y - 1 - y;
+    const unsigned row_plus_offset = height_matrix.GetSize().x * row_plus_index;
 
     const unsigned row_minus_index = y >= quantisation_effective
       ? quantisation_effective : y;
-    const unsigned row_minus_offset = height_matrix.GetWidth() * row_minus_index;
+    const unsigned row_minus_offset = height_matrix.GetSize().x * row_minus_index;
 
     const unsigned p31 = row_plus_index + row_minus_index;
 
@@ -315,7 +311,7 @@ RasterRenderer::GenerateSlopeImage(unsigned height_scale,
     unsigned contour_row_base = ContourInterval(*src, contour_height_scale);
     unsigned char *contour_this_column_base = contour_column_base;
 
-    for (unsigned x = 0; x < height_matrix.GetWidth(); ++x, ++src) {
+    for (unsigned x = 0; x < height_matrix.GetSize().x; ++x, ++src) {
       const auto e = *src;
       if (gcc_likely(!e.IsSpecial())) {
         unsigned h = std::max(0, (int)e.GetValue());
@@ -337,7 +333,7 @@ RasterRenderer::GenerateSlopeImage(unsigned height_scale,
 
         const unsigned column_plus_index = x < (unsigned)border.right
           ? quantisation_effective
-          : height_matrix.GetWidth() - 1 - x;
+          : height_matrix.GetSize().x - 1 - x;
         const unsigned column_minus_index = x >= (unsigned)border.left
           ? quantisation_effective : x;
 
@@ -459,7 +455,7 @@ RasterRenderer::ContourStart(const unsigned contour_height_scale) noexcept
   // initialise column to first row
   const auto *src = height_matrix.GetData();
   unsigned char *col_base = contour_column_base;
-  for (unsigned x = height_matrix.GetWidth(); x > 0; --x)
+  for (unsigned x = height_matrix.GetSize().x; x > 0; --x)
     *col_base++ = ContourInterval(*src++, contour_height_scale);
 }
 
@@ -471,12 +467,11 @@ RasterRenderer::Draw([[maybe_unused]] Canvas &canvas,
 #ifdef ENABLE_OPENGL
   if (bounds.IsValid() && bounds.Overlaps(projection.GetScreenBounds()))
     DrawGeoBitmap(*image,
-                  PixelSize(height_matrix.GetWidth(),
-                            height_matrix.GetHeight()),
+                  PixelSize{height_matrix.GetSize()},
                   bounds,
                   projection);
 #else
-  image->StretchTo({height_matrix.GetWidth(), height_matrix.GetHeight()},
+  image->StretchTo(PixelSize{height_matrix.GetSize()},
                    canvas, projection.GetScreenSize(),
                    transparent_white);
 #endif

src/Terrain/RasterRenderer.hpp
@@ -70,12 +70,8 @@ public:
     return height_matrix;
   }
 
-  unsigned GetWidth() const noexcept {
-    return height_matrix.GetWidth();
-  }
-
-  unsigned GetHeight() const noexcept {
-    return height_matrix.GetHeight();
+  UnsignedPoint2D GetSize() const noexcept {
+    return height_matrix.GetSize();
   }
 
 #ifdef ENABLE_OPENGL

test/src/RunHeightMatrix.cpp
@@ -52,8 +52,7 @@ try {
   HeightMatrix matrix;
 #ifdef ENABLE_OPENGL
   matrix.Fill(map, projection.GetScreenBounds(),
-              projection.GetScreenSize().width,
-              projection.GetScreenSize().height,
+              (UnsignedPoint2D)projection.GetScreenSize(),
               false);
 #else
   matrix.Fill(map, projection, 1, false);

commit c002543c98f962b3a1e67120912ef189d98058b1

Terrain/HeightMatrix: use screen height, not quantized height
Fixes corrupt terrain caused by commit 35f47267854ccfedde

Closes https://github.com/XCSoar/XCSoar/issues/1229

src/Terrain/HeightMatrix.cpp
@@ -69,7 +69,7 @@ HeightMatrix::Fill(const RasterMap &map, const WindowProjection &projection,
   SetSize((UnsignedPoint2D)screen_size, quantisation_pixels);
 
   auto p = data.data();
-  for (unsigned y = 0; y < size.y;
+  for (unsigned y = 0; y < screen_size.height;
        y += quantisation_pixels, p += size.x) {
     map.ScanLine(projection.ScreenToGeo({0, (int)y}),
                  projection.ScreenToGeo({(int)screen_size.width, (int)y}),