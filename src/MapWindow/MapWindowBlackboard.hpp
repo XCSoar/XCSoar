XCSoar/XCSoar

commit ec59dbac8bba9198af64d29d2ed2de657a6237d1

MapWindow: keep showing disappeared FLARM traffic for some time
Closes https://github.com/XCSoar/XCSoar/issues/1083

src/MapWindow/MapWindowBlackboard.hpp
@@ -9,6 +9,8 @@
 #include "thread/Debug.hpp"
 #include "UIState.hpp"
 
+#include <map>
+
 /**
  * Blackboard used by map window: provides read-only access to local
  * copies of data required by map window
@@ -20,6 +22,12 @@ class MapWindowBlackboard:
   public MapSettingsBlackboard
 {
   UIState ui_state;
+
+  /**
+   * FLARM traffic that has disappeared, but will remain on the map
+   * (greyed out) for some time.
+   */
+  std::map<FlarmId, FlarmTraffic> fading_flarm_traffic;
 
 protected:
   [[gnu::const]]
@@ -34,6 +42,11 @@ protected:
     assert(InDrawThread());
 
     return BaseBlackboard::Calculated();
+  }
+
+  [[gnu::const]]
+  const auto &GetFadingFlarmTraffic() const noexcept {
+    return fading_flarm_traffic;
   }
 
   [[gnu::const]]

src/Dialogs/Settings/Panels/SymbolsConfigPanel.cpp
@@ -14,6 +14,7 @@
 enum ControlIndex {
   DISPLAY_TRACK_BEARING,
   ENABLE_FLARM_MAP,
+  FADE_TRAFFIC,
   TRAIL_LENGTH,
   TRAIL_DRIFT,
   TRAIL_TYPE,
@@ -137,6 +138,9 @@ SymbolsConfigPanel::Prepare([[maybe_unused]] ContainerWindow &parent,
   AddBoolean(_("FLARM traffic"), _("This enables the display of FLARM traffic on the map window."),
              settings_map.show_flarm_on_map);
 
+  AddBoolean(_("Fade traffic"), _("Keep showing traffic for a while after it has disappeared."),
+             settings_map.fade_traffic);
+
   AddEnum(_("Trail length"),
           _("Determines whether and how long a snail trail is drawn behind the glider."),
           trail_length_list,
@@ -193,6 +197,9 @@ SymbolsConfigPanel::Save(bool &_changed) noexcept
 
   changed |= SaveValue(ENABLE_FLARM_MAP, ProfileKeys::EnableFLARMMap,
                        settings_map.show_flarm_on_map);
+
+  changed |= SaveValue(FADE_TRAFFIC, ProfileKeys::FadeTraffic,
+                       settings_map.fade_traffic);
 
   changed |= SaveValueEnum(TRAIL_LENGTH, ProfileKeys::SnailTrail, settings_map.trail.length);
 

src/Look/TrafficLook.cpp
@@ -19,6 +19,12 @@ TrafficLook::Initialise(const Font &_font)
   warning_in_altitude_range_brush.Create(warning_in_altitude_range_color);
   alarm_brush.Create(alarm_color);
 
+  fading_pen.Create(Pen::Style::DASH1, 1, fading_outline_color);
+
+#ifdef ENABLE_OPENGL
+  fading_brush.Create(fading_fill_color);
+#endif
+
   unsigned width = Layout::ScalePenWidth(2);
   team_pen_green.Create(width, team_color_green);
   team_pen_blue.Create(width, team_color_blue);

src/Look/TrafficLook.hpp
@@ -23,6 +23,14 @@ struct TrafficLook {
   Brush warning_in_altitude_range_brush;
   Brush alarm_brush;
 
+  static constexpr Color fading_outline_color = ColorWithAlpha({0x60, 0x60, 0x60}, 0xa0);
+  Pen fading_pen;
+
+#ifdef ENABLE_OPENGL
+  static constexpr Color fading_fill_color = ColorWithAlpha({0xc0, 0xc0, 0xc0}, 0x60);
+  Brush fading_brush;
+#endif
+
   static constexpr Color team_color_green = Color(0x74, 0xff, 0);
   static constexpr Color team_color_magenta = Color(0xff, 0, 0xcb);
   static constexpr Color team_color_blue = Color(0, 0x90, 0xff);

src/MapSettings.cpp
@@ -41,6 +41,7 @@ MapSettings::SetDefaults()
   cruise_scale = 1 / 60.;
   show_flarm_on_map = true;
   show_flarm_alarm_level = true;
+  fade_traffic = true;
   show_thermal_profile = true;
   final_glide_bar_mc0_enabled = true;
   final_glide_bar_display_mode = FinalGlideBarDisplayMode::ON;

src/MapSettings.hpp
@@ -156,6 +156,11 @@ struct MapSettings {
    */
   bool show_flarm_alarm_level;
 
+  /**
+   * Keep showing traffic for a while after it has disappeared?
+   */
+  bool fade_traffic;
+
   /** Display climb band on map */
   bool show_thermal_profile;
 

src/MapWindow/MapWindowBlackboard.cpp
@@ -2,6 +2,7 @@
 // Copyright The XCSoar Project
 
 #include "MapWindowBlackboard.hpp"
+#include "FLARM/Friends.hpp"
 
 void
 MapWindowBlackboard::ReadComputerSettings(const ComputerSettings &settings) noexcept
@@ -15,10 +16,57 @@ MapWindowBlackboard::ReadMapSettings(const MapSettings &settings) noexcept
   settings_map = settings;
 }
 
+[[gnu::pure]]
+static bool
+IsFriend(FlarmId id) noexcept
+{
+  return FlarmFriends::GetFriendColor(id) != FlarmColor::NONE;
+}
+
+static void
+UpdateFadingTraffic(bool fade_traffic,
+                    std::map<FlarmId, FlarmTraffic> &dest,
+                    const TrafficList &old_list, const TrafficList &new_list,
+                    TimeStamp now) noexcept
+{
+  if (!fade_traffic) {
+    dest.clear();
+    return;
+  }
+
+  if (new_list.modified.Modified(old_list.modified)||true) {
+    /* first add all items from the old list */
+    for (const auto &traffic : old_list.list)
+      if (traffic.location_available)
+        dest.try_emplace(traffic.id, traffic);
+
+    /* now remove all items that are in the new list; now only items
+       remain that have disappeared */
+    for (const auto &traffic : new_list.list)
+      if (auto i = dest.find(traffic.id); i != dest.end())
+        dest.erase(i);
+  }
+
+  /* remove all items that havn't been seen again for too long */
+  std::erase_if(dest, [now](const auto &i){
+    /* friends expire after 10 minutes, all others after one minute */
+    const auto max_age = IsFriend(i.first)
+      ? std::chrono::minutes{10}
+      : std::chrono::minutes{1};
+
+    return i.second.valid.IsOlderThan(now, max_age);
+  });
+}
+
 void
 MapWindowBlackboard::ReadBlackboard(const MoreData &nmea_info,
 				    const DerivedInfo &derived_info) noexcept
 {
+  UpdateFadingTraffic(settings_map.fade_traffic,
+                      fading_flarm_traffic, gps_info.flarm.traffic,
+                      nmea_info.flarm.traffic,
+                      nmea_info.clock);
+
   gps_info = nmea_info;
   calculated_info = derived_info;
 }

src/MapWindow/MapWindowTraffic.cpp
@@ -16,7 +16,7 @@
 
 static void
 DrawFlarmTraffic(Canvas &canvas, const WindowProjection &projection,
-                 const TrafficLook &look,
+                 const TrafficLook &look, bool fading,
                  const PixelPoint aircraft_pos,
                  const FlarmTraffic &traffic) noexcept
 {
@@ -32,7 +32,8 @@ DrawFlarmTraffic(Canvas &canvas, const WindowProjection &projection,
     return;
 
   TextInBoxMode mode;
-  mode.shape = LabelShape::OUTLINED;
+  if (!fading)
+    mode.shape = LabelShape::OUTLINED;
 
   // JMW TODO enhancement: decluttering of FLARM altitudes (sort by max lift)
 
@@ -48,7 +49,7 @@ DrawFlarmTraffic(Canvas &canvas, const WindowProjection &projection,
                 mode, projection.GetScreenRect());
     }
 
-    if (traffic.climb_rate_avg30s >= 0.1) {
+    if (!fading && traffic.climb_rate_avg30s >= 0.1) {
       // If average climb data available draw it to the canvas
       TCHAR label_avg[100];
       FormatUserVerticalSpeed(traffic.climb_rate_avg30s,
@@ -64,7 +65,8 @@ DrawFlarmTraffic(Canvas &canvas, const WindowProjection &projection,
   }
 
   auto color = FlarmFriends::GetFriendColor(traffic.id);
-  TrafficRenderer::Draw(canvas, look, traffic,
+
+  TrafficRenderer::Draw(canvas, look, fading, traffic,
                         traffic.track - projection.GetScreenAngle(),
                         color, sc);
 }
@@ -83,8 +85,6 @@ MapWindow::DrawFLARMTraffic(Canvas &canvas,
 
   // Return if FLARM data is not available
   const TrafficList &flarm = Basic().flarm.traffic;
-  if (flarm.IsEmpty())
-    return;
 
   const WindowProjection &projection = render_projection;
 
@@ -101,8 +101,17 @@ MapWindow::DrawFLARMTraffic(Canvas &canvas,
     if (!traffic.location_available)
       continue;
 
-    DrawFlarmTraffic(canvas, projection, traffic_look,
+    DrawFlarmTraffic(canvas, projection, traffic_look, false,
                      aircraft_pos, traffic);
+  }
+
+  if (const auto &fading = GetFadingFlarmTraffic(); !fading.empty()) {
+    for (const auto &[id, traffic] : fading) {
+      assert(traffic.location_available);
+
+      DrawFlarmTraffic(canvas, projection, traffic_look, true,
+                       aircraft_pos, traffic);
+    }
   }
 }
 

commit ea7d0c8862a4078a338edcd45b3524ec7928e588

MapWindow/Blackboard: initialise `gps_info` in constructor
Fixes a regression caused by commit ec59dbac8bba which caused
uninitialised data to be read from `gps_info`.

src/MapWindow/MapWindowBlackboard.hpp
@@ -30,6 +30,12 @@ class MapWindowBlackboard:
   std::map<FlarmId, FlarmTraffic> fading_flarm_traffic;
 
 protected:
+  MapWindowBlackboard() noexcept {
+    /* this needs to be initialised because ReadBlackboard() uses the
+       previous FLARM traffic list */
+    gps_info.Reset();
+  }
+
   [[gnu::const]]
   const MoreData &Basic() const noexcept {
     assert(InDrawThread());