XCSoar/XCSoar

commit 7425dffaeb8f0ea38166eac631ecc27df55cac08

time/BrokenDateTime: use std::chrono::duration in arithmetic operators
More type safety.

src/time/BrokenTime.hpp
@@ -26,6 +26,7 @@ Copyright_License {
 
 #include <type_traits>
 
+#include <chrono>
 #include <cstdint>
 
 /**
@@ -113,6 +114,16 @@ struct BrokenTime {
   static BrokenTime FromSecondOfDay(unsigned second_of_day) noexcept;
 
   /**
+   * Construct a BrokenTime object from the specified duration since
+   * midnight.  seconds which have passed on this day.
+   */
+  template<class Rep, class Period>
+  [[gnu::const]]
+  static BrokenTime FromSinceMidnight(const std::chrono::duration<Rep,Period> &since_midnight) noexcept {
+    return FromSecondOfDay(std::chrono::duration_cast<std::chrono::seconds>(since_midnight).count());
+  }
+
+  /**
    * A wrapper for FromSecondOfDay() which allows values bigger than
    * or equal to 3600*24.
    */
@@ -147,28 +158,14 @@ struct BrokenTime {
    * Returns a BrokenTime that has the specified number of seconds
    * added to it.  It properly wraps around midnight.
    *
-   * @param seconds the number of seconds to add
-   */
-  [[gnu::pure]]
-  BrokenTime operator+(unsigned seconds) const noexcept;
-
-  /**
-   * Returns a BrokenTime that has the specified number of seconds
-   * added to it.  It properly wraps around midnight.
-   *
    * @param seconds the number of seconds to add; may be negative
    */
   [[gnu::pure]]
-  BrokenTime operator+(int seconds) const noexcept;
+  BrokenTime operator+(std::chrono::seconds delta) const noexcept;
 
   [[gnu::pure]]
-  BrokenTime operator-(int seconds) const noexcept {
-    return *this + (-seconds);
-  }
-
-  [[gnu::pure]]
-  BrokenTime operator-(unsigned seconds) const noexcept {
-    return *this - int(seconds);
+  BrokenTime operator-(std::chrono::seconds delta) const noexcept {
+    return *this + (-delta);
   }
 };
 

python/src/Flight.cpp
@@ -269,7 +269,7 @@ PyObject* xcsoar_Flight_reduce(Pyxcsoar_Flight *self, PyObject *args, PyObject *
        is about the year 2242, which is far enough in the future :-) */
     end = BrokenDateTime::FromUnixTimeUTC(int64_t(2)<<32);
 
-  if (end - begin < 0) {
+  if ((end - begin).count() < 0) {
     PyErr_SetString(PyExc_ValueError, "Start time later then end time.");
     return nullptr;
   }

src/Computer/GlideComputer.cpp
@@ -80,16 +80,16 @@ GlideComputer::ProcessGPS(bool force)
 
   if (basic.time_available) {
     /* use UTC offset to calculate local time */
-    const int utc_offset_s = settings.utc_offset.AsSeconds();
+    const auto utc_offset = settings.utc_offset.ToDuration();
 
     calculated.date_time_local = basic.date_time_utc.IsDatePlausible()
       /* known date: apply UTC offset to BrokenDateTime, which may
          increment/decrement date */
-      ? basic.date_time_utc + utc_offset_s
+      ? basic.date_time_utc + utc_offset
       /* unknown date: apply UTC offset only to BrokenTime, leave the
          BrokenDate part invalid as it was */
       : BrokenDateTime(BrokenDate::Invalid(),
-                       ((const BrokenTime &)basic.date_time_utc) + utc_offset_s);
+                       ((const BrokenTime &)basic.date_time_utc) + utc_offset);
   } else
     calculated.date_time_local = BrokenDateTime::Invalid();
 

src/Device/Driver/Volkslogger/vlconv.cpp
@@ -220,7 +220,7 @@ public:
         // TDECL is used as a base
         T_FDT = TDECL;
         // Add the timezone
-        T_FDT = T_FDT + zz_min*60; // add timezone in seconds
+        T_FDT = T_FDT + std::chrono::minutes(zz_min); // add timezone in seconds
         FDT[0] = T_FDT.day;
         FDT[1] = T_FDT.month;
         FDT[2] = T_FDT.year % 100;
@@ -478,9 +478,9 @@ convert_gcs(FILE *Ausgabedatei,
   int ende;
 
   // relative time from the beginning of the logging
-  long time_relative = 0;
-  long temptime;
-  long decl_time;
+  std::chrono::system_clock::duration time_relative{};
+  std::chrono::seconds temptime;
+  std::chrono::system_clock::duration decl_time(-1);
   BrokenDateTime firsttime;
 
   BrokenDateTime realtime;
@@ -507,8 +507,6 @@ convert_gcs(FILE *Ausgabedatei,
   igcfix.lat = 0;
   igcfix.lon = 0;
 
-  decl_time = -1;
-
   ende = 0;
   p = bin_puffer;
 
@@ -523,10 +521,10 @@ convert_gcs(FILE *Ausgabedatei,
         return 0;
 
       // calculates backwards the time of the first fix
-      time_relative += p[1];
-      temptime = 65536L * p[2] + 256L * p[3] + p[4];
+      time_relative += std::chrono::seconds(p[1]);
+      temptime = std::chrono::seconds{65536L * p[2] + 256L * p[3] + p[4]};
       firsttime = BrokenDateTime(BrokenDate::Invalid(),
-                                 BrokenTime::FromSecondOfDay(temptime));
+                                 BrokenTime::FromSinceMidnight(temptime));
       firsttime.day = 10 * (p[7] >> 4) + (p[7] & 0x0f);
       firsttime.month = 10 * (p[6] >> 4) + (p[6] & 0x0f);
       firsttime.year = 10 * (p[5] >> 4) + (p[5] & 0x0f) + 1900;
@@ -547,7 +545,7 @@ convert_gcs(FILE *Ausgabedatei,
        l = 0;
        break;
        }
-       time_relative += p[2];
+       time_relative += std::chrono::seconds(p[2]);
        l = pos_ds_size[bfv][1];
        break;
        */
@@ -565,7 +563,7 @@ convert_gcs(FILE *Ausgabedatei,
       if (p + 8 > end)
         return 0;
 
-      time_relative += p[2];
+      time_relative += std::chrono::seconds(p[2]);
       igcfix.valid = ((p[0] & 0x10) >> 4) ? 'A' : 'V';
       if (Haupttyp == rectyp_pos) {
         l = pos_ds_size[bfv][0];
@@ -590,7 +588,7 @@ convert_gcs(FILE *Ausgabedatei,
       }
       break;
     case rectyp_sep:
-      time_relative = 0;
+      time_relative = {};
       bfv = p[0] & ~rectyp_msk;
       if (bfv > max_bfv) {
         // unsupported binary file version
@@ -617,7 +615,7 @@ convert_gcs(FILE *Ausgabedatei,
         p2 = p + 2;
         break;
       case rectyp_vrt:
-        time_relative += p[2];
+        time_relative += std::chrono::seconds(p[2]);
         p2 = p + 3;
         break;
       default:
@@ -868,7 +866,7 @@ convert_gcs(FILE *Ausgabedatei,
 
   if (igcfile_version >= 414 || (task.STA.koord.lat != 0)
       || (task.STA.koord.lon != 0)) {
-    if (decl_time >= 0) {
+    if (decl_time.count() >= 0) {
       task.hasdeclaration = 1;
       task.TDECL = firsttime + decl_time;
       task.print(igcfile_version, Ausgabedatei);
@@ -902,8 +900,8 @@ convert_gcs(FILE *Ausgabedatei,
         l = 0;
         break;
       }
-      time_relative += p[2];
-      realtime = realtime + p[2];
+      time_relative += std::chrono::seconds(p[2]);
+      realtime = realtime + std::chrono::seconds(p[2]);
       igcfix.valid = ((p[0] & 0x10) >> 4) ? 'A' : 'V';
       const unsigned press = unsigned(p[0] & 0x0f) << 8 | p[1];
       unsigned gpalt, fxa, enl;
@@ -989,7 +987,7 @@ convert_gcs(FILE *Ausgabedatei,
         p2 = p + 2;
         break;
       case rectyp_vrt:
-        realtime = realtime + p[2];
+        realtime = realtime + std::chrono::seconds(p[2]);
         p2 = p + 3;
         break;
       default:
@@ -1012,7 +1010,7 @@ convert_gcs(FILE *Ausgabedatei,
       ;
       break;
     case rectyp_tnd:
-      realtime = realtime + p[1];
+      realtime = realtime + std::chrono::seconds(p[1]);
       l = 8;
       break;
 
@@ -1041,7 +1039,7 @@ conv_dir(std::vector<DIRENTRY> &flights, const uint8_t *p, const size_t length)
 
   int olddate_flg = 0;
   int flight_of_day = 0;
-  long temptime;
+  std::chrono::seconds temptime;
   BrokenDateTime datetime1;
 
   int bfv = 0;
@@ -1178,9 +1176,9 @@ conv_dir(std::vector<DIRENTRY> &flights, const uint8_t *p, const size_t length)
         return false;
 
       // speichert in timetm1 den aktuellen tnd-DS ab
-      temptime = 65536L * p[2] + 256L * p[3] + p[4];
+      temptime = std::chrono::seconds{65536L * p[2] + 256L * p[3] + p[4]};
       datetime1 = BrokenDateTime(BrokenDate::Invalid(),
-                                 BrokenTime::FromSecondOfDay(temptime));
+                                 BrokenTime::FromSinceMidnight(temptime));
       datetime1.day = 10 * (p[7] >> 4) + (p[7] & 0x0f);
       datetime1.month = 10 * (p[6] >> 4) + (p[6] & 0x0f);
       datetime1.year = 10 * (p[5] >> 4) + (p[5] & 0x0f) + 1900;
@@ -1194,12 +1192,11 @@ conv_dir(std::vector<DIRENTRY> &flights, const uint8_t *p, const size_t length)
         return false;
 
       // setzt firsttime und lasttime aufgrund der Werte im sta-DS
-      temptime = 65536L * p[4] + 256L * p[5] + p[6]; // Aufzeichnungsbeginn
+      temptime = std::chrono::seconds{65536L * p[4] + 256L * p[5] + p[6]}; // Aufzeichnungsbeginn
       de.firsttime = datetime1 - temptime;
 
-      temptime = 65536L * p[1] + 256L * p[2] + p[3]; // Aufzeichnungsdauer
-      de.recordingtime = temptime;
-      de.lasttime=de.firsttime + temptime;
+      de.recordingtime = 65536L * p[1] + 256L * p[2] + p[3]; // Aufzeichnungsdauer
+      de.lasttime = de.firsttime + std::chrono::seconds(temptime);
 
       if (!olddate_flg) {
         olddate = de.firsttime;

src/Device/Simulator.cpp
@@ -58,7 +58,7 @@ Simulator::Touch(NMEAInfo &basic)
 
   basic.time_available.Update(basic.clock);
   basic.time += 1;
-  basic.date_time_utc = basic.date_time_utc + 1;
+  basic.date_time_utc = basic.date_time_utc + std::chrono::seconds{1};
 }
 
 void

src/FlightInfo.cpp
@@ -24,20 +24,22 @@ Copyright_License {
 #include "FlightInfo.hpp"
 #include "time/BrokenDateTime.hpp"
 
-int FlightInfo::Duration() const {
+std::chrono::system_clock::duration
+FlightInfo::Duration() const noexcept
+{
   if (!date.IsPlausible() ||
       !start_time.IsPlausible() || !end_time.IsPlausible())
-    return -1;
+    return std::chrono::seconds{-1};
 
-  int secs = BrokenDateTime(date, end_time) - BrokenDateTime(date, start_time);
+  auto duration = BrokenDateTime(date, end_time) - BrokenDateTime(date, start_time);
 
   // adjust for possible date advance between start and end (add one day)
-  if (secs < 0)
-    secs += 24 * 60 * 60;
+  if (duration.count() < 0)
+    duration += std::chrono::hours(24);
 
   // if still not a plausible duration return invalid duration
-  if (secs < 0 || secs > 14 * 60 * 60)
-    return -1;
+  if (duration.count() < 0 || duration > std::chrono::hours(14))
+    return std::chrono::seconds{-1};
 
-  return secs;
+  return duration;
 }

src/FlightInfo.hpp
@@ -32,7 +32,7 @@ struct FlightInfo {
 
   BrokenTime start_time, end_time;
 
-  int Duration() const;
+  std::chrono::system_clock::duration Duration() const noexcept;
 };
 
 #endif

src/InfoBoxes/Content/Task.cpp
@@ -268,7 +268,7 @@ UpdateInfoBoxNextETA(InfoBoxData &data)
   }
 
   const BrokenTime t = now_local +
-    unsigned(task_stats.current_leg.solution_remaining.time_elapsed);
+    std::chrono::seconds(long(task_stats.current_leg.solution_remaining.time_elapsed));
 
   // Set Value
   data.UnsafeFormatValue(_T("%02u:%02u"), t.hour, t.minute);
@@ -416,7 +416,7 @@ UpdateInfoBoxFinalETA(InfoBoxData &data)
   }
 
   const BrokenTime t = now_local +
-    unsigned(task_stats.total.solution_remaining.time_elapsed);
+    std::chrono::seconds(long(task_stats.total.solution_remaining.time_elapsed));
 
   // Set Value
   data.UnsafeFormatValue(_T("%02u:%02u"), t.hour, t.minute);
@@ -736,9 +736,9 @@ UpdateInfoBoxNextETAVMG(InfoBoxData &data)
     return;
   }
 
-  const int dd = (int)(d/v);
   const BrokenTime &now_local = CommonInterface::Calculated().date_time_local;
   if (now_local.IsPlausible()) {
+    const std::chrono::seconds dd{long(d/v)};
     const BrokenTime t = now_local + dd;
     data.UnsafeFormatValue(_T("%02u:%02u"), t.hour, t.minute);
     data.UnsafeFormatComment(_T("%02u"), t.second);

commit e57b03f88f9f940747cfccc40f0b8d9747e66d87

time/BrokenTime: pass parameters by value to `[[const]]` functions
At first, https://github.com/XCSoar/XCSoar/issues/1258 (durations all
"00:00" on Kobo/PowerOff) looked like a compiler bug, but it is caused
by a misplaced `[[gnu::const]]` attribute on BrokenTime
FromSinceMidnight() which receives a reference parameter.

This tells the compiler that the parameter value is enough to
calculate the return value, and no global state is used; in this case,
"global state" includes the dereferenced value of the reference
parameter.  Therefore, GCC figures that it only needs to reserve stack
space (so it can pass an address to the function), but is allowed to
optimize away the value calculation.

Very clever of GCC! (And not very clever of me to add [[gnu::const]]
to a function that receives a reference). This optimization is new in
GCC 13, and the bug became visible when we switched the Kobo build to
GCC 13 (commit c72c3b1274444).  It was really caused by commit
7425dffaeb8f0e

Closes https://github.com/XCSoar/XCSoar/issues/1258

src/time/BrokenTime.hpp
@@ -102,7 +102,7 @@ struct BrokenTime {
    */
   template<class Rep, class Period>
   [[gnu::const]]
-  static BrokenTime FromSinceMidnight(const std::chrono::duration<Rep,Period> &since_midnight) noexcept {
+  static BrokenTime FromSinceMidnight(const std::chrono::duration<Rep,Period> since_midnight) noexcept {
     return FromSecondOfDay(std::chrono::duration_cast<std::chrono::seconds>(since_midnight).count());
   }
 
@@ -119,7 +119,7 @@ struct BrokenTime {
    */
   template<class Rep, class Period>
   [[gnu::const]]
-  static BrokenTime FromSinceMidnightChecked(const std::chrono::duration<Rep,Period> &since_midnight) noexcept {
+  static BrokenTime FromSinceMidnightChecked(const std::chrono::duration<Rep,Period> since_midnight) noexcept {
     return FromSecondOfDayChecked(std::chrono::duration_cast<std::chrono::seconds>(since_midnight).count());
   }
 