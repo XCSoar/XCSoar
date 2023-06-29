XCSoar/XCSoar

commit 2c60ef55f22f76d9b4c83a6b90f2012e6a05ed72

Renderer/FlightListRenderer: do not clear list in Draw()
Allow reusing the object.

src/Renderer/FlightListRenderer.cpp
@@ -44,8 +44,10 @@ FlightListRenderer::Draw(Canvas &canvas, PixelRect rc)
   int y = rc.bottom - row_height * 2;
   canvas.Select(font);
 
-  while (!flights.empty() && y > (int) rc.top + (int) header_height + (int) row_height) {
-    const FlightInfo flight = flights.pop();
+  for (auto i = flights.end();
+       i != flights.begin() && y > int(rc.top + header_height + row_height);
+       --i) {
+    const auto &flight = *i;
     int x = rc.left + padding;
 
     StaticString<64> buffer;

commit 67da82fde5fb243f2a4d0d2e0a53fab9b1d3b145

Renderer/FlightList: fix off-by-one bug in `for` condition
Regression by commit 2c60ef55f22f76d9b4c83a6b90f2012e6a05ed72

src/Renderer/FlightListRenderer.cpp
@@ -45,8 +45,7 @@ FlightListRenderer::Draw(Canvas &canvas, PixelRect rc)
   canvas.Select(font);
 
   for (auto i = flights.end();
-       i != flights.begin() && y > int(rc.top + header_height + row_height);
-       --i) {
+       i-- != flights.begin() && y > int(rc.top + header_height + row_height);) {
     const auto &flight = *i;
     int x = rc.left + padding;
 