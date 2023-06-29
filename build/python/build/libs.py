40566c6a1c4ff3de1adb2513b33d160f1a050886
Max Kellermann <max.kellermann@gmail.com>
2023-06-29 21:21:19 +0200

    python/build/libs.py: update libpng to 1.6.40

--- build/python/build/libs.py
+++ build/python/build/libs.py
@@ -260,9 +260,9 @@ proj = CmakeProject(
 )
 
 libpng = CmakeProject(
-    'ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.39.tar.xz',
-    'http://downloads.sourceforge.net/project/libpng/libpng16/1.6.39/libpng-1.6.39.tar.xz',
-    '1f4696ce70b4ee5f85f1e1623dc1229b210029fa4b7aee573df3e2ba7b036937',
+    'ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.40.tar.xz',
+    'http://downloads.sourceforge.net/project/libpng/libpng16/1.6.40/libpng-1.6.40.tar.xz',
+    '535b479b2467ff231a3ec6d92a525906fb8ef27978be4f66dbe05d3f3a01b3a1',
     'lib/libpng.a',
     [
         '-DPNG_SHARED=OFF',
