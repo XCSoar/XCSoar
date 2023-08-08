1cf124c00f465e92353928e29daeaf4924645ba8
August2111 <Info@FlapsOnline.de>
2023-08-08 22:53:04 +0200

    PROGRAM_VERSION - use the value from OpenSoar.config

--- build/version.mk
+++ build/version.mk
@@ -1,10 +1,19 @@
-VERSION = $(strip $(shell cat $(topdir)/VERSION.txt))
-FULL_VERSION = $(VERSION)
+CONFIG = $(topdir)/OpenSoar.config
+include $(CONFIG)
 
-VERSION_CPPFLAGS = -DPROGRAM_VERSION=\"$(VERSION)\"
+# w/o VERSION.txt:
+ifeq ($(PROGRAM_VERSION),"")
+	PROGRAM_VERSION = $(strip $(shell cat $(topdir)/VERSION.txt))
+endif
+
+### echo "$(PROGRAM_VERSION)"
+
+FULL_VERSION = $(PROGRAM_VERSION)
+
+VERSION_CPPFLAGS = -DPROGRAM_VERSION=\"$(PROGRAM_VERSION)\"
 
 GIT_COMMIT_ID := $(shell git rev-parse --short --verify HEAD 2>$(NUL))
-RELEASE_COMMIT_ID := $(shell git rev-parse --short --verify "v$(VERSION)^{commit}" 2>$(NUL))
+RELEASE_COMMIT_ID := $(shell git rev-parse --short --verify "v$(PROGRAM_VERSION)^{commit}" 2>$(NUL))
 # only append the commit id for unreleased builds (no release tag)
 ifneq ($(GIT_COMMIT_ID),$(RELEASE_COMMIT_ID))
 VERSION_CPPFLAGS += -DGIT_COMMIT_ID=\"$(GIT_COMMIT_ID)\"
