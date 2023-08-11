2cc9430c2f3b45657d72990e92fff192e33c049d
August2111 <Info@FlapsOnline.de>
2023-08-11 16:52:05 +0200

    build/cmake/MakeAll.sh - organize the build of all targets ANDROID, WIN64 and UNIX
    in the this order, set the PROGRAM_VERSION from OpenSoar.config directly

--- build/cmake/MakeAll.sh
+++ build/cmake/MakeAll.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 if [ "$COMPLETE" == "" ]; then COMPLETE=y ; fi
-rm -rv output/data/temp/graphics/*
+# rm -rv output/data/temp/graphics/*
 
 #delete all old Android packages:
+
+BIN_NAME=output/ANDROID/bin/OpenSoar-unsigned.apk
 rm -rv output/ANDROID/opt
+rm -v  $BIN_NAME
 
 echo "============================================================================="
 echo "Make Android v8a (64 bit):"
 if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/arm64-v8a/opt/src ; fi
-make DEBUG=n TARGET=ANDROIDAARCH64
-cp -v output/ANDROID/bin/OpenSoar-unsigned.apk output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}-64.apk 
+# else rm -rv output/ANDROID/arm64-v8a/opt/src/Version.* ; fi
+# rm -rv output/include/ProgramVersion.*
 
+make DEBUG=n TARGET=ANDROIDAARCH64
+
+if [ -e $BIN_NAME ]; then    # exit ; fi
+cp -v $BIN_NAME output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}-64.apk 
+
+if [ ! "$ANDROID_ARM64_ONLY" == "y" ]; then
 echo "============================================================================="
 echo "Make Android v7a (32 bit):"
 if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/armeabi-v7a/opt/src ; fi
@@ -27,7 +36,7 @@ echo "Make Android x86:"
 if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/x86/opt/src ; fi
 make DEBUG=n TARGET=ANDROID86
 
-cp -v output/ANDROID/bin/OpenSoar-unsigned.apk output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}.apk 
+cp -v $BIN_NAME output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}.apk 
 
 if [ ! "$ANDROID_ONLY" == "y" ]
 then 
@@ -42,4 +51,12 @@ if [ "$COMPLETE" == "y" ]; then rm -rv output/UNIX/opt/src ; fi
 make DEBUG=n TARGET=UNIX
 cp -v output/UNIX/bin/OpenSoar output/UNIX/bin/OpenSoar-${PROGRAM_VERSION} 
 
+# ANDROID_ONLY:
+fi
+
+else
+echo "'OpenSoar-unsigned.apk' not available!" 
+fi
+
+# ANDROID_ARM64_ONLY:
 fi
