f9c6b7e68e1e213ac8175ca64d6c43d975252921
Max Kellermann <max.kellermann@gmail.com>
2023-09-26 14:28:50 +0200

    build/python/zlib: add missing Python type hint

--- build/python/build/zlib.py
+++ build/python/build/zlib.py
@@ -5,7 +5,7 @@ from build.makeproject import MakeProject
 from .toolchain import AnyToolchain
 
 class ZlibProject(MakeProject):
-    def __init__(self, url: Union[str, Sequence[str]], md5: str, installed,
+    def __init__(self, url: Union[str, Sequence[str]], md5: str, installed: str,
                  **kwargs):
         MakeProject.__init__(self, url, md5, installed, **kwargs)
 
