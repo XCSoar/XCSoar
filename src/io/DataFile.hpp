271f50e163be4c190ba0ea27b6d726b5e81aa4b7
Max Kellermann <max.kellermann@gmail.com>
2023-10-08 16:09:23 +0200

    io/DataFile: remove unused function OpenDataTextFile()

--- src/io/DataFile.hpp
+++ src/io/DataFile.hpp
@@ -3,14 +3,11 @@
 
 #pragma once
 
-#include "Charset.hpp"
-
 #include <memory>
+
 #include <tchar.h>
 
 class Reader;
-class TLineReader;
-class NLineReader;
 
 /**
  * Opens a file from the data directory.
@@ -21,24 +18,3 @@ class NLineReader;
  */
 std::unique_ptr<Reader>
 OpenDataFile(const TCHAR *name);
-
-/**
- * Opens a text file from the data directory.
- *
- * Throws exception on error.
- *
- * @param name the file name relative to the data directory
- * @param cs the character set of the input file
- */
-std::unique_ptr<TLineReader>
-OpenDataTextFile(const TCHAR *name, Charset cs=Charset::UTF8);
-
-/**
- * Opens a text file from the data directory.
- *
- * Throws exception on error.
- *
- * @param name the file name relative to the data directory
- */
-std::unique_ptr<NLineReader>
-OpenDataTextFileA(const TCHAR *name);
