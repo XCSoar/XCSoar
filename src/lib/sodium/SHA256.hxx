c828b250881a3433b945bf13d14919a89ad5f7a8
Max Kellermann <mk@cm4all.com>
2023-10-26 12:54:13 +0200

    lib/sodium/SHA256: remove std::string_view overloads and others
    Reduces header dependencies; it's easier to just use std::as_bytes()
    or AsBytes() in the caller.

--- src/lib/sodium/SHA256.hxx
+++ src/lib/sodium/SHA256.hxx
@@ -6,8 +6,8 @@
 #include <sodium/crypto_hash_sha256.h>
 
 #include <array>
+#include <cstddef>
 #include <span>
-#include <string_view>
 
 using SHA256DigestBuffer = std::array<std::byte, crypto_hash_sha256_BYTES>;
 using SHA256DigestView = std::span<const std::byte, crypto_hash_sha256_BYTES>;
@@ -24,21 +24,6 @@ public:
 		crypto_hash_sha256_update(&state,
 					  reinterpret_cast<const unsigned char *>(p.data()),
 					  p.size());
-	}
-
-	template<typename T>
-	void Update(std::span<const T> src) noexcept {
-		Update(std::as_bytes(src));
-	}
-
-	void Update(std::string_view src) noexcept {
-		Update(std::span<const char>{src});
-	}
-
-	template<typename T>
-	void UpdateT(const T &p) noexcept {
-		const std::span<const T> span{&p, 1};
-		Update(span);
 	}
 
 	void Final(std::span<std::byte, crypto_hash_sha256_BYTES> out) noexcept {
@@ -62,19 +47,4 @@ SHA256(std::span<const std::byte> src) noexcept
 			   reinterpret_cast<const unsigned char *>(src.data()),
 			   src.size());
 	return out;
-}
-
-template<typename T>
-[[gnu::pure]]
-inline auto
-SHA256(std::span<const T> src) noexcept
-{
-	return SHA256(std::as_bytes(src));
-}
-
-[[gnu::pure]]
-inline auto
-SHA256(std::string_view src) noexcept
-{
-	return SHA256(std::span<const char>{src});
 }
