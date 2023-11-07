2364fa1748a5be8bf285aa8c01a852a7b8c78a6e
August2111 <Info@FlapsOnline.de>
2023-11-07 19:52:40 +0100

    MakeLinux..sh - use branch opensoar-dev instead of dev-branch

--- build/cmake/MakeLinux.sh
+++ build/cmake/MakeLinux.sh
@@ -3,7 +3,8 @@
 git fetch OpenSoaring_OpenSoar
 # git reset --hard august/weglide-tmp 
 # git reset --hard flaps3/cmake
-git reset --hard OpenSoaring_OpenSoar/dev-branch
+git reset --hard OpenSoaring_OpenSoar/opensoar-dev
+# git reset --hard OpenSoaring_OpenSoar/dev-branch
 sudo chmod 757 -R ./build/cmake
 make TARGET=UNIX DEBUG=n
 output/UNIX/bin/xcsoar -fly -1000x500
