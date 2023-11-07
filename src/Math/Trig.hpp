534d13d083b4814172ba422d88f0d187498871b0
August2111 <Info@FlapsOnline.de>
2023-11-07 11:09:39 +0100

    Math/Trig.hpp - use define __MSVC__ instead of _MSC_VER

--- src/Math/Trig.hpp
+++ src/Math/Trig.hpp
@@ -13,7 +13,7 @@ sin_cos(const double thetha) noexcept {
   double s, c;
 #ifdef __APPLE__
   __sincos(thetha, &s, &c);
-#elif defined(_MSC_VER)
+#elif defined(__MSVC__)
   // STL and MSVC have no sincos...
   s = sin(thetha);
   c = cos(thetha);
