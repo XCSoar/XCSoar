22a04b42d4880905d9630d0a8a4d33c522d0d7b2
August2111 <Info@FlapsOnline.de>
2023-12-05 17:00:40 +0100

    Revert "Update util/AllocatedGrid.hxx"
    This reverts commit 97feb035a0ff4fd00b9b1ea3fbfb98d928ef32c8.

--- src/util/AllocatedGrid.hxx
+++ src/util/AllocatedGrid.hxx
@@ -139,21 +139,14 @@ public:
 		if (_width > width) {
 			const unsigned delta_w = _width - width;
 			const auto end = array.begin();
-                        /*/
+
 			for (auto in = array.begin() + (h - 1) * width,
 				     out = array.begin() + (h - 1) * _width + width;
 			     in > end; in -= width, out -= delta_w) {
 				out = std::move_backward(in, in + width, out);
 				std::fill(out - delta_w, out, fill);
 			}
-/*/
-			for (auto in = array.begin() + (h) * width,
-				     out = array.begin() + (h) * _width + width;
-			     in > end; in -= width, out -= delta_w) {
-				out = std::move_backward(in, in + width, out);
-				std::fill(out - delta_w, out, fill);
-			}
-                        /**/
+
 			width = _width;
 		}
 
