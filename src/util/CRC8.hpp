75f95ad7e077d31ff5dde9003b86d36d962a7e15
August2111 <Info@FlapsOnline.de>
2023-12-07 11:37:12 +0100

    CRC8 - on windows std::byte has no operators <==, ^ and ^= yet

--- src/util/CRC8.hpp
+++ src/util/CRC8.hpp
@@ -6,9 +6,27 @@
 #include <cstdint>
 #include <span>
 
+#ifndef HAVE_POSIX
+// on Windows the operators <<=, ^= and ^ not allowed yet for std::byte
+constexpr uint8_t
+UpdateCRC8(uint8_t d, uint8_t crc) noexcept
+{
+  constexpr uint8_t poly{0x69};
+
+  for (int count = 8; --count >= 0; d <<= 1) {
+    uint8_t tmp = crc ^ d;
+    crc <<= 1;
+    if ((tmp & 0x80) != 0)
+      crc ^= poly;
+  }
+  return crc;
+}
+#endif
+
 constexpr std::byte
 UpdateCRC8(std::byte d, std::byte crc) noexcept
 {
+#ifdef HAVE_POSIX
   constexpr std::byte poly{0x69};
 
   for (int count = 8; --count >= 0; d <<= 1) {
@@ -17,8 +35,10 @@ UpdateCRC8(std::byte d, std::byte crc) noexcept
     if ((tmp & std::byte{0x80}) != std::byte{})
       crc ^= poly;
   }
-
   return crc;
+#else
+  return std::byte{UpdateCRC8((uint8_t)d, (uint8_t)crc)};
+#endif
 }
 
 constexpr std::byte
