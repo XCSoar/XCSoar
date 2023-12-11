a4b7c7ec3ba86f3ff947977f09a539a3d6263b34
August2111 <Info@FlapsOnline.de>
2023-12-11 18:20:06 +0100

    cmake/MakeAll.sh - remove obsolet blanks

--- build/cmake/MakeAll.sh
+++ build/cmake/MakeAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$COMPLETE" == "" ]; then COMPLETE=y ; fi
+if [ "$COMPLETE" == "" ]; then COMPLETE=y; fi
 
 BIN_NAME=output/ANDROID/bin/OpenSoar-unsigned.apk
 
@@ -10,30 +10,30 @@ rm -v  $BIN_NAME
 
 echo "============================================================================="
 echo "Make Android v8a (64 bit):"
-if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/arm64-v8a/opt/src ; fi
+if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/arm64-v8a/opt/src; fi
 
 make DEBUG=n TARGET=ANDROIDAARCH64
 echo "Android arm64-v8a ready!"
 
 if [ -e $BIN_NAME ]; then
-if [ "$EXPORT_ANDROID_ARM64" == "y" ]; then cp -v $BIN_NAME output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}-64.apk ; fi 
+if [ "$EXPORT_ANDROID_ARM64" == "y" ]; then cp -v $BIN_NAME output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}-64.apk; fi 
 
 if [ ! "$ANDROID_ARM64_ONLY" == "y" ]; then
 echo "============================================================================="
 echo "Make Android v7a (32 bit):"
-if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/armeabi-v7a/opt/src ; fi
+if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/armeabi-v7a/opt/src; fi
 make DEBUG=n TARGET=ANDROID
 echo "Android armeabi_v7a ready!"
 
 echo "============================================================================="
 echo "Make Android x64:"
-if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/x86_64/opt/src ; fi
+if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/x86_64/opt/src; fi
 make DEBUG=n TARGET=ANDROIDX64
 echo "Android x86_64 ready!"
 
 echo "============================================================================="
 echo "Make Android x86:"
-if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/x86/opt/src ; fi
+if [ "$COMPLETE" == "y" ]; then rm -rv output/ANDROID/x86/opt/src; fi
 make DEBUG=n TARGET=ANDROID86
 echo "Android x86 ready!"
 
@@ -42,13 +42,13 @@ cp -v $BIN_NAME output/ANDROID/bin/OpenSoar-${PROGRAM_VERSION}.apk
 if [ ! "$ANDROID_ONLY" == "y" ]; then 
 echo "============================================================================="
 echo "Make Win64:"
-if [ "$COMPLETE" == "y" ]; then rm -rv output/WIN64/opt/src ; fi
+if [ "$COMPLETE" == "y" ]; then rm -rv output/WIN64/opt/src; fi
 make DEBUG=n TARGET=WIN64
 echo "Win64 ready!"
 cp -v output/WIN64/bin/OpenSoar.exe output/WIN64/bin/OpenSoar-${PROGRAM_VERSION}.exe 
 echo "============================================================================="
 echo "Make Linux:"
-if [ "$COMPLETE" == "y" ]; then rm -rv output/UNIX/opt/src ; fi
+if [ "$COMPLETE" == "y" ]; then rm -rv output/UNIX/opt/src; fi
 make DEBUG=n TARGET=UNIX
 echo "UNIX ready!"
 cp -v output/UNIX/bin/OpenSoar output/UNIX/bin/OpenSoar-${PROGRAM_VERSION} 
