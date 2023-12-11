4d484a75acea5dce2656c83c04796d41b7435b3f
August2111 <Info@FlapsOnline.de>
2023-12-11 23:33:35 +0100

    build/version.mk - read the git commit from git directly, remove error in pre- vious version

--- build/version.mk
+++ build/version.mk
@@ -3,21 +3,30 @@ include $(CONFIG)
 
 # w/o VERSION.txt:
 ifeq ($(PROGRAM_VERSION),"")
-	PROGRAM_VERSION = $(strip $(shell cat $(topdir)/VERSION.txt))
+    # take the version from XCSoar VERSION.txt
+    PROGRAM_VERSION = $(strip $(shell cat $(topdir)/VERSION.txt))
 endif
 
-### echo "$(PROGRAM_VERSION)"
+$(info PROGRAM_NAME      is $(PROGRAM_NAME))
+$(info PROGRAM_VERSION   is $(PROGRAM_VERSION))
 
 FULL_VERSION = $(PROGRAM_VERSION)
 
 VERSION_CPPFLAGS = -DPROGRAM_VERSION=\"$(PROGRAM_VERSION)\"
 
-GIT_COMMIT_ID := $(shell git rev-parse --short --verify HEAD 2>$(NUL))
-RELEASE_COMMIT_ID := $(shell git rev-parse --short --verify "v$(PROGRAM_VERSION)^{commit}" 2>$(NUL))
+GIT_COMMIT_ID := $(shell git rev-parse --short --verify HEAD )
+GIT_TAG=tags/opensoar-$(PROGRAM_VERSION)
+$(info GIT_TAG           is $(GIT_TAG) )
+RELEASE_COMMIT_ID := $(shell git rev-parse --short --verify $(GIT_TAG)^0 )
+
+$(info GIT_COMMIT_ID     is $(GIT_COMMIT_ID) )
+$(info RELEASE_COMMIT_ID is $(RELEASE_COMMIT_ID) )
+
 # only append the commit id for unreleased builds (no release tag)
-ifneq ($(GIT_COMMIT_ID),$(RELEASE_COMMIT_ID))
-VERSION_CPPFLAGS += -DGIT_COMMIT_ID=\"$(GIT_COMMIT_ID)\"
-FULL_VERSION := $(FULL_VERSION)~$(GIT_COMMIT_ID)
+ifneq ($(GIT_COMMIT_ID),$(RELEASE_COMMIT_ID) )
+    $(info Git commits: HEAD = $(GIT_COMMIT_ID) vs. RELEASE = $(RELEASE_COMMIT_ID) )
+    VERSION_CPPFLAGS += -DGIT_COMMIT_ID=\"$(GIT_COMMIT_ID)\"
+    FULL_VERSION := $(FULL_VERSION)~$(GIT_COMMIT_ID)
 endif
 
 $(call SRC_TO_OBJ,$(SRC)/Version.cpp): $(topdir)/VERSION.txt
