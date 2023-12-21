2cde11ea913fbad6c18d8c6e2ae356dd143bd7fb
Max Kellermann <max.kellermann@gmail.com>
2023-12-21 17:36:42 +0100

    util/CharUtil: add IsLowerHexDigit()

--- src/util/CharUtil.hxx
+++ src/util/CharUtil.hxx
@@ -127,9 +127,15 @@ ToLowerASCII(char ch) noexcept
 }
 
 constexpr bool
-IsHexDigit(char ch) noexcept
+IsLowerHexDigit(char ch) noexcept
 {
 	return IsDigitASCII(ch) ||
-		(ch >= 'a' && ch <= 'f') ||
+		(ch >= 'a' && ch <= 'f');
+}
+
+constexpr bool
+IsHexDigit(char ch) noexcept
+{
+	return IsLowerHexDigit(ch) ||
 		(ch >= 'A' && ch <= 'F');
 }
