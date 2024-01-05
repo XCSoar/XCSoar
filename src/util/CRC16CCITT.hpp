8a827c2025e8ff6fdfa6141982c7cc8c50181d31
Max Kellermann <max.kellermann@gmail.com>
2024-01-05 14:03:05 +0100

    util/CRC16CCITT: add `noexcept`

--- src/util/CRC16CCITT.hpp
+++ src/util/CRC16CCITT.hpp
@@ -10,14 +10,14 @@ extern const uint16_t crc16ccitt_table[256];
 
 [[gnu::const]]
 static inline uint16_t
-UpdateCRC16CCITT(uint8_t octet, uint16_t crc)
+UpdateCRC16CCITT(uint8_t octet, uint16_t crc) noexcept
 {
   return (crc << 8) ^ crc16ccitt_table[(crc >> 8) ^ octet];
 }
 
 [[gnu::pure]]
 static inline uint16_t
-UpdateCRC16CCITT(const uint8_t *data, const uint8_t *end, uint16_t crc)
+UpdateCRC16CCITT(const uint8_t *data, const uint8_t *end, uint16_t crc) noexcept
 {
   while (data < end)
     crc = UpdateCRC16CCITT(*data++, crc);
@@ -26,7 +26,7 @@ UpdateCRC16CCITT(const uint8_t *data, const uint8_t *end, uint16_t crc)
 
 [[gnu::pure]]
 static inline uint16_t
-UpdateCRC16CCITT(const void *data, size_t length, uint16_t crc)
+UpdateCRC16CCITT(const void *data, size_t length, uint16_t crc) noexcept
 {
   const uint8_t *p = (const uint8_t *)data, *end = p + length;
   return UpdateCRC16CCITT(p, end, crc);
