5bd4f47bf87931ce9ca3e217d83cc70dc08b1cbe
Andreas Lüthi <andreas.luethi@gmx.net>
2024-02-20 12:44:51 +0100

    Airspace/AirspaceParser: ignore unit for radio frequency and assume it is always in MHz
    This is necessary because airspace files (OpenAir and tnp) may contain frequencies both
    with and without units. Prior to commit 903981c7a9e9e829ef06f755b2ceaf778c307586, the
    unit was disregarded by the now removed 'Parse(const TCHAR *p)' function.

--- src/Airspace/AirspaceParser.cpp
+++ src/Airspace/AirspaceParser.cpp
@@ -21,6 +21,7 @@
 #include "util/ConvertString.hpp"
 #include "util/StaticString.hxx"
 #include "util/StringCompare.hxx"
+#include "util/StringSplit.hxx"
 
 #include <stdexcept>
 
@@ -570,6 +571,14 @@ ParseType(const char *buffer) noexcept
   return OTHER;
 }
 
+[[gnu::pure]]
+static std::string_view
+ReadRadioFrequency(const std::string_view line) noexcept
+{
+  const auto [frq, _] = Split(line, ' ');
+  return frq;
+}
+
 /**
  * Throws on error.
  */
@@ -667,7 +676,7 @@ ParseLine(Airspaces &airspace_database, unsigned line_number,
     case 'F':
     case 'f':
       if (input.SkipWhitespace())
-        temp_area.radio_frequency = RadioFrequency::Parse(input.c_str());
+        temp_area.radio_frequency = RadioFrequency::Parse(ReadRadioFrequency(input.c_str()));
       break;
     }
 
@@ -889,7 +898,7 @@ ParseLineTNP(Airspaces &airspace_database, unsigned line_number,
   } else if (input.SkipMatchIgnoreCase("BASE="sv)) {
     temp_area.base = ReadAltitude(input);
   } else if (input.SkipMatchIgnoreCase("RADIO="sv)) {
-    temp_area.radio_frequency = RadioFrequency::Parse(input.c_str());
+    temp_area.radio_frequency = RadioFrequency::Parse(ReadRadioFrequency(input.c_str()));
   } else if (input.SkipMatchIgnoreCase("ACTIVE="sv)) {
     if (input.MatchAllIgnoreCase("WEEKEND"))
       temp_area.days_of_operation.SetWeekend();
