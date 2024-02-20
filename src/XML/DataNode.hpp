XCSoar/XCSoar

commit 4d709b0b60e875fdd9bd69d463b859bb98f78bda

XML/Node: store attribute values as `char` string, not `TCHAR`

src/XML/DataNode.hpp
@@ -65,7 +65,7 @@ public:
    *
    * @return the value or nullptr if it does not exist
    */
-  virtual const TCHAR *GetAttribute(const char *name) const noexcept = 0;
+  virtual const char *GetAttribute(const char *name) const noexcept = 0;
 
   /**
    * Retrieve named attribute value, with numeric conversion
@@ -148,7 +148,7 @@ public:
    * @param name Name of attribute
    * @param value Value of attribute
    */
-  virtual void SetAttribute(const char *name, const TCHAR *value) noexcept = 0;
+  virtual void SetAttribute(const char *name, const char *value) noexcept = 0;
 
   /**
    * Set named attribute value, with numeric to text conversion

src/Task/Deserialiser.cpp
@@ -14,6 +14,7 @@
 #include "Task/Factory/AbstractTaskFactory.hpp"
 #include "XML/DataNode.hpp"
 #include "Engine/Waypoint/Waypoints.hpp"
+#include "util/ConvertString.hpp"
 
 #include <memory>
 
@@ -34,14 +35,16 @@ DeserialiseWaypoint(const ConstDataNode &node, const Waypoints *waypoints)
   GeoPoint loc;
   Deserialise(loc, *loc_node);
 
-  const TCHAR *name = node.GetAttribute("name");
-  if (name == nullptr)
+  const char *_name = node.GetAttribute("name");
+  if (_name == nullptr)
     // Turnpoints need names
     return nullptr;
 
+  const UTF8ToWideConverter name{_name};
+
   if (waypoints != nullptr) {
     // Try to find waypoint by name
-    auto from_database = waypoints->LookupName(name);
+    auto from_database = waypoints->LookupName(name.c_str());
 
     // If waypoint by name found and closer than 10m to the original
     if (from_database != nullptr &&
@@ -65,9 +68,9 @@ DeserialiseWaypoint(const ConstDataNode &node, const Waypoints *waypoints)
 
   node.GetAttribute("id", wp->id);
 
-  const TCHAR *comment = node.GetAttribute("comment");
+  const char *comment = node.GetAttribute("comment");
   if (comment != nullptr)
-    wp->comment.assign(comment);
+    wp->comment = UTF8ToWideConverter{comment};
 
   if (node.GetAttribute("altitude", wp->elevation))
     wp->has_elevation = true;
@@ -78,11 +81,11 @@ DeserialiseWaypoint(const ConstDataNode &node, const Waypoints *waypoints)
 static std::unique_ptr<ObservationZonePoint>
 DeserialiseOZ(const Waypoint &wp, const ConstDataNode &node, bool is_turnpoint)
 {
-  const TCHAR *type = node.GetAttribute("type");
+  const char *type = node.GetAttribute("type");
   if (type == nullptr)
     return nullptr;
 
-  if (StringIsEqual(type, _T("Line"))) {
+  if (StringIsEqual(type, "Line")) {
     auto ls = std::make_unique<LineSectorZone>(wp.location);
 
     double length;
@@ -90,7 +93,7 @@ DeserialiseOZ(const Waypoint &wp, const ConstDataNode &node, bool is_turnpoint)
       ls->SetLength(length);
 
     return ls;
-  } else if (StringIsEqual(type, _T("Cylinder"))) {
+  } else if (StringIsEqual(type, "Cylinder")) {
     auto ls = std::make_unique<CylinderZone>(wp.location);
 
     double radius;
@@ -98,9 +101,9 @@ DeserialiseOZ(const Waypoint &wp, const ConstDataNode &node, bool is_turnpoint)
       ls->SetRadius(radius);
 
     return ls;
-  } else if (StringIsEqual(type, _T("MatCylinder"))) {
+  } else if (StringIsEqual(type, "MatCylinder")) {
     return CylinderZone::CreateMatCylinderZone(wp.location);
-  } else if (StringIsEqual(type, _T("Sector"))) {
+  } else if (StringIsEqual(type, "Sector")) {
 
     double radius, inner_radius;
     Angle start, end;
@@ -121,16 +124,16 @@ DeserialiseOZ(const Waypoint &wp, const ConstDataNode &node, bool is_turnpoint)
       ls->SetEndRadial(end);
 
     return ls;
-  } else if (StringIsEqual(type, _T("FAISector")))
+  } else if (StringIsEqual(type, "FAISector"))
     return SymmetricSectorZone::CreateFAISectorZone(wp.location, is_turnpoint);
-  else if (StringIsEqual(type, _T("SymmetricQuadrant"))) {
+  else if (StringIsEqual(type, "SymmetricQuadrant")) {
     double radius = 10000;
     node.GetAttribute("radius", radius);
 
     return std::make_unique<SymmetricSectorZone>(wp.location, radius);
-  } else if (StringIsEqual(type, _T("Keyhole")))
+  } else if (StringIsEqual(type, "Keyhole"))
     return KeyholeZone::CreateDAeCKeyholeZone(wp.location);
-  else if (StringIsEqual(type, _T("CustomKeyhole"))) {
+  else if (StringIsEqual(type, "CustomKeyhole")) {
     double radius = 10000, inner_radius = 500;
     Angle angle = Angle::QuarterCircle();
 
@@ -142,11 +145,11 @@ DeserialiseOZ(const Waypoint &wp, const ConstDataNode &node, bool is_turnpoint)
       KeyholeZone::CreateCustomKeyholeZone(wp.location, radius, angle);
     keyhole->SetInnerRadius(inner_radius);
     return keyhole;
-  } else if (StringIsEqual(type, _T("BGAStartSector")))
+  } else if (StringIsEqual(type, "BGAStartSector"))
     return KeyholeZone::CreateBGAStartSectorZone(wp.location);
-  else if (StringIsEqual(type, _T("BGAFixedCourse")))
+  else if (StringIsEqual(type, "BGAFixedCourse"))
     return KeyholeZone::CreateBGAFixedCourseZone(wp.location);
-  else if (StringIsEqual(type, _T("BGAEnhancedOption")))
+  else if (StringIsEqual(type, "BGAEnhancedOption"))
     return KeyholeZone::CreateBGAEnhancedOptionZone(wp.location);
 
   return nullptr;
@@ -156,7 +159,7 @@ static void
 DeserialiseTaskpoint(AbstractTaskFactory &fact, const ConstDataNode &node,
                      const Waypoints *waypoints)
 {
-  const TCHAR *type = node.GetAttribute("type");
+  const char *type = node.GetAttribute("type");
   if (type == nullptr)
     return;
 
@@ -172,18 +175,18 @@ DeserialiseTaskpoint(AbstractTaskFactory &fact, const ConstDataNode &node,
   std::unique_ptr<OrderedTaskPoint> pt;
 
   if (auto oz_node = node.GetChildNamed("ObservationZone")) {
-    bool is_turnpoint = StringIsEqual(type, _T("Turn")) ||
-      StringIsEqual(type, _T("Area"));
+    bool is_turnpoint = StringIsEqual(type, "Turn") ||
+      StringIsEqual(type, "Area");
 
     oz = DeserialiseOZ(*wp, *oz_node, is_turnpoint);
   }
 
-  if (StringIsEqual(type, _T("Start"))) {
+  if (StringIsEqual(type, "Start")) {
     pt = oz != nullptr
       ? fact.CreateStart(std::move(oz), std::move(wp))
       : fact.CreateStart(std::move(wp));
 
-  } else if (StringIsEqual(type, _T("OptionalStart"))) {
+  } else if (StringIsEqual(type, "OptionalStart")) {
     pt = oz != nullptr
       ? fact.CreateStart(std::move(oz), std::move(wp))
       : fact.CreateStart(std::move(wp));
@@ -192,17 +195,17 @@ DeserialiseTaskpoint(AbstractTaskFactory &fact, const ConstDataNode &node,
     // don't let generic code below add it
     pt.reset();
 
-  } else if (StringIsEqual(type, _T("Turn"))) {
+  } else if (StringIsEqual(type, "Turn")) {
     pt = oz != nullptr
       ? fact.CreateASTPoint(std::move(oz), std::move(wp))
       : fact.CreateIntermediate(std::move(wp));
 
-  } else if (StringIsEqual(type, _T("Area"))) {
+  } else if (StringIsEqual(type, "Area")) {
     pt = oz != nullptr
       ? fact.CreateAATPoint(std::move(oz), std::move(wp))
       : fact.CreateIntermediate(std::move(wp));
 
-  } else if (StringIsEqual(type, _T("Finish"))) {
+  } else if (StringIsEqual(type, "Finish")) {
     pt = oz != nullptr
       ? fact.CreateFinish(std::move(oz), std::move(wp))
       : fact.CreateFinish(std::move(wp));
@@ -225,11 +228,11 @@ static bool
 GetHeightRef(const ConstDataNode &node, const char *nodename,
              AltitudeReference &value) noexcept
 {
-  const TCHAR *type = node.GetAttribute(nodename);
+  const char *type = node.GetAttribute(nodename);
   if (type == nullptr) {
     return false;
   }
-  if (StringIsEqual(type, _T("MSL"))) {
+  if (StringIsEqual(type, "MSL")) {
     value = AltitudeReference::MSL;
   } else {
     value = AltitudeReference::AGL;
@@ -269,27 +272,27 @@ Deserialise(OrderedTaskSettings &data, const ConstDataNode &node)
 static TaskFactoryType
 GetTaskFactoryType(const ConstDataNode &node)
 {
-  const TCHAR *type = node.GetAttribute("type");
+  const char *type = node.GetAttribute("type");
   if (type == nullptr)
     return TaskFactoryType::FAI_GENERAL;
 
-  if (StringIsEqual(type, _T("FAIGeneral")))
+  if (StringIsEqual(type, "FAIGeneral"))
     return TaskFactoryType::FAI_GENERAL;
-  else if (StringIsEqual(type, _T("FAITriangle")))
+  else if (StringIsEqual(type, "FAITriangle"))
     return TaskFactoryType::FAI_TRIANGLE;
-  else if (StringIsEqual(type, _T("FAIOR")))
+  else if (StringIsEqual(type, "FAIOR"))
     return TaskFactoryType::FAI_OR;
-  else if (StringIsEqual(type, _T("FAIGoal")))
+  else if (StringIsEqual(type, "FAIGoal"))
     return TaskFactoryType::FAI_GOAL;
-  else if (StringIsEqual(type, _T("RT")))
+  else if (StringIsEqual(type, "RT"))
     return TaskFactoryType::RACING;
-  else if (StringIsEqual(type, _T("AAT")))
+  else if (StringIsEqual(type, "AAT"))
     return TaskFactoryType::AAT;
-  else if (StringIsEqual(type, _T("MAT")))
+  else if (StringIsEqual(type, "MAT"))
     return TaskFactoryType::MAT;
-  else if (StringIsEqual(type, _T("Mixed")))
+  else if (StringIsEqual(type, "Mixed"))
     return TaskFactoryType::MIXED;
-  else if (StringIsEqual(type, _T("Touring")))
+  else if (StringIsEqual(type, "Touring"))
     return TaskFactoryType::TOURING;
 
   return TaskFactoryType::FAI_GENERAL;

src/XML/DataNode.cpp
@@ -7,6 +7,8 @@
 #include "util/NumberParser.hpp"
 #include "time/RoughTime.hpp"
 
+#include <fmt/format.h>
+
 ConstDataNode::~ConstDataNode() noexcept
 {
 }
@@ -48,7 +50,7 @@ ConstDataNode::GetAttribute(const char *name,
 bool
 ConstDataNode::GetAttribute(const char *name, double &value) const noexcept
 {
-  const TCHAR *val = GetAttribute(name);
+  const char *val = GetAttribute(name);
   if (val == nullptr)
     return false;
 
@@ -59,7 +61,7 @@ ConstDataNode::GetAttribute(const char *name, double &value) const noexcept
 bool
 ConstDataNode::GetAttribute(const char *name, int &value) const noexcept
 {
-  const TCHAR *val = GetAttribute(name);
+  const char *val = GetAttribute(name);
   if (val == nullptr)
     return false;
 
@@ -70,7 +72,7 @@ ConstDataNode::GetAttribute(const char *name, int &value) const noexcept
 bool
 ConstDataNode::GetAttribute(const char *name, unsigned &value) const noexcept
 {
-  const TCHAR *val = GetAttribute(name);
+  const char *val = GetAttribute(name);
   if (val == nullptr)
     return false;
 
@@ -81,7 +83,7 @@ ConstDataNode::GetAttribute(const char *name, unsigned &value) const noexcept
 bool
 ConstDataNode::GetAttribute(const char *name, bool &value) const noexcept
 {
-  const TCHAR *val = GetAttribute(name);
+  const char *val = GetAttribute(name);
   if (val == nullptr)
     return false;
 
@@ -92,11 +94,11 @@ ConstDataNode::GetAttribute(const char *name, bool &value) const noexcept
 RoughTime
 ConstDataNode::GetAttributeRoughTime(const char *name) const noexcept
 {
-  const TCHAR *p = GetAttribute(name);
+  const char *p = GetAttribute(name);
   if (p == nullptr)
     return RoughTime::Invalid();
 
-  TCHAR *endptr;
+  char *endptr;
   unsigned hours = ParseUnsigned(p, &endptr, 10);
   if (endptr == p || *endptr != ':' || hours >= 24)
     return RoughTime::Invalid();
@@ -130,33 +132,27 @@ WritableDataNode::SetAttribute(const char *name, Angle value) noexcept
 void
 WritableDataNode::SetAttribute(const char *name, double value) noexcept
 {
-  StaticString<48> buf;
-  buf.UnsafeFormat(_T("%g"), (double)value);
+  NarrowString<48> buf;
+  buf.UnsafeFormat("%g", value);
   SetAttribute(name, buf);
 }
 
 void
 WritableDataNode::SetAttribute(const char *name, int value) noexcept
 {
-  StaticString<24> buf;
-  buf.UnsafeFormat(_T("%d"), value);
-  SetAttribute(name, buf);
+  SetAttribute(name, fmt::format_int{value}.c_str());
 }
 
 void
 WritableDataNode::SetAttribute(const char *name, unsigned value) noexcept
 {
-  StaticString<24> buf;
-  buf.UnsafeFormat(_T("%d"), value);
-  SetAttribute(name, buf);
+  SetAttribute(name, fmt::format_int{value}.c_str());
 }
 
 void
 WritableDataNode::SetAttribute(const char *name, bool value) noexcept
 {
-  StaticString<4> buf;
-  buf.UnsafeFormat(_T("%d"), (int)value);
-  SetAttribute(name, buf);
+  SetAttribute(name, fmt::format_int{(int)value}.c_str());
 }
 
 void
@@ -166,7 +162,7 @@ WritableDataNode::SetAttribute(const char *name, RoughTime value) noexcept
     /* no-op */
     return;
 
-  StaticString<8> buffer;
-  buffer.UnsafeFormat(_T("%02u:%02u"), value.GetHour(), value.GetMinute());
+  NarrowString<8> buffer;
+  buffer.UnsafeFormat("%02u:%02u", value.GetHour(), value.GetMinute());
   SetAttribute(name, buffer);
 }

src/XML/DataNodeXML.cpp
@@ -47,12 +47,12 @@ ConstDataNodeXML::ListChildrenNamed(const char *name) const noexcept
 }
 
 void
-WritableDataNodeXML::SetAttribute(const char *name, const TCHAR *value) noexcept
+WritableDataNodeXML::SetAttribute(const char *name, const char *value) noexcept
 {
   node.AddAttribute(name, value);
 }
 
-const TCHAR *
+const char *
 ConstDataNodeXML::GetAttribute(const char *name) const noexcept
 {
   return node.GetAttribute(name);

src/XML/DataNodeXML.hpp
@@ -29,7 +29,7 @@ public:
   std::unique_ptr<ConstDataNode> GetChildNamed(const char *name) const noexcept override;
   List ListChildren() const noexcept override;
   List ListChildrenNamed(const char *name) const noexcept override;
-  const TCHAR *GetAttribute(const char *name) const noexcept override;
+  const char *GetAttribute(const char *name) const noexcept override;
 };
 
 /**
@@ -51,5 +51,5 @@ public:
 
   /* virtual methods from WritableDataNode */
   std::unique_ptr<WritableDataNode> AppendChild(const char *name) noexcept override;
-  void SetAttribute(const char *name, const TCHAR *value) noexcept override;
+  void SetAttribute(const char *name, const char *value) noexcept override;
 };

src/XML/Node.cpp
@@ -73,7 +73,7 @@ XMLNode::GetChildNode(const char *name) const noexcept
   return nullptr;
 }
 
-const TCHAR *
+const char *
 XMLNode::GetAttribute(const char *name) const noexcept
 {
   if (!d)

src/XML/Node.hpp
@@ -39,7 +39,6 @@
 #include <string_view>
 
 #include <cassert>
-#include <tchar.h>
 
 class BufferedOutputStream;
 
@@ -52,7 +51,7 @@ class XMLNode {
     /** Structure for XML attribute. */
     struct Attribute : private NonCopyable {
       std::string name;
-      tstring value;
+      std::string value;
 
       template<typename N, typename V>
       Attribute(N &&name, V &&value) noexcept
@@ -170,7 +169,7 @@ public:
    * if failing)
    */
   [[gnu::pure]]
-  const TCHAR *GetAttribute(const char *name) const noexcept;
+  const char *GetAttribute(const char *name) const noexcept;
 
   /**
    * Create an XML file from the head element.

src/XML/Parser.cpp
@@ -617,7 +617,7 @@ ParseXMLElement(XMLNode &node, Parser *pXML)
           // Eg.  'Attribute AnotherAttribute'
         case TokenType::TEXT:
           // Add the unvalued attribute to the list
-          node.AddAttribute(std::move(attribute_name), tstring_view{});
+          node.AddAttribute(std::move(attribute_name), std::string_view{});
           // Cache the token then indicate.  We are next to
           // look for the equals attribute
           attribute_name = token.text;
@@ -637,7 +637,7 @@ ParseXMLElement(XMLNode &node, Parser *pXML)
 
           if (!attribute_name.empty())
             // Add the unvalued attribute to the list
-            node.AddAttribute(std::move(attribute_name), tstring_view{});
+            node.AddAttribute(std::move(attribute_name), std::string_view{});
 
           // If this is the end of the tag then return to the caller
           if (token.type == TokenType::SHORT_HAND_CLOSE)
@@ -694,7 +694,7 @@ ParseXMLElement(XMLNode &node, Parser *pXML)
             if (value == nullptr)
               throw std::runtime_error("Unexpected token found");
 
-            node.AddAttribute(attribute_name, pXML->string_converter.Convert(value));
+            node.AddAttribute(attribute_name, value);
           }
 
           // Indicate we are searching for a new attribute

commit 1189da0cb4fb9867fd5962590feffba15e417c24

XML/DataNode: prevent implicit pointer-bool conversion
Commit 4d709b0b60e875fdd9bd69d463b859bb98f78bda caused bug
https://github.com/XCSoar/XCSoar/issues/1346 because now `TCHAR`
strings would be casted to `bool`.  This is a fragile API, and the
`deleted` overload attempts to prevent this.

Of course, this now causes the Windows build to fail; the fix for
https://github.com/XCSoar/XCSoar/issues/1346 will also fix this build
breakage.  Until then, it's better to break the build than to build an
executable which loses data.

src/XML/DataNode.hpp
@@ -196,4 +196,8 @@ public:
    * invalid.
    */
   void SetAttribute(const char *name, RoughTime value) noexcept;
+
+  /* just here to prevent implicit pointer-to-bool casts
+     (e.g. TCHAR/wchar_t strings) */
+  void SetAttribute(const char *name, const auto *value) noexcept = delete;
 };