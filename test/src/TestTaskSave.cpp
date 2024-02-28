d1acbf41f202dd990f1ea2e51afc0432ac7fba31
Andreas Lüthi <andreas.luethi@gmx.net>
2024-02-28 13:14:33 +0100

    test/TestTaskSave: Set a unique Waypoint.id for waypoint comparison. More tests added
    This fixes random failures on GH actions due to uninitialized ids.

--- test/src/TestTaskSave.cpp
+++ test/src/TestTaskSave.cpp
@@ -24,18 +24,19 @@ MakeGeoPoint(double longitude, double latitude) noexcept
 }
 
 static Waypoint
-MakeWaypoint(Waypoint wp, double altitude, tstring name) noexcept
+MakeWaypoint(Waypoint wp, double altitude, tstring name, unsigned id) noexcept
 {
   wp.name = name;
+  wp.id = id;
   wp.elevation = altitude;
   wp.has_elevation = true;
   return wp;
 }
 
 static Waypoint
-MakeWaypoint(double longitude, double latitude, double altitude, tstring name) noexcept
+MakeWaypoint(double longitude, double latitude, double altitude, tstring name, unsigned id) noexcept
 {
-  return MakeWaypoint(Waypoint(MakeGeoPoint(longitude, latitude)), altitude, name);
+  return MakeWaypoint(Waypoint(MakeGeoPoint(longitude, latitude)), altitude, name, id);
 }
 
 template<typename... Args>
@@ -45,9 +46,13 @@ MakeWaypointPtr(Args&&... args) noexcept
   return WaypointPtr(new Waypoint(MakeWaypoint(std::forward<Args>(args)...)));
 }
 
-static const auto wp1 = MakeWaypointPtr(0, 45, 50, _T("wp-01"));
-static const auto wp2 = MakeWaypointPtr(0, 45.3, 50,  _T("wp-02"));
-static const auto wp3 = MakeWaypointPtr(0, 46, 50, _T("wp-03"));
+static const auto wp1_str = _T("wp-01");
+static const auto wp2_str = _T("wp-02");
+static const auto wp3_str = _T("wp-03");
+
+static const auto wp1 = MakeWaypointPtr(0, 45, 50, wp1_str, 100);
+static const auto wp2 = MakeWaypointPtr(0, 45.3, 50,  wp2_str, 102);
+static const auto wp3 = MakeWaypointPtr(0, 46, 50, wp3_str, 103);
 
 static void
 TestTaskSave()
@@ -70,6 +75,15 @@ TestTaskSave()
 
   std::unique_ptr<OrderedTask> task_loaded = LoadTask(task_path, task_behaviour);
   ok1(task_loaded);
+
+  ok1(task_loaded.get()->IsValidIndex(0));
+  ok1(task_loaded.get()->IsValidIndex(1));
+  ok1(task_loaded.get()->IsValidIndex(2));
+
+  ok1(StringIsEqual(task_loaded.get()->GetTaskPoint(0).GetWaypoint().name.c_str(), wp1_str));
+  ok1(StringIsEqual(task_loaded.get()->GetTaskPoint(1).GetWaypoint().name.c_str(), wp2_str));
+  ok1(StringIsEqual(task_loaded.get()->GetTaskPoint(2).GetWaypoint().name.c_str(), wp3_str));
+
   ok1(task_loaded.get()->GetTaskPoint(0).GetWaypoint() == *wp1);
   ok1(task_loaded.get()->GetTaskPoint(1).GetWaypoint() == *wp2);
   ok1(task_loaded.get()->GetTaskPoint(2).GetWaypoint() == *wp3);
@@ -85,7 +99,7 @@ int main()
 {
   Directory::Create(Path{_T("output/results")});
 
-  plan_tests(4);
+  plan_tests(10);
   task_behaviour.SetDefaults();
   ordered_task_settings.SetDefaults();
   TestAll();
