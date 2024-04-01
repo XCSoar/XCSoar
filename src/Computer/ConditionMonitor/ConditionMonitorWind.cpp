bc40b2f437b57933fc30e84b2f60f2302098f5e5
bomilkar <ronald_niederhagen@freenet.de>
2024-04-01 13:54:16 +0200

    monitor and record wind readings

--- src/Computer/ConditionMonitor/ConditionMonitorWind.cpp
+++ src/Computer/ConditionMonitor/ConditionMonitorWind.cpp
@@ -5,6 +5,9 @@
 #include "NMEA/Derived.hpp"
 #include "Language/Language.hpp"
 #include "Message.hpp"
+#include "Interface.hpp"
+#include "Units/Units.hpp"
+#include "LogFile.hpp"
 
 bool
 ConditionMonitorWind::CheckCondition([[maybe_unused]] const NMEAInfo &basic,
@@ -20,6 +23,14 @@ ConditionMonitorWind::CheckCondition([[maybe_unused]] const NMEAInfo &basic,
 
   auto mag_change = fabs(wind.norm - last_wind.norm);
   auto dir_change = (wind.bearing - last_wind.bearing).AsDelta().Absolute();
+
+  // monitor wind vector
+  const BrokenDateTime t = basic.date_time_utc;
+  char log_buf[200];
+  snprintf (log_buf,sizeof(log_buf),"MonitorWind,%d,%d,%1.1f,%1.1f,%d",
+      (t.hour * 60 + t.minute) * 60 + t.second,calculated.circling,
+      Units::ToUserWindSpeed(wind.norm),wind.bearing.Degrees(),(int)basic.baro_altitude);
+  LogString(log_buf);
 
   if (mag_change > 2.5)
     return true;
