ee38d92cbb1f9fb0fef80f088d6dd1effa67a5f8
Jetsada Machom <zinuzoid@gmail.com>
2024-04-02 16:39:13 -0700

    [Skysight] Update regions
    # Conflicts:
    #	NEWS.txt
    
    # Conflicts:
    #	NEWS.txt
    
    # Conflicts:
    #	NEWS.txt
    
    # Conflicts:
    #	NEWS.txt

--- src/Weather/Skysight/SkysightRegions.cpp
+++ src/Weather/Skysight/SkysightRegions.cpp
@@ -7,8 +7,6 @@ const SkysightRegionDetails skysight_region_defaults[] = {
 	{_T("South Africa"), "SANEW"},
 	{_T("Europe"), "EUROPE"},
 	{_T("Western US"), "WEST_US"},
-	{_T("Alaska"), "HRRRAK"},
-	{_T("HRRR"), "HRRR"},
 	{_T("Eastern US"), "EAST_US"},
 	{_T("Argentina/Chile"), "ARGENTINA_CHILE"},
 	{_T("Brazil"), "BRAZIL"},
@@ -16,7 +14,5 @@ const SkysightRegionDetails skysight_region_defaults[] = {
 	{_T("New Zealand"), "NZ"},
 	{_T("Western Australia"), "WA"},
 	{_T("Eastern Australia"), "EAST_AUS"},
-	{_T("ICON"), "ICON"},
-	{_T("GFS"), "GFS"},
 	{nullptr, nullptr}
 };
