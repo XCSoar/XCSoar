7fc7858c071016dbe1fe32c9ec92bbfd48d6a39b
Philipp Wollschlegel <folken@kabelsalat.ch>
2024-08-12 23:47:16 +0200

    tools/changelog.sh: handle rc releases

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -2,7 +2,7 @@
 
 TAG="${1}"
 VERSION=$(echo "$TAG" | cut -f2 -d v)
-STARTLINENUMBER=$(grep -n -E "^Version $VERSION" NEWS.txt | cut -f1 -d:) 
+STARTLINENUMBER=$(grep -n -E "^Version $VERSION " NEWS.txt | cut -f1 -d:)
 if [ -z "${STARTLINENUMBER}" ]; then
   echo "ERROR: Version $1 not found in NEWS.txt"
   exit 1
