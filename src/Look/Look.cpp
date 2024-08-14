70df04ae9e58283708518c4aef35f75e89aec477
kobedegeest <kobedegeest@gmail.com>
2024-08-14 11:40:32 +0200

    src/Look/Look.cpp: apply dark mode to big thermal assistant widget

--- src/Look/Look.cpp
+++ src/Look/Look.cpp
@@ -12,7 +12,6 @@ Look::Initialise(const Font &map_font)
   traffic.Initialise(map_font);
   flarm_dialog.Initialise(traffic, false);
   gesture.Initialise();
-  thermal_assistant_dialog.Initialise(false, false);
   chart.Initialise();
 }
 
@@ -57,6 +56,7 @@ Look::InitialiseConfigured(const UISettings &settings,
                    info_box.title_font);
   wind_arrow_info_box.Initialise(map_bold_font, dark_mode);
   flarm_gauge.Initialise(traffic, true, dark_mode);
+  thermal_assistant_dialog.Initialise(false, dark_mode);
   thermal_assistant_gauge.Initialise(true, dark_mode);
   final_glide_bar.Initialise(map_bold_font);
   vario_bar.Initialise(map_bold_font);
