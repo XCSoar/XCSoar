83f32f6c8ebdc237775c34fac7acbd98e0766fd6
kobedegeest <kobedegeest@gmail.com>
2024-09-02 13:40:44 +0200

    src/Dialogs/Settings/Panels/GaugesConfigPanel.cpp: Move flarm radar location settings
    from Look -> ScreenLayout to the Gauges->Flarm, other page

--- src/Dialogs/Settings/Panels/GaugesConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/GaugesConfigPanel.cpp
@@ -14,6 +14,7 @@
 enum ControlIndex {
   EnableFLARMGauge,
   AutoCloseFlarmDialog,
+  AppFlarmLocation,
   TAPosition,
   EnableThermalProfile,
   FinalGlideBarDisplayModeControl,
@@ -28,6 +29,36 @@ static constexpr StaticEnumChoice final_glide_bar_display_mode_list[] = {
     N_("Always show final glide bar.") },
   { FinalGlideBarDisplayMode::AUTO, N_("Auto"),
     N_("Show final glide bar if approaching final glide range.") },
+  nullptr
+};
+
+static constexpr StaticEnumChoice flarm_display_location_list[] = {
+  { TrafficSettings::GaugeLocation::AUTO,
+    N_("Auto (follow infoboxes)") },
+  { TrafficSettings::GaugeLocation::TOP_LEFT,
+    N_("Top Left") },
+  { TrafficSettings::GaugeLocation::TOP_RIGHT,
+    N_("Top Right") },
+  { TrafficSettings::GaugeLocation::BOTTOM_LEFT,
+    N_("Bottom Left") },
+  { TrafficSettings::GaugeLocation::BOTTOM_RIGHT,
+    N_("Bottom Right") },
+  { TrafficSettings::GaugeLocation::CENTER_TOP,
+    N_("Center Top") },
+  { TrafficSettings::GaugeLocation::CENTER_BOTTOM,
+    N_("Center Bottom") },
+  { TrafficSettings::GaugeLocation::TOP_LEFT_AVOID_IB,
+    N_("Top Left (Avoid Infoboxes)") },
+  { TrafficSettings::GaugeLocation::TOP_RIGHT_AVOID_IB,
+    N_("Top Right (Avoid Infoboxes)") },
+  { TrafficSettings::GaugeLocation::BOTTOM_LEFT_AVOID_IB,
+    N_("Bottom Left (Avoid Infoboxes)") },
+  { TrafficSettings::GaugeLocation::BOTTOM_RIGHT_AVOID_IB,
+    N_("Bottom Right (Avoid Infoboxes)") },
+  { TrafficSettings::GaugeLocation::CENTER_TOP_AVOID_IB,
+    N_("Center Top (Avoid Infoboxes)") },
+  { TrafficSettings::GaugeLocation::CENTER_BOTTOM_AVOID_IB,
+    N_("Center Bottom (Avoid Infoboxes)") },
   nullptr
 };
 
@@ -91,6 +122,11 @@ GaugesConfigPanel::Prepare(ContainerWindow &parent,
              _("Setting this to \"On\" will automatically close the FLARM dialog if there is no traffic. \"Off\" will keep the dialog open even without current traffic."),
              ui_settings.traffic.auto_close_dialog);
   SetExpertRow(AutoCloseFlarmDialog);
+  
+  AddEnum(_("FLARM display"), _("Choose a location for the FLARM display."),
+          flarm_display_location_list,
+          (unsigned)ui_settings.traffic.gauge_location);
+  SetExpertRow(AppFlarmLocation);
 
   AddEnum(_("Thermal assistant"),
             _("Enable and select the position of the thermal assistant when overlayed on the main screen."),
@@ -141,7 +177,9 @@ GaugesConfigPanel::Save(bool &_changed) noexcept
                        ui_settings.traffic.auto_close_dialog);
 
   if (SaveValueEnum(TAPosition, ProfileKeys::TAPosition,
-                    ui_settings.thermal_assistant_position))
+                    ui_settings.thermal_assistant_position) || 
+      SaveValueEnum(AppFlarmLocation, ProfileKeys::FlarmLocation,
+                    ui_settings.traffic.gauge_location))
     CommonInterface::main_window->ReinitialiseLayout();
 
   changed |= SaveValue(EnableThermalProfile, ProfileKeys::EnableThermalProfile,
