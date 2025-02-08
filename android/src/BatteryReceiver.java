bebf2541f4b46d61ba4c3081a084f2318aa6d475
Philipp Wollschlegel <folken@kabelsalat.ch>
2025-02-08 16:20:48 +0100

    ANDROID: check for valid battery values, use scale factor

--- android/src/BatteryReceiver.java
+++ android/src/BatteryReceiver.java
@@ -3,17 +3,34 @@
 
 package org.xcsoar;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.BroadcastReceiver;
 import android.os.BatteryManager;
 
 class BatteryReceiver extends BroadcastReceiver {
-  private static native void setBatteryPercent(int level, int plugged);
+  private static native void setBatteryPercent(int batteryPct, int plugged);
 
-  @Override public void onReceive(Context context, Intent intent) {
-    int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-    int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
-    setBatteryPercent(level, plugged);
+  @Override public void onReceive(Context context, Intent intent)
+  {
+    if (intent == null ||
+        !intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+      return;
+    }
+
+    boolean isBatteryPresent =
+        intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false);
+    int batteryPct = 0;
+    int plugged = 1; // Assume plugged in if no battery is present
+
+    if (isBatteryPresent) {
+      int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+      int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+      batteryPct = (level >= 0 && scale > 0) ? (level * 100 / scale) : 0;
+      plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+    }
+
+    // Pass the calculated battery percentage and plugged status to native code
+    setBatteryPercent(batteryPct, plugged);
   }
 }
