986d1b45dd481cc996fdaaec1f0f342abf25f84b
Caz Yokoyama <caz@caztech.com>
2025-02-25 13:53:16 +0900

    Add height of Navigator pane/Widget in settings. config -> system -> gauges -> flarm,other -> navigator widget height: 15%
    Signed-off-by: Caz Yokoyama <caz@caztech.com>

---
+++ src/Gauge/NavigatorSettings.hpp
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright The XCSoar Project
+
+#pragma once
+
+struct NavigatorSettings {
+
+  unsigned navigator_height;
+
+  void SetDefaults() noexcept;
+};
