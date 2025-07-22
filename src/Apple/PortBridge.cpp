f0307742c3d9c45baac300e761701971cde0e07d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-07-22 16:12:39 +0200

    Add dummy implementation for PortBridge

---
+++ src/Apple/PortBridge.cpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright The XCSoar Project
+
+#include "PortBridge.hpp"
+
+#include <string.h>
+
+PortBridge::setListener(PortListener *_listener)
+{
+  // TODO
+}
+
+void
+PortBridge::setInputListener(DataHandler *handler)
+{
+  // TODO
+}
+
+std::size_t
+PortBridge::write(std::span<const std::byte> src)
+{
+  // TODO
+  return -1;
+}
