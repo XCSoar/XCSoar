6d089f8efbecae893f5943bff7c2db4866e7931d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-07-23 12:46:47 +0200

    Improve PortBridge

--- src/Apple/PortBridge.cpp
+++ src/Apple/PortBridge.cpp
@@ -2,23 +2,32 @@
 // Copyright The XCSoar Project
 
 #include "PortBridge.hpp"
+#include "NativeInputListener.hpp"
+#include "NativePortListener.hpp"
 
+#include <span>
 #include <string.h>
 
+PortBridge::PortBridge() {}
+
+void
 PortBridge::setListener(PortListener *_listener)
 {
+  (void)_listener;
   // TODO
 }
 
 void
 PortBridge::setInputListener(DataHandler *handler)
 {
+  (void)handler;
   // TODO
 }
 
 std::size_t
 PortBridge::write(std::span<const std::byte> src)
 {
+  (void)src;
   // TODO
   return -1;
 }
