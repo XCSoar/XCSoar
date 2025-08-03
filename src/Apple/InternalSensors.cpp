3473e06c2d2cc348f72be0b3e74f729b2fba641e
Yorick Reum <git@yorickreum.de>
2025-08-03 13:40:11 +0200

    iOS: src/Apple/InternalSensors: Add background location management to prevent crashes

--- src/Apple/InternalSensors.cpp
+++ src/Apple/InternalSensors.cpp
@@ -19,6 +19,20 @@
     self->listener = _listener;
     gregorian_calendar = [[NSCalendar alloc]
       initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+#if TARGET_OS_IPHONE
+    background_task = UIBackgroundTaskInvalid;
+    
+    // Register for background/foreground notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidEnterBackground:)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillEnterForeground:)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
+#endif
   }
   return self;
 }
@@ -42,6 +56,16 @@
   if ((status == kCLAuthorizationStatusAuthorizedAlways)
       || (status == kCLAuthorizationStatusAuthorizedWhenInUse)) {
     [manager startUpdatingLocation];
+    
+    // Configure for background location if we have "Always" permission
+    if (status == kCLAuthorizationStatusAuthorizedAlways) {
+      if (@available(iOS 9.0, *)) {
+        manager.allowsBackgroundLocationUpdates = YES;
+      }
+      if (@available(iOS 11.0, *)) {
+        manager.showsBackgroundLocationIndicator = NO;
+      }
+    }
   }
 }
 #endif
@@ -84,6 +108,41 @@
 {
   self->listener->OnConnected(0);
 }
+
+#if TARGET_OS_IPHONE
+- (void)applicationDidEnterBackground:(NSNotification *)notification
+{
+  // Start a background task to allow location updates to continue briefly
+  background_task = [[UIApplication sharedApplication] 
+    beginBackgroundTaskWithName:@"LocationUpdates" 
+    expirationHandler:^{
+      // Clean up when the background task expires
+      if (background_task != UIBackgroundTaskInvalid) {
+        [[UIApplication sharedApplication] endBackgroundTask:background_task];
+        background_task = UIBackgroundTaskInvalid;
+      }
+    }];
+}
+
+- (void)applicationWillEnterForeground:(NSNotification *)notification
+{
+  // End the background task when returning to foreground
+  if (background_task != UIBackgroundTaskInvalid) {
+    [[UIApplication sharedApplication] endBackgroundTask:background_task];
+    background_task = UIBackgroundTaskInvalid;
+  }
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  if (background_task != UIBackgroundTaskInvalid) {
+    [[UIApplication sharedApplication] endBackgroundTask:background_task];
+    background_task = UIBackgroundTaskInvalid;
+  }
+}
+#endif
+
 @end
 
 
@@ -117,15 +176,37 @@ void InternalSensors::Init()
   location_manager.desiredAccuracy =
       kCLLocationAccuracyBestForNavigation;
   location_manager.delegate = location_delegate;
+  
 #if TARGET_OS_IPHONE
+  // Configure location manager for background operation
+  location_manager.distanceFilter = 10.0; // Update every 10 meters
+  location_manager.pausesLocationUpdatesAutomatically = NO;
+  
   if ([location_manager
       respondsToSelector: @selector(requestWhenInUseAuthorization)]) {
     CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
-    if ((status == kCLAuthorizationStatusAuthorizedAlways)
-        || (status == kCLAuthorizationStatusAuthorizedWhenInUse)) {
+    if (status == kCLAuthorizationStatusAuthorizedAlways) {
+      // We already have "Always" permission, configure for background and start
+      if (@available(iOS 9.0, *)) {
+        location_manager.allowsBackgroundLocationUpdates = YES;
+      }
+      if (@available(iOS 11.0, *)) {
+        location_manager.showsBackgroundLocationIndicator = NO;
+      }
       [location_manager startUpdatingLocation];
-    } else {
+    } else if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+      // We have "When In Use", request upgrade to "Always" for background usage
+      if (@available(iOS 11.0, *)) {
+        [location_manager requestAlwaysAuthorization];
+      } else {
+        [location_manager startUpdatingLocation];
+      }
+    } else if (status == kCLAuthorizationStatusNotDetermined) {
+      // First time - request "When In Use" first
       [location_manager requestWhenInUseAuthorization];
+    } else {
+      // Denied or restricted - start without background capability
+      [location_manager startUpdatingLocation];
     }
   } else {
     [location_manager startUpdatingLocation];
