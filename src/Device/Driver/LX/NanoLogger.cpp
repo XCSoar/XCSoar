6c81110132a890fe5d99385b7890ea3f584de647
kobedegeest <kobedegeest@gmail.com>
2025-08-19 15:15:13 +0200

    Driver/LX/NanoLogger.cpp: Log timeout to log file

--- src/Device/Driver/LX/NanoLogger.cpp
+++ src/Device/Driver/LX/NanoLogger.cpp
@@ -14,9 +14,16 @@
 #include "NMEA/InputLine.hpp"
 #include "util/SpanCast.hxx"
 #include "util/StringCompare.hxx"
+#include "system/FileUtil.hpp"
+#include "util/TextFile.hxx"
+#include "io/FileLineReader.hpp"
+#include "util/StaticString.hxx"
+#include "LogFile.hpp"
 
 #include <algorithm>
 #include <stdlib.h>
+#include <fstream>
+#include <exception>
 
 #include <fmt/format.h>
 
@@ -311,6 +318,8 @@ DownloadFlightInner(Port &port, const char *filename, BufferedOutputStream &os,
       try {
         line = reader.ExpectLine("PLXVC,FLIGHT,A,", timeout);
       } catch (...) {
+        LogFormat("Communication with logger timed out, tries: %u, line: %u", request_retry_count, i);
+        LogError(std::current_exception(), "Download failing");
       }
       if (line == nullptr || !HandleFlightLine(line, os, i, row_count)) {
         if (request_retry_count > 5)
