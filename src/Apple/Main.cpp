e28c845e48edb14dd0ee2967a3dd5709329b969b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-08-23 20:39:58 +0200

    iOS: Improve audio support

---
+++ src/Apple/Main.cpp
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright The XCSoar Project
+
+#include "Main.hpp"
+#include "LogFile.hpp"
+#import <AVFoundation/AVFoundation.h>
+
+// Initialize apple services - this will be called from the main XCSoar startup
+void
+InitializeAppleServices()
+{
+  // Setup AVAudioSession for better audio playback
+  NSError *error = nil;
+  AVAudioSession *session = [AVAudioSession sharedInstance];
+  [session setCategory:AVAudioSessionCategoryPlayback
+           withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                 error:&error];
+  [session setActive:YES error:&error];
+  if (error) {
+    LogFormat("AVAudioSession initialize error: %s", [[error localizedDescription] UTF8String]);
+  }
+}
+
+// Cleanup apple services - this will be called from XCSoar shutdown
+void
+DeinitializeAppleServices()
+{
+  // Deinitialize AVAudioSession
+  NSError *error = nil;
+  [[AVAudioSession sharedInstance] setActive:NO error:&error];
+  if (error) {
+    LogFormat("AVAudioSession deinitialize error: %s", [[error localizedDescription] UTF8String]);
+  }
+}
