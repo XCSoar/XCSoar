1d73800acf3c44ecaa09642e2b7070f355e24e4b
Philipp Wollschlegel <folken@kabelsalat.ch>
2025-11-27 23:32:13 +0100

    ui/event: Fix gesture rotation for UNIX with SOFTWARE_ROTATE_DISPLAY
    Fix gesture rotation handling on X11 and Wayland when
    SOFTWARE_ROTATE_DISPLAY is enabled. The rotation transform was not
    being applied correctly to gesture coordinates, causing incorrect
    touch/gesture positions in rotated display modes.

--- src/ui/event/poll/Queue.hpp
+++ src/ui/event/poll/Queue.hpp
@@ -8,6 +8,10 @@
 #include "event/InjectEvent.hxx"
 #include "event/Loop.hxx"
 
+#ifdef ENABLE_OPENGL
+#include "ui/opengl/Features.hpp"
+#endif
+
 #ifdef USE_X11
 #include "X11Queue.hpp"
 #elif defined(USE_WAYLAND)
@@ -116,11 +120,15 @@ public:
   void SetScreenSize([[maybe_unused]] const PixelSize &screen_size) noexcept {
 #if !defined(NON_INTERACTIVE) && !defined(USE_X11) && !defined(USE_WAYLAND)
     input_queue.SetScreenSize(screen_size);
+#elif defined(ENABLE_OPENGL) && defined(SOFTWARE_ROTATE_DISPLAY) && (defined(USE_X11) || defined(USE_WAYLAND))
+    input_queue.SetScreenSize(screen_size);
 #endif
   }
 
   void SetDisplayOrientation([[maybe_unused]] DisplayOrientation orientation) noexcept {
 #if !defined(NON_INTERACTIVE) && !defined(USE_X11) && !defined(USE_WAYLAND) && !defined(USE_LIBINPUT)
+    input_queue.SetDisplayOrientation(orientation);
+#elif defined(ENABLE_OPENGL) && defined(SOFTWARE_ROTATE_DISPLAY) && (defined(USE_X11) || defined(USE_WAYLAND))
     input_queue.SetDisplayOrientation(orientation);
 #endif
   }
