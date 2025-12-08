0d90d02a291438fce9bb056b44b8d06be4fa877b
Stefan Schumann <stefan.schumann@posteo.de>
2025-12-08 18:05:05 +0100

    ActionInterface: add separate SetTransponderMode function

--- src/ApplyExternalSettings.cpp
+++ src/ApplyExternalSettings.cpp
@@ -175,11 +175,15 @@ TransponderProcess() noexcept
   static Validity last_transponder_mode;
 
   if (basic.settings.has_transponder_code.Modified(last_transponder_code)) {
-    ActionInterface::SetTransponderCode(basic.settings.transponder_code, basic.settings.transponder_mode, false);
+    ActionInterface::SetTransponderCode(basic.settings.transponder_code,
+                                        false);
     last_transponder_code = basic.settings.has_transponder_code;
-    if (basic.settings.has_transponder_mode.Modified(last_transponder_mode))
-      last_transponder_mode = basic.settings.has_transponder_mode;
+    modified = true;
+  }
 
+  if (basic.settings.has_transponder_mode.Modified(last_transponder_mode)) {
+    ActionInterface::SetTransponderMode(basic.settings.transponder_mode);
+    last_transponder_mode = basic.settings.has_transponder_mode;
     modified = true;
   }
 
