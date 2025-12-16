a95d54d314f287dbaba2513a27df90f6b11ff1ed
Philipp Wollschlegel <folken@kabelsalat.ch>
2025-12-16 22:15:32 +0100

    Form/Edit: Allow readonly fields to show full content in dialog
    Readonly WndProperty fields can now be clicked to display their full
    content in a dialog. If the content is an absolute path, an "Open"
    button is shown to open the path in the file manager (using xdg-open
    on Linux).
    
    This is particularly useful for displaying long paths that don't fit
    in the field width, such as the XCSoar data path in Site Files config.

--- src/system/RunFile.hpp
+++ src/system/RunFile.hpp
@@ -5,14 +5,19 @@
 
 #include <tchar.h>
 
+/* HAVE_RUN_FILE is defined for platforms that support external file references
+ * in waypoint details (file= entries). On Android, these are handled via
+ * ContentProvider (FileProvider.cpp) instead of RunFile(). */
 #if defined(ANDROID) || \
   (defined(HAVE_POSIX) && !defined(_WIN32) && !defined(KOBO))
 #define HAVE_RUN_FILE
+#endif
 
+/* RunFile() function is only available on non-Android POSIX platforms */
+#if !defined(ANDROID) && defined(HAVE_RUN_FILE)
 /**
  * Opens a file in the user's preferred application.
  */
 bool
 RunFile(const TCHAR *path) noexcept;
-
 #endif
