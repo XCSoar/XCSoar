434dcd27b7bc57f7b9e856bef6902b7b3217c721
Stefan Schumann <stefan.schumann@posteo.de>
2025-12-18 13:02:31 +0100

    Device/Driver/FLARM: add PFLAM message parser for messaging data

---
+++ test/src/FakeTraffic.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright The XCSoar Project
+
+#include "FLARM/Details.hpp"
+
+namespace FlarmDetails {
+
+void
+StoreMessagingRecord(const MessagingRecord &) noexcept
+{
+  // Stub - test doesn't need actual database storage
+}
+
+}  // namespace FlarmDetails
