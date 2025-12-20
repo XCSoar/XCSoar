1f75ed9f4616927d6c8693df328b9e532f0b1d72
Stefan Schumann <stefan.schumann@posteo.de>
2025-12-20 17:12:06 +0100

    FLARM/FlarmNetRecord: add Format()-function

--- src/FLARM/FlarmNetRecord.cpp
+++ src/FLARM/FlarmNetRecord.cpp
@@ -2,3 +2,11 @@
 // Copyright The XCSoar Project
 
 #include "FlarmNetRecord.hpp"
+#include <tchar.h>
+
+const TCHAR *
+FlarmNetRecord::Format([[maybe_unused]] StaticString<256> &buffer, 
+                        const TCHAR *value) const noexcept
+{
+  return (value != nullptr && !*value) ? nullptr : value;
+}
