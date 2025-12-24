ef391c0a0976026eddc7899e897deb899d5f1352
Philipp Wollschlegel <folken@kabelsalat.ch>
2025-12-25 00:51:43 +0100

    build/wayland.mk: Use pkg-config for wayland-protocols datadir
    Replace hardcoded /usr/share/wayland-protocols paths with pkg-config
    query to support non-standard install prefixes. Query pkg-config for
    wayland-protocols pkgdatadir and compose XML paths from that variable.
    
    Also includes translation updates and submodule updates.

--- build/wayland.mk
+++ build/wayland.mk
@@ -3,10 +3,11 @@ EGL = y
 OPENGL = y
 
 $(eval $(call pkg-config-library,WAYLAND,wayland-egl))
+$(eval $(call pkg-config-library,WAYLAND_CURSOR,wayland-cursor))
 $(eval $(call pkg-config-library,XKBCOMMON,xkbcommon))
 WAYLAND_FEATURE_CPPFLAGS = -DUSE_WAYLAND
-WAYLAND_CPPFLAGS += $(XKBCOMMON_CPPFLAGS)
-WAYLAND_LDLIBS += $(XKBCOMMON_LDLIBS)
+WAYLAND_CPPFLAGS += $(WAYLAND_CURSOR_CPPFLAGS) $(XKBCOMMON_CPPFLAGS)
+WAYLAND_LDLIBS += $(WAYLAND_CURSOR_LDLIBS) $(XKBCOMMON_LDLIBS)
 
 # Generate C sources and headers from the Wayland protocol
 # description; this is needed for interfaces which do not come
@@ -16,7 +17,9 @@ WAYLAND_GENERATED = $(TARGET_OUTPUT_DIR)/wayland-generated
 INCLUDES += -isystem $(WAYLAND_GENERATED)
 
 # from Debian package "wayland-protocols"
-XDG_SHELL_XML = /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml
+WAYLAND_PROTOCOLS_DATADIR = $(shell pkg-config --variable=pkgdatadir wayland-protocols)
+XDG_SHELL_XML = $(WAYLAND_PROTOCOLS_DATADIR)/stable/xdg-shell/xdg-shell.xml
+XDG_DECORATION_XML = $(WAYLAND_PROTOCOLS_DATADIR)/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml
 
 # from Debian package "libwayland-bin"
 WAYLAND_SCANNER = wayland-scanner
