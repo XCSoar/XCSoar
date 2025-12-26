e09aa5c7c5dd8eb1332dd48d6e9bea27a30e6eed
Philipp Wollschlegel <folken@kabelsalat.ch>
2025-12-26 01:04:08 +0100

    build/android: Fix AAPT2 XML parsing error for dirstamp files
    AAPT2 and AAPT were scanning RES_DIR and attempting to parse dirstamp
    files as XML resource files, causing 'Error parsing XML: no element
    found' errors. Remove all dirstamp files from RES_DIR before running
    AAPT2/AAPT to prevent them from being processed as resource files.
    
    This fixes the build error:
      ./output/ANDROID/android/res/values/dirstamp:1: error: Error parsing
      XML: no element found

--- build/wayland.mk
+++ build/wayland.mk
@@ -4,10 +4,9 @@ OPENGL = y
 
 $(eval $(call pkg-config-library,WAYLAND,wayland-egl))
 $(eval $(call pkg-config-library,WAYLAND_CURSOR,wayland-cursor))
-$(eval $(call pkg-config-library,XKBCOMMON,xkbcommon))
 WAYLAND_FEATURE_CPPFLAGS = -DUSE_WAYLAND
-WAYLAND_CPPFLAGS += $(WAYLAND_CURSOR_CPPFLAGS) $(XKBCOMMON_CPPFLAGS)
-WAYLAND_LDLIBS += $(WAYLAND_CURSOR_LDLIBS) $(XKBCOMMON_LDLIBS)
+WAYLAND_CPPFLAGS += $(WAYLAND_CURSOR_CPPFLAGS)
+WAYLAND_LDLIBS += $(WAYLAND_CURSOR_LDLIBS)
 
 # Generate C sources and headers from the Wayland protocol
 # description; this is needed for interfaces which do not come
@@ -17,17 +16,9 @@ WAYLAND_GENERATED = $(TARGET_OUTPUT_DIR)/wayland-generated
 INCLUDES += -isystem $(WAYLAND_GENERATED)
 
 # from Debian package "wayland-protocols"
-<<<<<<< Updated upstream
-XDG_SHELL_XML = /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml
-<<<<<<< HEAD
-=======
 WAYLAND_PROTOCOLS_DATADIR = $(shell pkg-config --variable=pkgdatadir wayland-protocols)
 XDG_SHELL_XML = $(WAYLAND_PROTOCOLS_DATADIR)/stable/xdg-shell/xdg-shell.xml
 XDG_DECORATION_XML = $(WAYLAND_PROTOCOLS_DATADIR)/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml
->>>>>>> Stashed changes
-=======
-XDG_DECORATION_XML = /usr/share/wayland-protocols/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml
->>>>>>> 40a93bf03d (ui/window/wayland: Implement window resizing)
 
 # from Debian package "libwayland-bin"
 WAYLAND_SCANNER = wayland-scanner
