308687728e16bc007789086b844ae11cecf745b7
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-12-30 22:53:16 +0100

    util/OpenLink: Add support for macOS

--- src/util/OpenLink.cpp
+++ src/util/OpenLink.cpp
@@ -9,16 +9,24 @@
 
 #if defined(__APPLE__) && TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#elif defined(__APPLE__) && defined(USE_APPKIT)
+#import <AppKit/AppKit.h>
 #endif
 
 bool OpenLink(const char *url) noexcept {
 #if defined(__APPLE__) && TARGET_OS_IPHONE
+  // iOS
   NSString *ns_url = [NSString stringWithUTF8String:url];
   NSURL *nsu = [NSURL URLWithString:ns_url];
   if ([[UIApplication sharedApplication] canOpenURL:nsu]) {
     [[UIApplication sharedApplication] openURL:nsu options:@{} completionHandler:nil];
     return true;
   }
+#elif defined(__APPLE__) && defined(USE_APPKIT)
+  // macOS
+  NSString *ns_url = [NSString stringWithUTF8String:url];
+  NSURL *nsu = [NSURL URLWithString:ns_url];
+  return [[NSWorkspace sharedWorkspace] openURL:nsu];
 #endif
   (void)url;
   return false;
