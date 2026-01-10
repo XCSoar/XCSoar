403181d752003267746fcbced8e0507fa39f79d9
Stefan Schumann <stefan.schumann@posteo.de>
2026-01-10 22:47:03 +0100

    MapWindow/GlueMapWindowOverlays: show network connection state

--- src/MapWindow/GlueMapWindowEvents.cpp
+++ src/MapWindow/GlueMapWindowEvents.cpp
@@ -448,6 +448,7 @@ GlueMapWindow::Render(Canvas &canvas, const PixelRect &rc) noexcept
       DrawThermalBand(canvas, rc);
     DrawStallRatio(canvas, rc);
     DrawFlightMode(canvas, rc);
+    DrawSystemStatus(canvas, rc);
     DrawFinalGlide(canvas, rc);
     DrawVario(canvas, rc);
     DrawGPSStatus(canvas, rc, Basic());
