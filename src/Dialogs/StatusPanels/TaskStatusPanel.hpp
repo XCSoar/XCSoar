08f2b7e0014cef92fec94bf5b57159bf2de36393
kobedegeest <kobedegeest@gmail.com>
2026-01-15 11:35:47 +0100

    Dialogs/StatusPanels/TaskStatusPanel.cpp: refresh on calculated update
    This closes #556
    
    Calling refresh immediately after changing MC value is to fast,
    calculation has no time to finish hence values are not updated after MC
    change.
    
    Now the panel is updated OnCalculatedUpdate().
    Addition benefit, panel also updates during flying not just after MC
    change.

--- src/Dialogs/StatusPanels/TaskStatusPanel.hpp
+++ src/Dialogs/StatusPanels/TaskStatusPanel.hpp
@@ -5,19 +5,30 @@
 
 #include "StatusPanel.hpp"
 #include "Form/DataField/Listener.hpp"
+#include "Blackboard/RateLimitedBlackboardListener.hpp"
 
-class TaskStatusPanel : public StatusPanel, DataFieldListener {
+class TaskStatusPanel
+ : public StatusPanel, DataFieldListener,
+   private NullBlackboardListener {
+  RateLimitedBlackboardListener rate_limiter;
+
+
 public:
   explicit TaskStatusPanel(const DialogLook &look) noexcept
-    :StatusPanel(look) {}
+    :StatusPanel(look), rate_limiter(*this, std::chrono::seconds(1),
+                                     std::chrono::milliseconds(500)) {}
 
   /* virtual methods from class StatusPanel */
   void Refresh() noexcept override;
 
   /* virtual methods from class Widget */
   void Prepare(ContainerWindow &parent, const PixelRect &rc) noexcept override;
+  void Show(const PixelRect &rc) noexcept override;
+  void Hide() noexcept override;
 
 private:
   /* virtual methods from DataFieldListener */
   void OnModified(DataField &df) noexcept override;
+  /* virtual methods from class BlackboardListener */
+  void OnCalculatedUpdate(const MoreData &basic, const DerivedInfo &calculated) override;
 };
