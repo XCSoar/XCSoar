b30df23fb5effa5dd90ba38521fabf343989ed45
Yorick Reum <git@yorickreum.de>
2026-01-18 16:13:37 +0100

    docs,Language/Language: add TCHAR deprecation guidelines

--- src/Language/Language.hpp
+++ src/Language/Language.hpp
@@ -10,11 +10,14 @@
 #include <libintl.h> // IWYU pragma: export
 
 #define _(x) gettext(x)
+#define _utf8(x) gettext(x)
 
 #ifdef gettext_noop
 #define N_(x) gettext_noop(x)
+#define N_utf8(x) gettext_noop(x)
 #else
 #define N_(x) (x)
+#define N_utf8(x) (x)
 #endif
 
 static inline void AllowLanguage() {}
@@ -38,11 +41,24 @@ void DisallowLanguage();
 [[gnu::const]]
 const TCHAR* gettext(const TCHAR* text);
 
+[[gnu::const]]
+static inline const char *gettext_utf8(const char *text) noexcept {
+  return text;
+}
+
 /**
  * For source compatibility with GNU gettext.
+ * TCHAR version (legacy, for TCHAR strings).
  */
 #define _(x) gettext(_T(x))
 #define N_(x) _T(x)
+
+/**
+ * UTF-8 string translation.
+ * Pass UTF-8 strings directly to gettext (stub mode returns them unchanged).
+ */
+#define _utf8(x) gettext_utf8(x)
+#define N_utf8(x) (x)
 
 void reset_gettext_cache();
 
