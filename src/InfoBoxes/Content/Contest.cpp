b6947c09a281963714449bf412427167c38d3f89
kobedegeest <kobedegeest@gmail.com>
2026-01-19 17:07:10 +0100

    InfoBoxes/Content/Contest: Replace panel with direct HandleClick
    Contest and ContestSpeed InfoBoxes now open the contest analysis
    dialog directly on short press.

--- src/InfoBoxes/Content/Contest.cpp
+++ src/InfoBoxes/Content/Contest.cpp
@@ -15,9 +15,12 @@
 
 #include <tchar.h>
 
-static void
+static bool
 ShowAnalysis8() noexcept
 {
+  if (!backend_components || !data_components)
+    return false;
+
   dlgAnalysisShowModal(UIGlobals::GetMainWindow(),
                        UIGlobals::GetLook(),
                        CommonInterface::Full(),
@@ -25,24 +28,13 @@ ShowAnalysis8() noexcept
                        data_components->airspaces.get(),
                        data_components->terrain.get(),
                        AnalysisPage::CONTEST);
+  return true;
 }
 
-static std::unique_ptr<Widget>
-LoadAnalysis8Panel([[maybe_unused]] unsigned id) noexcept
+bool
+InfoBoxContentContest::HandleClick() noexcept
 {
-  return std::make_unique<CallbackWidget>(ShowAnalysis8);
-}
-
-static constexpr
-InfoBoxPanel analysis8_infobox_panels[] = {
-  { N_("Analysis"), LoadAnalysis8Panel },
-  { nullptr, nullptr }
-};
-
-const InfoBoxPanel *
-InfoBoxContentContest::GetDialogContent() noexcept
-{
-  return analysis8_infobox_panels;
+  return ShowAnalysis8();
 }
 
 void
@@ -74,10 +66,10 @@ InfoBoxContentContest::Update(InfoBoxData &data) noexcept
   data.FmtComment(_T("{:.1f} pts"), result_contest.score);
 }
 
-const InfoBoxPanel *
-InfoBoxContentContestSpeed::GetDialogContent() noexcept
+bool
+InfoBoxContentContestSpeed::HandleClick() noexcept
 {
-  return analysis8_infobox_panels;
+  return ShowAnalysis8();
 }
 
 void
