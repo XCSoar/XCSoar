a02d22ab63e3abea9bd9a34e33dd19f6cc2cd842
Stefan Schumann <stefan.schumann@posteo.de>
2026-01-20 17:26:26 +0100

    Widget/MultiSelectListWidget: add multi-select widget for efficient selection
    Selecting multiple waypoints and airspaces currently requires navigating
    multiple single-select lists, which is annoying and confusing. Add a new
    multi-select list widget with checkbox support and single-click activation
    to simplify selection of multiple items.

--- build/libwidget.mk
+++ build/libwidget.mk
@@ -30,7 +30,8 @@ WIDGET_SOURCES = \
 	$(SRC)/Widget/DrawWidget.cpp \
 	$(SRC)/Widget/QuestionWidget.cpp \
 	$(SRC)/Widget/KeyboardWidget.cpp \
-	$(SRC)/Widget/ViewImageWidget.cpp
+	$(SRC)/Widget/ViewImageWidget.cpp \
+	$(SRC)/Widget/MultiSelectListWidget.cpp
 
 WIDGET_DEPENDS = SCREEN
 
