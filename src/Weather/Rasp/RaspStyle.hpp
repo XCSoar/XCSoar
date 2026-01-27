c9b9aebcc29474d9d358fb6d166b5fff5ac9f9a7
Simon GH <groundhog@gmx.net>
2026-01-27 20:40:08 +0100

    Weather/Rasp: Extend RaspStyle to also support colormap with alpha channel

--- src/Weather/Rasp/RaspStyle.hpp
+++ src/Weather/Rasp/RaspStyle.hpp
@@ -4,12 +4,27 @@
 #pragma once
 
 struct ColorRamp;
+struct ColorRampAlpha;
 
+/**
+ * Defines the rendering style for a RASP weather map.
+ *
+ * At least one of color_ramp (RGB, no transparency) or color_ramp_alpha (RGBA with
+ * transparency) must be set. RGBA has preference when both are set.
+ */
 struct RaspStyle {
   const char *name;
   const ColorRamp *color_ramp;
+  const ColorRampAlpha *color_ramp_alpha;
   unsigned height_scale;
   bool do_water;
+
+  /**
+   * Returns true if this style uses an alpha-enabled colormap.
+   */
+  constexpr bool HasAlpha() const noexcept {
+    return color_ramp_alpha != nullptr;
+  }
 };
 
 extern const RaspStyle rasp_styles[];
