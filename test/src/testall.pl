9558b86bade5370e7b4ced1e3c91bf37da0d7107
Philipp Wollschlegel <folken@ai-node.apwm.home>
2026-02-03 20:54:18 +0100

    test/testall.pl: Run Windows .exe tests under Wine on non-Windows hosts
    When the test file is a .exe and the OS is not Windows, run it via
    wine so that 'make check TARGET=WIN64' and 'make check TARGET=PC'
    succeed on Linux (and other Unix) hosts.

--- test/src/testall.pl
+++ test/src/testall.pl
@@ -9,7 +9,10 @@ my $harness = TAP::Harness->new({
 		my ($harness, $test_file) = @_;
 		# Let Perl tests run.
 		return undef if $test_file =~ /[.].pl$/;
-		if (-x $test_file) {
+		# Run Windows .exe under Wine when not on Windows
+		if ($test_file =~ /\.exe$/i && $^O ne 'MSWin32') {
+			return [ 'wine', $test_file ] if -f $test_file;
+		} elsif (-x $test_file) {
 			return [ $test_file ];
 		}
 		die "Unknown test file type - $test_file\n";
