3c5f0c150679afda5824deba0d844b401c615f18
Simon GH <groundhog@gmx.net>
2026-02-06 19:53:52 +0100

    Windows: OpenGL: Use Windows threads instead of pthread pthread is not available in Windows build

--- src/ui/canvas/custom/Cache.cpp
+++ src/ui/canvas/custom/Cache.cpp
@@ -200,7 +200,11 @@ TextCache::Result
 TextCache::Get(const Font &font, std::string_view text) noexcept
 {
 #ifdef ENABLE_OPENGL
+#ifdef _WIN32
+  assert(GetCurrentThreadId() == OpenGL::thread);
+#else
   assert(pthread_equal(pthread_self(), OpenGL::thread));
+#endif
 #endif
   assert(font.IsDefined());
 
@@ -261,7 +265,11 @@ void
 TextCache::Flush() noexcept
 {
 #ifdef ENABLE_OPENGL
+#ifdef _WIN32
+  assert(GetCurrentThreadId() == OpenGL::thread);
+#else
   assert(pthread_equal(pthread_self(), OpenGL::thread));
+#endif
 #endif
 
 #ifndef ENABLE_OPENGL
