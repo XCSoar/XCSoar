4757c3d2f7396d506d9da097936306301a925490
Simon GH <groundhog@gmx.net>
2026-02-06 19:53:52 +0100

    Windows: Fix inaccurate #ifdef logic in Windows builds Sometimes the #ifdef tests in the code were too broad: Testing for Windows when really they should be testing more specifically, e.g. for USE_GDI.

--- src/Audio/PCMPlayer.cpp
+++ src/Audio/PCMPlayer.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright The XCSoar Project
 
-#if !defined(ANDROID) && !defined(_WIN32)
+#if !defined(ANDROID) && (!defined(_WIN32) || defined(ENABLE_SDL))
 
 #include "PCMPlayer.hpp"
 
--- src/Form/Form.cpp
+++ src/Form/Form.cpp
@@ -241,7 +241,7 @@ WndForm::OnCancelMode() noexcept
   }
 }
 
-#ifdef _WIN32
+#ifdef USE_WINUSER
 
 bool
 WndForm::OnCommand(unsigned id, unsigned code) noexcept
