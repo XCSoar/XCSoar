cbb6bc2ba0f1a2cb5c2de1547080596517e1270e
Simon GH <groundhog@gmx.net>
2026-02-07 21:45:08 +0100

    Profile/Repository: Allow user-defined additional repositories Allows downloading one or more additional user repository files from URLs configured by the user. The entries are added to the main XCSoar repository content. Defines configuration key "UserRepositoriesList", which is an URL or a list of URLs separated by "|" (similar to existing path lists) pointing to the user repository location(s).

--- src/Repository/Glue.cpp
+++ src/Repository/Glue.cpp
@@ -5,9 +5,32 @@
 #include "net/http/DownloadManager.hpp"
 #include "system/Path.hpp"
 
+#include "util/StringFormat.hpp"
+#include "Profile/Keys.hpp"
+#include "Profile/Profile.hpp"
+#include "util/IterableSplitString.hxx"
+
+#include <vector>
+
 #define REPOSITORY_URI "http://download.xcsoar.org/repository"
 
 static bool repository_downloaded = false;
+
+std::vector<std::string> GetUserRepositoryURIs()
+{
+  std::vector<std::string> uris;
+
+  const char *src = Profile::Get(ProfileKeys::UserRepositoriesList);
+  if (src == nullptr) return uris;
+  if (*src == '\0') return uris;
+
+  for (auto i : TIterableSplitString(src, '|')) {
+    if (i.empty()) continue;
+    uris.emplace_back(i.data(), i.size());
+  }
+
+  return uris;
+}
 
 void
 EnqueueRepositoryDownload(bool force)
@@ -17,4 +40,16 @@ EnqueueRepositoryDownload(bool force)
 
   repository_downloaded = true;
   Net::DownloadManager::Enqueue(REPOSITORY_URI, Path("repository"));
+
+  // Enqueue additional user-defined repository URIs, if set
+  char filename[32];
+  std::vector<std::string> uris = GetUserRepositoryURIs();
+  int user_repository_index = 1;
+  for (const auto &uri : uris) {
+    if (uri.empty())
+      continue;
+    StringFormat(filename, std::size(filename), "user_repository_%d",
+                 user_repository_index++);
+    Net::DownloadManager::Enqueue(uri.c_str(), Path(filename));
+  }
 }
