b938714a574129f53d60b999ee822988f3075827
Simon GH <groundhog@gmx.net>
2026-02-07 21:45:08 +0100

    FileType/Downloads: Centrally define default download directories For target directories of downloaded assets, to be used in both FilePicker download and in FileManager downloads.

--- src/Dialogs/DownloadFilePicker.cpp
+++ src/Dialogs/DownloadFilePicker.cpp
@@ -20,7 +20,8 @@
 #include "ui/event/Notify.hpp"
 #include "ui/event/PeriodicTimer.hpp"
 #include "thread/Mutex.hxx"
-
+#include "LocalPath.hpp"
+#include "system/FileUtil.hpp"
 #include <vector>
 
 #include <cassert>
@@ -178,6 +179,7 @@ DownloadFilePickerWidget::OnPaintItem(Canvas &canvas, const PixelRect rc,
 void
 DownloadFilePickerWidget::Download()
 {
+  const int max_subdir_depth = 3;
   assert(Net::DownloadManager::IsAvailable());
 
   const unsigned current = GetList().GetCursorIndex();
@@ -186,7 +188,23 @@ DownloadFilePickerWidget::Download()
   const auto &file = items[current];
 
   try {
-    path = DownloadFileModal(_("Download"), file.GetURI(), file.GetName());
+    AllocatedPath dest_dir = GetFileTypeDefaultDir(file_type);
+
+    const Path file_path(file.GetName()); //AllocatedPath cannot take nullptr
+
+    if (!file_path.IsValidFilename())
+      throw std::runtime_error("Invalid download filename");
+
+    AllocatedPath relative_path(file_path);
+    if (dest_dir != nullptr) {
+      const auto dest_path = LocalPath(dest_dir);
+      Directory::CreateRecursive(dest_path);
+      if (!Directory::Exists(dest_path))
+        throw std::runtime_error("Directory does not exist and could not be created.");
+
+      relative_path = AllocatedPath::Build(Path(dest_dir), file_path);
+    }
+    path = DownloadFileModal(_("Download"), file.GetURI(), relative_path.c_str());
     if (path != nullptr)
       dialog.SetModalResult(mrOK);
   } catch (...) {
