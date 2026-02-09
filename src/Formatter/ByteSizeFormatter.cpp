9fe443827760ed233cdea4294f43b6ba59835629
Stefan Schumann <stefan.schumann@posteo.de>
2026-02-09 07:33:34 +0100

    Formatter/ByteSizeFormatter: switch from TCHAR to char

--- src/Formatter/ByteSizeFormatter.cpp
+++ src/Formatter/ByteSizeFormatter.cpp
@@ -8,16 +8,16 @@
 #include <cassert>
 
 void
-FormatByteSize(TCHAR *buffer, size_t size, uint64_t bytes, bool simple)
+FormatByteSize(char *buffer, size_t size, uint64_t bytes, bool simple)
 {
   assert(buffer != NULL);
   assert(size >= 8);
 
-  static const TCHAR *const units[] = {
-    _T("B"), _T("KB"), _T("MB"), _T("GB"), _T("TB")
+  static const char *const units[] = {
+    "B", "KB", "MB", "GB", "TB"
   };
-  static const TCHAR *const simple_units[] = {
-    _T("B"), _T("K"), _T("M"), _T("G"), _T("T")
+  static const char *const simple_units[] = {
+    "B", "K", "M", "G", "T"
   };
 
   double value = bytes;
@@ -25,15 +25,15 @@ FormatByteSize(TCHAR *buffer, size_t size, uint64_t bytes, bool simple)
   unsigned i = 0;
   for (; value >= 1024 && i < ARRAY_SIZE(units)-1; i++, value /= 1024);
 
-  const TCHAR *unit = simple ? simple_units[i] : units[i];
+  const char *unit = simple ? simple_units[i] : units[i];
 
-  const TCHAR *format;
+  const char *format;
   if (value >= 100 || i == 0)
-    format = simple ? _T("%.0f%s") : _T("%.0f %s");
+    format = simple ? "%.0f%s" : "%.0f %s";
   else if (value >= 10)
-    format = simple ? _T("%.1f%s") : _T("%.1f %s");
+    format = simple ? "%.1f%s" : "%.1f %s";
   else
-    format = simple ? _T("%.1f%s") : _T("%.2f %s");
+    format = simple ? "%.1f%s" : "%.2f %s";
 
   StringFormat(buffer, size, format, value, unit);
 }
