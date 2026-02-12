0891d9dc8b81f34d004143bd08532db482cb4c7d
Simon GH <groundhog@gmx.net>
2026-02-12 02:34:36 +0100

    Config/Files: Modal download of User repositories on URL change Instead of background reload, use modal dialog with progress bar after a change of the configured URL, so user gets clear confirmation of setting

--- src/Repository/Glue.cpp
+++ src/Repository/Glue.cpp
@@ -2,6 +2,7 @@
 // Copyright The XCSoar Project
 
 #include "Glue.hpp"
+#include "net/http/Features.hpp"
 #include "net/http/DownloadManager.hpp"
 #include "system/Path.hpp"
 
@@ -9,6 +10,12 @@
 #include "Profile/Keys.hpp"
 #include "Profile/Profile.hpp"
 #include "util/IterableSplitString.hxx"
+
+#ifdef HAVE_DOWNLOAD_MANAGER
+#include "Dialogs/DownloadFileModal.hpp"
+#include "Dialogs/Error.hpp"
+#include "Language/Language.hpp"
+#endif
 
 #include <vector>
 
@@ -33,23 +40,63 @@ std::vector<std::string> GetUserRepositoryURIs()
 }
 
 void
-EnqueueRepositoryDownload(bool force)
+EnqueueRepositoryDownload(bool force, bool main_repo, bool user_repo)
 {
-  if (repository_downloaded && !force)
-    return;
-
-  repository_downloaded = true;
-  Net::DownloadManager::Enqueue(REPOSITORY_URI, Path("repository"));
+  if (main_repo) {
+    if (!repository_downloaded || force) {
+      repository_downloaded = true;
+      Net::DownloadManager::Enqueue(REPOSITORY_URI, Path("repository"));
+    }
+  }
 
   // Enqueue additional user-defined repository URIs, if set
-  char filename[32];
-  std::vector<std::string> uris = GetUserRepositoryURIs();
-  int user_repository_index = 1;
-  for (const auto &uri : uris) {
-    if (uri.empty())
-      continue;
-    StringFormat(filename, std::size(filename), "user_repository_%d",
-                 user_repository_index++);
-    Net::DownloadManager::Enqueue(uri.c_str(), Path(filename));
+  if (user_repo) {
+    char filename[32];
+    std::vector<std::string> uris = GetUserRepositoryURIs();
+    int user_repository_index = 1;
+    for (const auto &uri : uris) {
+      if (uri.empty())
+        continue;
+      StringFormat(filename, std::size(filename), "user_repository_%d",
+                   user_repository_index++);
+      Net::DownloadManager::Enqueue(uri.c_str(), Path(filename));
+    }
   }
 }
+
+#ifdef HAVE_DOWNLOAD_MANAGER
+
+void
+DownloadRepositoriesModal(bool main_repo, bool user_repo)
+{
+  if (main_repo) {
+    try {
+      if (DownloadFileModal(_("Updating repository"), REPOSITORY_URI, "repository") == nullptr)
+        return; /* cancelled */
+    } catch (...) {
+      ShowError(std::current_exception(), _("Updating repository"));
+    }
+
+    repository_downloaded = true;
+  }
+
+  if (user_repo) {
+    const std::vector<std::string> uris = GetUserRepositoryURIs();
+    char filename[32];
+    int user_repository_index = 1;
+    for (const auto &uri : uris) {
+      if (uri.empty())
+        continue;
+      StringFormat(filename, sizeof(filename), "user_repository_%d",
+                   user_repository_index++);
+      try {
+        if (DownloadFileModal(_("Updating repository"), uri.c_str(), filename) == nullptr)
+          return; /* cancelled */
+      } catch (...) {
+        ShowError(std::current_exception(), _("Updating repository"));
+      }
+    }
+  }
+}
+
+#endif
