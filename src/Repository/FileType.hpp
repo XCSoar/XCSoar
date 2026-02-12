b052d26810d14743751e3a2d75264fd936a3178e
Stefan Schumann <stefan.schumann@posteo.de>
2026-02-12 11:07:07 +0100

    Repository/FileType: Extend type metadata API

--- src/Repository/FileType.hpp
+++ src/Repository/FileType.hpp
@@ -12,13 +12,25 @@ enum class FileType : uint8_t {
   WAYPOINTDETAILS,
   MAP,
   FLARMNET,
+  FLARMDB,
   IGC,
+  NMEA,
   RASP,
   XCI,
   TASK,
   CHECKLIST,
+  PROFILE,
+  PLANE,
+  COUNT,
 };
 
 class AllocatedPath;
 
 AllocatedPath GetFileTypeDefaultDir(const FileType file_type);
+
+/**
+ * Return NUL-separated list of file glob patterns for the given
+ * type (e.g. "*.txt\0*.air\0*.sua\0").  The list is terminated by
+ * an empty pattern.
+ */
+const char *GetFileTypePatterns(const FileType file_type) noexcept;
