65183d60e8194b89fe13182b7c92756682dfee87
Stefan Schumann <stefan.schumann@posteo.de>
2026-02-12 11:07:07 +0100

    Repository/FileType: Extend type metadata API

--- src/Repository/FileType.cpp
+++ src/Repository/FileType.cpp
@@ -5,29 +5,100 @@
 #include "system/Path.hpp"
 #include "Compatibility/path.h"
 
+const char *
+GetFileTypePatterns(const FileType file_type) noexcept
+{
+  switch (file_type) {
+  case FileType::AIRSPACE:
+    return "*.txt\0*.air\0*.sua\0";
+
+  case FileType::WAYPOINT:
+    return "*.dat\0*.xcw\0*.cup\0*.cupx\0*.wpz\0*.wpt\0";
+
+  case FileType::WAYPOINTDETAILS:
+    return "*.txt\0";
+
+  case FileType::MAP:
+    return "*.xcm\0*.lkm\0";
+
+  case FileType::FLARMNET:
+    return "*.fln\0";
+
+  case FileType::FLARMDB:
+    return "xcsoar-flarm.txt\0flarm-msg-data.csv\0";
+
+  case FileType::IGC:
+    return "*.igc\0";
+
+  case FileType::NMEA:
+    return "*.nmea\0";
+
+  case FileType::RASP:
+    return "*-rasp*.dat\0";
+
+  case FileType::XCI:
+    return "*.xci\0";
+
+  case FileType::TASK:
+    return "*.tsk\0*.cup\0*.igc\0";
+
+  case FileType::CHECKLIST:
+    return "*.xcc\0xcsoar-checklist.txt\0";
+
+  case FileType::PROFILE:
+    return "*.prf\0";
+
+  case FileType::PLANE:
+    return "*.xcp\0";
+
+  case FileType::UNKNOWN:
+  case FileType::COUNT:
+    return "\0";
+  }
+
+  return "\0";
+}
+
 AllocatedPath
 GetFileTypeDefaultDir(const FileType file_type)
 {
   switch (file_type) {
-  case FileType::RASP:
-    return AllocatedPath::Build("weather", "rasp");
-
-  case FileType::MAP:
-    return AllocatedPath("maps");
-
   case FileType::AIRSPACE:
     return AllocatedPath("airspace");
 
   case FileType::WAYPOINT:
     return AllocatedPath("waypoints");
 
-  case FileType::UNKNOWN:
   case FileType::WAYPOINTDETAILS:
+    return AllocatedPath::Build("waypoints", "details");
+
+  case FileType::MAP:
+    return AllocatedPath("maps");
+
+  case FileType::FLARMDB:
   case FileType::FLARMNET:
-  case FileType::IGC:
+    return AllocatedPath("flarm");
+
+  case FileType::RASP:
+    return AllocatedPath::Build("weather", "rasp");
+
   case FileType::XCI:
+    return AllocatedPath("input");
+
   case FileType::TASK:
+    return AllocatedPath("tasks");
+
   case FileType::CHECKLIST:
+    return AllocatedPath("checklists");
+
+  case FileType::IGC:
+  case FileType::NMEA:
+    return AllocatedPath("logs");
+
+  case FileType::PROFILE:
+  case FileType::PLANE:
+  case FileType::UNKNOWN:
+  case FileType::COUNT:
     return nullptr;
   }
 
