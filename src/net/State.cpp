e960852d7f9372dc162940e6481685800e9ddc6a
Yorick Reum <git@yorickreum.de>
2026-02-16 19:23:46 +0530

    net/State: Implements PollNetState for macOS using NWPathMonitor

--- src/net/State.cpp
+++ src/net/State.cpp
@@ -24,6 +24,7 @@ GetNetState()
 #else // non-Android platforms
 
 #if defined(HAVE_NET_STATE)
+#include <atomic>
 #include <mutex>
 #include <chrono>
 #include "time/PeriodClock.hpp"
@@ -33,6 +34,12 @@ GetNetState()
 #include <cstdio>
 #include <dirent.h>
 #include "system/FileUtil.hpp"
+#endif
+
+#if defined(__APPLE__)
+#if !TARGET_OS_IPHONE
+#include <Network/Network.h>
+#endif
 #endif
 
 #if defined(_WIN32)
@@ -45,6 +52,46 @@ namespace {
 static std::mutex net_state_mutex;
 static NetState cached_net_state = NetState::UNKNOWN;
 static PeriodClock last_update;
+
+#if defined(__APPLE__)
+#if !TARGET_OS_IPHONE
+static std::once_flag nw_path_monitor_once;
+static std::atomic<NetState> nw_path_monitor_state{NetState::UNKNOWN};
+static std::atomic<bool> nw_path_monitor_ready{false};
+
+static void
+InitPathMonitor() noexcept
+{
+  nw_path_monitor_t monitor = nw_path_monitor_create();
+  if (monitor == nullptr)
+    return;
+
+  dispatch_queue_t queue = dispatch_queue_create("org.xcsoar.net.path-monitor",
+                                                 DISPATCH_QUEUE_SERIAL);
+  if (queue == nullptr)
+    return;
+
+  nw_path_monitor_set_queue(monitor, queue);
+  nw_path_monitor_set_update_handler(monitor, ^(nw_path_t path) {
+    const nw_path_status_t status = nw_path_get_status(path);
+    const NetState state = status == nw_path_status_satisfied
+      ? NetState::CONNECTED
+      : status == nw_path_status_unsatisfied
+        ? NetState::DISCONNECTED
+        : NetState::UNKNOWN;
+
+    nw_path_monitor_state.store(state, std::memory_order_relaxed);
+    nw_path_monitor_ready.store(true, std::memory_order_release);
+  });
+  nw_path_monitor_start(monitor);
+
+  static nw_path_monitor_t global_monitor = monitor;
+  static dispatch_queue_t global_queue = queue;
+  (void)global_monitor;
+  (void)global_queue;
+}
+#endif
+#endif
 
 static NetState
 PollNetState() noexcept
@@ -81,6 +128,17 @@ PollNetState() noexcept
   DWORD flags = 0;
   BOOL connected = InternetGetConnectedState(&flags, 0);
   return connected ? NetState::CONNECTED : NetState::DISCONNECTED;
+
+#elif defined(__APPLE__)
+#if !TARGET_OS_IPHONE
+  std::call_once(nw_path_monitor_once, InitPathMonitor);
+  if (!nw_path_monitor_ready.load(std::memory_order_acquire))
+    return NetState::UNKNOWN;
+
+  return nw_path_monitor_state.load(std::memory_order_relaxed);
+#else
+  return NetState::UNKNOWN;
+#endif
 
 #else
   return NetState::UNKNOWN;
