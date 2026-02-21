8520db8fd152f38b4ab997ee2ee63ec5a6b6e87f
Simon GH <groundhog@gmx.net>
2026-02-21 16:19:48 +0100

    RaspRenderer: Refactor LookupWeatherTerrainStyle Into RaspStyle.cpp - it is not really related to the renderer itself

--- src/Weather/Rasp/RaspStyle.cpp
+++ src/Weather/Rasp/RaspStyle.cpp
@@ -2,3 +2,29 @@
 // Copyright The XCSoar Project
 
 #include "RaspStyle.hpp"
+#include "util/StringAPI.hxx"
+
+const RaspStyle &
+LookupWeatherTerrainStyle(const char *name)
+{
+  if (name == nullptr)
+    name = "";
+
+  const auto *i = rasp_styles;
+  while (i->name != nullptr && !StringIsEqual(i->name, name))
+    ++i;
+
+  if(i->name == nullptr) {
+    // If no exact match, try matching the short codes
+    // to the end of the field name
+    if(StringLength(name) >= 3) {
+      const char *short_name =
+        &name[StringLength(name) - 3];
+      i = rasp_colormaps_general;
+      while (i->name != nullptr
+             && !StringIsEqual(i->name, short_name))
+        ++i;
+    }
+  }
+  return *i;
+}
