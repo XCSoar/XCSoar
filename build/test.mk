15b0970ed25f820b5df79a9f33d7c5cf6925dd16
Simon GH <groundhog@gmx.net>
2026-02-21 16:16:06 +0100

    Weather/Rasp: Refactor color tables into separate file To prepare for functional code to move into RaspStyle.cpp

--- build/test.mk
+++ build/test.mk
@@ -2118,6 +2118,7 @@ RUN_MAP_WINDOW_SOURCES = \
 	$(SRC)/Weather/Rasp/RaspRenderer.cpp \
 	$(SRC)/Weather/Rasp/ColorMap.cpp \
 	$(SRC)/Weather/Rasp/RaspStyle.cpp \
+	$(SRC)/Weather/Rasp/RaspStylesData.cpp \
 	$(SRC)/Renderer/FAITriangleAreaRenderer.cpp \
 	$(SRC)/Renderer/OZRenderer.cpp \
 	$(SRC)/Renderer/TaskRenderer.cpp \
