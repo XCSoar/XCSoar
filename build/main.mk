87b5b2252285eeabdb82d4d85c183dd0a733e7e7
Stefan Schumann <stefan.schumann@posteo.de>
2026-02-24 13:46:03 +0100

    io/TarBackup: tar backup and restore helpers

--- build/main.mk
+++ build/main.mk
@@ -22,6 +22,7 @@ DIALOG_SOURCES = \
 	$(SRC)/Dialogs/DataManagement/DataManagement.cpp \
 	$(SRC)/Dialogs/DataManagement/ExportFlightsPanel.cpp \
 	$(SRC)/Dialogs/DataManagement/StorageLocationPickerDialog.cpp \
+	$(SRC)/io/TarBackup.cpp \
 	$(SRC)/Dialogs/DataManagement/FileTransferUtil.cpp \
 	$(SRC)/IGC/IgcMetaCache.cpp \
 	$(SRC)/Dialogs/Device/PortDataField.cpp \
