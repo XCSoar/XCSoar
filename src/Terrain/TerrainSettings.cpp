76271758a190c14e1f4ea9947004a5dd403f6ce5
Simon GH <groundhog@gmx.net>
2026-02-28 17:06:12 +0100

    TerrainRenderer/Config: Make contour line density adjustable Provides four presets for line density and line density scaling on zoom. Also refactors the dynamic terrain contour scaling logic out of RasterRenderer into terrain-related code.

--- src/Terrain/TerrainSettings.cpp
+++ src/Terrain/TerrainSettings.cpp
@@ -3,6 +3,8 @@
 
 #include "Terrain/TerrainSettings.hpp"
 
+#include <algorithm>
+
 void
 TerrainRendererSettings::SetDefaults()
 {
@@ -12,4 +14,73 @@ TerrainRendererSettings::SetDefaults()
   brightness = 192;
   ramp = 0;
   contours = Contours::OFF;
+}
+
+/**
+ * Maximum contour height scale (bit shift): stop the contour scaling
+ * at this level (contour interval > Mount Everest height)
+ */
+static constexpr unsigned CONTOUR_HEIGHT_SCALE_MAX = 15;
+
+/**
+ * Contour density preset definition:
+ * base_scale_offset: added to height_scale to compute the starting
+ *   contour_height_scale.  Higher values = coarser base interval.
+ * min_spacing_factor: contour interval is at least this multiple
+ *   of pixel_size.  Higher value = more thinning when zoomed out.
+ */
+
+struct ContourModeParams {
+  int base_scale_offset;
+  double min_spacing_factor;
+};
+
+// Mountains: coarse base interval (256 m at height_scale=4)
+static constexpr ContourModeParams CONTOUR_MOUNTAINS{4, 3.0};
+
+// Highlands: medium-coarse base interval (64 m at height_scale=4)
+static constexpr ContourModeParams CONTOUR_HIGHLANDS{2, 1.5};
+
+// Lowlands: medium-fine base interval (16 m at height_scale=4)
+static constexpr ContourModeParams CONTOUR_LOWLANDS{0, 1.0};
+
+// Superfine: very fine base interval (8 m at height_scale=4)
+static constexpr ContourModeParams CONTOUR_SUPERFINE{-1, 1.0};
+
+unsigned
+ContourSpacing(Contours contours, unsigned height_scale,
+               double pixel_size) noexcept
+{
+  if (contours == Contours::OFF)
+    return 0;
+
+  const auto &params = [&]() -> const ContourModeParams & {
+    switch (contours) {
+    case Contours::HIGHLANDS:
+      return CONTOUR_HIGHLANDS;
+    case Contours::LOWLANDS:
+      return CONTOUR_LOWLANDS;
+    case Contours::SUPERFINE:
+      return CONTOUR_SUPERFINE;
+    case Contours::MOUNTAINS:
+    default:
+      return CONTOUR_MOUNTAINS;
+    }
+  }();
+
+  unsigned contour_height_scale =
+    std::min((unsigned)std::max(0,
+                                (int)height_scale
+                                + params.base_scale_offset),
+             CONTOUR_HEIGHT_SCALE_MAX);
+
+  unsigned interval = 1u << contour_height_scale;
+  while (interval < (unsigned)(pixel_size
+                               * params.min_spacing_factor)
+         && contour_height_scale < CONTOUR_HEIGHT_SCALE_MAX) {
+    interval <<= 1;
+    contour_height_scale++;
+  }
+
+  return interval;
 }
