956090d698b0a67b6949701283c4cc53d0254dde
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-03-02 19:24:35 +0100

    InfoBoxes/Content/Time: Open Times status tab on click
    Make the Flight duration, Time local and Time UTC InfoBoxes clickable
    and route them to the Status dialog Times tab. This aligns these
    time-oriented boxes with their detailed status panel.

--- src/InfoBoxes/Content/Time.hpp
+++ src/InfoBoxes/Content/Time.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "InfoBoxes/Content/Base.hpp"
+
 struct InfoBoxData;
 
 void
@@ -13,3 +15,21 @@ UpdateInfoBoxTimeUTC(InfoBoxData &data) noexcept;
 
 void
 UpdateInfoBoxTimeFlight(InfoBoxData &data) noexcept;
+
+class InfoBoxContentTimeFlight final : public InfoBoxContent {
+public:
+  void Update(InfoBoxData &data) noexcept override;
+  bool HandleClick() noexcept override;
+};
+
+class InfoBoxContentTimeLocal final : public InfoBoxContent {
+public:
+  void Update(InfoBoxData &data) noexcept override;
+  bool HandleClick() noexcept override;
+};
+
+class InfoBoxContentTimeUTC final : public InfoBoxContent {
+public:
+  void Update(InfoBoxData &data) noexcept override;
+  bool HandleClick() noexcept override;
+};
