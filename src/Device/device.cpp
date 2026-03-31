47fff4f6cd2eabbe04b9f27a87045d3b4b398f1b
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-03-31 12:48:48 +0200

    Device: Default internal GPS to last slot
    On Android/iOS, move the default and fallback configuration for the built-in GPS
    & sensors to the last device slot, preserving early slots for external devices.

--- src/Device/device.cpp
+++ src/Device/device.cpp
@@ -114,7 +114,7 @@ devStartup(MultipleDevices &devices, const SystemSettings &settings)
     config.Clear();
     config.port_type = DeviceConfig::PortType::INTERNAL;
 
-    DeviceDescriptor &device = devices[0];
+    DeviceDescriptor &device = devices[NUMDEV - 1];
     devInitOne(device, config);
 #endif
   }
