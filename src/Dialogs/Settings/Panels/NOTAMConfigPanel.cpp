a355a8deb5f65e3e524bd2641c9f82f217e83098
Yorick Reum <git@yorickreum.de>
2026-04-06 22:57:28 +0200

    Dialogs: Add NOTAM briefing disclaimer

--- src/Dialogs/Settings/Panels/NOTAMConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/NOTAMConfigPanel.cpp
@@ -38,6 +38,7 @@
 enum ControlIndex {
 #ifdef HAVE_HTTP
   ENABLE_NOTAM,
+  NOTICE,
   API_URL,
   NOTAM_RADIUS,
   REFRESH_INTERVAL,
@@ -96,6 +97,8 @@ NOTAMConfigPanel::Prepare([[maybe_unused]] ContainerWindow &parent,
   AddBoolean(_("NOTAM Support"),
              _("Enable downloading and display of NOTAMs from aviation authorities."),
              computer.notam.enabled, this);
+  AddMultiLine(_("Notice: NOTAM display is for situational awareness only\n"
+                 "and does not replace proper pre-flight NOTAM briefing."));
 
   AddText(_("API URL"),
           _("Base URL of the NOTAM proxy API. Must be configured before NOTAMs can be fetched."),
