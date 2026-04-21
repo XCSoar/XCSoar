6ba3c2bd63f2d079272a0a1cd98ca68c73a1e4e7
pheinrich210 <pheinrich210@gmail.com>
2026-04-21 16:42:32 +0200

    Weather/xctherm: Add JWT authentication and API client
    Introduce XCThermAuth with login and refresh-token support, XCThermAPI
    for forecast access, MVT tile overlay, weather dialog and config UI,
    and profile keys for XCTherm credentials.

--- src/Profile/WeatherProfile.cpp
+++ src/Profile/WeatherProfile.cpp
@@ -5,6 +5,7 @@
 #include "Map.hpp"
 #include "Keys.hpp"
 #include "Weather/Settings.hpp"
+#include "util/StaticString.hxx"
 
 #ifdef HAVE_PCMET
 
@@ -32,5 +33,27 @@ Profile::Load(const ProfileMap &map, WeatherSettings &settings)
 
 #ifdef HAVE_HTTP
   map.Get(ProfileKeys::EnableThermalInformationMap, settings.enable_tim);
+  map.Get(ProfileKeys::XCThermEnabled, settings.xctherm.enabled);
+  map.Get(ProfileKeys::XCThermShowOnMainMap, settings.xctherm.show_on_main_map);
 #endif
+
+  {
+    StaticString<64> xctherm_email;
+    map.Get(ProfileKeys::XCThermEmail, xctherm_email);
+    if (!xctherm_email.empty())
+      settings.xctherm.credentials.email = xctherm_email;
+  }
+
+  {
+    StaticString<64> xctherm_password;
+    map.Get(ProfileKeys::XCThermPassword, xctherm_password);
+    if (!xctherm_password.empty())
+      settings.xctherm.credentials.password = xctherm_password;
+  }
+
+  map.Get(ProfileKeys::XCThermModel, settings.xctherm.model);
+  map.Get(ProfileKeys::XCThermParameter, settings.xctherm.parameter);
+  map.Get(ProfileKeys::XCThermWaveHeight, settings.xctherm.wave_height);
+  map.Get(ProfileKeys::XCThermVerticalWindAGL,
+          settings.xctherm.vertical_wind_agl);
 }
