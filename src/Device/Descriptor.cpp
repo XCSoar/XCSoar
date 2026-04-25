OpenSoaring/OpenSoar

Device/Descriptor.cpp - BUGFIX: don't show an error message window if env isn't  valid

src/Device/Descriptor.cpp
@@ -397,7 +397,10 @@ try {
       StaticString<256> msg;
       LockSetErrorMessage(what.c_str());
       msg.Format("%s: %s (%s)", _("Unable to open port"), name, what.c_str());
-      env.SetErrorMessage(msg);
+      if (!env.IsCancelled())
+        env.SetErrorMessage(msg);
+      else
+        LogFmt("Device-Error without Env: {}", msg.data());
     }
 
     return false;

XCSoar/XCSoar

src/Device/Descriptor.cpp
@@ -42,6 +42,7 @@
 #endif
 
 #include <cassert>
+#include <exception>
 #include <optional>
 
 class OpenDeviceJob final : public Job {
@@ -404,7 +405,14 @@ try {
 
       StaticString<256> _msg;
       _msg.Format("%s: %s (%s)", _("Unable to open port"), name, msg.c_str());
-      env.SetErrorMessage(_msg);
+      try {
+        if (!env.IsCancelled())
+          env.SetErrorMessage(_msg);
+        else
+          LogFmt("Device-Error without Env: {}", _msg.c_str());
+      } catch ([[maybe_unused]] const std::exception &ex) {
+        LogFmt("Device-Exception without Env: {}", _msg.c_str());
+      }
     }
 
     return false;