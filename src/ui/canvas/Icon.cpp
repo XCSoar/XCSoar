99fc5c9d6d15aa90c7379883da431897f5728e0f
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-05-26 01:23:57 +0200

    ui/canvas: Tint traffic icons with body and outline colours
    Add a dedicated traffic_icon_shader that partitions each pixel between
    altitude/alarm body colour and a contrasting outline in one pass.
    MaskedIcon draws rotated tinted icons without touching the solid-shader
    matrix; CanvasRotateShift restores solid_shader on destruction.

--- src/ui/canvas/Icon.cpp
+++ src/ui/canvas/Icon.cpp
@@ -3,6 +3,9 @@
 
 #include "Icon.hpp"
 #include "Canvas.hpp"
+#include "ui/canvas/Color.hpp"
+#include "Math/Angle.hpp"
+#include "Math/FastRotation.hpp"
 #include "Screen/Layout.hpp"
 
 #ifdef ENABLE_OPENGL
@@ -11,6 +14,7 @@
 
 #include "opengl/Shaders.hpp"
 #include "opengl/Program.hpp"
+#include "opengl/VertexPointer.hpp"
 #elif !defined(USE_GDI)
 #include "VirtualCanvas.hpp"
 #endif
@@ -154,6 +158,13 @@ MaskedIcon::Draw([[maybe_unused]] Canvas &canvas, PixelPoint p) const noexcept
 
 void
 MaskedIcon::Draw(Canvas &canvas, PixelPoint p,
+                 unsigned target_height, const Angle angle) const noexcept
+{
+  Draw(canvas, p, target_height, angle, COLOR_WHITE);
+}
+
+void
+MaskedIcon::Draw(Canvas &canvas, PixelPoint p,
                  unsigned target_height) const noexcept
 {
   assert(IsDefined());
@@ -166,13 +177,11 @@ MaskedIcon::Draw(Canvas &canvas, PixelPoint p,
   if (size.height == 0)
     return;
 
-  /* uniformly scaled size */
   const PixelSize scaled_size = {
     size.width * target_height / size.height,
     target_height,
   };
 
-  /* scale the hotspot (origin) proportionally */
   const PixelPoint dest = {
     p.x - int(origin.x * target_height / size.height),
     p.y - int(origin.y * target_height / size.height),
@@ -214,9 +223,6 @@ MaskedIcon::Draw(Canvas &canvas, PixelPoint p,
   canvas.SetTextColor(old_text_color);
   canvas.SetBackgroundColor(old_bg_color);
 #else
-  /* memory canvas: stretch each half (mask / icon) into a temporary
-     surface, then composite with CopyOr + CopyAnd (or CopyNotOr for
-     dark-mode inversion). */
   const Color old_text_color = canvas.GetTextColor();
   const bool inverse = !has_colors &&
     IsDarkBackground(old_text_color);
@@ -238,6 +244,165 @@ MaskedIcon::Draw(Canvas &canvas, PixelPoint p,
   }
 #endif
 }
+
+#ifdef ENABLE_OPENGL
+static void
+DrawTrafficIconQuad(const PixelSize &icon_size, GLTexture &texture,
+                    PixelPoint p, unsigned icon_height, Angle angle,
+                    Color body_color, Color outline_color) noexcept
+{
+  if (icon_height == 0 || icon_size.height == 0)
+    return;
+
+  const PixelSize scaled_size = {
+    icon_size.width * icon_height / icon_size.height,
+    icon_height,
+  };
+
+  OpenGL::traffic_icon_shader->Use();
+  body_color.Uniform(OpenGL::traffic_icon_body_color);
+  outline_color.Uniform(OpenGL::traffic_icon_outline_color);
+
+  texture.Bind();
+
+  BulkPixelPoint vertices[4];
+  if (angle != Angle::Zero()) {
+    const int half_width = int(scaled_size.width / 2);
+    const int half_height = int(scaled_size.height / 2);
+    const FastRotation rotation(angle);
+    const auto corner = [&](int x, int y) {
+      const auto rotated =
+        rotation.Rotate(DoublePoint2D{double(x), double(y)});
+      return PixelPoint{
+        p.x + int(rotated.x),
+        p.y + int(rotated.y),
+      };
+    };
+
+    vertices[0] = corner(-half_width, -half_height);
+    vertices[1] = corner(half_width, -half_height);
+    vertices[2] = corner(-half_width, half_height);
+    vertices[3] = corner(half_width, half_height);
+  } else {
+    const PixelPoint dest = {
+      p.x - int(icon_size.width * icon_height / icon_size.height / 2),
+      p.y - int(icon_height / 2),
+    };
+    const PixelRect rect(dest, scaled_size);
+    vertices[0] = rect.GetTopLeft();
+    vertices[1] = rect.GetTopRight();
+    vertices[2] = rect.GetBottomLeft();
+    vertices[3] = rect.GetBottomRight();
+  }
+
+  const ScopeVertexPointer vp(vertices);
+
+  const PixelRect src = texture.GetRect();
+  const PixelSize allocated = texture.GetAllocatedSize();
+  const GLfloat x0 = GLfloat(src.left) / allocated.width;
+  const GLfloat y0 = GLfloat(src.top) / allocated.height;
+  const GLfloat x1 = GLfloat(src.right) / allocated.width;
+  const GLfloat y1 = GLfloat(src.bottom) / allocated.height;
+  const bool flipped = texture.IsFlipped();
+
+  const GLfloat coord[] = {
+    x0, flipped ? y1 : y0,
+    x1, flipped ? y1 : y0,
+    x0, flipped ? y0 : y1,
+    x1, flipped ? y0 : y1,
+  };
+
+  glEnableVertexAttribArray(OpenGL::Attribute::TEXCOORD);
+  glVertexAttribPointer(OpenGL::Attribute::TEXCOORD, 2, GL_FLOAT, GL_FALSE,
+                        0, coord);
+
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+  glDisableVertexAttribArray(OpenGL::Attribute::TEXCOORD);
+}
+#endif
+
+void
+MaskedIcon::Draw(Canvas &canvas, PixelPoint p,
+                 unsigned target_height, const Angle angle,
+                 const Color body_color) const noexcept
+{
+  assert(IsDefined());
+
+  if (target_height == 0 || target_height == size.height) {
+    Draw(canvas, p);
+    return;
+  }
+
+  if (size.height == 0)
+    return;
+
+  const Color outline_color = IsDarkBackground(canvas.GetTextColor())
+    ? COLOR_WHITE
+    : COLOR_BLACK;
+
+#ifdef ENABLE_OPENGL
+  const ScopeAlphaBlend alpha_blend;
+  GLTexture &texture = *bitmap.GetNative();
+  DrawTrafficIconQuad(size, texture, p, target_height, angle,
+                      body_color, outline_color);
+#else
+  const PixelSize scaled_size = {
+    size.width * target_height / size.height,
+    target_height,
+  };
+  const PixelPoint dest = {
+    p.x - int(origin.x * target_height / size.height),
+    p.y - int(origin.y * target_height / size.height),
+  };
+
+  DrawMonochromeIcon(bitmap, size, canvas, dest, scaled_size, false);
+#endif
+}
+
+#if !defined(ENABLE_OPENGL)
+static void
+DrawMonochromeIcon(const Bitmap &bitmap, const PixelSize &icon_size,
+                   Canvas &canvas, const PixelPoint dest,
+                   const PixelSize &scaled_size, bool inverse) noexcept
+{
+#ifdef USE_GDI
+  const Color old_text_color = canvas.GetTextColor();
+  const Color old_bg_color = canvas.GetBackgroundColor();
+  canvas.SetTextColor(COLOR_BLACK);
+  canvas.SetBackgroundColor(COLOR_WHITE);
+
+  if (inverse) {
+    canvas.Stretch(dest, scaled_size,
+                   bitmap, {(int)icon_size.width, 0}, icon_size, MERGEPAINT);
+  } else {
+    canvas.Stretch(dest, scaled_size,
+                   bitmap, {0, 0}, icon_size, SRCPAINT);
+    canvas.Stretch(dest, scaled_size,
+                   bitmap, {(int)icon_size.width, 0}, icon_size, SRCAND);
+  }
+
+  canvas.SetTextColor(old_text_color);
+  canvas.SetBackgroundColor(old_bg_color);
+#else
+  VirtualCanvas temp{scaled_size};
+
+  if (inverse) {
+    temp.Stretch({0, 0}, scaled_size,
+                 bitmap, {(int)icon_size.width, 0}, icon_size);
+    canvas.CopyNotOr(dest, scaled_size, temp, {0, 0});
+  } else {
+    temp.Stretch({0, 0}, scaled_size,
+                 bitmap, {0, 0}, icon_size);
+    canvas.CopyOr(dest, scaled_size, temp, {0, 0});
+
+    temp.Stretch({0, 0}, scaled_size,
+                 bitmap, {(int)icon_size.width, 0}, icon_size);
+    canvas.CopyAnd(dest, scaled_size, temp, {0, 0});
+  }
+#endif
+}
+#endif
 
 void
 MaskedIcon::Draw(Canvas &canvas, const PixelRect &rc,
