60c44580bebbd2aa0c5008662bc2eb0215b64a55
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-06-08 19:03:20 +0200

    Dialogs/Weather/RASPDialog: Show file age and open file manager
    Add a read-only Modified row with the local timestamp of the selected
    RASP file so pilots can see how fresh the forecast is without leaving
    the weather dialog.
    
    Add an Update button on HTTP builds that opens the existing file
    manager instead of duplicating repository download logic. After the
    file manager closes, reload the configured RASP store and refresh the
    display. Validate BrokenDateTime before ToLocal() in GetFileModifiedTime().

--- src/Dialogs/Weather/RASPDialog.cpp
+++ src/Dialogs/Weather/RASPDialog.cpp
@@ -7,20 +7,37 @@
 #include "Weather/Rasp/RaspStore.hpp"
 #include "Profile/Keys.hpp"
 #include "Profile/Profile.hpp"
+#include "Form/Button.hpp"
 #include "Form/Edit.hpp"
 #include "Repository/FileType.hpp"
 #include "DataGlobals.hpp"
 #include "UIGlobals.hpp"
+#include "UtilsSettings.hpp"
 #include "Language/Language.hpp"
+#include "util/StaticString.hxx"
+#include "net/http/Features.hpp"
+#ifdef HAVE_DOWNLOAD_MANAGER
+#include "Dialogs/FileManager.hpp"
+#include "net/http/DownloadManager.hpp"
+#endif
 
 class RASPSettingsPanel final
   : public RowFormWidget {
 
   enum Controls {
     FILE,
+    MODIFIED,
   };
 
   std::shared_ptr<RaspStore> rasp;
+
+#ifdef HAVE_DOWNLOAD_MANAGER
+  Button *update_button = nullptr;
+#endif
+
+  void ReloadRasp();
+  void UpdateModifiedDisplay();
+  void UpdateClicked();
 
 public:
   explicit RASPSettingsPanel(std::shared_ptr<RaspStore> &&_rasp) noexcept
@@ -32,6 +49,50 @@ public:
 };
 
 void
+RASPSettingsPanel::ReloadRasp()
+{
+  rasp = LoadConfiguredRasp(false);
+  DataGlobals::SetRasp(rasp);
+  RaspFileChanged = true;
+  Profile::Save();
+  UpdateModifiedDisplay();
+}
+
+void
+RASPSettingsPanel::UpdateModifiedDisplay()
+{
+  StaticString<32> buffer;
+
+  if (rasp != nullptr) {
+    const BrokenDateTime modified = rasp->GetFileModifiedTime();
+    if (modified.IsPlausible()) {
+      buffer.Format("%04u-%02u-%02u %02u:%02u",
+                      modified.year, modified.month, modified.day,
+                      modified.hour, modified.minute);
+    }
+  }
+
+  if (buffer.empty())
+    buffer = _("Unknown");
+
+  SetText(MODIFIED, buffer.c_str());
+}
+
+void
+RASPSettingsPanel::UpdateClicked()
+{
+#ifdef HAVE_DOWNLOAD_MANAGER
+  ShowFileManager();
+
+  const AllocatedPath profile_path = Profile::GetPath(ProfileKeys::RaspFile);
+  if (profile_path != nullptr)
+    LoadValue(FILE, Path(profile_path));
+
+  ReloadRasp();
+#endif
+}
+
+void
 RASPSettingsPanel::Prepare([[maybe_unused]] ContainerWindow &parent,
                            [[maybe_unused]] const PixelRect &rc) noexcept
 {
@@ -41,11 +102,20 @@ RASPSettingsPanel::Prepare([[maybe_unused]] ContainerWindow &parent,
                             FileType::RASP);
   wp->GetDataField()->SetOnModified([this]{
     if (SaveValueFileReader(FILE, ProfileKeys::RaspFile)) {
-      rasp = LoadConfiguredRasp(false);
-      DataGlobals::SetRasp(rasp);
-      Profile::Save();
+      ReloadRasp();
+      GetControl(FILE).RefreshDisplay();
     }
   });
+
+  AddReadOnly(_("Modified"),
+              _("Local date and time of the selected RASP file."));
+  UpdateModifiedDisplay();
+
+#ifdef HAVE_DOWNLOAD_MANAGER
+  update_button = AddButton(_("Update"), [this]{ UpdateClicked(); });
+  if (!Net::DownloadManager::IsAvailable())
+    update_button->SetEnabled(false);
+#endif
 }
 
 bool
