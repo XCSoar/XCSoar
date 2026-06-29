1ab0130b600098d14d435a26c5a8c161b0b5462d
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-06-29 22:46:50 +0200

    FLARM/Traffic: Add OGN and SkyLines source types
    Extend FlarmTraffic::SourceType so online traffic merged from the
    cloud and SkyLines can be tagged and labelled separately from device
    FLARM and ADS-B sources.

--- src/FLARM/Traffic.cpp
+++ src/FLARM/Traffic.cpp
@@ -3,6 +3,33 @@
 
 #include "FLARM/Traffic.hpp"
 
+#include "util/Macros.hpp"
+
+#include <cstddef>
+
+namespace {
+
+struct SourceStringEntry {
+  FlarmTraffic::SourceType type;
+  const char *label;
+};
+
+constexpr SourceStringEntry source_strings[] = {
+  { FlarmTraffic::SourceType::FLARM, "FLARM" },
+  { FlarmTraffic::SourceType::ADSB, "ADS-B" },
+  { FlarmTraffic::SourceType::ADSR, "ADS-R" },
+  { FlarmTraffic::SourceType::TISB, "TIS-B" },
+  { FlarmTraffic::SourceType::MODES, "Mode-S" },
+  { FlarmTraffic::SourceType::OGN, "OGN" },
+  { FlarmTraffic::SourceType::SKYLINES, "SkyLines" },
+  { FlarmTraffic::SourceType::CLOUD, "Cloud" },
+};
+
+static_assert(ARRAY_SIZE(source_strings) == 8,
+              "source_strings must list every SourceType label");
+
+} // namespace
+
 static constexpr const char *acTypes[] = {
   "Unknown",
   "Glider",
@@ -35,20 +62,20 @@ FlarmTraffic::GetTypeString(AircraftType type) noexcept
 const char *
 FlarmTraffic::GetSourceString(SourceType source) noexcept
 {
-  switch (source) {
-  case SourceType::FLARM:
-    return "FLARM";
-  case SourceType::ADSB:
-    return "ADS-B";
-  case SourceType::ADSR:
-    return "ADS-R";
-  case SourceType::TISB:
-    return "TIS-B";
-  case SourceType::MODES:
-    return "Mode-S";
+  for (const auto &entry : source_strings) {
+    if (entry.type == source)
+      return entry.label;
   }
 
   return "Unknown";
+}
+
+bool
+FlarmTraffic::IsInjectedSource(SourceType source) noexcept
+{
+  return source == SourceType::OGN ||
+    source == SourceType::SKYLINES ||
+    source == SourceType::CLOUD;
 }
 
 void
@@ -73,4 +100,24 @@ FlarmTraffic::Update(const FlarmTraffic &other) noexcept
   rssi = other.rssi;
   rssi_available = other.rssi_available;
   no_track = other.no_track;
+}
+
+void
+FlarmTraffic::UpdateOnline(const FlarmTraffic &built) noexcept
+{
+  location = built.location;
+  location_available = built.location_available;
+  altitude = built.altitude;
+  altitude_available = built.altitude_available;
+  name = built.name;
+  source = built.source;
+  type = built.type;
+  id_type = built.id_type;
+  relative_north = built.relative_north;
+  relative_east = built.relative_east;
+  relative_altitude = built.relative_altitude;
+  if (built.track_received) {
+    track = built.track;
+    track_received = true;
+  }
 }
