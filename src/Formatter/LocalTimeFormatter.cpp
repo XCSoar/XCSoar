7892915d9cda38b73c113521b315be1aa2b02f04
Stefan Schumann <stefan.schumann@posteo.de>
2026-07-13 18:10:25 +0200

    Formatter/LocalTimeFormatter: format local date and time
    Add the YYYY-MM-DD HH:MM formatter needed where a local forecast timestamp is displayed.

--- src/Formatter/LocalTimeFormatter.cpp
+++ src/Formatter/LocalTimeFormatter.cpp
@@ -3,12 +3,28 @@
 
 #include "LocalTimeFormatter.hpp"
 #include "TimeFormatter.hpp"
+#include "time/BrokenDateTime.hpp"
 #include "time/LocalTime.hpp"
 #include "time/RoughTime.hpp"
+#include "util/StringFormat.hpp"
+
+#include <chrono>
 
 void
 FormatLocalTimeHHMM(char *buffer, TimeStamp time,
                     RoughTimeDelta utc_offset) noexcept
 {
   FormatTimeHHMM(buffer, TimeLocal(time, utc_offset));
+}
+
+void
+FormatLocalDateTimeYYYYMMDDHHMM(char *buffer, TimeStamp time,
+                                RoughTimeDelta utc_offset) noexcept
+{
+  const auto duration = std::chrono::duration_cast<std::chrono::system_clock::duration>(
+    TimeLocal(time, utc_offset).ToDuration());
+  const BrokenDateTime date_time{std::chrono::system_clock::time_point{duration}};
+  StringFormat(buffer, 17, "%04u-%02u-%02u %02u:%02u",
+               date_time.year, date_time.month, date_time.day,
+               date_time.hour, date_time.minute);
 }
