c4b0f3fa5831dbe6e5e5eb314cbcb5591ad0bcb6
Simon GH <groundhog@gmx.net>
2026-07-15 01:05:14 +0200

    RasterRenderer: Enable alpha for StretchTo in non-OpenGL path The alpha parameter had been missing in the non-OpenGL versions of the StretchTo method for rendering RawBitmaps to the screen. This commit aligns GL and non-GL paths more closely. Limitation: The combination of alpha and a non-identity transform is not yet supported. This is different in the OpnGL path. For non-OpenGL, the alpha channel takes precedence.
    This feature enables global layer transparency on non-GL
    weather rendering (The OpenGL path had this already)

--- src/Terrain/RasterRenderer.cpp
+++ src/Terrain/RasterRenderer.cpp
@@ -780,6 +780,6 @@ RasterRenderer::Draw([[maybe_unused]] Canvas &canvas,
 #else
   image->StretchTo(PixelSize{height_matrix.GetSize()},
                    canvas, projection.GetScreenSize(),
-                   transparent_white, has_alpha);
+                   transparent_white, has_alpha, alpha);
 #endif
 }
