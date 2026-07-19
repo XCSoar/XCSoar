abc60b0270821bef496fc3bf404e9c7648e31750
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-07-20 01:45:52 +0200

    Dialogs/ComboPicker: Add optional Setup button
    Allow list pickers (weather layer/level/altitude) to offer a Setup action
    that opens Info → Weather.

--- test/src/FakeListPicker.cpp
+++ test/src/FakeListPicker.cpp
@@ -6,7 +6,11 @@
 bool
 ComboPicker([[maybe_unused]] const char *caption,
             [[maybe_unused]] DataField &df,
-            [[maybe_unused]] const char *help_text)
+            [[maybe_unused]] const char *help_text,
+            [[maybe_unused]] const char *extra_caption,
+            bool *extra_selected)
 {
+  if (extra_selected != nullptr)
+    *extra_selected = false;
   return false;
 }
