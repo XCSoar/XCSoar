2f3ac167421ef36d9b79be9bb07976d7c7e790bd
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-08-27 11:54:22 +0000

    Apple/Vibrator: Add haptic feedback implementation for iOS

--- build/main.mk
+++ build/main.mk
@@ -658,6 +658,7 @@ XCSOAR_SOURCES += \
 	$(SRC)/Apple/PathProvider.cpp \
 	$(SRC)/Apple/InternalSensors.cpp \
 	$(SRC)/Apple/KeyboardDetection.cpp \
+	$(SRC)/Apple/Vibrator.cpp \
 	$(SRC)/Device/SmartDeviceSensors.cpp
 endif
 
