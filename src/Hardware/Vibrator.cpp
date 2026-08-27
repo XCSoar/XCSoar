ab47cd8ca4d90c8db635138dc438147455b0ccb3
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-08-27 07:35:44 +0000

    Hardware/Vibrator: Enable HAVE_VIBRATOR on iOS

--- src/Hardware/Vibrator.cpp
+++ src/Hardware/Vibrator.cpp
@@ -3,22 +3,35 @@
 
 #include "Vibrator.hpp"
 
-#ifdef ANDROID
+#ifdef HAVE_VIBRATOR
 
+#ifdef ANDROID
 #include "Android/Vibrator.hpp"
 #include "Android/Main.hpp"
+#include "java/Global.hxx"
+#else
+#include "Apple/Vibrator.hpp"
+#endif
 
 bool
 HaveVibrator() noexcept
 {
+#ifdef ANDROID
   return vibrator != nullptr;
+#else
+  return Apple::HaveHapticFeedback();
+#endif
 }
 
 void
 VibrateShort() noexcept
 {
+#ifdef ANDROID
   if (vibrator != nullptr)
     vibrator->Vibrate(Java::GetEnv(), 25);
+#else
+  Apple::VibrateShort();
+#endif
 }
 
-#endif /* Android */
+#endif /* HAVE_VIBRATOR */
