d152b794e8dcbc236cdb240b9a333ee40faf6810
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-08-27 07:56:54 +0000

    Form/Button: Give haptic feedback only when the button goes down

--- src/Form/Button.cpp
+++ src/Form/Button.cpp
@@ -133,7 +133,8 @@ Button::SetDown(bool _down)
     return;
 
 #ifdef HAVE_VIBRATOR
-  Vibrate(HapticFeedbackType::PRESS);
+  if (_down)
+    Vibrate(HapticFeedbackType::PRESS);
 #endif
 
   down = _down;
