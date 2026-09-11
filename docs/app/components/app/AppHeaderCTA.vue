a3ed6dc12b5be7b9395d205e050a4d59b671fa43
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-09-11 20:40:24 +0000

    Docs: Add xcsoar.org link to header

---
+++ docs/app/components/app/AppHeaderCTA.vue
@@ -0,0 +1,11 @@
+<template>
+    <UButton
+        to="https://xcsoar.org"
+        target="_blank"
+        color="neutral"
+        variant="ghost"
+        icon="i-lucide-globe"
+        label="xcsoar.org"
+        class="hidden sm:inline-flex"
+    />
+</template>
