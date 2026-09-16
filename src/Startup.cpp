4bfdd28815d4cfa4b1f47604b1cc562ccbd260ec
Simon GH <groundhog@gmx.net>
2026-09-17 01:59:39 +0200

    Startup.cpp: Fix init.lua startup log message The log message for the missing optional init.lua file did not show the actual path as intended, but only the placeholder %s.
    (cherry picked from commit da0dd20aa9b971dc1c227e48aeda2756b26f2915)

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -190,7 +190,7 @@ AfterStartup()
     if (File::Exists(init_path))
       Lua::StartFile(init_path);
     else
-      LogDebug("Optional %s not found", init_path.c_str());
+      LogDebug("Optional {} not found", init_path.c_str());
   } catch (...) {
     LogError(std::current_exception());
   }
