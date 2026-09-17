e761d30adbacc7d8c75c1af06da45a43dc0af525
Kai Horstmann <horstmannkai@hotmail.com>
2026-09-18 00:54:03 +0200

    DRM/KMS: Prevent rendering while a page flip is in progress
    The sun4i display driver for older Allwinner SoCs has a quirk when
    page flips are executed non-blocking.
    When OpenGL rendering occurs while a page flip is on-going, i.e.
    waiting for the vertical gap, the page flip is not finished.
    When continuous re-draws of the screen happen during continuous mouse
    dragging no screen refresh happens because the rendered scene in the
    back-buffer is not flipped to the front until there is a sufficient
    break in GL rendering allows the display driver to finish the flip.
    This quirk affects particularly Cubieboard 2 which is the staple for
    OpenVario.
    
    This commit splits the method TopCanvas::Flip().
    
    The part which deals with checking if a page flip is pending is
    split off into \n
    bool *TopCanvas::CheckAndFinishPendingFlip()*\n
    When a flip is pending it tries to process DRM events which indicate
    the end of the on-going flip.
    It returns true when no page flip was pending or a pending flip could be finished.
    
    The remaining part of TopCanvas::Flip() calls CheckAndFinishPendingFlip().
    When :CheckAndFinishPendingFlip() returns true it resumes swapping EGL buffers,
    locks the new front buffer, and schedules the page flip to the new front
    frame buffer.
    
    The actual functional change of this commit is in TopWindow::Refresh():
    Here TopCanvas::CheckAndFinishPendingFlip() is called at the beginning.
    If it returns false the method returns immediately, preventing OpenGL
    rendering while a page flip is on-going.
    This way display refresh is ensured also during continuous mouse dragging.
    With this commit smooth mouse movement is achieved even when a Cubieboard
    is connected to a monitor with 1920x1200 resolution.

--- src/ui/canvas/egl/TopCanvas.cpp
+++ src/ui/canvas/egl/TopCanvas.cpp
@@ -145,6 +145,43 @@ TopCanvas::ReleaseSurface() noexcept
   surface = EGL_NO_SURFACE;
 }
 
+#ifdef MESA_KMS
+bool TopCanvas::CheckAndFinishPendingFlip() {
+	const FileDescriptor dri_fd = display.GetDriFD();
+	
+	const auto process_drm_events = [&]() {
+	  while (true) {
+	    const int handle_event_ret = drmHandleEvent(dri_fd.Get(), &evctx);
+	    if (handle_event_ret == 0)
+		continue;
+	
+	    if (errno == EAGAIN)
+	      break;
+	
+	    fprintf(stderr, "drmHandleEvent() failed: %d\n", handle_event_ret);
+	    exit(EXIT_FAILURE);
+	  }
+	};
+	
+	process_drm_events();
+	
+	if (page_flip_pending) {
+	  if (!page_flip_finished) {
+	    return false;
+      }
+	  page_flip_pending = false;
+	  page_flip_finished = false;
+	
+	  if (current_bo != nullptr)
+	    gbm_surface_release_buffer(gbm_surface, current_bo);
+	
+	  current_bo = next_bo;
+	}
+	
+	return true;
+}
+#endif /* #ifdef MESA_KMS */
+
 void
 TopCanvas::Flip()
 {
@@ -152,35 +189,8 @@ TopCanvas::Flip()
 
 #ifdef MESA_KMS
   const FileDescriptor dri_fd = display.GetDriFD();
-
-  const auto process_drm_events = [&]() {
-    while (true) {
-      const int handle_event_ret = drmHandleEvent(dri_fd.Get(), &evctx);
-      if (handle_event_ret == 0)
-        continue;
-
-      if (errno == EAGAIN)
-        break;
-
-      fprintf(stderr, "drmHandleEvent() failed: %d\n", handle_event_ret);
-      exit(EXIT_FAILURE);
-    }
-  };
-
-  process_drm_events();
-
-  if (page_flip_pending) {
-    if (!page_flip_finished)
-      return;
-
-    page_flip_pending = false;
-    page_flip_finished = false;
-
-    if (current_bo != nullptr)
-      gbm_surface_release_buffer(gbm_surface, current_bo);
-
-    current_bo = next_bo;
-    next_bo = nullptr;
+  if (!CheckAndFinishPendingFlip()) {
+    return;
   }
 #endif
 
@@ -220,7 +230,7 @@ TopCanvas::Flip()
                                              &page_flip_finished);
     if (0 != page_flip_ret) {
       fprintf(stderr, "drmModePageFlip() failed: %d\n", page_flip_ret);
-      exit(EXIT_FAILURE);
+      exit(EXIT_FAILURE);	
     }
     next_bo = new_bo;
     page_flip_pending = true;
