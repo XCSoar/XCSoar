e6e5931c8ef841373b9af2524eead997d29d97cc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-09-24 18:56:01 +0000

    Renderer/TextInBox: Add the PILL label shape

--- src/Renderer/LabelShape.hpp
+++ src/Renderer/LabelShape.hpp
@@ -12,4 +12,10 @@ enum class LabelShape : uint8_t {
   OUTLINED_INVERTED,
   ROUNDED_WHITE,
   ROUNDED_BLACK,
+
+  /**
+   * White background with fully rounded ends, slightly transparent,
+   * floating above the map on a soft shadow.
+   */
+  PILL,
 };
