9c2d58889b118ad442b92fdb85507c37f8e63276
Philipp Wollschlegel <folken@kabelsalat.ch>
2026-09-26 12:02:44 +0200

    BatteryTimer: Do not quit the simulator on low battery
    The 5% shutdown was added so a PDA running the simulator would not
    drain its battery. A low-battery reminder remains; the simulator
    should keep running, as a real flight already does.

--- src/BatteryTimer.cpp
+++ src/BatteryTimer.cpp
@@ -5,9 +5,6 @@
 #include "Hardware/Battery.hpp"
 #include "Hardware/PowerInfo.hpp"
 #include "Hardware/PowerGlobal.hpp"
-#include "UIActions.hpp"
-#include "LogFile.hpp"
-#include "Simulator.hpp"
 #include "Language/Language.hpp"
 #include "Message.hpp"
 
@@ -28,32 +25,16 @@ BatteryTimer::Process()
   const auto &battery = info.battery;
   const auto &external = info.external;
 
-  /* Battery status - simulator only - for safety of battery data
-     note: Simulator only - more important to keep running in your plane
-  */
-
-  // JMW, maybe this should be active always...
-  // we don't want the PDA to be completely depleted.
-
   if (external.status == Power::ExternalInfo::Status::OFF) {
-    if (!battery.remaining_percent || *battery.remaining_percent <= 0) {
+    if (!battery.remaining_percent || *battery.remaining_percent <= 0)
       return;
-    }
 
-    if (is_simulator() && battery.remaining_percent &&
-        *battery.remaining_percent < BATTERY_EXIT) {
-      LogString("Battery low exit...");
-      // TODO feature: Warning message on battery shutdown
-      UIActions::SignalShutdown(true);
+    if (*battery.remaining_percent < BATTERY_WARNING) {
+      if (last_warning.CheckUpdate(BATTERY_REMINDER))
+        // TODO feature: Show the user what the batt status is.
+        Message::AddMessage(_("Battery low"));
     } else {
-      if (battery.remaining_percent &&
-          *battery.remaining_percent < BATTERY_WARNING) {
-        if (last_warning.CheckUpdate(BATTERY_REMINDER))
-          // TODO feature: Show the user what the batt status is.
-          Message::AddMessage(_("Battery low"));
-      } else {
-        last_warning.Reset();
-      }
+      last_warning.Reset();
     }
   }
 #endif /* HAVE_BATTERY */
